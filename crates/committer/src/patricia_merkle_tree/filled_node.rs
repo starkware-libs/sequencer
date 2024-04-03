002befc73464efd28186b125c33f950e6699ef51
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2024-04-03 16:00:33 +0300

    fix: implement LeafDataTrait for LeafData (#13)

--- crates/committer/src/patricia_merkle_tree/filled_node.rs
+++ crates/committer/src/patricia_merkle_tree/filled_node.rs
@@ -41,3 +41,21 @@ pub(crate) enum LeafData {
         nonce: Nonce,
     },
 }
+
+impl LeafDataTrait for LeafData {
+    fn is_empty(&self) -> bool {
+        match self {
+            LeafData::StorageValue(value) => *value == Felt::ZERO,
+            LeafData::CompiledClassHash(class_hash) => class_hash.0 == Felt::ZERO,
+            LeafData::StateTreeTuple {
+                class_hash,
+                contract_state_root_hash,
+                nonce,
+            } => {
+                nonce.0 == Felt::ZERO
+                    && class_hash.0 == Felt::ZERO
+                    && *contract_state_root_hash == Felt::ZERO
+            }
+        }
+    }
+}
