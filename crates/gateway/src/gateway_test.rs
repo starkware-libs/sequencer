33475f5bb7e817472be69d9f65540a7f38a6fe37
giladchase <gilad@starkware.co>
2024-04-08 13:43:22 +0300

    refactor: simplify test (#26)
    refactor test:
    - dont need to pass through BufReader
    - extract cast to bytes into test util
    - cast string lossy to save unwrap
    - spacing
    - use better str compare
    
    chore:
    - `hyper` version is way too old, bump it.
    - add `pretty_assertions` for nicer diff.
    
    Co-Authored-By: Gilad Chase <gilad@starkware.com>

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,9 +1,12 @@
 use crate::gateway::add_transaction;
-use axum::{body::HttpBody, response::IntoResponse};
+use axum::{
+    body::{Bytes, HttpBody},
+    response::{IntoResponse, Response},
+};
+use pretty_assertions::assert_str_eq;
 use rstest::rstest;
 use starknet_api::external_transaction::ExternalTransaction;
 use std::fs::File;
-use std::io::BufReader;
 
 // TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
 // into JSON for testing.
@@ -16,13 +19,15 @@ use std::io::BufReader;
 #[case("./src/json_files_for_testing/invoke_v3.json", "INVOKE")]
 #[tokio::test]
 async fn test_add_transaction(#[case] json_file_path: &str, #[case] expected_response: &str) {
-    let file = File::open(json_file_path).unwrap();
-    let reader = BufReader::new(file);
-    let transaction: ExternalTransaction = serde_json::from_reader(reader).unwrap();
-    let response = add_transaction(transaction.into()).await.into_response();
-    let response_bytes = response.into_body().collect().await.unwrap().to_bytes();
-    assert_eq!(
-        &String::from_utf8(response_bytes.to_vec()).unwrap(),
-        expected_response
-    );
+    let json_file = File::open(json_file_path).unwrap();
+    let tx: ExternalTransaction = serde_json::from_reader(json_file).unwrap();
+
+    let response = add_transaction(tx.into()).await.into_response();
+    let response_bytes = &to_bytes(response).await;
+
+    assert_str_eq!(&String::from_utf8_lossy(response_bytes), expected_response);
+}
+
+async fn to_bytes(res: Response) -> Bytes {
+    res.into_body().collect().await.unwrap().to_bytes()
 }
