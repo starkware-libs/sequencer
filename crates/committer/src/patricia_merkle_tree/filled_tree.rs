9c29d0443154933581363d3536fe06f7fd60deb7
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2024-04-09 15:29:29 +0300

    fix: change FilledTree signature for interior mutability (#24)

--- crates/committer/src/patricia_merkle_tree/filled_tree.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 use crate::patricia_merkle_tree::filled_node::FilledNode;
 use crate::patricia_merkle_tree::types::{LeafDataTrait, NodeIndex};
@@ -7,5 +8,22 @@ use crate::patricia_merkle_tree::types::{LeafDataTrait, NodeIndex};
 /// FilledTree consists of all nodes which were modified in the update, including their updated
 /// data and hashes.
 pub(crate) trait FilledTree<L: LeafDataTrait> {
-    fn get_all_nodes(&self) -> HashMap<NodeIndex, &FilledNode<L>>;
+    fn get_all_nodes(&self) -> &HashMap<NodeIndex, Mutex<FilledNode<L>>>;
+}
+
+pub(crate) struct FilledTreeImpl<L: LeafDataTrait> {
+    tree_map: HashMap<NodeIndex, Mutex<FilledNode<L>>>,
+}
+
+#[allow(dead_code)]
+impl<L: LeafDataTrait> FilledTreeImpl<L> {
+    pub(crate) fn new(tree_map: HashMap<NodeIndex, Mutex<FilledNode<L>>>) -> Self {
+        Self { tree_map }
+    }
+}
+
+impl<L: LeafDataTrait> FilledTree<L> for FilledTreeImpl<L> {
+    fn get_all_nodes(&self) -> &HashMap<NodeIndex, Mutex<FilledNode<L>>> {
+        &self.tree_map
+    }
 }
