b2ab9f7c981da1990337bb165305fa7c594c99dc
YaelD <70628564+Yael-Starkware@users.noreply.github.com>
2024-05-05 13:23:13 +0300

    feat: rpc state reader (#64)

---
+++ crates/gateway/src/rpc_objects.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use starknet_api::{block::BlockNumber, core::ContractAddress};
+
+// Starknet Spec error codes:
+// TODO(yael 30/4/2024): consider turning these into an enum.
+pub const RPC_ERROR_BLOCK_NOT_FOUND: u16 = 24;
+pub const RPC_ERROR_CONTRACT_ADDRESS_NOT_FOUND: u16 = 20;
+
+#[derive(Deserialize, Serialize)]
+pub enum BlockId {
+    #[serde(rename = "block_number")]
+    Number(BlockNumber),
+    // There are additional options in the spec that are not implemented here
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetNonceParams {
+    pub block_id: BlockId,
+    pub contract_address: ContractAddress,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum RpcResponse {
+    Success(RpcSuccessResponse),
+    Error(RpcErrorResponse),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcSuccessResponse {
+    pub jsonrpc: Option<String>,
+    pub result: Value,
+    pub id: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcErrorResponse {
+    pub jsonrpc: Option<String>,
+    pub error: RpcSpecError,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcSpecError {
+    pub code: u16,
+    pub message: String,
+}
---
+++ crates/gateway/src/rpc_state_reader.rs
@@ -0,0 +1,110 @@
+use blockifier::execution::contract_class::ContractClass;
+use blockifier::state::errors::StateError;
+use blockifier::state::state_api::{StateReader, StateResult};
+use reqwest::blocking::Client as BlockingClient;
+use serde_json::{json, Value};
+use starknet_api::block::BlockNumber;
+use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::StorageKey;
+use url::Url;
+
+use crate::rpc_objects::{
+    BlockId, GetNonceParams, RpcResponse, RPC_ERROR_BLOCK_NOT_FOUND,
+    RPC_ERROR_CONTRACT_ADDRESS_NOT_FOUND,
+};
+
+pub struct RpcStateReader {
+    pub url: Url,
+    pub json_rpc_version: String,
+    pub block_number: BlockNumber,
+}
+
+impl RpcStateReader {
+    // Note: This function is blocking though it is sending a request to the rpc server and waiting
+    // for the response.
+    pub fn send_rpc_request(&self, request_body: serde_json::Value) -> Result<Value, StateError> {
+        let client = BlockingClient::new();
+        let response = client
+            .post(self.url.clone())
+            .header("Content-Type", "application/json")
+            .json(&request_body)
+            .send()
+            .map_err(|e| {
+                StateError::StateReadError(format!("Rpc request failed with error {:?}", e))
+            })?;
+
+        if !response.status().is_success() {
+            return Err(StateError::StateReadError(format!(
+                "RPC ERROR, code {}",
+                response.status()
+            )));
+        }
+
+        let rpc_response: RpcResponse = response.json::<RpcResponse>().map_err(|e| {
+            StateError::StateReadError(format!("Couldn't parse json rpc response {}", e))
+        })?;
+
+        match rpc_response {
+            RpcResponse::Success(rpc_success_response) => Ok(rpc_success_response.result),
+            RpcResponse::Error(rpc_error_response) => match rpc_error_response.error.code {
+                RPC_ERROR_BLOCK_NOT_FOUND => Err(StateError::StateReadError(format!(
+                    "Block not found, request: {}",
+                    request_body
+                ))),
+                RPC_ERROR_CONTRACT_ADDRESS_NOT_FOUND => Err(StateError::StateReadError(format!(
+                    "Contract address not found, request: {}",
+                    request_body
+                ))),
+                _ => Err(StateError::StateReadError(format!(
+                    "Unexpected error code {}",
+                    rpc_error_response.error.code
+                ))),
+            },
+        }
+    }
+}
+
+impl StateReader for RpcStateReader {
+    #[allow(unused_variables)]
+    fn get_storage_at(
+        &self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+    ) -> StateResult<StarkFelt> {
+        todo!()
+    }
+
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> StateResult<Nonce> {
+        let get_nonce_params = GetNonceParams {
+            block_id: BlockId::Number(self.block_number),
+            contract_address,
+        };
+        let request_body = json!({
+            "jsonrpc": self.json_rpc_version,
+            "id": 0,
+            "method": "starknet_getNonce",
+            "params": json!(get_nonce_params),
+        });
+
+        let result = self.send_rpc_request(request_body)?;
+        let nonce: Nonce = serde_json::from_value(result)
+            .map_err(|_| StateError::StateReadError("Bad rpc result".to_string()))?;
+        Ok(nonce)
+    }
+
+    #[allow(unused_variables)]
+    fn get_compiled_contract_class(&self, class_hash: ClassHash) -> StateResult<ContractClass> {
+        todo!()
+    }
+
+    #[allow(unused_variables)]
+    fn get_class_hash_at(&self, contract_address: ContractAddress) -> StateResult<ClassHash> {
+        todo!()
+    }
+
+    #[allow(unused_variables)]
+    fn get_compiled_class_hash(&self, class_hash: ClassHash) -> StateResult<CompiledClassHash> {
+        todo!()
+    }
+}
