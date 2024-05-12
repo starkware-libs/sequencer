2f8a3495c7548eec47ee46fa492319c20c479e26
Arnon Hod <arnon@starkware.co>
2024-05-12 12:01:29 +0300

    test: add test fix infra for gateway network connection config (#111)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/mempool/111)
    <!-- Reviewable:end -->

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::Command;
+use papyrus_config::dumping::SerializeConfig;
 use papyrus_config::loading::load_and_process_config;
+use serde::Deserialize;
 use validator::Validate;
 
 use crate::config::GatewayNetworkConfig;
@@ -10,18 +12,39 @@ use crate::config::GatewayNetworkConfig;
 const TEST_FILES_FOLDER: &str = "./src/json_files_for_testing";
 const NETWORK_CONFIG_FILE: &str = "gateway_network_config.json";
 
-fn get_config_file(file_name: &str) -> Result<GatewayNetworkConfig, papyrus_config::ConfigError> {
-    let config_file = File::open(Path::new(TEST_FILES_FOLDER).join(file_name)).unwrap();
-    load_and_process_config::<GatewayNetworkConfig>(config_file, Command::new(""), vec![])
+fn get_config_file_path(file_name: &str) -> PathBuf {
+    Path::new(TEST_FILES_FOLDER).join(file_name)
+}
+
+fn get_config_from_file<T: for<'a> Deserialize<'a>>(
+    file_path: PathBuf,
+) -> Result<T, papyrus_config::ConfigError> {
+    let config_file = File::open(file_path).unwrap();
+    load_and_process_config(config_file, Command::new(""), vec![])
+}
+
+/// Read the valid config file and validate its content.
+fn test_valid_network_config_body(fix: bool) {
+    let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
+
+    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
+    if fix {
+        expected_config.dump_to_file(&vec![], file_path.to_str().unwrap()).unwrap();
+    }
+    let loaded_config = get_config_from_file::<GatewayNetworkConfig>(file_path).unwrap();
+
+    assert!(loaded_config.validate().is_ok());
+    assert_eq!(loaded_config, expected_config);
 }
 
 #[test]
 fn test_valid_config() {
-    // Read the valid config file and validate its content.
-    let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
+    test_valid_network_config_body(false);
+}
 
-    let loaded_config = get_config_file(NETWORK_CONFIG_FILE).unwrap();
-
-    assert!(loaded_config.validate().is_ok());
-    assert_eq!(loaded_config, expected_config);
+#[test]
+#[ignore]
+/// Fix the config file for test_valid_config. Run with 'cargo test -- --ignored'.
+fn fix_test_valid_config() {
+    test_valid_network_config_body(true);
 }
