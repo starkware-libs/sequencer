92bd23df5c818c6d7aaef029e20cf448d898b9e2
Uriel Korach <uriel@starkware.co>
2024-05-29 15:03:50 +0300

    test: add empty test for client/server rpc implementation

---
+++ crates/mempool_infra/src/component_client_rpc.rs
@@ -0,0 +1,3 @@
+#[cfg(test)]
+#[path = "component_server_client_rpc_test.rs"]
+mod component_server_client_rpc_test;
--- crates/mempool_infra/src/lib.rs
+++ crates/mempool_infra/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod component_client;
+pub mod component_client_rpc;
 pub mod component_runner;
 pub mod component_server;
 pub mod network_component;
