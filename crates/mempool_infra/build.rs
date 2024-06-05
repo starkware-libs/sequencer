dbc0b15a406bdb485ca3535e4559cbc7f5a29cd8
Uriel Korach <uriel@starkware.co>
2024-06-05 16:31:43 +0300

    test: test components communication using tonic rpcs

--- crates/mempool_infra/build.rs
+++ crates/mempool_infra/build.rs
@@ -1,18 +1,23 @@
-use std::env::set_var;
+use std::env::{set_var, var};
 
 use protoc_prebuilt::init;
 use tonic_build::{configure, Builder};
 
 fn main() {
-    set_var("PROTOC_PREBUILT_NOT_ADD_GITHUB_TOKEN", "true");
-    let (protoc_bin, _) = init("27.0").unwrap();
-    set_var("PROTOC", protoc_bin);
+    if var("PROFILE").unwrap() == "debug" {
+        set_var("PROTOC_PREBUILT_NOT_ADD_GITHUB_TOKEN", "true");
+        let (protoc_bin, _) = init("27.0").unwrap();
+        set_var("PROTOC", protoc_bin);
 
-    let builder: Builder = configure();
-    builder
-        .compile(
-            &["proto/component_a_service.proto", "proto/component_b_service.proto"],
-            &["proto"],
-        )
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+        let builder: Builder = configure();
+        builder
+            .compile(
+                &[
+                    "tests/common/proto/component_a_service.proto",
+                    "tests/common/proto/component_b_service.proto",
+                ],
+                &["tests/common/proto"],
+            )
+            .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+    }
 }
--- crates/mempool_infra/src/lib.rs
+++ crates/mempool_infra/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod component_client;
-pub mod component_client_rpc;
 pub mod component_definitions;
 pub mod component_runner;
 pub mod component_server;
