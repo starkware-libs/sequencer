241dd77460c5253599aa9c22422a6afcf1d1eed0
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2024-06-09 16:38:27 +0300

    fix: patricia should be 251 (#210)

--- crates/committer/src/patricia_merkle_tree/node_data/leaf_serde.rs
+++ crates/committer/src/patricia_merkle_tree/node_data/leaf_serde.rs
@@ -1,4 +1,5 @@
 use crate::patricia_merkle_tree::node_data::leaf::{ContractState, LeafDataImpl};
+use crate::patricia_merkle_tree::types::SubTreeHeight;
 use crate::storage::db_object::DBObject;
 use crate::storage::storage_trait::{StoragePrefix, StorageValue};
 
@@ -23,11 +24,11 @@ impl DBObject for LeafDataImpl {
                 storage_root_hash,
                 nonce,
             }) => {
-                // TODO(Aviv, 8/5/2024): Use height from the input.
                 let json_string = format!(
-                    r#"{{"contract_hash": "{}", "storage_commitment_tree": {{"root": "{}", "height": 252}}, "nonce": "{}"}}"#,
+                    r#"{{"contract_hash": "{}", "storage_commitment_tree": {{"root": "{}", "height": {}}}, "nonce": "{}"}}"#,
                     class_hash.0.to_fixed_hex_string(),
                     storage_root_hash.0.to_fixed_hex_string(),
+                    SubTreeHeight::ACTUAL_HEIGHT,
                     nonce.0.to_hex(),
                 );
                 StorageValue(json_string.into_bytes())
