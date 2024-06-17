starkware-libs/mempool

chore: split communication-related functionality from mempool and mempool-types modules
commit-id:8c4b0394

crates/gateway/src/gateway.rs
@@ -7,7 +7,8 @@ use axum::routing::{get, post};
 use axum::{Json, Router};
 use starknet_api::external_transaction::ExternalTransaction;
 use starknet_api::transaction::TransactionHash;
-use starknet_mempool_types::mempool_types::{Account, MempoolClient, MempoolInput};
+use starknet_mempool_types::mempool_communication_types::MempoolClient;
+use starknet_mempool_types::mempool_types::{Account, MempoolInput};
 
 use crate::config::{GatewayConfig, GatewayNetworkConfig};
 use crate::errors::{GatewayError, GatewayRunError};

crates/mempool/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod mempool;
+pub mod mempool_communication;
 pub(crate) mod priority_queue;

crates/mempool_types/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod errors;
+pub mod mempool_communication_types;
 pub mod mempool_types;

crates/tests-integration/tests/end_to_end_test.rs
@@ -11,9 +11,10 @@ use starknet_gateway::config::{
 use starknet_gateway::gateway::Gateway;
 use starknet_gateway::starknet_api_test_utils::invoke_tx;
 use starknet_gateway::state_reader_test_utils::test_state_reader_factory;
-use starknet_mempool::mempool::{create_mempool_server, Mempool};
+use starknet_mempool::mempool::Mempool;
+use starknet_mempool::mempool_communication::create_mempool_server;
 use starknet_mempool_integration_tests::integration_test_utils::GatewayClient;
-use starknet_mempool_types::mempool_types::{
+use starknet_mempool_types::mempool_communication_types::{
     MempoolClient, MempoolClientImpl, MempoolRequestAndResponseSender,
 };
 use tokio::sync::mpsc::channel;

starkware-libs/sequencer

crates/gateway/src/gateway.rs
@@ -7,7 +7,8 @@ use axum::routing::{get, post};
 use axum::{Json, Router};
 use starknet_api::external_transaction::ExternalTransaction;
 use starknet_api::transaction::TransactionHash;
-use starknet_mempool_types::mempool_types::{Account, MempoolInput, SharedMempoolClient};
+use starknet_mempool_types::communication::SharedMempoolClient;
+use starknet_mempool_types::mempool_types::{Account, MempoolInput};
 
 use crate::config::{GatewayConfig, GatewayNetworkConfig};
 use crate::errors::{GatewayError, GatewayRunError};

crates/mempool/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod communication;
 pub mod mempool;
 pub(crate) mod priority_queue;

crates/mempool_types/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod communication;
 pub mod errors;
 pub mod mempool_types;

crates/tests-integration/tests/end_to_end_test.rs
@@ -11,9 +11,10 @@ use starknet_gateway::config::{
 use starknet_gateway::gateway::Gateway;
 use starknet_gateway::starknet_api_test_utils::invoke_tx;
 use starknet_gateway::state_reader_test_utils::rpc_test_state_reader_factory;
-use starknet_mempool::mempool::{create_mempool_server, Mempool};
+use starknet_mempool::communication::create_mempool_server;
+use starknet_mempool::mempool::Mempool;
 use starknet_mempool_integration_tests::integration_test_utils::GatewayClient;
-use starknet_mempool_types::mempool_types::{
+use starknet_mempool_types::communication::{
     MempoolClient, MempoolClientImpl, MempoolRequestAndResponseSender, SharedMempoolClient,
 };
 use tokio::sync::mpsc::channel;