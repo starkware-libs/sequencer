4b8f762d4eb53edbca2af03de9c8b378fa0fbf5c
Arnon Hod <arnon@starkware.co>
2024-07-01 13:57:37 +0300

    chore: move faulty account sierra file to test utils (#327)

--- crates/starknet_sierra_compile/src/compile_test.rs
+++ crates/starknet_sierra_compile/src/compile_test.rs
@@ -1,17 +1,17 @@
+use std::env;
 use std::path::Path;
 
 use assert_matches::assert_matches;
 use cairo_lang_starknet_classes::allowed_libfuncs::AllowedLibfuncsError;
+use test_utils::{get_absolute_path, FAULTY_ACCOUNT_CLASS_FILE, TEST_FILES_FOLDER};
 
 use crate::compile::{compile_sierra_to_casm, CompilationUtilError};
 use crate::test_utils::contract_class_from_file;
 
-const FAULTY_ACCOUNT_SIERRA_FILE: &str = "account_faulty.sierra.json";
-const TEST_FILES_FOLDER: &str = "./tests/fixtures";
-
 #[test]
 fn test_compile_sierra_to_casm() {
-    let sierra_path = &Path::new(TEST_FILES_FOLDER).join(FAULTY_ACCOUNT_SIERRA_FILE);
+    env::set_current_dir(get_absolute_path(TEST_FILES_FOLDER)).expect("Failed to set current dir.");
+    let sierra_path = Path::new(FAULTY_ACCOUNT_CLASS_FILE);
     let expected_casm_contract_length = 72304;
 
     let contract_class = contract_class_from_file(sierra_path);
@@ -24,7 +24,8 @@ fn test_compile_sierra_to_casm() {
 // TODO(Arni, 1/5/2024): Add a test for panic result test.
 #[test]
 fn test_negative_flow_compile_sierra_to_casm() {
-    let sierra_path = &Path::new(TEST_FILES_FOLDER).join(FAULTY_ACCOUNT_SIERRA_FILE);
+    env::set_current_dir(get_absolute_path(TEST_FILES_FOLDER)).expect("Failed to set current dir.");
+    let sierra_path = Path::new(FAULTY_ACCOUNT_CLASS_FILE);
 
     let mut contract_class = contract_class_from_file(sierra_path);
     // Truncate the sierra program to trigger an error.
