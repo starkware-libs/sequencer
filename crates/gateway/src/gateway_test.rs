starkware-libs/mempool

feat: adding empty server and refactoring ComponentServer
commit-id:689882a6

crates/gateway/src/gateway_test.rs
@@ -11,6 +11,7 @@ use starknet_api::rpc_transaction::RPCTransaction;
 use starknet_api::transaction::TransactionHash;
 use starknet_mempool::communication::create_mempool_server;
 use starknet_mempool::mempool::Mempool;
+use starknet_mempool_infra::component_server::CommunicationServer;
 use starknet_mempool_types::communication::{MempoolClientImpl, MempoolRequestAndResponseSender};
 use tokio::sync::mpsc::channel;
 use tokio::task;
@@ -76,6 +77,7 @@ async fn test_add_tx(
 ) {
     // TODO(Tsabary): wrap creation of channels in dedicated functions, take channel capacity from
     // config.
+
     let (tx_mempool, rx_mempool) =
         channel::<MempoolRequestAndResponseSender>(MEMPOOL_INVOCATIONS_QUEUE_SIZE);
     let mut mempool_server = create_mempool_server(mempool, rx_mempool);

crates/mempool_infra/src/lib.rs
@@ -2,3 +2,4 @@ pub mod component_client;
 pub mod component_definitions;
 pub mod component_runner;
 pub mod component_server;
+pub mod empty_server;

crates/mempool_infra/tests/component_server_client_test.rs
@@ -6,7 +6,7 @@ use starknet_mempool_infra::component_client::ComponentClient;
 use starknet_mempool_infra::component_definitions::{
     ComponentRequestAndResponseSender, ComponentRequestHandler,
 };
-use starknet_mempool_infra::component_server::ComponentServer;
+use starknet_mempool_infra::component_server::{CommunicationServer, ComponentServer};
 use tokio::sync::mpsc::{channel, Sender};
 use tokio::task;
 

crates/tests-integration/src/integration_test_setup.rs
@@ -7,6 +7,7 @@ use starknet_gateway::config::GatewayNetworkConfig;
 use starknet_gateway::errors::GatewayError;
 use starknet_mempool::communication::create_mempool_server;
 use starknet_mempool::mempool::Mempool;
+use starknet_mempool_infra::component_server::CommunicationServer;
 use starknet_mempool_types::communication::{
     MempoolClient, MempoolClientImpl, MempoolRequestAndResponseSender,
 };

starkware-libs/sequencer

crates/gateway/src/gateway_test.rs
@@ -15,6 +15,7 @@ use starknet_api::rpc_transaction::{RPCDeclareTransaction, RPCTransaction};
 use starknet_api::transaction::TransactionHash;
 use starknet_mempool::communication::create_mempool_server;
 use starknet_mempool::mempool::Mempool;
+use starknet_mempool_infra::component_server::ComponentServerStarter;
 use starknet_mempool_types::communication::{MempoolClientImpl, MempoolRequestAndResponseSender};
 use starknet_sierra_compile::errors::CompilationUtilError;
 use test_utils::starknet_api_test_utils::{declare_tx, deploy_account_tx, invoke_tx};

crates/gateway/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod communication;
 pub mod compiler_version;
 pub mod config;
 pub mod errors;

crates/mempool_infra/tests/component_server_client_test.rs
@@ -6,7 +6,7 @@ use starknet_mempool_infra::component_client::ComponentClient;
 use starknet_mempool_infra::component_definitions::{
     ComponentRequestAndResponseSender, ComponentRequestHandler,
 };
-use starknet_mempool_infra::component_server::ComponentServer;
+use starknet_mempool_infra::component_server::{ComponentServer, ComponentServerStarter};
 use tokio::sync::mpsc::{channel, Sender};
 use tokio::task;
 

crates/tests-integration/src/integration_test_setup.rs
@@ -7,6 +7,7 @@ use starknet_gateway::config::GatewayNetworkConfig;
 use starknet_gateway::errors::GatewayError;
 use starknet_mempool::communication::create_mempool_server;
 use starknet_mempool::mempool::Mempool;
+use starknet_mempool_infra::component_server::ComponentServerStarter;
 use starknet_mempool_types::communication::{
     MempoolClient, MempoolClientImpl, MempoolRequestAndResponseSender,
 };