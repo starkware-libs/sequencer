cbc4f67a61c5e75b769be418b42f62b3acf76ce5
Lev Roitman <lev@starkware.co>
2024-07-08 14:17:13 +0300

    refactor: move trace configuration to infra crate
    commit-id:cf4a80d3

--- crates/mempool_node/src/main.rs
+++ crates/mempool_node/src/main.rs
@@ -3,26 +3,12 @@ use std::process::exit;
 
 use papyrus_config::validators::config_validate;
 use papyrus_config::ConfigError;
+use starknet_mempool_infra::trace_util::configure_tracing;
 use starknet_mempool_node::communication::{create_node_channels, create_node_clients};
 use starknet_mempool_node::components::create_components;
 use starknet_mempool_node::config::MempoolNodeConfig;
 use starknet_mempool_node::servers::{create_servers, run_server_components};
-use tracing::metadata::LevelFilter;
 use tracing::{error, info};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::{fmt, EnvFilter};
-
-const DEFAULT_LEVEL: LevelFilter = LevelFilter::INFO;
-
-fn configure_tracing() {
-    let fmt_layer = fmt::layer().compact().with_target(false);
-    let level_filter_layer =
-        EnvFilter::builder().with_default_directive(DEFAULT_LEVEL.into()).from_env_lossy();
-
-    // This sets a single subscriber to all of the threads. We may want to implement different
-    // subscriber for some threads and use set_global_default instead of init.
-    tracing_subscriber::registry().with(fmt_layer).with(level_filter_layer).init();
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
