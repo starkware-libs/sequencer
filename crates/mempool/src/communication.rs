starkware-libs/mempool

feat: adding component start(initialization) to the server start
commit-id:77f186d4

crates/mempool/src/communication.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use starknet_mempool_infra::component_definitions::ComponentRequestHandler;
+use starknet_mempool_infra::component_runner::ComponentRunner;
 use starknet_mempool_infra::component_server::ComponentServer;
 use starknet_mempool_types::communication::{
     MempoolRequest, MempoolRequestAndResponseSender, MempoolResponse,
@@ -52,3 +53,6 @@ impl ComponentRequestHandler<MempoolRequest, MempoolResponse> for MempoolCommuni
         }
     }
 }
+
+#[async_trait]
+impl ComponentRunner for MempoolCommunicationWrapper {}

crates/mempool_infra/tests/common/mod.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use starknet_mempool_infra::component_client::ClientResult;
-
+use starknet_mempool_infra::component_runner::ComponentRunner;
 pub(crate) type ValueA = u32;
 pub(crate) type ValueB = u8;
 
@@ -55,6 +55,9 @@ impl ComponentA {
     }
 }
 
+#[async_trait]
+impl ComponentRunner for ComponentA {}
+
 pub(crate) struct ComponentB {
     value: ValueB,
     _a: Box<dyn ComponentAClientTrait>,
@@ -69,3 +72,6 @@ impl ComponentB {
         self.value
     }
 }
+
+#[async_trait]
+impl ComponentRunner for ComponentB {}

starkware-libs/sequencer

crates/mempool/src/communication.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use starknet_mempool_infra::component_definitions::ComponentRequestHandler;
+use starknet_mempool_infra::component_runner::ComponentStarter;
 use starknet_mempool_infra::component_server::ComponentServer;
 use starknet_mempool_types::communication::{
     MempoolRequest, MempoolRequestAndResponseSender, MempoolResponse,
@@ -52,3 +53,6 @@ impl ComponentRequestHandler<MempoolRequest, MempoolResponse> for MempoolCommuni
         }
     }
 }
+
+#[async_trait]
+impl ComponentStarter for MempoolCommunicationWrapper {}

crates/mempool_infra/tests/common/mod.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use starknet_mempool_infra::component_client::ClientResult;
+use starknet_mempool_infra::component_runner::ComponentStarter;
 
 pub(crate) type ValueA = u32;
 pub(crate) type ValueB = u8;
@@ -55,6 +56,9 @@ impl ComponentA {
     }
 }
 
+#[async_trait]
+impl ComponentStarter for ComponentA {}
+
 pub(crate) struct ComponentB {
     value: ValueB,
     _a: Box<dyn ComponentAClientTrait>,
@@ -69,3 +73,6 @@ impl ComponentB {
         self.value
     }
 }
+
+#[async_trait]
+impl ComponentStarter for ComponentB {}