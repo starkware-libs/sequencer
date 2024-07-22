d0414476a32c956d039f3dd9eccbc43f64d21242
Dori Medini <dori@starkware.co>
2024-07-22 17:12:02 +0300

    tmp
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/lib.rs
+++ crates/blockifier/src/lib.rs
@@ -10,7 +10,7 @@
 ))]
 
 #[cfg(feature = "jemalloc")]
-// Override default allocator.
+// Override default allocator..
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
