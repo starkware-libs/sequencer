d2d9fa4e860b6eea95d1e632a8a5c1ca23d7c07f
Uriel Korach <uriel@starkware.co>
2024-07-25 16:03:38 +0300

    feat: add functionality to use remote mempool client/server

--- crates/mempool_types/src/communication.rs
+++ crates/mempool_types/src/communication.rs
@@ -3,7 +3,12 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use mockall::predicate::*;
 use mockall::*;
-use starknet_mempool_infra::component_client::{ClientError, LocalComponentClient};
+use serde::{Deserialize, Serialize};
+use starknet_mempool_infra::component_client::{
+    ClientError,
+    LocalComponentClient,
+    RemoteComponentClient,
+};
 use starknet_mempool_infra::component_definitions::ComponentRequestAndResponseSender;
 use thiserror::Error;
 
@@ -11,6 +16,7 @@ use crate::errors::MempoolError;
 use crate::mempool_types::{MempoolInput, ThinTransaction};
 
 pub type MempoolClientImpl = LocalComponentClient<MempoolRequest, MempoolResponse>;
+pub type RemoteMempoolClientImpl = RemoteComponentClient<MempoolRequest, MempoolResponse>;
 pub type MempoolResult<T> = Result<T, MempoolError>;
 pub type MempoolClientResult<T> = Result<T, MempoolClientError>;
 pub type MempoolRequestAndResponseSender =
@@ -26,13 +32,13 @@ pub trait MempoolClient: Send + Sync {
     async fn get_txs(&self, n_txs: usize) -> MempoolClientResult<Vec<ThinTransaction>>;
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum MempoolRequest {
     AddTransaction(MempoolInput),
     GetTransactions(usize),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum MempoolResponse {
     AddTransaction(MempoolResult<()>),
     GetTransactions(MempoolResult<Vec<ThinTransaction>>),
@@ -63,6 +69,33 @@ impl MempoolClient for MempoolClientImpl {
     async fn get_txs(&self, n_txs: usize) -> MempoolClientResult<Vec<ThinTransaction>> {
         let request = MempoolRequest::GetTransactions(n_txs);
         let response = self.send(request).await;
+        match response {
+            MempoolResponse::GetTransactions(Ok(response)) => Ok(response),
+            MempoolResponse::GetTransactions(Err(response)) => {
+                Err(MempoolClientError::MempoolError(response))
+            }
+            _ => Err(MempoolClientError::ClientError(ClientError::UnexpectedResponse)),
+        }
+    }
+}
+
+#[async_trait]
+impl MempoolClient for RemoteMempoolClientImpl {
+    async fn add_tx(&self, mempool_input: MempoolInput) -> MempoolClientResult<()> {
+        let request = MempoolRequest::AddTransaction(mempool_input);
+        let response = self.send(request).await?;
+        match response {
+            MempoolResponse::AddTransaction(Ok(response)) => Ok(response),
+            MempoolResponse::AddTransaction(Err(response)) => {
+                Err(MempoolClientError::MempoolError(response))
+            }
+            _ => Err(MempoolClientError::ClientError(ClientError::UnexpectedResponse)),
+        }
+    }
+
+    async fn get_txs(&self, n_txs: usize) -> MempoolClientResult<Vec<ThinTransaction>> {
+        let request = MempoolRequest::GetTransactions(n_txs);
+        let response = self.send(request).await?;
         match response {
             MempoolResponse::GetTransactions(Ok(response)) => Ok(response),
             MempoolResponse::GetTransactions(Err(response)) => {
