8ed6848cc75d66d8ade4f5f9d621021fa6737c30
Mohammad Nassar <mohammad@starkware.co>
2024-07-25 13:27:36 +0300

    test(mempool): flow test send tx twice

--- crates/mempool/src/mempool_test.rs
+++ crates/mempool/src/mempool_test.rs
@@ -573,3 +573,34 @@ fn test_commit_block_from_different_leader() {
     // Assert.
     assert_eq_mempool_queue(&mempool, &[tx_address0_nonce6])
 }
+
+#[rstest]
+#[ignore]
+fn test_flow_send_same_nonce_tx_after_previous_not_included() {
+    // Setup.
+    let tx_nonce3 =
+        add_tx_input!(tip: 10, tx_hash: 1, sender_address: "0x0", tx_nonce: 3_u8, account_nonce: 3_u8).tx;
+    let tx_input_nonce4 = add_tx_input!(tip: 11, tx_hash: 2, sender_address: "0x0", tx_nonce: 4_u8, account_nonce: 5_u8);
+    let tx_nonce5 =
+        add_tx_input!(tip: 12, tx_hash: 3, sender_address: "0x0", tx_nonce: 5_u8, account_nonce: 3_u8).tx;
+
+    let queue_txs = [TransactionReference::new(&tx_nonce3)];
+    let pool_txs = [&tx_nonce3, &tx_input_nonce4.tx, &tx_nonce5].map(|tx| tx.clone());
+    let mut mempool: Mempool = MempoolState::new(pool_txs, queue_txs).into();
+
+    // Test.
+    let txs = mempool.get_txs(2).unwrap();
+    assert_eq!(txs, &[tx_nonce3, tx_input_nonce4.tx.clone()]);
+
+    // Transaction with nonce 4 is not included in the block.
+    let state_changes =
+        HashMap::from([(contract_address!("0x0"), AccountState { nonce: Nonce(felt!(3_u16)) })]);
+    assert!(mempool.commit_block(state_changes).is_ok());
+
+    add_tx(&mut mempool, &tx_input_nonce4);
+    let txs = mempool.get_txs(1).unwrap();
+
+    // Assert.
+    assert_eq!(txs, &[tx_input_nonce4.tx]);
+    assert_eq_mempool_queue(&mempool, &[tx_nonce5]);
+}
