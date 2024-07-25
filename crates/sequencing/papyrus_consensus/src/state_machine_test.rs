97127d7d42f11fdeb846ef3e094901c908bf5039
Asmaa Magdoub <asmaa@starkware.co>
2024-07-25 11:33:12 +0300

    test(consensus): update sm tests

--- crates/sequencing/papyrus_consensus/src/state_machine_test.rs
+++ crates/sequencing/papyrus_consensus/src/state_machine_test.rs
@@ -97,27 +97,137 @@ fn validator_receives_votes_first() {
     );
     assert!(events.is_empty(), "{:?}", events);
 }
-// TODO(Asmaa): Add this test when we support NIL votes.
-// #[test]
-// fn buffer_events_during_get_proposal() {
-//     let mut state_machine = StateMachine::new(*PROPOSER_ID, 4);
-//     let leader_fn = |_: Round| *PROPOSER_ID;
-//     let mut events = state_machine.start(&leader_fn);
-//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, 0));
-//     assert!(events.is_empty(), "{:?}", events);
 
-//     // TODO(matan): When we support NIL votes, we should send them. Real votes without the
-// proposal     // doesn't make sense.
-//     events.append(&mut state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH,
-// ROUND)));     events.append(&mut
-// state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));     events.append(&
-// mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));     events.
-// append(&mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));
-//     assert!(events.is_empty(), "{:?}", events);
+#[test]
+fn buffer_events_during_get_proposal() {
+    let mut state_machine = StateMachine::new(*PROPOSER_ID, 4);
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, 0));
+    assert!(events.is_empty(), "{:?}", events);
 
-//     // Node finishes building the proposal.
-//     events = state_machine.handle_event(StateMachineEvent::GetProposal(None, 0));
-//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
-//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
-//     assert!(events.is_empty(), "{:?}", events);
-// }
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Node finishes building the proposal.
+    events =
+        state_machine.handle_event(StateMachineEvent::GetProposal(BLOCK_HASH, ROUND), &leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
+    assert!(events.is_empty(), "{:?}", events);
+}
+
+#[test]
+fn buffer_nil_votes_during_get_proposal() {
+    let mut state_machine = StateMachine::new(*PROPOSER_ID, 4);
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, 0));
+    assert!(events.is_empty(), "{:?}", events);
+
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(None, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(None, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(None, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Node finishes building the proposal.
+    events =
+        state_machine.handle_event(StateMachineEvent::GetProposal(BLOCK_HASH, ROUND), &leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(None, ROUND));
+    assert!(events.is_empty(), "{:?}", events);
+}
+
+#[test]
+fn no_precommit_without_proposal_even_with_prevote_quorum() {
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Receives votes from all the other nodes first (more than minimum for a quorum).
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // We don't have the proposal yet.
+    events.append(
+        &mut state_machine
+            .handle_event(StateMachineEvent::Precommit(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Finally the proposal arrives.
+    events = state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH, ROUND), &leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
+    assert!(events.is_empty(), "{:?}", events);
+}
+
+#[test]
+fn no_decision_without_proposal_even_with_precommit_quorum() {
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Receives votes from all the other nodes first (more than minimum for a quorum).
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine
+            .handle_event(StateMachineEvent::Precommit(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    events.append(
+        &mut state_machine
+            .handle_event(StateMachineEvent::Precommit(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // We don't have the proposal yet.
+    events.append(
+        &mut state_machine
+            .handle_event(StateMachineEvent::Precommit(BLOCK_HASH, ROUND), &leader_fn),
+    );
+    assert!(events.is_empty(), "{:?}", events);
+
+    // Finally the proposal arrives.
+    events = state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH, ROUND), &leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
+    assert_eq!(
+        events.pop_front().unwrap(),
+        StateMachineEvent::Decision(BLOCK_HASH.unwrap(), ROUND)
+    );
+    assert!(events.is_empty(), "{:?}", events);
+}
