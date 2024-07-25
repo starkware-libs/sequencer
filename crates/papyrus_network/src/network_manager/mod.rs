7162e713f1f0c5c1e7069ebd17b38c40d5ef1b69
Shahak Shama <shahak@starkware.co>
2024-07-25 08:12:16 +0300

    chore(network): fix confusing log of peer assignment

--- crates/papyrus_network/src/network_manager/mod.rs
+++ crates/papyrus_network/src/network_manager/mod.rs
@@ -469,7 +469,10 @@ impl<SwarmT: SwarmTrait> GenericNetworkManager<SwarmT> {
             client_payload;
         match self.swarm.send_query(query, PeerId::random(), protocol.clone()) {
             Ok(outbound_session_id) => {
-                debug!("Sent query to peer. outbound_session_id: {outbound_session_id:?}");
+                debug!(
+                    "Network received new query. waiting for peer assignment. \
+                     outbound_session_id: {outbound_session_id:?}"
+                );
                 self.num_active_outbound_sessions += 1;
                 gauge!(
                     papyrus_metrics::PAPYRUS_NUM_ACTIVE_OUTBOUND_SESSIONS,
