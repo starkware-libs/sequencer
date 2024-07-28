354e052c2fccf2646ecbb03e05b258a19c3ad4c6
eitanm-starkware <eitan.m@starkware.co>
2024-07-28 14:09:05 +0300

    fix(network): make streamhashmap not end

--- crates/papyrus_network/src/utils.rs
+++ crates/papyrus_network/src/utils.rs
@@ -2,7 +2,7 @@ use std::collections::hash_map::{Keys, ValuesMut};
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::task::{Context, Poll, Waker};
 
 use futures::stream::{Stream, StreamExt};
 
@@ -11,13 +11,13 @@ use futures::stream::{Stream, StreamExt};
 // Stream trait from tokio_stream and not from futures.
 pub(crate) struct StreamHashMap<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> {
     map: HashMap<K, V>,
-    finished_streams: HashSet<K>,
+    wakers_waiting_for_new_stream: Vec<Waker>,
 }
 
 impl<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> StreamHashMap<K, V> {
     #[allow(dead_code)]
     pub fn new(map: HashMap<K, V>) -> Self {
-        Self { map, finished_streams: Default::default() }
+        Self { map, wakers_waiting_for_new_stream: Default::default() }
     }
 
     #[allow(dead_code)]
@@ -36,7 +36,11 @@ impl<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> StreamHashMap<K, V> {
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        self.map.insert(key, value)
+        let res = self.map.insert(key, value);
+        for waker in self.wakers_waiting_for_new_stream.drain(..) {
+            waker.wake();
+        }
+        res
     }
 }
 
@@ -45,24 +49,20 @@ impl<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> Stream for StreamHashMap<K
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let unpinned_self = Pin::into_inner(self);
-        let mut finished = true;
+        let mut finished_streams = HashSet::new();
         for (key, stream) in &mut unpinned_self.map {
             match stream.poll_next_unpin(cx) {
                 Poll::Ready(Some(value)) => {
                     return Poll::Ready(Some((key.clone(), value)));
                 }
                 Poll::Ready(None) => {
-                    unpinned_self.finished_streams.insert(key.clone());
+                    finished_streams.insert(key.clone());
                 }
-                Poll::Pending => {
-                    finished = false;
-                }
+                Poll::Pending => {}
             }
         }
-        if finished {
-            // TODO(shahak): Make StreamHashMap not end in order to accept new inserted streams.
-            return Poll::Ready(None);
-        }
+        HashMap::retain(&mut unpinned_self.map, |key, _| !finished_streams.contains(key));
+        unpinned_self.wakers_waiting_for_new_stream.push(cx.waker().clone());
         Poll::Pending
     }
 }
