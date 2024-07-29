c7513a4cd8db2c680753038cc06a5fd0b8abc22b
asmaa-starkware <163830216+asmaastarkware@users.noreply.github.com>
2024-07-29 11:25:35 +0300

    refactor(consensus): change StartRound to GetProposal and pass leader_fn to sm from shc (#63)

--- crates/sequencing/papyrus_consensus/src/state_machine_test.rs
+++ crates/sequencing/papyrus_consensus/src/state_machine_test.rs
@@ -1,9 +1,16 @@
+use lazy_static::lazy_static;
 use starknet_api::block::BlockHash;
 use starknet_types_core::felt::Felt;
 use test_case::test_case;
 
 use super::Round;
 use crate::state_machine::{StateMachine, StateMachineEvent};
+use crate::types::ValidatorId;
+
+lazy_static! {
+    static ref VALIDATOR_ID: ValidatorId = 1_u32.into();
+    static ref PROPOSER_ID: ValidatorId = 0_u32.into();
+}
 
 const BLOCK_HASH: BlockHash = BlockHash(Felt::ONE);
 const ROUND: Round = 0;
@@ -11,16 +18,17 @@ const ROUND: Round = 0;
 #[test_case(true; "proposer")]
 #[test_case(false; "validator")]
 fn events_arrive_in_ideal_order(is_proposer: bool) {
-    let mut state_machine = StateMachine::new(4);
-
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, ROUND));
+    let id = if is_proposer { *PROPOSER_ID } else { *VALIDATOR_ID };
+    let mut state_machine = StateMachine::new(id, 4);
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
     if is_proposer {
-        events = state_machine.handle_event(StateMachineEvent::StartRound(Some(BLOCK_HASH), ROUND));
+        assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, ROUND));
+        events =
+            state_machine.handle_event(StateMachineEvent::GetProposal(Some(BLOCK_HASH), ROUND));
         assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
     } else {
-        state_machine.handle_event(StateMachineEvent::StartRound(None, ROUND));
-        assert!(events.is_empty());
+        assert!(events.is_empty(), "{:?}", events);
         events = state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
     }
     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
@@ -43,12 +51,10 @@ fn events_arrive_in_ideal_order(is_proposer: bool) {
 
 #[test]
 fn validator_receives_votes_first() {
-    let mut state_machine = StateMachine::new(4);
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
 
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, ROUND));
-    assert!(events.is_empty(), "{:?}", events);
-    events = state_machine.handle_event(StateMachineEvent::StartRound(None, ROUND));
+    let leader_fn = |_: Round| *PROPOSER_ID;
+    let mut events = state_machine.start(&leader_fn);
     assert!(events.is_empty(), "{:?}", events);
 
     // Receives votes from all the other nodes first (more than minimum for a quorum).
@@ -67,25 +73,27 @@ fn validator_receives_votes_first() {
     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Decision(BLOCK_HASH, ROUND));
     assert!(events.is_empty(), "{:?}", events);
 }
+// TODO(Asmaa): Add this test when we support NIL votes.
+// #[test]
+// fn buffer_events_during_get_proposal() {
+//     let mut state_machine = StateMachine::new(*PROPOSER_ID, 4);
+//     let leader_fn = |_: Round| *PROPOSER_ID;
+//     let mut events = state_machine.start(&leader_fn);
+//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, 0));
+//     assert!(events.is_empty(), "{:?}", events);
 
-#[test]
-fn buffer_events_during_start_round() {
-    let mut state_machine = StateMachine::new(4);
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, 0));
-    assert!(events.is_empty(), "{:?}", events);
+//     // TODO(matan): When we support NIL votes, we should send them. Real votes without the
+// proposal     // doesn't make sense.
+//     events.append(&mut state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH,
+// ROUND)));     events.append(&mut
+// state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));     events.append(&
+// mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));     events.
+// append(&mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));
+//     assert!(events.is_empty(), "{:?}", events);
 
-    // TODO(matan): When we support NIL votes, we should send them. Real votes without the proposal
-    // doesn't make sense.
-    events.append(&mut state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH, ROUND)));
-    events.append(&mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));
-    events.append(&mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));
-    events.append(&mut state_machine.handle_event(StateMachineEvent::Prevote(BLOCK_HASH, ROUND)));
-    assert!(events.is_empty(), "{:?}", events);
-
-    // Node finishes building the proposal.
-    events = state_machine.handle_event(StateMachineEvent::StartRound(None, 0));
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
-    assert!(events.is_empty(), "{:?}", events);
-}
+//     // Node finishes building the proposal.
+//     events = state_machine.handle_event(StateMachineEvent::GetProposal(None, 0));
+//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
+//     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
+//     assert!(events.is_empty(), "{:?}", events);
+// }
