b118b804eb65a4b1a98badd856a3da10b184789e
Mohammad Nassar <mohammad@starkware.co>
2024-07-30 15:18:16 +0300

    test(mempool): impl tests for calculate gas price method

--- crates/blockifier/src/blockifier/block_test.rs
+++ crates/blockifier/src/blockifier/block_test.rs
@@ -1,10 +1,16 @@
+use pretty_assertions::assert_eq;
 use starknet_api::block::BlockNumber;
 use starknet_api::core::ContractAddress;
 use starknet_api::felt;
 use starknet_api::state::StorageKey;
 
 use crate::abi::constants;
-use crate::blockifier::block::{pre_process_block, BlockNumberHashPair};
+use crate::blockifier::block::{
+    pre_process_block,
+    BlockInfo,
+    BlockNumberHashPair,
+    GAS_PRICE_MAX_CHANGE_DENOMINATOR,
+};
 use crate::context::ChainInfo;
 use crate::state::state_api::StateReader;
 use crate::test_utils::contracts::FeatureContract;
@@ -46,4 +52,79 @@ fn test_pre_process_block() {
         ),
         format!("{}", error.unwrap_err())
     );
+}
+
+fn compute_and_check_price_range(
+    prev_price: u64,
+    gas_used: u64,
+    gas_target: u64,
+    should_increase: bool,
+) -> u64 {
+    let new_price = BlockInfo::calculate_next_base_gas_price(prev_price, gas_used, gas_target);
+
+    let prev_price_f64 = f64::from(u32::try_from(prev_price).unwrap());
+    let new_price_f64 = f64::from(u32::try_from(new_price).unwrap());
+
+    let ratio = new_price_f64 / prev_price_f64;
+    let max_change = 1.0 / f64::from(u32::try_from(GAS_PRICE_MAX_CHANGE_DENOMINATOR).unwrap());
+    if should_increase {
+        assert!((1.0..=1.0 + max_change).contains(&ratio));
+    } else {
+        assert!((1.0 - max_change..=1.0).contains(&ratio));
+    }
+    new_price
+}
+
+#[test]
+fn test_congestion_increases_price() {
+    let mut prev_price = 100;
+    let current_gas_target = 100;
+    let gas_usages = [150, 160];
+
+    for gas_used in gas_usages {
+        prev_price = compute_and_check_price_range(prev_price, gas_used, current_gas_target, true);
+    }
+}
+
+#[test]
+fn test_reduced_gas_usage_decreases_price() {
+    let mut prev_price = 100;
+    let current_gas_target = 100;
+    let gas_usages = [90, 80];
+
+    for gas_used in gas_usages {
+        prev_price = compute_and_check_price_range(prev_price, gas_used, current_gas_target, false);
+    }
+}
+
+#[test]
+fn test_stable_gas_usage() {
+    let mut current_price = 100;
+    let mut prev_price = 100;
+    let current_gas_target = 100;
+    let gas_usages = [100, 100];
+
+    for &gas_used in &gas_usages {
+        current_price =
+            BlockInfo::calculate_next_base_gas_price(current_price, gas_used, current_gas_target);
+        assert_eq!(current_price, prev_price);
+        prev_price = current_price;
+    }
+}
+
+#[test]
+// This test ensures that the gas price calculation does not overflow with extreme values,
+// verifying that `calculate_next_base_gas_price` does not panic.
+fn test_gas_price_with_extreme_values() {
+    let price = u64::MAX;
+    let gas_target = u64::MAX / 2;
+    let gas_used = 0;
+    BlockInfo::calculate_next_base_gas_price(price, gas_used, gas_target);
+
+    // To avoid overflow when updating the price, the value is set below a certain threshold so that
+    // the new price does not exceed u64::MAX.
+    let price = u64::MAX - (u64::MAX / u64::try_from(GAS_PRICE_MAX_CHANGE_DENOMINATOR).unwrap());
+    let gas_target = u64::MAX / 2;
+    let gas_used = u64::MAX;
+    BlockInfo::calculate_next_base_gas_price(price, gas_used, gas_target);
 }
