e4dd3b023516fd16c1de7509cec26cf3f3a16669
Arnon Hod <arnon@starkware.co>
2024-07-31 11:30:32 +0300

    chore: use chain info in stateful validator config (#99)

--- crates/gateway/src/stateful_transaction_validator_test.rs
+++ crates/gateway/src/stateful_transaction_validator_test.rs
@@ -43,7 +43,7 @@ fn stateful_validator(block_context: BlockContext) -> StatefulTransactionValidat
             max_nonce_for_validation_skip: Default::default(),
             validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
             max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
-            chain_info: block_context.chain_info().clone().into(),
+            chain_info: block_context.chain_info().clone(),
         },
     }
 }
@@ -124,7 +124,7 @@ fn test_instantiate_validator() {
             max_nonce_for_validation_skip: Default::default(),
             validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
             max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
-            chain_info: block_context.chain_info().clone().into(),
+            chain_info: block_context.chain_info().clone(),
         },
     };
     let blockifier_validator = stateful_validator.instantiate_validator(&mock_state_reader_factory);
