ef8318be50c2851c3ef05fbb906097cdd3bea3da
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-08-06 19:00:20 +0300

    refactor: split syscalls into separate files

---
+++ crates/blockifier/src/execution/syscalls/syscall_tests/call_contract.rs
@@ -0,0 +1,60 @@
+use pretty_assertions::assert_eq;
+use starknet_api::felt;
+use test_case::test_case;
+
+use super::consts::REQUIRED_GAS_CALL_CONTRACT_TEST;
+use crate::abi::abi_utils::selector_from_name;
+use crate::context::ChainInfo;
+use crate::execution::call_info::{CallExecution, Retdata};
+use crate::execution::entry_point::CallEntryPoint;
+use crate::execution::syscalls::syscall_tests::utils::assert_consistent_contract_version;
+use crate::retdata;
+use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::initial_test_state::test_state;
+use crate::test_utils::{create_calldata, trivial_external_entry_point_new, CairoVersion, BALANCE};
+
+#[test_case(
+    FeatureContract::TestContract(CairoVersion::Cairo1),
+    FeatureContract::TestContract(CairoVersion::Cairo1),
+    REQUIRED_GAS_CALL_CONTRACT_TEST;
+    "Call Contract between two contracts using VM"
+)]
+fn test_call_contract(
+    outer_contract: FeatureContract,
+    inner_contract: FeatureContract,
+    expected_gas: u64,
+) {
+    let chain_info = &ChainInfo::create_for_testing();
+    let mut state = test_state(chain_info, BALANCE, &[(outer_contract, 1), (inner_contract, 1)]);
+
+    assert_consistent_contract_version(outer_contract, &state);
+    assert_consistent_contract_version(inner_contract, &state);
+
+    let outer_entry_point_selector = selector_from_name("test_call_contract");
+    let calldata = create_calldata(
+        inner_contract.get_instance_address(0),
+        "test_storage_read_write",
+        &[
+            felt!(405_u16), // Calldata: address.
+            felt!(48_u8),   // Calldata: value.
+        ],
+    );
+    let entry_point_call = CallEntryPoint {
+        entry_point_selector: outer_entry_point_selector,
+        calldata,
+        ..trivial_external_entry_point_new(outer_contract)
+    };
+
+    assert_eq!(
+        entry_point_call.execute_directly(&mut state).unwrap().execution,
+        CallExecution {
+            retdata: retdata![felt!(48_u8)],
+            gas_consumed: expected_gas,
+            ..CallExecution::default()
+        }
+    );
+
+    // ensure that the fallback system didn't replace the contract
+    assert_consistent_contract_version(outer_contract, &state);
+    assert_consistent_contract_version(inner_contract, &state);
+}
---
+++ crates/blockifier/src/execution/syscalls/syscall_tests/storage_read_write.rs
@@ -0,0 +1,50 @@
+use starknet_api::state::StorageKey;
+use starknet_api::transaction::Calldata;
+use starknet_api::{calldata, felt};
+use test_case::test_case;
+
+use crate::abi::abi_utils::selector_from_name;
+use crate::context::ChainInfo;
+use crate::execution::call_info::{CallExecution, Retdata};
+use crate::execution::entry_point::CallEntryPoint;
+use crate::execution::syscalls::syscall_tests::consts::REQUIRED_GAS_STORAGE_READ_WRITE_TEST;
+use crate::execution::syscalls::syscall_tests::utils::assert_consistent_contract_version;
+use crate::retdata;
+use crate::state::state_api::StateReader;
+use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::initial_test_state::test_state;
+use crate::test_utils::{trivial_external_entry_point_new, CairoVersion, BALANCE};
+
+#[test_case(FeatureContract::TestContract(CairoVersion::Cairo1), REQUIRED_GAS_STORAGE_READ_WRITE_TEST; "VM")]
+fn test_storage_read_write(test_contract: FeatureContract, expected_gas: u64) {
+    let chain_info = &ChainInfo::create_for_testing();
+    let mut state = test_state(chain_info, BALANCE, &[(test_contract, 1)]);
+
+    assert_consistent_contract_version(test_contract, &state);
+
+    let key = felt!(1234_u16);
+    let value = felt!(18_u8);
+    let calldata = calldata![key, value];
+    let entry_point_call = CallEntryPoint {
+        calldata,
+        entry_point_selector: selector_from_name("test_storage_read_write"),
+        ..trivial_external_entry_point_new(test_contract)
+    };
+    let storage_address = entry_point_call.storage_address;
+    assert_eq!(
+        entry_point_call.execute_directly(&mut state).unwrap().execution,
+        CallExecution {
+            retdata: retdata![value],
+            gas_consumed: expected_gas,
+            ..CallExecution::default()
+        }
+    );
+
+    // Verify that the state has changed.
+    let value_from_state =
+        state.get_storage_at(storage_address, StorageKey::try_from(key).unwrap()).unwrap();
+    assert_eq!(value_from_state, value);
+
+    // ensure that the fallback system didn't replace the contract
+    assert_consistent_contract_version(test_contract, &state);
+}
