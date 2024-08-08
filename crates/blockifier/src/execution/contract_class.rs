4c6e2b3f106428b9838792cfd5587a9a053a7d86
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-08-08 13:33:49 +0300

    fix: make comments capitalized

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -95,7 +95,7 @@ impl ContractClass {
         match self {
             ContractClass::V0(class) => class.estimate_casm_hash_computation_resources(),
             ContractClass::V1(class) => class.estimate_casm_hash_computation_resources(),
-            ContractClass::V1Native(_) => todo!("sierra estimate casm hash computation resources"),
+            ContractClass::V1Native(_) => todo!("Sierra estimate casm hash computation resources."),
         }
     }
 
@@ -108,7 +108,7 @@ impl ContractClass {
                 panic!("get_visited_segments is not supported for v0 contracts.")
             }
             ContractClass::V1(class) => class.get_visited_segments(visited_pcs),
-            ContractClass::V1Native(_) => todo!("sierra visited segments"),
+            ContractClass::V1Native(_) => todo!("Sierra visited segments."),
         }
     }
 
@@ -116,7 +116,7 @@ impl ContractClass {
         match self {
             ContractClass::V0(class) => class.bytecode_length(),
             ContractClass::V1(class) => class.bytecode_length(),
-            ContractClass::V1Native(_) => todo!("sierra estimate casm hash computation resources"),
+            ContractClass::V1Native(_) => todo!("Sierra estimate casm hash computation resources."),
         }
     }
 
