d43f98b050599ac57d070c22ea47dc2d71bf41ff
Lev Roitman <lev@starkware.co>
2024-08-12 20:19:24 +0300

    feat: adding batcher to main node function
    commit-id:2a81357c

--- crates/mempool_node/src/communication.rs
+++ crates/mempool_node/src/communication.rs
@@ -1,5 +1,10 @@
 use std::sync::Arc;
 
+use starknet_batcher_types::communication::{
+    BatcherRequestAndResponseSender,
+    LocalBatcherClientImpl,
+    SharedBatcherClient,
+};
 use starknet_mempool_infra::component_definitions::ComponentCommunication;
 use starknet_mempool_types::communication::{
     LocalMempoolClientImpl,
@@ -11,33 +16,50 @@ use tokio::sync::mpsc::{channel, Receiver, Sender};
 use crate::config::MempoolNodeConfig;
 
 pub struct MempoolNodeCommunication {
+    batcher_channel: ComponentCommunication<BatcherRequestAndResponseSender>,
     mempool_channel: ComponentCommunication<MempoolRequestAndResponseSender>,
 }
 
 impl MempoolNodeCommunication {
+    pub fn take_batcher_tx(&mut self) -> Sender<BatcherRequestAndResponseSender> {
+        self.batcher_channel.take_tx()
+    }
+
+    pub fn take_batcher_rx(&mut self) -> Receiver<BatcherRequestAndResponseSender> {
+        self.batcher_channel.take_rx()
+    }
+
     pub fn take_mempool_tx(&mut self) -> Sender<MempoolRequestAndResponseSender> {
         self.mempool_channel.take_tx()
     }
+
     pub fn take_mempool_rx(&mut self) -> Receiver<MempoolRequestAndResponseSender> {
         self.mempool_channel.take_rx()
     }
 }
 
 pub fn create_node_channels() -> MempoolNodeCommunication {
-    const MEMPOOL_INVOCATIONS_QUEUE_SIZE: usize = 32;
+    const DEFAULT_INVOCATIONS_QUEUE_SIZE: usize = 32;
     let (tx_mempool, rx_mempool) =
-        channel::<MempoolRequestAndResponseSender>(MEMPOOL_INVOCATIONS_QUEUE_SIZE);
+        channel::<MempoolRequestAndResponseSender>(DEFAULT_INVOCATIONS_QUEUE_SIZE);
+    let (tx_batcher, rx_batcher) =
+        channel::<BatcherRequestAndResponseSender>(DEFAULT_INVOCATIONS_QUEUE_SIZE);
     MempoolNodeCommunication {
         mempool_channel: ComponentCommunication::new(Some(tx_mempool), Some(rx_mempool)),
+        batcher_channel: ComponentCommunication::new(Some(tx_batcher), Some(rx_batcher)),
     }
 }
 
 pub struct MempoolNodeClients {
+    batcher_client: Option<SharedBatcherClient>,
     mempool_client: Option<SharedMempoolClient>,
     // TODO (Lev 25/06/2024): Change to Option<Box<dyn MemPoolClient>>.
 }
 
 impl MempoolNodeClients {
+    pub fn get_batcher_client(&self) -> Option<SharedBatcherClient> {
+        self.batcher_client.clone()
+    }
     pub fn get_mempool_client(&self) -> Option<SharedMempoolClient> {
         self.mempool_client.clone()
     }
@@ -47,9 +69,13 @@ pub fn create_node_clients(
     config: &MempoolNodeConfig,
     channels: &mut MempoolNodeCommunication,
 ) -> MempoolNodeClients {
+    let batcher_client: Option<SharedBatcherClient> = match config.components.batcher.execute {
+        true => Some(Arc::new(LocalBatcherClientImpl::new(channels.take_batcher_tx()))),
+        false => None,
+    };
     let mempool_client: Option<SharedMempoolClient> = match config.components.gateway.execute {
         true => Some(Arc::new(LocalMempoolClientImpl::new(channels.take_mempool_tx()))),
         false => None,
     };
-    MempoolNodeClients { mempool_client }
+    MempoolNodeClients { batcher_client, mempool_client }
 }
