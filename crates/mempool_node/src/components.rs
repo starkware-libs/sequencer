ce82441f0d6db6c7b46d05717cae5f58f6204365
Lev Roitman <lev@starkware.co>
2024-08-12 20:19:24 +0300

    feat: adding batcher to main node function
    commit-id:2a81357c

--- crates/mempool_node/src/components.rs
+++ crates/mempool_node/src/components.rs
@@ -1,3 +1,4 @@
+use starknet_batcher::batcher::{create_batcher, Batcher};
 use starknet_gateway::compilation::GatewayCompiler;
 use starknet_gateway::gateway::{create_gateway, Gateway};
 use starknet_mempool::mempool::Mempool;
@@ -6,11 +7,20 @@ use crate::communication::MempoolNodeClients;
 use crate::config::MempoolNodeConfig;
 
 pub struct Components {
+    pub batcher: Option<Batcher>,
     pub gateway: Option<Gateway>,
     pub mempool: Option<Mempool>,
 }
 
 pub fn create_components(config: &MempoolNodeConfig, clients: &MempoolNodeClients) -> Components {
+    let batcher = if config.components.batcher.execute {
+        let mempool_client =
+            clients.get_mempool_client().expect("Mempool Client should be available");
+        Some(create_batcher(config.batcher_config.clone(), mempool_client))
+    } else {
+        None
+    };
+
     let gateway = if config.components.gateway.execute {
         let mempool_client =
             clients.get_mempool_client().expect("Mempool Client should be available");
@@ -28,5 +38,5 @@ pub fn create_components(config: &MempoolNodeConfig, clients: &MempoolNodeClient
 
     let mempool = if config.components.mempool.execute { Some(Mempool::empty()) } else { None };
 
-    Components { gateway, mempool }
+    Components { batcher, gateway, mempool }
 }
