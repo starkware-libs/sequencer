a87f24d77603d4ebbde5c9588da5d7a89be73c43
Yair Bakalchuk <yair@starkware.co>
2024-08-12 17:16:50 +0300

    feat(batcher): proposals manager module

---
+++ crates/batcher/src/proposals_manager_test.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+
+use assert_matches::assert_matches;
+use starknet_api::block::BlockNumber;
+use starknet_mempool_infra::trace_util::configure_tracing;
+use starknet_mempool_types::communication::MockMempoolClient;
+
+use crate::proposals_manager::consensus::MockConsensusClient;
+use crate::proposals_manager::{ProposalsManager, ProposalsManagerConfig, ProposalsManagerError};
+
+#[tokio::test]
+async fn multiple_proposals_generation() {
+    configure_tracing();
+    let mut mempool_client = MockMempoolClient::new();
+    mempool_client.expect_get_txs().returning(|_| Ok(vec![]));
+    let consensus_client = MockConsensusClient::new();
+    let mut proposals_manager = ProposalsManager::new(
+        ProposalsManagerConfig::default(),
+        Arc::new(mempool_client),
+        Arc::new(consensus_client),
+    );
+    proposals_manager.generate_block_proposal(0, BlockNumber::default(), None).await.unwrap();
+
+    let another_generate_request =
+        proposals_manager.generate_block_proposal(1, BlockNumber::default(), None).await;
+    assert_matches!(
+        another_generate_request.unwrap_err(),
+        ProposalsManagerError::AlreadyGeneratingProposal {
+            current_generating_proposal_id: 0,
+            new_proposal_id: 1
+        }
+    );
+}
