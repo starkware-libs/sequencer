f92adc2517638dcc507785965b9d48e90013579b
Arnon Hod <arnon@starkware.co>
2024-08-12 13:37:05 +0300

    chore: use cairo lang util to extract sierra program version id (#84)

--- crates/gateway/src/test_utils.rs
+++ crates/gateway/src/test_utils.rs
@@ -4,8 +4,13 @@ use crate::compiler_version::VersionId;
 
 pub fn create_sierra_program(version_id: &VersionId) -> Vec<Felt> {
     vec![
+        // Sierra Version ID.
         Felt::from(u64::try_from(version_id.major).unwrap()),
         Felt::from(u64::try_from(version_id.minor).unwrap()),
         Felt::from(u64::try_from(version_id.patch).unwrap()),
+        // Compiler Version ID.
+        Felt::from(u64::try_from(0).unwrap()),
+        Felt::from(u64::try_from(0).unwrap()),
+        Felt::from(u64::try_from(0).unwrap()),
     ]
 }
