1f3245417aea017ad0c96a5154804f40e54b26a5
Ayelet Zilber <ayelet@starkware.co>
2024-08-14 14:08:59 +0300

    fix(mempool): in add_tx, delete queue transaction with lower nonce that account nonce

--- crates/mempool/src/mempool.rs
+++ crates/mempool/src/mempool.rs
@@ -135,7 +135,11 @@ impl Mempool {
         let MempoolInput { tx, account: Account { sender_address, state: AccountState { nonce } } } =
             input;
 
-        // Remove transactions with lower nonce than the account nonce.
+        // Note: != is actually equivalent to > here, as lower nonces are rejected in validation.
+        if self.tx_queue.get_nonce(sender_address).is_some_and(|queued_nonce| queued_nonce != nonce)
+        {
+            self.tx_queue.remove(sender_address);
+        }
         self.tx_pool.remove_up_to_nonce(sender_address, nonce);
 
         self.tx_pool.insert((&tx).into())?;
