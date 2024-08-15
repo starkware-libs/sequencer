38aac2677a2af2a5004a947dbe4cb0977fbfbd97
Arni Hod <arnon@starkware.co>
2024-08-15 16:36:13 +0300

    refactor: create executable tx in the gateway process_tx

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -21,7 +21,7 @@ use crate::gateway::{add_tx, AppState, SharedMempoolClient};
 use crate::state_reader_test_utils::{local_test_state_reader_factory, TestStateReaderFactory};
 use crate::stateful_transaction_validator::StatefulTransactionValidator;
 use crate::stateless_transaction_validator::StatelessTransactionValidator;
-use crate::utils::rpc_tx_to_account_tx;
+use crate::utils::compile_to_casm_and_convert_rpc_to_executable_tx;
 
 pub fn app_state(
     mempool_client: SharedMempoolClient,
@@ -94,18 +94,11 @@ async fn to_bytes(res: Response) -> Bytes {
 }
 
 fn calculate_hash(rpc_tx: &RpcTransaction) -> TransactionHash {
-    let optional_class_info = match &rpc_tx {
-        RpcTransaction::Declare(_declare_tx) => {
-            panic!("Declare transactions are not supported in this test")
-        }
-        _ => None,
-    };
-
-    let account_tx = rpc_tx_to_account_tx(
+    let executable_tx = compile_to_casm_and_convert_rpc_to_executable_tx(
         rpc_tx,
-        optional_class_info,
+        &GatewayCompiler::new_cairo_lang_compiler(Default::default()),
         &ChainInfo::create_for_testing().chain_id,
     )
     .unwrap();
-    account_tx.tx_hash()
+    executable_tx.tx_hash()
 }
--- crates/gateway/src/utils.rs
+++ crates/gateway/src/utils.rs
@@ -6,6 +6,11 @@ use blockifier::transaction::transactions::{
     InvokeTransaction as BlockifierInvokeTransaction,
 };
 use starknet_api::core::{calculate_contract_address, ChainId, ClassHash, ContractAddress};
+use starknet_api::executable_transaction::{
+    DeployAccountTransaction as ExecutableDeployAccountTransaction,
+    InvokeTransaction as ExecutableInvokeTransaction,
+    Transaction as ExecutableTransaction,
+};
 use starknet_api::rpc_transaction::{
     RpcDeclareTransaction,
     RpcDeployAccountTransaction,
@@ -17,13 +22,62 @@ use starknet_api::transaction::{
     DeclareTransactionV3,
     DeployAccountTransaction,
     DeployAccountTransactionV3,
+    InvokeTransaction,
     InvokeTransactionV3,
     TransactionHasher,
 };
 use tracing::error;
 
+use crate::compilation::GatewayCompiler;
 use crate::errors::{GatewaySpecError, StatefulTransactionValidatorResult};
 
+/// Converts an RPC transaction to an executable transaction.
+/// This conversion is dependent on the chain ID.
+/// Note, that for declare transaction this step is heavy, as it requires compilation of Sierra to
+/// executable contract class.
+pub fn compile_to_casm_and_convert_rpc_to_executable_tx(
+    rpc_tx: &RpcTransaction,
+    gateway_compiler: &GatewayCompiler,
+    chain_id: &ChainId,
+) -> Result<ExecutableTransaction, GatewaySpecError> {
+    Ok(match rpc_tx {
+        RpcTransaction::Declare(rpc_declare_tx) => ExecutableTransaction::Declare(
+            gateway_compiler.process_declare_tx(rpc_declare_tx, chain_id).map_err(|error| {
+                error!(
+                    "Failed to convert RPC declare transaction to executable transaction: {}",
+                    error
+                );
+                GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+            })?,
+        ),
+        RpcTransaction::DeployAccount(rpc_deploy_account_tx) => {
+            ExecutableTransaction::DeployAccount(
+                ExecutableDeployAccountTransaction::from_rpc_tx(rpc_deploy_account_tx, chain_id)
+                    .map_err(|error| {
+                        error!(
+                            "Failed to convert RPC deploy account transaction to executable \
+                             transaction: {}",
+                            error
+                        );
+                        GatewaySpecError::UnexpectedError {
+                            data: "Internal server error".to_owned(),
+                        }
+                    })?,
+            )
+        }
+        RpcTransaction::Invoke(rpc_invoke_tx) => ExecutableTransaction::Invoke(
+            ExecutableInvokeTransaction::from_rpc_tx(rpc_invoke_tx, chain_id).map_err(|error| {
+                error!(
+                    "Failed to convert RPC invoke transaction to executable transaction: {}",
+                    error
+                );
+                GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+            })?,
+        ),
+    })
+}
+
+// TODO(Arni): Delete this function.
 pub fn rpc_tx_to_account_tx(
     rpc_tx: &RpcTransaction,
     // FIXME(yael 15/4/24): calculate class_info inside the function once compilation code is ready
@@ -98,7 +152,7 @@ pub fn rpc_tx_to_account_tx(
             Ok(AccountTransaction::DeployAccount(deploy_account_tx))
         }
         RpcTransaction::Invoke(RpcInvokeTransaction::V3(tx)) => {
-            let invoke_tx = starknet_api::transaction::InvokeTransaction::V3(InvokeTransactionV3 {
+            let invoke_tx = InvokeTransaction::V3(InvokeTransactionV3 {
                 resource_bounds: tx.resource_bounds.clone().into(),
                 tip: tx.tip,
                 signature: tx.signature.clone(),
