b514e14bb6e65abe6e5a8d621038b8b74ba2b2f9
Mohammad Nassar <mohammad@starkware.co>
2024-08-18 09:13:47 +0300

    feat(mempool): implement update gas price for tx queue

--- crates/mempool/src/transaction_queue.rs
+++ crates/mempool/src/transaction_queue.rs
@@ -2,6 +2,7 @@ use std::cmp::Ordering;
 use std::collections::{BTreeSet, HashMap};
 
 use starknet_api::core::{ContractAddress, Nonce};
+use starknet_api::transaction::{Resource, ResourceBounds};
 
 use crate::mempool::TransactionReference;
 
@@ -74,6 +75,45 @@ impl TransactionQueue {
 
     pub fn is_empty(&self) -> bool {
         self.priority_queue.is_empty()
+    }
+
+    pub fn update_gas_price_threshold(&mut self, threshold: u128) {
+        if threshold < self.gas_price_threshold {
+            let resource_bounds: Vec<(Resource, ResourceBounds)> = vec![
+                (Resource::L1Gas, ResourceBounds::default()),
+                (Resource::L2Gas, ResourceBounds { max_amount: 0, max_price_per_unit: threshold }),
+            ];
+
+            let at_pending_tx = PendingTransaction(TransactionReference {
+                resource_bounds: resource_bounds.try_into().unwrap(),
+                ..Default::default()
+            });
+
+            // Split off the pending queue at the given transaction higher than the threshold.
+            let mut split_point = self.pending_queue.split_off(&at_pending_tx);
+            split_point.remove(&at_pending_tx);
+
+            // Insert all transactions from the split point into the priority queue
+            for pending_tx in &split_point {
+                self.priority_queue.insert(pending_tx.clone().0.into());
+            }
+        } else {
+            let mut to_remove = Vec::new();
+
+            for priority_tx in &self.priority_queue {
+                if priority_tx.get_l2_gas_price() < threshold {
+                    self.pending_queue.insert(priority_tx.0.clone().into());
+                    to_remove.push(priority_tx.clone());
+                }
+            }
+
+            // Remove moved transactions from the priority queue
+            for tx in to_remove {
+                self.priority_queue.remove(&tx);
+            }
+        }
+
+        self.gas_price_threshold = threshold;
     }
 }
 
