b6332ce9b7b82af50d463d35238855b4b580a89f
Nimrod Weiss <nimrod@starkware.co>
2024-08-18 11:49:41 +0300

    refactor(fee): rename ResourceBoundsMapping => DeprecatedResourceBoundsMapping

--- crates/blockifier/src/transaction/post_execution_test.rs
+++ crates/blockifier/src/transaction/post_execution_test.rs
@@ -2,7 +2,12 @@ use assert_matches::assert_matches;
 use rstest::rstest;
 use starknet_api::core::{ContractAddress, PatriciaKey};
 use starknet_api::state::StorageKey;
-use starknet_api::transaction::{Calldata, Fee, ResourceBoundsMapping, TransactionVersion};
+use starknet_api::transaction::{
+    Calldata,
+    DeprecatedResourceBoundsMapping,
+    Fee,
+    TransactionVersion,
+};
 use starknet_api::{felt, patricia_key};
 use starknet_types_core::felt::Felt;
 
@@ -67,7 +72,7 @@ fn calldata_for_write_and_transfer(
 #[case(TransactionVersion::THREE, FeeType::Strk)]
 fn test_revert_on_overdraft(
     max_fee: Fee,
-    max_resource_bounds: ResourceBoundsMapping,
+    max_resource_bounds: DeprecatedResourceBoundsMapping,
     block_context: BlockContext,
     #[case] version: TransactionVersion,
     #[case] fee_type: FeeType,
@@ -212,7 +217,7 @@ fn test_revert_on_overdraft(
 #[case(TransactionVersion::THREE, "Insufficient max L1 gas", true)]
 fn test_revert_on_resource_overuse(
     max_fee: Fee,
-    max_resource_bounds: ResourceBoundsMapping,
+    max_resource_bounds: DeprecatedResourceBoundsMapping,
     block_context: BlockContext,
     #[case] version: TransactionVersion,
     #[case] expected_error_prefix: &str,
