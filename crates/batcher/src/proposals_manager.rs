64c50d0ceaae285df10d312a8512b38f12c28aff
Yair Bakalchuk <yair@starkware.co>
2024-08-19 16:40:08 +0300

    feat(batcher): function for waiting for a task to be done

--- crates/batcher/src/proposals_manager.rs
+++ crates/batcher/src/proposals_manager.rs
@@ -1,14 +1,18 @@
 use std::collections::BTreeMap;
 use std::sync::Arc;
 
+#[cfg(test)]
+use mockall::automock;
 use papyrus_config::dumping::{ser_param, SerializeConfig};
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
 use starknet_api::block::BlockNumber;
+use starknet_api::state::StateDiff;
 use starknet_mempool_types::communication::{MempoolClientError, SharedMempoolClient};
+use starknet_mempool_types::mempool_types::ThinTransaction;
 use thiserror::Error;
 use tokio::sync::Mutex;
-use tracing::{debug, error, info, instrument, Instrument};
+use tracing::{debug, error, info, instrument, Instrument, Level};
 
 // TODO: Should be defined in SN_API probably (shared with the consensus).
 pub type ProposalId = u64;
@@ -46,8 +50,12 @@ pub enum ProposalsManagerError {
         current_generating_proposal_id: ProposalId,
         new_proposal_id: ProposalId,
     },
+    #[error("Internal error, check the logs.")]
+    InternalError,
     #[error(transparent)]
     MempoolError(#[from] MempoolClientError),
+    #[error("No proposal is currently being generated.")]
+    NoProposalInGeneration,
 }
 
 pub type ProposalsManagerResult<T> = Result<T, ProposalsManagerError>;
@@ -64,22 +72,30 @@ pub type ProposalsManagerResult<T> = Result<T, ProposalsManagerError>;
 pub(crate) struct ProposalsManager {
     config: ProposalsManagerConfig,
     mempool_client: SharedMempoolClient,
+    // To be able to mock BlockBuilder in tests.
+    block_builder_factory: Arc<dyn BlockBuilderFactory>,
     /// The block proposal that is currently being proposed, if any.
     /// At any given time, there can be only one proposal being actively executed (either proposed
     /// or validated).
     proposal_in_generation: Arc<Mutex<Option<ProposalId>>>,
-    generation_task_handle: Option<tokio::task::JoinHandle<ProposalsManagerResult<()>>>,
+    generation_task_handle:
+        Option<tokio::task::JoinHandle<ProposalsManagerResult<(ProposalId, StateDiff)>>>,
 }
 
 impl ProposalsManager {
     // TODO: Remove dead_code attribute.
     #[allow(dead_code)]
-    pub fn new(config: ProposalsManagerConfig, mempool_client: SharedMempoolClient) -> Self {
+    pub fn new(
+        config: ProposalsManagerConfig,
+        mempool_client: SharedMempoolClient,
+        block_builder_factory: Arc<dyn BlockBuilderFactory>,
+    ) -> Self {
         Self {
             config,
             mempool_client,
             proposal_in_generation: Arc::new(Mutex::new(None)),
             generation_task_handle: None,
+            block_builder_factory,
         }
     }
 
@@ -87,7 +103,7 @@ impl ProposalsManager {
     /// transactions from the mempool.
     // TODO: Understand why clippy is complaining about blocks_in_conditions.
     #[allow(clippy::blocks_in_conditions)]
-    #[instrument(skip(self), err)]
+    #[instrument(skip(self), ret, err)]
     pub async fn generate_block_proposal(
         &mut self,
         proposal_id: ProposalId,
@@ -100,10 +116,28 @@ impl ProposalsManager {
             self.mempool_client.clone(),
             self.proposal_in_generation.clone(),
             self.config.max_txs_per_mempool_request,
+            self.block_builder_factory.clone(),
         );
 
         self.generation_task_handle = Some(tokio::spawn(generate_block_task.in_current_span()));
+        // Allow the task to start.
+        tokio::task::yield_now().await;
         Ok(())
+    }
+
+    /// Waits for the generation task to finish and returns the generated proposal.
+    #[instrument(skip(self), ret(level = Level::TRACE), err)]
+    pub async fn wait_for_ready_block(
+        &mut self,
+    ) -> ProposalsManagerResult<(ProposalId, StateDiff)> {
+        let Some(task_handle) = self.generation_task_handle.take() else {
+            debug!("No proposal is currently being generated.");
+            return Err(ProposalsManagerError::NoProposalInGeneration);
+        };
+        task_handle.await.unwrap_or_else(|join_error| {
+            error!("Error while waiting for the generation task to finish: {}", join_error);
+            Err(ProposalsManagerError::InternalError)
+        })
     }
 
     // Checks if there is already a proposal being generated, and if not, sets the given proposal_id
@@ -129,15 +163,16 @@ impl ProposalsManager {
 
     // Generates a block proposal with transactions from the mempool.
     // Not a passing &self method, because it's called from tokio::spawn.
-    #[instrument(skip(mempool_client), err)]
+    #[instrument(skip(mempool_client, block_builder_factory), ret(level = Level::TRACE), err)]
     async fn generate_block_task(
         mempool_client: SharedMempoolClient,
         proposal_in_generation: Arc<Mutex<Option<ProposalId>>>,
         max_txs_per_mempool_request: usize,
-    ) -> ProposalsManagerResult<()> {
+        block_builder_factory: Arc<dyn BlockBuilderFactory>,
+    ) -> ProposalsManagerResult<(ProposalId, StateDiff)> {
         debug!("Starting a new block proposal generation task.");
         // TODO: notify the mempool that we are starting a new proposal generation.
-        let block_builder = block_builder::BlockBuilder {};
+        let block_builder = block_builder_factory.create_block_builder();
         loop {
             let mempool_txs = mempool_client.get_txs(max_txs_per_mempool_request).await?;
             // TODO: Get L1 transactions.
@@ -152,40 +187,26 @@ impl ProposalsManager {
             // is empty).
             tokio::task::yield_now().await;
         }
-        // TODO: Get state diff.
-        let mut proposal_id = proposal_in_generation.lock().await;
-        *proposal_id = None;
-        Ok(())
+        let Some(finished_proposal_id) = proposal_in_generation.lock().await.take() else {
+            error!("proposal_in_generation is None during the task run.");
+            return Err(ProposalsManagerError::InternalError);
+        };
+
+        debug!("Finished generating proposal with id {}.", finished_proposal_id);
+        let state_diff = block_builder.get_state_diff();
+        Ok((finished_proposal_id, state_diff))
     }
 }
 
-// TODO: Should be defined elsewhere.
+#[cfg_attr(test, automock)]
+pub trait BlockBuilderTrait: Send {
+    /// Returning true if the block is ready to be proposed.
+    fn add_txs(&self, _txs: Vec<ThinTransaction>) -> bool;
+    fn get_state_diff(&self) -> StateDiff;
+}
+
 #[allow(dead_code)]
-mod block_builder {
-    use starknet_api::state::StateDiff;
-    use starknet_mempool_types::mempool_types::ThinTransaction;
-
-    #[derive(Debug, PartialEq)]
-    pub enum Status {
-        Building,
-        Ready,
-        Timeout,
-    }
-
-    pub struct BlockBuilder {}
-
-    impl BlockBuilder {
-        pub fn status(&self) -> Status {
-            Status::Building
-        }
-
-        /// Returning true if the block is ready to be proposed.
-        pub fn add_txs(&self, _txs: Vec<ThinTransaction>) -> bool {
-            false
-        }
-
-        pub fn close_block(&self) -> StateDiff {
-            StateDiff::default()
-        }
-    }
+#[cfg_attr(test, automock)]
+pub(crate) trait BlockBuilderFactory: Send + Sync {
+    fn create_block_builder(&self) -> Box<dyn BlockBuilderTrait>;
 }
--- crates/batcher/src/proposals_manager_test.rs
+++ crates/batcher/src/proposals_manager_test.rs
@@ -2,16 +2,106 @@ use std::sync::Arc;
 
 use assert_matches::assert_matches;
 use starknet_api::block::BlockNumber;
+use starknet_api::state::StateDiff;
 use starknet_mempool_types::communication::MockMempoolClient;
+use starknet_mempool_types::mempool_types::ThinTransaction;
 
-use crate::proposals_manager::{ProposalsManager, ProposalsManagerConfig, ProposalsManagerError};
+use crate::proposals_manager::{
+    MockBlockBuilderFactory,
+    MockBlockBuilderTrait,
+    ProposalsManager,
+    ProposalsManagerConfig,
+    ProposalsManagerError,
+};
 
 #[tokio::test]
-async fn multiple_proposals_generation() {
+async fn proposal_generation_succeeds() {
+    let mut block_builder_factory = MockBlockBuilderFactory::new();
+    block_builder_factory.expect_create_block_builder().once().returning(|| {
+        let mut mock_block_builder = MockBlockBuilderTrait::new();
+        // We expect to add transactions to the arbitrary amount of times.
+        mock_block_builder.expect_add_txs().times(2).returning(|_thin_txs| false);
+        // Now let's return true to finish the block.
+        mock_block_builder.expect_add_txs().once().returning(|_thin_txs| true);
+        mock_block_builder.expect_get_state_diff().once().returning(StateDiff::default);
+
+        Box::new(mock_block_builder)
+    });
+
+    // We expect to fetch transactions 3 times: 2 times when the block is not ready and another time
+    // that will close the block.
+    let mut mempool_client = MockMempoolClient::new();
+    mempool_client
+        .expect_get_txs()
+        .times(3)
+        .returning(|_| Ok(vec![ThinTransaction::default(); 10]));
+
+    let mut proposals_manager = ProposalsManager::new(
+        ProposalsManagerConfig::default(),
+        Arc::new(mempool_client),
+        Arc::new(block_builder_factory),
+    );
+    proposals_manager.generate_block_proposal(0, BlockNumber::default(), None).await.unwrap();
+
+    let (proposal_id, state_diff) = proposals_manager.wait_for_ready_block().await.unwrap();
+
+    assert_eq!(proposal_id, 0);
+    assert_eq!(state_diff, Default::default());
+}
+
+#[tokio::test]
+async fn consecutive_proposals_generation_succeed() {
+    let mut block_builder_factory = MockBlockBuilderFactory::new();
+    block_builder_factory.expect_create_block_builder().times(2).returning(|| {
+        let mut mock_block_builder = MockBlockBuilderTrait::new();
+        // We expect to add transactions to the arbitrary amount of times.
+        mock_block_builder.expect_add_txs().times(2).returning(|_thin_txs| false);
+        // Now let's return true to finish the block.
+        mock_block_builder.expect_add_txs().once().returning(|_thin_txs| true);
+        mock_block_builder.expect_get_state_diff().once().returning(StateDiff::default);
+
+        Box::new(mock_block_builder)
+    });
+
+    // We expect to fetch transactions 6 times: 3 fetches per block. 2 times when the block is not
+    // ready and another time that will close the block.
+    let mut mempool_client = MockMempoolClient::new();
+    mempool_client
+        .expect_get_txs()
+        .times(6)
+        .returning(|_| Ok(vec![ThinTransaction::default(); 10]));
+
+    let mut proposals_manager = ProposalsManager::new(
+        ProposalsManagerConfig::default(),
+        Arc::new(mempool_client),
+        Arc::new(block_builder_factory),
+    );
+    proposals_manager.generate_block_proposal(0, BlockNumber::default(), None).await.unwrap();
+
+    let (proposal_id, state_diff) = proposals_manager.wait_for_ready_block().await.unwrap();
+
+    assert_eq!(proposal_id, 0);
+    assert_eq!(state_diff, Default::default());
+
+    proposals_manager.generate_block_proposal(1, BlockNumber(1), None).await.unwrap();
+
+    let (proposal_id, state_diff) = proposals_manager.wait_for_ready_block().await.unwrap();
+
+    assert_eq!(proposal_id, 1);
+    assert_eq!(state_diff, Default::default());
+}
+
+
+#[tokio::test]
+async fn multiple_proposals_generation_fails() {
+    let block_builder_factory = MockBlockBuilderFactory::new();
     let mut mempool_client = MockMempoolClient::new();
     mempool_client.expect_get_txs().returning(|_| Ok(vec![]));
-    let mut proposals_manager =
-        ProposalsManager::new(ProposalsManagerConfig::default(), Arc::new(mempool_client));
+    let mut proposals_manager = ProposalsManager::new(
+        ProposalsManagerConfig::default(),
+        Arc::new(mempool_client),
+        Arc::new(block_builder_factory),
+    );
     proposals_manager.generate_block_proposal(0, BlockNumber::default(), None).await.unwrap();
 
     let another_generate_request =
