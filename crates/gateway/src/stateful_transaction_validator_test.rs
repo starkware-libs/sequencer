0a628778103afda44388982538826043a6807a10
Arni Hod <arnon@starkware.co>
2024-08-19 15:26:03 +0300

    refactor: share the chain info as a config for the gateway

--- crates/gateway/src/stateful_transaction_validator_test.rs
+++ crates/gateway/src/stateful_transaction_validator_test.rs
@@ -2,7 +2,7 @@ use blockifier::blockifier::stateful_validator::{
     StatefulValidatorError as BlockifierStatefulValidatorError,
     StatefulValidatorResult as BlockifierStatefulValidatorResult,
 };
-use blockifier::context::BlockContext;
+use blockifier::context::ChainInfo;
 use blockifier::test_utils::CairoVersion;
 use blockifier::transaction::errors::{TransactionFeeError, TransactionPreValidationError};
 use mempool_test_utils::starknet_api_test_utils::{
@@ -44,20 +44,8 @@ pub const STATEFUL_VALIDATOR_FEE_ERROR: BlockifierStatefulValidatorError =
     );
 
 #[fixture]
-fn block_context() -> BlockContext {
-    BlockContext::create_for_testing()
-}
-
-#[fixture]
-fn stateful_validator(block_context: BlockContext) -> StatefulTransactionValidator {
-    StatefulTransactionValidator {
-        config: StatefulTransactionValidatorConfig {
-            max_nonce_for_validation_skip: Default::default(),
-            validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
-            max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
-            chain_info: block_context.chain_info().clone(),
-        },
-    }
+fn stateful_validator() -> StatefulTransactionValidator {
+    StatefulTransactionValidator { config: StatefulTransactionValidatorConfig::default() }
 }
 
 // TODO(Arni): consider testing declare and deploy account.
@@ -90,8 +78,8 @@ fn test_stateful_tx_validator(
     assert_eq!(result, expected_result_as_stateful_transaction_result);
 }
 
-#[test]
-fn test_instantiate_validator() {
+#[rstest]
+fn test_instantiate_validator(stateful_validator: StatefulTransactionValidator) {
     let state_reader_factory = local_test_state_reader_factory(CairoVersion::Cairo1, false);
 
     let mut mock_state_reader_factory = MockStateReaderFactory::new();
@@ -111,16 +99,8 @@ fn test_instantiate_validator() {
         .with(eq(latest_block))
         .return_once(move |_| state_reader);
 
-    let block_context = &BlockContext::create_for_testing();
-    let stateful_validator = StatefulTransactionValidator {
-        config: StatefulTransactionValidatorConfig {
-            max_nonce_for_validation_skip: Default::default(),
-            validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
-            max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
-            chain_info: block_context.chain_info().clone(),
-        },
-    };
-    let blockifier_validator = stateful_validator.instantiate_validator(&mock_state_reader_factory);
+    let blockifier_validator = stateful_validator
+        .instantiate_validator(&mock_state_reader_factory, &ChainInfo::create_for_testing());
     assert!(blockifier_validator.is_ok());
 }
 
