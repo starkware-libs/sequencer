7a53f190507b233a75c2f4f5c9f216e4a45affc4
Arni Hod <arnon@starkware.co>
2024-08-19 17:29:40 +0300

    chore: streamline the starknet api contract class object

--- crates/starknet_api/src/contract_class.rs
+++ crates/starknet_api/src/contract_class.rs
@@ -1,23 +1,11 @@
 use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
 use serde::{Deserialize, Serialize};
 
-/// Compiled contract class.
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub enum ContractClass {
-    V1(ContractClassV1),
-}
-
-/// Compiled contract class variant for Cairo 1 contracts.
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub enum ContractClassV1 {
-    Casm(CasmContractClass),
-}
-
 /// All relevant information about a declared contract class, including the compiled contract class
 /// and other parameters derived from the original declare transaction required for billing.
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct ClassInfo {
-    pub contract_class: ContractClass,
+    pub casm_contract_class: CasmContractClass,
     pub sierra_program_length: usize,
     pub abi_length: usize,
 }
