caa12c9ab63d21733a9c38a102f2f31f456ddba4
Dori Medini <dori@starkware.co>
2024-08-20 16:40:02 +0300

    chore(ci): workspace version alignment and tests
    Signed-off-by: Dori Medini <dori@starkware.co>

---
+++ workspace_tests/version_integrity_test.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum DependencyValue {
+    String(String),
+    Object { version: String, path: Option<String> },
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Package {
+    version: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct WorkspaceFields {
+    package: Package,
+    members: Vec<String>,
+    dependencies: HashMap<String, DependencyValue>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct CargoToml {
+    workspace: WorkspaceFields,
+}
+
+struct LocalCrate {
+    name: String,
+    path: String,
+    version: String,
+}
+
+static ROOT_TOML: Lazy<CargoToml> = Lazy::new(|| {
+    let root_toml: CargoToml = toml::from_str(include_str!("../Cargo.toml")).unwrap();
+    root_toml
+});
+
+impl CargoToml {
+    fn local_crates(&self) -> Vec<LocalCrate> {
+        self.workspace
+            .dependencies
+            .iter()
+            .filter_map(|(name, value)| {
+                if let DependencyValue::Object { path, version } = value {
+                    if let Some(path) = path {
+                        return Some(LocalCrate {
+                            name: name.to_string(),
+                            path: path.to_string(),
+                            version: version.to_string(),
+                        });
+                    }
+                }
+                None
+            })
+            .collect()
+    }
+
+    fn workspace_version(&self) -> &str {
+        &self.workspace.package.version
+    }
+}
+
+// Tests.
+
+#[test]
+fn test_local_dependencies_are_members() {
+    for LocalCrate { path, name, .. } in ROOT_TOML.local_crates().iter() {
+        assert!(
+            ROOT_TOML.workspace.members.contains(&path),
+            "Crate '{name}' at path '{path}' is not a member of the workspace."
+        );
+    }
+}
+
+#[test]
+fn test_version_alignment() {
+    let workspace_version = ROOT_TOML.workspace_version();
+    for LocalCrate { name, version, .. } in ROOT_TOML.local_crates().iter() {
+        assert_eq!(
+            workspace_version, version,
+            "Crate '{name}' has version '{version}', instead of '{workspace_version}'."
+        );
+    }
+}
