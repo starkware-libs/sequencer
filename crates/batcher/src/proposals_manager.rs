d63909a06cad0cfaea58f037c2f5091e1884cddf
Yair Bakalchuk <yair@starkware.co>
2024-08-20 17:50:27 +0300

    feat(batcher): mock BlockBuilder for tests

--- crates/batcher/src/proposals_manager.rs
+++ crates/batcher/src/proposals_manager.rs
@@ -1,6 +1,9 @@
 use std::collections::BTreeMap;
 use std::sync::Arc;
 
+use async_trait::async_trait;
+#[cfg(test)]
+use mockall::automock;
 use papyrus_config::dumping::{ser_param, SerializeConfig};
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
@@ -10,7 +13,7 @@ use starknet_mempool_types::communication::{MempoolClientError, SharedMempoolCli
 use thiserror::Error;
 use tokio::sync::Mutex;
 use tokio_stream::wrappers::ReceiverStream;
-use tracing::{debug, error, info, instrument};
+use tracing::{debug, error, info, instrument, Instrument};
 
 // TODO: Should be defined in SN_API probably (shared with the consensus).
 pub type ProposalId = u64;
@@ -81,13 +84,24 @@ pub(crate) struct ProposalsManager {
     /// At any given time, there can be only one proposal being actively executed (either proposed
     /// or validated).
     proposal_in_generation: Arc<Mutex<Option<ProposalId>>>,
+    // To be able to mock BlockBuilder in tests.
+    block_builder_factory: Arc<dyn BlockBuilderFactory>,
 }
 
 impl ProposalsManager {
     // TODO: Remove dead_code attribute.
     #[allow(dead_code)]
-    pub fn new(config: ProposalsManagerConfig, mempool_client: SharedMempoolClient) -> Self {
-        Self { config, mempool_client, proposal_in_generation: Arc::new(Mutex::new(None)) }
+    pub fn new(
+        config: ProposalsManagerConfig,
+        mempool_client: SharedMempoolClient,
+        block_builder_factory: Arc<dyn BlockBuilderFactory>,
+    ) -> Self {
+        Self {
+            config,
+            mempool_client,
+            proposal_in_generation: Arc::new(Mutex::new(None)),
+            block_builder_factory,
+        }
     }
 
     /// Starts a new block proposal generation task for the given proposal_id and height with
@@ -110,10 +124,12 @@ impl ProposalsManager {
                 timeout,
                 mempool_client: self.mempool_client.clone(),
                 max_txs_per_mempool_request: self.config.max_txs_per_mempool_request,
-                sender,
+                sender: Arc::new(sender),
                 proposal_in_generation: self.proposal_in_generation.clone(),
+                block_builder_factory: self.block_builder_factory.clone(),
             }
-            .run(),
+            .run()
+            .in_current_span(),
         );
 
         Ok(ReceiverStream::new(receiver))
@@ -140,39 +156,20 @@ impl ProposalsManager {
     }
 }
 
-// TODO: Should be defined elsewhere.
+#[async_trait]
+pub trait BlockBuilderTrait: Send {
+    /// Returning true if the block is ready to be proposed.
+    async fn add_txs_and_stream(
+        &self,
+        txs: Vec<Transaction>,
+        sender: Arc<tokio::sync::mpsc::Sender<Transaction>>,
+    ) -> bool;
+}
+
 #[allow(dead_code)]
-mod block_builder {
-    use starknet_api::executable_transaction::Transaction;
-    use starknet_api::state::StateDiff;
-
-    #[derive(Debug, PartialEq)]
-    pub enum Status {
-        Building,
-        Ready,
-        Timeout,
-    }
-
-    pub struct BlockBuilder {}
-
-    impl BlockBuilder {
-        pub fn status(&self) -> Status {
-            Status::Building
-        }
-
-        /// Returning true if the block is ready to be proposed.
-        pub fn add_txs_and_stream(
-            &self,
-            _txs: &[Transaction],
-            _sender: &tokio::sync::mpsc::Sender<Transaction>,
-        ) -> bool {
-            false
-        }
-
-        pub fn close_block(&self) -> StateDiff {
-            StateDiff::default()
-        }
-    }
+#[cfg_attr(test, automock)]
+pub(crate) trait BlockBuilderFactory: Send + Sync {
+    fn create_block_builder(&self) -> Box<dyn BlockBuilderTrait>;
 }
 
 #[allow(dead_code)]
@@ -180,14 +177,15 @@ struct ProposalGenerationTask {
     pub timeout: tokio::time::Instant,
     pub mempool_client: SharedMempoolClient,
     pub max_txs_per_mempool_request: usize,
-    pub sender: tokio::sync::mpsc::Sender<Transaction>,
+    pub sender: Arc<tokio::sync::mpsc::Sender<Transaction>>,
     pub proposal_in_generation: Arc<Mutex<Option<ProposalId>>>,
+    pub block_builder_factory: Arc<dyn BlockBuilderFactory>,
 }
 
 impl ProposalGenerationTask {
     #[allow(dead_code)]
     async fn run(self) -> ProposalsManagerResult<()> {
-        let block_builder = block_builder::BlockBuilder {};
+        let block_builder = self.block_builder_factory.create_block_builder();
         loop {
             if tokio::time::Instant::now() > self.timeout {
                 info!("Proposal reached timeout.");
@@ -204,8 +202,7 @@ impl ProposalGenerationTask {
             debug!("Adding {} mempool transactions to proposal in generation.", mempool_txs.len());
             // TODO: This is cpu bound operation, should use spawn_blocking / Rayon / std::thread
             // here or from inside the function.
-            let is_block_ready =
-                block_builder.add_txs_and_stream(mempool_txs.as_slice(), &self.sender);
+            let is_block_ready = block_builder.add_txs_and_stream(mempool_txs, self.sender.clone()).await;
             if is_block_ready {
                 break;
             }
