0365936b60c2c532c4bc64aec71ea6181ff4500f
Nimrod Weiss <nimrod@starkware.co>
2024-08-20 14:32:59 +0300

    build(fee): tx_context and create_tx_info cannot_fail

--- crates/blockifier/src/transaction/errors.rs
+++ crates/blockifier/src/transaction/errors.rs
@@ -112,8 +112,6 @@ pub enum TransactionExecutionError {
     InvalidSegmentStructure(usize, usize),
     #[error(transparent)]
     ProgramError(#[from] ProgramError),
-    #[error(transparent)]
-    TransactionInfoCreationError(#[from] TransactionInfoCreationError),
 }
 
 #[derive(Debug, Error)]
@@ -139,13 +137,4 @@ pub enum ParseError {
 pub enum NumericConversionError {
     #[error("Conversion of {0} to u128 unsuccessful.")]
     U128ToUsizeError(u128),
-}
-
-// TODO(Nimrod): Delete this error once `create_tx_info` stops returning a `Result`.
-#[derive(Debug, Error)]
-pub enum TransactionInfoCreationError {
-    #[error("Invalid ResourceMapping combination was given: {0}")]
-    InvalidResourceMapping(String),
-    #[error(transparent)]
-    StarknetAPIError(#[from] StarknetApiError),
 }
