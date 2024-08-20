37203b7a0ade575be10228b51fa9a8e54a2ce52a
Alon Lukatch <alon.l@starkware.co>
2024-08-20 12:56:46 +0300

    fix(network): add node version to identifys agent version

--- crates/papyrus_network/src/mixed_behaviour.rs
+++ crates/papyrus_network/src/mixed_behaviour.rs
@@ -63,6 +63,7 @@ impl MixedBehaviour {
         bootstrap_peer_multiaddr: Option<Multiaddr>,
         streamed_bytes_config: sqmr::Config,
         chain_id: ChainId,
+        version: Option<String>,
     ) -> Self {
         let public_key = keypair.public();
         let local_peer_id = PeerId::from_public_key(&public_key);
@@ -84,10 +85,16 @@ impl MixedBehaviour {
                     )
                 })
                 .into(),
-            identify: identify::Behaviour::new(identify::Config::new(
-                IDENTIFY_PROTOCOL_VERSION.to_string(),
-                public_key,
-            )),
+            identify: match version {
+                Some(version) => identify::Behaviour::new(
+                    identify::Config::new(IDENTIFY_PROTOCOL_VERSION.to_string(), public_key)
+                        .with_agent_version(version),
+                ),
+                None => identify::Behaviour::new(identify::Config::new(
+                    IDENTIFY_PROTOCOL_VERSION.to_string(),
+                    public_key,
+                )),
+            },
             // TODO: change kademlia protocol name
             kademlia: kad::Behaviour::with_config(
                 local_peer_id,
--- crates/papyrus_network/src/network_manager/mod.rs
+++ crates/papyrus_network/src/network_manager/mod.rs
@@ -549,7 +549,7 @@ fn send_now<Item>(sender: &mut GenericSender<Item>, item: Item, buffer_full_mess
 pub type NetworkManager = GenericNetworkManager<Swarm<mixed_behaviour::MixedBehaviour>>;
 
 impl NetworkManager {
-    pub fn new(config: NetworkConfig) -> Self {
+    pub fn new(config: NetworkConfig, version: Option<String>) -> Self {
         let NetworkConfig {
             tcp_port,
             quic_port: _,
@@ -571,6 +571,7 @@ impl NetworkManager {
                 bootstrap_peer_multiaddr.clone(),
                 sqmr::Config { session_timeout },
                 chain_id,
+                version,
             )
         });
         Self::generic_new(swarm)
