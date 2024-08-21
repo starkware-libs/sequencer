3c862cce022d5e526182f8a4ef9b6666441a99fc
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-08-21 13:01:49 +0300

    feat: add native syscall handler

--- crates/blockifier/src/execution/native.rs
+++ crates/blockifier/src/execution/native.rs
@@ -1,1 +1,2 @@
+pub mod syscall_handler;
 pub mod utils;
--- crates/blockifier/src/execution/native/utils.rs
+++ crates/blockifier/src/execution/native/utils.rs
@@ -1,11 +1,48 @@
 use cairo_lang_starknet_classes::contract_class::ContractEntryPoint;
+use itertools::Itertools;
 use num_traits::ToBytes;
 use starknet_api::core::EntryPointSelector;
 use starknet_types_core::felt::Felt;
+
+#[cfg(test)]
+#[path = "utils_test.rs"]
+pub mod test;
 
 pub fn contract_entrypoint_to_entrypoint_selector(
     entrypoint: &ContractEntryPoint,
 ) -> EntryPointSelector {
     let selector_felt = Felt::from_bytes_be_slice(&entrypoint.selector.to_be_bytes());
     EntryPointSelector(selector_felt)
+}
+
+pub fn encode_str_as_felts(msg: &str) -> Vec<Felt> {
+    const CHUNK_SIZE: usize = 32;
+
+    let data = msg.as_bytes().chunks(CHUNK_SIZE - 1);
+    let mut encoding = vec![Felt::default(); data.len()];
+    for (i, data_chunk) in data.enumerate() {
+        let mut chunk = [0_u8; CHUNK_SIZE];
+        chunk[1..data_chunk.len() + 1].copy_from_slice(data_chunk);
+        encoding[i] = Felt::from_bytes_be(&chunk);
+    }
+    encoding
+}
+
+pub fn decode_felts_as_str(encoding: &[Felt]) -> String {
+    let bytes_err: Vec<_> =
+        encoding.iter().flat_map(|felt| felt.to_bytes_be()[1..32].to_vec()).collect();
+
+    match String::from_utf8(bytes_err) {
+        Ok(s) => s.trim_matches('\0').to_owned(),
+        Err(_) => {
+            let err_msgs = encoding
+                .iter()
+                .map(|felt| match String::from_utf8(felt.to_bytes_be()[1..32].to_vec()) {
+                    Ok(s) => format!("{} ({})", s.trim_matches('\0'), felt),
+                    Err(_) => felt.to_string(),
+                })
+                .join(", ");
+            format!("[{}]", err_msgs)
+        }
+    }
 }
