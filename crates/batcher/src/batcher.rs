774c9a3b8c366669f750b359e8c749da2099ae22
Yair Bakalchuk <yair@starkware.co>
2024-08-22 15:11:57 +0300

    feat(batcher): generate proposal from a batcher server callback

--- crates/batcher/src/batcher.rs
+++ crates/batcher/src/batcher.rs
@@ -2,16 +2,19 @@ use std::sync::Arc;
 
 #[cfg(test)]
 use mockall::automock;
+use starknet_batcher_types::batcher_types::BuildProposalInput;
+use starknet_batcher_types::errors::BatcherError;
 use starknet_mempool_infra::starters::DefaultComponentStarter;
 use starknet_mempool_types::communication::SharedMempoolClient;
 
 use crate::config::BatcherConfig;
+use crate::proposal_manager::{BlockBuilderFactory, ProposalManager, ProposalManagerError};
 
-// TODO(Tsabary/Yael/Dafna): Replace with actual batcher code.
 pub struct Batcher {
     pub config: BatcherConfig,
     pub mempool_client: SharedMempoolClient,
     pub storage: Arc<dyn BatcherStorageReaderTrait>,
+    proposal_manager: ProposalManager,
 }
 
 impl Batcher {
@@ -20,7 +23,46 @@ impl Batcher {
         mempool_client: SharedMempoolClient,
         storage: Arc<dyn BatcherStorageReaderTrait>,
     ) -> Self {
-        Self { config, mempool_client, storage }
+        Self {
+            config: config.clone(),
+            mempool_client: mempool_client.clone(),
+            storage,
+            proposal_manager: ProposalManager::new(
+                config.proposal_manager.clone(),
+                mempool_client.clone(),
+                Arc::new(BlockBuilderFactory {}),
+            ),
+        }
+    }
+
+    pub async fn build_proposal(
+        &mut self,
+        build_proposal_input: BuildProposalInput,
+    ) -> Result<(), BatcherError> {
+        // TODO: Save the receiver as a stream for later use.
+        let (content_sender, _content_receiver) =
+            tokio::sync::mpsc::channel(self.config.outstream_content_buffer_size);
+        let deadline =
+            tokio::time::Instant::from_std(build_proposal_input.deadline_as_instant().map_err(
+                |_| BatcherError::TimeToDeadlineError { deadline: build_proposal_input.deadline },
+            )?);
+        self.proposal_manager
+            .build_block_proposal(build_proposal_input.proposal_id, deadline, content_sender)
+            .await
+            .map_err(|err| match err {
+                ProposalManagerError::AlreadyGeneratingProposal {
+                    current_generating_proposal_id,
+                    new_proposal_id,
+                } => BatcherError::ServerBusy {
+                    active_proposal_id: current_generating_proposal_id,
+                    new_proposal_id,
+                },
+                ProposalManagerError::MempoolError(..) => BatcherError::InternalError,
+                ProposalManagerError::ProposalAlreadyExists { proposal_id } => {
+                    BatcherError::ProposalAlreadyExists { proposal_id }
+                }
+            })?;
+        Ok(())
     }
 }
 
