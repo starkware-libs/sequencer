344d014a6dfea6cfd42eb92121fb921c52db2f2d
nadin <nadin@starkware.co>
2024-08-24 22:16:19 +0300

    feat(ci): add no path dependencies test

--- workspace_tests/version_integrity_test.rs
+++ workspace_tests/version_integrity_test.rs
@@ -3,7 +3,7 @@ use crate::toml_utils::{LocalCrate, ROOT_TOML};
 #[test]
 fn test_path_dependencies_are_members() {
     let non_member_path_crates: Vec<_> = ROOT_TOML
-        .path_dependencies()
+        .workspace_path_dependencies()
         .filter(|LocalCrate { path, .. }| !ROOT_TOML.members().contains(path))
         .collect();
     assert!(
@@ -17,7 +17,7 @@ fn test_path_dependencies_are_members() {
 fn test_version_alignment() {
     let workspace_version = ROOT_TOML.workspace_version();
     let crates_with_incorrect_version: Vec<_> = ROOT_TOML
-        .path_dependencies()
+        .workspace_path_dependencies()
         .filter(|LocalCrate { version, .. }| version != workspace_version)
         .collect();
     assert!(
