f57228946825a8878665f54eaf68efcc53a20cb0
nadin <nadin@starkware.co>
2024-08-24 22:16:19 +0300

    feat(ci): add no path dependencies test

--- workspace_tests/toml_utils.rs
+++ workspace_tests/toml_utils.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 use std::sync::LazyLock;
 
 use serde::{Deserialize, Serialize};
@@ -8,6 +10,7 @@ use serde::{Deserialize, Serialize};
 pub(crate) enum DependencyValue {
     String(String),
     Object { version: String, path: Option<String> },
+    CrateObject { workspace: Option<bool>, features: Option<Vec<String>>, path: Option<String> },
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -27,6 +30,11 @@ pub(crate) struct CargoToml {
     workspace: WorkspaceFields,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct CrateCargoToml {
+    dependencies: Option<HashMap<String, DependencyValue>>,
+}
+
 #[derive(Debug)]
 pub(crate) struct LocalCrate {
     pub(crate) path: String,
@@ -41,7 +49,15 @@ pub(crate) static ROOT_TOML: LazyLock<CargoToml> = LazyLock::new(|| {
 });
 
 impl CargoToml {
-    pub(crate) fn path_dependencies(&self) -> impl Iterator<Item = LocalCrate> + '_ {
+    pub(crate) fn members(&self) -> &Vec<String> {
+        &self.workspace.members
+    }
+
+    pub(crate) fn workspace_version(&self) -> &str {
+        &self.workspace.package.version
+    }
+
+    pub(crate) fn workspace_path_dependencies(&self) -> impl Iterator<Item = LocalCrate> + '_ {
         self.workspace.dependencies.iter().filter_map(|(_name, value)| {
             if let DependencyValue::Object { path: Some(path), version } = value {
                 Some(LocalCrate { path: path.to_string(), version: version.to_string() })
@@ -51,11 +67,41 @@ impl CargoToml {
         })
     }
 
-    pub(crate) fn members(&self) -> &Vec<String> {
-        &self.workspace.members
+    pub(crate) fn member_cargo_tomls(&self) -> Vec<CrateCargoToml> {
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let crates_dir = format!("{}/../", manifest_dir);
+
+        let mut cargo_tomls: Vec<CrateCargoToml> = Vec::new();
+
+        for member in self.members() {
+            let cargo_toml_path = Path::new(&crates_dir).join(member).join("Cargo.toml");
+
+            let cargo_toml_content = fs::read_to_string(&cargo_toml_path)
+                .expect(&format!("Failed to read {:?}", cargo_toml_path));
+
+            let cargo_toml: CrateCargoToml = toml::from_str(&cargo_toml_content).unwrap();
+            cargo_tomls.push(cargo_toml);
+        }
+        cargo_tomls
+    }
+}
+
+impl CrateCargoToml {
+    pub(crate) fn has_dependencies(&self) -> bool {
+        self.dependencies.is_some()
     }
 
-    pub(crate) fn workspace_version(&self) -> &str {
-        &self.workspace.package.version
+    pub(crate) fn crate_path_dependencies(&self) -> impl Iterator<Item = LocalCrate> + '_ {
+        if let Some(dependencies) = &self.dependencies {
+            dependencies.iter().filter_map(|(_name, value)| {
+                if let DependencyValue::Object { path: Some(path), version } = value {
+                    Some(LocalCrate { path: path.to_string(), version: version.to_string() })
+                } else {
+                    None
+                }
+            })
+        } else {
+            panic!("No dependencies exist");
+        }
     }
 }
--- workspace_tests/version_integrity_test.rs
+++ workspace_tests/version_integrity_test.rs
@@ -3,7 +3,7 @@ use crate::toml_utils::{LocalCrate, ROOT_TOML};
 #[test]
 fn test_path_dependencies_are_members() {
     let non_member_path_crates: Vec<_> = ROOT_TOML
-        .path_dependencies()
+        .workspace_path_dependencies()
         .filter(|LocalCrate { path, .. }| !ROOT_TOML.members().contains(path))
         .collect();
     assert!(
@@ -17,7 +17,7 @@ fn test_path_dependencies_are_members() {
 fn test_version_alignment() {
     let workspace_version = ROOT_TOML.workspace_version();
     let crates_with_incorrect_version: Vec<_> = ROOT_TOML
-        .path_dependencies()
+        .workspace_path_dependencies()
         .filter(|LocalCrate { version, .. }| version != workspace_version)
         .collect();
     assert!(
@@ -25,4 +25,21 @@ fn test_version_alignment() {
         "The following crates have versions different from the workspace version \
          '{workspace_version}': {crates_with_incorrect_version:?}."
     );
+}
+
+#[test]
+fn validate_no_path_dependencies() {
+    let mut all_crate_paths: Vec<LocalCrate> = Vec::new();
+    for crate_cargo_toml in ROOT_TOML.member_cargo_tomls().iter() {
+        if crate_cargo_toml.has_dependencies() {
+            let crate_paths: Vec<LocalCrate> = crate_cargo_toml.crate_path_dependencies().collect();
+            all_crate_paths.extend(crate_paths);
+        } else {
+            println!("No dependencies exist");
+        }
+        assert!(
+            all_crate_paths.is_empty(),
+            "The following crates have path dependency {all_crate_paths:?}."
+        );
+    }
 }
