19027bfdc85be07ac75aad9c2288f789b9251bd6
eitanm-starkware <eitan.m@starkware.co>
2024-08-25 15:30:17 +0300

    fix(network): increase sleep for e2e broadcast test

--- crates/papyrus_network/src/e2e_broadcast_test.rs
+++ crates/papyrus_network/src/e2e_broadcast_test.rs
@@ -102,7 +102,7 @@ async fn broadcast_subscriber_end_to_end_test() {
             TIMEOUT, async move {
                 // TODO(shahak): Remove this sleep once we fix the bug of broadcasting while there
                 // are no peers.
-                tokio::time::sleep(Duration::from_millis(100)).await;
+                tokio::time::sleep(Duration::from_secs(1)).await;
                 let number1 = Number(1);
                 let number2 = Number(2);
                 let mut broadcasted_messages_receiver2_1 =
