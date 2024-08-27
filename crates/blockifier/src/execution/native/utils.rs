3ad9b4b071a2c3cf3ae5d0e7aefa1f5a505242a8
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-08-27 12:23:44 +0300

    fix: address some of the review comments

--- crates/blockifier/src/execution/native/utils.rs
+++ crates/blockifier/src/execution/native/utils.rs
@@ -1,11 +1,9 @@
 use cairo_lang_starknet_classes::contract_class::ContractEntryPoint;
-use num_traits::ToBytes;
 use starknet_api::core::EntryPointSelector;
 use starknet_types_core::felt::Felt;
 
 pub fn contract_entrypoint_to_entrypoint_selector(
     entrypoint: &ContractEntryPoint,
 ) -> EntryPointSelector {
-    let selector_felt = Felt::from_bytes_be_slice(&entrypoint.selector.to_be_bytes());
-    EntryPointSelector(selector_felt)
+    EntryPointSelector(Felt::from(&entrypoint.selector))
 }
