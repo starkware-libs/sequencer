8682c8bb8f1b32a11e3c75cd84fe03da4a9a344d
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-08-28 16:05:21 +0200

    fix: make secp module `pub` to allow usage on snos
    * fix: pub for fields for SecpGetPointFromXRequest
    
    * fix: pub for secp module

--- crates/blockifier/src/execution/syscalls/secp.rs
+++ crates/blockifier/src/execution/syscalls/secp.rs
@@ -169,10 +169,10 @@ pub fn secp256r1_add(
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct SecpGetPointFromXRequest {
-    x: BigUint,
+    pub x: BigUint,
     // The parity of the y coordinate, assuming a point with the given x coordinate exists.
     // True means the y coordinate is odd.
-    y_parity: bool,
+    pub y_parity: bool,
 }
 
 impl SyscallRequest for SecpGetPointFromXRequest {
