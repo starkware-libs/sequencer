5bb3556711852768f2653effb76f3e5cb7555bb5
Ayelet Zilber <ayelet@starkware.co>
2024-08-29 17:31:17 +0300

    feat(mempool): add insert func to suspended tx pool

--- crates/mempool/src/suspended_transaction_pool.rs
+++ crates/mempool/src/suspended_transaction_pool.rs
@@ -4,11 +4,19 @@ use starknet_api::core::{ContractAddress, Nonce};
 
 use crate::mempool::TransactionReference;
 
-type AddressNonceToTransaction = HashMap<(ContractAddress, Nonce), TransactionReference>;
+type _AddressNonceToTransaction = HashMap<(ContractAddress, Nonce), TransactionReference>;
 
-#[derive(Debug, Default)]
-pub struct SuspendedTransactionPool {
-    _suspended_tx_pool: AddressNonceToTransaction,
+#[derive(Debug)]
+pub struct _SuspendedTransactionPool {
+    suspended_tx_pool: _AddressNonceToTransaction,
 }
 
-impl SuspendedTransactionPool {}
+impl _SuspendedTransactionPool {
+    pub fn _insert(&mut self, tx: TransactionReference) {
+        assert_eq!(
+            self.suspended_tx_pool.insert((tx.sender_address, tx.nonce), tx),
+            None,
+            "Keys should be unique; duplicates are checked prior."
+        );
+    }
+}
