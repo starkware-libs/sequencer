2ea95c6a623d9eccf3e4f508c147f865f8fd2796
Arni Hod <arnon@starkware.co>
2024-09-01 17:27:35 +0300

    chore: define creator for executable transaction

--- crates/gateway/src/utils.rs
+++ crates/gateway/src/utils.rs
@@ -6,6 +6,11 @@ use blockifier::transaction::transactions::{
     InvokeTransaction as BlockifierInvokeTransaction,
 };
 use starknet_api::core::{calculate_contract_address, ChainId, ClassHash, ContractAddress};
+use starknet_api::executable_transaction::{
+    DeployAccountTransaction as ExecutableDeployAccountTransaction,
+    InvokeTransaction as ExecutableInvokeTransaction,
+    Transaction as ExecutableTransaction,
+};
 use starknet_api::rpc_transaction::{
     RpcDeclareTransaction,
     RpcDeployAccountTransaction,
@@ -22,8 +27,62 @@ use starknet_api::transaction::{
 };
 use tracing::error;
 
-use crate::errors::{GatewaySpecError, StatefulTransactionValidatorResult};
+use crate::compilation::GatewayCompiler;
+use crate::errors::{GatewayResult, GatewaySpecError, StatefulTransactionValidatorResult};
 
+/// Converts an RPC transaction to an executable transaction.
+/// Note, for declare transaction this step is heavy, as it requires compilation of Sierra to
+/// executable contract class.
+pub fn compile_contract_and_build_executable_tx(
+    rpc_tx: RpcTransaction,
+    gateway_compiler: &GatewayCompiler,
+    chain_id: &ChainId,
+) -> GatewayResult<ExecutableTransaction> {
+    Ok(match rpc_tx {
+        RpcTransaction::Declare(rpc_declare_tx) => {
+            let executable_declare_tx =
+                gateway_compiler.process_declare_tx(rpc_declare_tx, chain_id).map_err(|error| {
+                    error!(
+                        "Failed to convert RPC declare transaction to executable transaction: {}",
+                        error
+                    );
+                    GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+                })?;
+            ExecutableTransaction::Declare(executable_declare_tx)
+        }
+        RpcTransaction::DeployAccount(rpc_deploy_account_tx) => {
+            let executable_deploy_account_tx =
+                ExecutableDeployAccountTransaction::from_rpc_tx(rpc_deploy_account_tx, chain_id)
+                    .map_err(|error| {
+                        error!(
+                            "Failed to convert RPC deploy account transaction to executable \
+                             transaction: {}",
+                            error
+                        );
+                        GatewaySpecError::UnexpectedError {
+                            data: "Internal server error".to_owned(),
+                        }
+                    })?;
+            ExecutableTransaction::DeployAccount(executable_deploy_account_tx)
+        }
+        RpcTransaction::Invoke(rpc_invoke_tx) => {
+            let executable_invoke_tx = ExecutableInvokeTransaction::from_rpc_tx(
+                rpc_invoke_tx,
+                chain_id,
+            )
+            .map_err(|error| {
+                error!(
+                    "Failed to convert RPC invoke transaction to executable transaction: {}",
+                    error
+                );
+                GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+            })?;
+            ExecutableTransaction::Invoke(executable_invoke_tx)
+        }
+    })
+}
+
+// TODO(Arni): Remove this function.
 pub fn rpc_tx_to_account_tx(
     rpc_tx: &RpcTransaction,
     // FIXME(yael 15/4/24): calculate class_info inside the function once compilation code is ready
