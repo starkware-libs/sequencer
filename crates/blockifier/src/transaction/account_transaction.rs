4d8394d61ec389ee9d1b98e66af879fb6116eaee
meship-starkware <meshi@starkware.co>
2024-09-03 13:58:43 +0300

    refactor(blockifier): add getters for exclusive v3 txs fields

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -3,11 +3,15 @@ use std::sync::Arc;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use starknet_api::calldata;
 use starknet_api::core::{ContractAddress, EntryPointSelector, Nonce};
+use starknet_api::data_availability::DataAvailabilityMode;
 use starknet_api::deprecated_contract_class::EntryPointType;
 use starknet_api::transaction::{
     Calldata,
+    DeprecatedResourceBoundsMapping,
     Fee,
+    PaymasterData,
     ResourceBounds,
+    Tip,
     TransactionHash,
     TransactionSignature,
     TransactionVersion,
@@ -128,7 +132,15 @@ impl HasRelatedFeeType for AccountTransaction {
 }
 
 impl AccountTransaction {
-    implement_account_tx_inner_getters!((signature, TransactionSignature), (nonce, Nonce));
+    implement_account_tx_inner_getters!(
+        (signature, TransactionSignature),
+        (nonce, Nonce),
+        (resource_bounds, DeprecatedResourceBoundsMapping),
+        (tip, Tip),
+        (nonce_data_availability_mode, DataAvailabilityMode),
+        (fee_data_availability_mode, DataAvailabilityMode),
+        (paymaster_data, PaymasterData)
+    );
 
     pub fn sender_address(&self) -> ContractAddress {
         match self {
