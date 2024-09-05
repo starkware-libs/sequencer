57a3c0470edba331614f2f6f082a9bfd6924543e
Alon Lukatch <alon.l@starkware.co>
2024-09-05 12:00:14 +0300

    draft

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -1,20 +1,25 @@
 use std::env;
 use std::io::Result;
 
+use tonic_build::{configure, Builder};
+
 fn main() -> Result<()> {
     println!("Building");
     let (protoc_bin, _) = protoc_prebuilt::init("27.0").unwrap();
     env::set_var("PROTOC", protoc_bin);
-    prost_build::compile_protos(
-        &[
-            "src/proto/p2p/proto/class.proto",
-            "src/proto/p2p/proto/event.proto",
-            "src/proto/p2p/proto/header.proto",
-            "src/proto/p2p/proto/state.proto",
-            "src/proto/p2p/proto/transaction.proto",
-            "src/proto/p2p/proto/consensus.proto",
-        ],
-        &["src/proto/"],
-    )?;
+    let builder: Builder = configure();
+    builder
+        .compile(
+            &[
+                "src/proto/p2p/proto/class.proto",
+                "src/proto/p2p/proto/event.proto",
+                "src/proto/p2p/proto/header.proto",
+                "src/proto/p2p/proto/state.proto",
+                "src/proto/p2p/proto/transaction.proto",
+                "src/proto/p2p/proto/consensus.proto",
+            ],
+            &["src/proto/"],
+        )
+        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
     Ok(())
 }
