13235ae04e61e49936a980e447cb38fbec1d3c69
Alon-Lukatch-Starkware <alon.l@starkware.co>
2024-09-05 12:03:31 +0300

    feat(network): change broadcast api to allow serialization of BroadcastedMessageManager (#695)
    * feat(network): change broadcast api to allow serialization of BroadcastedMessageManager
    
    * feat(network): fix CR comments

--- crates/papyrus_network/src/network_manager/test_utils.rs
+++ crates/papyrus_network/src/network_manager/test_utils.rs
@@ -5,8 +5,10 @@ use futures::sink::With;
 use futures::stream::Map;
 use futures::{FutureExt, SinkExt, StreamExt};
 use libp2p::gossipsub::SubscriptionError;
+use libp2p::PeerId;
 
 use super::{
+    BroadcastClientChannels,
     BroadcastedMessageManager,
     GenericReceiver,
     ReportReceiver,
@@ -74,8 +76,36 @@ where
 }
 
 pub fn create_test_broadcasted_message_manager() -> BroadcastedMessageManager {
-    let (report_sender, _report_receiver) = oneshot::channel::<()>();
-    BroadcastedMessageManager { report_sender }
+    BroadcastedMessageManager { peer_id: PeerId::random() }
+}
+
+// TODO: remove either this method or the one below
+// TODO: also return reported_messages_receiver and continue_propagation_receiver, possibly wrapped
+// in a struct
+pub fn create_test_broadcast_client_channels<T>()
+-> (Sender<(Bytes, BroadcastedMessageManager)>, BroadcastClientChannels<T>)
+where
+    T: TryFrom<Bytes>,
+{
+    let (broadcasted_messages_sender, broadcasted_messages_receiver) =
+        futures::channel::mpsc::channel(CHANNEL_BUFFER_SIZE);
+    let (reported_messages_sender, _mock_reported_messages_receiver) =
+        futures::channel::mpsc::channel(CHANNEL_BUFFER_SIZE);
+    let (continue_propagation_sender, _mock_continue_propagation_receiver) =
+        futures::channel::mpsc::channel(CHANNEL_BUFFER_SIZE);
+
+    let broadcasted_messages_fn: BroadcastReceivedMessagesConverterFn<T> =
+        |(x, broadcasted_message_manager)| (T::try_from(x), broadcasted_message_manager);
+    let broadcasted_messages_receiver = broadcasted_messages_receiver.map(broadcasted_messages_fn);
+
+    (
+        broadcasted_messages_sender,
+        BroadcastClientChannels {
+            broadcasted_messages_receiver,
+            reported_messages_sender: Box::new(reported_messages_sender),
+            continue_propagation_sender: Box::new(continue_propagation_sender),
+        },
+    )
 }
 
 const CHANNEL_BUFFER_SIZE: usize = 10000;
@@ -98,8 +128,20 @@ where
         |(x, report_sender)| (T::try_from(x), report_sender);
     let broadcasted_messages_receiver = broadcasted_messages_receiver.map(broadcasted_messages_fn);
 
+    let (reported_messages_sender, _mock_reported_messages_receiver) =
+        futures::channel::mpsc::channel(CHANNEL_BUFFER_SIZE);
+
+    let (continue_propagation_sender, _mock_continue_propagation_receiver) =
+        futures::channel::mpsc::channel(CHANNEL_BUFFER_SIZE);
+
+    let broadcast_client_channels = BroadcastClientChannels {
+        broadcasted_messages_receiver,
+        reported_messages_sender: Box::new(reported_messages_sender),
+        continue_propagation_sender: Box::new(continue_propagation_sender),
+    };
+
     let subscriber_channels =
-        BroadcastTopicChannels { messages_to_broadcast_sender, broadcasted_messages_receiver };
+        BroadcastTopicChannels { messages_to_broadcast_sender, broadcast_client_channels };
 
     let mock_broadcasted_messages_fn: MockBroadcastedMessagesFn<T> =
         |(x, report_call_back)| ready(Ok((Bytes::from(x), report_call_back)));
