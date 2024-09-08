8a31645d9620b369d61f7b1eaad242fcd90428d7
Yael Doweck <yael@starkware.co>
2024-09-08 14:07:33 +0300

    feat(batcher): adding block_builder build_block functionality

--- crates/batcher/src/lib.rs
+++ crates/batcher/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod batcher;
 pub mod block_builder;
+#[cfg(test)]
+mod block_builder_test;
 pub mod communication;
 pub mod config;
 pub mod fee_market;
@@ -7,3 +9,5 @@ pub mod papyrus_state;
 mod proposal_manager;
 #[cfg(test)]
 mod proposal_manager_test;
+#[cfg(test)]
+mod test_utils;
