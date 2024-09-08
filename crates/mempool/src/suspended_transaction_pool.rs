c9094e9e0e9a9981cf52e149a58af9685389979c
Ayelet Zilber <ayelet@starkware.co>
2024-09-08 14:40:14 +0300

    feat(mempool): add Evictable trait for SuspendedTransactionPool

--- crates/mempool/src/suspended_transaction_pool.rs
+++ crates/mempool/src/suspended_transaction_pool.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use starknet_api::core::{ContractAddress, Nonce};
 
+use crate::eviction_manager::_Evictable;
 use crate::mempool::TransactionReference;
 
 type AddressNonceToTransaction = HashMap<(ContractAddress, Nonce), TransactionReference>;
@@ -11,4 +12,16 @@ pub struct SuspendedTransactionPool {
     _suspended_tx_pool: AddressNonceToTransaction,
 }
 
-impl SuspendedTransactionPool {}
+impl _Evictable for SuspendedTransactionPool {
+    fn _insert(&mut self, _tx: TransactionReference) -> bool {
+        todo!()
+    }
+
+    fn _suggest_tx_to_evict(&self) -> Option<TransactionReference> {
+        todo!()
+    }
+
+    fn _align_with_current_state(&mut self, _address: ContractAddress, _nonce: Nonce) {
+        todo!()
+    }
+}
