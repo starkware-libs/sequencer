2f7d3cacbc86f194e08bc8f9f83690ff8f1714af
Arni Hod <arnon@starkware.co>
2024-09-09 11:55:24 +0300

    test: use the exeutable tx creator test util for the account tx creator test util

--- crates/blockifier/src/test_utils/invoke.rs
+++ crates/blockifier/src/test_utils/invoke.rs
@@ -1,18 +1,13 @@
-use starknet_api::executable_transaction::{
-    AccountTransaction as ExecutableTransaction,
-    InvokeTransaction as ExecutableInvokeTransaction,
-};
+use starknet_api::executable_transaction::AccountTransaction as ExecutableTransaction;
 use starknet_api::test_utils::invoke::InvokeTxArgs;
 
 use crate::transaction::account_transaction::AccountTransaction;
 
 pub fn invoke_tx(invoke_args: InvokeTxArgs) -> AccountTransaction {
-    let tx_hash = invoke_args.tx_hash;
     let only_query = invoke_args.only_query;
-    let invoke_tx = starknet_api::test_utils::invoke::invoke_tx(invoke_args);
-
-    let invoke_tx =
-        ExecutableTransaction::Invoke(ExecutableInvokeTransaction { tx: invoke_tx, tx_hash });
+    let invoke_tx = ExecutableTransaction::Invoke(
+        starknet_api::test_utils::invoke::executable_invoke_tx(invoke_args),
+    );
 
     match only_query {
         true => AccountTransaction::new_for_query(invoke_tx),
