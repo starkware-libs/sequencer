32cd9503ec27ada691f03b361ac59ca40ecd4a88
Yair Bakalchuk <yair@starkware.co>
2024-09-09 12:51:53 +0300

    feat(batcher): batcher client trait

--- crates/batcher_types/src/errors.rs
+++ crates/batcher_types/src/errors.rs
@@ -1,9 +1,20 @@
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-// TODO(Tsabary/Yael/Dafna): Populate with actual errors.
+use crate::batcher_types::ProposalId;
+
 #[derive(Clone, Debug, Error, PartialEq, Eq, Serialize, Deserialize)]
 pub enum BatcherError {
-    #[error("Placeholder error message")]
-    Placeholder,
+    #[error(
+        "There is already an active proposal {}, can't start proposal {}.",
+        active_proposal_id,
+        new_proposal_id
+    )]
+    ServerBusy { active_proposal_id: ProposalId, new_proposal_id: ProposalId },
+    #[error("Internal server error.")]
+    InternalError,
+    #[error("Proposal with ID {proposal_id} already exists.")]
+    ProposalAlreadyExists { proposal_id: ProposalId },
+    #[error("Proposal with ID {proposal_id} not found.")]
+    ProposalNotFound { proposal_id: ProposalId },
 }
