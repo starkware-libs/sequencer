e36c0d03f9b3cc7f4981a50a7613b0db8434992e
Arni Hod <arnon@starkware.co>
2024-09-09 10:37:55 +0300

    test: streamline the v0 version tx for invoke tx towards code dedup in test utils

--- crates/blockifier/src/test_utils/invoke.rs
+++ crates/blockifier/src/test_utils/invoke.rs
@@ -1,26 +1,13 @@
 use starknet_api::test_utils::invoke::InvokeTxArgs;
-use starknet_api::transaction::{InvokeTransactionV0, TransactionHash, TransactionVersion};
+use starknet_api::transaction::TransactionHash;
 
-use crate::abi::abi_utils::selector_from_name;
-use crate::transaction::constants::EXECUTE_ENTRY_POINT_NAME;
 use crate::transaction::transactions::InvokeTransaction;
 
 pub fn invoke_tx(invoke_args: InvokeTxArgs) -> InvokeTransaction {
     let default_tx_hash = TransactionHash::default();
     let only_query = invoke_args.only_query;
     // TODO: Make TransactionVersion an enum and use match here.
-    let invoke_tx = if invoke_args.version == TransactionVersion::ZERO {
-        starknet_api::transaction::InvokeTransaction::V0(InvokeTransactionV0 {
-            max_fee: invoke_args.max_fee,
-            calldata: invoke_args.calldata,
-            contract_address: invoke_args.sender_address,
-            signature: invoke_args.signature,
-            // V0 transactions should always select the `__execute__` entry point.
-            entry_point_selector: selector_from_name(EXECUTE_ENTRY_POINT_NAME),
-        })
-    } else {
-        starknet_api::test_utils::invoke::invoke_tx(invoke_args)
-    };
+    let invoke_tx = starknet_api::test_utils::invoke::invoke_tx(invoke_args);
 
     match only_query {
         true => InvokeTransaction::new_for_query(invoke_tx, default_tx_hash),
