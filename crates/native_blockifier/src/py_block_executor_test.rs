114e3b9cda4b1e3ecea408d1d8907dc0cb000045
Tzahi Taub <tzahi@starkware.co>
2024-09-15 15:42:40 +0300

    feat(blockifier): add RunResource to distinguis vm and sierra run

--- crates/native_blockifier/src/py_block_executor_test.rs
+++ crates/native_blockifier/src/py_block_executor_test.rs
@@ -19,7 +19,9 @@ use crate::test_utils::MockStorage;
 #[test]
 fn global_contract_cache_update() {
     // Initialize executor and set a contract class on the state.
-    let casm = CasmContractClass::default();
+    let mut casm = CasmContractClass::default();
+    // TODO(tzahi): Remove once the default value of compiler version is of a legal value.
+    casm.compiler_version = "0.1.0".to_string();
     let contract_class = ContractClass::V1(ContractClassV1::try_from(casm.clone()).unwrap());
     let class_hash = class_hash!("0x1");
 
