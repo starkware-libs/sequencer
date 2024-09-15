9fe43d7e7e94cb58249878a1972daf7dd2b4f175
Itay Tsabary <itayt@starkware.co>
2024-09-15 20:32:10 +0300

    feat: add internal_add_tx to gateway
    commit-id:48cabf08

--- crates/gateway/src/communication.rs
+++ crates/gateway/src/communication.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use starknet_mempool_infra::component_server::{create_empty_server, EmptyServer};
 
 use crate::gateway::Gateway;
@@ -6,4 +7,18 @@ pub type GatewayServer = EmptyServer<Gateway>;
 
 pub fn create_gateway_server(gateway: Gateway) -> GatewayServer {
     create_empty_server(gateway)
+}
+use starknet_gateway_types::communication::{GatewayRequest, GatewayResponse};
+use starknet_gateway_types::errors::GatewayError;
+use starknet_mempool_infra::component_definitions::ComponentRequestHandler;
+
+#[async_trait]
+impl ComponentRequestHandler<GatewayRequest, GatewayResponse> for Gateway {
+    async fn handle_request(&mut self, request: GatewayRequest) -> GatewayResponse {
+        match request {
+            GatewayRequest::AddTransaction(gateway_input) => GatewayResponse::AddTransaction(
+                self.add_tx(gateway_input.rpc_tx).await.map_err(GatewayError::GatewaySpecError),
+            ),
+        }
+    }
 }
