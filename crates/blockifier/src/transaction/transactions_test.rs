10ca196563f5a5992a2f45c96f40fe78c547e87e
Aviv Greenburg <aviv.g@starkware.co>
2024-09-15 16:11:44 +0300

    refactor(blockifier): remove enforce fee from actual fee

--- crates/blockifier/src/transaction/transactions_test.rs
+++ crates/blockifier/src/transaction/transactions_test.rs
@@ -1345,10 +1345,11 @@ fn test_declare_tx(
         None,
         ExecutionSummary::default(),
     );
-
+    // For V0 transactions, the max fee is always 0.
+    let max_fee = if tx_version == TransactionVersion::ZERO { Fee(0) } else { Fee(MAX_FEE) };
     let account_tx = declare_tx(
         declare_tx_args! {
-            max_fee: Fee(MAX_FEE),
+            max_fee,
             sender_address,
             version: tx_version,
             resource_bounds: max_l1_resource_bounds,
@@ -1367,7 +1368,8 @@ fn test_declare_tx(
     );
     let fee_type = &account_tx.fee_type();
     let tx_context = &block_context.to_tx_context(&account_tx);
-    let actual_execution_info = account_tx.execute(state, block_context, true, true).unwrap();
+    let actual_execution_info =
+        account_tx.execute(state, block_context, tx_context.tx_info.enforce_fee(), true).unwrap(); //aviv: changed from true, true
 
     // Build expected validate call info.
     let expected_validate_call_info = declare_validate_callinfo(
@@ -1382,14 +1384,24 @@ fn test_declare_tx(
     );
 
     // Build expected fee transfer call info.
-    let expected_actual_fee =
-        actual_execution_info.receipt.resources.calculate_tx_fee(block_context, fee_type);
-    let expected_fee_transfer_call_info = expected_fee_transfer_call_info(
-        tx_context,
-        sender_address,
-        expected_actual_fee,
-        FeatureContract::ERC20(CairoVersion::Cairo0).get_class_hash(),
-    );
+    // aviv added 'if' condition. not pretty solution:
+    let expected_actual_fee = if tx_context.tx_info.signed_version() == TransactionVersion::ZERO {
+        Fee(0)
+    } else {
+        actual_execution_info.receipt.resources.calculate_tx_fee(block_context, fee_type)
+    };
+    // aviv added 'if' condition. not pretty solution:
+    let expected_fee_transfer_call_info =
+        if tx_context.tx_info.signed_version() == TransactionVersion::ZERO {
+            None
+        } else {
+            expected_fee_transfer_call_info(
+                tx_context,
+                sender_address,
+                expected_actual_fee,
+                FeatureContract::ERC20(CairoVersion::Cairo0).get_class_hash(),
+            )
+        };
 
     let da_gas = starknet_resources.get_state_changes_cost(use_kzg_da);
     let expected_cairo_resources = get_expected_cairo_resources(
