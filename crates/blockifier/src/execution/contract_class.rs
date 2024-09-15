28c3e233662bf86ee796ddffcb5d54d18b6cc4fd
Tzahi Taub <tzahi@starkware.co>
2024-09-15 15:42:40 +0300

    wip

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -18,8 +18,9 @@ use cairo_vm::types::program::Program;
 use cairo_vm::types::relocatable::MaybeRelocatable;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use itertools::Itertools;
+use semver::Version;
 use serde::de::Error as DeserializationError;
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize};
 use starknet_api::core::EntryPointSelector;
 use starknet_api::deprecated_contract_class::{
     ContractClass as DeprecatedContractClass,
@@ -44,6 +45,19 @@ use crate::transaction::errors::TransactionExecutionError;
 pub mod test;
 
 pub type ContractClassResult<T> = Result<T, ContractClassError>;
+
+/// The resource used to run a contract function.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize)]
+pub enum RunResource {
+    CairoSteps, // AKA VM mode.
+    SierraGas,  // AKA Sierra mode.
+}
+
+impl Default for RunResource {
+    fn default() -> Self {
+        RunResource::CairoSteps
+    }
+}
 
 /// Represents a runnable Starknet contract class (meaning, the program is runnable by the VM).
 #[derive(Clone, Debug, Eq, PartialEq, derive_more::From)]
@@ -93,11 +107,19 @@ impl ContractClass {
             ContractClass::V1(class) => class.bytecode_length(),
         }
     }
+
+    /// Returns whether this contract should run using VM resources or Sierra gas.
+    pub fn run_resource(&self, min_sierra_version: Version) -> RunResource {
+        match self {
+            ContractClass::V0(_) => RunResource::CairoSteps,
+            ContractClass::V1(contract_class) => contract_class.run_resource(min_sierra_version),
+        }
+    }
 }
 
 // V0.
 
-/// Represents a runnable Cario 0 Starknet contract class (meaning, the program is runnable by the
+/// Represents a runnable Cairo 0 Starknet contract class (meaning, the program is runnable by the
 /// VM). We wrap the actual class in an Arc to avoid cloning the program when cloning the
 /// class.
 // Note: when deserializing from a SN API class JSON string, the ABI field is ignored
@@ -142,6 +164,10 @@ impl ContractClassV0 {
             n_memory_holes: 0,
             builtin_instance_counter: HashMap::from([(BuiltinName::pedersen, hashed_data_size)]),
         }
+    }
+
+    pub fn run_resource(&self) -> RunResource {
+        RunResource::CairoSteps
     }
 
     pub fn try_from_json_string(raw_contract_class: &str) -> Result<ContractClassV0, ProgramError> {
@@ -219,6 +245,17 @@ impl ContractClassV1 {
                 selector: call.entry_point_selector,
                 typ: call.entry_point_type,
             }),
+        }
+    }
+
+    /// Returns whether this contract should run using VM resources or Sierra gas.
+    pub fn run_resource(&self, min_sierra_version: &Version) -> RunResource {
+        let res = Version::parse(&self.compiler_version);
+        if res.is_ok_and(|version| &version <= min_sierra_version) {
+            RunResource::SierraGas
+        } else {
+            // If version is not a valid semver, fall back to VM execution.
+            RunResource::CairoSteps
         }
     }
 
