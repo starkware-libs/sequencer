4b0018270b578a7219eed3bdabe810554e9ce72b
Aviv Greenburg <aviv.g@starkware.co>
2024-09-15 16:11:44 +0300

    chore(blockifier): have charge_fee flag represent enforce_fee return value

--- crates/blockifier/src/concurrency/versioned_state_test.rs
+++ crates/blockifier/src/concurrency/versioned_state_test.rs
@@ -269,14 +269,17 @@ fn test_run_parallel_txs(max_l1_resource_bounds: ValidResourceBounds) {
 
     let block_context_1 = block_context.clone();
     let block_context_2 = block_context.clone();
+
     // Execute transactions
     thread::scope(|s| {
         s.spawn(move || {
-            let result = account_tx_1.execute(&mut state_1, &block_context_1, true, true);
+            let charge_fee_1 = account_tx_1.create_tx_info().enforce_fee();
+            let result = account_tx_1.execute(&mut state_1, &block_context_1, charge_fee_1, true);
             assert_eq!(result.is_err(), enforce_fee);
         });
         s.spawn(move || {
-            account_tx_2.execute(&mut state_2, &block_context_2, true, true).unwrap();
+            let charge_fee_2 = account_tx_2.create_tx_info().enforce_fee();
+            account_tx_2.execute(&mut state_2, &block_context_2, charge_fee_2, true).unwrap();
             // Check that the constructor wrote ctor_arg to the storage.
             let storage_key = get_storage_var_address("ctor_arg", &[]);
             let deployed_contract_address = calculate_contract_address(
