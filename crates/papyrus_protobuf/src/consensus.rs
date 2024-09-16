9c736f5c299592dd1e8d0653f2daa1b8c7bab813
Guy Nir <guy.n@starkware.co>
2024-09-16 10:50:00 +0300

    feat: add handler to reconstruct stream of messages

--- crates/papyrus_protobuf/src/consensus.rs
+++ crates/papyrus_protobuf/src/consensus.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use futures::channel::{mpsc, oneshot};
 use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::core::ContractAddress;
@@ -50,6 +52,98 @@ pub struct StreamMessage<T: Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufCon
     pub stream_id: u64,
     pub chunk_id: u64,
     pub fin: bool,
+}
+
+pub struct StreamHandler<T: Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufConversionError> {
+    pub sender: mpsc::Sender<StreamMessage<T>>, 
+    pub receiver: mpsc::Receiver<StreamMessage<T>>,
+    
+    // these dictionaries are keyed on the stream_id
+    pub next_chunk_ids: BTreeMap<u64, u32>,
+
+    // there is a separate message buffer for each stream,
+    // each message buffer is keyed by the chunk_id of the first message in 
+    // a contiguous sequence of messages (saved in a Vec)
+    pub message_buffers: BTreeMap<u64, BTreeMap<u32, Vec<StreamMessage<T>>>>,
+
+}
+
+// impl<T: Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufConversionError>> 
+impl<T: Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufConversionError>> StreamHandler<T>{
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel(100);
+        StreamHandler {
+            sender,
+            receiver,
+            next_chunk_ids: BTreeMap::new(),
+            message_buffers: BTreeMap::new(),
+        }
+    }
+
+    pub async fn send(&mut self, message: StreamMessage<T>) {
+        self.sender.try_send(message).expect("Send should succeed");
+    }
+
+    pub async fn receive(&mut self) -> StreamMessage<T> {
+        self.receiver.try_next().await.expect("Receive should succeed").expect("Receive should succeed")
+    }
+
+    pub async fn listen(&mut self) {
+        loop {
+            let message = self.receive().await;
+            let stream_id = message.stream_id;
+            let chunk_id = message.chunk_id;
+            let next_chunk_id = self.next_chunk_ids.entry(stream_id).or_insert(0);
+            
+            // this means we can just send the message without buffering it
+            if chunk_id == *next_chunk_id {
+                self.send(message).await;
+                *next_chunk_id += 1;
+                if message.fin {  // remove the buffer if the stream is finished
+                    self.message_buffers.remove(&stream_id);  
+                }
+            } else {  // save the message in the buffer. 
+                self.store(message);
+            }
+        }
+        
+    }
+    
+    fn store(&mut self, message: StreamMessage<T>) {
+        let stream_id = message.stream_id;
+        let chunk_id = message.chunk_id;
+        let mut buffer = self.message_buffers.entry(stream_id).or_insert(BTreeMap::new());
+        for id in buffer.keys() {
+            if id > chunk_id {
+                buffer.insert(chunk_id, vec![message]);
+                return;
+            }
+        }
+        // self.message_buffers
+        //     .entry(stream_id)
+        //     .or_insert(BTreeMap::new())
+        //     .entry(chunk_id)
+        //     .or_insert(Vec::new())
+        //     .push(message);
+    }
+
+    // Tries to drain as many messages as possible from the buffer (in order)
+    // DOES NOT guarantee that the buffer will be empty after calling this function
+    fn drain_buffer(&mut self, stream_id: u64) {
+        if let Some(mut buffer) = self.message_buffers.get(&stream_id){
+            let mut chunk_id = self.next_chunk_ids.entry(stream_id).or_insert(0);
+
+            // try to drain each vec of message one by one, if they are in order
+            while let Some(messages) = buffer.get(chunk_id) {
+                for message in messages {
+                    self.send(message);
+                    *chunk_id += 1;
+                }
+                buffer.remove(chunk_id);
+            }
+        }
+    }
+
 }
 
 // TODO(Guy): Remove after implementing broadcast streams.
