cfff0f39a75c84b004ce8711979bc20a7a223677
Yair Bakalchuk <yair@starkware.co>
2024-09-16 10:07:28 +0300

    refactor(batcher): rename files

--- crates/batcher/src/lib.rs
+++ crates/batcher/src/lib.rs
@@ -2,6 +2,6 @@ pub mod batcher;
 pub mod communication;
 pub mod config;
 pub mod fee_market;
-pub mod proposals_manager;
+pub mod proposal_manager;
 #[cfg(test)]
-mod proposals_manager_test;
+mod proposal_manager_test;
