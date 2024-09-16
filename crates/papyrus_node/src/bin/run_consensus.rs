a796d5f642ec59979c18add750328c30ba91b685
Matan Markind <matan@starkware.co>
2024-09-16 19:58:17 +0300

    refactor(consensus): move test config out of NodeConfig

--- crates/papyrus_node/src/bin/run_consensus.rs
+++ crates/papyrus_node/src/bin/run_consensus.rs
@@ -1,5 +1,6 @@
 use std::env::args;
 
+use clap::Parser;
 use futures::stream::StreamExt;
 use papyrus_config::ConfigError;
 use papyrus_consensus::config::ConsensusConfig;
@@ -13,6 +14,67 @@ use papyrus_storage::StorageReader;
 use starknet_api::block::BlockNumber;
 use tokio::task::JoinHandle;
 use tracing::info;
+
+// Test arguments passed on the command line are prefixed with `test.<ARG_NAME>`.
+const TEST_ARG_PREFIX: &str = "--test.";
+
+/// Split the elements of `input_args` into 2 groups:
+/// 1. Those prefixed with "--test."
+/// 2. Other.
+///
+/// Presumes input is: program_name (--flag_name value)*
+pub fn split_args(input_args: Vec<String>) -> (Vec<String>, Vec<String>) {
+    input_args[1..].chunks(2).fold(
+        (vec![input_args[0].clone()], vec![input_args[0].clone()]),
+        |(mut matching_args, mut mismatched_args), input_arg| {
+            let (name, value) = (&input_arg[0], &input_arg[1]);
+            // String leading `--` for comparison.
+            if &name[..TEST_ARG_PREFIX.len()] == TEST_ARG_PREFIX {
+                matching_args.push(format!("--{}", name[TEST_ARG_PREFIX.len()..].to_string()));
+                matching_args.push(value.clone());
+            } else {
+                mismatched_args.push(name.clone());
+                mismatched_args.push(value.clone());
+            }
+            (matching_args, mismatched_args)
+        },
+    )
+}
+
+/// Build both the node and test configs from the command line arguments.
+pub fn build_configs<T: Parser + Default>() -> Result<(T, NodeConfig), ConfigError> {
+    let input_args = args().collect::<Vec<_>>();
+    let (test_input_args, node_input_args) = split_args(input_args);
+    dbg!(&test_input_args, &node_input_args);
+
+    let mut test_config = T::default();
+    test_config.update_from(test_input_args.iter());
+
+    let node_config = NodeConfig::load_and_process(node_input_args);
+    if let Err(ConfigError::CommandInput(clap_err)) = node_config {
+        clap_err.exit();
+    }
+    let node_config = node_config?;
+    Ok((test_config, node_config))
+}
+
+#[derive(Parser, Debug)]
+struct TestConfig {
+    #[arg(long)]
+    pub network_topic: String,
+
+    #[arg(long)]
+    pub num: i32,
+
+    #[arg(long)]
+    pub num_validators: Option<u64>,
+}
+
+impl Default for TestConfig {
+    fn default() -> Self {
+        Self { network_topic: String::from(""), num: -1, num_validators: None }
+    }
+}
 
 fn build_consensus(
     config: &ConsensusConfig,
@@ -57,19 +119,16 @@ fn build_consensus(
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config = NodeConfig::load_and_process(args().collect());
-    if let Err(ConfigError::CommandInput(clap_err)) = config {
-        clap_err.exit();
-    }
-    let config = config?;
+    let (test_config, node_config) = build_configs::<TestConfig>()?;
+    dbg!(&test_config);
 
-    let mut utils = PapyrusUtilities::new(&config)?;
+    let mut utils = PapyrusUtilities::new(&node_config)?;
     let mut tasks = PapyrusTaskHandles::default();
 
     tasks.consensus_handle = build_consensus(
-        config.consensus.as_ref().unwrap(),
+        node_config.consensus.as_ref().unwrap(),
         utils.storage_reader.clone(),
         utils.maybe_network_manager.as_mut().unwrap(),
     )?;
-    run(config, utils, tasks).await
+    run(node_config, utils, tasks).await
 }
