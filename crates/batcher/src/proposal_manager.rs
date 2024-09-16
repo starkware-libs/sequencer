532f389a14c1e8bf2bee494fef341c97a9428d0b
Yair Bakalchuk <yair@starkware.co>
2024-09-16 10:07:28 +0300

    refactor(batcher): rename files

--- crates/batcher/src/proposal_manager.rs
+++ crates/batcher/src/proposal_manager.rs
@@ -7,8 +7,8 @@ use serde::{Deserialize, Serialize};
 use starknet_api::executable_transaction::Transaction;
 use starknet_mempool_types::communication::{MempoolClientError, SharedMempoolClient};
 use thiserror::Error;
+use tokio::select;
 use tokio::sync::Mutex;
-use tokio::{pin, select};
 use tokio_stream::wrappers::ReceiverStream;
 use tracing::{debug, error, info, instrument, trace, Instrument};
 
@@ -178,63 +178,69 @@ struct BuildProposalTask {
 #[allow(dead_code)]
 impl BuildProposalTask {
     async fn run(mut self) -> ProposalsManagerResult<()> {
-        let res = {
-            // TODO: Should we use a different config for the stream buffer size?
-            // We convert the receiver to a stream and pass it to the block builder while using the
-            // sender to feed the stream.
-            let (mempool_tx_sender, mempool_tx_receiver) =
-                tokio::sync::mpsc::channel::<Transaction>(self.max_txs_per_mempool_request);
-            let mempool_tx_stream = ReceiverStream::new(mempool_tx_receiver);
-            // Need to pin the future to be able to use it in multiple select! expressions.
-            // See: https://docs.rs/tokio/latest/tokio/macro.select.html#:~:text=Using%20the%20same%20future%20in%20multiple%20select!%20expressions%20can%20be%20done%20by%20passing%20a%20reference%20to%20the%20future.%20Doing%20so%20requires%20the%20future%20to%20be%20Unpin.%20A%20future%20can%20be%20made%20Unpin%20by%20either%20using%20Box%3A%3Apin%20or%20stack%20pinning.
-            let building_future = self.block_builder.build_block(
-                self.deadline,
-                mempool_tx_stream,
-                self.output_content_sender.clone(),
-            );
-            pin!(building_future);
-            loop {
-                select! {
-                    // This will send txs from the mempool to the stream we provided to the block builder.
-                    fetch_result = Self::feed_more_mempool_txs(
-                        &self.mempool_client,
-                        self.max_txs_per_mempool_request,
-                        &mempool_tx_sender,
-                    ) => {
-                        if let Err(err) = fetch_result {
-                            error!("Failed to feed more mempool txs: {}.", err);
-                            // TODO: Notify the mempool about remaining txs.
-                            break Err(err);
-                        }
-                        continue;
-                    },
-                    builder_done = &mut building_future => {
-                        info!("Block builder finished.");
-                        break Ok(builder_done);
-                    }
-                };
+        // TODO: Should we use a different config for the stream buffer size?
+        // We convert the receiver to a stream and pass it to the block builder while using the
+        // sender to feed the stream.
+        let (mempool_tx_sender, mempool_tx_receiver) =
+            tokio::sync::mpsc::channel::<Transaction>(self.max_txs_per_mempool_request);
+        let mempool_tx_stream = ReceiverStream::new(mempool_tx_receiver);
+        let building_future = self.block_builder.build_block(
+            self.deadline,
+            mempool_tx_stream,
+            self.output_content_sender.clone(),
+        );
+
+        let feed_mempool_txs_future = Self::feed_mempool_txs(
+            &self.mempool_client,
+            self.max_txs_per_mempool_request,
+            &mempool_tx_sender,
+        );
+
+        // Wait for either the block builder to finish or the feeding of transactions to error.
+        // The other task will be cancelled.
+        let res = select! {
+            // This will send txs from the mempool to the stream we provided to the block builder.
+            feeding_error = feed_mempool_txs_future => {
+                error!("Failed to feed more mempool txs: {}.", feeding_error);
+                // TODO: Notify the mempool about remaining txs.
+                Err(feeding_error)
+            },
+            builder_done = building_future => {
+                info!("Block builder finished.");
+                Ok(builder_done)
             }
         };
         self.active_proposal_finished().await;
         res
     }
 
-    async fn feed_more_mempool_txs(
+    /// Feeds transactions from the mempool to the mempool_tx_sender channel.
+    /// Returns only on error or when the task is cancelled.
+    async fn feed_mempool_txs(
         mempool_client: &SharedMempoolClient,
         max_txs_per_mempool_request: usize,
         mempool_tx_sender: &tokio::sync::mpsc::Sender<Transaction>,
-    ) -> ProposalsManagerResult<()> {
-        // TODO: Get L1 transactions.
-        let mempool_txs = mempool_client.get_txs(max_txs_per_mempool_request).await?;
-        trace!("Feeding {} transactions from the mempool to the block builder.", mempool_txs.len());
-        for tx in mempool_txs {
-            mempool_tx_sender.send(tx).await.map_err(|err| {
-                // TODO: should we return the rest of the txs to the mempool?
-                error!("Failed to send transaction to the block builder: {}.", err);
-                ProposalManagerError::InternalError
-            })?;
+    ) -> ProposalManagerError {
+        loop {
+            // TODO: Get L1 transactions.
+            let mempool_txs = match mempool_client.get_txs(max_txs_per_mempool_request).await {
+                Ok(txs) => txs,
+                Err(e) => return e.into(),
+            };
+            trace!(
+                "Feeding {} transactions from the mempool to the block builder.",
+                mempool_txs.len()
+            );
+            for tx in mempool_txs {
+                if let Err(e) = mempool_tx_sender.send(tx).await.map_err(|err| {
+                    // TODO: should we return the rest of the txs to the mempool?
+                    error!("Failed to send transaction to the block builder: {}.", err);
+                    ProposalManagerError::InternalError
+                }) {
+                    return e;
+                }
+            }
         }
-        Ok(())
     }
 
     async fn active_proposal_finished(&mut self) {
