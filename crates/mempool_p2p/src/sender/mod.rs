b25359ea75d036d556d29b7ff995c75e1b528f9c
Alon Lukatch <alon.l@starkware.co>
2024-09-17 15:58:34 +0300

    cr comments draft

--- crates/mempool_p2p/src/sender/mod.rs
+++ crates/mempool_p2p/src/sender/mod.rs
@@ -18,3 +18,22 @@ impl ComponentRequestHandler<MempoolP2pSenderRequest, MempoolP2pSenderResponse>
         unimplemented!()
     }
 }
+
+pub struct EmptyMempoolP2pSenderClient;
+
+#[async_trait]
+impl MempoolP2pSenderClient for EmptyMempoolP2pSenderClient {
+    async fn add_transaction(
+        &self,
+        _transaction: RpcTransaction,
+    ) -> MempoolP2pSenderClientResult<()> {
+        Ok(())
+    }
+
+    async fn continue_propagation(
+        &self,
+        _propagation_manager: BroadcastedMessageManager,
+    ) -> MempoolP2pSenderClientResult<()> {
+        Ok(())
+    }
+}
