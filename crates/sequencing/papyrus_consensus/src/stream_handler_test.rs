c6ada2688aea8da1f9d332718642b5505c6c308a
Guy Nir <guy.n@starkware.co>
2024-09-17 15:12:32 +0300

    feat: add checking for max buffered messages and max streams

--- crates/sequencing/papyrus_consensus/src/stream_handler_test.rs
+++ crates/sequencing/papyrus_consensus/src/stream_handler_test.rs
@@ -26,15 +26,23 @@ mod tests {
         matching == a.len() && matching == b.len()
     }
 
+    fn setup_test() -> (
+        StreamHandler<ConsensusMessage>,
+        futures::channel::mpsc::Sender<StreamMessage<ConsensusMessage>>,
+        futures::channel::mpsc::Receiver<StreamMessage<ConsensusMessage>>,
+    ) {
+        let (tx_input, rx_input) =
+            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
+        let (tx_output, rx_output) =
+            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
+        let config = StreamHandlerConfig::default();
+        let handler = StreamHandler::new(config, tx_output, rx_input);
+        (handler, tx_input, rx_output)
+    }
+
     #[tokio::test]
     async fn test_stream_handler_in_order() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, mut rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let config = StreamHandlerConfig::default();
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, mut rx_output) = setup_test();
 
         let stream_id = 127;
         for i in 0..10 {
@@ -66,14 +74,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_stream_handler_in_reverse() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, mut rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let mut config = StreamHandlerConfig::default();
-        config.timeout_millis = Some(100);
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, mut rx_output) = setup_test();
+        h.config.timeout_millis = Some(100);
 
         let stream_id = 127;
         for i in 0..5 {
@@ -122,14 +124,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_stream_handler_multiple_streams() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, mut rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let mut config = StreamHandlerConfig::default();
-        config.timeout_millis = Some(100);
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, mut rx_output) = setup_test();
+        h.config.timeout_millis = Some(100);
 
         let stream_id1 = 127; // send all messages in order (except the first one)
         let stream_id2 = 10; // send in reverse order (except the first one)
@@ -159,7 +155,7 @@ mod tests {
             h
         });
         let mut h = join_handle.await.expect("Task should succeed");
-        // println!("Handler.message_buffers: {:?}", h.message_buffers);
+
         assert!(do_vecs_match(&h.message_buffers.clone().into_keys().collect(), &vec![1, 10, 127]));
 
         // the first case we have all message from 1 to 9 buffered into one contiguous sequence
@@ -282,16 +278,10 @@ mod tests {
     #[tokio::test]
     #[should_panic]
     async fn test_stream_handler_duplicate_message_fails() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, _rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let mut config = StreamHandlerConfig::default();
-        config.timeout_millis = Some(100);
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, _rx_output) = setup_test();
         tx_input.try_send(make_random_message(13, 42, false)).expect("Send should succeed");
         tx_input.try_send(make_random_message(13, 42, false)).expect("Send should succeed");
+        tx_input.close_channel(); // this should signal the handler to break out of the loop
 
         // this should panic since we are sending the same message twice!
         let join_handle = tokio::spawn(async move {
@@ -306,16 +296,10 @@ mod tests {
     #[tokio::test]
     #[should_panic]
     async fn test_stream_handler_after_fin_message_fails() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, _rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let mut config = StreamHandlerConfig::default();
-        config.timeout_millis = Some(100);
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, _rx_output) = setup_test();
         tx_input.try_send(make_random_message(13, 42, true)).expect("Send should succeed");
         tx_input.try_send(make_random_message(13, 45, false)).expect("Send should succeed");
+        tx_input.close_channel(); // this should signal the handler to break out of the loop
 
         // this should panic since the fin was received on chunk_id 42, but we are sending 45
         let join_handle = tokio::spawn(async move {
@@ -330,18 +314,54 @@ mod tests {
     #[tokio::test]
     #[should_panic]
     async fn test_stream_handler_after_fin_message_reverse_fails() {
-        let (mut tx_input, rx_input) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-        let (tx_output, _rx_output) =
-            futures::channel::mpsc::channel::<StreamMessage<ConsensusMessage>>(100);
-
-        let mut config = StreamHandlerConfig::default();
-        config.timeout_millis = Some(100);
-        let mut h = StreamHandler::new(config, tx_output, rx_input);
+        let (mut h, mut tx_input, _rx_output) = setup_test();
         tx_input.try_send(make_random_message(13, 45, false)).expect("Send should succeed");
         tx_input.try_send(make_random_message(13, 42, true)).expect("Send should succeed");
+        tx_input.close_channel(); // this should signal the handler to break out of the loop
 
         // this should panic since the fin was received on chunk_id 42, but we are sending 45
+        let join_handle = tokio::spawn(async move {
+            h.listen().await;
+            h
+        });
+
+        h = join_handle.await.expect("Task should succeed");
+        println!("Handler.message_buffers: {:?}", h.message_buffers);
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_stream_handler_max_buffer_fails() {
+        let (mut h, mut tx_input, _rx_output) = setup_test();
+        h.config.max_buffer_size = Some(10);
+        // skip the first message, so the messages all get buffered
+        for i in 0..11 {
+            tx_input.try_send(make_random_message(13, i + 1, false)).expect("Send should succeed");
+        }
+        tx_input.close_channel(); // this should signal the handler to break out of the loop
+
+        // this should panic since there are too many buffered messages
+        let join_handle = tokio::spawn(async move {
+            h.listen().await;
+            h
+        });
+
+        h = join_handle.await.expect("Task should succeed");
+        println!("Handler.message_buffers: {:?}", h.message_buffers);
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_stream_handler_max_streams_fails() {
+        let (mut h, mut tx_input, _rx_output) = setup_test();
+        h.config.max_num_streams = Some(10);
+        // skip the first message, so the messages all get buffered
+        for i in 0..11 {
+            tx_input.try_send(make_random_message(i, 1, false)).expect("Send should succeed");
+        }
+        tx_input.close_channel(); // this should signal the handler to break out of the loop
+
+        // this should panic since there are too many streams at the same time
         let join_handle = tokio::spawn(async move {
             h.listen().await;
             h
