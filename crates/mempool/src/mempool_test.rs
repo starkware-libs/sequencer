ef3815c7f03214682532a9dc68677fa9232c6df8
Ayelet Zilber <ayelet@starkware.co>
2024-09-18 14:54:57 +0300

    chore(mempool): move account nonces update check to add tx test

--- crates/mempool/src/mempool_test.rs
+++ crates/mempool/src/mempool_test.rs
@@ -461,14 +461,17 @@ fn test_add_tx(mut mempool: Mempool) {
     add_tx_inputs.sort_by_key(|input| std::cmp::Reverse(input.tx.tip().unwrap()));
 
     // Assert: transactions are ordered by priority.
+    let expected_account_nonces = [("0x0", 0), ("0x1", 0), ("0x2", 0)];
     let expected_queue_txs: Vec<TransactionReference> =
         add_tx_inputs.iter().map(|input| TransactionReference::new(&input.tx)).collect();
     let expected_pool_txs = add_tx_inputs.into_iter().map(|input| input.tx);
     let expected_mempool_content = MempoolContentBuilder::new()
+        .with_account_nonces(expected_account_nonces)
         .with_pool(expected_pool_txs)
         .with_priority_queue(expected_queue_txs)
         .build();
     expected_mempool_content.assert_eq_pool_and_priority_queue_content(&mempool);
+    expected_mempool_content.assert_eq_account_nonces(&mempool);
 }
 
 #[rstest]
@@ -825,20 +828,6 @@ fn test_commit_block_from_different_leader() {
 }
 
 // `account_nonces` tests.
-
-#[rstest]
-fn test_account_nonces_update_in_add_tx(mut mempool: Mempool) {
-    // Setup.
-    let input = add_tx_input!(tx_nonce: 1, account_nonce: 1);
-
-    // Test: update through new input.
-    add_tx(&mut mempool, &input);
-
-    // Assert.
-    let expected_mempool_content =
-        MempoolContentBuilder::new().with_account_nonces([("0x0", 1)]).build();
-    expected_mempool_content.assert_eq_account_nonces(&mempool);
-}
 
 #[rstest]
 fn test_account_nonce_does_not_decrease_in_add_tx() {
