7ca8962de58ac48f38aa9bd51fecd6196af9d305
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-09-18 15:03:43 +0300

    refactor: make `verify_constructor` be part of `CallEntryPoint`

--- crates/blockifier/src/execution/entry_point.rs
+++ crates/blockifier/src/execution/entry_point.rs
@@ -12,6 +12,7 @@ use starknet_types_core::felt::Felt;
 
 use crate::abi::abi_utils::selector_from_name;
 use crate::abi::constants;
+use crate::abi::constants::CONSTRUCTOR_ENTRY_POINT_NAME;
 use crate::context::{BlockContext, TransactionContext};
 use crate::execution::call_info::CallInfo;
 use crate::execution::common_hints::ExecutionMode;
@@ -104,6 +105,16 @@ impl CallEntryPoint {
         let contract_class = state.get_compiled_contract_class(class_hash)?;
 
         execute_entry_point_call_wrapper(self, contract_class, state, resources, context)
+    }
+
+    pub fn verify_constructor(&self) -> Result<(), PreExecutionError> {
+        if self.entry_point_type == EntryPointType::Constructor
+            && self.entry_point_selector != selector_from_name(CONSTRUCTOR_ENTRY_POINT_NAME)
+        {
+            Err(PreExecutionError::InvalidConstructorEntryPointName)
+        } else {
+            Ok(())
+        }
     }
 }
 
