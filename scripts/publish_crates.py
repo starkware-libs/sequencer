72fe1b60be6f138cc30512b72e84b5a9fede902f
nadin <nadin@starkware.co>
2024-09-19 17:15:24 +0300

    feat: support multi-crate publish

--- scripts/publish_crates.py
+++ scripts/publish_crates.py
@@ -47,49 +47,54 @@ def verify_unpublished(crates: List[str], version: Optional[str] = None):
     ), f"Crates {already_published} have already been published with {version=}."
 
 
-def get_package_and_dependencies_in_order(crate: str) -> List[str]:
+def get_package_and_dependencies_in_order(crates: List[str]) -> List[str]:
     """
-    Returns a list of all local (member) crates that the input crate depends on, in topological
+    Returns a list of all local (member) crates that the input crates depends on, in topological
     order. I.e, if crate A depends on crate B, then B will appear before A in the list.
-    The output list also includes the input crate (last element).
+    The output list also includes the input crates.
+    Each crate is only included once in the output list.
     """
-    # We use the `depth` prefix to easily sort the dependencies in topological order: higher depth
-    # means the crate is depended on by the crate at the lower depth.
-    prefixed_tree = (
-        subprocess.check_output(["cargo", "tree", "-p", crate, "--prefix", "depth"])
-        .decode()
-        .splitlines()
-    )
-    # Clean up the lines to only keep the *local* crate names with their depth prefix.
-    # Skip all non-local crates ('(/home' should appear in lines describing local crates).
-    prefixed_local_crates = [line.split()[0].strip() for line in prefixed_tree if "(/home" in line]
+    all_crates_dependencies: List[str] = []
+    for crate in crates:
+        # We use the `depth` prefix to easily sort the dependencies in topological order: higher
+        # depth means the crate is depended on by the crate at the lower depth.
+        prefixed_tree = (
+            subprocess.check_output(["cargo", "tree", "-p", crate, "--prefix", "depth"])
+            .decode()
+            .splitlines()
+        )
+        # Clean up the lines to only keep the *local* crate names with their depth prefix.
+        # Skip all non-local crates ('(/home' should appear in lines describing local crates).
+        prefixed_local_crates = [
+            line.split()[0].strip() for line in prefixed_tree if "(/home" in line
+        ]
 
-    # Reverse order to iterate in descending depth order.
-    ordered_dependencies = []
-    for dependency_with_depth in reversed(sorted(prefixed_local_crates)):
-        # Strip the leading depth number (package names do not start with integers).
-        dependency = dependency_with_depth.lstrip("0123456789")
-        # The same package may appear multiple times, and with different depths. Always keep the
-        # highest depth only.
-        if dependency not in ordered_dependencies:
-            ordered_dependencies.append(dependency)
+        # Reverse order to iterate in descending depth order.
+        ordered_dependencies = []
+        for dependency_with_depth in reversed(sorted(prefixed_local_crates)):
+            # Strip the leading depth number (package names do not start with integers).
+            dependency = dependency_with_depth.lstrip("0123456789")
+            # The same package may appear multiple times, and with different depths. Always keep the
+            # highest depth only.
+            if dependency not in ordered_dependencies and dependency not in all_crates_dependencies:
+                ordered_dependencies.append(dependency)
     return ordered_dependencies
 
 
-def publish_crate_and_dependencies(crate: str, dry_run: bool):
-    dependencies = get_package_and_dependencies_in_order(crate=crate)
-    assert crate == dependencies[-1], f"{crate} should be the last element of '{dependencies}'."
+def publish_crates_and_dependencies(crates: List[str], dry_run: bool):
+
+    all_crates_dependencies = get_package_and_dependencies_in_order(crates=crates)
 
     # Do not attempt to publish anything if even one of the dependencies is already published.
-    verify_unpublished(crates=dependencies)
+    verify_unpublished(crates=all_crates_dependencies)
 
     base_command_template = "cargo publish -p {crate}" + f"{' --dry-run' if dry_run else ''}"
     # Publish order is important.
     cmd = " && ".join(
-        [base_command_template.format(crate=dependency) for dependency in dependencies]
+        [base_command_template.format(crate=dependency) for dependency in all_crates_dependencies]
     )
 
-    print(f"Publishing {crate} ({dry_run=}) and its dependencies: {dependencies}...")
+    print(f"Publishing crates: {all_crates_dependencies} ({dry_run=})...")
     print(cmd, flush=True)
     subprocess.run(cmd, check=True, shell=True)
     print(f"Done.")
@@ -97,15 +102,18 @@ def publish_crate_and_dependencies(crate: str, dry_run: bool):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Publish a crate and it's dependencies in the local workspace."
+        description="Publish multiple crates and their dependencies in the local workspace."
     )
     parser.add_argument(
-        "--crate", type=str, help="Crate to publish (dependencies will also be published)."
+        "--crates",
+        type=str,
+        help="List of crates to publish (dependencies will also be published).",
+        nargs="+",
     )
     parser.add_argument("--dry_run", required=False, action="store_true", help="Dry run.")
     args = parser.parse_args()
 
-    publish_crate_and_dependencies(crate=args.crate, dry_run=args.dry_run)
+    publish_crates_and_dependencies(crates=args.crates, dry_run=args.dry_run)
 
 
 if __name__ == "__main__":
