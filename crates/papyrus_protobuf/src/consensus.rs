98884b03f51134cf299c8502402531a207c78c3a
Guy Nir <guy.n@starkware.co>
2024-09-19 17:58:22 +0300

    feat: add protobuf and converters for ProposalParts and related structs

--- crates/papyrus_protobuf/src/consensus.rs
+++ crates/papyrus_protobuf/src/consensus.rs
@@ -1,4 +1,8 @@
 use futures::channel::{mpsc, oneshot};
+#[cfg(any(feature = "testing", test))]
+use papyrus_test_utils::{auto_impl_get_test_instance, get_number_of_variants, GetTestInstance};
+#[cfg(any(feature = "testing", test))]
+use rand::Rng;
 use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::core::ContractAddress;
 use starknet_api::transaction::Transaction;
@@ -50,6 +54,100 @@ pub struct StreamMessage<T: Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufCon
     pub stream_id: u64,
     pub chunk_id: u64,
     pub fin: bool,
+}
+
+/// This message must be sent first when proposing a new block.
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProposalInit {
+    /// The height of the consensus (block number).
+    pub height: u64,
+    /// The current round of the consensus.
+    pub round: u32,
+    /// The last round that was valid.
+    pub valid_round: Option<u32>,
+    /// Address of the one who proposed the block.
+    pub proposer: ContractAddress,
+}
+
+/// There is one or more batches of transactions in a proposed block.
+#[derive(Debug, Clone, PartialEq)]
+pub struct TransactionBatch {
+    /// The transactions in the batch.
+    pub transactions: Vec<Transaction>,
+}
+
+/// The propsal is done when receiving this fin message, which contains the block hash.
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProposalFin {
+    /// The block hash of the proposed block.
+    pub proposal_content_id: BlockHash,
+}
+
+/// A part of the proposal.
+#[derive(Debug, Clone, PartialEq)]
+pub enum ProposalPart {
+    /// The initialization part of the proposal.
+    Init(ProposalInit),
+    /// A part of the proposal that contains one or more transactions.
+    Transactions(TransactionBatch),
+    /// The final part of the proposal, including the block hash.
+    Fin(ProposalFin),
+}
+
+#[cfg(any(feature = "testing", test))]
+auto_impl_get_test_instance! {
+    pub enum ConsensusMessage {
+        Proposal(Proposal) = 0,
+        Vote(Vote) = 1,
+    }
+    pub struct Proposal {
+        pub height: u64,
+        pub round: u32,
+        pub proposer: ContractAddress,
+        pub transactions: Vec<Transaction>,
+        pub block_hash: BlockHash,
+    }
+    pub struct Vote {
+        pub vote_type: VoteType,
+        pub height: u64,
+        pub round: u32,
+        pub block_hash: Option<BlockHash>,
+        pub voter: ContractAddress,
+    }
+    pub enum VoteType {
+        Prevote = 0,
+        Precommit = 1,
+    }
+    pub struct ProposalInit {
+        pub height: u64,
+        pub round: u32,
+        pub valid_round: Option<u32>,
+        pub proposer: ContractAddress,
+    }
+    pub struct ProposalFin {
+        pub proposal_content_id: BlockHash,
+    }
+    pub struct TransactionBatch {
+        pub transactions: Vec<Transaction>,
+    }
+    pub enum ProposalPart {
+        Init(ProposalInit) = 0,
+        Fin(ProposalFin) = 1,
+        Transactions(TransactionBatch) = 2,
+    }
+
+}
+
+#[cfg(any(feature = "testing", test))]
+impl GetTestInstance for StreamMessage<ConsensusMessage> {
+    fn get_test_instance(rng: &mut rand_chacha::ChaCha8Rng) -> Self {
+        Self {
+            message: ConsensusMessage::Proposal(Proposal::default()),
+            stream_id: rng.gen_range(0..100),
+            message_id: rng.gen_range(0..1000),
+            fin: rng.gen_bool(0.5),
+        }
+    }
 }
 
 // TODO(Guy): Remove after implementing broadcast streams.
