b15d62b0aac72b0fbdba42aa22a57dc43b37bcba
Alon Lukatch <alon.l@starkware.co>
2024-09-22 12:29:38 +0300

    refactor(protobuf): use Option instead of Result when processing Protoc version

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -1,46 +1,43 @@
-use std::io::{Error, ErrorKind};
 use std::process::Command;
 use std::{env, io};
 
-fn parse_protoc_version(protoc_version_str: &str) -> io::Result<(u32, u32)> {
+fn parse_protoc_version(protoc_version_str: &str) -> Option<(u32, u32)> {
     // Returns Result<(major, minor)> numbers. If the minor doesn't exist, returns 0 as minor. If
     // the major doesn't exist, returns an error.
-    let version_numbers: Vec<u32> =
-        protoc_version_str.split('.').map(|part| part.parse::<u32>().unwrap()).collect();
-    Ok((
-        version_numbers.first().ok_or(Error::new(ErrorKind::Other, ""))?.to_owned(),
-        version_numbers.get(1).unwrap_or(&0).to_owned(),
-    ))
+    let mut version_numbers_str = protoc_version_str.split('.');
+    let major: u32 = match version_numbers_str.next() {
+        Some(major) => major.parse::<u32>().ok()?,
+        None => return None,
+    };
+    let minor: u32 = match version_numbers_str.next() {
+        Some(minor) => minor.parse::<u32>().ok()?,
+        None => 0,
+    };
+    Some((major, minor))
 }
 
-fn validate_preinstalled_protoc() -> io::Result<()> {
+fn get_preinstalled_protoc_version() -> Option<(u32, u32)> {
     let protoc = env::var("PROTOC").unwrap_or("protoc".to_string());
 
     let protoc_version =
-        String::from_utf8_lossy(&Command::new(protoc).arg("--version").output()?.stdout)
+        String::from_utf8_lossy(&Command::new(protoc).arg("--version").output().ok()?.stdout)
             .to_string();
 
     let parts: Vec<&str> = protoc_version.split_whitespace().collect();
+    // The returned string is in the format "libprotoc 25.1". We need to extract the version
     let protoc_version_str = match parts.get(1) {
         Some(version) => version,
-        None => return Err(Error::new(ErrorKind::Other, "protoc version not found")),
+        None => return None,
     };
     let (major, minor) = parse_protoc_version(protoc_version_str)?;
-
-    if major < 3 || (major == 3 && minor < 15) {
-        Err(Error::new(
-            ErrorKind::Other,
-            "protoc version is too old. version 3.15.x or greater is needed.",
-        ))
-    } else {
-        Ok(())
-    }
+    // Protoc versions before 3.15 are not supported.
+    if (major < 3) || (major == 3 && minor < 15) { None } else { Some((major, minor)) }
 }
 
 fn main() -> io::Result<()> {
     // If Protoc is installed use it, if not compile using prebuilt protoc.
     println!("Building");
-    if validate_preinstalled_protoc().is_err() {
+    if get_preinstalled_protoc_version().is_none() {
         println!("Building using prebuilt protoc");
         let (protoc_bin, _) = protoc_prebuilt::init("27.0").expect(
         "Please run `gh auth login` to enable protoc compilation.\n
