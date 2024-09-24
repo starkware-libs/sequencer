7ff3ff18b6891900c8a551e5e0a25f3b56d6bbc4
Yair Bakalchuk <yair@starkware.co>
2024-09-24 14:11:15 +0300

    feat(batcher, mempool_node): add local storage to the batcher

--- crates/batcher/src/batcher.rs
+++ crates/batcher/src/batcher.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
+#[cfg(test)]
+use mockall::automock;
 use starknet_mempool_infra::component_runner::ComponentStarter;
 use starknet_mempool_types::communication::SharedMempoolClient;
 
@@ -8,17 +12,29 @@ use crate::config::BatcherConfig;
 pub struct Batcher {
     pub config: BatcherConfig,
     pub mempool_client: SharedMempoolClient,
+    pub storage: Arc<dyn BatcherStorageReaderTrait>,
 }
 
 impl Batcher {
-    pub fn new(config: BatcherConfig, mempool_client: SharedMempoolClient) -> Self {
-        Self { config, mempool_client }
+    pub fn new(
+        config: BatcherConfig,
+        mempool_client: SharedMempoolClient,
+        storage: Arc<dyn BatcherStorageReaderTrait>,
+    ) -> Self {
+        Self { config, mempool_client, storage }
     }
 }
 
 pub fn create_batcher(config: BatcherConfig, mempool_client: SharedMempoolClient) -> Batcher {
-    Batcher::new(config, mempool_client)
+    let (storage_reader, _storage_writer) = papyrus_storage::open_storage(config.storage.clone())
+        .expect("Failed to open batcher's storage");
+    Batcher::new(config, mempool_client, Arc::new(storage_reader))
 }
 
 #[async_trait]
 impl ComponentStarter for Batcher {}
+
+#[cfg_attr(test, automock)]
+pub trait BatcherStorageReaderTrait: Send + Sync {}
+
+impl BatcherStorageReaderTrait for papyrus_storage::StorageReader {}
