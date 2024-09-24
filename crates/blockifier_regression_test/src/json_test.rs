f0a53f24be2a7216e386b573b3e5c6f4bc79d2dd
AvivYossef-starkware <aviv.y@starkware.co>
2024-09-24 13:18:21 +0300

    build: blockifier regression test compile class

--- crates/blockifier_regression_test/src/json_test.rs
+++ crates/blockifier_regression_test/src/json_test.rs
@@ -3,12 +3,21 @@ use pretty_assertions::assert_eq;
 use rstest::{fixture, rstest};
 use starknet_api::block::BlockNumber;
 use starknet_api::test_utils::read_json_file;
+use starknet_core::types::ContractClass;
 use starknet_gateway::rpc_objects::BlockHeader;
+
+use crate::state_reader::compile::legacy_to_contract_class_v0;
 
 #[fixture]
 fn block_header() -> BlockHeader {
     serde_json::from_value(read_json_file("json_objects/block_header.json"))
         .expect("Failed to deserialize block header")
+}
+
+#[fixture]
+fn deprecated_contract_class() -> ContractClass {
+    serde_json::from_value(read_json_file("json_objects/deprecated_contract_class.json"))
+        .expect("Failed to deserialize deprecated contact class")
 }
 
 /// Test that deserialize block header from JSON file works(in the fixture).
@@ -24,4 +33,15 @@ fn test_block_header_to_block_info(block_header: BlockHeader) {
         block_header.try_into().expect("Failed to convert BlockHeader to block info");
     // Sanity check.
     assert_eq!(block_info.block_number, BlockNumber(700000));
+}
+
+#[rstest]
+fn test_compile_deprecated_contract_class(deprecated_contract_class: ContractClass) {
+    match deprecated_contract_class {
+        ContractClass::Legacy(legacy) => {
+            // Compile the contract class.
+            assert!(legacy_to_contract_class_v0(legacy).is_ok());
+        }
+        _ => panic!("Expected a legacy contract class"),
+    }
 }
--- crates/blockifier_regression_test/src/state_reader/rpc_test.rs
+++ crates/blockifier_regression_test/src/state_reader/rpc_test.rs
@@ -4,7 +4,11 @@ use blockifier::versioned_constants::StarknetVersion;
 use pretty_assertions::assert_eq;
 use rstest::{fixture, rstest};
 use starknet_api::block::BlockNumber;
+use starknet_api::core::ClassHash;
+use starknet_api::{class_hash, felt};
+use starknet_core::types::ContractClass::{Legacy, Sierra};
 
+use crate::state_reader::compile::legacy_to_contract_class_v0;
 use crate::state_reader::test_state_reader::TestStateReader;
 
 #[fixture]
@@ -28,4 +32,31 @@ pub fn test_get_block_info(test_state_reader: TestStateReader, test_block_number
 #[rstest]
 pub fn test_get_starknet_version(test_state_reader: TestStateReader) {
     assert_eq!(test_state_reader.get_starknet_version().unwrap(), StarknetVersion::V0_13_2_1)
+}
+
+#[rstest]
+pub fn test_get_contract_class(test_state_reader: TestStateReader, test_block_number: BlockNumber) {
+    // An example of existing class hash in Mainnet.
+    let class_hash =
+        class_hash!("0x3131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e");
+
+    // Test getting the contract class using RPC request.
+    let deprecated_contract_class =
+        test_state_reader.get_contract_class(&class_hash).unwrap_or_else(|err| {
+            panic!(
+                "Error retrieving deprecated contract class for class hash {}: {}
+            This class hash exist in Mainnet Block Number: {}",
+                class_hash, test_block_number, err
+            );
+        });
+
+    // Test compiling the contract class.
+    match deprecated_contract_class {
+        Legacy(legacy) => {
+            // Test compiling the contract class.
+            assert!(legacy_to_contract_class_v0(legacy).is_ok());
+        }
+        // This contract class is deprecated.
+        Sierra(_) => panic!("Expected a legacy contract class"),
+    }
 }
