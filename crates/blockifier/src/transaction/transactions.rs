6643538ef6c45c0e2a2e0079787aeffc984313e5
Arnon Hod <arnon@starkware.co>
2024-09-25 13:13:32 +0300

    chore: remove clone of casm class on conversion in stateful validator (#983)

--- crates/blockifier/src/transaction/transactions.rs
+++ crates/blockifier/src/transaction/transactions.rs
@@ -133,6 +133,18 @@ pub struct DeclareTransaction {
     pub class_info: ClassInfo,
 }
 
+/// This implementation of try_from clones the base transaction struct. This method's advantage is
+/// that it does not clone the Casm contract class code.
+impl TryFrom<&starknet_api::executable_transaction::DeclareTransaction> for DeclareTransaction {
+    type Error = TransactionExecutionError;
+
+    fn try_from(
+        declare_tx: &starknet_api::executable_transaction::DeclareTransaction,
+    ) -> Result<Self, Self::Error> {
+        Self::new_from_ref_to_executable_tx(declare_tx, false)
+    }
+}
+
 impl TryFrom<starknet_api::executable_transaction::DeclareTransaction> for DeclareTransaction {
     type Error = TransactionExecutionError;
 
@@ -190,8 +202,17 @@ impl DeclareTransaction {
         Self::create(declare_tx, tx_hash, class_info, true)
     }
 
-    // TODO(Arni): Consider creaing a version of this function that gets a reference to the
-    // declare_tx.
+    fn new_from_ref_to_executable_tx(
+        declare_tx: &starknet_api::executable_transaction::DeclareTransaction,
+        only_query: bool,
+    ) -> Result<Self, TransactionExecutionError> {
+        let starknet_api::executable_transaction::DeclareTransaction { tx, tx_hash, class_info } =
+            declare_tx;
+        let class_info = class_info.try_into()?;
+
+        Self::create(tx.clone(), *tx_hash, class_info, only_query)
+    }
+
     fn new_from_executable_tx(
         declare_tx: starknet_api::executable_transaction::DeclareTransaction,
         only_query: bool,
