b656fb798b852cbf912c9f76aa0ece1ddde80b10
AvivYossef-starkware <aviv.y@starkware.co>
2024-09-25 17:00:27 +0300

    build: blockifier regression test serde rpc tx to starknet api tx

--- crates/blockifier_regression_test/src/state_reader/raw_rpc_json_test.rs
+++ crates/blockifier_regression_test/src/state_reader/raw_rpc_json_test.rs
@@ -1,12 +1,15 @@
+use assert_matches::assert_matches;
 use blockifier::blockifier::block::BlockInfo;
 use pretty_assertions::assert_eq;
 use rstest::{fixture, rstest};
 use starknet_api::block::BlockNumber;
 use starknet_api::test_utils::read_json_file;
+use starknet_api::transaction::{InvokeTransaction, Transaction};
 use starknet_core::types::ContractClass;
 use starknet_gateway::rpc_objects::BlockHeader;
 
 use crate::state_reader::compile::legacy_to_contract_class_v0;
+use crate::state_reader::utils::deserialize_transaction_json_to_starknet_api_tx;
 
 #[fixture]
 fn block_header() -> BlockHeader {
@@ -44,4 +47,21 @@ fn test_compile_deprecated_contract_class(deprecated_contract_class: ContractCla
         }
         _ => panic!("Expected a legacy contract class"),
     }
+}
+
+#[test]
+fn deserialize_invoke_txs() {
+    let invoke_tx_v1 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["invoke_v1"].clone(),
+    )
+    .expect("Failed to deserialize invoke v1 tx");
+
+    assert_matches!(invoke_tx_v1, Transaction::Invoke(InvokeTransaction::V1(..)));
+
+    let invoke_tx_v3 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["invoke_v3"].clone(),
+    )
+    .expect("Failed to deserialize invoke v3 tx");
+
+    assert_matches!(invoke_tx_v3, Transaction::Invoke(InvokeTransaction::V3(..)));
 }
