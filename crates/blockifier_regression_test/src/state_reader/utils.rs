8deb32692605cac6d39dbef9e7119e17f6b4152a
AvivYossef-starkware <aviv.y@starkware.co>
2024-09-25 17:00:27 +0300

    build: blockifier regression test serde rpc tx to starknet api tx

--- crates/blockifier_regression_test/src/state_reader/utils.rs
+++ crates/blockifier_regression_test/src/state_reader/utils.rs
@@ -1,6 +1,13 @@
 use blockifier::context::{ChainInfo, FeeTokenAddresses};
 use papyrus_execution::{ETH_FEE_CONTRACT_ADDRESS, STRK_FEE_CONTRACT_ADDRESS};
+use serde_json::Value;
 use starknet_api::core::{ChainId, ContractAddress, PatriciaKey};
+use starknet_api::transaction::{
+    DeclareTransaction,
+    DeployAccountTransaction,
+    InvokeTransaction,
+    Transaction,
+};
 use starknet_api::{contract_address, felt, patricia_key};
 use starknet_gateway::config::RpcStateReaderConfig;
 
@@ -26,4 +33,55 @@ pub fn get_rpc_state_reader_config() -> RpcStateReaderConfig {
 /// Returns the chain info of mainnet.
 pub fn get_chain_info() -> ChainInfo {
     ChainInfo { chain_id: ChainId::Mainnet, fee_token_addresses: get_fee_token_addresses() }
+}
+
+pub fn deserialize_transaction_json_to_starknet_api_tx(
+    mut raw_transaction: Value,
+) -> serde_json::Result<Transaction> {
+    let tx_type: String = serde_json::from_value(raw_transaction["type"].clone())?;
+    let tx_version: String = serde_json::from_value(raw_transaction["version"].clone())?;
+
+    match (tx_type.as_str(), tx_version.as_str()) {
+        ("INVOKE", "0x0") => {
+            Ok(Transaction::Invoke(InvokeTransaction::V0(serde_json::from_value(raw_transaction)?)))
+        }
+        ("INVOKE", "0x1") => {
+            Ok(Transaction::Invoke(InvokeTransaction::V1(serde_json::from_value(raw_transaction)?)))
+        }
+        ("INVOKE", "0x3") => {
+            if let Some(resource_bounds) =
+                raw_transaction.get_mut("resource_bounds").and_then(Value::as_object_mut)
+            {
+                if let Some(l1_gas_value) = resource_bounds.remove("l1_gas") {
+                    resource_bounds.insert("L1_GAS".to_string(), l1_gas_value);
+                }
+                if let Some(l2_gas_value) = resource_bounds.remove("l2_gas") {
+                    resource_bounds.insert("L2_GAS".to_string(), l2_gas_value);
+                }
+            }
+            Ok(Transaction::Invoke(InvokeTransaction::V3(serde_json::from_value(raw_transaction)?)))
+        }
+        ("DEPLOY_ACCOUNT", "0x1") => Ok(Transaction::DeployAccount(DeployAccountTransaction::V1(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("DEPLOY_ACCOUNT", "0x3") => Ok(Transaction::DeployAccount(DeployAccountTransaction::V3(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("DECLARE", "0x0") => Ok(Transaction::Declare(DeclareTransaction::V0(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("DECLARE", "0x1") => Ok(Transaction::Declare(DeclareTransaction::V1(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("DECLARE", "0x2") => Ok(Transaction::Declare(DeclareTransaction::V2(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("DECLARE", "0x3") => Ok(Transaction::Declare(DeclareTransaction::V3(
+            serde_json::from_value(raw_transaction)?,
+        ))),
+        ("L1_HANDLER", _) => Ok(Transaction::L1Handler(serde_json::from_value(raw_transaction)?)),
+        (tx_type, tx_version) => Err(serde::de::Error::custom(format!(
+            "unimplemented transaction type: {tx_type} version: {tx_version}"
+        ))),
+    }
 }
