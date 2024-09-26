d2ab9d8ed6fa2387d87560f30024ae347e0b64e6
Aviv Greenburg <aviv.g@starkware.co>
2024-09-26 10:58:17 +0300

    refactor(blockifier): account_tx from enum to struct w api::executable_tx

--- crates/blockifier/src/test_utils/declare.rs
+++ crates/blockifier/src/test_utils/declare.rs
@@ -1,13 +1,15 @@
 use starknet_api::contract_class::ClassInfo;
+use starknet_api::executable_transaction::{DeclareTransaction, Transaction};
 use starknet_api::test_utils::declare::DeclareTxArgs;
 
 use crate::transaction::account_transaction::AccountTransaction;
-use crate::transaction::transactions::DeclareTransaction;
 
 pub fn declare_tx(declare_tx_args: DeclareTxArgs, class_info: ClassInfo) -> AccountTransaction {
     let tx_hash = declare_tx_args.tx_hash;
     let declare_tx = starknet_api::test_utils::declare::declare_tx(declare_tx_args);
-    let executable_declare = DeclareTransaction::new(declare_tx, tx_hash, class_info).unwrap();
 
-    AccountTransaction::Declare(executable_declare)
+    let executable_tx =
+        Transaction::Declare(DeclareTransaction { tx: declare_tx, tx_hash, class_info });
+
+    AccountTransaction::new(executable_tx)
 }
