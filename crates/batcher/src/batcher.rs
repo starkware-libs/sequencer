4f32e785b07ae3e5e709f5b72d2e50a4a987c8db
Gilad Chase <gilad@starkware.com>
2024-09-27 12:09:39 +0300

    fix(tests-integration): reenable e2e test

--- crates/batcher/src/batcher.rs
+++ crates/batcher/src/batcher.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use async_trait::async_trait;
 #[cfg(test)]
 use mockall::automock;
+use papyrus_storage::test_utils::get_test_storage;
 use starknet_mempool_infra::component_runner::ComponentStarter;
 use starknet_mempool_types::communication::SharedMempoolClient;
 
@@ -26,8 +27,9 @@ impl Batcher {
 }
 
 pub fn create_batcher(config: BatcherConfig, mempool_client: SharedMempoolClient) -> Batcher {
-    let (storage_reader, _storage_writer) = papyrus_storage::open_storage(config.storage.clone())
-        .expect("Failed to open batcher's storage");
+    // let (storage_reader, _storage_writer) = papyrus_storage::open_storage(config.storage.clone())
+    //     .expect("Failed to open batcher's storage");
+    let (storage_reader, _storage_writer) = get_test_storage().0;
     Batcher::new(config, mempool_client, Arc::new(storage_reader))
 }
 
