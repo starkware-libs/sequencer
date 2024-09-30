58eaeed08ad47d36358ec5babe74eeb1a7e6f222
Itay Tsabary <itayt@starkware.co>
2024-09-30 11:25:13 +0300

    chore(mempool_node): rename config
    commit-id:e9d31884

--- crates/mempool_node/src/utils.rs
+++ crates/mempool_node/src/utils.rs
@@ -1,10 +1,10 @@
 use crate::communication::{create_node_channels, create_node_clients, SequencerNodeClients};
 use crate::components::create_components;
-use crate::config::MempoolNodeConfig;
+use crate::config::SequencerNodeConfig;
 use crate::servers::{create_servers, SequencerNodeServers};
 
 pub fn create_clients_servers_from_config(
-    config: &MempoolNodeConfig,
+    config: &SequencerNodeConfig,
 ) -> (SequencerNodeClients, SequencerNodeServers) {
     let mut channels = create_node_channels();
     let clients = create_node_clients(config, &mut channels);
