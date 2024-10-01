71313780e7bddd661ccfd116c3b245e5d78a125f
AvivYossef-starkware <141143145+AvivYossef-starkware@users.noreply.github.com>
2024-10-01 11:09:25 +0300

    build: blockifier regression test compile class (#980)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/sequencer/980)
    <!-- Reviewable:end -->

--- crates/blockifier_regression_test/src/state_reader/test_state_reader.rs
+++ crates/blockifier_regression_test/src/state_reader/test_state_reader.rs
@@ -3,21 +3,24 @@ use blockifier::blockifier::config::TransactionExecutorConfig;
 use blockifier::blockifier::transaction_executor::TransactionExecutor;
 use blockifier::bouncer::BouncerConfig;
 use blockifier::context::BlockContext;
-use blockifier::execution::contract_class::ContractClass;
+use blockifier::execution::contract_class::ContractClass as BlockifierContractClass;
 use blockifier::state::cached_state::CachedState;
 use blockifier::state::errors::StateError;
 use blockifier::state::state_api::{StateReader, StateResult};
 use blockifier::versioned_constants::{StarknetVersion, VersionedConstants};
-use serde_json::to_value;
+use serde_json::{json, to_value};
 use starknet_api::block::{BlockNumber, GasPrice};
 use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
 use starknet_api::state::StorageKey;
+use starknet_core::types::ContractClass as StarknetContractClass;
+use starknet_core::types::ContractClass::{Legacy, Sierra};
 use starknet_gateway::config::RpcStateReaderConfig;
 use starknet_gateway::errors::serde_err_to_state_err;
 use starknet_gateway::rpc_objects::{BlockHeader, GetBlockWithTxHashesParams, ResourcePrice};
 use starknet_gateway::rpc_state_reader::RpcStateReader;
 use starknet_types_core::felt::Felt;
 
+use crate::state_reader::compile::{legacy_to_contract_class_v0, sierra_to_contact_class_v1};
 use crate::state_reader::utils::{get_chain_info, get_rpc_state_reader_config};
 
 pub struct TestStateReader(RpcStateReader);
@@ -39,8 +42,16 @@ impl StateReader for TestStateReader {
         self.0.get_class_hash_at(contract_address)
     }
 
-    fn get_compiled_contract_class(&self, _class_hash: ClassHash) -> StateResult<ContractClass> {
-        todo!()
+    /// Returns the contract class of the given class hash.
+    /// Compile the contract class if it is Sierra.
+    fn get_compiled_contract_class(
+        &self,
+        class_hash: ClassHash,
+    ) -> StateResult<BlockifierContractClass> {
+        match self.get_contract_class(&class_hash)? {
+            Sierra(sierra) => sierra_to_contact_class_v1(sierra),
+            Legacy(legacy) => legacy_to_contract_class_v0(legacy),
+        }
     }
 
     fn get_compiled_class_hash(&self, class_hash: ClassHash) -> StateResult<CompiledClassHash> {
@@ -95,6 +106,17 @@ impl TestStateReader {
         StarknetVersion::try_from(raw_version.as_str()).map_err(|err| {
             StateError::StateReadError(format!("Failed to match starknet version: {}", err))
         })
+    }
+
+    pub fn get_contract_class(&self, class_hash: &ClassHash) -> StateResult<StarknetContractClass> {
+        let params = json!({
+            "block_id": self.0.block_id,
+            "class_hash": class_hash.0.to_string(),
+        });
+        let contract_class: StarknetContractClass =
+            serde_json::from_value(self.0.send_rpc_request("starknet_getClass", params.clone())?)
+                .map_err(serde_err_to_state_err)?;
+        Ok(contract_class)
     }
 
     pub fn get_versioned_constants(&self) -> StateResult<&'static VersionedConstants> {
