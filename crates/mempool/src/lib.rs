d7c2184cb7fa0efe79a0578bdc5403dc1263c1a6
Elin Tulchinsky <elin@starkware.co>
2024-10-06 10:54:03 +0300

    refactor(mempool): move test utils to a separate file

--- crates/mempool/src/lib.rs
+++ crates/mempool/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod communication;
 pub mod mempool;
 pub(crate) mod suspended_transaction_pool;
+#[cfg(feature = "testing")]
+pub mod test_utils;
 pub(crate) mod transaction_pool;
 pub(crate) mod transaction_queue;
