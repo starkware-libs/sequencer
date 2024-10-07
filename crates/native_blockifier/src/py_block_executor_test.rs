b580dce8bc5d9fd7ea7909036900be82b0080abe
Noa Oved <104720318+noaov1@users.noreply.github.com>
2024-10-07 16:33:47 +0300

    refactor(execution): avoid using try from reference in declare transaction (#1170)

--- crates/native_blockifier/src/py_block_executor_test.rs
+++ crates/native_blockifier/src/py_block_executor_test.rs
@@ -20,7 +20,7 @@ use crate::test_utils::MockStorage;
 fn global_contract_cache_update() {
     // Initialize executor and set a contract class on the state.
     let casm = CasmContractClass { compiler_version: "0.1.0".to_string(), ..Default::default() };
-    let contract_class = ContractClass::V1(ContractClassV1::try_from(&casm).unwrap());
+    let contract_class = ContractClass::V1(ContractClassV1::try_from(casm.clone()).unwrap());
     let class_hash = class_hash!("0x1");
 
     let temp_storage_path = tempfile::tempdir().unwrap().into_path();
