6081bb8e00c544a2cafdf04afa6911c264c53c31
eitanm-starkware <eitan.m@starkware.co>
2024-10-07 17:06:52 +0300

    feat(mempool_p2p): create init for receiver and sender

--- crates/mempool_p2p/src/receiver/mod.rs
+++ crates/mempool_p2p/src/receiver/mod.rs
@@ -1,5 +1,28 @@
+use papyrus_network::network_manager::{
+    BroadcastTopicClient,
+    BroadcastTopicServer,
+    NetworkManager,
+};
+use papyrus_protobuf::mempool::RpcTransactionWrapper;
 use starknet_mempool_infra::component_definitions::ComponentStarter;
 
-pub struct MempoolP2pReceiver;
+pub struct MempoolP2pReceiver {
+    #[allow(dead_code)]
+    network_manager: Option<NetworkManager>,
+    #[allow(dead_code)]
+    broadcasted_messages_server: BroadcastTopicServer<RpcTransactionWrapper>,
+    #[allow(dead_code)]
+    broadcast_messages_client: BroadcastTopicClient<RpcTransactionWrapper>,
+}
+
+impl MempoolP2pReceiver {
+    pub fn new(
+        network_manager: Option<NetworkManager>,
+        broadcasted_messages_server: BroadcastTopicServer<RpcTransactionWrapper>,
+        broadcast_messages_client: BroadcastTopicClient<RpcTransactionWrapper>,
+    ) -> Self {
+        Self { network_manager, broadcasted_messages_server, broadcast_messages_client }
+    }
+}
 
 impl ComponentStarter for MempoolP2pReceiver {}
--- crates/mempool_p2p/src/sender/mod.rs
+++ crates/mempool_p2p/src/sender/mod.rs
@@ -1,11 +1,22 @@
 use async_trait::async_trait;
+use papyrus_network::network_manager::BroadcastTopicClient;
+use papyrus_protobuf::mempool::RpcTransactionWrapper;
 use starknet_mempool_infra::component_definitions::ComponentRequestHandler;
 use starknet_mempool_p2p_types::communication::{
     MempoolP2pSenderRequest,
     MempoolP2pSenderResponse,
 };
 
-pub struct MempoolP2pSender;
+pub struct MempoolP2pSender {
+    #[allow(dead_code)]
+    broadcast_topic_client: BroadcastTopicClient<RpcTransactionWrapper>,
+}
+
+impl MempoolP2pSender {
+    pub fn new(broadcast_topic_client: BroadcastTopicClient<RpcTransactionWrapper>) -> Self {
+        Self { broadcast_topic_client }
+    }
+}
 
 #[async_trait]
 impl ComponentRequestHandler<MempoolP2pSenderRequest, MempoolP2pSenderResponse>
