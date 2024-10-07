d79365606e222288f223b4e87cf4fa85b66fd8a4
Guy Nir <guy.n@starkware.co>
2024-10-07 11:43:53 +0300

    fix: update formatting to allow for fin/content

--- crates/papyrus_protobuf/src/consensus.rs
+++ crates/papyrus_protobuf/src/consensus.rs
@@ -64,16 +64,23 @@ where
     T: Clone + Into<Vec<u8>> + TryFrom<Vec<u8>, Error = ProtobufConversionError>,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let message: Vec<u8> = self.message.clone().into();
         // TODO(guyn): add option to display when message is Fin and doesn't have content (PR #1048)
-        write!(
-            f,
-            "StreamMessage {{ stream_id: {}, message_id: {}, message_length: {}, fin: {} }}",
-            self.stream_id,
-            self.message_id,
-            message.len(),
-            self.fin
-        )
+        if let StreamMessageBody::Content(message) = &self.message {
+            let message: Vec<u8> = message.clone().into();
+            write!(
+                f,
+                "StreamMessage {{ stream_id: {}, message_id: {}, message_length: {}}}",
+                self.stream_id,
+                self.message_id,
+                message.len(),
+            )
+        } else {
+            write!(
+                f,
+                "StreamMessage {{ stream_id: {}, message_id: {}, message is fin }}",
+                self.stream_id, self.message_id,
+            )
+        }
     }
 }
 
