6058f2fe20a8336e5983a9e21575bf445568a6fd
eitanm-starkware <eitan.m@starkware.co>
2024-10-08 11:02:48 +0300

    feat(mempool_p2p): impl component starter for mempoolp2preceiver

--- crates/mempool_p2p/src/receiver/mod.rs
+++ crates/mempool_p2p/src/receiver/mod.rs
@@ -1,20 +1,21 @@
+use async_trait::async_trait;
+use futures::StreamExt;
 use papyrus_network::network_manager::{
     BroadcastTopicClient,
+    BroadcastTopicClientTrait,
     BroadcastTopicServer,
     NetworkManager,
 };
 use papyrus_protobuf::mempool::RpcTransactionWrapper;
 use starknet_gateway_types::communication::SharedGatewayClient;
 use starknet_mempool_infra::component_definitions::ComponentStarter;
+use starknet_mempool_infra::errors::ComponentError;
+use tracing::{error, warn};
 
 pub struct MempoolP2pReceiver {
-    #[allow(dead_code)]
     network_manager: Option<NetworkManager>,
-    #[allow(dead_code)]
     broadcasted_messages_server: BroadcastTopicServer<RpcTransactionWrapper>,
-    #[allow(dead_code)]
     broadcast_messages_client: BroadcastTopicClient<RpcTransactionWrapper>,
-    #[allow(dead_code)]
     gateway_client: SharedGatewayClient,
 }
 
@@ -34,4 +35,42 @@ impl MempoolP2pReceiver {
     }
 }
 
-impl ComponentStarter for MempoolP2pReceiver {}
+#[async_trait]
+impl ComponentStarter for MempoolP2pReceiver {
+    async fn start(&mut self) -> Result<(), ComponentError> {
+        let maybe_network = self.network_manager.take();
+        match maybe_network {
+            Some(network) => loop {
+                tokio::select! {
+                    _ = network.run() => panic!("Mempool Network Manager has unexpectedly ended"),
+                    Some((maybe_message, broadcasted_message_manager)) = self.broadcasted_messages_server.next() => {
+                        match maybe_message {
+                            Ok(message) => {
+                                //TODO(eitan): Add message metadata.
+                                match self.gateway_client.add_tx(GatewayInput { rpc_tx: message.0, message_metadata: None }).await {
+                                    Ok(_tx_hash) => {}
+                                    Err(e) => {
+                                        warn!("Failed to forward transaction from p2p receiver to gateway: {:?}", e);
+                                        if let Err(e) = self.broadcast_messages_client.report_peer(broadcasted_message_manager).await {
+                                            warn!("Failed to report peer: {:?}", e);
+                                        }
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                warn!("Failed to receive transaction from network: {:?}", e);
+                                if let Err(e) = self.broadcast_messages_client.report_peer(broadcasted_message_manager).await {
+                                    warn!("Failed to report peer: {:?}", e);
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            None => {
+                error!("Mempool P2P Network Manager is not initialized.");
+                Err(ComponentError::ComponentConfigError)
+            }
+        }
+    }
+}
