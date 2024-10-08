a168b7ee96a0a684a863753b44d8e36efe33ae5b
Rodrigo <rodrodpino@gmail.com>
2024-10-07 22:48:07 -0400

    chore: update script to install llvm-19

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -30,13 +30,13 @@ function setup_llvm_deps() {
         ;;
     Linux)
         $SUDO bash -c 'curl https://apt.llvm.org/llvm.sh -Lo llvm.sh
-        bash ./llvm.sh 18 all
+        bash ./llvm.sh 19 all
         apt update && apt install -y \
             libgmp3-dev \
-            libmlir-18-dev \
-            libpolly-18-dev \
+            libmlir-19-dev \
+            libpolly-19-dev \
             libzstd-dev \
-            mlir-18-tools
+            mlir-19-tools
         '
         ;;
     *)
