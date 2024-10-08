60146a3f7164467a330409899be7153c89038392
eitanm-starkware <eitan.m@starkware.co>
2024-10-08 14:46:04 +0300

    chore(mempool_p2p): add gateway client to receiver

--- crates/mempool_p2p/src/receiver/mod.rs
+++ crates/mempool_p2p/src/receiver/mod.rs
@@ -4,6 +4,7 @@ use papyrus_network::network_manager::{
     NetworkManager,
 };
 use papyrus_protobuf::mempool::RpcTransactionWrapper;
+use starknet_gateway_types::communication::SharedGatewayClient;
 use starknet_mempool_infra::component_definitions::ComponentStarter;
 
 pub struct MempoolP2pReceiver {
@@ -13,6 +14,8 @@ pub struct MempoolP2pReceiver {
     broadcasted_messages_server: BroadcastTopicServer<RpcTransactionWrapper>,
     #[allow(dead_code)]
     broadcast_messages_client: BroadcastTopicClient<RpcTransactionWrapper>,
+    #[allow(dead_code)]
+    gateway_client: SharedGatewayClient,
 }
 
 impl MempoolP2pReceiver {
@@ -20,8 +23,14 @@ impl MempoolP2pReceiver {
         network_manager: Option<NetworkManager>,
         broadcasted_messages_server: BroadcastTopicServer<RpcTransactionWrapper>,
         broadcast_messages_client: BroadcastTopicClient<RpcTransactionWrapper>,
+        gateway_client: SharedGatewayClient,
     ) -> Self {
-        Self { network_manager, broadcasted_messages_server, broadcast_messages_client }
+        Self {
+            network_manager,
+            broadcasted_messages_server,
+            broadcast_messages_client,
+            gateway_client,
+        }
     }
 }
 
