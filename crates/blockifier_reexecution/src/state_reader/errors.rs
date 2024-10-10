69f644f3af89b615fec8baf3c13f583920c715fd
Aner Ben Efraim <aner@starkware.co>
2024-10-10 16:44:44 +0300

    build(blockifier_reexecution): add reexecution errors

---
+++ crates/blockifier_reexecution/src/state_reader/errors.rs
@@ -0,0 +1,18 @@
+use blockifier::state::errors::StateError;
+use serde_json::Error as SerdeError;
+use starknet_gateway::errors::RPCStateReaderError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+#[allow(clippy::enum_variant_names)]
+pub enum ReexecutionError {
+    #[error(transparent)]
+    StateError(#[from] StateError),
+    #[error(transparent)]
+    RPCError(#[from] RPCStateReaderError),
+    #[error(transparent)]
+    SerdeError(#[from] SerdeError),
+    /// Represents all unexpected errors that may occur while reading from state.
+    #[error("Failed to read from state: {0}.")]
+    StateReadError(String),
+}
