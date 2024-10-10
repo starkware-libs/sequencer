ec42b47ec8de56e059896613001db2c502cc72c4
Lev Roitman <lev@starkware.co>
2024-10-10 13:31:08 +0300

    feat: adding sequencer motoring endpoint
    commit-id:8c8f1a23

---
+++ crates/sequencer_monitoring_endpoint/src/communication.rs
@@ -0,0 +1,11 @@
+use starknet_mempool_infra::component_server::{create_empty_server, WrapperServer};
+
+use crate::sequencer_monitoring_endpoint::SequencerMonitoringEndpoint;
+
+pub type SequencerMonitoringServer = WrapperServer<SequencerMonitoringEndpoint>;
+
+pub fn create_sequenser_monitoring_server(
+    sequencer_monitoring_endpont: SequencerMonitoringEndpoint,
+) -> SequencerMonitoringServer {
+    create_empty_server(sequencer_monitoring_endpont)
+}
