b950e4365bebe31b3a555585357f016431fac2bb
Itay Tsabary <itayt@starkware.co>
2024-10-11 12:52:19 +0300

    chore(tests-integration): change rpc state reader visibility, sort module
    commit-id:25b0246d

--- crates/tests-integration/src/integration_test_utils.rs
+++ crates/tests-integration/src/integration_test_utils.rs
@@ -19,24 +19,6 @@ use starknet_http_server::config::HttpServerConfig;
 use starknet_mempool_node::config::SequencerNodeConfig;
 use tokio::net::TcpListener;
 
-async fn create_gateway_config() -> GatewayConfig {
-    let stateless_tx_validator_config = StatelessTransactionValidatorConfig {
-        validate_non_zero_l1_gas_fee: true,
-        max_calldata_length: 10,
-        max_signature_length: 2,
-        ..Default::default()
-    };
-    let stateful_tx_validator_config = StatefulTransactionValidatorConfig::default();
-    let chain_info = ChainInfo::create_for_testing();
-
-    GatewayConfig { stateless_tx_validator_config, stateful_tx_validator_config, chain_info }
-}
-
-async fn create_http_server_config() -> HttpServerConfig {
-    let socket = get_available_socket().await;
-    HttpServerConfig { ip: socket.ip(), port: socket.port() }
-}
-
 pub async fn create_config(
     rpc_server_addr: SocketAddr,
     batcher_storage_config: StorageConfig,
@@ -52,6 +34,30 @@ pub async fn create_config(
         rpc_state_reader_config,
         ..SequencerNodeConfig::default()
     }
+}
+
+pub fn test_rpc_state_reader_config(rpc_server_addr: SocketAddr) -> RpcStateReaderConfig {
+    const RPC_SPEC_VERION: &str = "V0_8";
+    const JSON_RPC_VERSION: &str = "2.0";
+    RpcStateReaderConfig {
+        url: format!("http://{rpc_server_addr:?}/rpc/{RPC_SPEC_VERION}"),
+        json_rpc_version: JSON_RPC_VERSION.to_string(),
+    }
+}
+
+/// Returns a unique IP address and port for testing purposes.
+///
+/// Tests run in parallel, so servers (like RPC or web) running on separate tests must have
+/// different ports, otherwise the server will fail with "address already in use".
+pub async fn get_available_socket() -> SocketAddr {
+    // Dynamically select port.
+    // First, set the port to 0 (dynamic port).
+    TcpListener::bind("127.0.0.1:0")
+        .await
+        .expect("Failed to bind to address")
+        // Then, resolve to the actual selected port.
+        .local_addr()
+        .expect("Failed to get local address")
 }
 
 /// A test utility client for interacting with an http server.
@@ -92,30 +98,24 @@ impl HttpTestClient {
     }
 }
 
-fn test_rpc_state_reader_config(rpc_server_addr: SocketAddr) -> RpcStateReaderConfig {
-    const RPC_SPEC_VERION: &str = "V0_8";
-    const JSON_RPC_VERSION: &str = "2.0";
-    RpcStateReaderConfig {
-        url: format!("http://{rpc_server_addr:?}/rpc/{RPC_SPEC_VERION}"),
-        json_rpc_version: JSON_RPC_VERSION.to_string(),
-    }
+async fn create_gateway_config() -> GatewayConfig {
+    let stateless_tx_validator_config = StatelessTransactionValidatorConfig {
+        validate_non_zero_l1_gas_fee: true,
+        max_calldata_length: 10,
+        max_signature_length: 2,
+        ..Default::default()
+    };
+    let stateful_tx_validator_config = StatefulTransactionValidatorConfig::default();
+    let chain_info = ChainInfo::create_for_testing();
+
+    GatewayConfig { stateless_tx_validator_config, stateful_tx_validator_config, chain_info }
+}
+
+async fn create_http_server_config() -> HttpServerConfig {
+    let socket = get_available_socket().await;
+    HttpServerConfig { ip: socket.ip(), port: socket.port() }
 }
 
 fn create_batcher_config(batcher_storage_config: StorageConfig) -> BatcherConfig {
     BatcherConfig { storage: batcher_storage_config, ..Default::default() }
-}
-
-/// Returns a unique IP address and port for testing purposes.
-///
-/// Tests run in parallel, so servers (like RPC or web) running on separate tests must have
-/// different ports, otherwise the server will fail with "address already in use".
-pub async fn get_available_socket() -> SocketAddr {
-    // Dynamically select port.
-    // First, set the port to 0 (dynamic port).
-    TcpListener::bind("127.0.0.1:0")
-        .await
-        .expect("Failed to bind to address")
-        // Then, resolve to the actual selected port.
-        .local_addr()
-        .expect("Failed to get local address")
 }
