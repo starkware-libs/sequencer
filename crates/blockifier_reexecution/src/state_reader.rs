2652eff2a5d1d6c95f687cb8c2a203f0a62d51cc
Aner Ben Efraim <aner@starkware.co>
2024-10-11 15:54:18 +0300

    chore(blockifier_reexecution): full reexecution poc

--- crates/blockifier_reexecution/src/state_reader.rs
+++ crates/blockifier_reexecution/src/state_reader.rs
@@ -3,7 +3,7 @@ mod errors;
 #[cfg(test)]
 pub mod raw_rpc_json_test;
 #[cfg(test)]
-#[cfg(feature = "blockifier_regression_https_testing")]
+//#[cfg(feature = "blockifier_regression_https_testing")]
 pub mod rpc_https_test;
 pub mod serde_utils;
 pub mod test_state_reader;
