c161ccaa31464e401f96e3101bdbf3354374ce21
matan-starkware <97523054+matan-starkware@users.noreply.github.com>
2024-10-13 13:14:32 +0300

    feat(consensus): implement ComponentStarter for the consensus manager component (#1199)
    Run node with:
    ```
    cargo run --package starknet_mempool_node --bin starknet_mempool_node -- \
      --consensus_manager_config.consensus_config.validator_id 0x0 \
      --batcher_config.storage.db_config.enforce_file_exists false
    ```

--- crates/papyrus_protobuf/src/consensus.rs
+++ crates/papyrus_protobuf/src/consensus.rs
@@ -1,6 +1,7 @@
 use futures::channel::{mpsc, oneshot};
 use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::core::ContractAddress;
+use starknet_api::executable_transaction::Transaction as ExecutableTransaction;
 use starknet_api::transaction::Transaction;
 
 use crate::converters::ProtobufConversionError;
@@ -104,6 +105,28 @@ impl From<ProposalWrapper>
             content_sender.try_send(tx).expect("Send should succeed");
         }
         content_sender.close_channel();
+
+        let (fin_sender, fin_receiver) = oneshot::channel();
+        fin_sender.send(val.0.block_hash).expect("Send should succeed");
+
+        (proposal_init, content_receiver, fin_receiver)
+    }
+}
+
+impl From<ProposalWrapper>
+    for (
+        (BlockNumber, u32, ContractAddress, Option<u32>),
+        mpsc::Receiver<Vec<ExecutableTransaction>>,
+        oneshot::Receiver<BlockHash>,
+    )
+{
+    fn from(val: ProposalWrapper) -> Self {
+        let proposal_init =
+            (BlockNumber(val.0.height), val.0.round, val.0.proposer, val.0.valid_round);
+
+        let (_, content_receiver) = mpsc::channel(0);
+        // This should only be used for Milestone 1, and then removed once streaming is supported.
+        println!("Cannot build ExecutableTransaction from Transaction.");
 
         let (fin_sender, fin_receiver) = oneshot::channel();
         fin_sender.send(val.0.block_hash).expect("Send should succeed");
