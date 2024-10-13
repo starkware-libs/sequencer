5308539e39fe0e9bf625d5c9a7acc0e21b78ca75
Mohammad Nassar <mohammad@starkware.co>
2024-10-13 12:43:58 +0300

    test(mempool): two queues test get txs

--- crates/mempool/src/mempool_test.rs
+++ crates/mempool/src/mempool_test.rs
@@ -94,6 +94,12 @@ impl MempoolContentBuilder {
         self
     }
 
+    fn with_gas_price_threshold(mut self, gas_price_threshold: u128) -> Self {
+        self.tx_queue_content_builder =
+            self.tx_queue_content_builder.with_gas_price_threshold(gas_price_threshold);
+        self
+    }
+
     fn with_fee_escalation_percentage(mut self, fee_escalation_percentage: u8) -> Self {
         self.fee_escalation_percentage = fee_escalation_percentage;
         self
@@ -200,16 +206,28 @@ fn test_get_txs_does_not_remove_returned_txs_from_pool() {
 #[rstest]
 fn test_get_txs_replenishes_queue_only_between_chunks() {
     // Setup.
-    let tx_address_0_nonce_0 = tx!(tip: 20, tx_hash: 1, sender_address: "0x0", tx_nonce: 0);
-    let tx_address_0_nonce_1 = tx!(tip: 20, tx_hash: 2, sender_address: "0x0", tx_nonce: 1);
-    let tx_address_1_nonce_0 = tx!(tip: 10, tx_hash: 3, sender_address: "0x1", tx_nonce: 0);
+    let priority_tx_address_0_nonce_0 = tx!(tip: 20, tx_hash: 1, sender_address: "0x0", tx_nonce: 0,
+        max_l2_gas_price: 100);
+    let pending_tx_address_0_nonce_1 = tx!(tip: 20, tx_hash: 2, sender_address: "0x0", tx_nonce: 1,
+        max_l2_gas_price: 99);
+    let priority_tx_address_0_nonce_2 = tx!(tip: 20, tx_hash: 3, sender_address: "0x0", tx_nonce: 2,
+        max_l2_gas_price: 100);
+    let priority_tx_address_1_nonce_0 = tx!(tip: 10, tx_hash: 4, sender_address: "0x1", tx_nonce: 0,
+        max_l2_gas_price: 100);
 
-    let queue_txs = [&tx_address_0_nonce_0, &tx_address_1_nonce_0].map(TransactionReference::new);
-    let pool_txs =
-        [&tx_address_0_nonce_0, &tx_address_0_nonce_1, &tx_address_1_nonce_0].map(|tx| tx.clone());
+    let queue_txs = [&priority_tx_address_0_nonce_0, &priority_tx_address_1_nonce_0]
+        .map(TransactionReference::new);
+    let pool_txs = [
+        &priority_tx_address_0_nonce_0,
+        &pending_tx_address_0_nonce_1,
+        &priority_tx_address_0_nonce_2,
+        &priority_tx_address_1_nonce_0,
+    ]
+    .map(|tx| tx.clone());
     let mut mempool = MempoolContentBuilder::new()
         .with_pool(pool_txs)
         .with_priority_queue(queue_txs)
+        .with_gas_price_threshold(100)
         .build_into_mempool();
 
     // Test and assert: all transactions returned.
@@ -218,9 +236,12 @@ fn test_get_txs_replenishes_queue_only_between_chunks() {
     get_txs_and_assert_expected(
         &mut mempool,
         3,
-        &[tx_address_0_nonce_0, tx_address_1_nonce_0, tx_address_0_nonce_1],
+        &[priority_tx_address_0_nonce_0, priority_tx_address_1_nonce_0],
     );
-    let expected_mempool_content = MempoolContentBuilder::new().with_priority_queue([]).build();
+    let expected_mempool_content = MempoolContentBuilder::new()
+        .with_priority_queue([])
+        ._with_pending_queue([TransactionReference::new(&pending_tx_address_0_nonce_1)])
+        .build();
     expected_mempool_content.assert_eq(&mempool);
 }
 
