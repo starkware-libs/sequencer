d3a772a2430935f4ceb06b69d3a0ddb1484d52fd
nadin <nadin@starkware.co>
2024-10-14 17:15:14 +0300

    refactor: clean config test
    commit-id:4aa9e5ef

--- crates/mempool_node/src/config/config_test.rs
+++ crates/mempool_node/src/config/config_test.rs
@@ -6,140 +6,21 @@ use assert_matches::assert_matches;
 use colored::Colorize;
 use mempool_test_utils::get_absolute_path;
 use papyrus_config::dumping::SerializeConfig;
-use papyrus_config::validators::{ParsedValidationError, ParsedValidationErrors};
 use rstest::rstest;
 use starknet_mempool_infra::component_definitions::{
     LocalComponentCommunicationConfig,
     RemoteClientConfig,
     RemoteServerConfig,
 };
-use validator::{Validate, ValidationErrors};
+use validator::Validate;
 
 use crate::config::{
-    ComponentConfig,
     ComponentExecutionConfig,
     ComponentExecutionMode,
     SequencerNodeConfig,
     CONFIG_POINTERS,
     DEFAULT_CONFIG_PATH,
 };
-
-fn check_validation_error(
-    validation_result: Result<(), ValidationErrors>,
-    code_str: &str,
-    message_str: &str,
-) {
-    assert_matches!(validation_result.unwrap_err(), validation_errors => {
-        let parsed_errors = ParsedValidationErrors::from(validation_errors);
-        assert_eq!(parsed_errors.0.len(), 1);
-        let parsed_validation_error = &parsed_errors.0[0];
-        assert_matches!(
-            parsed_validation_error,
-            ParsedValidationError { param_path, code, message, params}
-            if (
-                param_path == "__all__" &&
-                code == code_str &&
-                params.is_empty() &&
-                *message == Some(message_str.to_string())
-            )
-        )
-    });
-}
-
-/// Test the validation of the struct ComponentExecutionConfig.
-/// Validates that execution mode of the component and the local/remote config are at sync.
-#[rstest]
-#[case(
-    ComponentExecutionMode::Local,
-    Some(LocalComponentCommunicationConfig::default()),
-    Some(RemoteClientConfig::default()),
-    Some(RemoteServerConfig::default()),
-    "Local config and Remote config are mutually exclusive in Local mode execution, can't be both \
-     active."
-)]
-#[case(
-    ComponentExecutionMode::Local,
-    Some(LocalComponentCommunicationConfig::default()),
-    None,
-    Some(RemoteServerConfig::default()),
-    "Local config and Remote config are mutually exclusive in Local mode execution, can't be both \
-     active."
-)]
-#[case(
-    ComponentExecutionMode::Local,
-    Some(LocalComponentCommunicationConfig::default()),
-    Some(RemoteClientConfig::default()),
-    None,
-    "Local config and Remote config are mutually exclusive in Local mode execution, can't be both \
-     active."
-)]
-#[case(
-    ComponentExecutionMode::Local,
-    None,
-    Some(RemoteClientConfig::default()),
-    Some(RemoteServerConfig::default()),
-    "Local communication config is missing."
-)]
-#[case(
-    ComponentExecutionMode::Local,
-    None,
-    None,
-    Some(RemoteServerConfig::default()),
-    "Local communication config is missing."
-)]
-#[case(
-    ComponentExecutionMode::Local,
-    None,
-    Some(RemoteClientConfig::default()),
-    None,
-    "Local communication config is missing."
-)]
-#[case(ComponentExecutionMode::Local, None, None, None, "Local communication config is missing.")]
-#[case(
-    ComponentExecutionMode::Remote,
-    Some(LocalComponentCommunicationConfig::default()),
-    None,
-    None,
-    "Remote communication config is missing."
-)]
-#[case(
-    ComponentExecutionMode::Remote,
-    None,
-    Some(RemoteClientConfig::default()),
-    Some(RemoteServerConfig::default()),
-    "Remote client and Remote server are mutually exclusive in Remote mode execution, can't be \
-     both active."
-)]
-#[case(
-    ComponentExecutionMode::Remote,
-    Some(LocalComponentCommunicationConfig::default()),
-    Some(RemoteClientConfig::default()),
-    Some(RemoteServerConfig::default()),
-    "Remote client and Remote server are mutually exclusive in Remote mode execution, can't be \
-     both active."
-)]
-#[case(ComponentExecutionMode::Remote, None, None, None, "Remote communication config is missing.")]
-fn test_invalid_component_execution_config(
-    #[case] execution_mode: ComponentExecutionMode,
-    #[case] local_config: Option<LocalComponentCommunicationConfig>,
-    #[case] remote_client_config: Option<RemoteClientConfig>,
-    #[case] remote_server_config: Option<RemoteServerConfig>,
-    #[case] expected_error_message: &str,
-) {
-    // Initialize an invalid config and check that the validator finds an error.
-    let component_exe_config = ComponentExecutionConfig {
-        execution_mode,
-        local_config,
-        remote_client_config,
-        remote_server_config,
-        ..ComponentExecutionConfig::default()
-    };
-    check_validation_error(
-        component_exe_config.validate(),
-        "Invalid component configuration.",
-        expected_error_message,
-    );
-}
 
 /// Test the validation of the struct ComponentExecutionConfig.
 /// Validates that execution mode of the component and the local/remote config are at sync.
@@ -168,72 +49,6 @@ fn test_valid_component_execution_config(
         ..ComponentExecutionConfig::default()
     };
     assert_eq!(component_exe_config.validate(), Ok(()));
-}
-
-#[test]
-fn test_invalid_components_config() {
-    // Initialize an invalid config and check that the validator finds an error.
-    let component_config = ComponentConfig {
-        batcher: ComponentExecutionConfig { execute: false, ..ComponentExecutionConfig::default() },
-        consensus_manager: ComponentExecutionConfig {
-            execute: false,
-            ..ComponentExecutionConfig::default()
-        },
-        gateway: ComponentExecutionConfig { execute: false, ..ComponentExecutionConfig::default() },
-        http_server: ComponentExecutionConfig {
-            execute: false,
-            ..ComponentExecutionConfig::default()
-        },
-        mempool: ComponentExecutionConfig { execute: false, ..ComponentExecutionConfig::default() },
-    };
-
-    check_validation_error(
-        component_config.validate(),
-        "Invalid components configuration.",
-        "At least one component should be allowed to execute.",
-    );
-}
-
-/// Test the validation of the struct ComponentConfig.
-/// The validation validates at least one of the components is set with execute: true.
-#[rstest]
-#[case(true, false, false, false, false)]
-#[case(false, true, false, false, false)]
-#[case(false, false, true, false, false)]
-#[case(false, false, false, true, false)]
-#[case(false, false, false, false, true)]
-fn test_valid_components_config(
-    #[case] batcher_component_execute: bool,
-    #[case] consensus_manager_component_execute: bool,
-    #[case] gateway_component_execute: bool,
-    #[case] http_server_component_execute: bool,
-    #[case] mempool_component_execute: bool,
-) {
-    // Initialize an invalid config and check that the validator finds an error.
-    let component_config = ComponentConfig {
-        batcher: ComponentExecutionConfig {
-            execute: batcher_component_execute,
-            ..ComponentExecutionConfig::default()
-        },
-        consensus_manager: ComponentExecutionConfig {
-            execute: consensus_manager_component_execute,
-            ..ComponentExecutionConfig::default()
-        },
-        gateway: ComponentExecutionConfig {
-            execute: gateway_component_execute,
-            ..ComponentExecutionConfig::default()
-        },
-        http_server: ComponentExecutionConfig {
-            execute: http_server_component_execute,
-            ..ComponentExecutionConfig::default()
-        },
-        mempool: ComponentExecutionConfig {
-            execute: mempool_component_execute,
-            ..ComponentExecutionConfig::default()
-        },
-    };
-
-    assert_matches!(component_config.validate(), Ok(()));
 }
 
 /// Test the validation of the struct SequencerNodeConfig and that the default config file is up to
