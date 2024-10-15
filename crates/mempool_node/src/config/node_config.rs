a4d66af540f07642a1008e87a77726e0e56941e8
Shahak Shama <shahak@starkware.co>
2024-10-15 16:33:41 +0300

    feat(mempool_node): add NetworkConfig to SequencerNodeConfig

--- crates/mempool_node/src/config/node_config.rs
+++ crates/mempool_node/src/config/node_config.rs
@@ -14,6 +14,7 @@ use starknet_batcher::config::BatcherConfig;
 use starknet_consensus_manager::config::ConsensusManagerConfig;
 use starknet_gateway::config::{GatewayConfig, RpcStateReaderConfig};
 use starknet_http_server::config::HttpServerConfig;
+use starknet_mempool_p2p::MempoolP2pConfig;
 use starknet_monitoring_endpoint::config::MonitoringEndpointConfig;
 use starknet_sierra_compile::config::SierraToCasmCompilationConfig;
 use validator::Validate;
@@ -34,6 +35,7 @@ pub static CONFIG_POINTERS: LazyLock<ConfigPointers> = LazyLock::new(|| {
             "batcher_config.block_builder_config.chain_info.chain_id".to_owned(),
             "batcher_config.storage.db_config.chain_id".to_owned(),
             "gateway_config.chain_info.chain_id".to_owned(),
+            "mempool_p2p_config.network_config.chain_id".to_owned(),
         ],
     )]
 });
@@ -61,6 +63,8 @@ pub struct SequencerNodeConfig {
     pub compiler_config: SierraToCasmCompilationConfig,
     #[validate]
     pub monitoring_endpoint_config: MonitoringEndpointConfig,
+    #[validate]
+    pub mempool_p2p_config: MempoolP2pConfig,
 }
 
 impl SerializeConfig for SequencerNodeConfig {
@@ -81,6 +85,7 @@ impl SerializeConfig for SequencerNodeConfig {
                 self.monitoring_endpoint_config.dump(),
                 "monitoring_endpoint_config",
             ),
+            append_sub_config_name(self.mempool_p2p_config.dump(), "mempool_p2p_config"),
         ];
 
         sub_configs.into_iter().flatten().collect()
@@ -99,6 +104,7 @@ impl Default for SequencerNodeConfig {
             rpc_state_reader_config: Default::default(),
             compiler_config: Default::default(),
             monitoring_endpoint_config: Default::default(),
+            mempool_p2p_config: Default::default(),
         }
     }
 }
