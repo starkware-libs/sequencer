c877dd9bb79bb84b898a7cd478c9085dd5efa403
Itay Tsabary <itayt@starkware.co>
2024-10-15 10:58:54 +0300

    chore(mempool_node): remove unused consensus manager
    commit-id:f6c787cc

--- crates/consensus_manager_types/src/consensus_manager_types.rs
+++ crates/consensus_manager_types/src/consensus_manager_types.rs
@@ -3,8 +3,6 @@ use std::fmt::Debug;
 use derive_more::Display;
 use serde::{Deserialize, Serialize};
 
-use crate::errors::ConsensusManagerError;
-
 // TODO (Matan) decide on the id structure
 #[derive(
     Copy,
@@ -21,21 +19,3 @@ use crate::errors::ConsensusManagerError;
     Hash,
 )]
 pub struct ProposalId(pub u64);
-
-// TODO(Tsabary/Matan): Populate the data structure used to invoke the consensus manager.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ConsensusManagerFnOneInput {}
-
-// TODO(Tsabary/Matan): Populate the data structure used to invoke the consensus manager.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ConsensusManagerFnTwoInput {}
-
-// TODO(Tsabary/Matan): Replace with the actual return type of the consensus manager function.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ConsensusManagerFnOneReturnValue {}
-
-// TODO(Tsabary/Matan): Replace with the actual return type of the consensus manager function.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ConsensusManagerFnTwoReturnValue {}
-
-pub type ConsensusManagerResult<T> = Result<T, ConsensusManagerError>;
