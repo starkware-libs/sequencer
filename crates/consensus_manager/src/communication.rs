56a4e9e92924fede9b14ba87e2bb1dea475e5a08
Itay Tsabary <itayt@starkware.co>
2024-10-15 10:43:35 +0300

    chore(consensus): remove unused request handling code
    commit-id:9eed0cac

--- crates/consensus_manager/src/communication.rs
+++ crates/consensus_manager/src/communication.rs
@@ -1,9 +1,3 @@
-use async_trait::async_trait;
-use starknet_consensus_manager_types::communication::{
-    ConsensusManagerRequest,
-    ConsensusManagerResponse,
-};
-use starknet_mempool_infra::component_definitions::ComponentRequestHandler;
 use starknet_mempool_infra::component_server::WrapperServer;
 
 use crate::consensus_manager::ConsensusManager;
@@ -14,29 +8,4 @@ pub fn create_consensus_manager_server(
     consensus_manager: ConsensusManager,
 ) -> ConsensusManagerServer {
     WrapperServer::new(consensus_manager)
-}
-
-#[async_trait]
-impl ComponentRequestHandler<ConsensusManagerRequest, ConsensusManagerResponse>
-    for ConsensusManager
-{
-    async fn handle_request(
-        &mut self,
-        request: ConsensusManagerRequest,
-    ) -> ConsensusManagerResponse {
-        match request {
-            ConsensusManagerRequest::ConsensusManagerFnOne(_consensus_manager_input) => {
-                // TODO(Tsabary/Matan): Invoke a function that returns a
-                // ConsensusManagerResult<ConsensusManagerFnOneReturnValue>, and return
-                // the ConsensusManagerResponse::ConsensusManagerFnOneInput accordingly.
-                unimplemented!()
-            }
-            ConsensusManagerRequest::ConsensusManagerFnTwo(_consensus_manager_input) => {
-                // TODO(Tsabary/Matan): Invoke a function that returns a
-                // ConsensusManagerResult<ConsensusManagerFnTwoReturnValue>, and return
-                // the ConsensusManagerResponse::ConsensusManagerFnTwoInput accordingly.
-                unimplemented!()
-            }
-        }
-    }
 }
