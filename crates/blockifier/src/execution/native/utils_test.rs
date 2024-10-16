012b59cbe9c6dc1d5e1f881bf9bcb670469234d3
Rodrigo <rodrodpino@gmail.com>
2024-10-15 20:18:33 -0400

    refactor: use ExecutionFailed error instead of NativeExecutionError

--- crates/blockifier/src/execution/native/utils_test.rs
+++ crates/blockifier/src/execution/native/utils_test.rs
@@ -5,6 +5,7 @@ use starknet_api::core::EntryPointSelector;
 use starknet_api::execution_utils::format_panic_data;
 use starknet_types_core::felt::Felt;
 
+use crate::execution::execution_utils::format_panic_data;
 use crate::execution::native::utils::{
     contract_entrypoint_to_entrypoint_selector,
     encode_str_as_felts,
