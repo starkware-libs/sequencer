2f2469f2b5982129a83ebc2c50a65195945674e9
Dori Medini <dori@starkware.co>
2024-10-19 18:10:07 +0300

    test(blockifier): add class hash (if set) to cairo1 revert trace

--- crates/blockifier/src/execution/stack_trace_test.rs
+++ crates/blockifier/src/execution/stack_trace_test.rs
@@ -215,12 +215,12 @@ An ASSERT_EQ instruction failed: 1 != 0.
 0: Error in the called contract (contract address: {account_address_felt:#064x}, class hash: \
          {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
 Execution failed. Failure reason:
-Error in contract (contract address: {account_address_felt:#064x}, selector: \
-         {execute_selector_felt:#064x}):
-Error in contract (contract address: {test_contract_address_felt:#064x}, selector: \
-         {external_entry_point_selector_felt:#064x}):
-Error in contract (contract address: {test_contract_address_2_felt:#064x}, selector: \
-         {inner_entry_point_selector_felt:#064x}):
+Error in contract (contract address: {account_address_felt:#064x}, class hash: \
+         {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
+Error in contract (contract address: {test_contract_address_felt:#064x}, class hash: \
+         {test_contract_hash:#064x}, selector: {external_entry_point_selector_felt:#064x}):
+Error in contract (contract address: {test_contract_address_2_felt:#064x}, class hash: \
+         {test_contract_hash:#064x}, selector: {inner_entry_point_selector_felt:#064x}):
 0x6661696c ('fail').
 "
     );
@@ -338,12 +338,12 @@ Unknown location (pc=0:{expected_pc1})
 0: Error in the called contract (contract address: {account_address_felt:#064x}, class hash: \
                  {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
 Execution failed. Failure reason:
-Error in contract (contract address: {account_address_felt:#064x}, selector: \
-                 {execute_selector_felt:#064x}):
-Error in contract (contract address: {contract_address_felt:#064x}, selector: \
-                 {invoke_call_chain_selector_felt:#064x}):
-Error in contract (contract address: {contract_address_felt:#064x}, selector: \
-                 {invoke_call_chain_selector_felt:#064x}):
+Error in contract (contract address: {account_address_felt:#064x}, class hash: \
+                 {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
+Error in contract (contract address: {contract_address_felt:#064x}, class hash: \
+                 {test_contract_hash:#064x}, selector: {invoke_call_chain_selector_felt:#064x}):
+Error in contract (contract address: {contract_address_felt:#064x}, class hash: \
+                 {test_contract_hash:#064x}, selector: {invoke_call_chain_selector_felt:#064x}):
 {expected_error}.
 "
             )
@@ -491,14 +491,14 @@ Unknown location (pc=0:{expected_pc3})
 0: Error in the called contract (contract address: {account_address_felt:#064x}, class hash: \
                  {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
 Execution failed. Failure reason:
-Error in contract (contract address: {account_address_felt:#064x}, selector: \
-                 {execute_selector_felt:#064x}):
-Error in contract (contract address: {address_felt:#064x}, selector: \
-                 {invoke_call_chain_selector_felt:#064x}):
-Error in contract (contract address: {address_felt:#064x}, selector: \
-                 {invoke_call_chain_selector_felt:#064x}):
-Error in contract (contract address: {address_felt:#064x}, selector: \
-                 {last_func_selector_felt:#064x}):
+Error in contract (contract address: {account_address_felt:#064x}, class hash: \
+                 {account_contract_hash:#064x}, selector: {execute_selector_felt:#064x}):
+Error in contract (contract address: {address_felt:#064x}, class hash: {test_contract_hash:#064x}, \
+                 selector: {invoke_call_chain_selector_felt:#064x}):
+Error in contract (contract address: {address_felt:#064x}, class hash: {test_contract_hash:#064x}, \
+                 selector: {invoke_call_chain_selector_felt:#064x}):
+Error in contract (contract address: {address_felt:#064x}, class hash: {test_contract_hash:#064x}, \
+                 selector: {last_func_selector_felt:#064x}):
 {expected_error}.
 "
             )
@@ -598,8 +598,10 @@ An ASSERT_EQ instruction failed: 1 != 0.
         ),
         CairoVersion::Cairo1 => format!(
             "The `validate` entry point panicked with:
-Error in contract (contract address: {contract_address:#064x}, selector: {selector:#064x}):
-0x496e76616c6964207363656e6172696f ('Invalid scenario')."
+Error in contract (contract address: {contract_address:#064x}, class hash: {:#064x}, selector: \
+             {selector:#064x}):
+0x496e76616c6964207363656e6172696f ('Invalid scenario').",
+            class_hash.0
         ),
     };
 
@@ -666,10 +668,11 @@ An ASSERT_EQ instruction failed: 1 != 0.
             .to_string(),
             CairoVersion::Cairo1 => format!(
                 "Execution failed. Failure reason:
-Error in contract (contract address: {expected_address:#064x}, selector: \
+Error in contract (contract address: {expected_address:#064x}, class hash: {:#064x}, selector: \
                  {expected_selector:#064x}):
 0x496e76616c6964207363656e6172696f ('Invalid scenario').
-"
+",
+                class_hash.0
             )
             .to_string(),
         };
@@ -799,11 +802,13 @@ Error at pc=0:{}:
 Error at pc=0:{}:
 {frame_2}
 Execution failed. Failure reason:
-Error in contract (contract address: {expected_address:#064x}, selector: {:#064x}):
+Error in contract (contract address: {expected_address:#064x}, class hash: {:#064x}, selector: \
+                 {:#064x}):
 0x496e76616c6964207363656e6172696f ('Invalid scenario').
 ",
                 execute_offset + 205,
                 deploy_offset + 194,
+                faulty_class_hash.0,
                 ctor_selector.0
             )
         }
