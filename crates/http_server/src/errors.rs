f5e66d5d7808fd4425f2058fc308bb354ed5f0b3
Itay Tsabary <itayt@starkware.co>
2024-10-21 14:20:51 +0300

    chore: clean http server module
    commit-id:654340c1

--- crates/http_server/src/errors.rs
+++ crates/http_server/src/errors.rs
@@ -1,1 +1,8 @@
+use thiserror::Error;
 
+/// Errors originating from `[`HttpServer::run`]` command.
+#[derive(Debug, Error)]
+pub enum HttpServerRunError {
+    #[error(transparent)]
+    ServerStartupError(#[from] hyper::Error),
+}
