6ebe7b8e93343b059ec1ada93e3cd1ed2ebbd7d4
Itay Tsabary <itayt@starkware.co>
2024-10-21 11:46:48 +0300

    chore(mempool_infra): server tracing
    commit-id:1ade9262

--- crates/mempool_infra/src/component_server/definitions.rs
+++ crates/mempool_infra/src/component_server/definitions.rs
@@ -1,9 +1,10 @@
 use std::any::type_name;
+use std::fmt::Debug;
 
 use async_trait::async_trait;
 use thiserror::Error;
 use tokio::sync::mpsc::Receiver;
-use tracing::info;
+use tracing::{debug, info};
 
 use crate::component_definitions::{ComponentRequestAndResponseSender, ComponentRequestHandler};
 use crate::errors::ComponentServerError;
@@ -18,28 +19,28 @@ pub async fn request_response_loop<Request, Response, Component>(
     component: &mut Component,
 ) where
     Component: ComponentRequestHandler<Request, Response> + Send + Sync,
-    Request: Send + Sync,
-    Response: Send + Sync,
+    Request: Send + Sync + Debug,
+    Response: Send + Sync + Debug,
 {
-    info!("Starting server loop for component {}", type_name::<Component>());
+    info!("Starting server for component {}", type_name::<Component>());
 
     // TODO(Tsabary): Move this function to be part of the `local_server` module.
     while let Some(request_and_res_tx) = rx.recv().await {
-        info!("Component {} received request", type_name::<Component>());
-
         let request = request_and_res_tx.request;
         let tx = request_and_res_tx.tx;
+        debug!("Component {} received request {:?}", type_name::<Component>(), request);
 
-        let res = component.handle_request(request).await;
+        let response = component.handle_request(request).await;
+        debug!("Component {} is sending response {:?}", type_name::<Component>(), response);
 
         // TODO(Tsabary): revert `try_send` to `send` once the client is guaranteed to be alive,
-        // i.e., tx.send(res).await.expect("Response connection should be open.");
+        // i.e., tx.send(response).await.expect("Response connection should be open.");
         // Tries sending the response to the client. If the client has disconnected then this
         // becomes a null operation.
-        let _ = tx.try_send(res);
+        let _ = tx.try_send(response);
     }
 
-    info!("Finished server loop for component {}", type_name::<Component>());
+    info!("Stopping server for component {}", type_name::<Component>());
 }
 
 // TODO(Tsabary): Create an error module and move this error there.
