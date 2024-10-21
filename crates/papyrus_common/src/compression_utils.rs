c66f84861a13ad3eb60bcdda06eecabadf408a6e
Noam Spiegelstein <noam.s@starkware.co>
2024-10-21 23:52:42 +0300

    chore: implement decode_and_decompress + test

--- crates/papyrus_common/src/compression_utils.rs
+++ crates/papyrus_common/src/compression_utils.rs
@@ -1,6 +1,18 @@
+use std::io::Read;
+
 #[cfg(test)]
 #[path = "compression_utils_test.rs"]
 mod compression_utils_test;
+
+#[derive(thiserror::Error, Debug)]
+pub enum CompressionError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+    #[error(transparent)]
+    Decode(#[from] base64::DecodeError),
+}
 
 // Compress the value using gzip with the default compression level and encode it in base64.
 pub fn compress_and_encode(value: serde_json::Value) -> Result<String, std::io::Error> {
@@ -8,4 +20,14 @@ pub fn compress_and_encode(value: serde_json::Value) -> Result<String, std::io::
     serde_json::to_writer(&mut compressor, &value)?;
     let compressed_data = compressor.finish()?;
     Ok(base64::encode(compressed_data))
+}
+
+// Decompress the value from base64 and gzip.
+pub fn decode_and_decompress(value: &str) -> Result<serde_json::Value, CompressionError> {
+    let decoded_data = base64::decode(value)?;
+    let mut decompressor = flate2::read::GzDecoder::new(&decoded_data[..]);
+    let mut decompressed_data = String::new();
+    decompressor.read_to_string(&mut decompressed_data)?;
+    let json_value = serde_json::from_str(&decompressed_data)?;
+    Ok(json_value)
 }
--- crates/papyrus_common/src/compression_utils_test.rs
+++ crates/papyrus_common/src/compression_utils_test.rs
@@ -1,12 +1,21 @@
 use pretty_assertions::assert_eq;
 use starknet_api::test_utils::read_json_file;
 
-use super::compress_and_encode;
+use super::{compress_and_encode, decode_and_decompress};
 
 #[test]
 fn compress_and_encode_hardcoded_value() {
     let sierra_program = read_json_file("sierra_program.json");
     let expected_value = read_json_file("sierra_program_base64.json").as_str().unwrap().to_owned();
     let value = compress_and_encode(sierra_program).unwrap();
+    assert_eq!(value, expected_value);
+}
+
+#[test]
+fn decode_and_decompress_hardcoded_value() {
+    let sierra_program_base64 =
+        read_json_file("sierra_program_base64.json").as_str().unwrap().to_owned();
+    let expected_value = read_json_file("sierra_program.json");
+    let value = decode_and_decompress(&sierra_program_base64).unwrap();
     assert_eq!(value, expected_value);
 }
