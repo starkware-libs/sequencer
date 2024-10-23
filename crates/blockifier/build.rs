c7b025f74ec2119b8e754aaea52bef68c47bff0f
Rodrigo <rodrodpino@gmail.com>
2024-10-23 01:22:28 +0100

    feat: rework native runtime dependency CI
    Instead of custom script, swapped for the addition of Cairo Native as a git submodule.
    The runtime will be compiled once the compilation is succesful.
    
    A known caveat of this approach (using build.rs) is that if there is nothing new to
    compile and the native_runtime.a library won't be recompiled, even if it was deleted or updated.
    
    Another caveat is that both the git submodule and Cairo Native
    dependency need be updated at the same time. We cannot directly set the
    submodule as a dependency because cairo native is also a workspace (such
    as this repo) and there cannot be two workspace roots.

---
+++ crates/blockifier/build.rs
@@ -0,0 +1,52 @@
+use std::path::PathBuf;
+use std::process::Command;
+
+fn compile_cairo_native_aot_runtime() {
+    let runtime_target_dir = std::env::current_dir()
+        .expect("Failed to get current directory")
+        .join(PathBuf::from("./cairo_native/target/"));
+
+    let status = Command::new("cargo")
+        .args([
+            "build",
+            "--release",
+            "-p",
+            "cairo-native-runtime",
+            "--message-format=json",
+            "--target-dir",
+            runtime_target_dir.to_str().unwrap(),
+        ])
+        .current_dir("cairo_native")
+        .status()
+        .expect("Failed to execute cargo");
+    if !status.success() {
+        panic!("Building cairo native runtime failed: {status}")
+    }
+
+    let runtime_target_path =
+        runtime_target_dir.join(PathBuf::from("release/libcairo_native_runtime.a"));
+
+    let runtime_expected_path = {
+        let expected_path_env = std::env::var("CAIRO_NATIVE_RUNTIME_LIBRARY")
+            .expect("'CAIRO_NATIVE_RUNTIME_LIBRARY' variable is not set");
+        let expected_path = PathBuf::from(&expected_path_env);
+
+        if expected_path.is_absolute() {
+            expected_path
+        } else {
+            std::env::current_dir().expect("Failed to get current directory").join(expected_path)
+        }
+    };
+
+    std::fs::copy(&runtime_target_path, &runtime_expected_path).expect(&format!(
+        "Cannot copy cairo native runtime from \"{}\" to \"{}\"",
+        runtime_target_path.to_str().unwrap(),
+        runtime_expected_path.to_str().unwrap(),
+    ));
+}
+
+fn main() {
+    // todo(rdr): known issue of this approach is that if the user deletes libcairo_native_runtime.a
+    // and re-compiles with further changes, `build.rs` won't activate
+    compile_cairo_native_aot_runtime();
+}
