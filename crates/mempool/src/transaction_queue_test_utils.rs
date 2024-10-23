70f3f0ecf03695bcc12c9572bd2c67ddf772dc38
Mohammad Nassar <mohammad@starkware.co>
2024-10-23 14:21:39 +0300

    fix(mempool): fix order bug in transaction queue content

--- crates/mempool/src/transaction_queue_test_utils.rs
+++ crates/mempool/src/transaction_queue_test_utils.rs
@@ -1,29 +1,33 @@
-use std::collections::{BTreeSet, HashMap};
+use std::collections::HashMap;
 
-use pretty_assertions::assert_eq;
 use starknet_api::block::GasPrice;
 
 use crate::mempool::TransactionReference;
 use crate::transaction_queue::{PendingTransaction, PriorityTransaction, TransactionQueue};
+
+type OptionalPriorityTransactions = Option<Vec<PriorityTransaction>>;
+type OptionalPendingTransactions = Option<Vec<PendingTransaction>>;
 
 /// Represents the internal content of the transaction queue.
 /// Enables customized (and potentially inconsistent) creation for unit testing.
 /// Note: gas price threshold is only used for building the (non-test) queue struct.
 #[derive(Debug, Default)]
 pub struct TransactionQueueContent {
-    priority_queue: Option<BTreeSet<PriorityTransaction>>,
-    pending_queue: Option<BTreeSet<PendingTransaction>>,
+    priority_queue: OptionalPriorityTransactions,
+    pending_queue: OptionalPendingTransactions,
     gas_price_threshold: Option<GasPrice>,
 }
 
 impl TransactionQueueContent {
     pub fn assert_eq(&self, tx_queue: &TransactionQueue) {
         if let Some(priority_queue) = &self.priority_queue {
-            assert_eq!(&tx_queue.priority_queue, priority_queue);
+            let tx_priority_queue: Vec<_> = tx_queue.priority_queue.iter().rev().copied().collect();
+            assert!(priority_queue.iter().copied().eq(tx_priority_queue));
         }
 
         if let Some(pending_queue) = &self.pending_queue {
-            assert_eq!(&tx_queue.pending_queue, pending_queue);
+            let tx_pending_queue: Vec<_> = tx_queue.pending_queue.iter().rev().copied().collect();
+            assert!(pending_queue.iter().copied().eq(tx_pending_queue));
         }
     }
 
@@ -45,14 +49,19 @@ impl TransactionQueueContent {
             }
         }
 
-        TransactionQueue { priority_queue, pending_queue, address_to_tx, gas_price_threshold }
+        TransactionQueue {
+            priority_queue: priority_queue.into_iter().collect(),
+            pending_queue: pending_queue.into_iter().collect(),
+            address_to_tx,
+            gas_price_threshold,
+        }
     }
 }
 
 #[derive(Debug, Default)]
 pub struct TransactionQueueContentBuilder {
-    priority_queue: Option<BTreeSet<PriorityTransaction>>,
-    pending_queue: Option<BTreeSet<PendingTransaction>>,
+    priority_queue: OptionalPriorityTransactions,
+    pending_queue: OptionalPendingTransactions,
     gas_price_threshold: Option<GasPrice>,
 }
 
