1c8e24286d6496d04044e4a527a2a2034c99230c
Pearson White <pearson.white@nethermind.io>
2024-10-23 23:40:58 +0300

    feat(blockifier): add emit_event cairo native syscall

--- crates/blockifier/src/execution/native/syscall_handler.rs
+++ crates/blockifier/src/execution/native/syscall_handler.rs
@@ -12,10 +12,12 @@ use cairo_native::starknet::{
 };
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use starknet_api::state::StorageKey;
+use starknet_api::transaction::{EventContent, EventData, EventKey};
 use starknet_types_core::felt::Felt;
 
 use crate::execution::call_info::{CallInfo, OrderedEvent, OrderedL2ToL1Message, Retdata};
 use crate::execution::entry_point::{CallEntryPoint, EntryPointExecutionContext};
+use crate::execution::syscalls::exceeds_event_size_limit;
 use crate::execution::syscalls::hint_processor::{SyscallExecutionError, OUT_OF_GAS_ERROR};
 use crate::state::state_api::State;
 
@@ -244,11 +246,29 @@ impl<'state> StarknetSyscallHandler for &mut NativeSyscallHandler<'state> {
 
     fn emit_event(
         &mut self,
-        _keys: &[Felt],
-        _data: &[Felt],
-        _remaining_gas: &mut u128,
+        keys: &[Felt],
+        data: &[Felt],
+        remaining_gas: &mut u128,
     ) -> SyscallResult<()> {
-        todo!("Implement emit_event syscall.");
+        self.pre_execute_syscall(remaining_gas, self.context.gas_costs().emit_event_gas_cost)?;
+
+        let order = self.context.n_emitted_events;
+        let event = EventContent {
+            keys: keys.iter().copied().map(EventKey).collect(),
+            data: EventData(data.to_vec()),
+        };
+
+        exceeds_event_size_limit(
+            self.context.versioned_constants(),
+            self.context.n_emitted_events + 1,
+            &event,
+        )
+        .map_err(|e| self.handle_error(remaining_gas, e.into()))?;
+
+        self.events.push(OrderedEvent { order, event });
+        self.context.n_emitted_events += 1;
+
+        Ok(())
     }
 
     fn send_message_to_l1(
