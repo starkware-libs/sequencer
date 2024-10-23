67216ac9616d475a5411a9be8031418d0e44e8a7
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-10-23 20:10:33 +0300

    feat: add support for `keccak` syscall (without syscall counter increase)

--- crates/blockifier/src/execution/syscalls/syscall_tests/keccak.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/keccak.rs
@@ -10,6 +10,7 @@ use crate::test_utils::contracts::FeatureContract;
 use crate::test_utils::initial_test_state::test_state;
 use crate::test_utils::{trivial_external_entry_point_new, CairoVersion, BALANCE};
 
+#[test_case(FeatureContract::TestContract(CairoVersion::Native), 265110; "Native")]
 #[test_case(FeatureContract::TestContract(CairoVersion::Cairo1), 255110; "VM")]
 fn test_keccak(test_contract: FeatureContract, expected_gas: u64) {
     let chain_info = &ChainInfo::create_for_testing();
@@ -22,7 +23,7 @@ fn test_keccak(test_contract: FeatureContract, expected_gas: u64) {
         ..trivial_external_entry_point_new(test_contract)
     };
 
-    assert_eq!(
+    pretty_assertions::assert_eq!(
         entry_point_call.execute_directly(&mut state).unwrap().execution,
         CallExecution { gas_consumed: expected_gas, ..CallExecution::from_retdata(retdata![]) }
     );
