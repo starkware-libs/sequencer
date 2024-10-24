7746e756a449e06d0090cfecdb57afea71d53663
Dori Medini <dori@starkware.co>
2024-10-24 13:17:05 +0300

    feat(ci): codestyle phases now run through py script on changes only
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TODO: Revert clippy::unwrap_used to -D after fixing all the unwraps.
-cargo clippy --workspace --all-targets --all-features -- \
+cargo clippy --all-targets --all-features $@ -- \
     -D future-incompatible \
     -D nonstandard-style \
     -D rust-2018-idioms \
