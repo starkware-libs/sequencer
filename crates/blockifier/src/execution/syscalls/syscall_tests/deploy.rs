6cfaa1303cb5618ccca20a8570b8a5a5c5fb39b3
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-10-24 13:38:54 +0300

    feat(blockifier): add `deploy` syscall implementation

--- crates/blockifier/src/execution/syscalls/syscall_tests/deploy.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/deploy.rs
@@ -15,6 +15,10 @@ use crate::test_utils::initial_test_state::test_state;
 use crate::test_utils::{calldata_for_deploy_test, trivial_external_entry_point_new, CairoVersion};
 
 #[test_case(FeatureContract::TestContract(CairoVersion::Cairo1), 205200;"VM")]
+#[cfg_attr(
+    feature = "cairo_native",
+    test_case(FeatureContract::TestContract(CairoVersion::Native), 216800;"Native")
+)]
 fn no_constructor(deployer_contract: FeatureContract, expected_gas: u64) {
     // TODO(Yoni): share the init code of the tests in this file.
 
@@ -63,6 +67,10 @@ fn no_constructor(deployer_contract: FeatureContract, expected_gas: u64) {
 }
 
 #[test_case(FeatureContract::TestContract(CairoVersion::Cairo1);"VM")]
+#[cfg_attr(
+    feature = "cairo_native",
+    test_case(FeatureContract::TestContract(CairoVersion::Native);"Native")
+)]
 fn no_constructor_nonempty_calldata(deployer_contract: FeatureContract) {
     let empty_contract = FeatureContract::Empty(CairoVersion::Cairo1);
     let class_hash = empty_contract.get_class_hash();
@@ -89,6 +97,10 @@ fn no_constructor_nonempty_calldata(deployer_contract: FeatureContract) {
 }
 
 #[test_case(FeatureContract::TestContract(CairoVersion::Cairo1),214550, 4610;"VM")]
+#[cfg_attr(
+    feature = "cairo_native",
+    test_case(FeatureContract::TestContract(CairoVersion::Native),236750, 15210;"Native")
+)]
 fn with_constructor(
     deployer_contract: FeatureContract,
     expected_gas: u64,
@@ -151,6 +163,10 @@ fn with_constructor(
 }
 
 #[test_case(FeatureContract::TestContract(CairoVersion::Cairo1);"VM")]
+#[cfg_attr(
+    feature = "cairo_native",
+    test_case(FeatureContract::TestContract(CairoVersion::Native);"Native")
+)]
 fn to_unavailable_address(deployer_contract: FeatureContract) {
     let empty_contract = FeatureContract::Empty(CairoVersion::Cairo1);
     let mut state = test_state(
