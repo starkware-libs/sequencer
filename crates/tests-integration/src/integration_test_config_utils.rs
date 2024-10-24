c0c00540113a0998ef0e282c96870f57fa14f156
Itay Tsabary <itayt@starkware.co>
2024-10-24 20:12:18 +0300

    chore(sequencer_node): drop redundant chain_id field from config
    commit-id:f9153e31

--- crates/tests-integration/src/integration_test_config_utils.rs
+++ crates/tests-integration/src/integration_test_config_utils.rs
@@ -1,9 +1,10 @@
 use std::fs::File;
 use std::io::Write;
 
+use anyhow::Result;
 use serde_json::{json, Value};
+use starknet_api::core::ChainId;
 use starknet_sequencer_node::config::SequencerNodeConfig;
-use tokio::io::Result;
 use tracing::info;
 
 // TODO(Tsabary): Move here all config-related functions from "integration_test_utils.rs".
@@ -42,15 +43,27 @@ macro_rules! config_fields_to_json {
 /// cargo run --bin starknet_sequencer_node -- --config_file NODE_CONFIG_CHANGES_FILE_PATH
 /// Transaction generator:
 /// cargo run --bin run_test_tx_generator -- --config_file TX_GEN_CONFIG_CHANGES_FILE_PATH
-pub fn dump_config_file_changes(config: SequencerNodeConfig) -> anyhow::Result<()> {
+pub fn dump_config_file_changes(config: SequencerNodeConfig, chain_id: ChainId) -> Result<()> {
     // Dump config changes file for the sequencer node.
     let json_data = config_fields_to_json!(
-        config.chain_id,
+        chain_id,
         config.rpc_state_reader_config.json_rpc_version,
         config.rpc_state_reader_config.url,
         config.batcher_config.storage.db_config.path_prefix,
         config.http_server_config.ip,
         config.http_server_config.port,
+        config
+            .batcher_config
+            .block_builder_config
+            .chain_info
+            .fee_token_addresses
+            .eth_fee_token_address,
+        config
+            .batcher_config
+            .block_builder_config
+            .chain_info
+            .fee_token_addresses
+            .strk_fee_token_address,
         config.gateway_config.chain_info.fee_token_addresses.eth_fee_token_address,
         config.gateway_config.chain_info.fee_token_addresses.strk_fee_token_address,
         config.consensus_manager_config.consensus_config.start_height,
@@ -58,8 +71,11 @@ pub fn dump_config_file_changes(config: SequencerNodeConfig) -> anyhow::Result<(
     dump_json_data(json_data, NODE_CONFIG_CHANGES_FILE_PATH)?;
 
     //  Dump config changes file for the transaction generator.
-    let json_data =
-        config_fields_to_json!(config.http_server_config.ip, config.http_server_config.port,);
+    let json_data = config_fields_to_json!(
+        config.http_server_config.ip,
+        config.http_server_config.port,
+        chain_id,
+    );
     dump_json_data(json_data, TX_GEN_CONFIG_CHANGES_FILE_PATH)?;
 
     Ok(())
