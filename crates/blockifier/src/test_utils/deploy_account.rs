35f5ff7f325195b0439f5794f340e7402b091d3d
Arnon Hod <arnon@starkware.co>
2024-10-27 09:23:23 +0200

    chore: align the tx_hash parameter in the account tx creator test util (#1073)

--- crates/blockifier/src/test_utils/deploy_account.rs
+++ crates/blockifier/src/test_utils/deploy_account.rs
@@ -1,7 +1,6 @@
 use starknet_api::core::calculate_contract_address;
 use starknet_api::test_utils::deploy_account::DeployAccountTxArgs;
 use starknet_api::test_utils::NonceManager;
-use starknet_api::transaction::TransactionHash;
 
 use crate::transaction::transactions::DeployAccountTransaction;
 
@@ -9,7 +8,7 @@ pub fn deploy_account_tx(
     deploy_tx_args: DeployAccountTxArgs,
     nonce_manager: &mut NonceManager,
 ) -> DeployAccountTransaction {
-    let default_tx_hash = TransactionHash::default();
+    let tx_hash = deploy_tx_args.tx_hash;
     let contract_address = calculate_contract_address(
         deploy_tx_args.contract_address_salt,
         deploy_tx_args.class_hash,
@@ -22,5 +21,5 @@ pub fn deploy_account_tx(
         deploy_tx_args,
         nonce_manager.next(contract_address),
     );
-    DeployAccountTransaction::new(deploy_account_tx, default_tx_hash, contract_address)
+    DeployAccountTransaction::new(deploy_account_tx, tx_hash, contract_address)
 }
