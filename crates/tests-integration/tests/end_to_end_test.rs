f40da2478f25f22f668dd732dae4f114f7132a1c
Arnon Hod <arnon@starkware.co>
2024-10-27 09:59:45 +0200

    test: add get proposal content loop to end to end test (#1407)

--- crates/tests-integration/tests/end_to_end_test.rs
+++ crates/tests-integration/tests/end_to_end_test.rs
@@ -3,7 +3,13 @@ use pretty_assertions::assert_eq;
 use rstest::{fixture, rstest};
 use starknet_api::block::BlockNumber;
 use starknet_api::transaction::TransactionHash;
-use starknet_batcher_types::batcher_types::{BuildProposalInput, ProposalId, StartHeightInput};
+use starknet_batcher_types::batcher_types::{
+    BuildProposalInput,
+    GetProposalContent,
+    GetProposalContentInput,
+    ProposalId,
+    StartHeightInput,
+};
 use starknet_batcher_types::communication::SharedBatcherClient;
 use starknet_integration_tests::integration_test_setup::IntegrationTestSetup;
 use starknet_integration_tests::integration_test_utils::{
@@ -23,26 +29,26 @@ async fn test_end_to_end(tx_generator: MultiAccountTransactionGenerator) {
     let mock_running_system = IntegrationTestSetup::new_from_tx_generator(&tx_generator).await;
 
     // Create and send transactions.
-    let expected_tx_hashes = run_integration_test_scenario(tx_generator, &|tx| {
+    let expected_batched_tx_hashes = run_integration_test_scenario(tx_generator, &|tx| {
         mock_running_system.assert_add_tx_success(tx)
     })
     .await;
 
     // Test.
-    let mempool_txs = mock_running_system.get_txs(4).await;
-
-    run_consensus_for_end_to_end_test(&mock_running_system.batcher_client).await;
-
-    // Assert.
-    let actual_tx_hashes: Vec<TransactionHash> =
-        mempool_txs.iter().map(|tx| tx.tx_hash()).collect();
-    assert_eq!(expected_tx_hashes, *actual_tx_hashes);
+    run_consensus_for_end_to_end_test(
+        &mock_running_system.batcher_client,
+        &expected_batched_tx_hashes,
+    )
+    .await;
 }
 
 /// This function should mirror
 /// [`run_consensus`](papyrus_consensus::manager::run_consensus). It makes requests
 /// from the batcher client and asserts the expected responses were received.
-pub async fn run_consensus_for_end_to_end_test(batcher_client: &SharedBatcherClient) {
+pub async fn run_consensus_for_end_to_end_test(
+    batcher_client: &SharedBatcherClient,
+    expected_batched_tx_hashes: &[TransactionHash],
+) {
     // Setup. Holds the state of the consensus manager.
 
     // Set start height.
@@ -67,4 +73,23 @@ pub async fn run_consensus_for_end_to_end_test(batcher_client: &SharedBatcherCli
         })
         .await
         .unwrap();
+
+    // Get proposal content.
+    let mut executed_tx_hashes: Vec<TransactionHash> = vec![];
+    let _proposal_commitment = loop {
+        let response = batcher_client
+            .get_proposal_content(GetProposalContentInput { proposal_id })
+            .await
+            .unwrap();
+        match response.content {
+            GetProposalContent::Txs(batched_txs) => {
+                executed_tx_hashes.append(&mut batched_txs.iter().map(|tx| tx.tx_hash()).collect());
+            }
+            GetProposalContent::Finished(proposal_commitment) => {
+                break proposal_commitment;
+            }
+        }
+    };
+
+    assert_eq!(expected_batched_tx_hashes, executed_tx_hashes);
 }
