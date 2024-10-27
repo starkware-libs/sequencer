cf1000a17330fd27adba40406a258248e8aaa13e
Gilad Chase <gilad@starkware.com>
2024-10-27 09:00:03 +0200

    chore(ci): delete unused lints
    - `unwrap_used` is already allowed by default and not denied explicitly
      in any crate.
    - the tracing bug in question has been fixed last month.

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -6,5 +6,3 @@ cargo clippy --workspace --all-targets --all-features -- \
     -D nonstandard-style \
     -D rust-2018-idioms \
     -D unused \
-    -A clippy::unwrap_used \
-    -A clippy::blocks_in_conditions  # This is because of a bug in tracing: https://github.com/tokio-rs/tracing/issues/2876
