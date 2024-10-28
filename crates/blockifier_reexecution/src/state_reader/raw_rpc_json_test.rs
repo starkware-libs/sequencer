df80f6ed4f9c7b7e86b4a3b89db4657056c05444
AvivYossef-starkware <aviv.y@starkware.co>
2024-10-28 14:10:11 +0200

    build: blockifier reexecution add support for declare tx

--- crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
+++ crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
@@ -4,7 +4,7 @@ use pretty_assertions::assert_eq;
 use rstest::{fixture, rstest};
 use starknet_api::block::BlockNumber;
 use starknet_api::test_utils::read_json_file;
-use starknet_api::transaction::{InvokeTransaction, Transaction};
+use starknet_api::transaction::{DeclareTransaction, DeployAccountTransaction, InvokeTransaction, Transaction};
 use starknet_core::types::ContractClass;
 use starknet_gateway::rpc_objects::BlockHeader;
 
@@ -64,4 +64,50 @@ fn deserialize_invoke_txs() {
     .expect("Failed to deserialize invoke v3 tx");
 
     assert_matches!(invoke_tx_v3, Transaction::Invoke(InvokeTransaction::V3(..)));
+}
+
+#[test]
+fn deserialize_deploy_account_txs() {
+    let deploy_account_tx_v1 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["deploy_account_v1"].clone(),
+    )
+    .expect("Failed to deserialize deploy account v1 tx");
+
+    assert_matches!(
+        deploy_account_tx_v1,
+        Transaction::DeployAccount(DeployAccountTransaction::V1(..))
+    );
+
+    let deploy_account_tx_v3 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["deploy_account_v3"].clone(),
+    )
+    .expect("Failed to deserialize deploy account v3 tx");
+
+    assert_matches!(
+        deploy_account_tx_v3,
+        Transaction::DeployAccount(DeployAccountTransaction::V3(..))
+    );
+}
+
+#[test]
+fn deserialize_declare_txs() {
+    let declare_tx_v1 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["declare_v1"].clone(),
+    )
+    .expect("Failed to deserialize declare v1 tx");
+
+    assert_matches!(declare_tx_v1, Transaction::Declare(DeclareTransaction::V1(..)));
+    let declare_tx_v2 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["declare_v2"].clone(),
+    )
+    .expect("Failed to deserialize declare v2 tx");
+
+    assert_matches!(declare_tx_v2, Transaction::Declare(DeclareTransaction::V2(..)));
+
+    let declare_tx_v3 = deserialize_transaction_json_to_starknet_api_tx(
+        read_json_file("raw_rpc_json_objects/transactions.json")["declare_v3"].clone(),
+    )
+    .expect("Failed to deserialize declare v3 tx");
+
+    assert_matches!(declare_tx_v3, Transaction::Declare(DeclareTransaction::V3(..)));
 }
--- crates/blockifier_reexecution/src/state_reader/rpc_https_test.rs
+++ crates/blockifier_reexecution/src/state_reader/rpc_https_test.rs
@@ -6,7 +6,7 @@ use starknet_api::block::{BlockNumber, StarknetVersion};
 use starknet_api::class_hash;
 use starknet_api::core::{ClassHash, ContractAddress};
 use starknet_api::test_utils::read_json_file;
-use starknet_api::transaction::Transaction;
+use starknet_api::transaction::{DeclareTransaction, Transaction};
 use starknet_core::types::ContractClass::{Legacy, Sierra};
 
 use crate::state_reader::compile::legacy_to_contract_class_v0;
@@ -20,9 +20,25 @@ const EXAMPLE_BLOCK_NUMBER: u64 = 700000;
 const EXAMPLE_CONTACT_CLASS_HASH: &str =
     "0x3131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e";
 
-const EXAMPLE_DEPLOY_ACCOUNT_BLOCK_NUMBER: u64 = 837408;
-const EXAMPLE_DEPLOY_ACCOUNT_TX_HASH: &str =
+const EXAMPLE_DEPLOY_ACCOUNT_V1_BLOCK_NUMBER: u64 = 837408;
+const EXAMPLE_DEPLOY_ACCOUNT_V1_TX_HASH: &str =
     "0x02a2e13cd94f911ea18c20a81e853314e37de58d49d13aa3a92370accd4338e8";
+
+const EXAMPLE_DEPLOY_ACCOUNT_V3_BLOCK_NUMBER: u64 = 837792;
+const EXAMPLE_DEPLOY_ACCOUNT_V3_TX_HASH: &str =
+    "0x04422b1300d2a55fb0138f8a97819d6dc04fe1d57e332b657ce8167e6572c958";
+
+const EXAMPLE_DECLARE_V1_BLOCK_NUMBER: u64 = 837461;
+const EXAMPLE_DECLARE_V1_TX_HASH: &str =
+    "0x04e9239ebc8512a508f21620cf570e9d938f31190770224d0f6d33ab93fefaf4";
+
+const EXAMPLE_DECLARE_V2_BLOCK_NUMBER: u64 = 822636;
+const EXAMPLE_DECLARE_V2_TX_HASH: &str =
+    "0x0409d159fbcab271ffc1693b08d9198f4bbff7e344e1624dadc2d9a67a960226";
+
+const EXAMPLE_DECLARE_V3_BLOCK_NUMBER: u64 = 825013;
+const EXAMPLE_DECLARE_V3_TX_HASH: &str =
+    "0x03ab43c0913f95b901b49ed1aa6009b31ebe0ad7ba62da49fc6de7f3854b496f";
 
 #[fixture]
 pub fn test_block_number() -> BlockNumber {
@@ -104,13 +120,47 @@ pub fn test_get_invoke_tx_by_hash(test_state_reader: TestStateReader) {
     assert_matches!(actual_tx, Transaction::Invoke(..));
 }
 
-#[rstest]
-pub fn test_get_deploy_account_tx_by_hash() {
+#[rstest(
+    block_number,
+    tx_hash,
+    version,
+    case(EXAMPLE_DEPLOY_ACCOUNT_V1_BLOCK_NUMBER, EXAMPLE_DEPLOY_ACCOUNT_V1_TX_HASH, 1),
+    case(EXAMPLE_DEPLOY_ACCOUNT_V3_BLOCK_NUMBER, EXAMPLE_DEPLOY_ACCOUNT_V3_TX_HASH, 3)
+)]
+pub fn test_get_deploy_account_tx_by_hash(block_number: u64, tx_hash: &str, version: u64) {
     // Create StateReader with block number that contain the deploy account tx.
-    let state_reader =
-        TestStateReader::new_for_testing(BlockNumber(EXAMPLE_DEPLOY_ACCOUNT_BLOCK_NUMBER));
-    let actual_tx = state_reader.get_tx_by_hash(EXAMPLE_DEPLOY_ACCOUNT_TX_HASH).unwrap();
-    assert_matches!(actual_tx, Transaction::DeployAccount(..));
+    use starknet_api::transaction::DeployAccountTransaction;
+    let state_reader = TestStateReader::new_for_testing(BlockNumber(block_number));
+    let actual_tx = state_reader.get_tx_by_hash(tx_hash).unwrap();
+    match version {
+        1 => {
+            assert_matches!(actual_tx, Transaction::DeployAccount(DeployAccountTransaction::V1(..)))
+        }
+        3 => {
+            assert_matches!(actual_tx, Transaction::DeployAccount(DeployAccountTransaction::V3(..)))
+        }
+        _ => panic!("Invalid version"),
+    }
+}
+
+#[rstest(
+    block_number,
+    tx_hash,
+    expected_version,
+    case(EXAMPLE_DECLARE_V1_BLOCK_NUMBER, EXAMPLE_DECLARE_V1_TX_HASH, 1),
+    case(EXAMPLE_DECLARE_V2_BLOCK_NUMBER, EXAMPLE_DECLARE_V2_TX_HASH, 2),
+    case(EXAMPLE_DECLARE_V3_BLOCK_NUMBER, EXAMPLE_DECLARE_V3_TX_HASH, 3)
+)]
+pub fn test_get_declare_tx_by_hash(block_number: u64, tx_hash: &str, expected_version: u64) {
+    // Create StateReader with block number that contain the declare tx.
+    let state_reader = TestStateReader::new_for_testing(BlockNumber(block_number));
+    let actual_tx = state_reader.get_tx_by_hash(tx_hash).unwrap();
+    match expected_version {
+        1 => assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V1(..))),
+        2 => assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V2(..))),
+        3 => assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V3(..))),
+        _ => panic!("Invalid expected version"),
+    }
 }
 
 #[rstest]
