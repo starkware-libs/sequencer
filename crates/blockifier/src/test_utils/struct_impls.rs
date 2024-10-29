586415b256b337d0b6b07736d9d43d83eb36e8bf
Rodrigo <rodrodpino@gmail.com>
2024-10-29 09:09:18 +0000

    chore: add Native as a compilation feature

--- crates/blockifier/src/test_utils/struct_impls.rs
+++ crates/blockifier/src/test_utils/struct_impls.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+#[cfg(feature = "cairo_native")]
 use cairo_native::executor::AotNativeExecutor;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use serde_json::Value;
@@ -16,12 +17,14 @@ use crate::bouncer::{BouncerConfig, BouncerWeights, BuiltinCount};
 use crate::context::{BlockContext, ChainInfo, FeeTokenAddresses, TransactionContext};
 use crate::execution::call_info::{CallExecution, CallInfo, Retdata};
 use crate::execution::common_hints::ExecutionMode;
-use crate::execution::contract_class::{ContractClassV0, ContractClassV1, NativeContractClassV1};
+use crate::execution::contract_class::{ContractClassV0, ContractClassV1};
 use crate::execution::entry_point::{
     CallEntryPoint,
     EntryPointExecutionContext,
     EntryPointExecutionResult,
 };
+#[cfg(feature = "cairo_native")]
+use crate::execution::native::contract_class::NativeContractClassV1;
 use crate::state::state_api::State;
 use crate::test_utils::{
     get_raw_contract_class,
@@ -250,6 +253,7 @@ impl BouncerWeights {
     }
 }
 
+#[cfg(feature = "cairo_native")]
 impl NativeContractClassV1 {
     /// Convenience function to construct a NativeContractClassV1 from a raw contract class.
     /// If control over the compilation is desired use [Self::new] instead.
@@ -275,7 +279,10 @@ impl NativeContractClassV1 {
         let sierra_program = sierra_contract_class.extract_sierra_program()?;
         let executor = compile_and_load(&sierra_program)?;
 
-        Ok(Self::new(executor, sierra_contract_class))
+        let casm_contract_class = cairo_lang_starknet_classes::casm_contract_class::CasmContractClass::from_contract_class(sierra_contract_class.clone(), false, usize::MAX)?;
+        let casm = ContractClassV1::try_from(casm_contract_class)?;
+
+        Ok(Self::new(executor, sierra_contract_class, casm))
     }
 
     pub fn from_file(contract_path: &str) -> Self {
