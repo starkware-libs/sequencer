494a7f0b0e1dd0de1906a721440cbc36b2023c7f
AvivYossef-starkware <aviv.y@starkware.co>
2024-10-29 10:35:48 +0200

    build(blockifier_reexecution): convert declare starknet api to declare blockifier

--- crates/blockifier_reexecution/src/state_reader/utils.rs
+++ crates/blockifier_reexecution/src/state_reader/utils.rs
@@ -1,12 +1,8 @@
 use blockifier::context::{ChainInfo, FeeTokenAddresses};
-use blockifier::transaction::transaction_execution::Transaction as BlockifierTransaction;
 use indexmap::IndexMap;
 use papyrus_execution::{eth_fee_contract_address, strk_fee_contract_address};
 use starknet_api::core::ChainId;
-use starknet_api::transaction::{Transaction, TransactionHash};
 use starknet_gateway::config::RpcStateReaderConfig;
-
-use crate::state_reader::test_state_reader::ReexecutionResult;
 
 pub const RPC_NODE_URL: &str = "https://free-rpc.nethermind.io/mainnet-juno/";
 pub const JSON_RPC_VERSION: &str = "2.0";
@@ -32,21 +28,6 @@ pub fn get_chain_info() -> ChainInfo {
     ChainInfo { chain_id: ChainId::Mainnet, fee_token_addresses: get_fee_token_addresses() }
 }
 
-// TODO(Aner): extend/refactor to accomodate all types of transactions.
-#[allow(dead_code)]
-pub(crate) fn from_api_txs_to_blockifier_txs(
-    txs_and_hashes: Vec<(Transaction, TransactionHash)>,
-) -> ReexecutionResult<Vec<BlockifierTransaction>> {
-    Ok(txs_and_hashes
-        .into_iter()
-        .map(|(tx, tx_hash)| match tx {
-            Transaction::Invoke(_) => {
-                BlockifierTransaction::from_api(tx, tx_hash, None, None, None, false)
-            }
-            _ => unimplemented!(),
-        })
-        .collect::<Result<_, _>>()?)
-}
 // TODO(Aner): import the following functions instead, to reduce code duplication.
 pub(crate) fn disjoint_hashmap_union<K: std::hash::Hash + std::cmp::Eq, V>(
     map1: IndexMap<K, V>,
