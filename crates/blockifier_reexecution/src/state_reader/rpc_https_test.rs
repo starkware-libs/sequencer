7f36712b9c0a1ce644e7bc41e5f7b87fa7bffe6c
AvivYossef-starkware <141143145+AvivYossef-starkware@users.noreply.github.com>
2024-10-30 10:31:32 +0200

    test(blockifier_reexecution): add support for declare and deploy account tx with rpc (#1604)

--- crates/blockifier_reexecution/src/state_reader/rpc_https_test.rs
+++ crates/blockifier_reexecution/src/state_reader/rpc_https_test.rs
@@ -6,7 +6,12 @@ use starknet_api::block::{BlockNumber, StarknetVersion};
 use starknet_api::class_hash;
 use starknet_api::core::ClassHash;
 use starknet_api::test_utils::read_json_file;
-use starknet_api::transaction::Transaction;
+use starknet_api::transaction::{
+    DeclareTransaction,
+    DeployAccountTransaction,
+    Transaction,
+    TransactionVersion,
+};
 use starknet_core::types::ContractClass::{Legacy, Sierra};
 
 use crate::state_reader::compile::legacy_to_contract_class_v0;
@@ -19,6 +24,26 @@ const EXAMPLE_BLOCK_NUMBER: u64 = 700000;
 
 const EXAMPLE_CONTACT_CLASS_HASH: &str =
     "0x3131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e";
+
+const EXAMPLE_DEPLOY_ACCOUNT_V1_BLOCK_NUMBER: u64 = 837408;
+const EXAMPLE_DEPLOY_ACCOUNT_V1_TX_HASH: &str =
+    "0x02a2e13cd94f911ea18c20a81e853314e37de58d49d13aa3a92370accd4338e8";
+
+const EXAMPLE_DEPLOY_ACCOUNT_V3_BLOCK_NUMBER: u64 = 837792;
+const EXAMPLE_DEPLOY_ACCOUNT_V3_TX_HASH: &str =
+    "0x04422b1300d2a55fb0138f8a97819d6dc04fe1d57e332b657ce8167e6572c958";
+
+const EXAMPLE_DECLARE_V1_BLOCK_NUMBER: u64 = 837461;
+const EXAMPLE_DECLARE_V1_TX_HASH: &str =
+    "0x04e9239ebc8512a508f21620cf570e9d938f31190770224d0f6d33ab93fefaf4";
+
+const EXAMPLE_DECLARE_V2_BLOCK_NUMBER: u64 = 822636;
+const EXAMPLE_DECLARE_V2_TX_HASH: &str =
+    "0x0409d159fbcab271ffc1693b08d9198f4bbff7e344e1624dadc2d9a67a960226";
+
+const EXAMPLE_DECLARE_V3_BLOCK_NUMBER: u64 = 825013;
+const EXAMPLE_DECLARE_V3_TX_HASH: &str =
+    "0x03ab43c0913f95b901b49ed1aa6009b31ebe0ad7ba62da49fc6de7f3854b496f";
 
 #[fixture]
 pub fn test_block_number() -> BlockNumber {
@@ -95,9 +120,60 @@ pub fn test_get_tx_hashes(test_state_reader: TestStateReader, test_block_number:
 }
 
 #[rstest]
-pub fn test_get_tx_by_hash(test_state_reader: TestStateReader) {
+pub fn test_get_invoke_tx_by_hash(test_state_reader: TestStateReader) {
     let actual_tx = test_state_reader.get_tx_by_hash(EXAMPLE_INVOKE_TX_HASH).unwrap();
     assert_matches!(actual_tx, Transaction::Invoke(..));
+}
+
+#[rstest]
+#[case(
+    EXAMPLE_DEPLOY_ACCOUNT_V1_BLOCK_NUMBER,
+    EXAMPLE_DEPLOY_ACCOUNT_V1_TX_HASH,
+    TransactionVersion::ONE
+)]
+#[case(
+    EXAMPLE_DEPLOY_ACCOUNT_V3_BLOCK_NUMBER,
+    EXAMPLE_DEPLOY_ACCOUNT_V3_TX_HASH,
+    TransactionVersion::THREE
+)]
+pub fn test_get_deploy_account_tx_by_hash(
+    #[case] block_number: u64,
+    #[case] tx_hash: &str,
+    #[case] version: TransactionVersion,
+) {
+    // Create StateReader with block number that contain the deploy account tx.
+    let state_reader = TestStateReader::new_for_testing(BlockNumber(block_number));
+    let actual_tx = state_reader.get_tx_by_hash(tx_hash).unwrap();
+    if version == TransactionVersion::ONE {
+        assert_matches!(actual_tx, Transaction::DeployAccount(DeployAccountTransaction::V1(..)))
+    } else if version == TransactionVersion::THREE {
+        assert_matches!(actual_tx, Transaction::DeployAccount(DeployAccountTransaction::V3(..)))
+    } else {
+        panic!("Invalid version")
+    }
+}
+
+#[rstest]
+#[case(EXAMPLE_DECLARE_V1_BLOCK_NUMBER, EXAMPLE_DECLARE_V1_TX_HASH, TransactionVersion::ONE)]
+#[case(EXAMPLE_DECLARE_V2_BLOCK_NUMBER, EXAMPLE_DECLARE_V2_TX_HASH, TransactionVersion::TWO)]
+#[case(EXAMPLE_DECLARE_V3_BLOCK_NUMBER, EXAMPLE_DECLARE_V3_TX_HASH, TransactionVersion::THREE)]
+pub fn test_get_declare_tx_by_hash(
+    #[case] block_number: u64,
+    #[case] tx_hash: &str,
+    #[case] expected_version: TransactionVersion,
+) {
+    // Create StateReader with block number that contain the declare tx.
+    let state_reader = TestStateReader::new_for_testing(BlockNumber(block_number));
+    let actual_tx = state_reader.get_tx_by_hash(tx_hash).unwrap();
+    if expected_version == TransactionVersion::ONE {
+        assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V1(..)))
+    } else if expected_version == TransactionVersion::TWO {
+        assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V2(..)))
+    } else if expected_version == TransactionVersion::THREE {
+        assert_matches!(actual_tx, Transaction::Declare(DeclareTransaction::V3(..)))
+    } else {
+        panic!("Invalid expected version")
+    }
 }
 
 #[rstest]
