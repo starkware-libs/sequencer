f8fd51cc7e42ac51e2ec349e93452bc49f0671cf
Itay Tsabary <itayt@starkware.co>
2024-10-31 08:49:31 +0200

    feat(sequencer_node): add required params test util
    commit-id:3b853d89

--- crates/mempool_node/src/config/mod.rs
+++ crates/mempool_node/src/config/mod.rs
@@ -4,7 +4,10 @@ mod config_test;
 pub mod component_config;
 pub mod component_execution_config;
 pub mod node_config;
+#[cfg(any(feature = "testing", test))]
+pub mod test_utils;
 
+// TODO(Tsabary): Remove these, and replace with direct imports.
 pub use component_config::*;
 pub use component_execution_config::*;
 pub use node_config::*;
