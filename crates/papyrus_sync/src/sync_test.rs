a9b7a266e24593dbedc7f1611e4a047e0c663cc7
Arnon Hod <arnon@starkware.co>
2024-11-04 15:27:14 +0200

    chore: simplify papyrus sync test with contract address creation (#1760)

--- crates/papyrus_sync/src/sync_test.rs
+++ crates/papyrus_sync/src/sync_test.rs
@@ -13,11 +13,11 @@ use papyrus_storage::{StorageReader, StorageWriter};
 use papyrus_test_utils::{get_rng, GetTestInstance};
 use pretty_assertions::assert_eq;
 use starknet_api::block::{BlockHash, BlockHeader, BlockHeaderWithoutHash, BlockNumber};
-use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::core::{ClassHash, CompiledClassHash, Nonce};
 use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContractClass;
 use starknet_api::hash::StarkHash;
 use starknet_api::state::{ContractClass, StateDiff};
-use starknet_api::{felt, patricia_key, storage_key};
+use starknet_api::{contract_address, felt, storage_key};
 use starknet_client::reader::objects::pending_data::{
     AcceptedOnL2ExtraData,
     DeprecatedPendingBlock,
@@ -48,12 +48,12 @@ use crate::{
 #[test]
 fn state_sorted() {
     let hash0 = felt!("0x0");
-    let patricia_key0 = patricia_key!("0x0");
+    let contract_address_0 = contract_address!("0x0");
     let hash1 = felt!("0x1");
-    let patricia_key1 = patricia_key!("0x1");
+    let contract_address_1 = contract_address!("0x1");
 
-    let dep_contract_0 = (ContractAddress(patricia_key0), ClassHash(hash0));
-    let dep_contract_1 = (ContractAddress(patricia_key1), ClassHash(hash1));
+    let dep_contract_0 = (contract_address_0, ClassHash(hash0));
+    let dep_contract_1 = (contract_address_1, ClassHash(hash1));
     let storage_key_0 = storage_key!("0x0");
     let storage_key_1 = storage_key!("0x1");
     let declare_class_0 =
@@ -62,10 +62,10 @@ fn state_sorted() {
         (ClassHash(hash1), (CompiledClassHash::default(), ContractClass::default()));
     let deprecated_declared_0 = (ClassHash(hash0), DeprecatedContractClass::default());
     let deprecated_declared_1 = (ClassHash(hash1), DeprecatedContractClass::default());
-    let nonce_0 = (ContractAddress(patricia_key0), Nonce(hash0));
-    let nonce_1 = (ContractAddress(patricia_key1), Nonce(hash1));
-    let replaced_class_0 = (ContractAddress(patricia_key0), ClassHash(hash0));
-    let replaced_class_1 = (ContractAddress(patricia_key1), ClassHash(hash1));
+    let nonce_0 = (contract_address_0, Nonce(hash0));
+    let nonce_1 = (contract_address_1, Nonce(hash1));
+    let replaced_class_0 = (contract_address_0, ClassHash(hash0));
+    let replaced_class_1 = (contract_address_1, ClassHash(hash1));
 
     let unsorted_deployed_contracts = IndexMap::from([dep_contract_1, dep_contract_0]);
     let unsorted_declared_classes =
@@ -75,8 +75,8 @@ fn state_sorted() {
     let unsorted_nonces = IndexMap::from([nonce_1, nonce_0]);
     let unsorted_storage_entries = IndexMap::from([(storage_key_1, hash1), (storage_key_0, hash0)]);
     let unsorted_storage_diffs = IndexMap::from([
-        (ContractAddress(patricia_key1), unsorted_storage_entries.clone()),
-        (ContractAddress(patricia_key0), unsorted_storage_entries),
+        (contract_address_1, unsorted_storage_entries.clone()),
+        (contract_address_0, unsorted_storage_entries),
     ]);
     let unsorted_replaced_classes = IndexMap::from([replaced_class_1, replaced_class_0]);
 
@@ -95,8 +95,8 @@ fn state_sorted() {
     let sorted_nonces = IndexMap::from([nonce_0, nonce_1]);
     let sorted_storage_entries = IndexMap::from([(storage_key_0, hash0), (storage_key_1, hash1)]);
     let sorted_storage_diffs = IndexMap::from([
-        (ContractAddress(patricia_key0), sorted_storage_entries.clone()),
-        (ContractAddress(patricia_key1), sorted_storage_entries.clone()),
+        (contract_address_0, sorted_storage_entries.clone()),
+        (contract_address_1, sorted_storage_entries.clone()),
     ]);
     let sorted_replaced_classes = IndexMap::from([replaced_class_0, replaced_class_1]);
 
