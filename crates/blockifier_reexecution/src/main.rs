c959287361b5759a175a0ec2f9e16adc50171fe5
Aner Ben Efraim <aner@starkware.co>
2024-11-05 18:09:05 +0200

    chore(blockifier_reexecution): move reexecution to seperate function

--- crates/blockifier_reexecution/src/main.rs
+++ crates/blockifier_reexecution/src/main.rs
@@ -1,17 +1,15 @@
-use blockifier::state::cached_state::CachedState;
-use blockifier::state::state_api::StateReader;
-use blockifier_reexecution::assert_eq_state_diff;
 use blockifier_reexecution::state_reader::test_state_reader::{
-    ConsecutiveStateReaders,
     ConsecutiveTestStateReaders,
     OfflineConsecutiveStateReaders,
     SerializableDataPrevBlock,
     SerializableOfflineReexecutionData,
 };
-use blockifier_reexecution::state_reader::utils::JSON_RPC_VERSION;
+use blockifier_reexecution::state_reader::utils::{
+    reexecute_and_verify_correctness,
+    JSON_RPC_VERSION,
+};
 use clap::{Args, Parser, Subcommand};
 use starknet_api::block::BlockNumber;
-use starknet_api::core::ContractAddress;
 use starknet_gateway::config::RpcStateReaderConfig;
 
 /// BlockifierReexecution CLI.
@@ -66,29 +64,6 @@ enum Command {
 #[derive(Debug, Args)]
 struct GlobalOptions {}
 
-pub fn reexecution_test<S: StateReader + Send + Sync, T: ConsecutiveStateReaders<S>>(
-    consecutive_state_readers: T,
-) -> Option<CachedState<S>> {
-    let mut expected_state_diff = consecutive_state_readers.get_next_block_state_diff().unwrap();
-
-    let all_txs_in_next_block = consecutive_state_readers.get_next_block_txs().unwrap();
-
-    let mut transaction_executor =
-        consecutive_state_readers.get_transaction_executor(None).unwrap();
-
-    transaction_executor.execute_txs(&all_txs_in_next_block);
-    // Finalize block and read actual statediff.
-    let (actual_state_diff, _, _) =
-        transaction_executor.finalize().expect("Couldn't finalize block");
-
-    // TODO(Aner): compute correct block hash at storage slot 0x1 instead of removing it.
-    expected_state_diff.storage_updates.shift_remove(&ContractAddress(1_u128.into()));
-
-    assert_eq_state_diff!(expected_state_diff, actual_state_diff);
-
-    transaction_executor.block_state
-}
-
 /// Main entry point of the blockifier reexecution CLI.
 fn main() {
     let args = BlockifierReexecutionCliArgs::parse();
@@ -102,7 +77,7 @@ fn main() {
                 json_rpc_version: JSON_RPC_VERSION.to_string(),
             };
 
-            reexecution_test(ConsecutiveTestStateReaders::new(
+            reexecute_and_verify_correctness(ConsecutiveTestStateReaders::new(
                 BlockNumber(block_number - 1),
                 Some(config),
                 false,
@@ -133,7 +108,7 @@ fn main() {
                 consecutive_state_readers.get_serializable_data_next_block().unwrap();
 
             // Run the reexecution test and get the state maps and contract class mapping.
-            let block_state = reexecution_test(consecutive_state_readers).unwrap();
+            let block_state = reexecute_and_verify_correctness(consecutive_state_readers).unwrap();
             let serializable_data_prev_block = SerializableDataPrevBlock {
                 state_maps: block_state.get_initial_reads().unwrap().into(),
                 contract_class_mapping: block_state
@@ -162,7 +137,7 @@ fn main() {
                 "./crates/blockifier_reexecution/resources/block_{block_number}"
             )) + "/reexecution_data.json";
 
-            reexecution_test(
+            reexecute_and_verify_correctness(
                 OfflineConsecutiveStateReaders::new_from_file(&full_file_path).unwrap(),
             );
 
