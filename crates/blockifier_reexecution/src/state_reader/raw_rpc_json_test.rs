eb9fc34389a1ce5e6153d58fa3bb7e8225e4d671
Aner Ben Efraim <aner@starkware.co>
2024-11-06 14:14:37 +0200

    chore(blockifier_reexecution): add blockifier reexecution test to ci

--- crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
+++ crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
@@ -164,7 +164,6 @@ fn serialize_state_maps() {
 }
 
 #[rstest]
-// TODO(Aner): Add block for each starknet version and for declare, deploy, replace_class, etc.
 #[case::v_0_13_0(600001)]
 #[case::v_0_13_1(620978)]
 #[case::v_0_13_1_1(649367)]
