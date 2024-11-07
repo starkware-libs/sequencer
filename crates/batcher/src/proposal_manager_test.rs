ce7890c48acdfce35a12241f27bce7a0a47e9400
Yair Bakalchuk <yair@starkware.co>
2024-11-07 16:11:04 +0200

    refactor(batcher): create the tx provider in the batcher

--- crates/batcher/src/proposal_manager_test.rs
+++ crates/batcher/src/proposal_manager_test.rs
@@ -23,7 +23,7 @@ use crate::proposal_manager::{
     ProposalOutput,
     StartHeightError,
 };
-use crate::transaction_provider::MockL1ProviderClient;
+use crate::transaction_provider::{MockL1ProviderClient, ProposeTransactionProvider};
 
 const INITIAL_HEIGHT: BlockNumber = BlockNumber(3);
 const BLOCK_GENERATION_TIMEOUT: tokio::time::Duration = tokio::time::Duration::from_secs(1);
@@ -39,8 +39,8 @@ fn output_streaming() -> (
 
 struct MockDependencies {
     block_builder_factory: MockBlockBuilderFactoryTrait,
-    l1_provider_client: MockL1ProviderClient,
-    mempool_client: MockMempoolClient,
+    l1_provider_client: Arc<MockL1ProviderClient>,
+    mempool_client: Arc<MockMempoolClient>,
     storage_reader: MockBatcherStorageReaderTrait,
 }
 
@@ -50,14 +50,14 @@ impl MockDependencies {
             let mut mock_block_builder = MockBlockBuilderTrait::new();
             mock_block_builder
                 .expect_build_block()
-                .return_once(move |_, _, _| Ok(BlockExecutionArtifacts::create_for_testing()));
+                .return_once(move |_, _| Ok(BlockExecutionArtifacts::create_for_testing()));
             Ok(Box::new(mock_block_builder))
         };
 
         self.block_builder_factory
             .expect_create_block_builder()
             .times(times)
-            .returning(move |_, _| simulate_build_block());
+            .returning(move |_, _, _| simulate_build_block());
     }
 
     // This function simulates a long build block operation. This is required for a test that
@@ -65,7 +65,7 @@ impl MockDependencies {
     fn expect_long_build_block(&mut self, times: usize) {
         let simulate_long_build_block = || -> BlockBuilderResult<Box<dyn BlockBuilderTrait>> {
             let mut mock_block_builder = MockBlockBuilderTrait::new();
-            mock_block_builder.expect_build_block().return_once(move |_, _, _| {
+            mock_block_builder.expect_build_block().return_once(move |_, _| {
                 std::thread::sleep(BLOCK_GENERATION_TIMEOUT * 10);
                 Ok(BlockExecutionArtifacts::create_for_testing())
             });
@@ -75,7 +75,7 @@ impl MockDependencies {
         self.block_builder_factory
             .expect_create_block_builder()
             .times(times)
-            .returning(move |_, _| simulate_long_build_block());
+            .returning(move |_, _, _| simulate_long_build_block());
     }
 }
 
@@ -84,17 +84,22 @@ fn mock_dependencies() -> MockDependencies {
     let mut storage_reader = MockBatcherStorageReaderTrait::new();
     storage_reader.expect_height().returning(|| Ok(INITIAL_HEIGHT));
     MockDependencies {
-        l1_provider_client: MockL1ProviderClient::new(),
+        l1_provider_client: Arc::new(MockL1ProviderClient::new()),
         block_builder_factory: MockBlockBuilderFactoryTrait::new(),
-        mempool_client: MockMempoolClient::new(),
+        mempool_client: Arc::new(MockMempoolClient::new()),
         storage_reader,
+    }
+}
+
+fn propose_tx_provider(mock_dependencies: &MockDependencies) -> ProposeTransactionProvider {
+    ProposeTransactionProvider {
+        mempool_client: mock_dependencies.mempool_client.clone(),
+        l1_provider_client: mock_dependencies.l1_provider_client.clone(),
     }
 }
 
 fn init_proposal_manager(mock_dependencies: MockDependencies) -> ProposalManager {
     ProposalManager::new(
-        Arc::new(mock_dependencies.l1_provider_client),
-        Arc::new(mock_dependencies.mempool_client),
         Arc::new(mock_dependencies.block_builder_factory),
         Arc::new(mock_dependencies.storage_reader),
     )
@@ -106,11 +111,12 @@ fn proposal_deadline() -> tokio::time::Instant {
 
 async fn build_and_await_block_proposal(
     proposal_manager: &mut ProposalManager,
+    tx_provider: ProposeTransactionProvider,
     proposal_id: ProposalId,
 ) {
     let (output_sender, _receiver) = output_streaming();
     proposal_manager
-        .build_block_proposal(proposal_id, None, proposal_deadline(), output_sender)
+        .build_block_proposal(proposal_id, None, proposal_deadline(), output_sender, tx_provider)
         .await
         .unwrap();
 
@@ -157,9 +163,16 @@ async fn proposal_generation_fails_without_start_height(
         tokio::sync::mpsc::UnboundedReceiver<Transaction>,
     ),
 ) {
+    let tx_provider = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
     let err = proposal_manager
-        .build_block_proposal(ProposalId(0), None, proposal_deadline(), output_streaming.0)
+        .build_block_proposal(
+            ProposalId(0),
+            None,
+            proposal_deadline(),
+            output_streaming.0,
+            tx_provider,
+        )
         .await;
     assert_matches!(err, Err(BuildProposalError::NoActiveHeight));
 }
@@ -169,10 +182,11 @@ async fn proposal_generation_fails_without_start_height(
 async fn proposal_generation_success(mut mock_dependencies: MockDependencies) {
     mock_dependencies.expect_build_block(1);
 
+    let tx_provider = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
 
     proposal_manager.start_height(INITIAL_HEIGHT).await.unwrap();
-    build_and_await_block_proposal(&mut proposal_manager, ProposalId(0)).await;
+    build_and_await_block_proposal(&mut proposal_manager, tx_provider, ProposalId(0)).await;
 }
 
 #[rstest]
@@ -180,14 +194,16 @@ async fn proposal_generation_success(mut mock_dependencies: MockDependencies) {
 async fn consecutive_proposal_generations_success(mut mock_dependencies: MockDependencies) {
     mock_dependencies.expect_build_block(2);
 
+    let tx_provider_0 = propose_tx_provider(&mock_dependencies);
+    let tx_provider_1 = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
 
     proposal_manager.start_height(INITIAL_HEIGHT).await.unwrap();
 
     // Generate two consecutive proposals (awaiting on them to make sure they finished
     // successfully).
-    build_and_await_block_proposal(&mut proposal_manager, ProposalId(0)).await;
-    build_and_await_block_proposal(&mut proposal_manager, ProposalId(1)).await;
+    build_and_await_block_proposal(&mut proposal_manager, tx_provider_0, ProposalId(0)).await;
+    build_and_await_block_proposal(&mut proposal_manager, tx_provider_1, ProposalId(1)).await;
 }
 
 // This test checks that trying to generate a proposal while another one is being generated will
@@ -199,6 +215,8 @@ async fn multiple_proposals_generation_fail(mut mock_dependencies: MockDependenc
     // Generate a block builder with a very long build block operation.
     mock_dependencies.expect_long_build_block(1);
 
+    let tx_provider_0 = propose_tx_provider(&mock_dependencies);
+    let tx_provider_1 = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
 
     proposal_manager.start_height(INITIAL_HEIGHT).await.unwrap();
@@ -206,14 +224,26 @@ async fn multiple_proposals_generation_fail(mut mock_dependencies: MockDependenc
     // Build a proposal that will take a very long time to finish.
     let (output_sender_0, _rec_0) = output_streaming();
     proposal_manager
-        .build_block_proposal(ProposalId(0), None, proposal_deadline(), output_sender_0)
+        .build_block_proposal(
+            ProposalId(0),
+            None,
+            proposal_deadline(),
+            output_sender_0,
+            tx_provider_0,
+        )
         .await
         .unwrap();
 
     // Try to generate another proposal while the first one is still being generated.
     let (output_sender_1, _rec_1) = output_streaming();
     let another_generate_request = proposal_manager
-        .build_block_proposal(ProposalId(1), None, proposal_deadline(), output_sender_1)
+        .build_block_proposal(
+            ProposalId(1),
+            None,
+            proposal_deadline(),
+            output_sender_1,
+            tx_provider_1,
+        )
         .await;
     assert_matches!(
         another_generate_request,
@@ -229,11 +259,12 @@ async fn multiple_proposals_generation_fail(mut mock_dependencies: MockDependenc
 async fn test_take_proposal_result_no_active_proposal(mut mock_dependencies: MockDependencies) {
     mock_dependencies.expect_build_block(1);
 
+    let tx_provider = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
 
     proposal_manager.start_height(INITIAL_HEIGHT).await.unwrap();
 
-    build_and_await_block_proposal(&mut proposal_manager, ProposalId(0)).await;
+    build_and_await_block_proposal(&mut proposal_manager, tx_provider, ProposalId(0)).await;
 
     let expected_proposal_output =
         ProposalOutput::from(BlockExecutionArtifacts::create_for_testing());
@@ -255,14 +286,22 @@ async fn test_abort_and_restart_height(mut mock_dependencies: MockDependencies) 
 
     // Start a new height and create a proposal.
     let (output_tx_sender, _receiver) = output_streaming();
+    let tx_provider_0 = propose_tx_provider(&mock_dependencies);
+    let tx_provider_1 = propose_tx_provider(&mock_dependencies);
     let mut proposal_manager = init_proposal_manager(mock_dependencies);
     proposal_manager.start_height(INITIAL_HEIGHT).await.unwrap();
-    build_and_await_block_proposal(&mut proposal_manager, ProposalId(0)).await;
+    build_and_await_block_proposal(&mut proposal_manager, tx_provider_0, ProposalId(0)).await;
 
     // Start a new proposal, which will remain active.
     assert!(
         proposal_manager
-            .build_block_proposal(ProposalId(1), None, proposal_deadline(), output_tx_sender)
+            .build_block_proposal(
+                ProposalId(1),
+                None,
+                proposal_deadline(),
+                output_tx_sender,
+                tx_provider_1
+            )
             .await
             .is_ok()
     );
