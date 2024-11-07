1253519780ee2759943996f3e007f55e9f9314b4
Avi Cohen <avi.cohen@starkware.co>
2024-11-07 22:26:24 +0200

    wip

--- crates/bin/cairo-native-compile/src/main.rs
+++ crates/bin/cairo-native-compile/src/main.rs
@@ -1,4 +1,6 @@
-use cairo_native::executor::contract::AotContractExecutor;
+use cairo_lang_sierra::ProgramParser;
+use cairo_native::executor::AotContractExecutor;
+use cairo_native::OptLevel;
 use std::path::PathBuf;
 use std::process;
 
@@ -13,9 +15,25 @@ struct Args {
     output: Option<String>,
 }
 
+const DEFAULT_SIERRA_FILE: &str = "sierra_file.sierra";
+const DEFAULT_OUTPUT_FILE: &str = "output_file";
+
 fn main() {
     let args = Args::parse();
     println!("Path : {:?}", args.path);
     println!("Child PID: {}", process::id());
-    // Your task code here
+    let path = PathBuf::from(DEFAULT_SIERRA_FILE);
+
+    let sierra_str = std::fs::read_to_string(&path).unwrap_or_else(|err| {
+        eprintln!("Error reading Sierra file: {}", err);
+        process::exit(1);
+    });
+    let sierra_program = ProgramParser::new().parse(&sierra_str).unwrap_or_else(|err| {
+        eprintln!("Error parsing Sierra file: {}", err);
+        process::exit(1);
+    });
+
+    let _contract_executor = AotContractExecutor::new(&sierra_program, OptLevel::default())
+        .unwrap()
+        .save(DEFAULT_OUTPUT_FILE);
 }
