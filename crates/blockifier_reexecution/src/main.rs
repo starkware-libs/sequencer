9569d8d4e55c236f143bcecaaa8550956d70dd16
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2024-11-07 18:09:45 +0200

    chore(blockifier_reexecution): serialize reexecution data to file (#1762)
    * chore(blockifier_reexecution): serialize reexecution data to file
    
    * chore(blockifier_reexecution): implement get_old_block_hash_and_number for offline state reader
    
    * chore(blockifier_reexecution): offline reexecution from file (#1764)
    
    * chore(blockifier_reexecution): offline reexecution from file
    
    * chore(blockifier_reexecution): use pre_process_and_create
    
    * chore(blockifier_reexecution): refactor cli to reduce code duplication (#1814)
    
    * chore(blockifier_reexecution): refactor cli to reduce code duplication
    
    * chore(blockifier_reexecution): explicitly state command args
    
    * chore(blockifier_reexecution): separate old block hash
    
    * chore(blockifier_reexecution): move reexecution to seperate function
    
    * test(blockifier_reexecution): reexecution test on blocks

--- crates/blockifier_reexecution/src/main.rs
+++ crates/blockifier_reexecution/src/main.rs
@@ -1,9 +1,13 @@
-use blockifier_reexecution::assert_eq_state_diff;
 use blockifier_reexecution::state_reader::test_state_reader::{
-    ConsecutiveStateReaders,
     ConsecutiveTestStateReaders,
+    OfflineConsecutiveStateReaders,
+    SerializableDataPrevBlock,
+    SerializableOfflineReexecutionData,
 };
-use blockifier_reexecution::state_reader::utils::JSON_RPC_VERSION;
+use blockifier_reexecution::state_reader::utils::{
+    reexecute_and_verify_correctness,
+    JSON_RPC_VERSION,
+};
 use clap::{Args, Parser, Subcommand};
 use starknet_api::block::BlockNumber;
 use starknet_gateway::config::RpcStateReaderConfig;
@@ -19,35 +23,47 @@ pub struct BlockifierReexecutionCliArgs {
     command: Command,
 }
 
-#[derive(Args, Debug)]
-struct SharedArgs {
-    /// Node url.
-    /// Default: https://free-rpc.nethermind.io/mainnet-juno/. Won't work for big tests.
-    #[clap(long, short = 'n', default_value = "https://free-rpc.nethermind.io/mainnet-juno/")]
-    node_url: String,
-
-    /// Block number.
-    #[clap(long, short = 'b')]
-    block_number: u64,
-}
-
 #[derive(Debug, Subcommand)]
 enum Command {
     /// Runs the RPC test.
     RpcTest {
-        #[clap(flatten)]
-        url_and_block_number: SharedArgs,
+        /// Node url.
+        #[clap(long, short = 'n')]
+        node_url: String,
+
+        /// Block number.
+        #[clap(long, short = 'b')]
+        block_number: u64,
     },
 
     /// Writes the RPC queries to json files.
     WriteRpcRepliesToJson {
-        #[clap(flatten)]
-        url_and_block_number: SharedArgs,
+        /// Node url.
+        #[clap(long, short = 'n')]
+        node_url: String,
 
-        /// Directory path to json files.
-        /// Default: "./crates/blockifier_reexecution/resources/block_{block_number}".
-        #[clap(long, default_value = None)]
-        directory_path: Option<String>,
+        /// Block number.
+        #[clap(long, short = 'b')]
+        block_number: u64,
+
+        // Directory path to json files. Default:
+        // "./crates/blockifier_reexecution/resources/block_{block_number}".
+        // TODO(Aner): add possibility to retrieve files from gc bucket.
+        #[clap(long, short = 'd', default_value = None)]
+        full_file_path: Option<String>,
+    },
+
+    // Reexecutes the block from JSON files.
+    ReexecuteBlock {
+        /// Block number.
+        #[clap(long, short = 'b')]
+        block_number: u64,
+
+        // Directory path to json files. Default:
+        // "./crates/blockifier_reexecution/resources/block_{block_number}".
+        // TODO(Aner): add possibility to retrieve files from gc bucket.
+        #[clap(long, short = 'd', default_value = None)]
+        full_file_path: Option<String>,
     },
 }
 
@@ -59,7 +75,7 @@ fn main() {
     let args = BlockifierReexecutionCliArgs::parse();
 
     match args.command {
-        Command::RpcTest { url_and_block_number: SharedArgs { node_url, block_number } } => {
+        Command::RpcTest { node_url, block_number } => {
             println!("Running RPC test for block number {block_number} using node url {node_url}.",);
 
             let config = RpcStateReaderConfig {
@@ -67,31 +83,72 @@ fn main() {
                 json_rpc_version: JSON_RPC_VERSION.to_string(),
             };
 
-            let test_state_readers_last_and_current_block = ConsecutiveTestStateReaders::new(
+            reexecute_and_verify_correctness(ConsecutiveTestStateReaders::new(
                 BlockNumber(block_number - 1),
                 Some(config),
                 false,
-            );
-
-            let all_txs_in_next_block =
-                test_state_readers_last_and_current_block.get_next_block_txs().unwrap();
-
-            let expected_state_diff =
-                test_state_readers_last_and_current_block.get_next_block_state_diff().unwrap();
-
-            let mut transaction_executor =
-                test_state_readers_last_and_current_block.get_transaction_executor(None).unwrap();
-
-            transaction_executor.execute_txs(&all_txs_in_next_block);
-            // Finalize block and read actual statediff.
-            let (actual_state_diff, _, _) =
-                transaction_executor.finalize().expect("Couldn't finalize block");
+            ));
 
             // Compare the expected and actual state differences
             // by avoiding discrepancies caused by insertion order
-            assert_eq_state_diff!(expected_state_diff, actual_state_diff);
             println!("RPC test passed successfully.");
         }
-        Command::WriteRpcRepliesToJson { .. } => todo!(),
+
+        Command::WriteRpcRepliesToJson { node_url, block_number, full_file_path } => {
+            let full_file_path = full_file_path.unwrap_or(format!(
+                "./crates/blockifier_reexecution/resources/block_{block_number}/reexecution_data.\
+                 json"
+            ));
+
+            // TODO(Aner): refactor to reduce code duplication.
+            let config = RpcStateReaderConfig {
+                url: node_url,
+                json_rpc_version: JSON_RPC_VERSION.to_string(),
+            };
+
+            let consecutive_state_readers =
+                ConsecutiveTestStateReaders::new(BlockNumber(block_number - 1), Some(config), true);
+
+            let serializable_data_next_block =
+                consecutive_state_readers.get_serializable_data_next_block().unwrap();
+
+            let old_block_hash = consecutive_state_readers.get_old_block_hash().unwrap();
+
+            // Run the reexecution test and get the state maps and contract class mapping.
+            let block_state = reexecute_and_verify_correctness(consecutive_state_readers).unwrap();
+            let serializable_data_prev_block = SerializableDataPrevBlock {
+                state_maps: block_state.get_initial_reads().unwrap().into(),
+                contract_class_mapping: block_state
+                    .state
+                    .get_contract_class_mapping_dumper()
+                    .unwrap(),
+            };
+
+            // Write the reexecution data to a json file.
+            SerializableOfflineReexecutionData {
+                serializable_data_prev_block,
+                serializable_data_next_block,
+                old_block_hash,
+            }
+            .write_to_file(&full_file_path)
+            .unwrap();
+
+            println!(
+                "RPC replies required for reexecuting block {block_number} written to json file."
+            );
+        }
+
+        Command::ReexecuteBlock { block_number, full_file_path } => {
+            let full_file_path = full_file_path.unwrap_or(format!(
+                "./crates/blockifier_reexecution/resources/block_{block_number}/reexecution_data.\
+                 json"
+            ));
+
+            reexecute_and_verify_correctness(
+                OfflineConsecutiveStateReaders::new_from_file(&full_file_path).unwrap(),
+            );
+
+            println!("Reexecution test for block {block_number} passed successfully.");
+        }
     }
 }
