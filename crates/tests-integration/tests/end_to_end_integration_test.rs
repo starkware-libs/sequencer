fd128a6b65226fd01c3011a27b0d92e6e25cbf20
Itay Tsabary <itayt@starkware.co>
2024-11-08 11:51:23 +0200

    test(tests-integration): run node in integration test
    commit-id:61787bd4

--- crates/tests-integration/tests/end_to_end_integration_test.rs
+++ crates/tests-integration/tests/end_to_end_integration_test.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Stdio;
+
 use mempool_test_utils::starknet_api_test_utils::MultiAccountTransactionGenerator;
 use rstest::{fixture, rstest};
 use starknet_integration_tests::integration_test_config_utils::dump_config_file_changes;
@@ -8,11 +12,47 @@ use starknet_integration_tests::integration_test_utils::{
 use starknet_integration_tests::state_reader::{spawn_test_rpc_state_reader, StorageTestSetup};
 use starknet_sequencer_infra::trace_util::configure_tracing;
 use tempfile::tempdir;
+use tokio::process::{Child, Command};
+use tokio::task::{self, JoinHandle};
 use tracing::info;
 
 #[fixture]
 fn tx_generator() -> MultiAccountTransactionGenerator {
     create_integration_test_tx_generator()
+}
+
+// TODO(Tsabary): Move to a suitable util location.
+async fn spawn_node_child_task(node_config_path: PathBuf) -> Child {
+    // Get the current working directory for the project
+    let project_path = env::current_dir().expect("Failed to get current directory").join("../..");
+
+    // TODO(Tsabary): Change invocation from "cargo run" to separate compilation and invocation
+    // (build, and then invoke the binary).
+    Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("starknet_sequencer_node")
+        .arg("--quiet")
+        .current_dir(&project_path)
+        .arg("--")
+        .arg("--config_file")
+        .arg(node_config_path.to_str().unwrap())
+        .stderr(Stdio::inherit())
+        .stdout(Stdio::null())
+        .kill_on_drop(true) // Required for stopping the node when the handle is dropped.
+        .spawn()
+        .expect("Failed to spawn the sequencer node.")
+}
+
+async fn spawn_run_node(node_config_path: PathBuf) -> JoinHandle<()> {
+    task::spawn(async move {
+        info!("Running the node from its spawned task.");
+        let _node_run_result = spawn_node_child_task(node_config_path).
+            await. // awaits the completion of spawn_node_child_task.
+            wait(). // runs the node until completion -- should be running indefinitely.
+            await; // awaits the completion of the node.
+        panic!("Node stopped unexpectedly.");
+    })
 }
 
 #[rstest]
@@ -37,6 +77,8 @@ async fn test_end_to_end_integration(tx_generator: MultiAccountTransactionGenera
     // this scope, such that the handle is not dropped and the storage is maintained.
     let temp_dir = tempdir().unwrap();
     // TODO(Tsabary): pass path instead of temp dir.
+    let (node_config_path, _) = dump_config_file_changes(&config, required_params, &temp_dir);
 
-    let (_node_config_path, _) = dump_config_file_changes(&config, required_params, &temp_dir);
+    info!("Running sequencer node.");
+    let _node_run_handle = spawn_run_node(node_config_path).await;
 }
