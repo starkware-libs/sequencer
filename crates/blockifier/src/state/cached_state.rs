447c9dece30137b36bcb6f76deb6341159d09e40
Yoav Gross <yoav@starkware.co>
2024-11-10 15:38:08 +0200

    feat(blockifier): implement the allocated keys logic

--- crates/blockifier/src/state/cached_state.rs
+++ crates/blockifier/src/state/cached_state.rs
@@ -686,7 +686,12 @@ pub struct AllocatedKeys(HashSet<StorageEntry>);
 impl AllocatedKeys {
     pub fn extend(&mut self, state_change: &StateChanges) {
         self.0.extend(&state_change.allocated_keys.0);
-        // TODO: Remove keys that are set back to zero.
+        // Remove keys that are set back to zero.
+        state_change.state_maps.storage.iter().for_each(|(k, v)| {
+            if v == &Felt::ZERO {
+                self.0.remove(k);
+            }
+        });
     }
 
     pub fn count(&self) -> usize {
@@ -695,12 +700,22 @@ impl AllocatedKeys {
 
     /// Collect entries that turn zero -> nonzero.
     pub fn from_storage_diff(
-        _updated_storage: &HashMap<StorageEntry, Felt>,
-        _base_storage: &HashMap<StorageEntry, Felt>,
+        updated_storage: &HashMap<StorageEntry, Felt>,
+        base_storage: &HashMap<StorageEntry, Felt>,
     ) -> Self {
         Self(
-            HashSet::new()
-            // TODO: Calculate the difference between the updated_storage and the base_storage.
+            updated_storage
+                .iter()
+                .filter_map(|(k, v)| {
+                    let base_value = base_storage.get(k);
+                    if *v != Felt::ZERO && (base_value.is_none() || base_value == Some(&Felt::ZERO))
+                    {
+                        Some(*k)
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
         )
     }
 }
