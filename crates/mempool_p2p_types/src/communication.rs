93a8e4944ab44bdee35ed380b7eefb68a27821c0
nadin-Starkware <nadin@starkware.co>
2024-11-11 18:32:57 +0200

    feat: implement MempoolP2pPropagatorClient trait for RemoteMempoolP2pPropagatorClient
    commit-id:c83181e0

--- crates/mempool_p2p_types/src/communication.rs
+++ crates/mempool_p2p_types/src/communication.rs
@@ -93,3 +93,40 @@ impl MempoolP2pPropagatorClient for LocalMempoolP2pPropagatorClient {
         )
     }
 }
+
+#[async_trait]
+impl MempoolP2pPropagatorClient for RemoteMempoolP2pPropagatorClient {
+    async fn add_transaction(
+        &self,
+        transaction: RpcTransaction,
+    ) -> MempoolP2pPropagatorClientResult<()> {
+        let request = MempoolP2pPropagatorRequest::AddTransaction(transaction);
+        let response = match self.send(request).await {
+            Ok(resp) => resp,
+            Err(client_error) => return Err(MempoolP2pPropagatorClientError::from(client_error)),
+        };
+        handle_response_variants!(
+            MempoolP2pPropagatorResponse,
+            AddTransaction,
+            MempoolP2pPropagatorClientError,
+            MempoolP2pPropagatorError
+        )
+    }
+
+    async fn continue_propagation(
+        &self,
+        propagation_metadata: BroadcastedMessageMetadata,
+    ) -> MempoolP2pPropagatorClientResult<()> {
+        let request = MempoolP2pPropagatorRequest::ContinuePropagation(propagation_metadata);
+        let response = match self.send(request).await {
+            Ok(resp) => resp,
+            Err(client_error) => return Err(MempoolP2pPropagatorClientError::from(client_error)),
+        };
+        handle_response_variants!(
+            MempoolP2pPropagatorResponse,
+            ContinuePropagation,
+            MempoolP2pPropagatorClientError,
+            MempoolP2pPropagatorError
+        )
+    }
+}
