44866d0094969821a5d8c043e8cedc7eb0694b37
avivg-starkware <aviv.g@starkware.co>
2024-11-11 13:17:48 +0200

    refactor(blockifier): change return type of invoke_tx deploy_account_tx to account_transaction (#1643)
    …tx to account_transaction

--- crates/blockifier/src/test_utils/deploy_account.rs
+++ crates/blockifier/src/test_utils/deploy_account.rs
@@ -1,15 +1,20 @@
 use starknet_api::test_utils::deploy_account::DeployAccountTxArgs;
 use starknet_api::test_utils::NonceManager;
 
+use crate::transaction::account_transaction::AccountTransaction;
 use crate::transaction::transactions::DeployAccountTransaction;
 
 pub fn deploy_account_tx(
     deploy_tx_args: DeployAccountTxArgs,
     nonce_manager: &mut NonceManager,
-) -> DeployAccountTransaction {
+) -> AccountTransaction {
     let deploy_account_tx = starknet_api::test_utils::deploy_account::executable_deploy_account_tx(
         deploy_tx_args,
         nonce_manager,
     );
-    DeployAccountTransaction { tx: deploy_account_tx, only_query: false }
+
+    // TODO(AvivG): use the "new" method.
+    let executable_deploy_account_tx =
+        DeployAccountTransaction { tx: deploy_account_tx, only_query: false };
+    AccountTransaction::DeployAccount(executable_deploy_account_tx)
 }
