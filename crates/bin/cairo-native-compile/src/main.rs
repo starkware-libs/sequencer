54bfb8585be7df91cbb1183c34d108b67025546f
Avi Cohen <avi.cohen@starkware.co>
2024-11-11 13:26:09 +0200

    Include many compilation attempts

--- crates/bin/cairo-native-compile/src/main.rs
+++ crates/bin/cairo-native-compile/src/main.rs
@@ -1,7 +1,8 @@
-use cairo_lang_sierra::ProgramParser;
+use cairo_lang_sierra::program::Program;
+use cairo_lang_starknet_classes::contract_class::ContractClass;
 use cairo_native::executor::AotContractExecutor;
 use cairo_native::OptLevel;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process;
 
 use clap::Parser;
@@ -11,29 +12,77 @@ use clap::Parser;
 struct Args {
     /// The path of the Sierra file to compile.
     path: PathBuf,
-    /// The output file name (default: stdout).
+    /// The output file name (default: output.so).
     output: Option<String>,
 }
 
-const DEFAULT_SIERRA_FILE: &str = "sierra_file.sierra";
-const DEFAULT_OUTPUT_FILE: &str = "output_file";
+const DEFAULT_OUTPUT_FILE: &str = "./output.so";
 
 fn main() {
     let args = Args::parse();
-    println!("Path : {:?}", args.path);
-    println!("Child PID: {}", process::id());
-    let path = PathBuf::from(DEFAULT_SIERRA_FILE);
+    let path = args.path;
+    let output = args.output.unwrap_or_else(|| DEFAULT_OUTPUT_FILE.to_string());
 
-    let sierra_str = std::fs::read_to_string(&path).unwrap_or_else(|err| {
+    println!("Loading Sierra program from file: {:?}", &path);
+    let sierra_program = load_sierra_program_from_file(&path);
+
+    println!("Compiling Sierra program into file {:?}", &output);
+    let start = std::time::Instant::now();
+    let mut contract_executor = AotContractExecutor::new(&sierra_program, OptLevel::default())
+        .unwrap_or_else(|err| {
+            eprintln!("Error compiling Sierra program: {}", err);
+            process::exit(1);
+        });
+    contract_executor.save(output.clone()).unwrap_or_else(|err| {
+        eprintln!("Error saving compiled program: {}", err);
+        process::exit(1);
+    });
+    println!("Compilation successful. Elapsed: {:?}", start.elapsed());
+
+    println!("Attempting to load compiled program from file {:?}", &output);
+    let loaded_executor =
+        AotContractExecutor::load(Path::new(DEFAULT_OUTPUT_FILE)).unwrap_or_else(|err| {
+            eprintln!("Error loading compiled program: {}", err);
+            process::exit(1);
+        });
+    println!("Program loaded successfully");
+    println!("Program: {:?}", loaded_executor);
+}
+
+fn load_sierra_program_from_file(path: &PathBuf) -> Program {
+    let raw_contract_class = std::fs::read_to_string(path).unwrap_or_else(|err| {
         eprintln!("Error reading Sierra file: {}", err);
         process::exit(1);
     });
-    let sierra_program = ProgramParser::new().parse(&sierra_str).unwrap_or_else(|err| {
-        eprintln!("Error parsing Sierra file: {}", err);
+    let contract_class: ContractClass = serde_json::from_str(&raw_contract_class)
+        .unwrap_or_else(|err| {
+            eprintln!("Error deserializing Sierra file into contract class: {}", err);
+            process::exit(1);
+        });
+    contract_class.extract_sierra_program().unwrap_or_else(|err| {
+        eprintln!("Error extracting Sierra program from contract class: {}", err);
+        process::exit(1);
+    })
+}
+
+#[cfg(test)]
+fn test_compilation() {
+    let sierra_program = load_sierra_program_from_file(&PathBuf::from("test.sierra"));
+    let mut contract_executor = AotContractExecutor::new(&sierra_program, OptLevel::default())
+        .unwrap_or_else(|err| {
+            eprintln!("Error compiling Sierra program: {}", err);
+            process::exit(1);
+        });
+    contract_executor.save("test.so".to_string()).unwrap_or_else(|err| {
+        eprintln!("Error saving compiled program: {}", err);
         process::exit(1);
     });
-
-    let _contract_executor = AotContractExecutor::new(&sierra_program, OptLevel::default())
-        .unwrap()
-        .save(DEFAULT_OUTPUT_FILE);
+    let loaded_executor =
+        AotContractExecutor::load(Path::new("test.so")).unwrap_or_else(|err| {
+            eprintln!("Error loading compiled program: {}", err);
+            process::exit(1);
+        });
+    assert_eq!(contract_executor, loaded_executor);
 }
+
+
