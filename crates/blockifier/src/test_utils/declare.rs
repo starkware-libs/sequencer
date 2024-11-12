2d81fe6c5cfaebb117cbf5d18820aaf056dee34c
avivg-starkware <aviv.g@starkware.co>
2024-11-12 14:25:27 +0200

    fix(blockifier): remove redundant TODO comment addressed in a prev PR #661 (#1976)

--- crates/blockifier/src/test_utils/declare.rs
+++ crates/blockifier/src/test_utils/declare.rs
@@ -7,8 +7,6 @@ use crate::transaction::account_transaction::AccountTransaction;
 pub fn declare_tx(declare_tx_args: DeclareTxArgs, class_info: ClassInfo) -> AccountTransaction {
     let tx_hash = declare_tx_args.tx_hash;
     let declare_tx = starknet_api::test_utils::declare::declare_tx(declare_tx_args);
-    // TODO(AvivG): use starknet_api::test_utils::declare::executable_declare_tx to
-    // create executable_declare.
 
     let executable_tx =
         Transaction::Declare(DeclareTransaction { tx: declare_tx, tx_hash, class_info });
