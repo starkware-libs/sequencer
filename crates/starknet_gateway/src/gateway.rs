b90009977e3742007b26b72f4e46ed1619379cf3
Yair Bakalchuk <yair@starkware.co>
2024-11-12 12:59:10 +0200

    refactor(starknet_gateway): remove usage of AppState

--- crates/starknet_gateway/src/gateway.rs
+++ crates/starknet_gateway/src/gateway.rs
@@ -11,7 +11,7 @@ use starknet_mempool_types::communication::{AddTransactionArgsWrapper, SharedMem
 use starknet_mempool_types::mempool_types::{AccountState, AddTransactionArgs};
 use starknet_sequencer_infra::component_definitions::ComponentStarter;
 use starknet_sierra_compile::config::SierraToCasmCompilationConfig;
-use tracing::{error, instrument};
+use tracing::{error, instrument, Span};
 
 use crate::compilation::GatewayCompiler;
 use crate::config::{GatewayConfig, RpcStateReaderConfig};
@@ -30,15 +30,10 @@ pub mod gateway_test;
 
 pub struct Gateway {
     pub config: GatewayConfig,
-    app_state: AppState,
-}
-
-#[derive(Clone)]
-pub struct AppState {
-    pub stateless_tx_validator: StatelessTransactionValidator,
+    pub stateless_tx_validator: Arc<StatelessTransactionValidator>,
     pub stateful_tx_validator: Arc<StatefulTransactionValidator>,
     pub state_reader_factory: Arc<dyn StateReaderFactory>,
-    pub gateway_compiler: GatewayCompiler,
+    pub gateway_compiler: Arc<GatewayCompiler>,
     pub mempool_client: SharedMempoolClient,
     pub chain_info: ChainInfo,
 }
@@ -50,101 +45,106 @@ impl Gateway {
         gateway_compiler: GatewayCompiler,
         mempool_client: SharedMempoolClient,
     ) -> Self {
-        let app_state = AppState {
-            stateless_tx_validator: StatelessTransactionValidator {
+        Self {
+            config: config.clone(),
+            stateless_tx_validator: Arc::new(StatelessTransactionValidator {
                 config: config.stateless_tx_validator_config.clone(),
-            },
+            }),
             stateful_tx_validator: Arc::new(StatefulTransactionValidator {
                 config: config.stateful_tx_validator_config.clone(),
             }),
             state_reader_factory,
-            gateway_compiler,
+            gateway_compiler: Arc::new(gateway_compiler),
             mempool_client,
             chain_info: config.chain_info.clone(),
-        };
-        Gateway { config, app_state }
-    }
-
-    pub async fn add_tx(
-        &mut self,
-        tx: RpcTransaction,
-        p2p_message_metadata: Option<BroadcastedMessageMetadata>,
-    ) -> GatewayResult<TransactionHash> {
-        let app_state = self.app_state.clone();
-        internal_add_tx(app_state, tx, p2p_message_metadata).await
-    }
-}
-
-// TODO(Yair): consider consolidating internal_add_tx into add_tx.
-
-#[instrument(skip(app_state))]
-async fn internal_add_tx(
-    app_state: AppState,
-    tx: RpcTransaction,
-    p2p_message_metadata: Option<BroadcastedMessageMetadata>,
-) -> GatewayResult<TransactionHash> {
-    let add_tx_args = tokio::task::spawn_blocking(move || {
-        process_tx(
-            app_state.stateless_tx_validator,
-            app_state.stateful_tx_validator.as_ref(),
-            app_state.state_reader_factory.as_ref(),
-            app_state.gateway_compiler,
-            &app_state.chain_info,
-            tx,
-        )
-    })
-    .await
-    .map_err(|join_err| {
-        error!("Failed to process tx: {}", join_err);
-        GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
-    })??;
-
-    let tx_hash = add_tx_args.tx.tx_hash();
-
-    let add_tx_args = AddTransactionArgsWrapper { args: add_tx_args, p2p_message_metadata };
-    app_state.mempool_client.add_tx(add_tx_args).await.map_err(|e| {
-        error!("Failed to send tx to mempool: {}", e);
-        GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
-    })?;
-    // TODO: Also return `ContractAddress` for deploy and `ClassHash` for Declare.
-    Ok(tx_hash)
-}
-
-fn process_tx(
-    stateless_tx_validator: StatelessTransactionValidator,
-    stateful_tx_validator: &StatefulTransactionValidator,
-    state_reader_factory: &dyn StateReaderFactory,
-    gateway_compiler: GatewayCompiler,
-    chain_info: &ChainInfo,
-    tx: RpcTransaction,
-) -> GatewayResult<AddTransactionArgs> {
-    // TODO(Arni, 1/5/2024): Perform congestion control.
-
-    // Perform stateless validations.
-    stateless_tx_validator.validate(&tx)?;
-
-    let executable_tx =
-        compile_contract_and_build_executable_tx(tx, &gateway_compiler, &chain_info.chain_id)?;
-
-    // Perfom post compilation validations.
-    if let AccountTransaction::Declare(executable_declare_tx) = &executable_tx {
-        if !executable_declare_tx.validate_compiled_class_hash() {
-            return Err(GatewaySpecError::CompiledClassHashMismatch);
         }
     }
 
-    let mut validator =
-        stateful_tx_validator.instantiate_validator(state_reader_factory, chain_info)?;
-    let address = executable_tx.contract_address();
-    let nonce = validator.get_nonce(address).map_err(|e| {
-        error!("Failed to get nonce for sender address {}: {}", address, e);
-        GatewaySpecError::UnexpectedError { data: "Internal server error.".to_owned() }
-    })?;
+    #[instrument(skip(self))]
+    pub async fn add_tx(
+        &self,
+        tx: RpcTransaction,
+        p2p_message_metadata: Option<BroadcastedMessageMetadata>,
+    ) -> GatewayResult<TransactionHash> {
+        let blocking_task = ProcessTxBlockingTask::new(self, tx);
+        // Run the blocking task in the current span.
+        let curr_span = Span::current();
+        let add_tx_args =
+            tokio::task::spawn_blocking(move || curr_span.in_scope(|| blocking_task.process_tx()))
+                .await
+                .map_err(|join_err| {
+                    error!("Failed to process tx: {}", join_err);
+                    GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+                })??;
 
-    stateful_tx_validator.run_validate(&executable_tx, nonce, validator)?;
+        let tx_hash = add_tx_args.tx.tx_hash();
 
-    // TODO(Arni): Add the Sierra and the Casm to the mempool input.
-    Ok(AddTransactionArgs { tx: executable_tx, account_state: AccountState { address, nonce } })
+        let add_tx_args = AddTransactionArgsWrapper { args: add_tx_args, p2p_message_metadata };
+        self.mempool_client.add_tx(add_tx_args).await.map_err(|e| {
+            error!("Failed to send tx to mempool: {}", e);
+            GatewaySpecError::UnexpectedError { data: "Internal server error".to_owned() }
+        })?;
+        // TODO: Also return `ContractAddress` for deploy and `ClassHash` for Declare.
+        Ok(tx_hash)
+    }
+}
+
+/// CPU-intensive transaction processing, spawned in a blocking thread to avoid blocking other tasks
+/// from running.
+struct ProcessTxBlockingTask {
+    stateless_tx_validator: Arc<StatelessTransactionValidator>,
+    stateful_tx_validator: Arc<StatefulTransactionValidator>,
+    state_reader_factory: Arc<dyn StateReaderFactory>,
+    gateway_compiler: Arc<GatewayCompiler>,
+    chain_info: ChainInfo,
+    tx: RpcTransaction,
+}
+
+impl ProcessTxBlockingTask {
+    pub fn new(gateway: &Gateway, tx: RpcTransaction) -> Self {
+        Self {
+            stateless_tx_validator: gateway.stateless_tx_validator.clone(),
+            stateful_tx_validator: gateway.stateful_tx_validator.clone(),
+            state_reader_factory: gateway.state_reader_factory.clone(),
+            gateway_compiler: gateway.gateway_compiler.clone(),
+            chain_info: gateway.chain_info.clone(),
+            tx,
+        }
+    }
+
+    fn process_tx(self) -> GatewayResult<AddTransactionArgs> {
+        // TODO(Arni, 1/5/2024): Perform congestion control.
+
+        // Perform stateless validations.
+        self.stateless_tx_validator.validate(&self.tx)?;
+
+        let executable_tx = compile_contract_and_build_executable_tx(
+            self.tx,
+            self.gateway_compiler.as_ref(),
+            &self.chain_info.chain_id,
+        )?;
+
+        // Perfom post compilation validations.
+        if let AccountTransaction::Declare(executable_declare_tx) = &executable_tx {
+            if !executable_declare_tx.validate_compiled_class_hash() {
+                return Err(GatewaySpecError::CompiledClassHashMismatch);
+            }
+        }
+
+        let mut validator = self
+            .stateful_tx_validator
+            .instantiate_validator(self.state_reader_factory.as_ref(), &self.chain_info)?;
+        let address = executable_tx.contract_address();
+        let nonce = validator.get_nonce(address).map_err(|e| {
+            error!("Failed to get nonce for sender address {}: {}", address, e);
+            GatewaySpecError::UnexpectedError { data: "Internal server error.".to_owned() }
+        })?;
+
+        self.stateful_tx_validator.run_validate(&executable_tx, nonce, validator)?;
+
+        // TODO(Arni): Add the Sierra and the Casm to the mempool input.
+        Ok(AddTransactionArgs { tx: executable_tx, account_state: AccountState { address, nonce } })
+    }
 }
 
 pub fn create_gateway(
--- crates/starknet_gateway/src/gateway_test.rs
+++ crates/starknet_gateway/src/gateway_test.rs
@@ -7,6 +7,7 @@ use mempool_test_utils::starknet_api_test_utils::{declare_tx, invoke_tx};
 use mockall::predicate::eq;
 use papyrus_network_types::network_types::BroadcastedMessageMetadata;
 use papyrus_test_utils::{get_rng, GetTestInstance};
+use rstest::{fixture, rstest};
 use starknet_api::core::{ChainId, CompiledClassHash, ContractAddress};
 use starknet_api::executable_transaction::{AccountTransaction, InvokeTransaction};
 use starknet_api::rpc_transaction::{RpcDeclareTransaction, RpcTransaction};
@@ -16,29 +17,62 @@ use starknet_mempool_types::mempool_types::{AccountState, AddTransactionArgs};
 use starknet_sierra_compile::config::SierraToCasmCompilationConfig;
 
 use crate::compilation::GatewayCompiler;
-use crate::config::{StatefulTransactionValidatorConfig, StatelessTransactionValidatorConfig};
-use crate::gateway::{internal_add_tx, AppState, SharedMempoolClient};
+use crate::config::{
+    GatewayConfig,
+    StatefulTransactionValidatorConfig,
+    StatelessTransactionValidatorConfig,
+};
+use crate::gateway::Gateway;
 use crate::state_reader_test_utils::{local_test_state_reader_factory, TestStateReaderFactory};
-use crate::stateful_transaction_validator::StatefulTransactionValidator;
-use crate::stateless_transaction_validator::StatelessTransactionValidator;
 
-pub fn app_state(
-    mempool_client: SharedMempoolClient,
-    state_reader_factory: TestStateReaderFactory,
-) -> AppState {
-    AppState {
-        stateless_tx_validator: StatelessTransactionValidator {
-            config: StatelessTransactionValidatorConfig::default(),
-        },
-        stateful_tx_validator: Arc::new(StatefulTransactionValidator {
-            config: StatefulTransactionValidatorConfig::default(),
-        }),
-        gateway_compiler: GatewayCompiler::new_command_line_compiler(
-            SierraToCasmCompilationConfig::default(),
-        ),
-        state_reader_factory: Arc::new(state_reader_factory),
-        mempool_client,
+#[fixture]
+fn config() -> GatewayConfig {
+    GatewayConfig {
+        stateless_tx_validator_config: StatelessTransactionValidatorConfig::default(),
+        stateful_tx_validator_config: StatefulTransactionValidatorConfig::default(),
         chain_info: ChainInfo::create_for_testing(),
+    }
+}
+
+#[fixture]
+fn compiler() -> GatewayCompiler {
+    GatewayCompiler::new_command_line_compiler(SierraToCasmCompilationConfig::default())
+}
+
+#[fixture]
+fn state_reader_factory() -> TestStateReaderFactory {
+    local_test_state_reader_factory(CairoVersion::Cairo1, false)
+}
+
+#[fixture]
+fn mock_dependencies(
+    config: GatewayConfig,
+    compiler: GatewayCompiler,
+    state_reader_factory: TestStateReaderFactory,
+) -> MockDependencies {
+    let mock_mempool_client = MockMempoolClient::new();
+    MockDependencies { config, compiler, state_reader_factory, mock_mempool_client }
+}
+
+struct MockDependencies {
+    config: GatewayConfig,
+    compiler: GatewayCompiler,
+    state_reader_factory: TestStateReaderFactory,
+    mock_mempool_client: MockMempoolClient,
+}
+
+impl MockDependencies {
+    fn gateway(self) -> Gateway {
+        Gateway::new(
+            self.config,
+            Arc::new(self.state_reader_factory),
+            self.compiler,
+            Arc::new(self.mock_mempool_client),
+        )
+    }
+
+    fn expect_add_tx(&mut self, args: AddTransactionArgsWrapper) {
+        self.mock_mempool_client.expect_add_tx().once().with(eq(args)).return_once(|_| Ok(()));
     }
 }
 
@@ -56,8 +90,9 @@ fn create_tx() -> (RpcTransaction, SenderAddress) {
 }
 
 // TODO: add test with Some broadcasted message metadata
+#[rstest]
 #[tokio::test]
-async fn test_add_tx() {
+async fn test_add_tx(mut mock_dependencies: MockDependencies) {
     let (rpc_tx, address) = create_tx();
     let rpc_invoke_tx =
         assert_matches!(rpc_tx.clone(), RpcTransaction::Invoke(rpc_invoke_tx) => rpc_invoke_tx);
@@ -67,24 +102,19 @@ async fn test_add_tx() {
 
     let tx_hash = executable_tx.tx_hash();
 
-    let mut mock_mempool_client = MockMempoolClient::new();
     let p2p_message_metadata = Some(BroadcastedMessageMetadata::get_test_instance(&mut get_rng()));
     let add_tx_args = AddTransactionArgs {
         tx: executable_tx,
         account_state: AccountState { address, nonce: *rpc_tx.nonce() },
     };
-    mock_mempool_client
-        .expect_add_tx()
-        .once()
-        .with(eq(AddTransactionArgsWrapper {
-            args: add_tx_args,
-            p2p_message_metadata: p2p_message_metadata.clone(),
-        }))
-        .return_once(|_| Ok(()));
-    let state_reader_factory = local_test_state_reader_factory(CairoVersion::Cairo1, false);
-    let app_state = app_state(Arc::new(mock_mempool_client), state_reader_factory);
+    mock_dependencies.expect_add_tx(AddTransactionArgsWrapper {
+        args: add_tx_args,
+        p2p_message_metadata: p2p_message_metadata.clone(),
+    });
 
-    let response_tx_hash = internal_add_tx(app_state, rpc_tx, p2p_message_metadata).await.unwrap();
+    let gateway = mock_dependencies.gateway();
+
+    let response_tx_hash = gateway.add_tx(rpc_tx, p2p_message_metadata).await.unwrap();
 
     assert_eq!(tx_hash, response_tx_hash);
 }
@@ -95,17 +125,16 @@ async fn test_add_tx() {
 // TODO(shahak): Test that when an error occurs in handle_request, then it returns the given p2p
 // metadata.
 
+#[rstest]
 #[tokio::test]
-async fn test_compiled_class_hash_mismatch() {
+async fn test_compiled_class_hash_mismatch(mock_dependencies: MockDependencies) {
     let mut declare_tx =
         assert_matches!(declare_tx(), RpcTransaction::Declare(RpcDeclareTransaction::V3(tx)) => tx);
     declare_tx.compiled_class_hash = CompiledClassHash::default();
     let tx = RpcTransaction::Declare(RpcDeclareTransaction::V3(declare_tx));
 
-    let mock_mempool_client = MockMempoolClient::new();
-    let state_reader_factory = local_test_state_reader_factory(CairoVersion::Cairo1, false);
-    let app_state = app_state(Arc::new(mock_mempool_client), state_reader_factory);
+    let gateway = mock_dependencies.gateway();
 
-    let err = internal_add_tx(app_state, tx, None).await.unwrap_err();
+    let err = gateway.add_tx(tx, None).await.unwrap_err();
     assert_matches!(err, GatewaySpecError::CompiledClassHashMismatch);
 }
