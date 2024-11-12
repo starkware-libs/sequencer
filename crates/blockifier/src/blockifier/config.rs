a01c092fc1727ea4ba2fbc027ce6754dee44a30a
Aviv Greenburg <aviv.g@starkware.co>
2024-11-12 17:24:27 +0200

    chore(blockifier): add flag run_native

--- crates/blockifier/src/blockifier/config.rs
+++ crates/blockifier/src/blockifier/config.rs
@@ -4,6 +4,8 @@ use papyrus_config::dumping::{append_sub_config_name, ser_param, SerializeConfig
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
 
+use crate::state::global_cache::GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST;
+
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct TransactionExecutorConfig {
     pub concurrency_config: ConcurrencyConfig,
@@ -56,6 +58,47 @@ impl SerializeConfig for ConcurrencyConfig {
                 "chunk_size",
                 &self.chunk_size,
                 "The size of the transaction chunk executed in parallel.",
+                ParamPrivacyInput::Public,
+            ),
+        ])
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct CairoNativeConfig {
+    pub run_cairo_native: bool,
+    pub block_compilation: bool,
+    pub global_contract_cache_size: usize,
+}
+impl CairoNativeConfig {
+    pub fn default() -> Self {
+        Self {
+            run_cairo_native: false,
+            block_compilation: false,
+            global_contract_cache_size: GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST,
+        }
+    }
+}
+
+impl SerializeConfig for CairoNativeConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from_iter([
+            ser_param(
+                "run_cairo_native",
+                &self.run_cairo_native,
+                "Enables Cairo native execution.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "block_compilation",
+                &self.block_compilation,
+                "Block Sequencer main program while compiling sierra, for testing.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "global_contract_cache_size",
+                &self.global_contract_cache_size,
+                "The size of the global contract cache.",
                 ParamPrivacyInput::Public,
             ),
         ])
