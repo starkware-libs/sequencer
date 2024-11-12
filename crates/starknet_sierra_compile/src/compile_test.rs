06418747ae0321a769f14c7771085e0bfda8f363
Avi Cohen <avi.cohen@starkware.co>
2024-11-12 17:25:19 +0200

    feat(cairo_native): integrate native compilation into CommandLineCompiler

--- crates/starknet_sierra_compile/src/compile_test.rs
+++ crates/starknet_sierra_compile/src/compile_test.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::path::Path;
 
 use assert_matches::assert_matches;
+use cairo_lang_starknet_classes::contract_class::ContractClass;
 use mempool_test_utils::{get_absolute_path, FAULTY_ACCOUNT_CLASS_FILE, TEST_FILES_FOLDER};
 use rstest::rstest;
 
@@ -11,6 +12,8 @@ use crate::config::SierraToCasmCompilationConfig;
 use crate::errors::CompilationUtilError;
 use crate::test_utils::contract_class_from_file;
 use crate::SierraToCasmCompiler;
+#[cfg(feature = "cairo_native")]
+use crate::SierraToNativeCompiler;
 
 const SIERRA_TO_CASM_COMPILATION_CONFIG: SierraToCasmCompilationConfig =
     SierraToCasmCompilationConfig { max_bytecode_size: 81920 };
@@ -18,20 +21,30 @@ const SIERRA_TO_CASM_COMPILATION_CONFIG: SierraToCasmCompilationConfig =
 fn cairo_lang_compiler() -> CairoLangSierraToCasmCompiler {
     CairoLangSierraToCasmCompiler { config: SIERRA_TO_CASM_COMPILATION_CONFIG }
 }
-fn commnad_line_compiler() -> CommandLineCompiler {
+fn command_line_compiler() -> CommandLineCompiler {
     CommandLineCompiler::new(SIERRA_TO_CASM_COMPILATION_CONFIG)
+}
+fn get_test_contract() -> ContractClass {
+    env::set_current_dir(get_absolute_path(TEST_FILES_FOLDER)).expect("Failed to set current dir.");
+    let sierra_path = Path::new(FAULTY_ACCOUNT_CLASS_FILE);
+    contract_class_from_file(sierra_path)
+}
+
+fn get_faulty_test_contract() -> ContractClass {
+    let mut contract_class = get_test_contract();
+    // Truncate the sierra program to trigger an error.
+    contract_class.sierra_program = contract_class.sierra_program[..100].to_vec();
+    contract_class
 }
 
 // TODO: use the other compiler as well.
 #[rstest]
 #[case::cairo_lang_compiler(cairo_lang_compiler())]
-#[case::command_line_compiler(commnad_line_compiler())]
+#[case::command_line_compiler(command_line_compiler())]
 fn test_compile_sierra_to_casm(#[case] compiler: impl SierraToCasmCompiler) {
-    env::set_current_dir(get_absolute_path(TEST_FILES_FOLDER)).expect("Failed to set current dir.");
-    let sierra_path = Path::new(FAULTY_ACCOUNT_CLASS_FILE);
     let expected_casm_contract_length = 72304;
 
-    let contract_class = contract_class_from_file(sierra_path);
+    let contract_class = get_test_contract();
     let casm_contract = compiler.compile(contract_class).unwrap();
     let serialized_casm = serde_json::to_string_pretty(&casm_contract).unwrap().into_bytes();
 
@@ -41,15 +54,30 @@ fn test_compile_sierra_to_casm(#[case] compiler: impl SierraToCasmCompiler) {
 // TODO(Arni, 1/5/2024): Add a test for panic result test.
 #[rstest]
 #[case::cairo_lang_compiler(cairo_lang_compiler())]
-#[case::command_line_compiler(commnad_line_compiler())]
+#[case::command_line_compiler(command_line_compiler())]
 fn test_negative_flow_compile_sierra_to_casm(#[case] compiler: impl SierraToCasmCompiler) {
-    env::set_current_dir(get_absolute_path(TEST_FILES_FOLDER)).expect("Failed to set current dir.");
-    let sierra_path = Path::new(FAULTY_ACCOUNT_CLASS_FILE);
-
-    let mut contract_class = contract_class_from_file(sierra_path);
-    // Truncate the sierra program to trigger an error.
-    contract_class.sierra_program = contract_class.sierra_program[..100].to_vec();
+    let contract_class = get_faulty_test_contract();
 
     let result = compiler.compile(contract_class);
+    assert_matches!(result, Err(CompilationUtilError::CompilationError(..)));
+}
+
+#[cfg(feature = "cairo_native")]
+#[test]
+fn test_compile_sierra_to_native() {
+    let compiler = command_line_compiler();
+    let contract_class = get_test_contract();
+
+    // TODO(Avi, 1/1/2025): Check size/memory/time limits.
+    let _native_contract_executor = compiler.compile_to_native(contract_class).unwrap();
+}
+
+#[cfg(feature = "cairo_native")]
+#[test]
+fn test_negative_flow_compile_sierra_to_native() {
+    let compiler = command_line_compiler();
+    let contract_class = get_faulty_test_contract();
+
+    let result = compiler.compile_to_native(contract_class);
     assert_matches!(result, Err(CompilationUtilError::CompilationError(..)));
 }
