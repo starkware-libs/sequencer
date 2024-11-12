fb93f2ab8d92bb6303c3e2a4d9a0583f57213dac
eitanm-starkware <eitan.m@starkware.co>
2024-11-12 17:00:10 +0200

    chore(network): stress test utils

--- crates/papyrus_network/src/bin/network_stress_test/converters.rs
+++ crates/papyrus_network/src/bin/network_stress_test/converters.rs
@@ -1,9 +1,19 @@
+use std::mem::size_of;
 use std::time::{Duration, SystemTime};
 
-struct StressTestMessage {
-    id: u32,
-    payload: Vec<u8>,
-    time: SystemTime,
+pub const METADATA_SIZE: usize = size_of::<SystemTime>() + size_of::<u32>();
+
+#[derive(Debug, Clone)]
+pub struct StressTestMessage {
+    pub id: u32,
+    pub payload: Vec<u8>,
+    pub time: SystemTime,
+}
+
+impl StressTestMessage {
+    pub fn new(id: u32, payload: Vec<u8>) -> Self {
+        StressTestMessage { id, payload, time: SystemTime::now() }
+    }
 }
 
 impl From<StressTestMessage> for Vec<u8> {
@@ -24,7 +34,7 @@ impl From<Vec<u8>> for StressTestMessage {
     // This auto implements TryFrom<Vec<u8>> for StressTestMessage
     fn from(mut value: Vec<u8>) -> Self {
         let vec_size = value.len();
-        let payload_size = vec_size - 12;
+        let payload_size = vec_size - METADATA_SIZE;
         let id_and_time = value.split_off(payload_size);
         let id = u32::from_be_bytes(id_and_time[0..4].try_into().unwrap());
         let seconds = u64::from_be_bytes(id_and_time[4..12].try_into().unwrap());
