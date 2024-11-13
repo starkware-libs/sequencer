9365978d3f1e8c11ad103ab8fe37d55ebc1592f1
eitanm-starkware <eitan.m@starkware.co>
2024-11-13 16:26:06 +0200

    feat(network): broadcast stress test

--- crates/papyrus_network/src/bin/network_stress_test/main.rs
+++ crates/papyrus_network/src/bin/network_stress_test/main.rs
@@ -1,3 +1,89 @@
-mod converters;
+use std::time::SystemTime;
+use std::vec;
 
-fn main() {}
+use clap::Command;
+use converters::{StressTestMessage, METADATA_SIZE};
+use futures::StreamExt;
+use libp2p::gossipsub::Topic;
+use papyrus_config::loading::load_and_process_config;
+use papyrus_network::network_manager::{BroadcastTopicClientTrait, NetworkManager};
+use tokio::time::timeout;
+use utils::{Record, TestConfig, BOOTSTRAP_CONFIG_FILE_PATH};
+
+mod converters;
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    TestConfig::create_config_files();
+    let args = std::env::args().collect::<Vec<String>>();
+    let default_path = BOOTSTRAP_CONFIG_FILE_PATH.to_string();
+    let config_path = args.get(1).unwrap_or(&default_path);
+    let file = std::fs::File::open(config_path).unwrap();
+    let TestConfig { network_config, buffer_size, message_size, num_messages, output_path } =
+        load_and_process_config(file, Command::new("Stress Test"), vec![]).unwrap();
+    let mut network_manager = NetworkManager::new(network_config, None);
+    let mut network_channels = network_manager
+        .register_broadcast_topic::<StressTestMessage>(
+            Topic::new("stress_test_topic".to_string()),
+            buffer_size,
+        )
+        .unwrap();
+    let mut output_vector = Vec::<Record>::new();
+    tokio::select! {
+        _ = network_manager.run() => {}
+        _ = async {
+            let mut i = 0;
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            loop {
+                let message = StressTestMessage::new(i, vec![0; message_size - METADATA_SIZE]);
+                network_channels.broadcast_topic_client.broadcast_message(message).await.unwrap();
+                i += 1;
+                if i == num_messages {
+                    println!("Finished sending messages");
+                    futures::future::pending::<()>().await;
+                }
+            }
+        } => {}
+        _ = async {
+            let mut i = 0;
+            loop {
+                let maybe_response = timeout(
+                    std::time::Duration::from_secs(60),
+                    network_channels.broadcasted_messages_receiver.next(),
+                ).await;
+                match maybe_response {
+                    Err(_) => {
+                        println!("Timeout on message {}", i);
+                        break;
+                    }
+                    Ok(None) => break,
+                    Ok(Some((received_message, _report_callback))) => {
+                        let received_message = received_message.unwrap();
+                        output_vector.push(Record {
+                            id: received_message.id,
+                            start_time: received_message.time,
+                            end_time: SystemTime::now(),
+                            duration: SystemTime::now()
+                                .duration_since(received_message.time)
+                                .unwrap()
+                                .as_micros(),
+                        });
+                        i += 1;
+                        if i == num_messages {
+                            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+                            break;
+                        }
+                    }
+                }
+            }
+        } => {
+            println!("Finished receiving messages");
+            let mut wtr = csv::Writer::from_path(output_path).unwrap();
+            for record in output_vector {
+                wtr.serialize(record).unwrap();
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+        }
+    }
+}
