a4c587ebb4c5178be4d52b654997e6706a83dca4
Shahak Shama <shahak@starkware.co>
2024-11-13 10:53:15 +0200

    chore(mempool_p2p): remove deprecated comment from error

--- crates/starknet_mempool_p2p_types/src/errors.rs
+++ crates/starknet_mempool_p2p_types/src/errors.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-// This error is defined even though it's empty to be compatible with the other components.
 #[derive(Debug, Error, Serialize, Deserialize, Clone)]
 pub enum MempoolP2pPropagatorError {
     #[error("Sender request error")]
