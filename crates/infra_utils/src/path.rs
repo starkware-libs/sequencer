5719c03981417432d0dfdbd70bc473b5af6bc3cd
Arni Hod <arnon@starkware.co>
2024-11-14 17:29:18 +0200

    chore(infra): move get_absolute_path function to infra_utils

--- crates/infra_utils/src/path.rs
+++ crates/infra_utils/src/path.rs
@@ -1,1 +1,23 @@
-// TODO(Arni): Move the function get_absolute_path to this file.
+use std::env;
+use std::path::PathBuf;
+
+// TODO(Tsabary/ Arni): consolidate with other get_absolute_path functions.
+/// Resolves a relative path from the project root directory and returns its absolute path.
+///
+/// # Arguments
+/// * `relative_path` - A string slice representing the relative path from the project root.
+///
+/// # Returns
+/// * An absolute `PathBuf` representing the resolved path starting from the project root.
+pub fn resolve_project_relative_path(relative_path: &str) -> PathBuf {
+    path_of_project_root().join(relative_path)
+}
+
+fn path_of_project_root() -> PathBuf {
+    env::var("CARGO_MANIFEST_DIR")
+        // Attempt to get the `CARGO_MANIFEST_DIR` environment variable and convert it to `PathBuf`.
+        // Ascend two directories ("../..") to get to the project root.
+        .map(|dir| PathBuf::from(dir).join("../.."))
+        // If `CARGO_MANIFEST_DIR` isn't set, fall back to the current working directory
+        .unwrap_or_else(|_| env::current_dir().expect("Failed to get current directory"))
+}
