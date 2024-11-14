bcb6338cfd088ecd8724aca52750fb79e3374981
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2024-11-14 14:15:15 +0200

    feat(sync): add StateSyncClient and SyncBlock (#2007)

---
+++ crates/starknet_state_sync_types/src/errors.rs
@@ -0,0 +1,6 @@
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+// This error is defined even though it's empty to be compatible with the other components.
+#[derive(Debug, Error, Serialize, Deserialize, Clone)]
+pub enum StateSyncError {}
