6945a559a76a22862097cfc418bee5db2c00dec9
Rodrigo <rodrodpino@gmail.com>
2024-11-14 21:45:12 +0000

    feat(blockifier): update to cairo native 0.2.2-alpha.0
    refactor(blockifier): modify entry point logic from native runnable class
    
    refactor(blockifier): apply general improvements
    
    refactor(blockifier): make casm constructor public

--- crates/blockifier/src/execution/native/entry_point_execution.rs
+++ crates/blockifier/src/execution/native/entry_point_execution.rs
@@ -1,4 +1,5 @@
 use cairo_native::execution_result::ContractExecutionResult;
+use cairo_native::utils::BuiltinCosts;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use num_traits::ToPrimitive;
 use starknet_api::execution_resources::GasAmount;
@@ -15,6 +16,7 @@ use crate::execution::native::contract_class::NativeContractClassV1;
 use crate::execution::native::syscall_handler::NativeSyscallHandler;
 use crate::state::state_api::State;
 
+// todo(rodrigo): add an `entry point not found` test for Native
 pub fn execute_entry_point_call(
     call: CallEntryPoint,
     contract_class: NativeContractClassV1,
@@ -22,15 +24,28 @@ pub fn execute_entry_point_call(
     resources: &mut ExecutionResources,
     context: &mut EntryPointExecutionContext,
 ) -> EntryPointExecutionResult<CallInfo> {
-    let function_id = contract_class.get_entry_point(&call)?;
+    let entry_point = contract_class.get_entry_point(&call)?;
 
     let mut syscall_handler: NativeSyscallHandler<'_> =
         NativeSyscallHandler::new(call, state, resources, context);
 
+    let gas_costs = &syscall_handler.context.versioned_constants().os_constants.gas_costs;
+    let builtin_costs = BuiltinCosts {
+        // todo(rodrigo): Unsure of what value `const` means, but 1 is the right value
+        r#const: 1,
+        pedersen: gas_costs.pedersen_gas_cost,
+        bitwise: gas_costs.bitwise_builtin_gas_cost,
+        ecop: gas_costs.ecop_gas_cost,
+        poseidon: gas_costs.poseidon_gas_cost,
+        add_mod: gas_costs.add_mod_gas_cost,
+        mul_mod: gas_costs.mul_mod_gas_cost,
+    };
+
     let execution_result = contract_class.executor.run(
-        &function_id,
+        entry_point.selector.0,
         &syscall_handler.call.calldata.0.clone(),
         Some(syscall_handler.call.initial_gas.into()),
+        Some(builtin_costs),
         &mut syscall_handler,
     );
 
