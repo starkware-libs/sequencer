278821cb6cbf5e5194bdf014d00618c510241276
Arni Hod <arnon@starkware.co>
2024-11-14 16:27:27 +0200

    feat: create infra utils crate

--- crates/starknet_sequencer_node/src/utils.rs
+++ crates/starknet_sequencer_node/src/utils.rs
@@ -22,7 +22,8 @@ pub fn create_node_modules(
 /// Returns the absolute path from the project root.
 pub fn get_absolute_path(relative_path: &str) -> PathBuf {
     let base_dir = env::var("CARGO_MANIFEST_DIR")
-        // Attempt to get the `CARGO_MANIFEST_DIR` environment variable and convert it to `PathBuf`. Ascend two directories ("../..") to get to the project root.
+        // Attempt to get the `CARGO_MANIFEST_DIR` environment variable and convert it to `PathBuf`.
+        // Ascend two directories ("../..") to get to the project root.
         .map(|dir| PathBuf::from(dir).join("../.."))
         // If `CARGO_MANIFEST_DIR` isn't set, fall back to the current working directory
         .unwrap_or_else(|_| env::current_dir().expect("Failed to get current directory"));
