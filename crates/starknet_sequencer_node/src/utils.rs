b6c6bd3034424ff05862f5416644a1ffffda8a4c
nadin-Starkware <nadin@starkware.co>
2024-11-14 11:05:47 +0200

    feat: support remote servers execution
    commit-id:02b017c7

--- crates/starknet_sequencer_node/src/utils.rs
+++ crates/starknet_sequencer_node/src/utils.rs
@@ -13,7 +13,7 @@ pub fn create_node_modules(
     let mut channels = create_node_channels();
     let clients = create_node_clients(config, &mut channels);
     let components = create_node_components(config, &clients);
-    let servers = create_node_servers(config, &mut channels, components);
+    let servers = create_node_servers(config, &mut channels, components, &clients);
 
     (clients, servers)
 }
