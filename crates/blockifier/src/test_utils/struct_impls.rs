44a17dbaa19d99b51dfc792e173a8cc0c0ae24f5
Rodrigo <rodrodpino@gmail.com>
2024-11-14 22:06:22 +0000

    refactor(blockifier): remove entry point logic from native runnable class

--- crates/blockifier/src/test_utils/struct_impls.rs
+++ crates/blockifier/src/test_utils/struct_impls.rs
@@ -258,16 +258,13 @@ impl NativeContractClassV1 {
         .expect("Cannot compile sierra into native");
 
         // Compile the sierra contract class into casm
-        let casm_contract_class = CasmContractClass::from_contract_class(
-            sierra_contract_class.clone(),
-            false,
-            usize::MAX,
-        )
-        .expect("Cannot compile sierra contract class into casm contract class");
+        let casm_contract_class =
+            CasmContractClass::from_contract_class(sierra_contract_class, false, usize::MAX)
+                .expect("Cannot compile sierra contract class into casm contract class");
         let casm = ContractClassV1::try_from(casm_contract_class)
             .expect("Cannot get ContractClassV1 from CasmContractClass");
 
-        NativeContractClassV1::new(executor, sierra_contract_class, casm)
+        NativeContractClassV1::new(executor, casm)
     }
 
     pub fn from_file(contract_path: &str) -> Self {
