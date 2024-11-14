6445486e0853c84541c514abda3d5c583754ee8c
Arni Hod <arnon@starkware.co>
2024-11-14 10:45:49 +0200

    chore(starknet_sierra_compile): improve doc and tests for the param max_bytecode_size

--- crates/starknet_sierra_compile/src/config.rs
+++ crates/starknet_sierra_compile/src/config.rs
@@ -8,21 +8,21 @@ use validator::Validate;
 #[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
 pub struct SierraToCasmCompilationConfig {
     /// CASM bytecode size limit.
-    pub max_bytecode_size: usize,
+    pub max_casm_bytecode_size: usize,
 }
 
 impl Default for SierraToCasmCompilationConfig {
     fn default() -> Self {
-        Self { max_bytecode_size: 81920 }
+        Self { max_casm_bytecode_size: 81920 }
     }
 }
 
 impl SerializeConfig for SierraToCasmCompilationConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         BTreeMap::from_iter([ser_param(
-            "max_bytecode_size",
-            &self.max_bytecode_size,
-            "Limitation of contract bytecode size.",
+            "max_casm_bytecode_size",
+            &self.max_casm_bytecode_size,
+            "Limitation of the result CASM contract bytecode size.",
             ParamPrivacyInput::Public,
         )])
     }
