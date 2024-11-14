13911df258fd3d02c15f8a5c0dcecddd57100516
Dori Medini <dori@starkware.co>
2024-11-14 14:02:37 +0200

    fix(starknet_gateway): error code can be negative, and responses have optional data
    Signed-off-by: Dori Medini <dori@starkware.co>

--- commitlint.config.js
+++ commitlint.config.js
@@ -55,6 +55,7 @@ const Configuration = {
             'skeleton',
             'starknet_api',
             'starknet_client',
+            'starknet_gateway',
             'state',
             'storage',
             'sync',
