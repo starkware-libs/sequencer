baee90f381a2d0e5188cebd1a18ebc616f696ad3
Guy Nir <guy.n@starkware.co>
2024-11-17 15:22:44 +0200

    test: move TransactionTestData to test_util

--- crates/starknet_api/src/transaction_hash_test.rs
+++ crates/starknet_api/src/transaction_hash_test.rs
@@ -1,13 +1,10 @@
 use pretty_assertions::assert_eq;
-use serde::{Deserialize, Serialize};
 use sha3::{Digest, Keccak256};
 use starknet_types_core::felt::Felt;
 
 use super::{get_transaction_hash, validate_transaction_hash, CONSTRUCTOR_ENTRY_POINT_SELECTOR};
-use crate::block::BlockNumber;
-use crate::core::ChainId;
-use crate::test_utils::read_json_file;
-use crate::transaction::{Transaction, TransactionHash, TransactionOptions};
+use crate::test_utils::{read_json_file, TransactionTestData};
+use crate::transaction::{Transaction, TransactionOptions};
 
 #[test]
 fn test_constructor_selector() {
@@ -19,18 +16,9 @@ fn test_constructor_selector() {
     assert_eq!(constructor_felt, CONSTRUCTOR_ENTRY_POINT_SELECTOR);
 }
 
-#[derive(Deserialize, Serialize)]
-struct TransactionTestData {
-    transaction: Transaction,
-    transaction_hash: TransactionHash,
-    only_query_transaction_hash: Option<TransactionHash>,
-    chain_id: ChainId,
-    block_number: BlockNumber,
-}
-
 #[test]
 fn test_transaction_hash() {
-    // The details were taken from Starknet Mainnet. You can found the transactions by hash in:
+    // The details were taken from Starknet Mainnet. You can find the transactions by hash in:
     // https://alpha-mainnet.starknet.io/feeder_gateway/get_transaction?transactionHash=<transaction_hash>
     let transactions_test_data_vec: Vec<TransactionTestData> =
         serde_json::from_value(read_json_file("transaction_hash.json")).unwrap();
@@ -64,7 +52,7 @@ fn test_transaction_hash() {
 
 #[test]
 fn test_deprecated_transaction_hash() {
-    // The details were taken from Starknet Mainnet. You can found the transactions by hash in:
+    // The details were taken from Starknet Mainnet. You can find the transactions by hash in:
     // https://alpha-mainnet.starknet.io/feeder_gateway/get_transaction?transactionHash=<transaction_hash>
     let transaction_test_data_vec: Vec<TransactionTestData> =
         serde_json::from_value(read_json_file("deprecated_transaction_hash.json")).unwrap();
