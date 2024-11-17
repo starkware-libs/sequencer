f9ac8da87911d61d1371595420dab5bf491d389e
Guy Nir <guy.n@starkware.co>
2024-11-17 15:31:44 +0200

    feat: allow conversion of (invoke) transaction into executable transaction

--- crates/starknet_api/src/transaction_test.rs
+++ crates/starknet_api/src/transaction_test.rs
@@ -1,5 +1,6 @@
 use crate::block::NonzeroGasPrice;
 use crate::execution_resources::GasAmount;
+use crate::test_utils::{read_json_file, TransactionTestData};
 use crate::transaction::Fee;
 
 #[test]
@@ -24,4 +25,16 @@ fn test_fee_div_ceil() {
         GasAmount(10),
         Fee(28).checked_div_ceil(NonzeroGasPrice::try_from(3_u8).unwrap()).unwrap()
     );
+}
+
+#[test]
+fn convert_executable_transaction_and_back() {
+    // The details were taken from Starknet Mainnet. You can find the transactions by hash in:
+    // https://alpha-mainnet.starknet.io/feeder_gateway/get_transaction?transactionHash=<transaction_hash>
+    let transactions_test_data_vec: Vec<TransactionTestData> =
+        serde_json::from_value(read_json_file("transaction_hash.json")).unwrap();
+
+    for transaction_test_data in transactions_test_data_vec {
+        print!("{:?}", transaction_test_data);
+    }
 }
