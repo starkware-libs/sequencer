3383ad42cbf8419d20e3f68d1157bbf084b3b73c
Guy Nir <guy.n@starkware.co>
2024-11-17 15:58:44 +0200

    feat: allow Transaction (and hash) to be converted into an ExecutableTransaction

--- crates/starknet_api/src/transaction_test.rs
+++ crates/starknet_api/src/transaction_test.rs
@@ -1,4 +1,6 @@
+use super::Transaction;
 use crate::block::NonzeroGasPrice;
+use crate::executable_transaction::Transaction as ExecutableTransaction;
 use crate::execution_resources::GasAmount;
 use crate::test_utils::{read_json_file, TransactionTestData};
 use crate::transaction::Fee;
@@ -31,10 +33,23 @@ fn test_fee_div_ceil() {
 fn convert_executable_transaction_and_back() {
     // The details were taken from Starknet Mainnet. You can find the transactions by hash in:
     // https://alpha-mainnet.starknet.io/feeder_gateway/get_transaction?transactionHash=<transaction_hash>
-    let transactions_test_data_vec: Vec<TransactionTestData> =
+    let mut transactions_test_data_vec: Vec<TransactionTestData> =
         serde_json::from_value(read_json_file("transaction_hash.json")).unwrap();
 
-    for transaction_test_data in transactions_test_data_vec {
-        print!("{:?}", transaction_test_data);
-    }
+    let (tx, tx_hash) = loop {
+        match transactions_test_data_vec.pop() {
+            Some(data) => {
+                if let Transaction::Invoke(tx) = data.transaction {
+                    // Do something with the data
+                    break (Transaction::Invoke(tx), data.transaction_hash);
+                }
+            }
+            None => {
+                panic!("Could not find a single Invoke transaction in the test data");
+            }
+        }
+    };
+    let executable_tx: ExecutableTransaction = (tx.clone(), tx_hash.clone()).into();
+    let tx_back = Transaction::from(executable_tx);
+    assert_eq!(tx, tx_back);
 }
