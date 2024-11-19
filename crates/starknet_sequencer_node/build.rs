16038832cc9a13afda5878ffb3d25d2950d5bb9e
Arnon Hod <arnon@starkware.co>
2024-11-19 12:27:46 +0200

    chore: fix typos in build files (#2168)

--- crates/starknet_sequencer_node/build.rs
+++ crates/starknet_sequencer_node/build.rs
@@ -1,3 +1,3 @@
 // Sets up the environment variable OUT_DIR, which holds the cairo compiler binary.
-// The binary is dowloaded to OUT_DIR by the starknet_sierra_compile crate.
+// The binary is downloaded to OUT_DIR by the starknet_sierra_compile crate.
 fn main() {}
