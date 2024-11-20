90968c1897f05e05183481468d942c08a787e224
giladchase <gilad@starkware.co>
2024-11-20 16:54:35 +0200

    feat(l1_provider): add `L1ProviderContent` (#2176)
    Follows the same pattern and API style as `MempoolContent`.
    
    Co-Authored-By: Gilad Chase <gilad@starkware.com>

--- crates/starknet_l1_provider/src/l1_provider_tests.rs
+++ crates/starknet_l1_provider/src/l1_provider_tests.rs
@@ -1,15 +1,14 @@
 use assert_matches::assert_matches;
-use indexmap::IndexMap;
 use pretty_assertions::assert_eq;
-use starknet_api::executable_transaction::L1HandlerTransaction;
 use starknet_api::hash::StarkHash;
 use starknet_api::l1_handler_tx_args;
 use starknet_api::test_utils::l1_handler::executable_l1_handler_tx;
 use starknet_api::transaction::TransactionHash;
 
 use crate::errors::L1ProviderError;
+use crate::test_utils::L1ProviderContentBuilder;
+use crate::L1Provider;
 use crate::ProviderState::{Propose, Validate};
-use crate::{L1Provider, TransactionManager};
 
 macro_rules! tx {
     (tx_hash: $tx_hash:expr) => {{
@@ -21,41 +20,27 @@ macro_rules! tx {
     }};
 }
 
-// TODO: change to something more robust once we have more tests.
-fn l1_provider(txs: Vec<L1HandlerTransaction>) -> L1Provider {
-    let n_txs = txs.len();
-    let awaiting_l2_inclusion: IndexMap<_, _> =
-        txs.into_iter().map(|tx| (tx.tx_hash, tx)).collect();
-    assert_eq!(
-        awaiting_l2_inclusion.len(),
-        n_txs,
-        "Transactions given to this constructor should have unique hashes."
-    );
-
-    L1Provider {
-        tx_manager: TransactionManager { txs: awaiting_l2_inclusion, ..Default::default() },
-        ..Default::default()
-    }
-}
-
 #[test]
 fn get_txs_happy_flow() {
     // Setup.
-    let txs = vec![tx!(tx_hash: 0), tx!(tx_hash: 1), tx!(tx_hash: 2)];
-    let mut l1_provider = l1_provider(txs.clone());
-    l1_provider.proposal_start().unwrap();
+    let txs = [tx!(tx_hash: 0), tx!(tx_hash: 1), tx!(tx_hash: 2)];
+    let mut l1_provider = L1ProviderContentBuilder::new()
+        .with_txs(txs.clone())
+        .with_state(Propose)
+        .build_into_l1_provider();
 
     // Test.
-    assert_eq!(l1_provider.get_txs(0).unwrap(), vec![]);
-    assert_eq!(l1_provider.get_txs(1).unwrap(), vec![txs[0].clone()]);
-    assert_eq!(l1_provider.get_txs(3).unwrap(), txs[1..=2].to_vec());
-    assert_eq!(l1_provider.get_txs(1).unwrap(), vec![]);
+    assert_eq!(l1_provider.get_txs(0).unwrap(), []);
+    assert_eq!(l1_provider.get_txs(1).unwrap(), [txs[0].clone()]);
+    assert_eq!(l1_provider.get_txs(3).unwrap(), txs[1..=2]);
+    assert_eq!(l1_provider.get_txs(1).unwrap(), []);
 }
 
 #[test]
 fn pending_state_errors() {
     // Setup.
-    let mut l1_provider = l1_provider(vec![tx!(tx_hash: 0)]);
+    let mut l1_provider =
+        L1ProviderContentBuilder::new().with_txs([tx!(tx_hash: 1)]).build_into_l1_provider();
 
     // Test.
     assert_matches!(
