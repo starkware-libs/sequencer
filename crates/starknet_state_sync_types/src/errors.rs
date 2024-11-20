ea8fef7df55643c63795a2c909bd8fe7d922efd2
Noam Spiegelstein <noam.s@starkware.co>
2024-11-20 09:06:59 +0200

    feat(sync): implement get_block in StateSync

--- crates/starknet_state_sync_types/src/errors.rs
+++ crates/starknet_state_sync_types/src/errors.rs
@@ -1,3 +1,4 @@
+use papyrus_storage::StorageError;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -5,4 +6,14 @@ use thiserror::Error;
 pub enum StateSyncError {
     #[error("Communication error between StateSync and StateSyncRunner")]
     RunnerCommunicationError,
+    // StorageError does not derive Serialize, Deserialize and Clone Traits.
+    // We put the string of the error instead.
+    #[error("Unexpected storage error: {0}")]
+    StorageError(String),
+}
+
+impl From<StorageError> for StateSyncError {
+    fn from(error: StorageError) -> Self {
+        StateSyncError::StorageError(error.to_string())
+    }
 }
