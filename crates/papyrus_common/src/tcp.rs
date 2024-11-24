d273db9e2594f2a5639024accbb0933865817431
Yair Bakalchuk <yair@starkware.co>
2024-11-24 17:27:06 +0200

    feat(tests_integration): one to many network config

--- crates/papyrus_common/src/tcp.rs
+++ crates/papyrus_common/src/tcp.rs
@@ -8,11 +8,15 @@ pub fn find_free_port() -> u16 {
     listener.local_addr().expect("Failed to get local address").port()
 }
 
-pub fn find_n_free_ports<const N: usize>() -> [u16; N] {
+pub fn find_n_free_ports(n: usize) -> Vec<u16> {
     // The socket is automatically closed when the function exits.
     // The port may still be available when accessed, but this is not guaranteed.
     // TODO(Asmaa): find a reliable way to ensure the port stays free.
-    let listeners: [TcpListener; N] =
-        core::array::from_fn(|_i| TcpListener::bind("0.0.0.0:0").expect("Failed to bind"));
-    core::array::from_fn(|i| listeners[i].local_addr().expect("Failed to get local address").port())
+    let mut ports = Vec::with_capacity(n);
+    for _ in 0..n {
+        let listener = TcpListener::bind("0.0.0.0:0").expect("Failed to bind");
+        let port = listener.local_addr().expect("Failed to get local address").port();
+        ports.push(port);
+    }
+    ports
 }
