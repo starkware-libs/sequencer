f92053a26177d31a4954cae763a91d4560686885
Itay Tsabary <itayt@starkware.co>
2024-11-25 11:14:34 +0200

    chore: add shell command builder
    commit-id:96859c27

---
+++ crates/infra_utils/src/command.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+use tokio::process::Command;
+
+use crate::path::project_path;
+
+#[cfg(test)]
+#[path = "command_test.rs"]
+mod command_test;
+
+/// Returns a shell command originating from the project root, with cargo environment variables
+/// filtered out.
+///
+/// # Arguments
+/// * `command_name` - The shell command name.
+///
+/// # Returns
+/// * A [`std::process::Command`] object with the current directory set to the project root, and
+///   cleared out cargo related environment variables.
+pub fn create_shell_command(command_name: &str) -> Command {
+    let project_path = project_path().expect("Failed to get project path");
+    let mut command = Command::new(command_name);
+    command.current_dir(&project_path);
+    // Filter out all CARGO_ environment variables.
+    env::vars().filter(|(key, _)| key.starts_with("CARGO_")).for_each(|(key, _)| {
+        command.env_remove(key);
+    });
+    command
+}
