3a5080501e402fa780930ad9aa228aab77b30ab2
Itay Tsabary <itayt@starkware.co>
2024-11-25 12:03:09 +0200

    chore(sequencer_node): replace run node shell command
    commit-id:3dd1cf11

--- crates/starknet_integration_tests/tests/end_to_end_integration_test.rs
+++ crates/starknet_integration_tests/tests/end_to_end_integration_test.rs
@@ -1,8 +1,8 @@
-use std::env;
 use std::path::PathBuf;
 use std::process::Stdio;
 use std::time::Duration;
 
+use infra_utils::command::create_shell_command;
 use mempool_test_utils::starknet_api_test_utils::{AccountId, MultiAccountTransactionGenerator};
 use papyrus_execution::execution_utils::get_nonce_at;
 use papyrus_storage::state::StateStorageReader;
@@ -15,7 +15,7 @@ use starknet_integration_tests::integration_test_setup::IntegrationTestSetup;
 use starknet_integration_tests::utils::{create_integration_test_tx_generator, send_account_txs};
 use starknet_sequencer_infra::trace_util::configure_tracing;
 use starknet_types_core::felt::Felt;
-use tokio::process::{Child, Command};
+use tokio::process::Child;
 use tokio::task::{self, JoinHandle};
 use tokio::time::interval;
 use tracing::{error, info};
@@ -27,18 +27,14 @@ fn tx_generator() -> MultiAccountTransactionGenerator {
 
 // TODO(Tsabary): Move to a suitable util location.
 async fn spawn_node_child_task(node_config_path: PathBuf) -> Child {
-    // Get the current working directory for the project
-    let project_path = env::current_dir().expect("Failed to get current directory").join("../..");
-
     // TODO(Tsabary): Capture output to a log file, and present it in case of a failure.
     // TODO(Tsabary): Change invocation from "cargo run" to separate compilation and invocation
     // (build, and then invoke the binary).
-    Command::new("cargo")
+    create_shell_command("cargo")
         .arg("run")
         .arg("--bin")
         .arg("starknet_sequencer_node")
         .arg("--quiet")
-        .current_dir(&project_path)
         .arg("--")
         .arg("--config_file")
         .arg(node_config_path.to_str().unwrap())
