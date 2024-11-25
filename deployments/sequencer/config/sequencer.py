db78c3e73b21d2cfeb389638b9478d2303dd5a30
Idan Shamam <idan@starkware.co>
2024-11-25 10:11:20 +0200

    chore(deployment): cdk8s deployment refactor

--- deployments/sequencer/config/sequencer.py
+++ deployments/sequencer/config/sequencer.py
@@ -1,25 +1,19 @@
-import typing
 import os
 import json
 import jsonschema
 
-
 from services.objects import Config
 
 
-def load_config(config_path):
-    root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../')
-    config_dir = os.path.join(root_dir, 'config/sequencer/')
-    
-    return json.loads(
-        open(os.path.join(config_dir, config_path), 'r').read()
-    )
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../')
+CONFIG_DIR = os.path.join(ROOT_DIR, 'config/sequencer/')
+
 
 class SequencerDevConfig(Config):
-    def __init__(self, mount_path: str, custom_config_path: typing.Optional[str]):
+    def __init__(self, mount_path: str, config_file_path: str = ""):
         super().__init__(
-            schema=load_config(config_path='default_config.json'),
-            config=load_config(config_path='presets/config.json') if not custom_config_path else json.loads(open(os.path.abspath(custom_config_path)).read()),
+            schema=json.loads(open(os.path.join(CONFIG_DIR, 'default_config.json'), 'r').read()),
+            config=json.loads(open(os.path.join(CONFIG_DIR, 'presets', 'config.json'), 'r').read()) if not config_file_path else json.loads(open(os.path.abspath(config_file_path)).read()),
             mount_path=mount_path
         )
 
--- deployments/sequencer/main.py
+++ deployments/sequencer/main.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 
 import dataclasses
-import os
 
-from constructs import Construct # type: ignore
-from cdk8s import App, Chart, YamlOutputType # type: ignore
-from typing import Dict, Any, Optional
+from constructs import Construct
+from cdk8s import App, Chart, YamlOutputType
+from typing import Optional
 
-from services.service import Service
+
 from config.sequencer import Config
-from services.objects import *
-from services import defaults
-
-
-env = os.getenv("ENV", "dev")
-
-
-if env == "dev":
-    system_preset = defaults.sequencer_dev
-elif env == "prod":
-    system_preset = defaults.sequencer_prod 
+from app.service import ServiceApp
+from services.topology import ServiceTopology, SequencerDev, SequencerProd
+from services.helpers import args
 
 
 @dataclasses.dataclass
@@ -38,36 +29,38 @@ class SequencerNode(Chart):
         self,
         scope: Construct,
         name: str,
-        namespace: str
+        namespace: str,
+        topology: ServiceTopology
     ):
         super().__init__(
             scope, name, disable_resource_name_hashes=True, namespace=namespace
         )
-        self.service = Service(
+        self.service = ServiceApp(
             self,
             name,
             namespace=namespace,
-            deployment=system_preset.deployment,
-            config=system_preset.config,
-            image=system_preset.image,
-            args=system_preset.args,
-            port_mappings=system_preset.port_mappings,
-            service_type=system_preset.service_type,
-            replicas=system_preset.replicas,
-            health_check=system_preset.health_check,
-            pvc=system_preset.pvc,
-            ingress=system_preset.ingress
+            topology=topology
         )
 
+def main():
+    if args.env == "dev":
+        system_preset = SequencerDev()
+    elif args.env == "prod":
+        system_preset = SequencerProd()
 
-app = App(
-    yaml_output_type=YamlOutputType.FOLDER_PER_CHART_FILE_PER_RESOURCE
-)
+    app = App(
+        yaml_output_type=YamlOutputType.FOLDER_PER_CHART_FILE_PER_RESOURCE
+    )
 
-sequencer_node = SequencerNode(
-    scope=app,
-    name=system_preset.name,
-    namespace=system_preset.namespace
-)
+    SequencerNode(
+        scope=app,
+        name="sequencer-node",
+        namespace=args.namespace,
+        topology=system_preset
+    )
 
-app.synth()
+    app.synth()
+
+
+if __name__ == "__main__":
+    main()
