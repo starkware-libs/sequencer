82c426319d65e02811a5991095d1efc0b12ea1fc
Yonatan Iluz <yonatan@starkware.co>
2024-11-26 18:37:26 +0200

    chore(cairo_native): update to v0.2.4

--- crates/blockifier/src/execution/native/entry_point_execution.rs
+++ crates/blockifier/src/execution/native/entry_point_execution.rs
@@ -27,9 +27,9 @@ pub fn execute_entry_point_call(
     let mut syscall_handler: NativeSyscallHandler<'_> =
         NativeSyscallHandler::new(call, state, context);
 
-    let gas_costs = &syscall_handler.base.context.versioned_constants().os_constants.gas_costs;
+    let gas_costs = &syscall_handler.base.context.gas_costs();
     let builtin_costs = BuiltinCosts {
-        // todo(rodrigo): Unsure of what value `const` means, but 1 is the right value
+        // todo(rodrigo): Unsure of what value `const` means, but 1 is the right value.
         r#const: 1,
         pedersen: gas_costs.pedersen_gas_cost,
         bitwise: gas_costs.bitwise_builtin_gas_cost,
@@ -39,13 +39,10 @@ pub fn execute_entry_point_call(
         mul_mod: gas_costs.mul_mod_gas_cost,
     };
 
-    // Fund the initial budget since the native executor charges it before the run.
-    // TODO(Yoni): revert once the VM is aligned with this.
-    let gas = syscall_handler.base.call.initial_gas + gas_costs.entry_point_initial_budget;
     let execution_result = contract_class.executor.run(
         entry_point.selector.0,
         &syscall_handler.base.call.calldata.0.clone(),
-        Some(gas),
+        syscall_handler.base.call.initial_gas,
         Some(builtin_costs),
         &mut syscall_handler,
     );
@@ -64,24 +61,16 @@ fn create_callinfo(
     call_result: ContractExecutionResult,
     syscall_handler: NativeSyscallHandler<'_>,
 ) -> Result<CallInfo, EntryPointExecutionError> {
-    let mut remaining_gas = call_result.remaining_gas;
+    let remaining_gas = call_result.remaining_gas;
 
     if remaining_gas > syscall_handler.base.call.initial_gas {
-        if remaining_gas - syscall_handler.base.call.initial_gas
-            <= syscall_handler.base.context.gas_costs().entry_point_initial_budget
-        {
-            // Revert the refund.
-            // TODO(Yoni): temporary hack - this is probably a bug. Investigate and fix native.
-            remaining_gas = syscall_handler.base.call.initial_gas;
-        } else {
-            return Err(PostExecutionError::MalformedReturnData {
-                error_message: format!(
-                    "Unexpected remaining gas. Used gas is greater than initial gas: {} > {}",
-                    remaining_gas, syscall_handler.base.call.initial_gas
-                ),
-            }
-            .into());
+        return Err(PostExecutionError::MalformedReturnData {
+            error_message: format!(
+                "Unexpected remaining gas. Used gas is greater than initial gas: {} > {}",
+                remaining_gas, syscall_handler.base.call.initial_gas
+            ),
         }
+        .into());
     }
 
     let gas_consumed = syscall_handler.base.call.initial_gas - remaining_gas;
