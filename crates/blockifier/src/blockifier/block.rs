13406378c5188bedfbdd1091399aa76eebfa0c90
Nimrod Weiss <nimrod@starkware.co>
2024-11-26 10:59:09 +0200

    feat(blockifier): add OS constants

--- crates/blockifier/src/blockifier/block.rs
+++ crates/blockifier/src/blockifier/block.rs
@@ -14,7 +14,7 @@ use crate::abi::constants;
 use crate::state::errors::StateError;
 use crate::state::state_api::{State, StateResult};
 use crate::transaction::objects::FeeType;
-use crate::versioned_constants::VersionedConstants;
+use crate::versioned_constants::{OsConstants, VersionedConstants};
 
 #[cfg(test)]
 #[path = "block_test.rs"]
@@ -119,12 +119,13 @@ pub fn pre_process_block(
     state: &mut dyn State,
     old_block_number_and_hash: Option<BlockHashAndNumber>,
     next_block_number: BlockNumber,
+    os_constants: &OsConstants,
 ) -> StateResult<()> {
     let should_block_hash_be_provided =
         next_block_number >= BlockNumber(constants::STORED_BLOCK_HASH_BUFFER);
     if let Some(BlockHashAndNumber { number, hash }) = old_block_number_and_hash {
         let block_hash_contract_address =
-            ContractAddress::from(constants::BLOCK_HASH_CONTRACT_ADDRESS);
+            os_constants.os_contract_addresses.block_hash_contract_address();
         let block_number_as_storage_key = StorageKey::from(number.0);
         state.set_storage_at(block_hash_contract_address, block_number_as_storage_key, hash.0)?;
     } else if should_block_hash_be_provided {
