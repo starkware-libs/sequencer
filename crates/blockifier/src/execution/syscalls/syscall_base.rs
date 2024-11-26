5b5e79e4029119636291c726b2c29f1ee7272fcc
Nimrod Weiss <nimrod@starkware.co>
2024-11-26 10:59:09 +0200

    feat(blockifier): add OS constants

--- crates/blockifier/src/execution/syscalls/syscall_base.rs
+++ crates/blockifier/src/execution/syscalls/syscall_base.rs
@@ -97,8 +97,14 @@ impl<'state> SyscallHandlerBase<'state> {
         }
 
         let key = StorageKey::try_from(Felt::from(requested_block_number))?;
-        let block_hash_contract_address =
-            ContractAddress::try_from(Felt::from(constants::BLOCK_HASH_CONTRACT_ADDRESS))?;
+        let block_hash_contract_address = self
+            .context
+            .tx_context
+            .block_context
+            .versioned_constants
+            .os_constants
+            .os_contract_addresses
+            .block_hash_contract_address();
         Ok(self.state.get_storage_at(block_hash_contract_address, key)?)
     }
 
