3faf3f51abeb735544ce8dbe9c9929fa28842809
Yoav Gross <yoav@starkware.co>
2024-11-26 15:24:18 +0200

    feat(starknet_api): checked mul for gas vector

--- crates/starknet_api/src/execution_resources.rs
+++ crates/starknet_api/src/execution_resources.rs
@@ -66,6 +66,10 @@ impl GasAmount {
     pub fn checked_mul(self, rhs: GasPrice) -> Option<Fee> {
         rhs.checked_mul(self)
     }
+
+    pub fn checked_factor_mul(self, factor: u64) -> Option<Self> {
+        self.0.checked_mul(factor).map(Self)
+    }
 }
 
 #[cfg_attr(
@@ -101,6 +105,20 @@ impl GasVector {
             self.l1_gas.checked_add(rhs.l1_gas),
             self.l1_data_gas.checked_add(rhs.l1_data_gas),
             self.l2_gas.checked_add(rhs.l2_gas),
+        ) {
+            (Some(l1_gas), Some(l1_data_gas), Some(l2_gas)) => {
+                Some(Self { l1_gas, l1_data_gas, l2_gas })
+            }
+            _ => None,
+        }
+    }
+
+    pub fn checked_mul(self, factor: usize) -> Option<Self> {
+        let u64_factor = factor.try_into().ok()?;
+        match (
+            self.l1_gas.checked_factor_mul(u64_factor),
+            self.l1_data_gas.checked_factor_mul(u64_factor),
+            self.l2_gas.checked_factor_mul(u64_factor),
         ) {
             (Some(l1_gas), Some(l1_data_gas), Some(l2_gas)) => {
                 Some(Self { l1_gas, l1_data_gas, l2_gas })
