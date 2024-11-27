33e017b5500f5d2a56efe511741b672bffe961f4
Aner Ben Efraim <aner@starkware.co>
2024-11-27 13:46:54 +0200

    refactor(blockifier_reexecution): move reexecution test to separate file

--- crates/blockifier_reexecution/src/state_reader.rs
+++ crates/blockifier_reexecution/src/state_reader.rs
@@ -4,6 +4,8 @@ mod errors;
 pub mod raw_rpc_json_test;
 pub mod reexecution_state_reader;
 #[cfg(test)]
+pub mod reexecution_test;
+#[cfg(test)]
 #[cfg(feature = "blockifier_regression_https_testing")]
 pub mod rpc_https_test;
 pub mod serde_utils;
