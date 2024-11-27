5188981d5279a79d8d8e5befee4e49b044332cd3
Aner Ben Efraim <aner@starkware.co>
2024-11-27 13:46:54 +0200

    refactor(blockifier_reexecution): move reexecution test to separate file

--- crates/blockifier_reexecution/src/state_reader.rs
+++ crates/blockifier_reexecution/src/state_reader.rs
@@ -4,7 +4,8 @@ mod errors;
 pub mod raw_rpc_json_test;
 pub mod reexecution_state_reader;
 #[cfg(test)]
-#[cfg(feature = "blockifier_regression_https_testing")]
+pub mod reexecution_test;
+#[cfg(all(test, feature = "blockifier_regression_https_testing"))]
 pub mod rpc_https_test;
 pub mod serde_utils;
 pub mod test_state_reader;
--- crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
+++ crates/blockifier_reexecution/src/state_reader/raw_rpc_json_test.rs
@@ -19,7 +19,7 @@ use starknet_gateway::rpc_objects::BlockHeader;
 
 use crate::state_reader::compile::legacy_to_contract_class_v0;
 use crate::state_reader::serde_utils::deserialize_transaction_json_to_starknet_api_tx;
-use crate::state_reader::utils::{reexecute_block_for_testing, ReexecutionStateMaps};
+use crate::state_reader::utils::ReexecutionStateMaps;
 
 #[fixture]
 fn block_header() -> BlockHeader {
@@ -161,23 +161,4 @@ fn serialize_state_maps() {
 
     assert_eq!(serializable_state_maps, deserialized_state_maps);
     assert_eq!(original_state_maps, deserialized_state_maps.try_into().unwrap());
-}
-
-#[rstest]
-#[case::v_0_13_0(600001)]
-#[case::v_0_13_1(620978)]
-#[case::v_0_13_1_1(649367)]
-#[case::v_0_13_2(685878)]
-#[case::v_0_13_2_1(700000)]
-#[case::invoke_with_replace_class_syscall(780008)]
-#[case::invoke_with_deploy_syscall(870136)]
-#[case::example_deploy_account_v1(837408)]
-#[case::example_deploy_account_v3(837792)]
-#[case::example_declare_v1(837461)]
-#[case::example_declare_v2(822636)]
-#[case::example_declare_v3(825013)]
-#[case::example_l1_handler(868429)]
-#[ignore = "Requires downloading JSON files prior to running; Long test, run with --release flag."]
-fn test_block_reexecution(#[case] block_number: u64) {
-    reexecute_block_for_testing(block_number);
 }
