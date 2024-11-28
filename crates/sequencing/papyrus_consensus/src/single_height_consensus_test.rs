b6a3940ab3a7fe2966a3ccfc4f1bc2e6645a1683
Arni Hod <arnon@starkware.co>
2024-11-28 15:42:41 +0200

    chore(starknet_consensus_manager): set proposer address in propose block input

--- crates/sequencing/papyrus_consensus/src/single_height_consensus_test.rs
+++ crates/sequencing/papyrus_consensus/src/single_height_consensus_test.rs
@@ -91,7 +91,7 @@ async fn proposer() {
         block_sender.send(BLOCK.id).unwrap();
         block_receiver
     });
-    context.expect_set_height_and_round().returning(move |_, _| ());
+    context.expect_set_height_and_round().returning(move |_, _, _| ());
     context
         .expect_broadcast()
         .times(1)
@@ -171,12 +171,12 @@ async fn validator(repeat_proposal: bool) {
     );
 
     context.expect_proposer().returning(move |_, _| *PROPOSER_ID);
-    context.expect_validate_proposal().times(1).returning(move |_, _, _, _| {
+    context.expect_validate_proposal().times(1).returning(move |_, _, _, _, _| {
         let (block_sender, block_receiver) = oneshot::channel();
         block_sender.send((BLOCK.id, PROPOSAL_FIN.clone())).unwrap();
         block_receiver
     });
-    context.expect_set_height_and_round().returning(move |_, _| ());
+    context.expect_set_height_and_round().returning(move |_, _, _| ());
     context
         .expect_broadcast()
         .times(1)
@@ -250,12 +250,12 @@ async fn vote_twice(same_vote: bool) {
     );
 
     context.expect_proposer().times(1).returning(move |_, _| *PROPOSER_ID);
-    context.expect_validate_proposal().times(1).returning(move |_, _, _, _| {
+    context.expect_validate_proposal().times(1).returning(move |_, _, _, _, _| {
         let (block_sender, block_receiver) = oneshot::channel();
         block_sender.send((BLOCK.id, PROPOSAL_FIN.clone())).unwrap();
         block_receiver
     });
-    context.expect_set_height_and_round().returning(move |_, _| ());
+    context.expect_set_height_and_round().returning(move |_, _, _| ());
     context
         .expect_broadcast()
         .times(1) // Shows the repeat vote is ignored.
@@ -324,7 +324,7 @@ async fn rebroadcast_votes() {
         block_sender.send(BLOCK.id).unwrap();
         block_receiver
     });
-    context.expect_set_height_and_round().returning(move |_, _| ());
+    context.expect_set_height_and_round().returning(move |_, _, _| ());
     context
         .expect_broadcast()
         .times(1)
@@ -386,7 +386,7 @@ async fn repropose() {
         block_sender.send(BLOCK.id).unwrap();
         block_receiver
     });
-    context.expect_set_height_and_round().returning(move |_, _| ());
+    context.expect_set_height_and_round().returning(move |_, _, _| ());
     context
         .expect_broadcast()
         .times(1)
