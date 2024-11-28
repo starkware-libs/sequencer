877b80497c753c9365294b0c28097c04d21d8bd7
Itay Tsabary <itayt@starkware.co>
2024-11-28 12:00:25 +0200

    chore(starknet_sequencer_infra): add run_until utility fn
    commit-id:194a4b6c

---
+++ crates/infra_utils/src/run_until.rs
@@ -0,0 +1,64 @@
+use tokio::time::{sleep, Duration};
+
+use crate::tracing::DynamicLogger;
+
+#[cfg(test)]
+#[path = "run_until_test.rs"]
+mod run_until_test;
+
+/// Runs an asynchronous function until a condition is met or max attempts are reached.
+///
+/// # Arguments
+/// - `interval`: Time between each attempt (in milliseconds).
+/// - `max_attempts`: Maximum number of attempts.
+/// - `executable`: An asynchronous function to execute, which returns a value of type `T`.
+/// - `condition`: A closure that takes a value of type `T` and returns `true` if the condition is
+///   met.
+/// - `logger`: Optional trace logger.
+///
+/// # Returns
+/// - `Option<T>`: Returns `Some(value)` if the condition is met within the attempts, otherwise
+///   `None`.
+pub async fn run_until<T, F, C>(
+    interval: u64,
+    max_attempts: usize,
+    mut executable: F,
+    condition: C,
+    logger: Option<DynamicLogger>,
+) -> Option<T>
+where
+    T: Clone + Send + std::fmt::Debug + 'static,
+    F: FnMut() -> T + Send,
+    C: Fn(&T) -> bool + Send + Sync,
+{
+    for attempt in 1..=max_attempts {
+        let result = executable();
+
+        // Log attempt message.
+        if let Some(config) = &logger {
+            let attempt_message =
+                format!("Attempt {}/{}, Value {:?}", attempt, max_attempts, result);
+            config.log_message(&attempt_message);
+        }
+
+        // Check if the condition is met.
+        if condition(&result) {
+            if let Some(config) = &logger {
+                let success_message =
+                    format!("Condition met on attempt {}/{}", attempt, max_attempts);
+                config.log_message(&success_message);
+            }
+            return Some(result);
+        }
+
+        // Wait for the interval before the next attempt.
+        sleep(Duration::from_millis(interval)).await;
+    }
+
+    if let Some(config) = &logger {
+        let failure_message = format!("Condition not met after {} attempts.", max_attempts);
+        config.log_message(&failure_message);
+    }
+
+    None
+}
