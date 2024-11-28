760b8b7251fc537256156ccb86ecbb2be90f067d
Yonatan-Starkware <yonatan.k@starkware.co>
2024-11-28 18:18:04 +0200

    chore(blockifier): add syscalls_gas_costs struct

--- crates/blockifier/src/execution/syscalls/syscall_base.rs
+++ crates/blockifier/src/execution/syscalls/syscall_base.rs
@@ -272,7 +272,7 @@ impl<'state> SyscallHandlerBase<'state> {
         // TODO(Ori, 1/2/2024): Write an indicative expect message explaining why the conversion
         // works.
         let n_rounds_as_u64 = u64::try_from(n_rounds).expect("Failed to convert usize to u64.");
-        let gas_cost = n_rounds_as_u64 * self.context.gas_costs().keccak_round_cost_gas_cost;
+        let gas_cost = n_rounds_as_u64 * self.context.gas_costs().syscalls.keccak_round_cost;
 
         if gas_cost > *remaining_gas {
             let out_of_gas_error = Felt::from_hex(OUT_OF_GAS_ERROR)
