013ee1123f3e874165ad693e53c6c1e27d8fbe56
nadin-Starkware <nadin@starkware.co>
2024-11-28 14:42:57 +0200

    test: failing DataAvailabilityMode deserialization
    commit-id:9b85e989

--- crates/starknet_sequencer_infra/src/serde_utils_test.rs
+++ crates/starknet_sequencer_infra/src/serde_utils_test.rs
@@ -1,16 +1,27 @@
 use std::fmt::Debug;
 
 use serde::{Deserialize, Serialize};
+use starknet_api::core::{ContractAddress, Nonce};
+use starknet_api::data_availability::{DataAvailabilityMode};
+use starknet_api::rpc_transaction::RpcInvokeTransactionV3;
+use starknet_api::transaction::fields::{
+    AccountDeploymentData,
+    AllResourceBounds,
+    Calldata,
+    PaymasterData,
+    Tip,
+    TransactionSignature,
+};
 use starknet_types_core::felt::Felt;
 
 use crate::serde_utils::BincodeSerdeWrapper;
 
 fn test_generic_data_serde<T>(data: T)
 where
-    T: Serialize + for<'de> Deserialize<'de> + Debug + Clone + Copy + PartialEq,
+    T: Serialize + for<'de> Deserialize<'de> + Debug + Clone + PartialEq,
 {
     // Serialize and deserialize the data.
-    let encoded = BincodeSerdeWrapper::new(data).to_bincode().unwrap();
+    let encoded = BincodeSerdeWrapper::new(data.clone()).to_bincode().unwrap();
     let decoded = BincodeSerdeWrapper::<T>::from_bincode(&encoded).unwrap();
 
     // Assert that the data is the same after serialization and deserialization.
@@ -39,4 +50,27 @@ fn test_serde_struct_type() {
 fn test_serde_felt() {
     let data: Felt = Felt::ONE;
     test_generic_data_serde(data);
+}
+
+#[test]
+fn test_serde_rpc_invoke_tx() {
+    let tx = RpcInvokeTransactionV3 {
+        sender_address: ContractAddress::default(),
+        calldata: Calldata::default(),
+        signature: TransactionSignature::default(),
+        nonce: Nonce::default(),
+        resource_bounds: AllResourceBounds::default(),
+        tip: Tip::default(),
+        paymaster_data: PaymasterData::default(),
+        account_deployment_data: AccountDeploymentData::default(),
+        nonce_data_availability_mode: DataAvailabilityMode::L1,
+        fee_data_availability_mode: DataAvailabilityMode::L1,
+    };
+    test_generic_data_serde(tx);
+}
+
+
+#[test]
+fn test_serde_sender_tx_fields() {
+    test_generic_data_serde(DataAvailabilityMode::L1);
 }
