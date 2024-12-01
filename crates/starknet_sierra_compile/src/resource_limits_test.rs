327a1f1128d5becdb25a377c5f6c5bfeb4fc4f03
Avi Cohen <avi.cohen@starkware.co>
2024-12-01 22:08:40 +0200

    feat: allow limiting system resources in compilation processes

---
+++ crates/starknet_sierra_compile/src/resource_limits_test.rs
@@ -0,0 +1,43 @@
+use std::process::Command;
+use std::time::Instant;
+
+use rstest::rstest;
+
+use crate::resource_limits::ResourcesLimits;
+
+#[rstest]
+fn test_cpu_time_limit() {
+    let cpu_limit = 1; // 1 second
+    let cpu_time_rlimit = ResourcesLimits::new(Some(1), None, None);
+
+    let start = Instant::now();
+    let mut command = Command::new("bash");
+    command.args(["-c", "while true; do :; done;"]);
+    cpu_time_rlimit.apply(&mut command);
+    command.spawn().expect("Failed to start CPU consuming process").wait().unwrap();
+    assert!(start.elapsed().as_secs() <= 1);
+}
+
+#[rstest]
+fn test_memory_size_limit() {
+    let memory_limit = 100 * 1024; // 100 KB
+    let memory_size_rlimit = ResourcesLimits::new(None, None, Some(memory_limit));
+
+    let mut command = Command::new("bash");
+    command.args(["-c", "a=(); while true; do a+=0; done;"]);
+    memory_size_rlimit.apply(&mut command);
+    command.spawn().expect("Failed to start memory consuming process").wait().unwrap();
+}
+
+#[rstest]
+fn test_file_size_limit() {
+    let file_limit = 10; // 10 bytes
+    let file_size_rlimit = ResourcesLimits::new(None, Some(file_limit), None);
+
+    let mut command = Command::new("bash");
+    command.args(["-c", "echo 0 > /tmp/file.txt; while true; do echo 0 >> /tmp/file.txt; done;"]);
+    file_size_rlimit.apply(&mut command);
+    command.spawn().expect("Failed to start disk consuming process").wait().unwrap();
+    assert!(std::fs::metadata("/tmp/file.txt").unwrap().len() <= file_limit);
+    std::fs::remove_file("/tmp/file.txt").unwrap();
+}
