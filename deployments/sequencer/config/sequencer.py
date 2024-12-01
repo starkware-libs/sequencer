a1083eba58fca4b70ba83693d9c459f5d53ff501
alon.dotan <alon.dotan@starkware.co>
2024-12-01 11:53:15 +0200

    chore(deployment): support unified deployment config

--- deployments/sequencer/config/sequencer.py
+++ deployments/sequencer/config/sequencer.py
@@ -12,12 +12,16 @@ CONFIG_DIR = os.path.join(ROOT_DIR, "config/sequencer/")
 class SequencerDevConfig(Config):
     def __init__(self, mount_path: str, config_file_path: str = ""):
         super().__init__(
-            schema=json.loads(open(os.path.join(CONFIG_DIR, "default_config.json"), "r").read()),
-            config=json.loads(open(os.path.join(CONFIG_DIR, "presets", "config.json"), "r").read())
-            if not config_file_path
-            else json.loads(open(os.path.abspath(config_file_path)).read()),
+            global_config=json.loads(
+                open(os.path.join(CONFIG_DIR, "default_config.json"), "r").read()
+            ),
+            config=(
+                json.loads(open(os.path.join(CONFIG_DIR, "presets", "config.json"), "r").read())
+                if not config_file_path
+                else json.loads(open(os.path.abspath(config_file_path)).read())
+            ),
             mount_path=mount_path,
         )
 
     def validate(self):
-        jsonschema.validate(self.config, schema=self.schema)
+        jsonschema.validate(self.config, schema=self.global_config)
