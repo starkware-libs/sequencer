623a146b2240bec6537d4b65e53735342f7f26e9
Matan Markind <matan@starkware.co>
2024-12-02 11:06:15 +0200

    DO NOT SUBMIT -- changes for sequencer context to broadcast votes

--- crates/sequencing/papyrus_consensus_orchestrator/src/sequencer_consensus_context.rs
+++ crates/sequencing/papyrus_consensus_orchestrator/src/sequencer_consensus_context.rs
@@ -67,7 +67,6 @@ pub struct SequencerConsensusContext {
     proposal_id: u64,
     current_height: Option<BlockNumber>,
     current_round: Round,
-    network_broadcast_client: BroadcastTopicClient<ProposalPart>,
     // The active proposal refers to the proposal being validated at the current height/round.
     // Building proposals are not tracked as active, as consensus can't move on to the next
     // height/round until building is done. Context only works on proposals for the
@@ -75,17 +74,24 @@ pub struct SequencerConsensusContext {
     active_proposal: Option<(Arc<Notify>, JoinHandle<()>)>,
     // Stores proposals for future rounds until the round is reached.
     queued_proposals: BTreeMap<Round, (ValidationParams, oneshot::Sender<ProposalContentId>)>,
+    // Used to broadcast proposals to other consensus nodes.
+    // TODO(Guy) switch to the actual streaming struct.
+    proposal_streaming_client: BroadcastTopicClient<ProposalPart>,
+    // Used to broadcast votes to other consensus nodes.
+    vote_broadcast_client: BroadcastTopicClient<ConsensusMessage>,
 }
 
 impl SequencerConsensusContext {
     pub fn new(
         batcher: Arc<dyn BatcherClient>,
-        network_broadcast_client: BroadcastTopicClient<ProposalPart>,
+        proposal_streaming_client: BroadcastTopicClient<ProposalPart>,
+        vote_broadcast_client: BroadcastTopicClient<ConsensusMessage>,
         num_validators: u64,
     ) -> Self {
         Self {
             batcher,
-            network_broadcast_client,
+            proposal_streaming_client,
+            vote_broadcast_client,
             validators: (0..num_validators).map(ValidatorId::from).collect(),
             valid_proposals: Arc::new(Mutex::new(HeightToIdToContent::new())),
             proposal_id: 0,
@@ -141,11 +147,11 @@ impl ConsensusContext for SequencerConsensusContext {
             .await
             .expect("Failed to initiate proposal build");
         debug!("Broadcasting proposal init: {proposal_init:?}");
-        self.network_broadcast_client
+        self.proposal_streaming_client
             .broadcast_message(ProposalPart::Init(proposal_init.clone()))
             .await
             .expect("Failed to broadcast proposal init");
-        let broadcast_client = self.network_broadcast_client.clone();
+        let broadcast_client = self.proposal_streaming_client.clone();
         tokio::spawn(
             async move {
                 stream_build_proposal(
@@ -209,7 +215,8 @@ impl ConsensusContext for SequencerConsensusContext {
     }
 
     async fn broadcast(&mut self, message: ConsensusMessage) -> Result<(), ConsensusError> {
-        debug!("No-op broadcasting message: {message:?}");
+        debug!("Broadcasting message: {message:?}");
+        self.vote_broadcast_client.broadcast_message(message).await?;
         Ok(())
     }
 
