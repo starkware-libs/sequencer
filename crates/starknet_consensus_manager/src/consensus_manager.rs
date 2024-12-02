b110cc30394cacc9bb19b98a232593ac9cb6968a
guy-starkware <guy.n@starkware.co>
2024-12-02 04:26:57 -0800

    feat: allow a streamed proposal channel on top of existing one (#2262)

--- crates/starknet_consensus_manager/src/consensus_manager.rs
+++ crates/starknet_consensus_manager/src/consensus_manager.rs
@@ -6,12 +6,17 @@ use futures::channel::mpsc::{self, SendError};
 use futures::future::Ready;
 use futures::{SinkExt, StreamExt};
 use libp2p::PeerId;
+use papyrus_consensus::stream_handler::StreamHandler;
 use papyrus_consensus::types::{BroadcastConsensusMessageChannel, ConsensusError};
 use papyrus_consensus_orchestrator::sequencer_consensus_context::SequencerConsensusContext;
 use papyrus_network::gossipsub_impl::Topic;
-use papyrus_network::network_manager::{BroadcastTopicClient, NetworkManager};
+use papyrus_network::network_manager::{
+    BroadcastTopicChannels,
+    BroadcastTopicClient,
+    NetworkManager,
+};
 use papyrus_network_types::network_types::BroadcastedMessageMetadata;
-use papyrus_protobuf::consensus::{ConsensusMessage, ProposalPart};
+use papyrus_protobuf::consensus::{ConsensusMessage, ProposalPart, StreamMessage};
 use starknet_batcher_types::communication::SharedBatcherClient;
 use starknet_sequencer_infra::component_definitions::ComponentStarter;
 use starknet_sequencer_infra::errors::ComponentError;
@@ -21,7 +26,11 @@ use crate::config::ConsensusManagerConfig;
 
 // TODO(Dan, Guy): move to config.
 pub const BROADCAST_BUFFER_SIZE: usize = 100;
+
 pub const CONSENSUS_PROPOSALS_TOPIC: &str = "consensus_proposals";
+
+// TODO(guyn): remove this once we have integrated streaming.
+pub const NETWORK_TOPIC2: &str = "streamed_consensus_proposals";
 
 #[derive(Clone)]
 pub struct ConsensusManager {
@@ -37,15 +46,33 @@ impl ConsensusManager {
     pub async fn run(&self) -> Result<(), ConsensusError> {
         let mut network_manager =
             NetworkManager::new(self.config.consensus_config.network_config.clone(), None);
-        let mut proposals_broadcast_channels = network_manager
+
+        // TODO(guyn): remove this channel once we have integrated streaming.
+        let mut old_proposals_broadcast_channels = network_manager
             .register_broadcast_topic::<ProposalPart>(
                 Topic::new(CONSENSUS_PROPOSALS_TOPIC),
                 BROADCAST_BUFFER_SIZE,
             )
             .expect("Failed to register broadcast topic");
+
+        let proposals_broadcast_channels = network_manager
+            .register_broadcast_topic::<StreamMessage<ProposalPart>>(
+                Topic::new(NETWORK_TOPIC2),
+                BROADCAST_BUFFER_SIZE,
+            )
+            .expect("Failed to register broadcast topic");
+        let BroadcastTopicChannels {
+            broadcasted_messages_receiver: inbound_network_receiver,
+            broadcast_topic_client: outbound_network_sender,
+        } = proposals_broadcast_channels;
+
+        let (outbound_internal_sender, inbound_internal_receiver, mut stream_handler_task_handle) =
+            StreamHandler::get_channels(inbound_network_receiver, outbound_network_sender);
+
         let context = SequencerConsensusContext::new(
             Arc::clone(&self.batcher_client),
-            proposals_broadcast_channels.broadcast_topic_client.clone(),
+            old_proposals_broadcast_channels.broadcast_topic_client.clone(),
+            outbound_internal_sender,
             self.config.consensus_config.num_validators,
         );
 
@@ -57,6 +84,7 @@ impl ConsensusManager {
             self.config.consensus_config.consensus_delay,
             self.config.consensus_config.timeouts.clone(),
             create_fake_network_channels(),
+            inbound_internal_receiver,
             futures::stream::pending(),
         );
 
@@ -70,9 +98,12 @@ impl ConsensusManager {
             network_result = &mut network_handle => {
                 panic!("Consensus' network task finished unexpectedly: {:?}", network_result);
             }
+            stream_handler_result = &mut stream_handler_task_handle => {
+                panic!("Consensus' stream handler task finished unexpectedly: {:?}", stream_handler_result);
+            }
             _ = async {
                 while let Some(_broadcasted_message) =
-                    proposals_broadcast_channels.broadcasted_messages_receiver.next().await
+                    old_proposals_broadcast_channels.broadcasted_messages_receiver.next().await
                 {
                     // TODO(matan): pass receiver to consensus and sender to context.
                 }
