e460a618f2a9ad0a0d8c70c924ce65500e4eb222
Arni Hod <arnon@starkware.co>
2024-12-02 09:56:10 +0200

    chore(starknet_consensus_manager): add proposal init into validate proposal input

--- crates/sequencing/papyrus_consensus/src/manager_test.rs
+++ crates/sequencing/papyrus_consensus/src/manager_test.rs
@@ -67,9 +67,7 @@ mock! {
 
         async fn validate_proposal(
             &mut self,
-            height: BlockNumber,
-            round: Round,
-            proposer: ValidatorId,
+            init: ProposalInit,
             timeout: Duration,
             content: mpsc::Receiver<ProposalPart>
         ) -> oneshot::Receiver<(ProposalContentId, ProposalFin)>;
@@ -116,7 +114,7 @@ async fn send_proposal(
 fn expect_validate_proposal(context: &mut MockTestContext, block_hash: Felt) {
     context
         .expect_validate_proposal()
-        .return_once(move |_, _, _, _, _| {
+        .return_once(move |_, _, _| {
             let (block_sender, block_receiver) = oneshot::channel();
             block_sender
                 .send((
@@ -368,7 +366,7 @@ async fn test_timeouts() {
 
     let mut context = MockTestContext::new();
     context.expect_set_height_and_round().returning(move |_, _| ());
-    context.expect_validate_proposal().returning(move |_, _, _, _, _| {
+    context.expect_validate_proposal().returning(move |_, _, _| {
         let (block_sender, block_receiver) = oneshot::channel();
         block_sender
             .send((BlockHash(Felt::ONE), ProposalFin { proposal_content_id: BlockHash(Felt::ONE) }))
--- crates/sequencing/papyrus_consensus/src/types.rs
+++ crates/sequencing/papyrus_consensus/src/types.rs
@@ -77,9 +77,7 @@ pub trait ConsensusContext {
     ///   by ConsensusContext.
     async fn validate_proposal(
         &mut self,
-        height: BlockNumber,
-        round: Round,
-        proposer: ValidatorId,
+        init: ProposalInit,
         timeout: Duration,
         content: mpsc::Receiver<Self::ProposalPart>,
     ) -> oneshot::Receiver<(ProposalContentId, ProposalFin)>;
