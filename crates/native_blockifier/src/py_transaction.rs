c5e3b6feddb77a7b5eda4a0882491fb211047f6e
Aviv Greenburg <aviv.g@starkware.co>
2024-12-02 14:21:02 +0200

    chore(blockifier): invoke() declare() deploy_account() change ret val to api_tx

--- crates/native_blockifier/src/py_transaction.rs
+++ crates/native_blockifier/src/py_transaction.rs
@@ -138,22 +138,17 @@ pub fn py_tx(
         TransactionType::Declare => {
             let non_optional_py_class_info: PyClassInfo = optional_py_class_info
                 .expect("A class info must be passed in a Declare transaction.");
-            AccountTransaction {
-                tx: ExecutableTransaction::Declare(py_declare(tx, non_optional_py_class_info)?),
-                only_query: false,
-            }
-            .into()
+            let tx = ExecutableTransaction::Declare(py_declare(tx, non_optional_py_class_info)?);
+            AccountTransaction { tx, only_query: false }.into()
         }
-        TransactionType::DeployAccount => AccountTransaction {
-            tx: ExecutableTransaction::DeployAccount(py_deploy_account(tx)?),
-            only_query: false,
+        TransactionType::DeployAccount => {
+            let tx = ExecutableTransaction::DeployAccount(py_deploy_account(tx)?);
+            AccountTransaction { tx, only_query: false }.into()
         }
-        .into(),
-        TransactionType::InvokeFunction => AccountTransaction {
-            tx: ExecutableTransaction::Invoke(py_invoke_function(tx)?),
-            only_query: false,
+        TransactionType::InvokeFunction => {
+            let tx = ExecutableTransaction::Invoke(py_invoke_function(tx)?);
+            AccountTransaction { tx, only_query: false }.into()
         }
-        .into(),
         TransactionType::L1Handler => py_l1_handler(tx)?.into(),
     })
 }
