1236dca0f35c4d79d592e12ce68dc595d247b93d
AvivYossef-starkware <aviv.y@starkware.co>
2024-12-03 16:39:53 +0200

    feat(papyrus_storage): get casm and sierra

--- crates/blockifier/src/state/errors.rs
+++ crates/blockifier/src/state/errors.rs
@@ -1,6 +1,8 @@
+use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
 use cairo_vm::types::errors::program_errors::ProgramError;
 use num_bigint::{BigUint, TryFromBigIntError};
 use starknet_api::core::{ClassHash, ContractAddress};
+use starknet_api::state::SierraContractClass;
 use starknet_api::StarknetApiError;
 use thiserror::Error;
 
@@ -8,6 +10,8 @@ use crate::abi::constants;
 
 #[derive(Debug, Error)]
 pub enum StateError {
+    #[error("CASM and Sierra mismatch for class hash {:#064x}: {message}.", class_hash.0)]
+    CasmAndSierraMismatch { class_hash: ClassHash, message: String },
     #[error(transparent)]
     FromBigUint(#[from] TryFromBigIntError<BigUint>),
     #[error(
@@ -28,4 +32,26 @@ pub enum StateError {
     /// Represents all unexpected errors that may occur while reading from state.
     #[error("Failed to read from state: {0}.")]
     StateReadError(String),
+}
+
+/// Ensures that the CASM and Sierra classes are coupled - Meaning that they both exist or are
+/// missing. Returns a `CasmAndSierraMismatch` error when there is an inconsistency in their
+/// existence.
+pub fn couple_casm_and_sierra(
+    class_hash: ClassHash,
+    option_casm: Option<CasmContractClass>,
+    option_sierra: Option<SierraContractClass>,
+) -> Result<Option<(CasmContractClass, SierraContractClass)>, StateError> {
+    match (option_casm, option_sierra) {
+        (Some(casm), Some(sierra)) => Ok(Some((casm, sierra))),
+        (Some(_), None) => Err(StateError::CasmAndSierraMismatch {
+            class_hash,
+            message: "Class exists in CASM but not in Sierra".to_string(),
+        }),
+        (None, Some(_)) => Err(StateError::CasmAndSierraMismatch {
+            class_hash,
+            message: "Class exists in Sierra but not in CASM".to_string(),
+        }),
+        (None, None) => Ok(None),
+    }
 }
