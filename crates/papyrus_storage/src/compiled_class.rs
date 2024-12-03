572f15e67be1da5856691c57fbdafdec1b4c7591
AvivYossef-starkware <aviv.y@starkware.co>
2024-12-03 16:39:53 +0200

    feat(papyrus_storage): get casm and sierra

--- crates/papyrus_storage/src/compiled_class.rs
+++ crates/papyrus_storage/src/compiled_class.rs
@@ -50,7 +50,9 @@ use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
 use papyrus_proc_macros::latency_histogram;
 use starknet_api::block::BlockNumber;
 use starknet_api::core::ClassHash;
+use starknet_api::state::SierraContractClass;
 
+use crate::class::ClassStorageReader;
 use crate::db::serialization::VersionZeroWrapper;
 use crate::db::table_types::{SimpleTable, Table};
 use crate::db::{DbTransaction, TableHandle, TransactionKind, RW};
@@ -61,6 +63,12 @@ use crate::{FileHandlers, MarkerKind, MarkersTable, OffsetKind, StorageResult, S
 pub trait CasmStorageReader {
     /// Returns the Cairo assembly of a class given its Sierra class hash.
     fn get_casm(&self, class_hash: &ClassHash) -> StorageResult<Option<CasmContractClass>>;
+    /// Returns the Cairo assembly of a class and its Sierra contract class given its Sierra class
+    /// hash.
+    fn get_casm_and_sierra(
+        &self,
+        class_hash: &ClassHash,
+    ) -> StorageResult<Option<(CasmContractClass, SierraContractClass)>>;
     /// The block marker is the first block number that doesn't exist yet.
     ///
     /// Note: If the last blocks don't contain any declared classes, the marker will point at the
@@ -83,6 +91,16 @@ impl<Mode: TransactionKind> CasmStorageReader for StorageTxn<'_, Mode> {
         let casm_table = self.open_table(&self.tables.casms)?;
         let casm_location = casm_table.get(&self.txn, class_hash)?;
         casm_location.map(|location| self.file_handlers.get_casm_unchecked(location)).transpose()
+    }
+
+    fn get_casm_and_sierra(
+        &self,
+        class_hash: &ClassHash,
+    ) -> StorageResult<Option<(CasmContractClass, SierraContractClass)>> {
+        match (self.get_casm(class_hash)?, self.get_class(class_hash)?) {
+            (Some(casm), Some(sierra)) => Ok(Some((casm, sierra))),
+            (_, _) => Ok(None),
+        }
     }
 
     fn get_compiled_class_marker(&self) -> StorageResult<BlockNumber> {
