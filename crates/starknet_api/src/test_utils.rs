611737cbc68a3bb18a4762d128085ef1e8ecdfbe
Itay Tsabary <itayt@starkware.co>
2024-12-04 15:06:56 +0200

    chore(starknet_api): rely on env::current_dir() instead of CARGO_MANIFEST_DIR
    commit-id:301ed4eb

--- crates/starknet_api/src/test_utils.rs
+++ crates/starknet_api/src/test_utils.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::path::{Path, PathBuf};
 
-use infra_utils::path::cargo_manifest_dir;
 use serde::{Deserialize, Serialize};
 use serde_json::to_string_pretty;
 use starknet_types_core::felt::Felt;
@@ -21,7 +20,7 @@ pub mod l1_handler;
 /// directory has a `resources` folder. The value for file_path should be the path to the required
 /// file in the folder "resources".
 pub fn path_in_resources<P: AsRef<Path>>(file_path: P) -> PathBuf {
-    cargo_manifest_dir().unwrap().join("resources").join(file_path)
+    std::env::current_dir().unwrap().join("resources").join(file_path)
 }
 
 /// Reads from the directory containing the manifest at run time, same as current working directory.
