4d59477eed8bb17f4cb34b03dbebe5fa422d9cca
Itay Tsabary <itayt@starkware.co>
2024-12-05 13:29:58 +0200

    chore(starknet_sierra_compile): avoid using OUT_DIR in run time
    commit-id:cd6fba29

--- crates/starknet_sierra_compile/src/paths.rs
+++ crates/starknet_sierra_compile/src/paths.rs
@@ -2,18 +2,7 @@
 // not contain functionality that is available in only in one of these modes. Specifically, it
 // must avoid relying on env variables such as 'CARGO_*' or 'OUT_DIR'.
 
-fn out_dir() -> std::path::PathBuf {
-    std::path::Path::new(
-        &std::env::var("OUT_DIR").expect("Failed to get the OUT_DIR environment variable"),
-    )
-    .to_path_buf()
-}
-
-/// Get the crate's `OUT_DIR` and navigate up to reach the `target/BUILD_FLAVOR` directory.
-/// This directory is shared across all crates in this project.
-fn target_dir() -> std::path::PathBuf {
-    let out_dir = out_dir();
-
+fn target_dir(out_dir: std::path::PathBuf) -> std::path::PathBuf {
     out_dir
         .ancestors()
         .nth(3)
@@ -21,20 +10,15 @@ fn target_dir() -> std::path::PathBuf {
         .to_path_buf()
 }
 
-fn shared_folder_dir() -> std::path::PathBuf {
-    target_dir().join("shared_executables")
+fn shared_folder_dir(out_dir: std::path::PathBuf) -> std::path::PathBuf {
+    target_dir(out_dir).join("shared_executables")
 }
 
-pub fn binary_path(binary_name: &str) -> std::path::PathBuf {
-    shared_folder_dir().join(binary_name)
-}
-
-#[cfg(feature = "cairo_native")]
-pub fn output_file_path() -> String {
-    out_dir().join("output.so").to_str().unwrap().into()
+pub fn binary_path(out_dir: std::path::PathBuf, binary_name: &str) -> std::path::PathBuf {
+    shared_folder_dir(out_dir).join(binary_name)
 }
 
 #[cfg(feature = "cairo_native")]
-pub fn repo_root_dir() -> std::path::PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("../..").to_path_buf()
+pub fn output_file_path(out_dir: std::path::PathBuf) -> String {
+    out_dir.join("output.so").to_str().unwrap().into()
 }
