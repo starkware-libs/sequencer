16d2b934b0187129844d92c39accc1900b64588e
Itay Tsabary <itayt@starkware.co>
2024-12-05 15:50:43 +0200

    chore: cleanups of CARGO_MANIFEST_DIR env var
    commit-id:c96f2d88

--- crates/infra_utils/src/path.rs
+++ crates/infra_utils/src/path.rs
@@ -1,5 +1,4 @@
-use std::path::{Path, PathBuf};
-use std::sync::LazyLock;
+use std::path::PathBuf;
 use std::{env, fs};
 
 #[cfg(test)]
@@ -7,13 +6,6 @@ use std::{env, fs};
 mod path_test;
 
 // TODO(tsabary): wrap path-related env::* invocations in the repo as utility functions
-static PATH_TO_CARGO_MANIFEST_DIR: LazyLock<Option<PathBuf>> =
-    LazyLock::new(|| env::var("CARGO_MANIFEST_DIR").ok().map(|dir| Path::new(&dir).into()));
-
-// TODO(Tsabary): should not be public. Use a getter instead.
-pub fn cargo_manifest_dir() -> Option<PathBuf> {
-    PATH_TO_CARGO_MANIFEST_DIR.clone()
-}
 
 // TODO(Tsabary/ Arni): consolidate with other get_absolute_path functions.
 /// Resolves a relative path from the project root directory and returns its absolute path.
@@ -40,10 +32,7 @@ pub fn project_path() -> Result<PathBuf, std::io::Error> {
 }
 
 fn path_of_project_root() -> PathBuf {
-    cargo_manifest_dir()
-        // Attempt to get the `CARGO_MANIFEST_DIR` environment variable and convert it to `PathBuf`.
-        // Ascend two directories ("../..") to get to the project root.
-        .map(|dir| dir.join("../.."))
-        // If `CARGO_MANIFEST_DIR` isn't set, fall back to the current working directory
-        .unwrap_or(env::current_dir().expect("Failed to get current directory"))
+    // Ascend two directories to get to the project root. This assumes that the project root is two
+    // directories above the current file.
+    PathBuf::from(env!("CARGO_MANIFEST_DIR")).ancestors().nth(2).expect("Cannot navigate up").into()
 }
