937947e330cf24cef8333a410f95c7951bc141ef
Yoav Gross <yoav@starkware.co>
2024-12-05 13:19:59 +0200

    feat(blockifier): iterate over aliases on the state diff

--- crates/blockifier/src/state/stateful_compression.rs
+++ crates/blockifier/src/state/stateful_compression.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 
 use starknet_api::core::{ContractAddress, PatriciaKey};
 use starknet_api::state::StorageKey;
 use starknet_types_core::felt::Felt;
 
-use super::cached_state::{CachedState, StorageEntry};
+use super::cached_state::{CachedState, StateMaps, StorageEntry};
 use super::state_api::{StateReader, StateResult};
 
 #[cfg(test)]
@@ -18,9 +18,47 @@ type AliasKey = StorageKey;
 const ALIAS_CONTRACT_ADDRESS: ContractAddress = ContractAddress::new(Felt::TWO);
 // The storage key of the alias counter in the alias contract.
 const ALIAS_COUNTER_STORAGE_KEY: StorageKey = StorageKey(PatriciaKey::new_unchecked(Felt::ZERO));
+// The maximal contract address for which aliases are not used and all keys are serialized as is,
+// without compression.
+const MAX_NON_COMPRESSED_CONTRACT_ADDRESS: ContractAddress =
+    ContractAddress::new(Felt::from_hex_unchecked("0xf"));
 // The minimal value for a key to be allocated an alias. Smaller keys are serialized as is (their
 // alias is identical to the key).
 const MIN_VALUE_FOR_ALIAS_ALLOC: Felt = Felt::from_hex_unchecked("0x80");
+
+/// Allocates aliases for the new addresses and storage keys in the alias contract.
+/// Iterates over the addresses in ascending order. For each address, sets an alias for the new
+/// storage keys (in ascending order) and for the address itself.
+pub fn state_diff_with_allocating_aliases<S: StateReader>(
+    state: &mut CachedState<S>,
+) -> StateResult<StateMaps> {
+    let mut state_diff = state.to_state_diff()?.state_maps;
+
+    // Collect the addresses and the storage keys that need aliases.
+    let addresses: BTreeSet<ContractAddress> =
+        state_diff.get_modified_contracts().into_iter().collect();
+    let mut sorted_storage_keys = HashMap::new();
+    for (address, storage_key) in state_diff.storage.keys() {
+        if address > &MAX_NON_COMPRESSED_CONTRACT_ADDRESS {
+            sorted_storage_keys.entry(address).or_insert_with(BTreeSet::new).insert(storage_key);
+        }
+    }
+
+    // Iterate over the addresses and the storage keys and update the aliases.
+    let mut alias_updater = AliasUpdater::new(state)?;
+    for address in addresses {
+        if let Some(&ref storage_keys) = sorted_storage_keys.get(&address) {
+            for key in storage_keys {
+                alias_updater.insert_alias(key)?;
+            }
+        }
+        alias_updater.insert_alias(&StorageKey(address.0))?;
+    }
+
+    let alias_storage_updates = alias_updater.finalize_updates();
+    state_diff.storage.extend(alias_storage_updates);
+    Ok(state_diff)
+}
 
 /// Generate updates for the alias contract with the new keys.
 struct AliasUpdater<'a, S: StateReader> {
