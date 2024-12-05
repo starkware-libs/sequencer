dc13f873ad7ca2426434f9d564b6f96016017e7e
Itay Tsabary <itayt@starkware.co>
2024-12-05 13:01:00 +0200

    chore(starknet_sierra_compile): set runtime-accessible out_dir env var
    commit-id:539f16db

--- crates/starknet_sierra_compile/build.rs
+++ crates/starknet_sierra_compile/build.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("cargo:rerun-if-changed=../../Cargo.lock");
     println!("cargo:rerun-if-changed=build.rs");
 
+    set_run_time_out_dir_env_var();
     install_starknet_sierra_compile();
     #[cfg(feature = "cairo_native")]
     install_starknet_native_compile();
@@ -117,3 +118,11 @@ fn install_compiler_binary(binary_name: &str, required_version: &str, cargo_inst
 
     println!("Successfully set executable file: {:?}", binary_path.display());
 }
+
+// Sets the `RUNTIME_ACCESSIBLE_OUT_DIR` environment variable to the `OUT_DIR` value, which will be
+// available only after the build is completed. Most importantly, it is available during runtime.
+fn set_run_time_out_dir_env_var() {
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR is not set");
+    println!("cargo:rustc-env={}={}", RUNTIME_ACCESSIBLE_OUT_DIR_ENV_VAR_NAME, out_dir);
+}
+
