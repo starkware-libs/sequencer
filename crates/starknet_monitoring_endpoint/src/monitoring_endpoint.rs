9786f69795b60916354706705de9a78b004b830d
Dan Brownstein <dan@starkware.co>
2024-12-05 11:53:49 +0200

    feat(starknet_monitoring_endpoint): add metrics endpoint

--- crates/starknet_monitoring_endpoint/src/monitoring_endpoint.rs
+++ crates/starknet_monitoring_endpoint/src/monitoring_endpoint.rs
@@ -2,9 +2,11 @@ use std::any::type_name;
 use std::net::SocketAddr;
 
 use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{async_trait, Router, Server};
 use hyper::Error;
+use metrics_exporter_prometheus::PrometheusHandle;
 use starknet_sequencer_infra::component_definitions::ComponentStarter;
 use starknet_sequencer_infra::errors::ComponentError;
 use tracing::{info, instrument};
@@ -19,15 +21,17 @@ pub(crate) const MONITORING_PREFIX: &str = "monitoring";
 pub(crate) const ALIVE: &str = "alive";
 pub(crate) const READY: &str = "ready";
 pub(crate) const VERSION: &str = "nodeVersion";
+pub(crate) const METRICS: &str = "metrics";
 
 pub struct MonitoringEndpoint {
     config: MonitoringEndpointConfig,
     version: &'static str,
+    prometheus_handle: Option<PrometheusHandle>,
 }
 
 impl MonitoringEndpoint {
     pub fn new(config: MonitoringEndpointConfig, version: &'static str) -> Self {
-        MonitoringEndpoint { config, version }
+        MonitoringEndpoint { config, version, prometheus_handle: None }
     }
 
     #[instrument(
@@ -41,13 +45,13 @@ impl MonitoringEndpoint {
         let MonitoringEndpointConfig { ip, port } = self.config;
         let endpoint_addr = SocketAddr::new(ip, port);
 
-        let app = self.app();
+        let app = self.app(self.prometheus_handle.clone());
         info!("MonitoringEndpoint running using socket: {}", endpoint_addr);
 
         Server::bind(&endpoint_addr).serve(app.into_make_service()).await
     }
 
-    fn app(&self) -> Router {
+    fn app(&self, prometheus_handle: Option<PrometheusHandle>) -> Router {
         let version = self.version.to_string();
 
         Router::new()
@@ -62,6 +66,10 @@ impl MonitoringEndpoint {
             .route(
                 format!("/{MONITORING_PREFIX}/{VERSION}").as_str(),
                 get(move || async { version }),
+            )
+            .route(
+                format!("/{MONITORING_PREFIX}/{METRICS}").as_str(),
+                get(move || metrics(prometheus_handle)),
             )
     }
 }
@@ -78,5 +86,16 @@ impl ComponentStarter for MonitoringEndpoint {
     async fn start(&mut self) -> Result<(), ComponentError> {
         info!("Starting component {}.", type_name::<Self>());
         self.run().await.map_err(|_| ComponentError::InternalComponentError)
+    }
+}
+
+/// Returns prometheus metrics.
+/// In case the node doesn’t collect metrics returns an empty response with status code 405: method
+/// not allowed.
+#[instrument(level = "debug", ret, skip(prometheus_handle))]
+async fn metrics(prometheus_handle: Option<PrometheusHandle>) -> Response {
+    match prometheus_handle {
+        Some(handle) => handle.render().into_response(),
+        None => StatusCode::METHOD_NOT_ALLOWED.into_response(),
     }
 }
