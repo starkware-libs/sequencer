93e133d89935de7a53275ee1575e85fc64294d0a
Yonatan Iluz <yonatan@starkware.co>
2024-12-09 15:21:46 +0200

    chore(native_blockifier): remove unused resource from actual_resources

--- crates/native_blockifier/src/py_block_executor.rs
+++ crates/native_blockifier/src/py_block_executor.rs
@@ -103,24 +103,6 @@ impl ThinTransactionExecutionInfo {
                 .iter()
                 .map(|(builtin, value)| (builtin.to_str_with_suffix().to_string(), *value)),
         );
-        // TODO(Yoni) remove these since we pass the gas vector in separate.
-        resources.extend(HashMap::from([
-            (
-                abi_constants::L1_GAS_USAGE.to_string(),
-                usize_from_u64(l1_gas.0)
-                    .expect("This conversion should not fail as the value is a converted usize."),
-            ),
-            (
-                abi_constants::BLOB_GAS_USAGE.to_string(),
-                usize_from_u64(l1_data_gas.0)
-                    .expect("This conversion should not fail as the value is a converted usize."),
-            ),
-            (
-                abi_constants::L2_GAS_USAGE.to_string(),
-                usize_from_u64(l2_gas.0)
-                    .expect("This conversion should not fail as the value is a converted usize."),
-            ),
-        ]));
         *resources.get_mut(abi_constants::N_STEPS_RESOURCE).unwrap_or(&mut 0) +=
             receipt.resources.computation.n_reverted_steps;
 
