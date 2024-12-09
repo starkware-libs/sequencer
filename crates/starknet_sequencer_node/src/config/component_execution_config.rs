b1056aa9725620c8bc4efda3a025597c06282cad
Itay Tsabary <itayt@starkware.co>
2024-12-09 10:09:25 +0200

    chore(starknet_sequencer_node): add active component execution config
    commit-id:794b2125

--- crates/starknet_sequencer_node/src/config/component_execution_config.rs
+++ crates/starknet_sequencer_node/src/config/component_execution_config.rs
@@ -69,6 +69,29 @@ impl Default for ReactiveComponentExecutionConfig {
     }
 }
 
+/// Active component configuration.
+#[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
+pub struct ActiveComponentExecutionConfig {
+    pub execution_mode: ActiveComponentExecutionMode,
+}
+
+impl SerializeConfig for ActiveComponentExecutionConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from_iter([ser_param(
+            "execution_mode",
+            &self.execution_mode,
+            "The component execution mode.",
+            ParamPrivacyInput::Public,
+        )])
+    }
+}
+
+impl Default for ActiveComponentExecutionConfig {
+    fn default() -> Self {
+        Self { execution_mode: ActiveComponentExecutionMode::Enabled }
+    }
+}
+
 /// Specific components default configurations.
 impl ReactiveComponentExecutionConfig {
     pub fn gateway_default_config() -> Self {
