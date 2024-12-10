1b9a550fef84c115c111924f5fc7d0b0eb5117f2
Alon Lukatch <alon.l@starkware.co>
2024-12-10 15:21:27 +0200

    feat(starknet_state_sync): pass new internal blocks from state sync to p2p sync client

--- crates/starknet_state_sync/src/lib.rs
+++ crates/starknet_state_sync/src/lib.rs
@@ -2,6 +2,8 @@ pub mod config;
 pub mod runner;
 
 use async_trait::async_trait;
+use futures::channel::mpsc::{channel, Sender};
+use futures::SinkExt;
 use papyrus_storage::body::BodyStorageReader;
 use papyrus_storage::state::StateStorageReader;
 use papyrus_storage::StorageReader;
@@ -13,18 +15,23 @@ use starknet_state_sync_types::communication::{
     StateSyncResponse,
     StateSyncResult,
 };
+use starknet_state_sync_types::errors::StateSyncError;
 use starknet_state_sync_types::state_sync_types::SyncBlock;
 
 use crate::config::StateSyncConfig;
 use crate::runner::StateSyncRunner;
 
+const BUFFER_SIZE: usize = 100000;
+
 pub fn create_state_sync_and_runner(config: StateSyncConfig) -> (StateSync, StateSyncRunner) {
-    let (state_sync_runner, storage_reader) = StateSyncRunner::new(config);
-    (StateSync { storage_reader }, state_sync_runner)
+    let (new_block_sender, new_block_receiver) = channel(BUFFER_SIZE);
+    let (state_sync_runner, storage_reader) = StateSyncRunner::new(config, new_block_receiver);
+    (StateSync { storage_reader, new_block_sender }, state_sync_runner)
 }
 
 pub struct StateSync {
     storage_reader: StorageReader,
+    new_block_sender: Sender<(BlockNumber, SyncBlock)>,
 }
 
 // TODO(shahak): Have StateSyncRunner call StateSync instead of the opposite once we stop supporting
@@ -36,8 +43,13 @@ impl ComponentRequestHandler<StateSyncRequest, StateSyncResponse> for StateSync 
             StateSyncRequest::GetBlock(block_number) => {
                 StateSyncResponse::GetBlock(self.get_block(block_number))
             }
-            StateSyncRequest::AddNewBlock(_block_number, _sync_block) => {
-                todo!()
+            StateSyncRequest::AddNewBlock(block_number, sync_block) => {
+                StateSyncResponse::AddNewBlock(
+                    self.new_block_sender
+                        .send((block_number, sync_block))
+                        .await
+                        .map_err(|_| StateSyncError::P2PSyncClientError),
+                )
             }
         }
     }
