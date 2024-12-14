ad682ac8f73828d1fe1d6847400c2e52f415a3a5
fuder.eth <139509124+vtjl10@users.noreply.github.com>
2024-12-14 17:31:40 +0100

    Update mod.rs

--- crates/papyrus_network/src/network_manager/mod.rs
+++ crates/papyrus_network/src/network_manager/mod.rs
@@ -629,7 +629,7 @@ impl NetworkManager {
         } = config;
 
         let listen_addresses = vec![
-            // TODO: uncomment once quic transpot works.
+            // TODO: uncomment once quic transport works.
             // format!("/ip4/0.0.0.0/udp/{quic_port}/quic-v1"),
             format!("/ip4/0.0.0.0/tcp/{tcp_port}"),
         ];
