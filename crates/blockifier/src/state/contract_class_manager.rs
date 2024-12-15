4265921587945ee4992eac1152c7237d9d2f5952
Aviv Greenburg <aviv.g@starkware.co>
2024-12-15 19:35:18 +0200

    chore(blockifier): create unit tests for contract_class_manager

--- crates/blockifier/src/state/contract_class_manager.rs
+++ crates/blockifier/src/state/contract_class_manager.rs
@@ -30,6 +30,9 @@ use crate::state::global_cache::{CachedCasm, ContractCaches};
 
 pub const DEFAULT_COMPILATION_REQUEST_CHANNEL_SIZE: usize = 1000;
 
+#[cfg(all(test, feature = "cairo_native"))]
+#[path = "contract_class_manager_test.rs"]
+mod contract_class_manager_test;
 /// Represents a request to compile a sierra contract class to a native compiled class.
 ///
 /// # Fields:
@@ -65,7 +68,6 @@ impl ContractClassManager {
     /// 2. `config.run_cairo_native` is `false`.
     /// 3. `config.wait_on_native_compilation` is `true`.
     pub fn start(config: ContractClassManagerConfig) -> ContractClassManager {
-        // TODO(Avi, 15/12/2024): Add the size of the channel to the config.
         let contract_caches = ContractCaches::new(config.contract_cache_size);
         #[cfg(not(feature = "cairo_native"))]
         return ContractClassManager { contract_caches };
@@ -126,20 +128,29 @@ impl ContractClassManager {
         }
 
         let sender = self.sender.as_ref().expect("Compilation channel not available.");
+        let class_hash = request.0;
         // TODO(Avi, 15/12/2024): Check for duplicated requests.
-        sender.try_send(request).unwrap_or_else(|err| match err {
-            TrySendError::Full((class_hash, _, _)) => {
-                log::error!(
-                    "Compilation request channel is full (size: {}). Compilation request for \
-                     class hash {} was not sent.",
-                    self.cairo_native_run_config.channel_size,
+        match sender.try_send(request) {
+            Ok(_) => {
+                log::info!(
+                    "Compilation request with class hash: {} was sent successfully.",
                     class_hash
-                )
+                );
             }
-            TrySendError::Disconnected(_) => {
-                panic!("Compilation request channel is closed.")
-            }
-        });
+            Err(err) => match err {
+                TrySendError::Full((class_hash, _, _)) => {
+                    log::error!(
+                        "Compilation request channel is full (size: {}). Compilation request for \
+                         class hash {} was not sent.",
+                        self.cairo_native_run_config.channel_size,
+                        class_hash
+                    )
+                }
+                TrySendError::Disconnected(_) => {
+                    panic!("Compilation request channel is closed.")
+                }
+            },
+        };
     }
 
     /// Returns the native compiled class for the given class hash, if it exists in cache.
@@ -205,6 +216,10 @@ fn process_compilation_request(
     let (class_hash, sierra, casm) = compilation_request;
     if contract_caches.get_native(&class_hash).is_some() {
         // The contract class is already compiled to native - skip the compilation.
+        log::debug!(
+            "Contract class with hash {} is already compiled to native. Skipping compilation.",
+            class_hash
+        );
         return;
     }
     let sierra_for_compilation = into_contract_class_for_compilation(sierra.as_ref());
