01f3e55e72aea37f48dc92dc9721d01187983fb2
AvivYossef-starkware <aviv.y@starkware.co>
2024-12-15 17:11:41 +0200

    chore(papyrus_execution):  get versioned contract class

--- crates/papyrus_execution/src/execution_utils.rs
+++ crates/papyrus_execution/src/execution_utils.rs
@@ -6,6 +6,7 @@ use blockifier::execution::contract_class::{
     CompiledClassV0,
     CompiledClassV1,
     RunnableCompiledClass,
+    VersionedRunnableCompiledClass,
 };
 use blockifier::state::cached_state::{CachedState, CommitmentStateDiff, MutRefState};
 use blockifier::state::state_api::StateReader;
@@ -19,6 +20,7 @@ use papyrus_storage::state::StateStorageReader;
 use papyrus_storage::{StorageError, StorageResult, StorageTxn};
 // Expose the tool for creating entry point selectors from function names.
 pub use starknet_api::abi::abi_utils::selector_from_name;
+use starknet_api::contract_class::SierraVersion;
 use starknet_api::core::{ClassHash, ContractAddress, Nonce};
 use starknet_api::state::{StateNumber, StorageKey, ThinStateDiff};
 use starknet_types_core::felt::Felt;
@@ -43,6 +45,8 @@ pub(crate) enum ExecutionUtilsError {
     StorageError(#[from] StorageError),
     #[error("Casm table not fully synced")]
     CasmTableNotSynced,
+    #[error(transparent)]
+    SierraValidationError(#[from] starknet_api::StarknetApiError),
 }
 
 /// Returns the execution config from the config file.
@@ -55,20 +59,25 @@ impl TryFrom<PathBuf> for ExecutionConfig {
     }
 }
 
-pub(crate) fn get_contract_class(
+pub(crate) fn get_versioned_contract_class(
     txn: &StorageTxn<'_, RO>,
     class_hash: &ClassHash,
     state_number: StateNumber,
-) -> Result<Option<RunnableCompiledClass>, ExecutionUtilsError> {
+) -> Result<Option<VersionedRunnableCompiledClass>, ExecutionUtilsError> {
     match txn.get_state_reader()?.get_class_definition_block_number(class_hash)? {
         Some(block_number) if state_number.is_before(block_number) => return Ok(None),
         Some(_block_number) => {
-            let Some(casm) = txn.get_casm(class_hash)? else {
+            let (Some(casm), Some(sierra)) = txn.get_casm_and_sierra(class_hash)? else {
                 return Err(ExecutionUtilsError::CasmTableNotSynced);
             };
-            return Ok(Some(RunnableCompiledClass::V1(
+            let runnable_compiled_class = RunnableCompiledClass::V1(
                 CompiledClassV1::try_from(casm).map_err(ExecutionUtilsError::ProgramError)?,
-            )));
+            );
+            let sierra_version = SierraVersion::extract_from_program(&sierra.sierra_program)?;
+            return Ok(Some(VersionedRunnableCompiledClass::Cairo1((
+                runnable_compiled_class,
+                sierra_version,
+            ))));
         }
         None => {}
     };
@@ -78,9 +87,9 @@ pub(crate) fn get_contract_class(
     else {
         return Ok(None);
     };
-    Ok(Some(RunnableCompiledClass::V0(
+    Ok(Some(VersionedRunnableCompiledClass::Cairo0(RunnableCompiledClass::V0(
         CompiledClassV0::try_from(deprecated_class).map_err(ExecutionUtilsError::ProgramError)?,
-    )))
+    ))))
 }
 
 /// Given an ExecutableTransactionInput, returns a function that will convert the corresponding
