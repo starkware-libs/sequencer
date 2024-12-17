606be3e03e3b5830020946c9c19bcd3268544ef5
Alon Haramati <alonh@starkware.co>
2024-12-17 10:26:17 +0200

    test(starknet_integration_tests): add second node

--- crates/starknet_integration_tests/src/integration_test_setup.rs
+++ crates/starknet_integration_tests/src/integration_test_setup.rs
@@ -1,14 +1,22 @@
 use std::net::SocketAddr;
 use std::path::PathBuf;
 
-use mempool_test_utils::starknet_api_test_utils::MultiAccountTransactionGenerator;
+use blockifier::context::ChainInfo;
+use mempool_test_utils::starknet_api_test_utils::{Contract, MultiAccountTransactionGenerator};
+use papyrus_network::network_manager::BroadcastTopicChannels;
+use papyrus_protobuf::consensus::{ProposalPart, StreamMessage};
 use papyrus_storage::StorageConfig;
+use starknet_api::rpc_transaction::RpcTransaction;
+use starknet_api::transaction::TransactionHash;
+use starknet_consensus_manager::config::ConsensusManagerConfig;
 use starknet_http_server::config::HttpServerConfig;
 use starknet_http_server::test_utils::HttpTestClient;
+use starknet_mempool_p2p::config::MempoolP2pConfig;
 use starknet_monitoring_endpoint::config::MonitoringEndpointConfig;
 use starknet_monitoring_endpoint::test_utils::IsAliveClient;
 use starknet_sequencer_infra::test_utils::AvailablePorts;
 use tempfile::{tempdir, TempDir};
+use tracing::instrument;
 
 use crate::config_utils::dump_config_file_changes;
 use crate::state_reader::{spawn_test_rpc_state_reader_with_socket, StorageTestSetup};
@@ -19,12 +27,89 @@ use crate::utils::{
     create_mempool_p2p_configs,
 };
 
-const SEQUENCER_INDEX: usize = 0;
-const SEQUENCER_INDICES: [usize; 1] = [SEQUENCER_INDEX];
+const SEQUENCER_0: usize = 0;
+const SEQUENCER_1: usize = 1;
+const SEQUENCER_INDICES: [usize; 2] = [SEQUENCER_0, SEQUENCER_1];
 
 pub struct IntegrationTestSetup {
+    pub sequencer_0: IntegrationSequencerSetup,
+    pub sequencer_1: IntegrationSequencerSetup,
+
+    // TODO: To validate test results instead of reading storage - delete this and use monitoring
+    // or use this.
+    // Channels for consensus proposals, used for validating test results.
+    pub consensus_proposals_channels: BroadcastTopicChannels<StreamMessage<ProposalPart>>,
+}
+
+impl IntegrationTestSetup {
+    pub async fn new_from_tx_generator(
+        tx_generator: &MultiAccountTransactionGenerator,
+        test_unique_index: u16,
+    ) -> Self {
+        let mut available_ports = AvailablePorts::new(test_unique_index, 0);
+        let chain_info = create_chain_info();
+        let accounts = tx_generator.accounts();
+
+        let (mut consensus_manager_configs, consensus_proposals_channels) =
+            create_consensus_manager_configs_and_channels(
+                SEQUENCER_INDICES.len(),
+                &mut available_ports,
+            );
+        let mut mempool_p2p_configs = create_mempool_p2p_configs(
+            SEQUENCER_INDICES.len(),
+            chain_info.chain_id.clone(),
+            &mut available_ports,
+        );
+
+        let sequencer_0_consensus_manager_config = consensus_manager_configs.remove(0);
+        let sequencer_0_mempool_p2p_config = mempool_p2p_configs.remove(0);
+        let sequencer_0 = IntegrationSequencerSetup::new(
+            accounts.clone(),
+            SEQUENCER_0,
+            chain_info.clone(),
+            sequencer_0_consensus_manager_config,
+            sequencer_0_mempool_p2p_config,
+            &mut available_ports,
+        )
+        .await;
+
+        let sequencer_1_consensus_manager_config = consensus_manager_configs.remove(0);
+        let sequencer_1_mempool_p2p_config = mempool_p2p_configs.remove(0);
+        let sequencer_1 = IntegrationSequencerSetup::new(
+            accounts.clone(),
+            SEQUENCER_1,
+            chain_info.clone(),
+            sequencer_1_consensus_manager_config,
+            sequencer_1_mempool_p2p_config,
+            &mut available_ports,
+        )
+        .await;
+
+        Self { sequencer_0, sequencer_1, consensus_proposals_channels }
+    }
+
+    pub async fn await_alive(&self, interval: u64, max_attempts: usize) {
+        self.sequencer_0
+            .is_alive_test_client
+            .await_alive(interval, max_attempts)
+            .await
+            .expect("Node 0 should be alive.");
+
+        self.sequencer_1
+            .is_alive_test_client
+            .await_alive(interval, max_attempts)
+            .await
+            .expect("Node 1 should be alive.");
+    }
+}
+
+pub struct IntegrationSequencerSetup {
+    /// Used to differentiate between different sequencer nodes.
+    pub sequencer_index: usize,
+
     // Client for adding transactions to the sequencer node.
     pub add_tx_http_client: HttpTestClient,
+
     // Client for checking liveness of the sequencer node.
     pub is_alive_test_client: IsAliveClient,
     // Path to the node configuration file.
@@ -33,8 +118,6 @@ pub struct IntegrationTestSetup {
     pub batcher_storage_config: StorageConfig,
     // Storage reader for the state sync.
     pub state_sync_storage_config: StorageConfig,
-    // Available ports for the test.
-    pub available_ports: AvailablePorts,
     // Handlers for the storage and config files, maintained so the files are not deleted. Since
     // these are only maintained to avoid dropping the handlers, private visibility suffices, and
     // as such, the '#[allow(dead_code)]' attributes are used to suppress the warning.
@@ -48,16 +131,18 @@ pub struct IntegrationTestSetup {
     state_sync_storage_handle: TempDir,
 }
 
-impl IntegrationTestSetup {
-    pub async fn new_from_tx_generator(
-        tx_generator: &MultiAccountTransactionGenerator,
-        test_unique_index: u16,
+impl IntegrationSequencerSetup {
+    #[instrument(skip(accounts, chain_info, consensus_manager_config), level = "debug")]
+    pub async fn new(
+        accounts: Vec<Contract>,
+        sequencer_index: usize,
+        chain_info: ChainInfo,
+        consensus_manager_config: ConsensusManagerConfig,
+        mempool_p2p_config: MempoolP2pConfig,
+        available_ports: &mut AvailablePorts,
     ) -> Self {
-        let mut available_ports = AvailablePorts::new(test_unique_index, 0);
-
-        let chain_info = create_chain_info();
         // Creating the storage for the test.
-        let storage_for_test = StorageTestSetup::new(tx_generator.accounts(), &chain_info);
+        let storage_for_test = StorageTestSetup::new(accounts, &chain_info);
 
         // Spawn a papyrus rpc server for a papyrus storage reader.
         let rpc_server_addr = spawn_test_rpc_state_reader_with_socket(
@@ -67,27 +152,16 @@ impl IntegrationTestSetup {
         )
         .await;
 
-        let (mut consensus_manager_configs, _consensus_proposals_channels) =
-            create_consensus_manager_configs_and_channels(
-                SEQUENCER_INDICES.len(),
-                &mut available_ports,
-            );
-        let mut mempool_p2p_configs = create_mempool_p2p_configs(
-            SEQUENCER_INDICES.len(),
-            chain_info.chain_id.clone(),
-            &mut available_ports,
-        );
-
         // Derive the configuration for the sequencer node.
         let (config, required_params) = create_config(
-            &mut available_ports,
-            SEQUENCER_INDEX,
+            available_ports,
+            sequencer_index,
             chain_info,
             rpc_server_addr,
             storage_for_test.batcher_storage_config,
             storage_for_test.state_sync_storage_config,
-            consensus_manager_configs.pop().unwrap(),
-            mempool_p2p_configs.pop().unwrap(),
+            consensus_manager_config,
+            mempool_p2p_config,
         )
         .await;
 
@@ -105,17 +179,21 @@ impl IntegrationTestSetup {
         let HttpServerConfig { ip, port } = config.http_server_config;
         let add_tx_http_client = HttpTestClient::new(SocketAddr::from((ip, port)));
 
-        IntegrationTestSetup {
+        Self {
+            sequencer_index,
             add_tx_http_client,
             is_alive_test_client,
             batcher_storage_handle: storage_for_test.batcher_storage_handle,
             batcher_storage_config: config.batcher_config.storage,
             rpc_storage_handle: storage_for_test.rpc_storage_handle,
-            available_ports,
             node_config_dir_handle,
             node_config_path,
             state_sync_storage_handle: storage_for_test.state_sync_storage_handle,
             state_sync_storage_config: config.state_sync_config.storage_config,
         }
+    }
+
+    pub async fn assert_add_tx_success(&self, tx: RpcTransaction) -> TransactionHash {
+        self.add_tx_http_client.assert_add_tx_success(tx).await
     }
 }
