6d7b844465c6afeb466d1bff19452f2e9d63bd74
Yonatan Iluz <yonatan@starkware.co>
2024-12-18 15:13:19 +0200

    chore(blockifier): enable get_block_hash syscall in validate mode

--- crates/blockifier/src/execution/syscalls/syscall_tests/get_block_hash.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/get_block_hash.rs
@@ -10,6 +10,7 @@ use crate::abi::constants;
 use crate::context::ChainInfo;
 use crate::execution::call_info::CallExecution;
 use crate::execution::entry_point::CallEntryPoint;
+use crate::retdata;
 use crate::state::cached_state::CachedState;
 use crate::state::state_api::State;
 use crate::test_utils::contracts::FeatureContract;
@@ -23,7 +24,6 @@ use crate::test_utils::{
     CURRENT_BLOCK_NUMBER,
 };
 use crate::versioned_constants::VersionedConstants;
-use crate::{check_entry_point_execution_error_for_custom_hint, retdata};
 
 fn initialize_state(test_contract: FeatureContract) -> (CachedState<DictStateReader>, Felt, Felt) {
     let chain_info = &ChainInfo::create_for_testing();
@@ -64,43 +64,6 @@ fn positive_flow(runnable_version: RunnableCairo1) {
 
 #[cfg_attr(feature = "cairo_native", test_case(RunnableCairo1::Native;"Native"))]
 #[test_case(RunnableCairo1::Casm;"VM")]
-fn negative_flow_execution_mode_validate(runnable_version: RunnableCairo1) {
-    let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1(runnable_version));
-    let (mut state, block_number, _) = initialize_state(test_contract);
-
-    let calldata = calldata![block_number];
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_get_block_hash"),
-        calldata,
-        ..trivial_external_entry_point_new(test_contract)
-    };
-
-    let error = entry_point_call.execute_directly_in_validate_mode(&mut state).unwrap_err();
-    #[cfg(feature = "cairo_native")]
-    if matches!(
-        test_contract,
-        FeatureContract::TestContract(CairoVersion::Cairo1(RunnableCairo1::Native))
-    ) {
-        assert!(
-            error
-                .to_string()
-                .contains("Unauthorized syscall get_block_hash in execution mode Validate.")
-        );
-    } else {
-        check_entry_point_execution_error_for_custom_hint!(
-            &error,
-            "Unauthorized syscall get_block_hash in execution mode Validate.",
-        );
-    }
-    #[cfg(not(feature = "cairo_native"))]
-    check_entry_point_execution_error_for_custom_hint!(
-        &error,
-        "Unauthorized syscall get_block_hash in execution mode Validate.",
-    );
-}
-
-#[cfg_attr(feature = "cairo_native", test_case(RunnableCairo1::Native;"Native"))]
-#[test_case(RunnableCairo1::Casm;"VM")]
 fn negative_flow_block_number_out_of_range(runnable_version: RunnableCairo1) {
     let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1(runnable_version));
     let (mut state, _, _) = initialize_state(test_contract);
@@ -114,10 +77,15 @@ fn negative_flow_block_number_out_of_range(runnable_version: RunnableCairo1) {
         ..trivial_external_entry_point_new(test_contract)
     };
 
-    let call_info = entry_point_call.execute_directly(&mut state).unwrap();
+    let call_info = entry_point_call.clone().execute_directly(&mut state).unwrap();
     assert!(call_info.execution.failed);
     assert_eq!(
         format_panic_data(&call_info.execution.retdata.0),
         "0x426c6f636b206e756d626572206f7574206f662072616e6765 ('Block number out of range')"
     );
+
+    let error = entry_point_call.execute_directly_in_validate_mode(&mut state).unwrap_err();
+    assert!(error.to_string().contains(
+        "Unauthorized syscall get_block_hash on recent blocks in execution mode Validate."
+    ));
 }
