c4145022a82818e52c979855f6471e13b43b38b3
Yael Doweck <yael@starkware.co>
2024-12-19 15:59:45 +0200

    feat(consensus): add central invoke transaction

--- crates/sequencing/papyrus_consensus_orchestrator/src/cende/central_objects_test.rs
+++ crates/sequencing/papyrus_consensus_orchestrator/src/cende/central_objects_test.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
+use std::sync::Arc;
 
 use indexmap::indexmap;
 use infra_utils::path::resolve_project_relative_path;
+use rstest::rstest;
 use serde_json::Value;
 use starknet_api::block::{
     BlockNumber,
@@ -12,9 +14,26 @@ use starknet_api::block::{
 };
 use starknet_api::core::{ClassHash, CompiledClassHash, Nonce};
 use starknet_api::data_availability::DataAvailabilityMode;
+use starknet_api::execution_resources::GasAmount;
+use starknet_api::transaction::fields::{
+    AllResourceBounds,
+    Calldata,
+    ResourceBounds,
+    ValidResourceBounds,
+};
+use starknet_api::transaction::TransactionHash;
 use starknet_api::{contract_address, felt, storage_key};
 
-use crate::cende::central_objects::{CentralBlockInfo, CentralResourcePrice, CentralStateDiff};
+use crate::cende::central_objects::{
+    CentralBlockInfo,
+    CentralInvokeTransaction,
+    CentralInvokeTransactionV3,
+    CentralResourcePrice,
+    CentralStateDiff,
+    CentralTransaction,
+    CentralTransactionTimestamp,
+    CentralTransactionWritten,
+};
 
 macro_rules! test_resources_dir {
     () => {
@@ -23,6 +42,8 @@ macro_rules! test_resources_dir {
 }
 pub const CENTRAL_STATE_DIFF_JSON_PATH: &str =
     concat!(test_resources_dir!(), "/central_state_diff.json");
+pub const CENTRAL_INVOKE_TX_JSON_PATH: &str =
+    concat!(test_resources_dir!(), "/central_invoke_tx.json");
 
 fn central_state_diff() -> CentralStateDiff {
     CentralStateDiff {
@@ -61,13 +82,41 @@ fn central_state_diff() -> CentralStateDiff {
     }
 }
 
-#[test]
-fn serialize_central_state_diff() {
-    let rust_central_state_diff = central_state_diff();
-    let rust_json = serde_json::to_value(&rust_central_state_diff).unwrap();
+fn invoke_transaction() -> CentralTransactionWritten {
+    CentralTransactionWritten {
+        tx: CentralTransaction::Invoke(CentralInvokeTransaction::V3(CentralInvokeTransactionV3 {
+            resource_bounds: ValidResourceBounds::AllResources(AllResourceBounds {
+                l1_gas: ResourceBounds {
+                    max_amount: GasAmount(1),
+                    max_price_per_unit: GasPrice(1),
+                },
+                l2_gas: ResourceBounds::default(),
+                l1_data_gas: ResourceBounds::default(),
+            }),
+            sender_address: contract_address!(
+                "0x14abfd58671a1a9b30de2fcd2a42e8bff2ce1096a7c70bc7995904965f277e"
+            ),
+            calldata: Calldata(Arc::new(vec![felt!(0_u8), felt!(1_u8)])),
+            signature: Default::default(),
+            nonce: Default::default(),
+            tip: Default::default(),
+            paymaster_data: Default::default(),
+            account_deployment_data: Default::default(),
+            nonce_data_availability_mode: Default::default(),
+            fee_data_availability_mode: Default::default(),
+            hash_value: TransactionHash(felt!(
+                "0x6efd067c859e6469d0f6d158e9ae408a9552eb8cc11f618ab3aef3e52450666"
+            )),
+        })),
+        time_created: CentralTransactionTimestamp(1734601615),
+    }
+}
 
-    let file =
-        File::open(resolve_project_relative_path(CENTRAL_STATE_DIFF_JSON_PATH).unwrap()).unwrap();
+#[rstest]
+#[case::state_diff(serde_json::to_value(central_state_diff()).unwrap(), CENTRAL_STATE_DIFF_JSON_PATH)]
+#[case::invoke_tx(serde_json::to_value(invoke_transaction()).unwrap(), CENTRAL_INVOKE_TX_JSON_PATH)]
+fn serialize_central_objects(#[case] rust_json: Value, #[case] python_json_path: &str) {
+    let file = File::open(resolve_project_relative_path(python_json_path).unwrap()).unwrap();
     let python_json: Value = serde_json::from_reader(file).unwrap();
 
     assert_eq!(rust_json, python_json,);
