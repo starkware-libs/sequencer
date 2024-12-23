f3b4c4e9fc478927fd3c31e008cb7c7d3890989d
Aviv Greenburg <aviv.g@starkware.co>
2024-12-23 10:59:24 +0200

    fix(blockifier): avoid negative consumed gas

--- crates/blockifier/src/execution/native/entry_point_execution.rs
+++ crates/blockifier/src/execution/native/entry_point_execution.rs
@@ -11,7 +11,7 @@ use crate::execution::entry_point::{
     EntryPointExecutionResult,
 };
 use crate::execution::entry_point_execution::gas_consumed_without_inner_calls;
-use crate::execution::errors::{EntryPointExecutionError, PostExecutionError};
+use crate::execution::errors::{EntryPointExecutionError, PostExecutionError, PreExecutionError};
 use crate::execution::native::contract_class::NativeCompiledClassV1;
 use crate::execution::native::syscall_handler::NativeSyscallHandler;
 use crate::state::state_api::State;
@@ -56,13 +56,24 @@ pub fn execute_entry_point_call(
     // TODO(Aviv/Yonatan): add these numbers to overridable VC.
     let stack_size_red_zone = 160 * 1024 * 1024;
     let target_stack_size = stack_size_red_zone + 10 * 1024 * 1024;
+
+    // Pre-charge entry point's initial budget to ensure sufficient gas for executing a minimal
+    // entry point code. When redepositing is used, the entry point is aware of this pre-charge
+    // and adjusts the gas counter accordingly if a smaller amount of gas is required.
+    let initial_budget = syscall_handler.base.context.gas_costs().base.entry_point_initial_budget;
+    let call_initial_gas = syscall_handler
+        .base
+        .call
+        .initial_gas
+        .checked_sub(initial_budget)
+        .ok_or(PreExecutionError::InsufficientEntryPointGas)?;
     // Use `maybe_grow` and not `grow` for performance, since in happy flows, only the main call
     // should trigger the growth.
     let execution_result = stacker::maybe_grow(stack_size_red_zone, target_stack_size, || {
         compiled_class.executor.run(
             entry_point.selector.0,
             &syscall_handler.base.call.calldata.0.clone(),
-            syscall_handler.base.call.initial_gas,
+            call_initial_gas,
             Some(builtin_costs),
             &mut syscall_handler,
         )
