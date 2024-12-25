6429ca02832ec6b36f5a79f14b5147a8cea2d3ca
Alon Lukatch <alon.l@starkware.co>
2024-12-25 15:09:18 +0200

    refactor(papyrus_storage): fix CR comments

--- crates/papyrus_storage/src/body/mod.rs
+++ crates/papyrus_storage/src/body/mod.rs
@@ -86,7 +86,7 @@ type AddressToTransactionIndexTable<'env> = TableHandle<
     NoVersionValueWrapper<NoValue>,
     CommonPrefix,
 >;
-// TODO: remove the dead code attribute.
+// TODO(alonl): remove the dead code attribute.
 #[allow(dead_code)]
 type EventsTableKey = TransactionIndex;
 #[allow(dead_code)]
