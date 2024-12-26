96663a2a7c72d4dc4027638c742dcc9826b73c15
Alon Lukatch <alon.l@starkware.co>
2024-12-26 12:24:23 +0200

    refactor(papyrus_storage): add unused refactored event iterators

--- crates/papyrus_storage/src/body/mod.rs
+++ crates/papyrus_storage/src/body/mod.rs
@@ -86,10 +86,7 @@ type AddressToTransactionIndexTable<'env> = TableHandle<
     NoVersionValueWrapper<NoValue>,
     CommonPrefix,
 >;
-// TODO: remove the dead code attribute.
-#[allow(dead_code)]
 type EventsTableKey = TransactionIndex;
-#[allow(dead_code)]
 type EventsTable<'env> =
     TableHandle<'env, EventsTableKey, VersionZeroWrapper<LocationInFile>, SimpleTable>;
 
