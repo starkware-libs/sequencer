06ab7b0fcb1cab3088461a0facfa413b6232b18c
Dvir Yosef <dvir@starkware.co>
2024-12-29 18:00:50 +0200

    refactor(sequencing): cende context, add logic and tests

---
+++ crates/sequencing/papyrus_consensus_orchestrator/src/cende/cende_test.rs
@@ -0,0 +1,50 @@
+use rstest::rstest;
+use starknet_api::block::BlockNumber;
+
+use super::{CendeAmbassador, RECORDER_WRITE_BLOB_PATH};
+use crate::cende::{BlobParameters, CendeConfig, CendeContext};
+
+const HEIGHT_TO_WRITE: u64 = 10;
+
+#[rstest]
+#[case::success(RECORDER_WRITE_BLOB_PATH, 200, Some(9), 1, true)]
+#[case::no_prev_block(RECORDER_WRITE_BLOB_PATH, 200, None, 0, false)]
+#[case::prev_block_height_mismatch(RECORDER_WRITE_BLOB_PATH, 200, Some(7), 0, false)]
+#[case::send_to_recorder_failed("different_recorder_path", 200, Some(9), 0, false)]
+#[case::recorder_return_error(RECORDER_WRITE_BLOB_PATH, 500, Some(9), 1, false)]
+#[tokio::test]
+async fn write_prev_height_blob(
+    #[case] path: &str,
+    #[case] status_code: usize,
+    #[case] prev_block: Option<u64>,
+    #[case] expected_calls: usize,
+    #[case] expected_result: bool,
+) {
+    let mut server = mockito::Server::new_async().await;
+    let url = server.url();
+    let mock = server.mock("POST", path).with_status(status_code).create();
+
+    let cende_ambassador = CendeAmbassador::new(CendeConfig { recorder_url: url.parse().unwrap() });
+
+    if let Some(prev_block) = prev_block {
+        cende_ambassador.prepare_blob_for_next_height(BlobParameters { height: prev_block }).await;
+    }
+
+    let receiver = cende_ambassador.write_prev_height_blob(BlockNumber(HEIGHT_TO_WRITE));
+
+    assert_eq!(receiver.await.unwrap(), expected_result);
+    mock.expect(expected_calls).assert();
+}
+
+#[tokio::test]
+async fn prepare_blob_for_next_height() {
+    let cende_ambassador =
+        CendeAmbassador::new(CendeConfig { recorder_url: "http://parsable_url".parse().unwrap() });
+
+    cende_ambassador.prepare_blob_for_next_height(BlobParameters { height: HEIGHT_TO_WRITE }).await;
+
+    assert_eq!(
+        cende_ambassador.prev_height_blob.lock().await.as_ref().unwrap().block_number.0,
+        HEIGHT_TO_WRITE
+    );
+}
