c4d7b492d0dc0e5bafc5e217a8a1d60a8b5bf894
Alon Haramati <alonh@starkware.co>
2024-12-29 13:47:47 +0200

    feat(starknet_batcher): fill in ThinStateDiff fields

--- crates/starknet_batcher/src/test_utils.rs
+++ crates/starknet_batcher/src/test_utils.rs
@@ -3,10 +3,10 @@ use std::ops::Range;
 use async_trait::async_trait;
 use blockifier::blockifier::transaction_executor::VisitedSegmentsMapping;
 use blockifier::bouncer::BouncerWeights;
-use blockifier::state::cached_state::CommitmentStateDiff;
 use indexmap::IndexMap;
 use starknet_api::executable_transaction::Transaction;
 use starknet_api::execution_resources::GasAmount;
+use starknet_api::state::ThinStateDiff;
 use starknet_api::test_utils::invoke::{executable_invoke_tx, InvokeTxArgs};
 use starknet_api::{class_hash, contract_address, nonce, tx_hash};
 use tokio::sync::mpsc::UnboundedSender;
@@ -72,17 +72,22 @@ pub fn test_txs(tx_hash_range: Range<usize>) -> Vec<Transaction> {
 
 impl BlockExecutionArtifacts {
     pub fn create_for_testing() -> Self {
-        // Use a non-empty commitment_state_diff to make the tests more realistic.
+        // Use a non-empty thin_state_diff to make the tests more realistic.
         Self {
             execution_infos: IndexMap::default(),
-            commitment_state_diff: CommitmentStateDiff {
-                address_to_class_hash: IndexMap::from_iter([(
+            state_diff: ThinStateDiff {
+                deployed_contracts: IndexMap::from_iter([(
                     contract_address!("0x7"),
                     class_hash!("0x11111111"),
                 )]),
-                storage_updates: IndexMap::new(),
-                class_hash_to_compiled_class_hash: IndexMap::new(),
-                address_to_nonce: IndexMap::from_iter([(contract_address!("0x7"), nonce!(1_u64))]),
+                storage_diffs: IndexMap::from_iter([
+                    (contract_address!("0x19"), IndexMap::new()),
+                    (contract_address!("0x8"), IndexMap::new()),
+                ]),
+                declared_classes: IndexMap::new(),
+                deprecated_declared_classes: Vec::new(),
+                nonces: IndexMap::from_iter([(contract_address!("0x7"), nonce!(1_u64))]),
+                replaced_classes: IndexMap::new(),
             },
             visited_segments_mapping: VisitedSegmentsMapping::default(),
             bouncer_weights: BouncerWeights::empty(),
