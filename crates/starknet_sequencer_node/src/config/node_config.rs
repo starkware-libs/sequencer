a73a7ce2cdd70ab7443a83b3eed11379f284d093
Avi Cohen <avi.cohen@starkware.co>
2024-12-30 19:59:59 +0200

    chore(cairo_native): add compiler file path configs

--- crates/starknet_sequencer_node/src/config/node_config.rs
+++ crates/starknet_sequencer_node/src/config/node_config.rs
@@ -26,7 +26,7 @@ use starknet_http_server::config::HttpServerConfig;
 use starknet_l1_provider::L1ProviderConfig;
 use starknet_mempool_p2p::config::MempoolP2pConfig;
 use starknet_monitoring_endpoint::config::MonitoringEndpointConfig;
-use starknet_sierra_compile::config::SierraToCasmCompilationConfig;
+use starknet_sierra_compile::config::SierraCompilationConfig;
 use starknet_state_sync::config::StateSyncConfig;
 use validator::Validate;
 
@@ -122,7 +122,7 @@ pub struct SequencerNodeConfig {
     #[validate]
     pub rpc_state_reader_config: RpcStateReaderConfig,
     #[validate]
-    pub compiler_config: SierraToCasmCompilationConfig,
+    pub compiler_config: SierraCompilationConfig,
     #[validate]
     pub l1_provider_config: L1ProviderConfig,
     #[validate]
