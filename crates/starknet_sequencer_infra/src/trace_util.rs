8bfb0b3fd7dc73e1519ed4da0dc3a4ab4eb6a0de
lev-starkware <155880815+lev-starkware@users.noreply.github.com>
2024-12-31 14:51:51 +0200

    feat(starknet_sequencer_infra): add macros to add sequencer PID to tracing (#3031)
    commit-id:ca8bc556

--- crates/starknet_sequencer_infra/src/trace_util.rs
+++ crates/starknet_sequencer_infra/src/trace_util.rs
@@ -7,6 +7,8 @@ const DEFAULT_LEVEL: LevelFilter = LevelFilter::INFO;
 // Define a OnceCell to ensure the configuration is initialized only once
 static TRACING_INITIALIZED: OnceCell<()> = OnceCell::const_new();
 
+pub static PID: std::sync::LazyLock<u32> = std::sync::LazyLock::new(std::process::id);
+
 pub async fn configure_tracing() {
     TRACING_INITIALIZED
         .get_or_init(|| async {
@@ -21,4 +23,46 @@ pub async fn configure_tracing() {
             tracing::info!("Tracing has been successfully initialized.");
         })
         .await;
+}
+
+#[macro_export]
+macro_rules! infra_event {
+    ($($arg:tt)*) => {{
+        tracing::event!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! infra_trace {
+    ($($arg:tt)*) => {{
+        tracing::trace!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! infra_debug {
+    ($($arg:tt)*) => {{
+        tracing::debug!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! infra_info {
+    ($($arg:tt)*) => {{
+        tracing::info!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! infra_warn {
+    ($($arg:tt)*) => {{
+        tracing::warn!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! infra_error {
+    ($($arg:tt)*) => {{
+        tracing::error!(PID = *$crate::trace_util::PID, $($arg)*);
+    }};
 }
