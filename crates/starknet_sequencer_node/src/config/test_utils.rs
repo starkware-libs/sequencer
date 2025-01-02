2a57cdfce3bba0addc5c106d458485d6db86864a
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-01-02 11:52:27 +0200

    chore(starknet_sequencer_node): move config_to_preset util (#3060)
    commit-id:f3b90442

--- crates/starknet_sequencer_node/src/config/test_utils.rs
+++ crates/starknet_sequencer_node/src/config/test_utils.rs
@@ -1,9 +1,8 @@
 use std::collections::HashSet;
-use std::vec::Vec; // Used by #[gen_field_names_fn].
 
 use papyrus_protobuf::consensus::DEFAULT_VALIDATOR_ID;
 use serde::Serialize;
-use serde_json::{to_value, Value};
+use serde_json::{to_value, Map, Value};
 use starknet_api::core::{ChainId, ContractAddress};
 use url::Url;
 
@@ -63,4 +62,41 @@ pub fn create_test_config_load_args(required_params: RequiredParams) -> Vec<Stri
     let mut cli_args = vec![node_command().to_string()];
     cli_args.extend(required_params.cli_args());
     cli_args
+}
+
+/// Transforms a nested JSON dictionary object into a simplified JSON dictionary object by
+/// extracting specific values from the inner dictionaries.
+///
+/// # Parameters
+/// - `config_map`: A reference to a `serde_json::Value` that must be a JSON dictionary object. Each
+///   key in the object maps to another JSON dictionary object.
+///
+/// # Returns
+/// - A `serde_json::Value` dictionary object where:
+///   - Each key is preserved from the top-level dictionary.
+///   - Each value corresponds to the `"value"` field of the nested JSON dictionary under the
+///     original key.
+///
+/// # Panics
+/// This function panics if the provided `config_map` is not a JSON dictionary object.
+pub fn config_to_preset(config_map: &Value) -> Value {
+    // Ensure the config_map is a JSON object.
+    if let Value::Object(map) = config_map {
+        let mut result = Map::new();
+
+        for (key, value) in map {
+            if let Value::Object(inner_map) = value {
+                // Extract the value.
+                if let Some(inner_value) = inner_map.get("value") {
+                    // Add it to the result map
+                    result.insert(key.clone(), inner_value.clone());
+                }
+            }
+        }
+
+        // Return the transformed result as a JSON object.
+        Value::Object(result)
+    } else {
+        panic!("Config map is not a JSON object: {:?}", config_map);
+    }
 }
