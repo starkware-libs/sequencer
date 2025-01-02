714472462b7de578e737f71602d065ef8d42970a
eitanm-starkware <eitan.m@starkware.co>
2025-01-02 10:19:07 +0200

    test(starknet_integration_tests): add p2p sync network

--- crates/starknet_integration_tests/src/integration_test_setup.rs
+++ crates/starknet_integration_tests/src/integration_test_setup.rs
@@ -15,6 +15,7 @@ use starknet_monitoring_endpoint::test_utils::IsAliveClient;
 use starknet_sequencer_infra::test_utils::AvailablePorts;
 use starknet_sequencer_node::config::component_config::ComponentConfig;
 use starknet_sequencer_node::test_utils::node_runner::spawn_run_node;
+use starknet_state_sync::config::StateSyncConfig;
 use tempfile::{tempdir, TempDir};
 use tokio::task::JoinHandle;
 use tracing::{info, instrument};
@@ -26,6 +27,7 @@ use crate::utils::{
     create_consensus_manager_configs_and_channels,
     create_mempool_p2p_configs,
     create_node_config,
+    create_state_sync_configs,
 };
 
 pub struct IntegrationTestSetup {
@@ -53,6 +55,12 @@ impl IntegrationTestSetup {
         let mut mempool_p2p_configs =
             create_mempool_p2p_configs(chain_info.chain_id.clone(), ports);
 
+        let mut state_sync_configs = create_state_sync_configs(
+            n_distributed_sequencers,
+            StorageConfig::default(),
+            &mut available_ports,
+        );
+
         let mut sequencers = vec![];
         for (sequencer_id, node_composition) in component_configs.iter().enumerate() {
             for component_config in node_composition {
@@ -66,6 +74,7 @@ impl IntegrationTestSetup {
                     chain_info.clone(),
                     consensus_manager_config,
                     mempool_p2p_config,
+                    state_sync_configs.remove(0),
                     &mut available_ports,
                     component_config.clone(),
                 )
@@ -139,6 +148,7 @@ pub struct IntegrationSequencerSetup {
 }
 
 impl IntegrationSequencerSetup {
+    #[allow(clippy::too_many_arguments)]
     #[instrument(skip(accounts, chain_info, consensus_manager_config), level = "debug")]
     pub async fn new(
         accounts: Vec<Contract>,
@@ -146,11 +156,14 @@ impl IntegrationSequencerSetup {
         chain_info: ChainInfo,
         consensus_manager_config: ConsensusManagerConfig,
         mempool_p2p_config: MempoolP2pConfig,
+        mut state_sync_config: StateSyncConfig,
         available_ports: &mut AvailablePorts,
         component_config: ComponentConfig,
     ) -> Self {
         // Creating the storage for the test.
         let storage_for_test = StorageTestSetup::new(accounts, &chain_info);
+
+        state_sync_config.storage_config = storage_for_test.state_sync_storage_config;
 
         // Derive the configuration for the sequencer node.
         let (config, required_params) = create_node_config(
@@ -158,7 +171,7 @@ impl IntegrationSequencerSetup {
             sequencer_index,
             chain_info,
             storage_for_test.batcher_storage_config,
-            storage_for_test.state_sync_storage_config,
+            state_sync_config,
             consensus_manager_config,
             mempool_p2p_config,
             component_config,
