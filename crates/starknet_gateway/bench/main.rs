97e76508c5fc97f7f2050fde031dd38f38e15748
Arnon Hod <arnon@starkware.co>
2025-01-05 18:37:51 +0200

    feat(starknet_gateway): create transaction generator towards benchmarking (#2911)
    * feat(starknet_gateway): create transaction generator towards benchmarking
    
    * chore(starknet_gateway): move the bench utils into the bench folder

--- crates/starknet_gateway/bench/main.rs
+++ crates/starknet_gateway/bench/main.rs
@@ -9,10 +9,26 @@
 //!
 //! Run the benchmarks using `cargo bench --bench gateway_bench`.
 
-use criterion::{criterion_group, criterion_main, Criterion};
+// import the Gateway test utilities.
+mod utils;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use utils::{BenchTestSetup, BenchTestSetupConfig};
 
 fn invoke_benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("invokes", |benchmark| benchmark.iter(|| {}));
+    let tx_generator_config = BenchTestSetupConfig::default();
+    let n_txs = tx_generator_config.n_txs;
+
+    let test_setup = BenchTestSetup::new(tx_generator_config);
+    criterion.bench_with_input(
+        BenchmarkId::new("invoke", n_txs),
+        &test_setup,
+        |bencher, test_setup| {
+            bencher
+                .to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter(|| test_setup.send_txs_to_gateway());
+        },
+    );
 }
 
 criterion_group!(benches, invoke_benchmark);
