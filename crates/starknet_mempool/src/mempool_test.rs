8f9b3c326c263a79ce8d85462d93f27d41ccc591
Arni Hod <arnon@starkware.co>
2025-01-06 09:04:26 +0200

    feat(starknet_mempool): implement has_tx_from_address

--- crates/starknet_mempool/src/mempool_test.rs
+++ crates/starknet_mempool/src/mempool_test.rs
@@ -884,3 +884,11 @@ fn test_rejected_tx_deleted_from_mempool(mut mempool: Mempool) {
         MempoolContentBuilder::new().with_pool(expected_pool_txs).build();
     expected_mempool_content.assert_eq(&mempool);
 }
+
+// TODO(Arni): Add positive flow.
+#[rstest]
+fn has_tx_from_address_negative_flow() {
+    let mempool = MempoolContentBuilder::new().build_into_mempool();
+
+    assert!(!mempool.has_tx_from_address(contract_address!(100_u32)));
+}
