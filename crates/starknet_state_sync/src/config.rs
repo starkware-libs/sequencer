cee7a16a269bf71a443842a92a60fe030f135e8c
Alon Lukatch <alon.l@starkware.co>
2025-01-07 16:55:26 +0200

    feat(starknet_state_sync): allow central sync or p2p sync

--- crates/starknet_state_sync/src/config.rs
+++ crates/starknet_state_sync/src/config.rs
@@ -1,23 +1,30 @@
 use std::collections::BTreeMap;
 use std::path::PathBuf;
+use std::result;
 
-use papyrus_config::dumping::{append_sub_config_name, SerializeConfig};
+use papyrus_base_layer::ethereum_base_layer_contract::EthereumBaseLayerConfig;
+use papyrus_config::dumping::{append_sub_config_name, ser_optional_sub_config, SerializeConfig};
 use papyrus_config::{ParamPath, SerializedParam};
 use papyrus_network::NetworkConfig;
 use papyrus_p2p_sync::client::P2pSyncClientConfig;
 use papyrus_storage::db::DbConfig;
 use papyrus_storage::StorageConfig;
+use papyrus_sync::sources::central::CentralSourceConfig;
+use papyrus_sync::SyncConfig;
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use validator::{Validate, ValidationError};
 
 const STATE_SYNC_TCP_PORT: u16 = 12345;
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Validate)]
+#[validate(schema(function = "validate_config"))]
 pub struct StateSyncConfig {
     #[validate]
     pub storage_config: StorageConfig,
-    // TODO(shahak): add validate to P2pSyncClientConfig
-    pub p2p_sync_client_config: P2pSyncClientConfig,
+    #[validate]
+    pub p2p_sync_client_config: Option<P2pSyncClientConfig>,
+    #[validate]
+    pub central_sync_client_config: Option<CentralSyncClientConfig>,
     #[validate]
     pub network_config: NetworkConfig,
 }
@@ -26,13 +33,23 @@ impl SerializeConfig for StateSyncConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         vec![
             append_sub_config_name(self.storage_config.dump(), "storage_config"),
-            append_sub_config_name(self.p2p_sync_client_config.dump(), "p2p_sync_client_config"),
             append_sub_config_name(self.network_config.dump(), "network_config"),
+            ser_optional_sub_config(&self.p2p_sync_client_config, "p2p_sync_client_config"),
+            ser_optional_sub_config(&self.central_sync_client_config, "central_sync_client_config"),
         ]
         .into_iter()
         .flatten()
         .collect()
     }
+}
+
+fn validate_config(config: &StateSyncConfig) -> result::Result<(), ValidationError> {
+    if config.central_sync_client_config.is_some() && config.p2p_sync_client_config.is_some() {
+        return Err(ValidationError::new(
+            "One of --sync.#is_none or --p2p_sync.#is_none must be turned on",
+        ));
+    }
+    Ok(())
 }
 
 impl Default for StateSyncConfig {
@@ -45,8 +62,29 @@ impl Default for StateSyncConfig {
                 },
                 ..Default::default()
             },
-            p2p_sync_client_config: Default::default(),
+            p2p_sync_client_config: Some(P2pSyncClientConfig::default()),
+            central_sync_client_config: None,
             network_config: NetworkConfig { tcp_port: STATE_SYNC_TCP_PORT, ..Default::default() },
         }
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize, Clone, PartialEq, Validate)]
+pub struct CentralSyncClientConfig {
+    pub sync_config: SyncConfig,
+    pub central_source_config: CentralSourceConfig,
+    pub base_layer_config: EthereumBaseLayerConfig,
+}
+
+impl SerializeConfig for CentralSyncClientConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        vec![
+            append_sub_config_name(self.sync_config.dump(), "sync_config"),
+            append_sub_config_name(self.central_source_config.dump(), "central_source_config"),
+            append_sub_config_name(self.base_layer_config.dump(), "base_layer_config"),
+        ]
+        .into_iter()
+        .flatten()
+        .collect()
     }
 }
