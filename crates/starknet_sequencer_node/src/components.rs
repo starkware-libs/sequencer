52b249d0f46b8901423ec6858bb0072f9ae41de6
Noam Spiegelstein <noam.s@starkware.co>
2025-01-13 14:46:05 +0200

    feat(starknet_gateway): add class manager client

--- crates/starknet_sequencer_node/src/components.rs
+++ crates/starknet_sequencer_node/src/components.rs
@@ -84,12 +84,14 @@ pub fn create_node_components(
             let state_sync_client = clients
                 .get_state_sync_shared_client()
                 .expect("State Sync Client should be available");
-
+            // TODO: Remove this and use the real client instead once implemented.
+            let class_manager_client = Arc::new(EmptyClassManagerClient);
             Some(create_gateway(
                 config.gateway_config.clone(),
                 state_sync_client,
                 config.compiler_config.clone(),
                 mempool_client,
+                class_manager_client,
             ))
         }
         ReactiveComponentExecutionMode::Disabled | ReactiveComponentExecutionMode::Remote => None,
