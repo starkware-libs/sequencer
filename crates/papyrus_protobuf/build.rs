be97e136910c08118a790d2c40befcc2cfa7d414
Alon Lukatch <alon.l@starkware.co>
2025-01-14 13:53:18 +0200

    refactor(papyrus_protobuf): move event.proto to sync folder

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -53,15 +53,16 @@ fn main() -> io::Result<()> {
     }
     prost_build::compile_protos(
         &[
-            // "src/proto/p2p/proto/rpc_transaction.proto",
             "src/proto/p2p/proto/class.proto",
-            "src/proto/p2p/proto/sync/class.proto",
-            "src/proto/p2p/proto/event.proto",
+            "src/proto/p2p/proto/consensus.proto",
             "src/proto/p2p/proto/header.proto",
+            "src/proto/p2p/proto/mempool/transaction.proto",
+            "src/proto/p2p/proto/sync/class.proto",
+            "src/proto/p2p/proto/sync/common.proto", /* built as a dependency so maybe we dont
+                                                      * need to specify here */
+            "src/proto/p2p/proto/sync/event.proto",
             "src/proto/p2p/proto/sync/state.proto",
             "src/proto/p2p/proto/transaction.proto",
-            "src/proto/p2p/proto/consensus.proto",
-            "src/proto/p2p/proto/mempool/transaction.proto",
         ],
         &["src/proto/"],
     )?;
