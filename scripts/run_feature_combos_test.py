7ce508a69162c2bcec38274a48c6b4a91959ebe5
dorimedini-starkware <dori@starkware.co>
2025-01-14 15:30:22 +0200

    fix(ci): fix run_feature_combos_test.py (#3301)
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/run_feature_combos_test.py
+++ scripts/run_feature_combos_test.py
@@ -1,19 +1,21 @@
 #!/bin/env python3
 
 import subprocess
+from typing import List
 from tests_utils import get_workspace_packages
 
 
-def build_without_features(package: str):
-    cmd = f"cargo build --package {package}"
-    print(f"Running '{cmd}'", flush=True)
+def run_command(cmd: List[str]):
+    print(f"Running '{' '.join(cmd)}'", flush=True)
     subprocess.run(cmd, check=True)
+
+
+def build_without_features(package: str):
+    run_command(cmd=["cargo", "build", "--package", package])
 
 
 def build_with_all_features(package: str):
-    cmd = f"cargo build --all-features --package {package}"
-    print(f"Running '{cmd}'", flush=True)
-    subprocess.run(cmd, check=True)
+    run_command(cmd=["cargo", "build", "--all-features", "--package", package])
 
 
 def main():
