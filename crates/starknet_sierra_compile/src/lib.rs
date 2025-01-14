87aa8f25cb69cba50d03651ec5f5f8aa0a7b5861
Elin Tulchinsky <elin@starkware.co>
2025-01-14 11:00:05 +0200

    feat(starknet_sierra_compile): add `SierraCompiler`
    A wrapper around the CLI compiler that also takes care of class serde.
    It will serve as the compilation CPU unit of the `ClassManager`.

--- crates/starknet_sierra_compile/src/lib.rs
+++ crates/starknet_sierra_compile/src/lib.rs
@@ -1,10 +1,18 @@
 //! A lib for compiling Sierra into Casm.
 use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
-use cairo_lang_starknet_classes::contract_class::ContractClass;
+use cairo_lang_starknet_classes::contract_class::ContractClass as CairoLangContractClass;
 #[cfg(feature = "cairo_native")]
 use cairo_native::executor::AotContractExecutor;
+use starknet_api::contract_class::{ContractClass, SierraVersion};
+use starknet_api::core::CompiledClassHash;
+use starknet_api::state::SierraContractClass;
+use starknet_api::StarknetApiError;
+use starknet_sierra_compile_types::{RawClass, RawExecutableClass, RawExecutableHashedClass};
+use thiserror::Error;
 
+use crate::command_line_compiler::CommandLineCompiler;
 use crate::errors::CompilationUtilError;
+use crate::utils::into_contract_class_for_compilation;
 
 pub mod command_line_compiler;
 pub mod config;
@@ -24,7 +32,7 @@ pub mod compile_test;
 pub trait SierraToCasmCompiler: Send + Sync {
     fn compile(
         &self,
-        contract_class: ContractClass,
+        contract_class: CairoLangContractClass,
     ) -> Result<CasmContractClass, CompilationUtilError>;
 }
 
@@ -32,6 +40,47 @@ pub trait SierraToCasmCompiler: Send + Sync {
 pub trait SierraToNativeCompiler: Send + Sync {
     fn compile_to_native(
         &self,
-        contract_class: ContractClass,
+        contract_class: CairoLangContractClass,
     ) -> Result<AotContractExecutor, CompilationUtilError>;
+}
+
+#[derive(Debug, Error)]
+pub enum SierraCompilerError {
+    #[error(transparent)]
+    ClassSerde(#[from] serde_json::Error),
+    #[error(transparent)]
+    CompilationFailed(#[from] CompilationUtilError),
+    #[error("Failed to parse Sierra version: {0}")]
+    SierraVersionFormat(StarknetApiError),
+}
+
+pub type SierraCompilerResult<T> = Result<T, SierraCompilerError>;
+
+// TODO: consider generalizing the compiler if invocation implementations are added.
+#[derive(Clone)]
+pub struct SierraCompiler {
+    compiler: CommandLineCompiler,
+}
+
+impl SierraCompiler {
+    pub fn new(compiler: CommandLineCompiler) -> Self {
+        Self { compiler }
+    }
+
+    // TODO(Elin): move (de)serialization to infra. layer.
+    pub fn compile(&self, class: RawClass) -> SierraCompilerResult<RawExecutableHashedClass> {
+        let class = SierraContractClass::try_from(class)?;
+        let sierra_version = SierraVersion::extract_from_program(&class.sierra_program)
+            .map_err(SierraCompilerError::SierraVersionFormat)?;
+        let class = into_contract_class_for_compilation(&class);
+
+        // TODO(Elin): handle resources (whether here or an infra. layer load-balancing).
+        let executable_class = self.compiler.compile(class)?;
+        // TODO(Elin): consider spawning a worker for hash calculation.
+        let executable_class_hash = CompiledClassHash(executable_class.compiled_class_hash());
+        let executable_class = ContractClass::V1((executable_class, sierra_version));
+        let executable_class = RawExecutableClass::try_from(executable_class)?;
+
+        Ok((executable_class, executable_class_hash))
+    }
 }
