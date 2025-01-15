faca91526c01c30d272077afbbd724ba923f38f3
Alon Lukatch <alon.l@starkware.co>
2025-01-15 14:21:25 +0200

    broken commit  - aligned .proto transaction files, still using old Transaction in consensus and MempoolTransaction

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -57,12 +57,11 @@ fn main() -> io::Result<()> {
             "src/proto/p2p/proto/consensus.proto",
             "src/proto/p2p/proto/mempool/transaction.proto",
             "src/proto/p2p/proto/sync/class.proto",
-            "src/proto/p2p/proto/sync/common.proto", /* built as a dependency so maybe we dont
-                                                      * need to specify here */
             "src/proto/p2p/proto/sync/event.proto",
             "src/proto/p2p/proto/sync/header.proto",
             "src/proto/p2p/proto/sync/state.proto",
             "src/proto/p2p/proto/sync/receipt.proto",
+            "src/proto/p2p/proto/sync/transaction.proto",
             "src/proto/p2p/proto/transaction.proto",
         ],
         &["src/proto/"],
