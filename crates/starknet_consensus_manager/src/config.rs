298f522b8b10f4be90c5124538226f395c360373
Guy Nir <guy.n@starkware.co>
2025-01-15 14:44:01 +0200

    chore(consensus): update ConsensusManagerConfig with new parameters

--- crates/starknet_consensus_manager/src/config.rs
+++ crates/starknet_consensus_manager/src/config.rs
@@ -1,20 +1,26 @@
 use std::collections::BTreeMap;
 
-use papyrus_config::dumping::{append_sub_config_name, SerializeConfig};
-use papyrus_config::{ParamPath, SerializedParam};
+use papyrus_config::dumping::{append_sub_config_name, ser_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use papyrus_consensus::config::ConsensusConfig;
 use papyrus_consensus::types::ContextConfig;
 use papyrus_consensus_orchestrator::cende::CendeConfig;
+use papyrus_network::NetworkConfig;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
 /// The consensus manager related configuration.
 /// TODO(Matan): Remove ConsensusManagerConfig if it's only field remains ConsensusConfig.
-#[derive(Clone, Default, Debug, Serialize, Deserialize, Validate, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
 pub struct ConsensusManagerConfig {
     pub consensus_config: ConsensusConfig,
     pub context_config: ContextConfig,
+    #[validate]
+    pub network_config: NetworkConfig,
     pub cende_config: CendeConfig,
+    pub votes_topic: String,
+    pub proposals_topic: String,
+    pub immediate_active_height: u64,
 }
 
 impl SerializeConfig for ConsensusManagerConfig {
@@ -23,8 +29,45 @@ impl SerializeConfig for ConsensusManagerConfig {
             append_sub_config_name(self.consensus_config.dump(), "consensus_config"),
             append_sub_config_name(self.context_config.dump(), "context_config"),
             append_sub_config_name(self.cende_config.dump(), "cende_config"),
+            append_sub_config_name(self.network_config.dump(), "network_config"),
         ];
+        let mut output_tree: BTreeMap<ParamPath, SerializedParam> =
+            sub_configs.into_iter().flatten().collect();
+        let additional_parameters = BTreeMap::from_iter([
+            ser_param(
+                "votes_topic",
+                &self.votes_topic,
+                "The topic for consensus votes.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "proposals_topic",
+                &self.proposals_topic,
+                "The topic for consensus proposals.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "immediate_active_height",
+                &self.immediate_active_height,
+                "The height at which the consensus manager becomes active.",
+                ParamPrivacyInput::Public,
+            ),
+        ]);
+        output_tree.extend(additional_parameters);
+        output_tree
+    }
+}
 
-        sub_configs.into_iter().flatten().collect()
+impl Default for ConsensusManagerConfig {
+    fn default() -> Self {
+        ConsensusManagerConfig {
+            consensus_config: ConsensusConfig::default(),
+            context_config: ContextConfig::default(),
+            cende_config: CendeConfig::default(),
+            network_config: NetworkConfig::default(),
+            votes_topic: "consensus_votes".to_string(),
+            proposals_topic: "consensus_proposals".to_string(),
+            immediate_active_height: 0,
+        }
     }
 }
