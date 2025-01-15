b2ad564c80fc1792828a1b21c07d69d24dc78fb6
meship-starkware <meshi@starkware.co>
2025-01-15 10:57:54 +0200

    test(cairo_native): add log tests to contract class manger tests

--- crates/blockifier/src/state/contract_class_manager_test.rs
+++ crates/blockifier/src/state/contract_class_manager_test.rs
@@ -1,12 +1,24 @@
-use std::sync::mpsc::sync_channel;
+use std::sync::mpsc::{sync_channel, TrySendError};
 use std::sync::Arc;
 
+use assert_matches::assert_matches;
 use rstest::rstest;
+use starknet_sierra_multicompile::command_line_compiler::CommandLineCompiler;
+use starknet_sierra_multicompile::config::SierraCompilationConfig;
 
-use crate::blockifier::config::CairoNativeRunConfig;
+use crate::blockifier::config::{CairoNativeRunConfig, ContractClassManagerConfig};
 use crate::execution::contract_class::{CompiledClassV1, RunnableCompiledClass};
-use crate::state::contract_class_manager::{CompilationRequest, ContractClassManager};
-use crate::state::global_cache::{ContractCaches, GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST};
+use crate::state::contract_class_manager::{
+    process_compilation_request,
+    CompilationRequest,
+    ContractClassManager,
+    ContractClassManagerError,
+};
+use crate::state::global_cache::{
+    CachedCairoNative,
+    ContractCaches,
+    GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST,
+};
 use crate::test_utils::contracts::FeatureContract;
 use crate::test_utils::{CairoVersion, RunnableCairo1};
 
@@ -30,6 +42,15 @@ fn create_test_request_from_contract(test_contract: FeatureContract) -> Compilat
 fn create_test_request() -> CompilationRequest {
     let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1(RunnableCairo1::Casm));
     create_test_request_from_contract(test_contract)
+}
+
+fn create_faulty_request() -> CompilationRequest {
+    let (class_hash, sierra, casm) = create_test_request();
+    let mut sierra = sierra.as_ref().clone();
+
+    // Truncate the sierra program to trigger an error.
+    sierra.sierra_program = sierra.sierra_program[..100].to_vec();
+    (class_hash, Arc::new(sierra), casm)
 }
 
 #[rstest]
@@ -68,6 +89,30 @@ fn test_start(#[case] run_cairo_native: bool, #[case] wait_on_native_compilation
     }
 }
 
+
+#[rstest]
+#[case::run_native_while_waiting(true, true)]
+#[case::run_native_without_waiting(true, false)]
+#[should_panic(expected = "Native compilation is disabled.")]
+#[case::run_without_native(false, false)]
+fn test_send_compilation_request(
+    #[case] run_cairo_native: bool,
+    #[case] wait_on_native_compilation: bool,
+) {
+    let native_config =
+    CairoNativeRunConfig { run_cairo_native, wait_on_native_compilation, ..Default::default() };
+    let config =
+    ContractClassManagerConfig { cairo_native_run_config: native_config, ..Default::default() };
+    let manager = ContractClassManager::start(config);
+    let request = create_test_request();
+    let class_hash = request.0;
+    let res = manager.send_compilation_request(request);
+    assert!(res.is_ok(), "Compilation request failed with the follow error {}.", res.unwrap_err());
+    if wait_on_native_compilation {
+        assert_matches!(manager.get_native(&class_hash).unwrap(), CachedCairoNative::Compiled(_));
+    }
+}
+
 #[test]
 #[should_panic(expected = "Compilation request channel is closed.")]
 fn test_send_compilation_request_channel_disconnected() {
@@ -90,4 +135,65 @@ fn test_send_compilation_request_channel_disconnected() {
     // Sending request with a disconnected channel should panic.
     let request = create_test_request();
     manager.send_compilation_request(request).unwrap();
+}
+
+#[test]
+fn test_send_compilation_request_channel_full() {
+    let native_config = CairoNativeRunConfig {
+        run_cairo_native: true,
+        wait_on_native_compilation: false,
+        channel_size: 0,
+    };
+    let manager = ContractClassManager::create_for_testing(native_config);
+    let request = create_test_request();
+    assert!(manager.sender.is_some(), "Sender should be Some");
+
+    // Fill the channel (it can only hold 1 message)
+    manager.send_compilation_request(request.clone()).unwrap();
+    // Should log an error without panicking
+    let result = manager.send_compilation_request(request.clone());
+    assert!(result.is_err(), "Compilation request should not be sent successfully.");
+
+    assert_eq!(
+        result.unwrap_err(),
+        ContractClassManagerError::TrySendError(TrySendError::Full(request))
+    );
+}
+
+#[rstest]
+#[case::success(create_test_request(), true)]
+#[case::failure(create_faulty_request(), false)]
+fn test_process_compilation_request(
+    #[case] request: CompilationRequest,
+    #[case] should_pass: bool,
+) {
+    use starknet_sierra_multicompile::errors::CompilationUtilError;
+
+    let native_config = CairoNativeRunConfig {
+        run_cairo_native: true,
+        channel_size: TEST_CHANNEL_SIZE,
+        wait_on_native_compilation: true,
+    };
+    let manager = ContractClassManager::create_for_testing(native_config);
+    let compiler_config = SierraCompilationConfig::default();
+    let compiler = Arc::new(CommandLineCompiler::new(compiler_config));
+    let res = process_compilation_request(
+        manager.contract_caches.clone(),
+        compiler.clone(),
+        request.clone(),
+    );
+
+    if should_pass {
+        assert!(res.is_ok(), "Compilation failed with error{}.", res.unwrap_err());
+        assert_matches!(manager.get_native(&request.0).unwrap(), CachedCairoNative::Compiled(_));
+    } else {
+        assert_matches!(
+            res.unwrap_err(),
+            ContractClassManagerError::CompilationError(CompilationUtilError::CompilationError(_))
+        );
+        assert_matches!(
+            manager.get_native(&request.0).unwrap(),
+            CachedCairoNative::CompilationFailed
+        );
+    }
 }
