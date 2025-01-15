53b06ea9b83f965d28551e7a8c2c71d6446b3616
giladchase <gilad@starkware.co>
2025-01-15 15:33:12 +0200

    feat(starknet_l1_provider): add `SoftDeleteIndexMap` (#3208)
    Previous implementation inside transaction manager didn't account for
    validate, the current implementation is more general, and can in the
    future perhaps be reused elsewhere after some changes are made.
    
    Co-authored-by: Gilad Chase <gilad@starkware.com>

--- crates/starknet_l1_provider_types/src/lib.rs
+++ crates/starknet_l1_provider_types/src/lib.rs
@@ -23,6 +23,7 @@ pub type SharedL1ProviderClient = Arc<dyn L1ProviderClient>;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum ValidationStatus {
+    AlreadyIncludedInPropsedBlock,
     AlreadyIncludedOnL2,
     ConsumedOnL1OrUnknown,
     Validated,
