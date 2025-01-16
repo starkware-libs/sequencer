f8e3c0cf77575874db5256b57eef4d54bbf9ed12
Gilad Chase <gilad@starkware.com>
2025-01-16 17:41:19 +0200

    feat(starknet_l1_provider): add commit_block

--- crates/starknet_l1_provider_types/src/errors.rs
+++ crates/starknet_l1_provider_types/src/errors.rs
@@ -7,21 +7,19 @@ use thiserror::Error;
 
 #[derive(Clone, Debug, Error, PartialEq, Eq, Serialize, Deserialize)]
 pub enum L1ProviderError {
-    #[error(
-        "`get_txs` called while in `Pending` state, likely due to a crash; restart block proposal"
-    )]
-    GetTransactionsInPendingState,
-    #[error("`get_txs` while in validate state")]
+    #[error("`get_txs` while in `Validate` state")]
     GetTransactionConsensusBug,
+    // This is likely due to a crash, restart block proposal.
+    #[error("`get_txs` called when provider is not in proposer state")]
+    OutOfSessionGetTransactions,
+    // This is likely due to a crash, restart block proposal.
+    #[error("`validate` called when provider is not in proposer state")]
+    OutOfSessionValidate,
     #[error("Unexpected height: expected {expected}, got {got}")]
     UnexpectedHeight { expected: BlockNumber, got: BlockNumber },
     #[error("Cannot transition from {from} to {to}")]
     UnexpectedProviderStateTransition { from: String, to: String },
-    #[error(
-        "`validate` called while in `Pending` state, likely due to a crash; restart block proposal"
-    )]
-    ValidateInPendingState,
-    #[error("`validate` called while in `Propose`")]
+    #[error("`validate` called while in `Propose` state")]
     ValidateTransactionConsensusBug,
 }
 
