ede993374568026ab41061881bf75cc6d9a8cf46
Yonatan-Starkware <yonatan.k@starkware.co>
2025-01-16 15:11:30 +0200

    test(blockifier): add builtins_test

--- crates/blockifier/src/execution/syscalls/syscall_tests/get_execution_info.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/get_execution_info.rs
@@ -90,7 +90,7 @@ use crate::transaction::objects::{
     ExecutionMode::Execute,
     TransactionVersion::ONE,
     false;
-    "Native: Legacy contract. Execute execution mode: block info should be as usual. Transaction 
+    "Native: Legacy contract. Execute execution mode: block info should be as usual. Transaction
     V1."
     )
 )]
@@ -101,7 +101,7 @@ use crate::transaction::objects::{
     ExecutionMode::Execute,
     TransactionVersion::THREE,
     false;
-    "Native: Legacy contract. Execute execution mode: block info should be as usual. Transaction 
+    "Native: Legacy contract. Execute execution mode: block info should be as usual. Transaction
     V3."
     )
 )]
