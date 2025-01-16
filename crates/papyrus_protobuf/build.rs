e04713ee7cfe8eb8b19bca57ebe021d950e4dae4
Alon Lukatch <alon.l@starkware.co>
2025-01-16 16:50:43 +0200

    refactor(papyrus_protobuf): align transaction.proto files

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -61,6 +61,7 @@ fn main() -> io::Result<()> {
             "src/proto/p2p/proto/sync/header.proto",
             "src/proto/p2p/proto/sync/state.proto",
             "src/proto/p2p/proto/sync/receipt.proto",
+            "src/proto/p2p/proto/sync/transaction.proto",
             "src/proto/p2p/proto/transaction.proto",
         ],
         &["src/proto/"],
