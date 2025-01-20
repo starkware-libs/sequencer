42fe293a8cbcf01dd1824ef6f67d5e52477b2d9c
Elin Tulchinsky <elin@starkware.co>
2025-01-20 14:29:28 +0200

    feat(papyrus_storage): add class hash to executable class hash table
    Will act as the atomic marker of a class, indicating its existence in
    the class manager.

---
+++ crates/papyrus_storage/src/class_hash.rs
@@ -0,0 +1,64 @@
+//! Interface for handling hashes of Starknet [classes (Cairo 1)](https://docs.rs/starknet_api/latest/starknet_api/state/struct.ContractClass.html).
+//! This is a table separate from Papyrus storage; scope and version do not apply on it.
+//! Use carefully, only within class manager code, which is responsible for maintaining this table.
+//!
+//! Import [`ClassHashStorageReader`] and [`ClassHashStorageWriter`] to read and write data related
+//! to classes using a [`StorageTxn`].
+
+use starknet_api::core::{ClassHash, CompiledClassHash};
+
+use crate::db::table_types::Table;
+use crate::db::{TransactionKind, RW};
+use crate::{StorageResult, StorageTxn};
+
+#[cfg(test)]
+#[path = "class_hash_test.rs"]
+mod class_hash_test;
+
+// TODO(Elin): consider implementing directly over `libmdbx`.
+
+/// Interface for reading executable class hashes.
+pub trait ClassHashStorageReader {
+    /// Returns the executable class hash corresponding to the given class hash.
+    /// Returns `None` if the class hash is not found.
+    fn get_executable_class_hash(
+        &self,
+        class_hash: &ClassHash,
+    ) -> StorageResult<Option<CompiledClassHash>>;
+}
+
+/// Interface for writing executable class hashes.
+pub trait ClassHashStorageWriter
+where
+    Self: Sized,
+{
+    /// Inserts the executable class hash corresponding to the given class hash.
+    /// An error is returned if the class hash already exists.
+    fn set_executable_class_hash(
+        self,
+        class_hash: &ClassHash,
+        executable_class_hash: CompiledClassHash,
+    ) -> StorageResult<Self>;
+}
+
+impl<Mode: TransactionKind> ClassHashStorageReader for StorageTxn<'_, Mode> {
+    fn get_executable_class_hash(
+        &self,
+        class_hash: &ClassHash,
+    ) -> StorageResult<Option<CompiledClassHash>> {
+        let table = self.open_table(&self.tables.class_hash_to_executable_class_hash)?;
+        Ok(table.get(&self.txn, class_hash)?)
+    }
+}
+
+impl ClassHashStorageWriter for StorageTxn<'_, RW> {
+    fn set_executable_class_hash(
+        self,
+        class_hash: &ClassHash,
+        executable_class_hash: CompiledClassHash,
+    ) -> StorageResult<Self> {
+        let table = self.open_table(&self.tables.class_hash_to_executable_class_hash)?;
+        table.insert(&self.txn, class_hash, &executable_class_hash)?;
+        Ok(self)
+    }
+}
