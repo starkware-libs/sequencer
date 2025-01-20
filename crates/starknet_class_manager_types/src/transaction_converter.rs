9bc5ddc9ad077ab401c1528ea88e13dedb12321a
Shahak Shama <shahak@starkware.co>
2025-01-20 16:04:40 +0200

    feat(starknet_class_manager_types): implement rpc to and from internal rpc converter

--- crates/starknet_class_manager_types/src/transaction_converter.rs
+++ crates/starknet_class_manager_types/src/transaction_converter.rs
@@ -1,7 +1,17 @@
 use async_trait::async_trait;
 use starknet_api::consensus_transaction::{ConsensusTransaction, InternalConsensusTransaction};
+use starknet_api::core::ChainId;
 use starknet_api::executable_transaction::AccountTransaction;
-use starknet_api::rpc_transaction::{InternalRpcTransaction, RpcTransaction};
+use starknet_api::rpc_transaction::{
+    DeployAccountTransactionV3WithAddress,
+    InternalRpcTransaction,
+    InternalRpcTransactionWithoutTxHash,
+    RpcDeclareTransaction,
+    RpcDeclareTransactionV3,
+    RpcDeployAccountTransaction,
+    RpcTransaction,
+};
+use starknet_api::transaction::CalculateContractAddress;
 use starknet_api::{executable_transaction, transaction, StarknetApiError};
 use thiserror::Error;
 
@@ -46,7 +56,8 @@ pub trait TransactionConverterTrait {
 }
 
 pub struct TransactionConverter {
-    _class_manager_client: SharedClassManagerClient,
+    class_manager_client: SharedClassManagerClient,
+    chain_id: ChainId,
 }
 
 #[async_trait]
@@ -67,16 +78,60 @@ impl TransactionConverterTrait for TransactionConverter {
 
     async fn convert_internal_rpc_tx_to_rpc_tx(
         &self,
-        _tx: InternalRpcTransaction,
+        tx: InternalRpcTransaction,
     ) -> TransactionConverterResult<RpcTransaction> {
-        todo!()
+        match tx.tx {
+            InternalRpcTransactionWithoutTxHash::Invoke(tx) => Ok(RpcTransaction::Invoke(tx)),
+            InternalRpcTransactionWithoutTxHash::Declare(tx) => {
+                let contract_class = self.class_manager_client.get_sierra(tx.class_hash).await?;
+
+                Ok(RpcTransaction::Declare(RpcDeclareTransaction::V3(RpcDeclareTransactionV3 {
+                    sender_address: tx.sender_address,
+                    compiled_class_hash: tx.compiled_class_hash,
+                    signature: tx.signature,
+                    nonce: tx.nonce,
+                    contract_class,
+                    resource_bounds: tx.resource_bounds,
+                    tip: tx.tip,
+                    paymaster_data: tx.paymaster_data,
+                    account_deployment_data: tx.account_deployment_data,
+                    nonce_data_availability_mode: tx.nonce_data_availability_mode,
+                    fee_data_availability_mode: tx.fee_data_availability_mode,
+                })))
+            }
+            InternalRpcTransactionWithoutTxHash::DeployAccount(
+                DeployAccountTransactionV3WithAddress { tx, .. },
+            ) => Ok(RpcTransaction::DeployAccount(tx)),
+        }
     }
 
     async fn convert_rpc_tx_to_internal_rpc_tx(
         &self,
-        _tx: RpcTransaction,
+        tx: RpcTransaction,
     ) -> TransactionConverterResult<InternalRpcTransaction> {
-        todo!()
+        // TODO(Arni): add calculate_transaction_hash to rpc transaction and use it here.
+        let starknet_api_tx = starknet_api::transaction::Transaction::from(tx.clone());
+        let tx_hash = starknet_api_tx.calculate_transaction_hash(&self.chain_id)?;
+
+        let tx_without_hash = match tx {
+            RpcTransaction::Invoke(tx) => InternalRpcTransactionWithoutTxHash::Invoke(tx),
+            RpcTransaction::Declare(_tx) => {
+                // TODO(shahak): Implement this once Elin adds class hash to return value of
+                // add_class.
+                todo!()
+            }
+            RpcTransaction::DeployAccount(RpcDeployAccountTransaction::V3(tx)) => {
+                let contract_address = tx.calculate_contract_address()?;
+                InternalRpcTransactionWithoutTxHash::DeployAccount(
+                    DeployAccountTransactionV3WithAddress {
+                        tx: RpcDeployAccountTransaction::V3(tx),
+                        contract_address,
+                    },
+                )
+            }
+        };
+
+        Ok(InternalRpcTransaction { tx: tx_without_hash, tx_hash })
     }
 
     async fn convert_internal_rpc_tx_to_executable_tx(
