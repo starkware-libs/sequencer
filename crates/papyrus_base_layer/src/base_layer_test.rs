45c27b7579210bd14d06d9602e9b285e069ef012
Gilad Chase <gilad@starkware.com>
2025-01-21 15:59:11 +0200

    feat(papyrus_base_layer): generalize get_latest_proved_block
    Allow any L1 block number. Will be used soon for initializing the
    `L1Provider`.
    Add test for error flow (happy flow is already covered by the
    `latest_proved_block_ethereum` test)

--- crates/papyrus_base_layer/src/base_layer_test.rs
+++ crates/papyrus_base_layer/src/base_layer_test.rs
@@ -1,15 +1,25 @@
+use alloy_primitives::Address as EthereumContractAddress;
 use pretty_assertions::assert_eq;
 use starknet_api::block::{BlockHash, BlockHashAndNumber, BlockNumber};
 use starknet_api::felt;
+use url::Url;
 
 use crate::ethereum_base_layer_contract::{EthereumBaseLayerConfig, EthereumBaseLayerContract};
-use crate::test_utils::get_test_ethereum_node;
+use crate::test_utils::{anvil, get_test_ethereum_node};
 use crate::BaseLayerContract;
 
 // TODO(Gilad): move to global test_utils crate and use everywhere instead of relying on the
 // confusing `#[ignore]` api to mark slow tests.
 fn in_ci() -> bool {
     std::env::var("CI").is_ok()
+}
+
+fn ethereum_base_layer_contract(
+    node_url: Url,
+    starknet_contract_address: EthereumContractAddress,
+) -> EthereumBaseLayerContract {
+    let config = EthereumBaseLayerConfig { node_url, starknet_contract_address };
+    EthereumBaseLayerContract::new(config)
 }
 
 #[tokio::test]
@@ -20,11 +30,8 @@ async fn latest_proved_block_ethereum() {
     }
 
     let (node_handle, starknet_contract_address) = get_test_ethereum_node();
-    let config = EthereumBaseLayerConfig {
-        node_url: node_handle.0.endpoint().parse().unwrap(),
-        starknet_contract_address,
-    };
-    let contract = EthereumBaseLayerContract::new(config);
+    let node_url = node_handle.0.endpoint().parse().unwrap();
+    let contract = ethereum_base_layer_contract(node_url, starknet_contract_address);
 
     let first_sn_state_update =
         BlockHashAndNumber { number: BlockNumber(100), hash: BlockHash(felt!("0x100")) };
@@ -45,4 +52,30 @@ async fn latest_proved_block_ethereum() {
         let latest_block = contract.latest_proved_block(scenario).await.unwrap();
         assert_eq!(latest_block, expected);
     }
+}
+
+#[tokio::test]
+// Note: the test requires ganache-cli installed, otherwise it is ignored.
+async fn get_proved_block_at_unknown_block_number() {
+    if !in_ci() {
+        return;
+    }
+
+    // TODO(Gilad): Moved into test-utils in an upcoming PR.
+    const DEFAULT_ANVIL_DEPLOY_ADDRESS: &str = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+    let anvil = anvil();
+    let node_url = anvil.endpoint_url();
+    let starknet_contract_address = DEFAULT_ANVIL_DEPLOY_ADDRESS.parse().unwrap();
+    let contract = ethereum_base_layer_contract(node_url, starknet_contract_address);
+
+    assert!(
+        contract
+            .get_proved_block_at(123)
+            .await
+            .unwrap_err()
+            // This error is nested way to deep.
+            .to_string()
+            .contains("BlockOutOfRangeError")
+    );
 }
