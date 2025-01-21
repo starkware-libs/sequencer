2b790260237186b6b91c20ba2270a066c8726f24
Itay Tsabary <itayt@starkware.co>
2025-01-21 16:08:31 +0200

    chore(starknet_sequencer_metrics): add initial value to counter metrics
    commit-id:b8fcb50f

--- crates/starknet_batcher/src/metrics.rs
+++ crates/starknet_batcher/src/metrics.rs
@@ -9,25 +9,37 @@ pub const STORAGE_HEIGHT: MetricGauge = MetricGauge {
 };
 
 // Proposal metrics.
-pub const PROPOSAL_STARTED: MetricCounter =
-    MetricCounter { name: "batcher_proposal_started", description: "Counter of proposals started" };
+pub const PROPOSAL_STARTED: MetricCounter = MetricCounter {
+    name: "batcher_proposal_started",
+    description: "Counter of proposals started",
+    initial_value: 0,
+};
 pub const PROPOSAL_SUCCEEDED: MetricCounter = MetricCounter {
     name: "batcher_proposal_succeeded",
     description: "Counter of successful proposals",
+    initial_value: 0,
 };
-pub const PROPOSAL_FAILED: MetricCounter =
-    MetricCounter { name: "batcher_proposal_failed", description: "Counter of failed proposals" };
-pub const PROPOSAL_ABORTED: MetricCounter =
-    MetricCounter { name: "batcher_proposal_aborted", description: "Counter of aborted proposals" };
+pub const PROPOSAL_FAILED: MetricCounter = MetricCounter {
+    name: "batcher_proposal_failed",
+    description: "Counter of failed proposals",
+    initial_value: 0,
+};
+pub const PROPOSAL_ABORTED: MetricCounter = MetricCounter {
+    name: "batcher_proposal_aborted",
+    description: "Counter of aborted proposals",
+    initial_value: 0,
+};
 
 // Transaction metrics.
 pub const BATCHED_TRANSACTIONS: MetricCounter = MetricCounter {
     name: "batcher_batched_transactions",
     description: "Counter of batched transactions",
+    initial_value: 0,
 };
 pub const REJECTED_TRANSACTIONS: MetricCounter = MetricCounter {
     name: "batcher_rejected_transactions",
     description: "Counter of rejected transactions",
+    initial_value: 0,
 };
 
 pub fn register_metrics(storage_height: BlockNumber) {
@@ -36,19 +48,19 @@ pub fn register_metrics(storage_height: BlockNumber) {
     #[allow(clippy::as_conversions)]
     storage_height_metric.set(storage_height.0 as f64);
 
-    counter!(PROPOSAL_STARTED.name).absolute(0);
+    counter!(PROPOSAL_STARTED.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(PROPOSAL_STARTED.name, PROPOSAL_STARTED.description);
-    counter!(PROPOSAL_SUCCEEDED.name).absolute(0);
+    counter!(PROPOSAL_SUCCEEDED.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(PROPOSAL_SUCCEEDED.name, PROPOSAL_SUCCEEDED.description);
-    counter!(PROPOSAL_FAILED.name).absolute(0);
+    counter!(PROPOSAL_FAILED.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(PROPOSAL_FAILED.name, PROPOSAL_FAILED.description);
-    counter!(PROPOSAL_ABORTED.name).absolute(0);
+    counter!(PROPOSAL_ABORTED.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(PROPOSAL_ABORTED.name, PROPOSAL_ABORTED.description);
 
     // In case of revert, consider calling `absolute`.
-    counter!(BATCHED_TRANSACTIONS.name).absolute(0);
+    counter!(BATCHED_TRANSACTIONS.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(BATCHED_TRANSACTIONS.name, BATCHED_TRANSACTIONS.description);
-    counter!(REJECTED_TRANSACTIONS.name).absolute(0);
+    counter!(REJECTED_TRANSACTIONS.name).absolute(PROPOSAL_STARTED.initial_value);
     describe_counter!(REJECTED_TRANSACTIONS.name, REJECTED_TRANSACTIONS.description);
 }
 
--- crates/starknet_sequencer_metrics/src/metrics.rs
+++ crates/starknet_sequencer_metrics/src/metrics.rs
@@ -1,6 +1,7 @@
 pub struct MetricCounter {
     pub name: &'static str,
     pub description: &'static str,
+    pub initial_value: u64,
 }
 
 pub struct MetricGauge {
