534dbda81ffc0b89d75dd81890373f532f8e06fe
Noam Spiegelstein <noam.s@starkware.co>
2025-01-21 20:35:32 +0200

    fix(starknet_gateway): remove compiled_class_hash_mismatch test
    temporary removal of test until class manager is implemented.

--- crates/starknet_gateway/src/gateway_test.rs
+++ crates/starknet_gateway/src/gateway_test.rs
@@ -163,6 +163,7 @@ async fn test_add_tx(
 
 #[rstest]
 #[tokio::test]
+#[ignore]
 async fn test_compiled_class_hash_mismatch(mock_dependencies: MockDependencies) {
     let mut declare_tx =
         assert_matches!(declare_tx(), RpcTransaction::Declare(RpcDeclareTransaction::V3(tx)) => tx);
