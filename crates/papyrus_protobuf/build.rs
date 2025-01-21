659531dbc96a1e5f46357994788e63eee851549c
Alon-Lukatch-Starkware <alon.l@starkware.co>
2025-01-21 13:16:14 +0200

    refactor(papyrus_protobuf): align header.proto (#3388)

--- crates/papyrus_protobuf/build.rs
+++ crates/papyrus_protobuf/build.rs
@@ -55,10 +55,10 @@ fn main() -> io::Result<()> {
         &[
             "src/proto/p2p/proto/class.proto",
             "src/proto/p2p/proto/consensus.proto",
-            "src/proto/p2p/proto/header.proto",
             "src/proto/p2p/proto/mempool/transaction.proto",
             "src/proto/p2p/proto/sync/class.proto",
             "src/proto/p2p/proto/sync/event.proto",
+            "src/proto/p2p/proto/sync/header.proto",
             "src/proto/p2p/proto/sync/state.proto",
             "src/proto/p2p/proto/transaction.proto",
         ],
