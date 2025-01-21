a40c0e6f5a37ccafeb81d391b833b31028d3b629
Yair Bakalchuk <yair@starkware.co>
2025-01-21 11:50:14 +0200

    chore(papyrus_common): remove unused code (moved to sn_api)

--- crates/papyrus_common/src/lib.rs
+++ crates/papyrus_common/src/lib.rs
@@ -1,6 +1,3 @@
-use starknet_types_core::felt::Felt;
-
-pub mod class_hash;
 pub mod compression_utils;
 pub mod deprecated_class_abi;
 pub mod metrics;
@@ -8,7 +5,3 @@ pub mod pending_classes;
 pub mod python_json;
 pub mod state;
 pub mod storage_query;
-
-pub(crate) fn usize_into_felt(u: usize) -> Felt {
-    u128::try_from(u).expect("Expect at most 128 bits").into()
-}
