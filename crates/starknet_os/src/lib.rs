9e5881bf72a919c7c7350a4cbdc5f073c684c52f
Yoav Gross <yoav@starkware.co>
2025-01-22 17:07:37 +0200

    feat: compute hash of deprecated classes with hints

--- crates/starknet_os/src/lib.rs
+++ crates/starknet_os/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
+pub mod hinted_class_hash;
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod hinted_class_hash_test;
