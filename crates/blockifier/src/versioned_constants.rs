c6641d100e2eefc5b8cdc1f8cc33a74979b881bb
Yonatan-Starkware <yonatan.k@starkware.co>
2025-01-23 16:13:20 +0200

    chore(blockifier): add range_check96_gas_cost

--- crates/blockifier/src/versioned_constants.rs
+++ crates/blockifier/src/versioned_constants.rs
@@ -791,6 +791,7 @@ pub struct BuiltinGasCosts {
     // Range check has a hard-coded cost higher than its proof percentage to avoid the overhead of
     // retrieving its price from the table.
     pub range_check: u64,
+    pub range_check96: u64,
     // Priced builtins.
     pub keccak: u64,
     pub pedersen: u64,
@@ -806,12 +807,13 @@ impl BuiltinGasCosts {
     pub fn get_builtin_gas_cost(&self, builtin: &BuiltinName) -> Result<u64, GasCostsError> {
         let gas_cost = match *builtin {
             BuiltinName::range_check => self.range_check,
+            BuiltinName::range_check96 => self.range_check96,
             BuiltinName::pedersen => self.pedersen,
             BuiltinName::bitwise => self.bitwise,
             BuiltinName::ec_op => self.ecop,
             BuiltinName::keccak => self.keccak,
             BuiltinName::poseidon => self.poseidon,
-            BuiltinName::range_check96 => self.range_check,
+            BuiltinName::range_check96 => self.range_check96,
             BuiltinName::add_mod => self.add_mod,
             BuiltinName::mul_mod => self.mul_mod,
             BuiltinName::ecdsa => self.ecdsa,
