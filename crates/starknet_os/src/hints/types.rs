965b6626569b957a110feb3570cf01fe144a9ca3
Dori Medini <dori@starkware.co>
2025-01-23 12:36:41 +0200

    feat(starknet_os): initial integration of hints, starting from block_context.rs
    Signed-off-by: Dori Medini <dori@starkware.co>

---
+++ crates/starknet_os/src/hints/types.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::{HintProcessor, HintReference};
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::{HintExtensionResult, HintResult, OsHintError};
+
+/// Hint enums map between the cairo-lang hint string and the hint implementation.
+pub trait HintEnum {
+    fn from_str(hint_str: &str) -> Result<Self, OsHintError>
+    where
+        Self: Sized;
+
+    fn to_str(&self) -> &'static str;
+}
+
+/// Hint implementations execute the hint logic.
+pub trait HintImplementation {
+    fn execute_hint(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        ids_data: &HashMap<String, HintReference>,
+        ap_tracking: &ApTracking,
+        constants: &HashMap<String, Felt>,
+    ) -> HintResult;
+}
+
+/// Hint extensions extend the current map of hints used by the VM.
+/// This behaviour achieves what the `vm_load_data` primitive does for cairo-lang and is needed to
+/// implement OS hints like `vm_load_program`.
+pub trait HintExtensionImplementation {
+    fn execute_hint_extensive(
+        &self,
+        hint_processor: &dyn HintProcessor,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        ids_data: &HashMap<String, HintReference>,
+        ap_tracking: &ApTracking,
+    ) -> HintExtensionResult;
+}
