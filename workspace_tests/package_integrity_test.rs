f11293bbd61ee32f3210de4e3429e946fe5173ab
Dori Medini <dori@starkware.co>
2025-01-24 19:55:33 +0200

    feat(workspace_tests): drop path requirement for dev dependencies

--- workspace_tests/package_integrity_test.rs
+++ workspace_tests/package_integrity_test.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::toml_utils::{CrateCargoToml, DependencyValue, PackageEntryValue, MEMBER_TOMLS};
+use crate::toml_utils::{DependencyValue, PackageEntryValue, MEMBER_TOMLS};
 
 /// Hard-coded list of crates that are allowed to use test code in their (non-dev) dependencies.
 /// Should only contain test-related crates.
@@ -108,58 +108,5 @@ fn test_no_testing_feature_in_business_logic() {
         "The following crates have (non-testing) dependencies with the 'testing' feature \
          activated. If the crate is a test crate, add it to {}.\n{testing_feature_deps:#?}",
         stringify!(CRATES_ALLOWED_TO_USE_TESTING_FEATURE)
-    );
-}
-
-/// For smooth publishing: all *local* (workspace member) dependencies in [dev-dependencies] should
-/// be via path, and not `workspace = true`.
-/// Reason:
-/// - `cargo publish` ignores path dependencies in dev-dependencies.
-/// - `cargo publish` DOES NOT ignore `workspace = true` dependencies in dev-dependencies, even if
-///   the workspace toml defines the dependency via path.
-/// - We do not need dev-dependencies published when publishing a crate.
-/// - We sometimes use self-references in dev-dependencies to activate features of self in tests.
-///   For example, starknet_api needs it's "testing" feature activated in test mode, so it has a
-///   dependency on self (starknet_api) in dev-dependencies. If we fail to ignore this self
-///   dependency, we will not be able to publish starknet_api.
-#[test]
-fn test_member_dev_dependencies_are_by_path() {
-    let member_crate_names: Vec<&String> =
-        MEMBER_TOMLS.values().map(CrateCargoToml::package_name).collect();
-    let package_to_bad_local_dev_deps: Vec<(String, Vec<String>)> = MEMBER_TOMLS
-        .iter()
-        .filter_map(|(path_str, toml)| {
-            if let Some(ref dev_dependencies) = toml.dev_dependencies {
-                // For each dep in dev-dependencies: if it's a workspace member, and not by path,
-                // add it to the list of bad local dev deps.
-                let mut bad_local_dev_deps: Vec<String> = dev_dependencies
-                    .iter()
-                    .filter_map(|(name, value)| {
-                        if member_crate_names.contains(&name) {
-                            match value {
-                                DependencyValue::Object { path: Some(_), .. } => None,
-                                _ => Some(name.clone()),
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                    .collect();
-                // If bad dev-deps exist for this package, return the package name and the bad deps.
-                if bad_local_dev_deps.is_empty() {
-                    None
-                } else {
-                    bad_local_dev_deps.sort();
-                    Some((path_str.clone(), bad_local_dev_deps))
-                }
-            } else {
-                None
-            }
-        })
-        .collect();
-    assert!(
-        package_to_bad_local_dev_deps.is_empty(),
-        "The following crates have local packages in the [dev-dependencies] area that are not by \
-         path: {package_to_bad_local_dev_deps:#?}."
     );
 }
