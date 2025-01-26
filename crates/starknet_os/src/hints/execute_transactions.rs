7ae02051ff71d35b86f6973b8bea4f3094c4768d
Dori Medini <dori@starkware.co>
2025-01-26 14:39:07 +0200

    feat(starknet_os): integrate execute_transactions hints

---
+++ crates/starknet_os/src/hints/execute_transactions.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::HintResult;
+
+pub fn start_tx_validate_declare_execution_context(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_sha256_segment_in_syscall_handler(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn log_remaining_txs(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn fill_holes_in_rc96_segment(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_component_hashes(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn sha2_finalize(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
