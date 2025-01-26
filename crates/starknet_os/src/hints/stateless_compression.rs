c503771f2bc56ef165d980e8ac5ac5d065c59a0e
Dori Medini <dori@starkware.co>
2025-01-26 14:29:51 +0200

    feat(starknet_os): integrate compression hints

---
+++ crates/starknet_os/src/hints/stateless_compression.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::HintResult;
+
+pub fn dictionary_from_bucket(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn get_prev_offset(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn compression_hint(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_decompressed_dst(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
