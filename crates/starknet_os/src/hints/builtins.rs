76105ea3d79f3580fed6f371ad2323d10ddfa18d
Dori Medini <dori@starkware.co>
2025-01-26 14:19:40 +0200

    feat(starknet_os): integrate builtins hints

---
+++ crates/starknet_os/src/hints/builtins.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::HintResult;
+
+pub const SELECTED_BUILTINS: &str =
+    "vm_enter_scope({'n_selected_builtins': ids.n_selected_builtins})";
+pub fn selected_builtins(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn select_builtin(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn update_builtin_ptrs(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
