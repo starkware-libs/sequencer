0a447c2fdd1eea2b7c652e3e0d4b95f46bbca372
Elin <elin@starkware.co>
2025-01-26 14:59:35 +0200

    feat(papyrus_storage): add class hash to executable class hash table (#3514)
    Will act as the atomic marker of a class, indicating its existence in
    the class manager.

---
+++ crates/papyrus_storage/src/class_hash_test.rs
@@ -0,0 +1,31 @@
+use starknet_api::core::{ClassHash, CompiledClassHash};
+use starknet_api::felt;
+
+use crate::class_hash::{ClassHashStorageReader, ClassHashStorageWriter};
+use crate::test_utils::get_test_storage;
+
+#[test]
+fn class_hash_storage() {
+    let ((reader, mut writer), _temp_dir) = get_test_storage();
+
+    // Non-existent entry.
+    let class_hash = ClassHash(felt!("0x1234"));
+    let executable_class_hash =
+        reader.begin_ro_txn().unwrap().get_executable_class_hash(&class_hash).unwrap();
+    assert_eq!(executable_class_hash, None);
+
+    // Insert an entry.
+    let expected_executable_class_hash = CompiledClassHash(felt!("0x5678"));
+    writer
+        .begin_rw_txn()
+        .unwrap()
+        .set_executable_class_hash(&class_hash, expected_executable_class_hash)
+        .unwrap()
+        .commit()
+        .unwrap();
+
+    // Read the inserted entry.
+    let executable_class_hash =
+        reader.begin_ro_txn().unwrap().get_executable_class_hash(&class_hash).unwrap();
+    assert_eq!(executable_class_hash, Some(expected_executable_class_hash));
+}
