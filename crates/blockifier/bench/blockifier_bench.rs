df1522929a7db23b4e7c8c7d4806f8474fc30b1f
Arni Hod <arnon@starkware.co>
2025-01-27 09:31:16 +0200

    WIP2

--- crates/blockifier/bench/blockifier_bench.rs
+++ crates/blockifier/bench/blockifier_bench.rs
@@ -15,6 +15,24 @@ use blockifier::test_utils::transfers_generator::{
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn transfers_benchmark(c: &mut Criterion) {
+    // Request jemalloc statistics
+    let mut buf = [0u8; 1024];
+    let stats = unsafe {
+        tikv_jemalloc_sys::mallctl(
+            b"stats.print\0".as_ptr() as *const _,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            buf.as_mut_ptr() as *mut _,
+            buf.len(),
+        )
+    };
+
+    if stats == 0 {
+        println!("Jemalloc stats:\n{}", String::from_utf8_lossy(&buf));
+    } else {
+        eprintln!("Failed to fetch jemalloc stats");
+    }
+
     let transfers_generator_config = TransfersGeneratorConfig {
         recipient_generator_type: RecipientGeneratorType::Random,
         ..Default::default()
