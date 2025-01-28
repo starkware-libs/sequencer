4ab85cb331d4fe32b3b70f03af94ffdcb5fe3f05
dorimedini-starkware <dori@starkware.co>
2025-01-28 11:54:05 +0200

    feat(starknet_os): integrate bls_field hints (#3683)
    commit-id:457cbaf9

---
+++ crates/starknet_os/src/hints/bls_field.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::HintResult;
+
+/// From the Cairo code, we can make the current assumptions:
+///
+/// * The limbs of value are in the range [0, BASE * 3).
+/// * value is in the range [0, 2 ** 256).
+pub fn compute_ids_low(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
