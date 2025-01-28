5f67e6039e87dea557553179bc8fa1a4726b496e
dorimedini-starkware <dori@starkware.co>
2025-01-28 18:48:50 +0200

    feat(starknet_os): integrate output hints (#3694)
    commit-id:71d9112b

---
+++ crates/starknet_os/src/hints/output.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::HintResult;
+
+pub fn set_tree_structure(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_state_updates_start(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_compressed_start(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
+
+pub fn set_n_updates_small(
+    _vm: &mut VirtualMachine,
+    _exec_scopes: &mut ExecutionScopes,
+    _ids_data: &HashMap<String, HintReference>,
+    _ap_tracking: &ApTracking,
+    _constants: &HashMap<String, Felt>,
+) -> HintResult {
+    todo!()
+}
