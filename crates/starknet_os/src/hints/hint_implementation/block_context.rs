28aadbe80b73cb2ea708ccf95155ea6892ba501e
dorimedini-starkware <dori@starkware.co>
2025-01-29 18:18:05 +0200

    refactor(starknet_os): remove duplicated hint impl signature - make it a struct (#3709)

--- crates/starknet_os/src/hints/hint_implementation/block_context.rs
+++ crates/starknet_os/src/hints/hint_implementation/block_context.rs
@@ -1,163 +1,68 @@
-use std::collections::HashMap;
-
-use cairo_vm::hint_processor::hint_processor_definition::{HintProcessor, HintReference};
-use cairo_vm::serde::deserialize_program::ApTracking;
-use cairo_vm::types::exec_scope::ExecutionScopes;
-use cairo_vm::vm::vm_core::VirtualMachine;
-use starknet_types_core::felt::Felt;
-
 use crate::hints::error::{HintExtensionResult, HintResult};
+use crate::hints::types::{HintArgs, HintExtensionArgs};
 
 // Hint implementations.
 
-pub fn load_class_facts(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn load_class_facts(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn load_class_inner(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn load_class_inner(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn bytecode_segment_structure(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn bytecode_segment_structure(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn block_number(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn block_number(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn block_timestamp(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn block_timestamp(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn chain_id(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn chain_id(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn fee_token_address(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn fee_token_address(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn deprecated_fee_token_address(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn deprecated_fee_token_address(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn sequencer_address(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn sequencer_address(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn get_block_mapping(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn get_block_mapping(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn elements_ge_10(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn elements_ge_10(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn elements_ge_2(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn elements_ge_2(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn is_leaf(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn is_leaf(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
-pub fn write_use_kzg_da_to_memory(
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
-    _constants: &HashMap<String, Felt>,
-) -> HintResult {
+pub fn write_use_kzg_da_to_memory(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
 
 // Hint extension implementations.
 
 pub fn load_class(
-    _hint_processor: &dyn HintProcessor,
-    _vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    _ids_data: &HashMap<String, HintReference>,
-    _ap_tracking: &ApTracking,
+    HintExtensionArgs { .. }: HintExtensionArgs<'_, '_, '_, '_, '_>,
 ) -> HintExtensionResult {
     todo!()
 }
