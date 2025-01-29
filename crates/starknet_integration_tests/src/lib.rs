ef171b5211c110a56d24097ad6c0cbc2aec8d281
nadin-Starkware <nadin@starkware.co>
2025-01-29 17:43:16 +0200

    chore(starknet_integration_tests): add setup bin for a single node
    commit-id:3b3e2193

--- crates/starknet_integration_tests/src/lib.rs
+++ crates/starknet_integration_tests/src/lib.rs
@@ -2,6 +2,7 @@ pub mod config_utils;
 pub mod end_to_end_integration;
 pub mod flow_test_setup;
 pub mod integration_test_setup;
+pub mod node_setup;
 pub mod sequencer_manager;
 pub mod state_reader;
 pub mod utils;
