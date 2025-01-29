c332cc6869fc8f40c25eee4405ae5bbc9547d5e1
amos rothberg <amos@starkware.co>
2025-01-29 17:05:10 +0200

    chore(starknet_committer_and_os_cli): moved committer logic into separate folder

--- crates/starknet_committer_and_os_cli/src/lib.rs
+++ crates/starknet_committer_and_os_cli/src/lib.rs
@@ -1,6 +1,4 @@
-pub mod block_hash;
-pub mod commands;
-pub mod filled_tree_output;
-pub mod parse_input;
-pub mod tests;
+pub mod committer_cli;
+pub mod os_cli;
+pub mod shared_utils;
 pub mod tracing_utils;
