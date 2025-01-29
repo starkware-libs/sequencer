c9a7163ae55e3aac723c1c97d6fd64cd1e127e62
Elin <elin@starkware.co>
2025-01-29 10:22:02 +0200

    feat(starknet_class_manager): implement class manager logic (#3760)
    commit-id:0ead92c1

--- crates/starknet_class_manager/src/class_manager.rs
+++ crates/starknet_class_manager/src/class_manager.rs
@@ -1,35 +1,74 @@
 use starknet_class_manager_types::{ClassId, ClassManagerResult, ExecutableClassHash};
-use starknet_sierra_multicompile_types::{RawClass, RawExecutableClass};
+use starknet_sierra_multicompile_types::{
+    RawClass,
+    RawExecutableClass,
+    SharedSierraCompilerClient,
+};
+
+use crate::class_storage::{CachedClassStorage, CachedClassStorageConfig, ClassStorage};
 
 #[cfg(test)]
 #[path = "class_manager_test.rs"]
 pub mod class_manager_test;
 
-pub struct ClassManager;
+#[derive(Clone, Copy, Debug)]
+pub struct ClassManagerConfig {
+    pub cached_class_storage_config: CachedClassStorageConfig,
+}
 
-// TODO(Elin): complete implementation.
-impl ClassManager {
+pub struct ClassManager<S: ClassStorage> {
+    pub config: ClassManagerConfig,
+    pub compiler: SharedSierraCompilerClient,
+    pub classes: CachedClassStorage<S>,
+}
+
+impl<S: ClassStorage> ClassManager<S> {
+    pub fn new(
+        config: ClassManagerConfig,
+        compiler: SharedSierraCompilerClient,
+        storage: S,
+    ) -> Self {
+        Self {
+            config,
+            compiler,
+            classes: CachedClassStorage::new(config.cached_class_storage_config, storage),
+        }
+    }
+}
+
+impl<S: ClassStorage> ClassManager<S> {
     pub async fn add_class(
         &mut self,
-        _class_id: ClassId,
-        _class: RawClass,
+        class_id: ClassId,
+        class: RawClass,
     ) -> ClassManagerResult<ExecutableClassHash> {
-        unimplemented!()
+        if let Ok(executable_class_hash) = self.classes.get_executable_class_hash(class_id) {
+            // Class already exists.
+            return Ok(executable_class_hash);
+        }
+
+        let (raw_executable_class, executable_class_hash) =
+            self.compiler.compile(class.clone()).await?;
+
+        self.classes.set_class(class_id, class, executable_class_hash, raw_executable_class)?;
+
+        Ok(executable_class_hash)
     }
 
-    pub fn get_executable(&self, _class_id: ClassId) -> ClassManagerResult<RawExecutableClass> {
-        unimplemented!()
+    pub fn get_executable(&self, class_id: ClassId) -> ClassManagerResult<RawExecutableClass> {
+        Ok(self.classes.get_executable(class_id)?)
     }
 
-    pub fn get_sierra(&self, _class_id: ClassId) -> ClassManagerResult<RawClass> {
-        unimplemented!()
+    pub fn get_sierra(&self, class_id: ClassId) -> ClassManagerResult<RawClass> {
+        Ok(self.classes.get_sierra(class_id)?)
     }
 
     pub fn add_deprecated_class(
         &mut self,
-        _class_id: ClassId,
-        _class: RawExecutableClass,
+        class_id: ClassId,
+        class: RawExecutableClass,
     ) -> ClassManagerResult<()> {
-        unimplemented!()
+        self.classes.set_deprecated_class(class_id, class)?;
+        Ok(())
     }
 }
