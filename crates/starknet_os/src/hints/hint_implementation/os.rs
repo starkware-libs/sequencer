f44e73e0d37d2e19229078069022cb25de08ac74
dorimedini-starkware <dori@starkware.co>
2025-01-29 19:10:24 +0200

    feat(starknet_os): add hints from moonsong's mod.rs (#3790)

--- crates/starknet_os/src/hints/hint_implementation/os.rs
+++ crates/starknet_os/src/hints/hint_implementation/os.rs
@@ -1,6 +1,14 @@
 use crate::hints::error::HintResult;
 use crate::hints::types::HintArgs;
 
+pub fn initialize_class_hashes(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
+
+pub fn initialize_state_changes(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
+
 pub fn write_full_output_to_memory(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
@@ -14,5 +22,9 @@ pub fn set_ap_to_prev_block_hash(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) 
 }
 
 pub fn set_ap_to_new_block_hash(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
+
+pub fn starknet_os_input(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
     todo!()
 }
