9d9f69c924b986e60825f0c9979670b563b1aa8e
Yael Doweck <yael@starkware.co>
2025-01-29 14:24:53 +0200

    test(starknet_integration_tests): add deploy_tx to the integration test

--- crates/starknet_integration_tests/src/end_to_end_integration.rs
+++ crates/starknet_integration_tests/src/end_to_end_integration.rs
@@ -6,9 +6,10 @@ use starknet_sequencer_node::test_utils::node_runner::get_node_executable_path;
 use tracing::info;
 
 use crate::sequencer_manager::{get_sequencer_setup_configs, IntegrationTestManager};
-use crate::utils::InvokeTxs;
+use crate::utils::{FirstBlock, InvokeTxs, N_TXS_IN_FIRST_BLOCK};
 
 pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGenerator) {
+    const EXPECTED_SECOND_BLOCK_NUMBER: BlockNumber = BlockNumber(2);
     const EXPECTED_BLOCK_NUMBER: BlockNumber = BlockNumber(10);
     const LATE_NODE_EXPECTED_BLOCK_NUMBER: BlockNumber = BlockNumber(25);
     const N_TXS: usize = 50;
@@ -41,9 +42,22 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
     // Run the nodes.
     integration_test_manager.run(filtered_nodes).await;
 
+    // Run the first block scenario to bootstrap the accounts.
+    integration_test_manager
+        .test_and_verify(tx_generator, 0, FirstBlock, SENDER_ACCOUNT, EXPECTED_SECOND_BLOCK_NUMBER)
+        .await;
+
     // Run the test.
     integration_test_manager
-        .test_and_verify(tx_generator, 0, InvokeTxs(N_TXS), SENDER_ACCOUNT, EXPECTED_BLOCK_NUMBER)
+        .test_and_verify(
+            tx_generator,
+            // TODO(Yael): consider removing this parameter and take it from the tx_generator
+            // instead.
+            N_TXS_IN_FIRST_BLOCK,
+            InvokeTxs(N_TXS),
+            SENDER_ACCOUNT,
+            EXPECTED_BLOCK_NUMBER,
+        )
         .await;
 
     // Run the late node.
@@ -53,7 +67,7 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
     integration_test_manager
         .test_and_verify(
             tx_generator,
-            N_TXS,
+            N_TXS + N_TXS_IN_FIRST_BLOCK,
             InvokeTxs(N_TXS),
             SENDER_ACCOUNT,
             LATE_NODE_EXPECTED_BLOCK_NUMBER,
