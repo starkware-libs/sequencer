e305e994becaee0df8cf198e7e91f70cbd87ccd7
amos rothberg <amos@starkware.co>
2025-01-30 13:43:50 +0200

    chore(starknet_committer_and_os_cli): moved block-hash logic into separate folder

---
+++ crates/starknet_committer_and_os_cli/src/block_hash_cli/run_block_hash_cli.rs
@@ -0,0 +1,58 @@
+use clap::{Parser, Subcommand};
+use starknet_api::block_hash::block_hash_calculator::{
+    calculate_block_commitments,
+    calculate_block_hash,
+};
+use tracing::info;
+
+use crate::committer_cli::block_hash::{BlockCommitmentsInput, BlockHashInput};
+use crate::shared_utils::read::{load_input, write_to_file};
+use crate::shared_utils::types::IoArgs;
+
+#[derive(Parser, Debug)]
+pub struct BlockHashCliCommand {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Calculates the block hash.
+    BlockHash {
+        #[clap(flatten)]
+        io_args: IoArgs,
+    },
+    /// Calculates commitments needed for the block hash.
+    BlockHashCommitments {
+        #[clap(flatten)]
+        io_args: IoArgs,
+    },
+}
+
+pub async fn run_block_hash_cli(block_hash_cli_command: BlockHashCliCommand) {
+    info!("Starting block-hash-cli with command: \n{:?}", block_hash_cli_command);
+    match block_hash_cli_command.command {
+        Command::BlockHash { io_args: IoArgs { input_path, output_path } } => {
+            let block_hash_input: BlockHashInput = load_input(input_path);
+            info!("Successfully loaded block hash input.");
+            let block_hash =
+                calculate_block_hash(block_hash_input.header, block_hash_input.block_commitments)
+                    .unwrap_or_else(|error| panic!("Failed to calculate block hash: {}", error));
+            write_to_file(&output_path, &block_hash);
+            info!("Successfully computed block hash {:?}.", block_hash);
+        }
+
+        Command::BlockHashCommitments { io_args: IoArgs { input_path, output_path } } => {
+            let commitments_input: BlockCommitmentsInput = load_input(input_path);
+            info!("Successfully loaded block hash commitment input.");
+            let commitments = calculate_block_commitments(
+                &commitments_input.transactions_data,
+                &commitments_input.state_diff,
+                commitments_input.l1_da_mode,
+                &commitments_input.starknet_version,
+            );
+            write_to_file(&output_path, &commitments);
+            info!("Successfully computed block hash commitment: \n{:?}", commitments);
+        }
+    }
+}
