d3614f4516ec61884c3b9f3b455ef985d1888995
amos rothberg <amos@starkware.co>
2025-02-02 13:05:53 +0200

    chore(starknet_committer_and_os_cli): generalize python tests

--- crates/starknet_committer_and_os_cli/src/committer_cli/run_committer_cli.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/run_committer_cli.rs
@@ -5,9 +5,8 @@ use tracing_subscriber::reload::Handle;
 use tracing_subscriber::Registry;
 
 use crate::committer_cli::commands::parse_and_commit;
-use crate::committer_cli::tests::python_tests::PythonTest;
-use crate::shared_utils::read::{read_input, write_to_file};
-use crate::shared_utils::types::IoArgs;
+use crate::committer_cli::tests::python_tests::CommitterPythonTestRunner;
+use crate::shared_utils::types::{run_python_test, IoArgs, PythonTestArg};
 
 #[derive(Parser, Debug)]
 pub struct CommitterCliCommand {
@@ -22,14 +21,7 @@ enum Command {
         #[clap(flatten)]
         io_args: IoArgs,
     },
-    PythonTest {
-        #[clap(flatten)]
-        io_args: IoArgs,
-
-        /// Test name.
-        #[clap(long)]
-        test_name: String,
-    },
+    PythonTest(PythonTestArg),
 }
 
 pub async fn run_committer_cli(
@@ -42,20 +34,8 @@ pub async fn run_committer_cli(
             parse_and_commit(input_path, output_path, log_filter_handle).await;
         }
 
-        Command::PythonTest { io_args: IoArgs { input_path, output_path }, test_name } => {
-            // Create PythonTest from test_name.
-            let test = PythonTest::try_from(test_name)
-                .unwrap_or_else(|error| panic!("Failed to create PythonTest: {}", error));
-            let input = read_input(input_path);
-
-            // Run relevant test.
-            let output = test
-                .run(Some(&input))
-                .await
-                .unwrap_or_else(|error| panic!("Failed to run test: {}", error));
-
-            // Write test's output.
-            write_to_file(&output_path, &output);
+        Command::PythonTest(python_test_arg) => {
+            run_python_test::<CommitterPythonTestRunner>(python_test_arg).await;
         }
     }
 }
