f8a81b3f0edc399974555cef98dca1271152949d
Arni Hod <arnon@starkware.co>
2025-02-02 11:15:28 +0200

    chore(blockifier): add benchmark with concurrency

--- crates/blockifier/bench/blockifier_bench.rs
+++ crates/blockifier/bench/blockifier_bench.rs
@@ -7,6 +7,7 @@
 //!
 //! Run the benchmarks using `cargo bench --bench blockifier_bench`.
 
+use blockifier::blockifier::config::ConcurrencyConfig;
 use blockifier::test_utils::transfers_generator::{
     RecipientGeneratorType,
     TransfersGenerator,
@@ -15,18 +16,36 @@ use blockifier::test_utils::transfers_generator::{
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn transfers_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("transfers");
+    let mut bench_scenarios = vec![];
+
+    let id = "with concurrency";
+    let concurrency_enabled = true;
     let transfers_generator_config = TransfersGeneratorConfig {
         recipient_generator_type: RecipientGeneratorType::Random,
+        concurrency_config: ConcurrencyConfig::create_for_testing(concurrency_enabled),
         ..Default::default()
     };
-    let mut transfers_generator = TransfersGenerator::new(transfers_generator_config);
-    // Create a benchmark group called "transfers", which iterates over the accounts round-robin
-    // and performs transfers.
-    c.bench_function("transfers", |benchmark| {
-        benchmark.iter(|| {
-            transfers_generator.execute_transfers();
-        })
-    });
+    bench_scenarios.push((id, transfers_generator_config.clone()));
+
+    let id = "without concurrency";
+    let concurrency_enabled = false;
+    let transfers_generator_config = TransfersGeneratorConfig {
+        concurrency_config: ConcurrencyConfig::create_for_testing(concurrency_enabled),
+        ..transfers_generator_config
+    };
+    bench_scenarios.push((id, transfers_generator_config));
+
+    for (id, transfers_generator_config) in bench_scenarios {
+        let mut transfers_generator = TransfersGenerator::new(transfers_generator_config);
+        // Create a benchmark group, which iterates over the accounts round-robin and performs
+        // transfers.
+        group.bench_function(id, |benchmark| {
+            benchmark.iter(|| {
+                transfers_generator.execute_transfers();
+            })
+        });
+    }
 }
 
 criterion_group!(benches, transfers_benchmark);
