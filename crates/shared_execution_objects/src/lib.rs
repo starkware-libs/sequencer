4b283a4594987a8321a464f161130852481bf85e
Yael Doweck <yael@starkware.co>
2025-02-02 15:47:48 +0200

    refactor(native_blockifier, starknet_consensus_orchestrator): share execution objects

---
+++ crates/shared_execution_objects/src/lib.rs
@@ -0,0 +1,7 @@
+//! The `shared_execution_objects` crate contains execution objects shared between different crates.
+
+/// Contains a rust version of objects on the centralized Python side. These objects are used when
+/// interacting with the centralized Python.
+pub mod central_objects;
+// TODO(Yael): add the execution objects from the blockifier: execution_info, bouncer_weights,
+// commitment_state_diff.
