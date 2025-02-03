3c62546df1a20f9ab6548ee3cefefcf33b1bff84
Yair Bakalchuk <yair@starkware.co>
2025-02-03 13:43:43 +0200

    chore(starknet_integration_tests): improve logging

--- crates/starknet_integration_tests/src/monitoring_utils.rs
+++ crates/starknet_integration_tests/src/monitoring_utils.rs
@@ -5,6 +5,8 @@ use starknet_monitoring_endpoint::test_utils::MonitoringClient;
 use starknet_sequencer_metrics::metric_definitions;
 use tracing::info;
 
+use crate::sequencer_manager::NodeSetup;
+
 /// Gets the latest block number from the batcher's metrics.
 pub async fn get_batcher_latest_block_number(
     batcher_monitoring_client: &MonitoringClient,
@@ -25,15 +27,20 @@ pub async fn await_batcher_block(
     interval: u64,
     target_block_number: BlockNumber,
     max_attempts: usize,
-    batcher_monitoring_client: &MonitoringClient,
+    node: &NodeSetup,
 ) -> Result<BlockNumber, ()> {
     let condition = |&latest_block_number: &BlockNumber| latest_block_number >= target_block_number;
     let get_latest_block_number_closure =
-        || get_batcher_latest_block_number(batcher_monitoring_client);
+        || get_batcher_latest_block_number(node.batcher_monitoring_client());
 
     let logger = CustomLogger::new(
         TraceLevel::Info,
-        Some("Waiting for storage to include block".to_string()),
+        Some(format!(
+            "Waiting for batcher height metric to reach block {target_block_number} in sequencer \
+             {} executable {}.",
+            node.get_node_index().unwrap(),
+            node.get_batcher_index()
+        )),
     );
 
     run_until(interval, max_attempts, get_latest_block_number_closure, condition, Some(logger))
@@ -41,12 +48,12 @@ pub async fn await_batcher_block(
         .ok_or(())
 }
 
-pub async fn await_execution(
-    monitoring_client: &MonitoringClient,
-    expected_block_number: BlockNumber,
-) {
-    info!("Awaiting until {expected_block_number} blocks have been created.");
-    await_batcher_block(5000, expected_block_number, 50, monitoring_client)
+pub async fn await_execution(node: &NodeSetup, expected_block_number: BlockNumber) {
+    info!(
+        "Awaiting until {expected_block_number} blocks have been created in sequencer {}.",
+        node.get_node_index().unwrap()
+    );
+    await_batcher_block(5000, expected_block_number, 50, node)
         .await
         .expect("Block number should have been reached.");
 }
