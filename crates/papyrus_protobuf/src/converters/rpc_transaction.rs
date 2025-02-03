b61fc9cebd42fc388d430202af16113dc4ddd181
Alon Lukatch <alon.l@starkware.co>
2025-02-03 15:08:52 +0200

    refactor(papyrus_protobuf): convert proto <-> rpc through snapi

--- crates/papyrus_protobuf/src/converters/rpc_transaction.rs
+++ crates/papyrus_protobuf/src/converters/rpc_transaction.rs
@@ -3,7 +3,6 @@
 mod rpc_transaction_test;
 
 use prost::Message;
-use starknet_api::core::{ClassHash, Nonce};
 use starknet_api::rpc_transaction::{
     RpcDeclareTransaction,
     RpcDeclareTransactionV3,
@@ -14,19 +13,10 @@ use starknet_api::rpc_transaction::{
     RpcTransaction,
 };
 use starknet_api::state::SierraContractClass;
-use starknet_api::transaction::fields::{
-    AccountDeploymentData,
-    AllResourceBounds,
-    Calldata,
-    ContractAddressSalt,
-    PaymasterData,
-    Tip,
-    TransactionSignature,
-    ValidResourceBounds,
-};
-use starknet_types_core::felt::Felt;
+use starknet_api::transaction::fields::{AllResourceBounds, ValidResourceBounds};
+use starknet_api::transaction::{DeployAccountTransactionV3, InvokeTransactionV3};
 
-use super::common::{enum_int_to_volition_domain, volition_domain_to_enum_int};
+use super::common::volition_domain_to_enum_int;
 use super::ProtobufConversionError;
 use crate::auto_impl_into_and_try_from_vec_u8;
 use crate::mempool::RpcTransactionWrapper;
@@ -97,221 +87,36 @@ impl From<RpcTransaction> for protobuf::MempoolTransaction {
 impl TryFrom<protobuf::DeployAccountV3> for RpcDeployAccountTransactionV3 {
     type Error = ProtobufConversionError;
     fn try_from(value: protobuf::DeployAccountV3) -> Result<Self, Self::Error> {
-        let resource_bounds = AllResourceBounds::try_from(value.resource_bounds.ok_or(
-            ProtobufConversionError::MissingField {
-                field_description: "DeployAccountV3::resource_bounds",
-            },
-        )?)?;
-
-        let tip = Tip(value.tip);
-
-        let signature = TransactionSignature(
-            value
-                .signature
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "DeployAccountV3::signature",
-                })?
-                .parts
-                .into_iter()
-                .map(Felt::try_from)
-                .collect::<Result<Vec<_>, _>>()?,
-        );
-
-        let nonce = Nonce(
-            value
-                .nonce
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "DeployAccountV3::nonce",
-                })?
-                .try_into()?,
-        );
-
-        let class_hash = ClassHash(
-            value
-                .class_hash
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "DeployAccountV3::class_hash",
-                })?
-                .try_into()?,
-        );
-
-        let contract_address_salt = ContractAddressSalt(
-            value
-                .address_salt
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "DeployAccountV3::address_salt",
-                })?
-                .try_into()?,
-        );
-
-        let constructor_calldata =
-            value.calldata.into_iter().map(Felt::try_from).collect::<Result<Vec<_>, _>>()?;
-
-        let constructor_calldata = Calldata(constructor_calldata.into());
-
-        let nonce_data_availability_mode =
-            enum_int_to_volition_domain(value.nonce_data_availability_mode)?;
-
-        let fee_data_availability_mode =
-            enum_int_to_volition_domain(value.fee_data_availability_mode)?;
-
-        let paymaster_data = PaymasterData(
-            value.paymaster_data.into_iter().map(Felt::try_from).collect::<Result<Vec<_>, _>>()?,
-        );
-
-        Ok(Self {
-            resource_bounds,
-            tip,
-            signature,
-            nonce,
-            class_hash,
-            contract_address_salt,
-            constructor_calldata,
-            nonce_data_availability_mode,
-            fee_data_availability_mode,
-            paymaster_data,
+        let snapi_deploy_account: DeployAccountTransactionV3 = value.try_into()?;
+        // This conversion can fail only if the resource_bounds are not AllResources.
+        snapi_deploy_account.try_into().map_err(|_| ProtobufConversionError::MissingField {
+            field_description: "resource_bounds",
         })
     }
 }
 
 impl From<RpcDeployAccountTransactionV3> for protobuf::DeployAccountV3 {
     fn from(value: RpcDeployAccountTransactionV3) -> Self {
-        Self {
-            resource_bounds: Some(value.resource_bounds.into()),
-            tip: value.tip.0,
-            signature: Some(protobuf::AccountSignature {
-                parts: value.signature.0.into_iter().map(|stark_felt| stark_felt.into()).collect(),
-            }),
-            nonce: Some(value.nonce.0.into()),
-            class_hash: Some(value.class_hash.0.into()),
-            address_salt: Some(value.contract_address_salt.0.into()),
-            calldata: value
-                .constructor_calldata
-                .0
-                .iter()
-                .map(|calldata| (*calldata).into())
-                .collect(),
-            nonce_data_availability_mode: volition_domain_to_enum_int(
-                value.nonce_data_availability_mode,
-            ),
-            fee_data_availability_mode: volition_domain_to_enum_int(
-                value.fee_data_availability_mode,
-            ),
-            paymaster_data: value
-                .paymaster_data
-                .0
-                .iter()
-                .map(|paymaster_data| (*paymaster_data).into())
-                .collect(),
-        }
+        let snapi_deploy_account: DeployAccountTransactionV3 = value.into();
+        snapi_deploy_account.into()
     }
 }
 
 impl TryFrom<protobuf::InvokeV3> for RpcInvokeTransactionV3 {
     type Error = ProtobufConversionError;
     fn try_from(value: protobuf::InvokeV3) -> Result<Self, Self::Error> {
-        let resource_bounds = AllResourceBounds::try_from(value.resource_bounds.ok_or(
-            ProtobufConversionError::MissingField {
-                field_description: "InvokeV3::resource_bounds",
-            },
-        )?)?;
-
-        let tip = Tip(value.tip);
-
-        let signature = TransactionSignature(
-            value
-                .signature
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "InvokeV3::signature",
-                })?
-                .parts
-                .into_iter()
-                .map(Felt::try_from)
-                .collect::<Result<Vec<_>, _>>()?,
-        );
-
-        let nonce = Nonce(
-            value
-                .nonce
-                .ok_or(ProtobufConversionError::MissingField {
-                    field_description: "InvokeV3::nonce",
-                })?
-                .try_into()?,
-        );
-
-        let sender_address = value
-            .sender
-            .ok_or(ProtobufConversionError::MissingField { field_description: "InvokeV3::sender" })?
-            .try_into()?;
-
-        let calldata =
-            value.calldata.into_iter().map(Felt::try_from).collect::<Result<Vec<_>, _>>()?;
-
-        let calldata = Calldata(calldata.into());
-
-        let nonce_data_availability_mode =
-            enum_int_to_volition_domain(value.nonce_data_availability_mode)?;
-
-        let fee_data_availability_mode =
-            enum_int_to_volition_domain(value.fee_data_availability_mode)?;
-
-        let paymaster_data = PaymasterData(
-            value.paymaster_data.into_iter().map(Felt::try_from).collect::<Result<Vec<_>, _>>()?,
-        );
-
-        let account_deployment_data = AccountDeploymentData(
-            value
-                .account_deployment_data
-                .into_iter()
-                .map(Felt::try_from)
-                .collect::<Result<Vec<_>, _>>()?,
-        );
-
-        Ok(Self {
-            resource_bounds,
-            tip,
-            signature,
-            nonce,
-            sender_address,
-            calldata,
-            nonce_data_availability_mode,
-            fee_data_availability_mode,
-            paymaster_data,
-            account_deployment_data,
+        let snapi_invoke: InvokeTransactionV3 = value.try_into()?;
+        // This conversion can fail only if the resource_bounds are not AllResources.
+        snapi_invoke.try_into().map_err(|_| ProtobufConversionError::MissingField {
+            field_description: "resource_bounds",
         })
     }
 }
 
 impl From<RpcInvokeTransactionV3> for protobuf::InvokeV3 {
     fn from(value: RpcInvokeTransactionV3) -> Self {
-        Self {
-            resource_bounds: Some(value.resource_bounds.into()),
-            tip: value.tip.0,
-            signature: Some(protobuf::AccountSignature {
-                parts: value.signature.0.into_iter().map(|stark_felt| stark_felt.into()).collect(),
-            }),
-            nonce: Some(value.nonce.0.into()),
-            sender: Some(value.sender_address.into()),
-            calldata: value.calldata.0.iter().map(|calldata| (*calldata).into()).collect(),
-            nonce_data_availability_mode: volition_domain_to_enum_int(
-                value.nonce_data_availability_mode,
-            ),
-            fee_data_availability_mode: volition_domain_to_enum_int(
-                value.fee_data_availability_mode,
-            ),
-            paymaster_data: value
-                .paymaster_data
-                .0
-                .iter()
-                .map(|paymaster_data| (*paymaster_data).into())
-                .collect(),
-            account_deployment_data: value
-                .account_deployment_data
-                .0
-                .iter()
-                .map(|account_deployment_data| (*account_deployment_data).into())
-                .collect(),
-        }
+        let snapi_invoke: InvokeTransactionV3 = value.into();
+        snapi_invoke.into()
     }
 }
 
