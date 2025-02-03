29201bd63d1ef9e31f603c52cd8e8089a6aaabdc
nadin-Starkware <nadin@starkware.co>
2025-02-03 20:09:40 +0200

    chore(starknet_integration_tests): run only happy flow in integration test
    commit-id:b1609ab0

--- crates/starknet_integration_tests/src/end_to_end_integration.rs
+++ crates/starknet_integration_tests/src/end_to_end_integration.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use mempool_test_utils::starknet_api_test_utils::{AccountId, MultiAccountTransactionGenerator};
 use starknet_api::block::BlockNumber;
 use starknet_sequencer_node::test_utils::node_runner::get_node_executable_path;
@@ -10,8 +8,7 @@ use crate::utils::{BootstrapTxs, InvokeTxs};
 
 pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGenerator) {
     const BLOCK_TO_WAIT_FOR_BOOTSTRAP: BlockNumber = BlockNumber(2);
-    const BLOCK_TO_WAIT_FOR_FIRST_ROUND: BlockNumber = BlockNumber(10);
-    const BLOCK_TO_WAIT_FOR_LATE_NODE: BlockNumber = BlockNumber(25);
+    const BLOCK_TO_WAIT_FOR_HAPPY_FLOW: BlockNumber = BlockNumber(15);
     const N_TXS: usize = 50;
     const SENDER_ACCOUNT: AccountId = 0;
     /// The number of consolidated local sequencers that participate in the test.
@@ -35,12 +32,8 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
     // invocation. Consider using the builder pattern.
     let mut integration_test_manager = IntegrationTestManager::new(sequencers_setup, Vec::new());
 
-    // Remove the node with index 1 to simulate a late node.
-    let mut filtered_nodes = node_indices.clone();
-    filtered_nodes.remove(&1);
-
     // Run the nodes.
-    integration_test_manager.run(filtered_nodes).await;
+    integration_test_manager.run(node_indices.clone()).await;
 
     // Run the first block scenario to bootstrap the accounts.
     integration_test_manager
@@ -53,20 +46,7 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
             tx_generator,
             InvokeTxs(N_TXS),
             SENDER_ACCOUNT,
-            BLOCK_TO_WAIT_FOR_FIRST_ROUND,
-        )
-        .await;
-
-    // Run the late node.
-    integration_test_manager.run(HashSet::from([1])).await;
-
-    // Run the tests after the late node joins.
-    integration_test_manager
-        .test_and_verify(
-            tx_generator,
-            InvokeTxs(N_TXS),
-            SENDER_ACCOUNT,
-            BLOCK_TO_WAIT_FOR_LATE_NODE,
+            BLOCK_TO_WAIT_FOR_HAPPY_FLOW,
         )
         .await;
 
