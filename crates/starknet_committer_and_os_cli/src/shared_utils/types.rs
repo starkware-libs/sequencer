1ebfab992f600af5a86539628b819f511ba917d0
amosStarkware <88497213+amosStarkware@users.noreply.github.com>
2025-02-03 18:00:01 +0200

    chore(starknet_committer_and_os_cli): generalize python tests (#3865)

--- crates/starknet_committer_and_os_cli/src/shared_utils/types.rs
+++ crates/starknet_committer_and_os_cli/src/shared_utils/types.rs
@@ -1,5 +1,10 @@
+use std::fmt::Debug;
+
 use clap::Args;
 
+use crate::shared_utils::read::{read_input, write_to_file};
+
+// TODO(Amos): Make this optional.
 #[derive(Debug, Args)]
 pub(crate) struct IoArgs {
     /// File path to input.
@@ -9,4 +14,62 @@ pub(crate) struct IoArgs {
     /// File path to output.
     #[clap(long, short = 'o', default_value = "stdout")]
     pub(crate) output_path: String,
+}
+
+#[derive(Debug, Args)]
+pub(crate) struct PythonTestArg {
+    #[clap(flatten)]
+    pub(crate) io_args: IoArgs,
+
+    /// Test name.
+    #[clap(long)]
+    pub(crate) test_name: String,
+}
+
+/// Error type for PythonTest enum.
+#[derive(Debug, thiserror::Error)]
+pub enum PythonTestError<E> {
+    #[error("Unknown test name: {0}")]
+    UnknownTestName(String),
+    #[error(transparent)]
+    ParseInputError(#[from] serde_json::Error),
+    #[error("None value found in input.")]
+    NoneInputError,
+    #[error(transparent)]
+    SpecificError(E),
+}
+
+pub(crate) trait PythonTestRunner: TryFrom<String> {
+    type SpecificError: Debug;
+
+    /// Returns the input string if it's `Some`, or an error if it's `None`.
+    fn non_optional_input(
+        input: Option<&str>,
+    ) -> Result<&str, PythonTestError<Self::SpecificError>> {
+        input.ok_or(PythonTestError::NoneInputError)
+    }
+
+    async fn run(
+        &self,
+        input: Option<&str>,
+    ) -> Result<String, PythonTestError<Self::SpecificError>>;
+}
+
+pub(crate) async fn run_python_test<PT: PythonTestRunner>(python_test_arg: PythonTestArg)
+where
+    <PT as TryFrom<String>>::Error: Debug,
+{
+    // Create PythonTest from test_name.
+    let test = PT::try_from(python_test_arg.test_name)
+        .unwrap_or_else(|error| panic!("Failed to create PythonTest: {:?}", error));
+    let input = read_input(python_test_arg.io_args.input_path);
+
+    // Run relevant test.
+    let output = test
+        .run(Some(&input))
+        .await
+        .unwrap_or_else(|error| panic!("Failed to run test: {:?}", error));
+
+    // Write test's output.
+    write_to_file(&python_test_arg.io_args.output_path, &output);
 }
