9f3f7cab42533dfab3d3908f97f03efcf1105393
nadin-Starkware <nadin@starkware.co>
2025-02-03 19:59:04 +0200

    feat(starknet_integration_tests): add db path argument to node_setup for configurable DB storage
    commit-id:2e49fc49

--- crates/starknet_integration_tests/src/bin/sequencer_node_setup.rs
+++ crates/starknet_integration_tests/src/bin/sequencer_node_setup.rs
@@ -1,11 +1,19 @@
+use std::env::args;
+use std::path::PathBuf;
+
 use starknet_integration_tests::node_setup::node_setup;
 use starknet_integration_tests::utils::create_integration_test_tx_generator;
 use starknet_sequencer_infra::trace_util::configure_tracing;
 use tracing::info;
+
 #[tokio::main]
 async fn main() {
     configure_tracing().await;
-    info!("Running integration test setup.");
+    info!("Running system test setup.");
+
+    // Parse command line arguments.
+    let args: Vec<String> = args().skip(1).collect();
+    let base_db_path = get_base_db_path(args);
 
     // TODO(Tsabary): remove the hook definition once we transition to proper usage of task
     // spawning.
@@ -20,5 +28,19 @@ async fn main() {
 
     // Run node setup.
     // Keep the sequenser_setups in a variable to avoid dropping it.
-    let _sequencer_setups = node_setup(&mut tx_generator, "./single_node_config.json").await;
+    let _sequencer_setups =
+        node_setup(&mut tx_generator, "./single_node_config.json", base_db_path).await;
+}
+
+// TODO(Nadin): Improve the argument parsing.
+pub fn get_base_db_path(args: Vec<String>) -> PathBuf {
+    let arg_name = "--base_db_path_dir";
+    match args.as_slice() {
+        [] => PathBuf::from("./data"),
+        [arg, path] if arg == arg_name => PathBuf::from(path),
+        _ => {
+            eprintln!("Error: Bad argument. The only allowed argument is '{}'.", arg_name);
+            std::process::exit(1);
+        }
+    }
 }
