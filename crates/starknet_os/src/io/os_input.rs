a35006c1466b7b34700fa09a7e2f8bd4992254fe
Dori Medini <dori@starkware.co>
2025-02-04 14:25:58 +0200

    feat(starknet_os): initial API definition, execution helper

---
+++ crates/starknet_os/src/io/os_input.rs
@@ -0,0 +1,4 @@
+/// All input needed to initialise the execution helper.
+// TODO(Dori): Add all fields needed to compute commitments, initialise a CachedState and other data
+//   required by the execution helper.
+pub struct StarknetOsInput {}
