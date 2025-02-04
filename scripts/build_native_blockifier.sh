4b84b3fe3a8f2a547ac235c434ead67a60d9383e
Adi Yakovian <adi@starkware.co>
2025-02-04 14:16:20 +0200

    fix(ci): return exit status in build native blockifier script

--- scripts/build_native_blockifier.sh
+++ scripts/build_native_blockifier.sh
@@ -9,11 +9,13 @@ function clean() {
 
 
 function build() {
+    ret=0
     echo "Building..."
     pypy3.9 -m venv /tmp/venv
     source /tmp/venv/bin/activate
-    cargo build --release -p native_blockifier --features "cairo_native" || clean
+    cargo build --release -p native_blockifier --features "cairo_native" || ret=$?
     clean
+    exit $ret
 }
 
 build
