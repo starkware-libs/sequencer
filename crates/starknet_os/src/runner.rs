76fdac452377c97bbf8679c2966b80aeabc2de9d
Dori Medini <dori@starkware.co>
2025-02-04 14:28:20 +0200

    feat(starknet_os): added PanickingStateReader and stateless OS runner function

--- crates/starknet_os/src/runner.rs
+++ crates/starknet_os/src/runner.rs
@@ -4,6 +4,7 @@ use cairo_vm::types::layout_name::LayoutName;
 
 use crate::errors::StarknetOsError;
 use crate::hint_processor::execution_helper::OsExecutionHelper;
+use crate::hint_processor::panicking_state_reader::PanickingStateReader;
 use crate::io::os_input::StarknetOsInput;
 use crate::io::os_output::StarknetOsRunnerOutput;
 
@@ -15,4 +16,15 @@ pub fn run_os<S: StateReader>(
 ) -> Result<StarknetOsRunnerOutput, StarknetOsError> {
     let _execution_helper = OsExecutionHelper::<S>::new(os_input);
     todo!()
+}
+
+/// Run the OS with a "stateless" state reader - panics if the state is accessed for data that was
+/// not pre-loaded as part of the input.
+pub fn run_os_stateless(
+    compiled_os: &[u8],
+    layout: LayoutName,
+    block_context: BlockContext,
+    os_input: &StarknetOsInput,
+) -> Result<StarknetOsRunnerOutput, StarknetOsError> {
+    run_os::<PanickingStateReader>(compiled_os, layout, block_context, os_input)
 }
