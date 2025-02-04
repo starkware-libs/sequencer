102970a0a2557af1e3779c4144109b4168484634
Cypher Pepe <125112044+cypherpepe@users.noreply.github.com>
2025-02-04 22:37:51 +0300

    Update broken link node_config.rs

--- crates/starknet_sequencer_node/src/config/node_config.rs
+++ crates/starknet_sequencer_node/src/config/node_config.rs
@@ -47,7 +47,7 @@ pub static CONFIG_POINTERS: LazyLock<ConfigPointers> = LazyLock::new(|| {
             ser_pointer_target_required_param(
                 "chain_id",
                 SerializationType::String,
-                "The chain to follow. For more details see https://docs.starknet.io/documentation/architecture_and_concepts/Blocks/transactions/#chain-id.",
+                "The chain to follow. For more details see https://docs.starknet.io/architecture-and-concepts/network-architecture/transactions/#chain-id.",
             ),
             set_pointing_param_paths(&[
                 "batcher_config.block_builder_config.chain_info.chain_id",
