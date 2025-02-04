1a19425bf3ccb4e51bd509510186f567b954b7dc
Elin <elin@starkware.co>
2025-02-04 15:54:51 +0200

    feat(starknet_class_manager): make class manager `Clone`able (#3939)
    This is required to allow it to act as a concurrent component, according
    to the current design.
    To avoid requiring `Clone` on all future implementations, I manually
    implemented it for `FsClassStorage`. If we decide to stick to this
    design longterm, it can be demanded and derived. I believe `Clone` is a
    stonger requirement than needed for concurrency, so made the minimal
    easy-to-refactor change possible.

--- crates/starknet_class_manager/src/lib.rs
+++ crates/starknet_class_manager/src/lib.rs
@@ -6,5 +6,18 @@ pub mod config;
 use crate::class_manager::ClassManager as GenericClassManager;
 use crate::class_storage::FsClassStorage;
 
-pub type FsClassManager = GenericClassManager<FsClassStorage>;
+pub struct FsClassManager(pub GenericClassManager<FsClassStorage>);
+
+impl Clone for FsClassManager {
+    fn clone(&self) -> Self {
+        let GenericClassManager { config, compiler, classes } = &self.0;
+
+        FsClassManager(GenericClassManager {
+            config: config.clone(),
+            compiler: compiler.clone(),
+            classes: classes.clone(),
+        })
+    }
+}
+
 pub use FsClassManager as ClassManager;
