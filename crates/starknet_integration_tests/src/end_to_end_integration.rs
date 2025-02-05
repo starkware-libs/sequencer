ec7a61ac6e84bd6c4a5bd75841c87ac3a50785ea
Shahak Shama <shahak@starkware.co>
2025-02-05 17:38:09 +0200

    copy integration test from noam

--- crates/starknet_integration_tests/src/end_to_end_integration.rs
+++ crates/starknet_integration_tests/src/end_to_end_integration.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::time::Duration;
+
 use mempool_test_utils::starknet_api_test_utils::{AccountId, MultiAccountTransactionGenerator};
 use starknet_api::block::BlockNumber;
 use starknet_sequencer_node::test_utils::node_runner::get_node_executable_path;
@@ -8,7 +11,9 @@ use crate::utils::{BootstrapTxs, InvokeTxs};
 
 pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGenerator) {
     const BLOCK_TO_WAIT_FOR_BOOTSTRAP: BlockNumber = BlockNumber(2);
-    const BLOCK_TO_WAIT_FOR_HAPPY_FLOW: BlockNumber = BlockNumber(15);
+    const BLOCK_TO_WAIT_FOR_FIRST_ROUND: BlockNumber = BlockNumber(10);
+    const BLOCK_TO_WAIT_FOR_LATE_NODE_TEMP: BlockNumber = BlockNumber(25);
+    const BLOCK_TO_WAIT_FOR_CONSENSUS_NODE: BlockNumber = BlockNumber(40);
     const N_TXS: usize = 50;
     const SENDER_ACCOUNT: AccountId = 0;
     /// The number of consolidated local sequencers that participate in the test.
@@ -44,9 +49,40 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
     integration_test_manager
         .test_and_verify(
             tx_generator,
+            // TODO(Yael): consider removing this parameter and take it from the tx_generator
+            // instead.
             InvokeTxs(N_TXS),
             SENDER_ACCOUNT,
-            BLOCK_TO_WAIT_FOR_HAPPY_FLOW,
+            BLOCK_TO_WAIT_FOR_FIRST_ROUND,
+        )
+        .await;
+
+    info!("Shutting down node {:?}.", 1);
+    integration_test_manager.shutdown_nodes(HashSet::from([1]));
+    integration_test_manager
+        .test_and_verify(
+            tx_generator,
+            InvokeTxs(N_TXS),
+            SENDER_ACCOUNT,
+            BLOCK_TO_WAIT_FOR_LATE_NODE_TEMP,
+        )
+        .await;
+
+    info!("Turning node {:?} back on.", 1);
+    integration_test_manager.run(HashSet::from([1])).await;
+
+    // Shutdown the node with index 2 to ensure that the shutdown node participates in the
+    // consensus.
+    info!("Sleeping for 30 seconds to allow the node to catch up.");
+    tokio::time::sleep(Duration::from_secs(30)).await;
+    integration_test_manager.shutdown_nodes(HashSet::from([2]));
+
+    integration_test_manager
+        .test_and_verify(
+            tx_generator,
+            InvokeTxs(N_TXS),
+            SENDER_ACCOUNT,
+            BLOCK_TO_WAIT_FOR_CONSENSUS_NODE,
         )
         .await;
 
@@ -54,4 +90,5 @@ pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGe
     integration_test_manager.shutdown_nodes(node_indices);
 
     info!("Integration test completed successfully!");
+    // Kill other node so we ar sure previous one is in consensus, then run more txs
 }
