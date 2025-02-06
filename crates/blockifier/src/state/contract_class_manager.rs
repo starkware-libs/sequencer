ba27928c732b2e507cba4dde73daf2364892f045
Yonatan Iluz <yonatan@starkware.co>
2025-02-06 11:18:33 +0200

    fix more conflicts

--- crates/blockifier/src/state/contract_class_manager.rs
+++ crates/blockifier/src/state/contract_class_manager.rs
@@ -1,7 +1,7 @@
 pub const DEFAULT_COMPILATION_REQUEST_CHANNEL_SIZE: usize = 2000;
 
 #[cfg(feature = "cairo_native")]
-pub type ContractClassManager = crate::state::native_class_manager::NativeClassManager;
+pub use crate::state::native_class_manager::NativeClassManager as ContractClassManager;
 
 #[cfg(not(feature = "cairo_native"))]
 pub mod trivial_class_manager {
@@ -13,32 +13,38 @@ pub mod trivial_class_manager {
     use crate::execution::contract_class::RunnableCompiledClass;
     use crate::state::global_cache::{CachedClass, RawClassCache};
 
-    pub type ContractClassManager = RawClassCache;
+    pub struct TrivialClassManager {
+        cache: RawClassCache,
+    }
 
     // Trivial implementation of the class manager for Native-less projects.
-    impl ContractClassManager {
+    impl TrivialClassManager {
         pub fn start(config: ContractClassManagerConfig) -> Self {
             assert!(
                 !config.cairo_native_run_config.run_cairo_native,
                 "Cairo Native feature is off."
             );
-            Self::new(config.contract_cache_size)
+            Self { cache: RawClassCache::new(config.contract_cache_size) }
         }
 
         pub fn get_runnable(&self, class_hash: &ClassHash) -> Option<RunnableCompiledClass> {
-            Some(self.get(class_hash)?.to_runnable())
+            Some(self.cache.get(class_hash)?.to_runnable())
         }
 
         pub fn set_and_compile(&self, class_hash: ClassHash, compiled_class: CachedClass) {
-            self.set(class_hash, compiled_class);
+            self.cache.set(class_hash, compiled_class);
+        }
+
+        pub fn clear(&mut self) {
+            self.cache.clear();
         }
 
         #[cfg(any(feature = "testing", test))]
         pub fn get_cache_size(&self) -> usize {
-            self.lock().cache_size()
+            self.cache.lock().cache_size()
         }
     }
 }
 
 #[cfg(not(feature = "cairo_native"))]
-pub use trivial_class_manager::ContractClassManager;
+pub use trivial_class_manager::TrivialClassManager as ContractClassManager;
