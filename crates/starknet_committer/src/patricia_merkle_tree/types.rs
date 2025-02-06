b3a9de41dfa3f9a604ae940372dccfa1e56c0d6c
Dori Medini <dori@starkware.co>
2025-02-06 10:47:37 +0200

    feat(starknet_patricia,starknet_committer): stop using custom ContractAddress type

--- crates/starknet_committer/src/patricia_merkle_tree/types.rs
+++ crates/starknet_committer/src/patricia_merkle_tree/types.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
+use starknet_api::core::ContractAddress;
 use starknet_patricia::impl_from_hex_for_felt_wrapper;
 use starknet_patricia::patricia_merkle_tree::filled_tree::tree::FilledTreeImpl;
 use starknet_patricia::patricia_merkle_tree::types::NodeIndex;
 use starknet_types_core::felt::{Felt, FromStrError};
 
-use crate::block_committer::input::{ContractAddress, StarknetStorageValue};
+use crate::block_committer::input::StarknetStorageValue;
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
 
 pub fn fixed_hex_string_no_prefix(felt: &Felt) -> String {
