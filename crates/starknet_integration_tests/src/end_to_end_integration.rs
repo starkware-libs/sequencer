5cefaf007ce725065169ecf321c2261c0d78e73d
noamsp-starkware <noam.s@starkware.co>
2025-02-06 10:39:27 +0200

    refactor(starknet_integration_tests): unify repeating code and add testing fns (#3949)

--- crates/starknet_integration_tests/src/end_to_end_integration.rs
+++ crates/starknet_integration_tests/src/end_to_end_integration.rs
@@ -1,55 +1,30 @@
-use mempool_test_utils::starknet_api_test_utils::{AccountId, MultiAccountTransactionGenerator};
 use starknet_api::block::BlockNumber;
-use starknet_sequencer_node::test_utils::node_runner::get_node_executable_path;
 use tracing::info;
 
-use crate::sequencer_manager::{get_sequencer_setup_configs, IntegrationTestManager};
-use crate::utils::{BootstrapTxs, InvokeTxs};
+use crate::sequencer_manager::IntegrationTestManager;
 
-pub async fn end_to_end_integration(tx_generator: &mut MultiAccountTransactionGenerator) {
-    const BLOCK_TO_WAIT_FOR_BOOTSTRAP: BlockNumber = BlockNumber(2);
-    const BLOCK_TO_WAIT_FOR_HAPPY_FLOW: BlockNumber = BlockNumber(15);
+pub async fn end_to_end_integration() {
+    const BLOCK_TO_WAIT_FOR: BlockNumber = BlockNumber(15);
     const N_TXS: usize = 50;
-    const SENDER_ACCOUNT: AccountId = 0;
     /// The number of consolidated local sequencers that participate in the test.
     const N_CONSOLIDATED_SEQUENCERS: usize = 3;
     /// The number of distributed remote sequencers that participate in the test.
     const N_DISTRIBUTED_SEQUENCERS: usize = 2;
 
-    info!("Checking that the sequencer node executable is present.");
-    get_node_executable_path();
-
     // Get the sequencer configurations.
-    let (sequencers_setup, node_indices) = get_sequencer_setup_configs(
-        tx_generator,
-        N_CONSOLIDATED_SEQUENCERS,
-        N_DISTRIBUTED_SEQUENCERS,
-        None,
-    )
-    .await;
+    let mut integration_test_manager =
+        IntegrationTestManager::new(N_CONSOLIDATED_SEQUENCERS, N_DISTRIBUTED_SEQUENCERS, None)
+            .await;
 
-    // Run the sequencers.
-    // TODO(Nadin, Tsabary): Refactor to separate the construction of SequencerManager from its
-    // invocation. Consider using the builder pattern.
-    let mut integration_test_manager = IntegrationTestManager::new(sequencers_setup, Vec::new());
-
+    let node_indices = integration_test_manager.get_node_indices();
     // Run the nodes.
-    integration_test_manager.run(node_indices.clone()).await;
+    integration_test_manager.run_nodes(node_indices.clone()).await;
 
     // Run the first block scenario to bootstrap the accounts.
-    integration_test_manager
-        .test_and_verify(tx_generator, BootstrapTxs, SENDER_ACCOUNT, BLOCK_TO_WAIT_FOR_BOOTSTRAP)
-        .await;
+    integration_test_manager.send_bootstrap_txs_and_verify().await;
 
     // Run the test.
-    integration_test_manager
-        .test_and_verify(
-            tx_generator,
-            InvokeTxs(N_TXS),
-            SENDER_ACCOUNT,
-            BLOCK_TO_WAIT_FOR_HAPPY_FLOW,
-        )
-        .await;
+    integration_test_manager.send_invoke_txs_and_verify(N_TXS, BLOCK_TO_WAIT_FOR).await;
 
     info!("Shutting down nodes.");
     integration_test_manager.shutdown_nodes(node_indices);
