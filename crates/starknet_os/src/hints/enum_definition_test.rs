54470ee97e54b56c3addd94f2905d662f683df4d
Tzahi <tzahi@starkware.co>
2025-02-09 18:06:47 +0200

    feat(starknet_os): split Hint to OsHint and syscall + test syscall equivalence with blockifier (#4038)

--- crates/starknet_os/src/hints/enum_definition_test.rs
+++ crates/starknet_os/src/hints/enum_definition_test.rs
@@ -1,24 +1,53 @@
 use std::collections::HashSet;
 
+use blockifier::execution::hint_code::SYSCALL_HINTS;
 use strum::IntoEnumIterator;
 
-use super::{Hint, HintExtension};
+use super::{HintExtension, OsHint, SyscallHint};
 use crate::hints::types::HintEnum;
 
 #[test]
 fn test_hint_strings_are_unique() {
-    let hint_strings = Hint::iter().map(|hint| hint.to_str()).collect::<Vec<_>>();
+    let hint_strings = OsHint::iter().map(|hint| hint.to_str()).collect::<Vec<_>>();
     let hint_extension_strings =
         HintExtension::iter().map(|hint| hint.to_str()).collect::<Vec<_>>();
+    let syscall_hint_strings = SyscallHint::iter().map(|hint| hint.to_str()).collect::<Vec<_>>();
     let hint_strings_set: HashSet<&&str> = HashSet::from_iter(hint_strings.iter());
     let hint_extension_strings_set = HashSet::from_iter(hint_extension_strings.iter());
+    let syscall_hint_strings_set: HashSet<&&str> = HashSet::from_iter(syscall_hint_strings.iter());
     assert_eq!(hint_strings.len(), hint_strings_set.len(), "Duplicate hint strings.");
     assert_eq!(
         hint_extension_strings.len(),
         hint_extension_strings_set.len(),
         "Duplicate hint extension strings."
     );
-    let ambiguous_strings =
-        hint_strings_set.intersection(&hint_extension_strings_set).collect::<Vec<_>>();
-    assert!(ambiguous_strings.is_empty(), "Ambiguous hint strings: {ambiguous_strings:?}");
+    assert_eq!(
+        syscall_hint_strings.len(),
+        syscall_hint_strings_set.len(),
+        "Duplicate syscall hint strings."
+    );
+
+    assert!(
+        hint_strings_set.is_disjoint(&hint_extension_strings_set),
+        "{:?}",
+        hint_strings_set.intersection(&hint_extension_strings_set).collect::<Vec<_>>()
+    );
+    assert!(
+        hint_strings_set.is_disjoint(&syscall_hint_strings_set),
+        "{:?}",
+        hint_strings_set.intersection(&syscall_hint_strings_set).collect::<Vec<_>>()
+    );
+    assert!(
+        hint_extension_strings_set.is_disjoint(&syscall_hint_strings_set),
+        "{:?}",
+        hint_extension_strings_set.intersection(&syscall_hint_strings_set).collect::<Vec<_>>()
+    );
+}
+
+#[test]
+fn test_syscall_compatibility_with_blockifier() {
+    let syscall_hint_strings =
+        SyscallHint::iter().map(|hint| hint.to_str()).collect::<HashSet<_>>();
+    let blockifier_syscall_strings: HashSet<_> = SYSCALL_HINTS.iter().cloned().collect();
+    assert_eq!(blockifier_syscall_strings, syscall_hint_strings);
 }
