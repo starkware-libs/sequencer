5d64db58dced3253db3d540b10c22a9fb755a65f
Lev Roitman <lev@starkware.co>
2025-02-11 13:42:56 +0200

    feat(starknet_sequencer_metrics,starknet_sequencer_infra): adding gauges for message queues depth
    commit-id:30292dc9

--- crates/starknet_sequencer_metrics/src/metric_definitions.rs
+++ crates/starknet_sequencer_metrics/src/metric_definitions.rs
@@ -105,6 +105,16 @@ define_gauge_metrics!(
     MetricScope::Batcher => {
         { STORAGE_HEIGHT, "batcher_storage_height", "The height of the batcher's storage" }
     },
+    MetricScope::Infra => {
+        { BATCHER_QUEUE_DEPTH, "batcher_queue_depth", "The depth of the batcher's message queue" },
+        { CLASS_MANAGER_QUEUE_DEPTH, "fs_class_manager_queue_depth", "The depth of the class manager's message queue" },
+        { GATEWAY_QUEUE_DEPTH, "gateway_queue_depth", "The depth of the gateway's message queue" },
+        { L1_PROVIDER_QUEUE_DEPTH, "l1_provider_queue_depth", "The depth of the L1 provider's message queue" },
+        { MEMPOOL_QUEUE_DEPTH, "mempool_communication_wrapper_queue_depth", "The depth of the mempool's message queue" },
+        { MEMPOOL_P2P_QUEUE_DEPTH, "mempool_p2p_propagator_queue_depth", "The depth of the mempool p2p's message queue" },
+        { SIERRA_COMPILER_QUEUE_DEPTH, "sierra_compiler_queue_depth", "The depth of the sierra compiler's message queue" },
+        { STATE_SYNC_QUEUE_DEPTH, "state_sync_queue_depth", "The depth of the state sync's message queue" },
+    },
     MetricScope::Network => {
         { MEMPOOL_P2P_NUM_CONNECTED_PEERS, "apollo_mempool_p2p_num_connected_peers", "The number of connected peers to the mempool p2p component" },
         { CONSENSUS_NUM_CONNECTED_PEERS, "apollo_consensus_num_connected_peers", "The number of connected peers to the consensus p2p component" },
@@ -160,5 +170,5 @@ define_counter_metrics!(
         { MEMPOOL_P2P_NUM_RECEIVED_MESSAGES, "apollo_mempool_num_received_messages", "The number of messages received by the mempool p2p component", 0 },
         { CONSENSUS_NUM_SENT_MESSAGES, "apollo_consensus_num_sent_messages", "The number of messages sent by the consensus p2p component", 0 },
         { CONSENSUS_NUM_RECEIVED_MESSAGES, "apollo_consensus_num_received_messages", "The number of messages received by the consensus p2p component", 0 },
-    }
+    },
 );
