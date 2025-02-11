24e93a29c60ff1a4b462aec4d1f727d68b0b6560
Noam Spiegelstein <noam.s@starkware.co>
2025-02-11 22:28:24 +0200

    WIP_INTEGRATION_REVER_FLOW_TEST

--- crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_revert_flow_integration_test.rs
+++ crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_revert_flow_integration_test.rs
@@ -1,6 +1,3 @@
-use std::thread::sleep;
-use std::time::Duration;
-
 use starknet_api::block::BlockNumber;
 use starknet_integration_tests::integration_test_utils::integration_test_setup;
 use starknet_integration_tests::sequencer_manager::IntegrationTestManager;
@@ -13,9 +10,9 @@ async fn main() {
 }
 
 pub async fn end_to_end_revert_flow_integration() {
-    const BLOCK_TO_REVERT_TO: BlockNumber = BlockNumber(10);
-    const BLOCK_TO_REVERT_FROM: BlockNumber = BlockNumber(20);
-    const BLOCK_TO_WAIT_FOR_AFTER_REVERT: BlockNumber = BlockNumber(25);
+    const BLOCK_TO_REVERT_TO: BlockNumber = BlockNumber(2);
+    const BLOCK_TO_REVERT_FROM: BlockNumber = BlockNumber(15);
+    const BLOCK_TO_WAIT_FOR_AFTER_REVERT: BlockNumber = BlockNumber(20);
     const N_TXS: usize = 50;
     /// The number of consolidated local sequencers that participate in the test.
     const N_CONSOLIDATED_SEQUENCERS: usize = 1;
@@ -32,18 +29,16 @@ pub async fn end_to_end_revert_flow_integration() {
     integration_test_manager.run_nodes(node_indices.clone()).await;
     // Run the first block scenario to bootstrap the accounts.
     integration_test_manager.send_bootstrap_txs_and_verify().await;
-    integration_test_manager.send_invoke_txs_and_verify(N_TXS, BLOCK_TO_REVERT_TO).await;
     // Snapshot the tx generator so we can restore it after the revert.
     let tx_gen_snapshot = integration_test_manager.tx_generator().snapshot();
     integration_test_manager.send_invoke_txs_and_verify(N_TXS, BLOCK_TO_REVERT_FROM).await;
     integration_test_manager.shutdown_nodes(node_indices.clone());
     integration_test_manager.update_revert_config_to_all_idle_nodes(Some(BLOCK_TO_REVERT_TO));
     integration_test_manager.run_nodes(node_indices.clone()).await;
-    // allow the nodes to revert the blocks.
-    sleep(Duration::from_secs(5));
+    integration_test_manager.await_reverted_block(BLOCK_TO_REVERT_TO).await;
     integration_test_manager.shutdown_nodes(node_indices.clone());
-    integration_test_manager.update_revert_config_to_all_idle_nodes(None);
     *integration_test_manager.tx_generator_mut() = tx_gen_snapshot;
+    integration_test_manager.update_revert_config_to_all_idle_nodes(None);
     integration_test_manager.run_nodes(node_indices.clone()).await;
     integration_test_manager
         .send_invoke_txs_and_verify(N_TXS, BLOCK_TO_WAIT_FOR_AFTER_REVERT)
