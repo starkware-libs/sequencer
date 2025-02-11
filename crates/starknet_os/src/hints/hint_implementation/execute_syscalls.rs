6ca09968a08980cd476a993d7da5cd2b5be34542
amos rothberg <amos@starkware.co>
2025-02-11 13:10:29 +0200

    chore(starknet_os): added remaining moonsong hints

---
+++ crates/starknet_os/src/hints/hint_implementation/execute_syscalls.rs
@@ -0,0 +1,8 @@
+use crate::hints::error::HintResult;
+use crate::hints::types::HintArgs;
+
+pub fn is_block_number_in_block_hash_buffer(
+    HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>,
+) -> HintResult {
+    todo!()
+}
