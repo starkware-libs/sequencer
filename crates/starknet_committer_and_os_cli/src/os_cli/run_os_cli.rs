d2dd99cc2e738cded3980805b119d099f9e7404a
amos rothberg <amos@starkware.co>
2025-02-11 17:38:26 +0200

    chore(starknet_committer_and_os_cli): add rust side of hint compatability test

--- crates/starknet_committer_and_os_cli/src/os_cli/run_os_cli.rs
+++ crates/starknet_committer_and_os_cli/src/os_cli/run_os_cli.rs
@@ -4,6 +4,9 @@ use tracing::level_filters::LevelFilter;
 use tracing_subscriber::reload::Handle;
 use tracing_subscriber::Registry;
 
+use crate::os_cli::tests::python_tests::OsPythonTestRunner;
+use crate::shared_utils::types::{run_python_test, PythonTestArg};
+
 #[derive(Parser, Debug)]
 pub struct OsCliCommand {
     #[clap(subcommand)]
@@ -12,16 +15,17 @@ pub struct OsCliCommand {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    NotImplemented,
+    PythonTest(PythonTestArg),
 }
 
 pub async fn run_os_cli(
     os_command: OsCliCommand,
     _log_filter_handle: Handle<LevelFilter, Registry>,
 ) {
+    info!("Starting starknet-os-cli with command: \n{:?}", os_command);
     match os_command.command {
-        Command::NotImplemented => {
-            info!("Not implemented");
+        Command::PythonTest(python_test_arg) => {
+            run_python_test::<OsPythonTestRunner>(python_test_arg).await;
         }
     }
 }
