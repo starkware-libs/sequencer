29aeb188206ab045e0cf1b0c4f69d3db3b8bfe4a
dorimedini-starkware <dori@starkware.co>
2025-02-11 19:52:43 +0200

    feat(starknet_patricia_storage): split between dynamic and static prefix (#4045)

--- crates/starknet_patricia_storage/src/db_object.rs
+++ crates/starknet_patricia_storage/src/db_object.rs
@@ -1,12 +1,25 @@
 use crate::errors::DeserializationError;
 use crate::storage_trait::{create_db_key, DbKey, DbKeyPrefix, DbValue};
 
-pub trait DBObject {
-    /// Serializes the given value.
-    fn serialize(&self) -> DbValue;
-
+pub trait HasDynamicPrefix {
     /// Returns the storage key prefix of the DB object.
     fn get_prefix(&self) -> DbKeyPrefix;
+}
+
+pub trait HasStaticPrefix {
+    /// Returns the storage key prefix of the DB object.
+    fn get_static_prefix() -> DbKeyPrefix;
+}
+
+impl<T: HasStaticPrefix> HasDynamicPrefix for T {
+    fn get_prefix(&self) -> DbKeyPrefix {
+        T::get_static_prefix()
+    }
+}
+
+pub trait DBObject: HasDynamicPrefix {
+    /// Serializes the given value.
+    fn serialize(&self) -> DbValue;
 
     /// Returns a `DbKey` from a prefix and a suffix.
     fn get_db_key(&self, suffix: &[u8]) -> DbKey {
