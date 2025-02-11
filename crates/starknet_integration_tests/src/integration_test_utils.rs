6d5e9830c5e0faf2e4de6ca615ccb03ddc46c149
noamsp-starkware <noam.s@starkware.co>
2025-02-11 14:02:15 +0200

    refactor(starknet_integration_tests): move common setup to a function (#4058)

--- crates/starknet_integration_tests/src/integration_test_utils.rs
+++ crates/starknet_integration_tests/src/integration_test_utils.rs
@@ -1,10 +1,12 @@
 use std::process::Command;
 
-use tracing::{error, info};
+use starknet_sequencer_infra::trace_util::configure_tracing;
+use starknet_sequencer_node::test_utils::node_runner::get_node_executable_path;
+use tracing::{error, info, warn};
 
 // TODO(Tsabary): remove the hook definition once we transition to proper usage of task
 // spawning.
-pub fn set_panic_hook() {
+fn set_panic_hook() {
     let default_panic = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |info| {
         log_susceptible_ports();
@@ -58,7 +60,7 @@ fn log_susceptible_ports() {
 /// which can lead to unpredictable port collisions in integration tests that rely on fixed port
 /// usage. This function sets a narrower range (40000-40200) to limit port allocation to a small,
 /// controlled set of ports, reducing the likelihood of conflicts.
-pub fn set_ephemeral_port_range() {
+fn set_ephemeral_port_range() {
     let output = Command::new("sudo")
         .arg("sysctl")
         .arg("-w")
@@ -79,4 +81,18 @@ pub fn set_ephemeral_port_range() {
             eprintln!("Error executing sysctl command: {}", e);
         }
     }
+}
+
+pub async fn integration_test_setup(test_specifier: &str, description: &str) {
+    configure_tracing().await;
+    info!("Running {description} test setup.");
+    set_ephemeral_port_range();
+    set_panic_hook();
+
+    let sequencer_path = get_node_executable_path();
+    warn!(
+        "This test uses a compiled sequencer node binary located at {sequencer_path}. Make sure \
+         to pre-compile the binary before running this test. Alternatively, you can compile the \
+         binary and run this test with './scripts/sequencer_integration_test.sh {test_specifier}'"
+    );
 }
