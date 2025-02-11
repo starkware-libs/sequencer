89af1719e992f8ea80f00df62050ca4ac26b4973
Dori Medini <dori@starkware.co>
2025-02-11 16:49:28 +0200

    feat(starknet_os): add resource / gas related hints

---
+++ crates/starknet_os/src/hints/hint_implementation/resources.rs
@@ -0,0 +1,14 @@
+use crate::hints::error::HintResult;
+use crate::hints::types::HintArgs;
+
+pub fn remaining_gas_gt_max(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
+
+pub fn debug_expected_initial_gas(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
+
+pub fn is_sierra_gas_mode(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_>) -> HintResult {
+    todo!()
+}
