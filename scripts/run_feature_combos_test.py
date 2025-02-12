3da48a6e57bf3d0479ad5be0f5f86298819867aa
Dori Medini <dori@starkware.co>
2025-02-12 17:17:49 +0200

    feat(ci): gather all errors in feature combos test
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/run_feature_combos_test.py
+++ scripts/run_feature_combos_test.py
@@ -21,11 +21,20 @@ def build_with_all_features(package: str):
 def main():
     packages = get_workspace_packages()
     print(f"Building {len(packages)} packages without features.", flush=True)
+    featureless_failures, feature_failures = {}, {}
     for package in packages:
-        build_without_features(package)
+        try:
+            build_without_features(package)
+        except Exception as e:
+            featureless_failures[package] = str(e)
     print(f"Building {len(packages)} packages with all features.", flush=True)
     for package in packages:
-        build_with_all_features(package)
+        try:
+            build_with_all_features(package)
+        except Exception as e:
+            feature_failures[package] = str(e)
+    failures = {"featureless": featureless_failures, "featured": feature_failures}
+    assert failures == {"featureless": {}, "featured": {}}
 
 
 if __name__ == "__main__":
