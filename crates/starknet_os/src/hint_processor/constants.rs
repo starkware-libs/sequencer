98152a15b7de0bb5ac5bf00b1a219737133a4f31
Dori Medini <dori@starkware.co>
2025-02-16 19:11:21 +0200

    feat(starknet_os): implement Constants::fetch

--- crates/starknet_os/src/hint_processor/constants.rs
+++ crates/starknet_os/src/hint_processor/constants.rs
@@ -1,5 +1,20 @@
+use std::collections::HashMap;
+
+use cairo_vm::vm::errors::hint_errors::HintError;
+use starknet_types_core::felt::Felt;
+
+#[derive(Clone, Copy)]
 pub enum Constants {
     MerkleHeight,
+}
+
+impl Constants {
+    /// Fetch the constant value from the VM constants map. Returns an error if the constant is not
+    /// found.
+    pub fn fetch<'a>(&self, constants: &'a HashMap<String, Felt>) -> Result<&'a Felt, HintError> {
+        let identifier_str: &'static str = (*self).into();
+        constants.get(identifier_str).ok_or(HintError::MissingConstant(Box::new(identifier_str)))
+    }
 }
 
 impl From<Constants> for &'static str {
