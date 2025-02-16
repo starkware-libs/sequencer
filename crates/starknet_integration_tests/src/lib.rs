d5be05c543cba32dcecb973c63a49d162127075e
nadin-Starkware <nadin@starkware.co>
2025-02-16 17:09:36 +0200

    feat(starknet_integration_tests): support multi nodes setup
    commit-id:5abc7943

--- crates/starknet_integration_tests/src/lib.rs
+++ crates/starknet_integration_tests/src/lib.rs
@@ -3,7 +3,6 @@ pub mod flow_test_setup;
 pub mod integration_test_setup;
 pub mod integration_test_utils;
 pub mod monitoring_utils;
-pub mod node_setup;
 pub mod sequencer_manager;
 pub mod state_reader;
 pub mod utils;
