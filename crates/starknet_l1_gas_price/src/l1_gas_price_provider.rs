2c49d7c890cbffa7efc7b18ff80c69905d6cf8d9
Guy Nir <guy.n@starkware.co>
2025-02-17 15:59:45 +0200

    feat(starknet_l1_gas_price): serialize the config for gas price scraper and provider

--- crates/starknet_l1_gas_price/src/l1_gas_price_provider.rs
+++ crates/starknet_l1_gas_price/src/l1_gas_price_provider.rs
@@ -1,8 +1,10 @@
-use std::collections::VecDeque;
+use std::collections::{BTreeMap, VecDeque};
 
 #[cfg(any(feature = "testing", test))]
 use mockall::automock;
 use papyrus_base_layer::PriceSample;
+use papyrus_config::dumping::{ser_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
 use starknet_api::block::{BlockNumber, BlockTimestamp};
 use starknet_l1_gas_price_types::errors::L1GasPriceProviderError;
@@ -44,6 +46,32 @@ impl Default for L1GasPriceProviderConfig {
             lag_margin_seconds: 60,
             storage_limit: usize::try_from(10 * MEAN_NUMBER_OF_BLOCKS).unwrap(),
         }
+    }
+}
+
+impl SerializeConfig for L1GasPriceProviderConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from([
+            ser_param(
+                "number_of_blocks_for_mean",
+                &self.number_of_blocks_for_mean,
+                "Number of blocks to use for the mean gas price calculation",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "lag_margin_seconds",
+                &self.lag_margin_seconds,
+                "Time difference between latest L1 block and last block used for gas price \
+                 calculation",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "storage_limit",
+                &self.storage_limit,
+                "Maximum number of L1 blocks to keep cached at any given time",
+                ParamPrivacyInput::Public,
+            ),
+        ])
     }
 }
 
