b98692650aa039281dcf926505301df81a484ec9
Nimrod Weiss <nimrod@starkware.co>
2025-02-17 12:13:31 +0200

    feat(starknet_os): implement sequencer address hint

--- crates/starknet_os/src/hints/hint_implementation/block_context.rs
+++ crates/starknet_os/src/hints/hint_implementation/block_context.rs
@@ -1,4 +1,5 @@
 use blockifier::state::state_api::StateReader;
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_into_ap;
 
 use crate::hints::error::{OsHintExtensionResult, OsHintResult};
 use crate::hints::types::HintArgs;
@@ -37,8 +38,11 @@ pub(crate) fn deprecated_fee_token_address<S: StateReader>(
     todo!()
 }
 
-pub(crate) fn sequencer_address<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
-    todo!()
+pub(crate) fn sequencer_address<S: StateReader>(
+    HintArgs { hint_processor, vm, .. }: HintArgs<'_, S>,
+) -> OsHintResult {
+    let address = hint_processor.execution_helper.os_input.block_info.sequencer_address;
+    Ok(insert_value_into_ap(vm, address.0.key())?)
 }
 
 pub(crate) fn get_block_mapping<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
