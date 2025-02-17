f39c721081734d8dcee5b5543718cebb493c4ec3
Yoav Gross <yoav@starkware.co>
2025-02-17 14:54:26 +0200

    feat(starknet_os): copy dictionary_from_bucket

--- crates/starknet_os/src/hints/hint_implementation/stateless_compression.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression.rs
@@ -1,10 +1,16 @@
-use crate::hints::error::HintResult;
-use crate::hints::types::HintArgs;
+/// Array that specifies the number of bits allocated to each bucket.
+/// Values requiring fewer bits will be placed in smaller-bit buckets,
+/// and values requiring more bits will be placed in larger-bit buckets.
+const N_BITS_PER_BUCKET: [usize; 6] = [252, 125, 83, 62, 31, 15];
+const TOTAL_N_BUCKETS: usize = N_BITS_PER_BUCKET.len() + 1;
 
 pub(crate) fn dictionary_from_bucket(
-    HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_, '_>,
+    HintArgs { exec_scopes, .. }: HintArgs<'_, '_, '_, '_, '_, '_>,
 ) -> HintResult {
-    todo!()
+    let initial_dict: HashMap<MaybeRelocatable, MaybeRelocatable> =
+        (0..TOTAL_N_BUCKETS).map(|bucket_index| (Felt252::from(bucket_index).into(), Felt252::ZERO.into())).collect();
+    exec_scopes.insert_box(vars::scopes::INITIAL_DICT, Box::new(initial_dict));
+    Ok(())
 }
 
 pub(crate) fn get_prev_offset(HintArgs { .. }: HintArgs<'_, '_, '_, '_, '_, '_>) -> HintResult {
