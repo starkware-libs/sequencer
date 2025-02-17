89bb23662fe3a5fd4f9ed57cf63d8bf97c6c0bd9
Nimrod Weiss <nimrod@starkware.co>
2025-02-17 09:47:24 +0200

    feat(starknet_os): add execution helper to hint processor

--- crates/starknet_os/src/hint_processor/snos_hint_processor.rs
+++ crates/starknet_os/src/hint_processor/snos_hint_processor.rs
@@ -1,3 +1,4 @@
+use blockifier::state::cached_state::CachedState;
 use cairo_vm::hint_processor::hint_processor_definition::HintProcessorLogic;
 use cairo_vm::stdlib::any::Any;
 use cairo_vm::stdlib::boxed::Box;
@@ -6,9 +7,13 @@ use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::vm::vm_core::VirtualMachine;
 use starknet_types_core::felt::Felt;
 
+use crate::hint_processor::execution_helper::OsExecutionHelper;
+use crate::hint_processor::panicking_state_reader::PanickingStateReader;
 use crate::hints::error::{HintExtensionResult, HintResult};
 
-pub struct SnosHintProcessor;
+pub struct SnosHintProcessor {
+    _execution_helper: OsExecutionHelper<CachedState<PanickingStateReader>>,
+}
 
 impl HintProcessorLogic for SnosHintProcessor {
     fn execute_hint(
