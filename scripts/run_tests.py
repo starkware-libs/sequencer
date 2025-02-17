678ab0e7e03050ccebeb0182037608ff857c0624
Dori Medini <dori@starkware.co>
2025-02-17 19:24:47 +0200

    feat(infra): bump rust version
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -12,7 +12,7 @@ from tests_utils import (
 )
 
 # Set of files which - if changed - should trigger tests for all packages.
-ALL_TEST_TRIGGERS: Set[str] = {"Cargo.toml", "Cargo.lock"}
+ALL_TEST_TRIGGERS: Set[str] = {"Cargo.toml", "Cargo.lock", "rust-toolchain.toml"}
 
 
 # Enum of base commands.
