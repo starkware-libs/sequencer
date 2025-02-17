b790d3b0f49f706aa0fb4ac8efa576f2f7f2aa55
Nimrod Weiss <nimrod@starkware.co>
2025-02-17 09:35:57 +0200

    feat(starknet_os): add block context to execution helper

--- crates/starknet_os/src/hint_processor/execution_helper.rs
+++ crates/starknet_os/src/hint_processor/execution_helper.rs
@@ -1,17 +1,23 @@
 use blockifier::state::cached_state::CachedState;
 use blockifier::state::state_api::StateReader;
+use starknet_api::block::BlockInfo;
 
 use crate::io::os_input::StarknetOsInput;
 
 /// A helper struct that provides access to the OS state and commitments.
 pub struct OsExecutionHelper<S: StateReader> {
+    _block_info: BlockInfo,
     _cached_state: CachedState<S>,
     _os_input: StarknetOsInput,
 }
 
 impl<S: StateReader> OsExecutionHelper<S> {
-    pub fn new(os_input: StarknetOsInput) -> Self {
-        Self { _cached_state: Self::initialize_cached_state(&os_input), _os_input: os_input }
+    pub fn new(os_input: StarknetOsInput, block_info: BlockInfo) -> Self {
+        Self {
+            _block_info: block_info,
+            _cached_state: Self::initialize_cached_state(&os_input),
+            _os_input: os_input,
+        }
     }
 
     // TODO(Dori): Create a cached state with all initial read values from the OS input.
