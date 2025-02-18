43c6a96149ebe37793ab0842c1bf834a6894b95b
Alon Haramati <alonh@starkware.co>
2025-02-18 12:54:43 +0200

    feat(ci): compile bench targets

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -30,7 +30,7 @@ class BaseCommand(Enum):
             return ["cargo", "test"] + package_args
         elif self == BaseCommand.CLIPPY:
             clippy_args = package_args if len(package_args) > 0 else ["--workspace"]
-            return ["cargo", "clippy"] + clippy_args + ["--all-targets"]
+            return ["cargo", "clippy"] + clippy_args + ["--all-targets", "--all-features"]
         elif self == BaseCommand.DOC:
             doc_args = package_args if len(package_args) > 0 else ["--workspace"]
             return ["cargo", "doc", "--document-private-items", "--no-deps"] + doc_args
@@ -49,10 +49,6 @@ def test_crates(crates: Set[str], base_command: BaseCommand):
     Runs tests for the given crates.
     If no crates provided, runs tests for all crates.
     """
-    args = []
-    for package in crates:
-        args.extend(["--package", package])
-
     # If crates is empty (i.e. changes_only is False), all packages will be tested (no args).
     cmd = base_command.cmd(crates=crates)
 
