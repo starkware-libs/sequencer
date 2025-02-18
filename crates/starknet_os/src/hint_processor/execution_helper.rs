b1141b0f19ad13ea76f3edbd8261e18c9a010553
Nimrod Weiss <nimrod@starkware.co>
2025-02-18 15:25:45 +0200

    feat(starknet_os): implement alias entry hint

--- crates/starknet_os/src/hint_processor/execution_helper.rs
+++ crates/starknet_os/src/hint_processor/execution_helper.rs
@@ -8,16 +8,12 @@ use crate::io::os_input::StarknetOsInput;
 pub struct OsExecutionHelper<S: StateReader> {
     pub(crate) cached_state: CachedState<S>,
     pub(crate) os_input: StarknetOsInput,
-    pub(crate) _os_program: Program,
+    pub(crate) os_program: Program,
 }
 
 impl<S: StateReader> OsExecutionHelper<S> {
     pub fn new(os_input: StarknetOsInput, os_program: Program) -> Self {
-        Self {
-            cached_state: Self::initialize_cached_state(&os_input),
-            os_input,
-            _os_program: os_program,
-        }
+        Self { cached_state: Self::initialize_cached_state(&os_input), os_input, os_program }
     }
 
     // TODO(Dori): Create a cached state with all initial read values from the OS input.
