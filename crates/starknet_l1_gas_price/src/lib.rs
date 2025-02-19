daedfe0d54c7a78973f8a0488bc5126055b12eea
Guy Nir <guy.n@starkware.co>
2025-02-19 15:23:47 +0200

    feat(starknet_l1_gas_price): add communications module

--- crates/starknet_l1_gas_price/src/lib.rs
+++ crates/starknet_l1_gas_price/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod communication;
 pub mod l1_gas_price_provider;
 pub mod l1_gas_price_scraper;
--- crates/starknet_l1_gas_price_types/src/lib.rs
+++ crates/starknet_l1_gas_price_types/src/lib.rs
@@ -7,13 +7,26 @@ use errors::{L1GasPriceClientError, L1GasPriceProviderError};
 #[cfg(any(feature = "testing", test))]
 use mockall::automock;
 use papyrus_base_layer::{L1BlockNumber, PriceSample};
-use starknet_api::block::BlockTimestamp;
+use serde::{Deserialize, Serialize};
+use starknet_api::block::{BlockNumber, BlockTimestamp};
 
 pub type SharedL1GasPriceClient = Arc<dyn L1GasPriceProviderClient>;
 pub type L1GasPriceProviderResult<T> = Result<T, L1GasPriceProviderError>;
 pub type L1GasPriceProviderClientResult<T> = Result<T, L1GasPriceClientError>;
 
 pub type PriceInfo = (u128, u128); // (base_fee_per_gas, blob_fee)
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum L1GasPriceRequest {
+    GetGasPrice(BlockTimestamp),
+    AddGasPrice(BlockNumber, PriceSample),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum L1GasPriceResponse {
+    GetGasPrice(L1GasPriceProviderResult<(u128, u128)>),
+    AddGasPrice(L1GasPriceProviderResult<()>),
+}
 
 /// Serves as the provider's shared interface. Requires `Send + Sync` to allow transferring and
 /// sharing resources (inputs, futures) across threads.
