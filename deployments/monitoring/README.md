38aa8c643727fd4d3f80be542b1452605f7a552e
RoeiE-starkware <roei.e@starkware.co>
2025-02-19 23:25:52 +0200

    chore(infra): support the new naming convention and multiple dashboards

--- deployments/monitoring/README.md
+++ deployments/monitoring/README.md
@@ -1,6 +1,8 @@
 Local monitoring stack
 first enable pipenv:
+
 ```bash
+pipenv --python 3.12
 pipenv shell
 ```
 
@@ -8,8 +10,15 @@ to deploy run:
 ```bash
 ./deploy_local_stack.sh up -d
 ```
+This will deploy node, Promethous and Grafana containers and upload the src/dummy_json.json dashboard to the grafana
+
 
 to destroy:
 ```bash
 ./deploy_local_stack.sh down
+```
+
+to remove pipenv:
+```bash
+pipenv --rm
 ```
--- deployments/monitoring/deploy_local_stack.sh
+++ deployments/monitoring/deploy_local_stack.sh
@@ -3,7 +3,7 @@
 export monitoring_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 # Deploy the monitoring stack locally
-docker-compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
+docker compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
 if [ $ret -ne 0 ]; then
     echo "Failed to deploy the monitoring stack locally"
     exit 1
@@ -12,4 +12,6 @@ fi
 if [ "$1" == "down" ]; then
     exit $ret
 fi
-${monitoring_dir}/src/generate_dashboard.py --input-file ${monitoring_dir}/src/dummy_json.json --output-dir /tmp/ --upload
+
+pip install -r ${monitoring_dir}/src/requirements.txt
+python ${monitoring_dir}/src/dashboard_builder.py builder -j ${monitoring_dir}/src/dummy_json.json -o /tmp/dashboard_builder -d -u
