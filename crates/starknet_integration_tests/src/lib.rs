176ec003f2a776684715aec186d61a3a8e73831f
Nadin Jbara <93648739+nadin-Starkware@users.noreply.github.com>
2025-02-20 15:45:32 +0200

    refactor(starknet_integration_tests): move node component configs to seperate mod (#4090)
    commit-id:505b2210

--- crates/starknet_integration_tests/src/lib.rs
+++ crates/starknet_integration_tests/src/lib.rs
@@ -2,6 +2,7 @@ pub mod flow_test_setup;
 pub mod integration_test_setup;
 pub mod integration_test_utils;
 pub mod monitoring_utils;
+pub mod node_component_configs;
 pub mod sequencer_manager;
 pub mod sequencer_simulator_utils;
 pub mod state_reader;
