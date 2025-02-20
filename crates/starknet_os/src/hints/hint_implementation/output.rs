288ab1db06c16eab9271914066ee1675200afd39
rotem-starkware <rotem@starkware.co>
2025-02-20 16:08:13 +0200

    feat(starknet_os): implement set state updates start hint

--- crates/starknet_os/src/hints/hint_implementation/output.rs
+++ crates/starknet_os/src/hints/hint_implementation/output.rs
@@ -4,10 +4,11 @@ use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
     insert_value_from_var_name,
 };
 use cairo_vm::vm::errors::hint_errors::HintError;
-use cairo_vm::Felt252;
+use starknet_types_core::felt::Felt;
 
 use crate::hints::error::HintResult;
 use crate::hints::types::HintArgs;
+use crate::hints::vars::{Ids, Scope};
 
 pub(crate) fn set_tree_structure<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
     todo!()
@@ -17,27 +18,29 @@ pub(crate) fn set_state_updates_start<S: StateReader>(
     HintArgs { vm, exec_scopes, ids_data, ap_tracking, .. }: HintArgs<'_, S>,
 ) -> HintResult {
     let use_kzg_da_felt =
-        get_integer_from_var_name(vars::ids::USE_KZG_DA, vm, ids_data, ap_tracking)?;
+        get_integer_from_var_name(Ids::UseKzgDa.into(), vm, ids_data, ap_tracking)?;
 
     // Set `use_kzg_da` in globals since it will be used in `process_data_availability`
-    exec_scopes.insert_value(vars::scopes::USE_KZG_DA, use_kzg_da_felt);
+    exec_scopes.insert_value(Scope::UseKzgDa.into(), use_kzg_da_felt);
 
-    // Recompute `compress_state_updates` until this issue is fixed
+    // Recompute `compress_state_updates` until this issue is fixed in our VM version:
     // https://github.com/lambdaclass/cairo-vm/issues/1897
-    let full_output = get_integer_from_var_name(vars::ids::FULL_OUTPUT, vm, ids_data, ap_tracking)?;
-    let compress_state_updates = Felt252::ONE - full_output;
+    // TODO(Rotem): fix code when we update to VM 2.0.0 (fix should be available in one of the RCs).
+
+    let full_output = get_integer_from_var_name(Ids::FullOutput.into(), vm, ids_data, ap_tracking)?;
+    let compress_state_updates = Felt::ONE - full_output;
 
     let use_kzg_da = match use_kzg_da_felt {
-        x if x == Felt252::ONE => Ok(true),
-        x if x == Felt252::ZERO => Ok(false),
+        x if x == Felt::ONE => Ok(true),
+        x if x == Felt::ZERO => Ok(false),
         _ => Err(HintError::CustomHint(
             "ids.use_kzg_da is not a boolean".to_string().into_boxed_str(),
         )),
     }?;
 
     let use_compress_state_updates = match compress_state_updates {
-        x if x == Felt252::ONE => Ok(true),
-        x if x == Felt252::ZERO => Ok(false),
+        x if x == Felt::ONE => Ok(true),
+        x if x == Felt::ZERO => Ok(false),
         _ => Err(HintError::CustomHint(
             "ids.compress_state_updates is not a boolean".to_string().into_boxed_str(),
         )),
@@ -45,7 +48,7 @@ pub(crate) fn set_state_updates_start<S: StateReader>(
 
     if use_kzg_da || use_compress_state_updates {
         insert_value_from_var_name(
-            vars::ids::STATE_UPDATES_START,
+            Ids::StateUpdatesStart.into(),
             vm.add_memory_segment(),
             vm,
             ids_data,
@@ -54,7 +57,7 @@ pub(crate) fn set_state_updates_start<S: StateReader>(
     } else {
         // Assign a temporary segment, to be relocated into the output segment.
         insert_value_from_var_name(
-            vars::ids::STATE_UPDATES_START,
+            Ids::StateUpdatesStart.into(),
             vm.add_temporary_segment(),
             vm,
             ids_data,
