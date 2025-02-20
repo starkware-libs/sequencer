b479fb96d8ee70dfe3e6d76b6d19a410361abfd2
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-02-20 19:52:16 +0200

    fix(deployment): missing hyphen and update readme instructions (#4336)
    commit-id:42874457

--- deployments/monitoring/deploy_local_stack.sh
+++ deployments/monitoring/deploy_local_stack.sh
@@ -3,7 +3,7 @@
 export monitoring_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 # Deploy the monitoring stack locally
-docker compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
+docker-compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
 if [ $ret -ne 0 ]; then
     echo "Failed to deploy the monitoring stack locally"
     exit 1
