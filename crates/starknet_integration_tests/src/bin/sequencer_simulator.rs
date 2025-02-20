71383028c52092436b02b8cd42b9fd7f856ea99d
Nadin Jbara <93648739+nadin-Starkware@users.noreply.github.com>
2025-02-20 12:04:24 +0200

    chore(starknet_integration_tests): pass url arg to simulator (#4282)
    commit-id:abd39e06

--- crates/starknet_integration_tests/src/bin/sequencer_simulator.rs
+++ crates/starknet_integration_tests/src/bin/sequencer_simulator.rs
@@ -1,5 +1,4 @@
-use std::env::args;
-
+use clap::Parser;
 use starknet_api::block::BlockNumber;
 use starknet_integration_tests::integration_test_utils::set_panic_hook;
 use starknet_integration_tests::sequencer_simulator_utils::SequencerSimulator;
@@ -19,9 +18,11 @@ async fn main() -> anyhow::Result<()> {
     const BLOCK_TO_WAIT_FOR_BOOTSTRAP: BlockNumber = BlockNumber(2);
     const N_TXS: usize = 50;
 
+    let args = Args::parse();
+
     let mut tx_generator = create_integration_test_tx_generator();
 
-    let sequencer_simulator = SequencerSimulator::new(args().collect());
+    let sequencer_simulator = SequencerSimulator::new(args.config_file, args.url);
 
     info!("Sending bootstrap txs");
     sequencer_simulator.send_txs(&mut tx_generator, &BootstrapTxs, ACCOUNT_ID_0).await;
@@ -38,4 +39,14 @@ async fn main() -> anyhow::Result<()> {
     info!("Simulation completed successfully");
 
     Ok(())
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "sequencer_simulator", about = "Run sequencer simulator.")]
+struct Args {
+    #[arg(long)]
+    config_file: String,
+
+    #[arg(long, default_value = "http://127.0.0.1:8080")]
+    url: String,
 }
