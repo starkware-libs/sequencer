9e1f22159a8f598a94a4979d865a1663a2b935e2
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-02-20 11:05:30 +0200

    feat(starknet_os): implement read alias counter hint (#4237)

--- crates/starknet_os/src/hints/hint_implementation/stateful_compression.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateful_compression.rs
@@ -1,7 +1,10 @@
 use blockifier::state::state_api::StateReader;
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_into_ap;
+use cairo_vm::vm::errors::hint_errors::HintError;
 
 use crate::hints::error::HintResult;
 use crate::hints::types::HintArgs;
+use crate::hints::vars::Const;
 
 pub(crate) fn enter_scope_with_aliases<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
@@ -37,8 +40,25 @@ pub(crate) fn write_next_alias_from_key<S: StateReader>(
     todo!()
 }
 
-pub(crate) fn read_alias_counter<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
-    todo!()
+pub(crate) fn read_alias_counter<S: StateReader>(
+    HintArgs { hint_processor, vm, constants, .. }: HintArgs<'_, S>,
+) -> HintResult {
+    let aliases_contract_address = Const::get_alias_contract_address(constants)?;
+    let alias_counter_storage_key = Const::get_alias_counter_storage_key(constants)?;
+    let alias_counter = hint_processor
+        .execution_helper
+        .cached_state
+        .get_storage_at(aliases_contract_address, alias_counter_storage_key)
+        .map_err(|_| {
+            HintError::CustomHint(
+                format!(
+                    "Failed to read alias contract {aliases_contract_address} counter at key \
+                     {alias_counter_storage_key:?}."
+                )
+                .into(),
+            )
+        })?;
+    insert_value_into_ap(vm, alias_counter)
 }
 
 pub(crate) fn initialize_alias_counter<S: StateReader>(
