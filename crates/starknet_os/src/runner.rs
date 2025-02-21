0c30d6c640fd2331a051c576c7b9c3096393e681
Dori Medini <dori@starkware.co>
2025-02-21 19:05:27 +0200

    feat(starknet_os): implementation with placeholders for run_os

--- crates/starknet_os/src/runner.rs
+++ crates/starknet_os/src/runner.rs
@@ -1,20 +1,76 @@
 use blockifier::context::BlockContext;
 use blockifier::state::state_api::StateReader;
+use cairo_vm::cairo_run::CairoRunConfig;
 use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::program::Program;
+use cairo_vm::vm::errors::vm_exception::VmException;
+use cairo_vm::vm::runners::cairo_runner::CairoRunner;
 
 use crate::errors::StarknetOsError;
 use crate::hint_processor::execution_helper::OsExecutionHelper;
 use crate::hint_processor::panicking_state_reader::PanickingStateReader;
+use crate::hint_processor::snos_hint_processor::{
+    DeprecatedSyscallHintProcessor,
+    SnosHintProcessor,
+    SyscallHintProcessor,
+};
 use crate::io::os_input::StarknetOsInput;
 use crate::io::os_output::StarknetOsRunnerOutput;
 
 pub fn run_os<S: StateReader>(
-    _compiled_os: &[u8],
-    _layout: LayoutName,
+    compiled_os: &[u8],
+    layout: LayoutName,
     _block_context: BlockContext,
     os_input: StarknetOsInput,
 ) -> Result<StarknetOsRunnerOutput, StarknetOsError> {
-    let _execution_helper = OsExecutionHelper::<S>::new(os_input);
+    // Init CairoRunConfig.
+    let cairo_run_config =
+        CairoRunConfig { layout, relocate_mem: true, trace_enabled: true, ..Default::default() };
+    let allow_missing_builtins = cairo_run_config.allow_missing_builtins.unwrap_or(false);
+
+    // Load the Starknet OS Program.
+    let os_program = Program::from_bytes(compiled_os, Some(cairo_run_config.entrypoint))?;
+
+    // Init cairo runner.
+    let mut cairo_runner = CairoRunner::new(
+        &os_program,
+        cairo_run_config.layout,
+        cairo_run_config.proof_mode,
+        cairo_run_config.trace_enabled,
+    )?;
+
+    // Init the Cairo VM.
+    let end = cairo_runner.initialize(allow_missing_builtins)?;
+
+    // Create execution helper.
+    let execution_helper = OsExecutionHelper::<S>::new(os_input);
+
+    // Create syscall handlers.
+    let syscall_handler = SyscallHintProcessor {};
+    let deprecated_syscall_handler = DeprecatedSyscallHintProcessor {};
+
+    // Create the hint processor.
+    let mut snos_hint_processor =
+        SnosHintProcessor::new(execution_helper, syscall_handler, deprecated_syscall_handler);
+
+    // Run the Cairo VM.
+    cairo_runner
+        .run_until_pc(end, &mut snos_hint_processor)
+        .map_err(|err| VmException::from_vm_error(&cairo_runner, err))?;
+
+    // End the Cairo VM run.
+    let disable_trace_padding = false;
+    cairo_runner.end_run(
+        cairo_run_config.disable_trace_padding,
+        disable_trace_padding,
+        &mut snos_hint_processor,
+    )?;
+
+    if cairo_run_config.proof_mode {
+        cairo_runner.finalize_segments()?;
+    }
+
+    // TODO(Dori): implement the rest (from moonsong).
     todo!()
 }
 
