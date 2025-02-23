90226d6a957c8e915a35298e3aba1669715acf54
Yoav Gross <yoav@starkware.co>
2025-02-23 16:42:46 +0200

    feat(starknet_os): trait for unique bucket element

--- crates/starknet_os/src/hints/hint_implementation/stateless_compression/tests.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression/tests.rs
@@ -1,9 +1,36 @@
+use std::cmp::min;
+
 use assert_matches::assert_matches;
 use rstest::rstest;
 use starknet_types_core::felt::Felt;
 
-use super::utils::BitsArray;
+use super::utils::{BitLength, BitsArray};
 use crate::hints::error::OsHintError;
+use crate::hints::hint_implementation::stateless_compression::utils::{
+    BucketElement125,
+    BucketElementTrait,
+};
+
+// Utils
+
+pub fn unpack_felts<const LENGTH: usize>(
+    compressed: &[Felt],
+    n_elms: usize,
+) -> Vec<BitsArray<LENGTH>> {
+    let n_elms_per_felt = BitLength::min_bit_length(LENGTH).unwrap().n_elems_in_felt();
+    let mut result = Vec::with_capacity(n_elms);
+
+    for felt in compressed {
+        let n_packed_elms = min(n_elms_per_felt, n_elms - result.len());
+        for chunk in felt.to_bits_le()[0..n_packed_elms * LENGTH].chunks_exact(LENGTH) {
+            result.push(BitsArray(chunk.try_into().unwrap()));
+        }
+    }
+
+    result
+}
+
+// Tests
 
 #[rstest]
 #[case::zero([false; 10], Felt::ZERO)]
@@ -23,4 +50,20 @@ fn test_overflow_bits_array(#[case] n_bits_felt: usize, #[case] felt: Felt) {
     assert_matches!(
         error, OsHintError::StatelessCompressionOverflow { n_bits, .. } if n_bits == n_bits_felt
     );
+}
+
+#[test]
+fn test_pack_and_unpack() {
+    let felts = [
+        Felt::from(34_u32),
+        Felt::from(0_u32),
+        Felt::from(11111_u32),
+        Felt::from(1034_u32),
+        Felt::from(3404_u32),
+    ];
+    let bucket: Vec<_> =
+        felts.into_iter().map(|f| BucketElement125::try_from(f).unwrap()).collect();
+    let packed = BucketElement125::pack_in_felts(&bucket).unwrap();
+    let unpacked = unpack_felts(packed.as_ref(), bucket.len());
+    assert_eq!(bucket, unpacked);
 }
