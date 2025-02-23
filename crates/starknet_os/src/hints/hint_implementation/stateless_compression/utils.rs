6ab150a2df9d5ac060e2b35d536c3613a8688cb9
Yoav Gross <yoav@starkware.co>
2025-02-23 16:42:46 +0200

    feat(starknet_os): bit length enum for unique buckets

--- crates/starknet_os/src/hints/hint_implementation/stateless_compression/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression/utils.rs
@@ -1,4 +1,61 @@
-/// Number of bits encoding each element (per bucket).
-pub(crate) const N_BITS_PER_BUCKET: [usize; 6] = [252, 125, 83, 62, 31, 15];
+use std::any::type_name;
+use std::cmp::max;
+
+use strum::EnumCount;
+use strum_macros::EnumCount;
+
+use crate::hints::error::OsHintError;
+
+pub(crate) const N_UNIQUE_BUCKETS: usize = BitLength::COUNT;
 /// Number of buckets, including the repeating values bucket.
-pub(crate) const TOTAL_N_BUCKETS: usize = N_BITS_PER_BUCKET.len() + 1;
+pub(crate) const TOTAL_N_BUCKETS: usize = N_UNIQUE_BUCKETS + 1;
+
+pub(crate) const MAX_N_BITS: usize = 251;
+
+#[derive(Debug, thiserror::Error)]
+enum ConversionError {
+    #[error("Convert {n_bits} bits for {type_name}.")]
+    StatelessCompressionOverflow { n_bits: usize, type_name: String },
+}
+
+#[derive(Debug, EnumCount)]
+enum BitLength {
+    Bits15,
+    Bits31,
+    Bits62,
+    Bits83,
+    Bits125,
+    Bits252,
+}
+
+impl BitLength {
+    const fn n_bits(&self) -> usize {
+        match self {
+            Self::Bits15 => 15,
+            Self::Bits31 => 31,
+            Self::Bits62 => 62,
+            Self::Bits83 => 83,
+            Self::Bits125 => 125,
+            Self::Bits252 => 252,
+        }
+    }
+
+    fn n_elems_in_felt(&self) -> usize {
+        max(MAX_N_BITS / self.n_bits(), 1)
+    }
+
+    fn min_bit_length(n_bits: usize) -> Result<Self, OsHintError> {
+        match n_bits {
+            _ if n_bits <= 15 => Ok(Self::Bits15),
+            _ if n_bits <= 31 => Ok(Self::Bits31),
+            _ if n_bits <= 62 => Ok(Self::Bits62),
+            _ if n_bits <= 83 => Ok(Self::Bits83),
+            _ if n_bits <= 125 => Ok(Self::Bits125),
+            _ if n_bits <= 252 => Ok(Self::Bits252),
+            _ => Err(OsHintError::StatelessCompressionOverflow {
+                n_bits,
+                type_name: type_name::<Self>().to_string(),
+            }),
+        }
+    }
+}
