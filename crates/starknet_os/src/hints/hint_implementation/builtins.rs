bf920c551c3a8c58ea6a5e2cf8c0c37a399318aa
Dori Medini <dori@starkware.co>
2025-02-23 15:55:57 +0200

    feat(starknet_os): rename hint result types

--- crates/starknet_os/src/hints/hint_implementation/builtins.rs
+++ crates/starknet_os/src/hints/hint_implementation/builtins.rs
@@ -1,16 +1,16 @@
 use blockifier::state::state_api::StateReader;
 
-use crate::hints::error::HintResult;
+use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
 
-pub(crate) fn selected_builtins<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn selected_builtins<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
 
-pub(crate) fn select_builtin<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn select_builtin<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
 
-pub(crate) fn update_builtin_ptrs<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn update_builtin_ptrs<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
--- crates/starknet_os/src/hints/hint_implementation/resources.rs
+++ crates/starknet_os/src/hints/hint_implementation/resources.rs
@@ -1,18 +1,18 @@
 use blockifier::state::state_api::StateReader;
 
-use crate::hints::error::HintResult;
+use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
 
-pub(crate) fn remaining_gas_gt_max<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn remaining_gas_gt_max<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn debug_expected_initial_gas<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
-pub(crate) fn is_sierra_gas_mode<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn is_sierra_gas_mode<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
