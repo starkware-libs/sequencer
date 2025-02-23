ed726894e388dfe3f5911553f47d5e817a1fbd62
Yoav Gross <yoav@starkware.co>
2025-02-23 16:42:46 +0200

    feat(starknet_os): bit length enum for unique buckets

--- crates/starknet_os/src/hints/hint_implementation/stateless_compression/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression/utils.rs
@@ -1,4 +1,59 @@
-/// Number of bits encoding each element (per bucket).
-pub(crate) const N_BITS_PER_BUCKET: [usize; 6] = [252, 125, 83, 62, 31, 15];
+use std::cmp::max;
+
+use strum::EnumCount;
+use strum_macros::EnumCount;
+
+pub(crate) const N_UNIQUE_BUCKETS: usize = BitLength::n_lengths();
 /// Number of buckets, including the repeating values bucket.
-pub(crate) const TOTAL_N_BUCKETS: usize = N_BITS_PER_BUCKET.len() + 1;
+pub(crate) const TOTAL_N_BUCKETS: usize = N_UNIQUE_BUCKETS + 1;
+
+pub(crate) const MAX_N_BITS: usize = 251;
+
+#[derive(Debug, EnumCount)]
+enum BitLength {
+    Bits15,
+    Bits31,
+    Bits62,
+    Bits83,
+    Bits125,
+    Bits252,
+}
+
+impl BitLength {
+    const fn n_bits(&self) -> usize {
+        match self {
+            BitLength::Bits15 => 15,
+            BitLength::Bits31 => 31,
+            BitLength::Bits62 => 62,
+            BitLength::Bits83 => 83,
+            BitLength::Bits125 => 125,
+            BitLength::Bits252 => 252,
+        }
+    }
+
+    fn n_elems_in_felt(&self) -> usize {
+        max(MAX_N_BITS / self.n_bits(), 1)
+    }
+
+    fn min_bit_length(n_bits: usize) -> Self {
+        if n_bits <= 15 {
+            BitLength::Bits15
+        } else if n_bits <= 31 {
+            BitLength::Bits31
+        } else if n_bits <= 62 {
+            BitLength::Bits62
+        } else if n_bits <= 83 {
+            BitLength::Bits83
+        } else if n_bits <= 125 {
+            BitLength::Bits125
+        } else if n_bits <= 252 {
+            BitLength::Bits252
+        } else {
+            panic!("Too many bits for BitLength {n_bits}")
+        }
+    }
+
+    const fn n_lengths() -> usize {
+        BitLength::COUNT
+    }
+}
