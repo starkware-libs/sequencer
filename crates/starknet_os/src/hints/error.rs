d1edfa90b1201677b6a265b7ccbfbbbb6a0c934e
Dori Medini <dori@starkware.co>
2025-02-25 12:59:32 +0200

    feat(starknet_os): implement os_input_transactions

--- crates/starknet_os/src/hints/error.rs
+++ crates/starknet_os/src/hints/error.rs
@@ -1,6 +1,8 @@
 use blockifier::state::errors::StateError;
 use cairo_vm::hint_processor::hint_processor_definition::HintExtension;
+use cairo_vm::types::errors::math_errors::MathError;
 use cairo_vm::vm::errors::hint_errors::HintError as VmHintError;
+use cairo_vm::vm::errors::memory_errors::MemoryError;
 use starknet_types_core::felt::Felt;
 
 use crate::hints::vars::{Const, Ids};
@@ -10,11 +12,15 @@ pub enum OsHintError {
     #[error("{id:?} value {felt} is not a boolean.")]
     BooleanIdExpected { id: Ids, felt: Felt },
     #[error("Failed to convert {variant:?} felt value {felt:?} to type {ty}: {reason:?}.")]
-    ConstConversionError { variant: Const, felt: Felt, ty: String, reason: String },
+    ConstConversion { variant: Const, felt: Felt, ty: String, reason: String },
     #[error(transparent)]
-    StateError(#[from] StateError),
+    Math(#[from] MathError),
     #[error(transparent)]
-    VmHintError(#[from] VmHintError),
+    Memory(#[from] MemoryError),
+    #[error(transparent)]
+    State(#[from] StateError),
+    #[error(transparent)]
+    VmHint(#[from] VmHintError),
     #[error("Unknown hint string: {0}")]
     UnknownHint(String),
 }
