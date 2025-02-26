9f6c846acdbad9cfd5265c4539d9276174e7181a
lev-starkware <155880815+lev-starkware@users.noreply.github.com>
2025-02-26 20:04:47 +0200

    chore(starknet_sequencer_metrics): extend the test to take multile arrays as input (#4494)
    commit-id:67457aed

--- crates/starknet_sequencer_dashboard/src/metric_definitions_test.rs
+++ crates/starknet_sequencer_dashboard/src/metric_definitions_test.rs
@@ -1,11 +1,27 @@
 use std::collections::HashSet;
 
-use starknet_sequencer_metrics::metric_definitions::ALL_METRICS;
+use starknet_sequencer_metrics::metric_definitions::{
+    BATCHER_ALL_METRICS,
+    GATEWAY_ALL_METRICS,
+    HTTP_SERVER_ALL_METRICS,
+    INFRA_ALL_METRICS,
+    NETWORK_ALL_METRICS,
+    PAPYRUS_SYNC_ALL_METRICS,
+};
 
 #[test]
 fn metric_names_no_duplications() {
-    let mut unique_metric_names: HashSet<&'static str> = HashSet::new();
-    for metric_name in ALL_METRICS {
-        assert!(unique_metric_names.insert(metric_name), "Duplicated metric name: {}", metric_name,);
+    let all_metric_names = BATCHER_ALL_METRICS
+        .iter()
+        .chain(GATEWAY_ALL_METRICS.iter())
+        .chain(HTTP_SERVER_ALL_METRICS.iter())
+        .chain(INFRA_ALL_METRICS.iter())
+        .chain(NETWORK_ALL_METRICS.iter())
+        .chain(PAPYRUS_SYNC_ALL_METRICS.iter())
+        .collect::<Vec<&&'static str>>();
+
+    let mut unique_metric_names: HashSet<&&'static str> = HashSet::new();
+    for metric_name in all_metric_names {
+        assert!(unique_metric_names.insert(metric_name), "Duplicated metric name: {}", metric_name);
     }
 }
