450f0804103479170cf8c2b864c9138c1686df5e
Dori Medini <dori@starkware.co>
2025-02-26 15:00:05 +0200

    feat(starknet_os): rename hint result types

--- crates/starknet_os/src/hints/hint_implementation/os.rs
+++ crates/starknet_os/src/hints/hint_implementation/os.rs
@@ -1,44 +1,44 @@
 use blockifier::state::state_api::StateReader;
 
-use crate::hints::error::HintResult;
+use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
 
 pub(crate) fn initialize_class_hashes<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn initialize_state_changes<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn write_full_output_to_memory<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn configure_kzg_manager<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn set_ap_to_prev_block_hash<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
 pub(crate) fn set_ap_to_new_block_hash<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
-) -> HintResult {
+) -> OsHintResult {
     todo!()
 }
 
-pub(crate) fn starknet_os_input<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> HintResult {
+pub(crate) fn starknet_os_input<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
     todo!()
 }
