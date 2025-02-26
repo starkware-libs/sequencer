135a96867161eb0e2d57dd599f740acdae56a3df
Nimrod Weiss <nimrod@starkware.co>
2025-02-26 11:34:34 +0200

    feat(starknet_os): add program as a field of execution helper

--- crates/starknet_os/src/errors.rs
+++ crates/starknet_os/src/errors.rs
@@ -1,2 +1,7 @@
+use cairo_vm::types::errors::program_errors::ProgramError;
+
 #[derive(Debug, thiserror::Error)]
-pub enum StarknetOsError {}
+pub enum StarknetOsError {
+    #[error(transparent)]
+    Program(#[from] ProgramError),
+}
