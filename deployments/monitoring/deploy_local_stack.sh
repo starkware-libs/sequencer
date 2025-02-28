ae3df2a0493c9bcccd9946b80fb20e94f8993177
Idan Shamam <idan@starkware.co>
2025-02-28 20:25:50 +0200

    chore(deployment): add node_setup, node_simulator, dummy_recorder to local monitoring stack

--- deployments/monitoring/deploy_local_stack.sh
+++ deployments/monitoring/deploy_local_stack.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-export monitoring_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+monitoring_dir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
 
-# Deploy the monitoring stack locally
-if command -v docker-compose &> /dev/null; then
-    echo "Running: docker-compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?"
-    docker-compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
+export monitoring_dir
+
+if command -v docker compose &> /dev/null; then
+  docker_compose="docker compose"
+elif command -v docker-compose &> /dev/null; then
+  docker_compose="docker-compose"
 else
-    echo "docker-compose not found, using docker compose"
-    docker compose -f ${monitoring_dir}/local/docker-compose.yml "$@"; ret=$?
+  echo "Error: docker compose is missing, please install and try again. Official docs: https://docs.docker.com/compose/install/linux/"
+  exit 1
 fi
-if [ $ret -ne 0 ]; then
+
+echo "Running: ${docker_compose} -f ${monitoring_dir}/local/docker-compose.yml $*"
+${docker_compose} -f "${monitoring_dir}"/local/docker-compose.yml "$@"
+ret=$?
+
+if [ "$ret" -ne 0 ]; then
     echo "Failed to deploy the monitoring stack locally"
     exit 1
 fi
 
 if [ "$1" == "down" ]; then
-    exit $ret
+    exit "$ret"
 fi
 
-pip install -r ${monitoring_dir}/src/requirements.txt
-python ${monitoring_dir}/src/dashboard_builder.py builder -j ${monitoring_dir}/../../Monitoring/sequencer/dev_grafana.json -o /tmp/dashboard_builder -d -u
+pip install -r "${monitoring_dir}"/src/requirements.txt
+python "${monitoring_dir}"/src/dashboard_builder.py builder -j "${monitoring_dir}"/../../Monitoring/sequencer/dev_grafana.json -o /tmp/dashboard_builder -d -u
+
+${docker_compose} -f "${monitoring_dir}"/local/docker-compose.yml logs -f
