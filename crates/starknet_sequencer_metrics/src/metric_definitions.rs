d58a76bd567e6543f6889e0ed97b40813b4a9699
Lev Roitman <lev@starkware.co>
2025-03-02 11:48:06 +0200

    chore(starknet_sequencer_metrics): moving Consensus metrics to consensus_manager
    commit-id:e68304db

--- crates/starknet_sequencer_metrics/src/metric_definitions.rs
+++ crates/starknet_sequencer_metrics/src/metric_definitions.rs
@@ -1,4 +1,4 @@
-use crate::metrics::{MetricCounter, MetricGauge, MetricScope};
+use crate::metrics::{MetricGauge, MetricScope};
 
 /// Macro to define all metric constants for specified scopes and store them in a collection.
 /// This generates:
@@ -44,16 +44,6 @@ macro_rules! define_metrics {
         )*
     };
 }
-
-define_metrics!(
-    Consensus => {
-        // Gauges
-        MetricGauge { CONSENSUS_NUM_CONNECTED_PEERS, "apollo_consensus_num_connected_peers", "The number of connected peers to the consensus p2p component" },
-        // Counters
-        MetricCounter { CONSENSUS_NUM_SENT_MESSAGES, "apollo_consensus_num_sent_messages", "The number of messages sent by the consensus p2p component", init = 0 },
-        MetricCounter { CONSENSUS_NUM_RECEIVED_MESSAGES, "apollo_consensus_num_received_messages", "The number of messages received by the consensus p2p component", init = 0 },
-    },
-);
 
 define_metrics!(
     Network => {
