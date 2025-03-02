2461265450b274a7f290979e039cd08122e9449a
yoavGrs <97383386+yoavGrs@users.noreply.github.com>
2025-03-02 15:04:21 +0200

    feat(starknet_os): generic bits array (#4391)

---
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression/tests.rs
@@ -0,0 +1,26 @@
+use assert_matches::assert_matches;
+use rstest::rstest;
+use starknet_types_core::felt::Felt;
+
+use super::utils::BitsArray;
+use crate::hints::error::OsHintError;
+
+#[rstest]
+#[case::zero([false; 10], Felt::ZERO)]
+#[case::thousand(
+    [false, false, false, true, false, true, true, true, true, true],
+    Felt::from(0b_0000_0011_1110_1000_u16),
+)]
+fn test_bits_array(#[case] expected: [bool; 10], #[case] felt: Felt) {
+    assert_eq!(BitsArray::<10>::try_from(felt).unwrap().0, expected);
+}
+
+#[rstest]
+#[case::max_fits(16, Felt::from(0xFFFF_u16))]
+#[case::overflow(252, Felt::MAX)]
+fn test_overflow_bits_array(#[case] n_bits_felt: usize, #[case] felt: Felt) {
+    let error = BitsArray::<10>::try_from(felt).unwrap_err();
+    assert_matches!(
+        error, OsHintError::StatelessCompressionOverflow { n_bits, .. } if n_bits == n_bits_felt
+    );
+}
