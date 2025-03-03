28c8f5a6bc4ae0433de7fa1caf8aa3318da61307
Nimrod Weiss <nimrod@starkware.co>
2025-03-03 09:49:02 +0200

    feat(starknet_os): remove compiled classes from os input

--- crates/starknet_os/src/hints/hint_implementation/deprecated_compiled_class.rs
+++ crates/starknet_os/src/hints/hint_implementation/deprecated_compiled_class.rs
@@ -1,6 +1,7 @@
 use std::any::Any;
 use std::collections::{HashMap, HashSet};
 
+use blockifier::execution::contract_class::{CompiledClassV0, RunnableCompiledClass};
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_from_var_name;
 use starknet_api::core::ClassHash;
@@ -12,21 +13,34 @@ use crate::hints::vars::{Ids, Scope};
 pub(crate) fn load_deprecated_class_facts<S: StateReader>(
     HintArgs { hint_processor, vm, exec_scopes, ids_data, ap_tracking, .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
-    let os_input = &hint_processor.execution_helper.os_input;
+    // TODO(Nimrod): See if we can avoid cloning here.
+    let deprecated_compiled_classes: HashMap<ClassHash, CompiledClassV0> = hint_processor
+        .execution_helper
+        .cached_state
+        .class_hash_to_class
+        .borrow()
+        .iter()
+        .filter_map(|(class_hash, class)| {
+            if let RunnableCompiledClass::V0(deprecated_class) = class {
+                Some((*class_hash, deprecated_class.clone()))
+            } else {
+                None
+            }
+        })
+        .collect();
     // TODO(Rotem): see if we can avoid cloning here.
     let deprecated_class_hashes: HashSet<ClassHash> =
-        HashSet::from_iter(os_input.deprecated_compiled_classes.keys().cloned());
+        HashSet::from_iter(deprecated_compiled_classes.keys().cloned());
     exec_scopes.insert_value(Scope::DeprecatedClassHashes.into(), deprecated_class_hashes);
 
     insert_value_from_var_name(
         Ids::NCompiledClassFacts.into(),
-        os_input.deprecated_compiled_classes.len(),
+        deprecated_compiled_classes.len(),
         vm,
         ids_data,
         ap_tracking,
     )?;
-    let scoped_classes: Box<dyn Any> =
-        Box::new(os_input.deprecated_compiled_classes.clone().into_iter());
+    let scoped_classes: Box<dyn Any> = Box::new(deprecated_compiled_classes.into_iter());
     exec_scopes
         .enter_scope(HashMap::from([(Scope::CompiledClassFacts.to_string(), scoped_classes)]));
 
