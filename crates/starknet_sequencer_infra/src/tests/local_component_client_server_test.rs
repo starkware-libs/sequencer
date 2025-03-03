e3ed1d0778b638d876db9d1797a269bf83081d76
Lev Roitman <lev@starkware.co>
2025-03-03 14:37:06 +0200

    chore(starknet_sequencer_infra): remove max_concurrency from the blocking local server
    commit-id:8b5dd272

--- crates/starknet_sequencer_infra/src/tests/local_component_client_server_test.rs
+++ crates/starknet_sequencer_infra/src/tests/local_component_client_server_test.rs
@@ -75,11 +75,10 @@ async fn local_client_server() {
     let component_a = ComponentA::new(Box::new(b_client.clone()));
     let component_b = ComponentB::new(setup_value, Box::new(a_client.clone()));
 
-    let max_concurrency = 1;
     let mut component_a_server =
-        LocalComponentServer::new(component_a, rx_a, max_concurrency, TEST_LOCAL_SERVER_METRICS);
+        LocalComponentServer::new(component_a, rx_a, TEST_LOCAL_SERVER_METRICS);
     let mut component_b_server =
-        LocalComponentServer::new(component_b, rx_b, max_concurrency, TEST_LOCAL_SERVER_METRICS);
+        LocalComponentServer::new(component_b, rx_b, TEST_LOCAL_SERVER_METRICS);
 
     task::spawn(async move {
         let _ = component_a_server.start().await;
