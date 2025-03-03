83f0732d81337bf6c7b897e7540d542f9ebbfa07
Guy Nir <guy.n@starkware.co>
2025-03-03 15:44:25 +0200

    chore(consensus): add timeout margins to config

--- crates/starknet_consensus_orchestrator/src/config.rs
+++ crates/starknet_consensus_orchestrator/src/config.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
 use std::fmt::Debug;
+use std::time::Duration;
 
+use papyrus_config::converters::deserialize_milliseconds_to_duration;
 use papyrus_config::dumping::{ser_param, ser_required_param, SerializeConfig};
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializationType, SerializedParam};
 use serde::{Deserialize, Serialize};
@@ -22,6 +24,16 @@ pub struct ContextConfig {
     pub l1_da_mode: bool,
     /// The address of the contract that builds the block.
     pub builder_address: ContractAddress,
+    /// Safety margin in milliseconds to make sure that the batcher completes building the proposal
+    /// with enough time for the Fin to be checked by validators.
+    #[serde(deserialize_with = "deserialize_milliseconds_to_duration")]
+    pub build_proposal_margin: Duration,
+    // When validating a proposal the Context is responsible for timeout handling. The Batcher
+    // though has a timeout as a defensive measure to make sure the proposal doesn't live
+    // forever if the Context crashes or has a bug.
+    /// Safety margin in milliseconds to allow the batcher to successfully validate a proposal.
+    #[serde(deserialize_with = "deserialize_milliseconds_to_duration")]
+    pub validate_proposal_margin: Duration,
 }
 
 impl SerializeConfig for ContextConfig {
@@ -64,6 +76,20 @@ impl SerializeConfig for ContextConfig {
                 "The address of the contract that builds the block.",
                 ParamPrivacyInput::Public,
             ),
+            ser_param(
+                "build_proposal_margin",
+                &self.build_proposal_margin.as_millis(),
+                "Safety margin (in ms) to make sure that the batcher completes building the \
+                 proposal with enough time for the Fin to be checked by validators.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "validate_proposal_margin",
+                &self.validate_proposal_margin.as_millis(),
+                "Safety margin (in ms) to make sure that consensus determines when to timeout \
+                 validating a proposal.",
+                ParamPrivacyInput::Public,
+            ),
         ])
     }
 }
@@ -77,6 +103,8 @@ impl Default for ContextConfig {
             block_timestamp_window: 1,
             l1_da_mode: true,
             builder_address: ContractAddress::default(),
+            build_proposal_margin: Duration::from_millis(1000),
+            validate_proposal_margin: Duration::from_millis(10_000),
         }
     }
 }
