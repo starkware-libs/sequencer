346dda2b618633a7711583045316f49c3821bc6c
amos rothberg <amos@starkware.co>
2025-03-03 17:10:56 +0200

    fix(starknet_os): fix run_cairo_0_entry_point

--- crates/starknet_os/src/test_utils/cairo_runner.rs
+++ crates/starknet_os/src/test_utils/cairo_runner.rs
@@ -2,7 +2,7 @@ use blockifier::execution::call_info::Retdata;
 use cairo_vm::hint_processor::hint_processor_definition::HintProcessor;
 use cairo_vm::types::layout_name::LayoutName;
 use cairo_vm::types::program::Program;
-use cairo_vm::types::relocatable::MaybeRelocatable;
+use cairo_vm::types::relocatable::{MaybeRelocatable, Relocatable};
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::vm::runners::cairo_runner::{CairoArg, CairoRunner};
 use cairo_vm::Felt252;
@@ -18,21 +18,17 @@ pub fn run_cairo_0_entry_point(
     let trace_enabled = true;
     let mut cairo_runner =
         CairoRunner::new(program, LayoutName::all_cairo, proof_mode, trace_enabled).unwrap();
-    cairo_runner.initialize_function_runner()?;
+    let allow_missing_builtins = false;
+    cairo_runner.initialize_builtins(allow_missing_builtins).unwrap();
+    let program_base: Option<Relocatable> = None;
+    cairo_runner.initialize_segments(program_base);
 
-    // Implicit args.
     let mut entrypoint_args: Vec<CairoArg> = vec![
         MaybeRelocatable::from(Felt252::from(2_i128)).into(), // this is the entry point selector
         // this would be the output_ptr for example if our cairo function uses it
         MaybeRelocatable::from((2, 0)).into(),
     ];
-    // Explicit args.
-    let calldata_start = cairo_runner.vm.add_memory_segment();
-    let calldata_end = cairo_runner.vm.load_data(calldata_start, args).unwrap();
-    entrypoint_args.extend([
-        MaybeRelocatable::from(calldata_start).into(),
-        MaybeRelocatable::from(calldata_end).into(),
-    ]);
+    entrypoint_args.extend(args.iter().map(|arg| CairoArg::from(arg.clone())));
     let entrypoint_args: Vec<&CairoArg> = entrypoint_args.iter().collect();
     let verify_secure = true;
     let program_segment_size: Option<usize> = None;
@@ -52,10 +48,19 @@ pub fn run_cairo_0_entry_point(
     let return_values = cairo_runner.vm.get_return_values(n_expected_return_values).unwrap();
     Ok(Retdata(
         return_values
-            .iter()
-            .map(|m| {
-                m.get_int()
-                    .unwrap_or_else(|| panic!("Could not convert return data {} to integer.", m))
+            .into_iter()
+            .map(|m| match m {
+                MaybeRelocatable::Int(i) => i,
+                MaybeRelocatable::RelocatableValue(relocatable) => cairo_runner
+                    .vm
+                    .get_integer(relocatable)
+                    .unwrap_or_else(|err| {
+                        panic!(
+                            "Could not convert relocatable {relocatable:?} to integer. error: \
+                             {err:?}"
+                        )
+                    })
+                    .into_owned(),
             })
             .collect(),
     ))
