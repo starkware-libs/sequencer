e06e77af3f8c40b67c2fe8680001145f6e8072f6
Alon Lukatch <alon.l@starkware.co>
2025-03-04 11:28:14 +0200

    refactor(starknet_integration_tests): make sequencer setup distributed configs modifiable

--- crates/starknet_integration_tests/src/bin/dump_test_preset_configs/system_test_dump_single_node_config.rs
+++ crates/starknet_integration_tests/src/bin/dump_test_preset_configs/system_test_dump_single_node_config.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use starknet_infra_utils::test_utils::TestIdentifier;
 use starknet_integration_tests::consts::SINGLE_NODE_CONFIG_PATH;
 use starknet_integration_tests::integration_test_utils::set_panic_hook;
+use starknet_integration_tests::node_component_configs::create_distributed_node_configs;
 use starknet_integration_tests::sequencer_manager::{get_sequencer_setup_configs, CustomPaths};
 use starknet_integration_tests::utils::create_integration_test_tx_generator;
 use starknet_sequencer_infra::trace_util::configure_tracing;
@@ -41,6 +42,7 @@ async fn main() {
         N_DISTRIBUTED_SEQUENCERS,
         Some(custom_paths),
         TestIdentifier::SystemTestDumpSingleNodeConfig,
+        create_distributed_node_configs,
     )
     .await;
 
