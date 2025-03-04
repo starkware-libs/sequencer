bf1666a7f6dcc15de6eac5d7a5da49de74131a4a
Itay Tsabary <itayt@starkware.co>
2025-03-04 13:48:07 +0200

    chore(starknet_sequencer_node): add deployment config

---
+++ crates/starknet_sequencer_node/src/deployment_definitions.rs
@@ -0,0 +1,20 @@
+use starknet_api::core::ChainId;
+
+use crate::deployment::{Deployment, Replicas, Service, ServiceName};
+
+// TODO(Tsabary): decide on the dir structure and naming convention for the deployment presets.
+
+// TODO(Tsabary): fill and order these.
+
+pub const MAIN_DEPLOYMENT_PRESET_PATH: &str = "config/sequencer/presets/main.json";
+
+const BATCHER_MAIN: Service =
+    Service::new(ServiceName::Batcher, "", false, Replicas::Single, Some(500));
+
+const GATEWAY_MAIN: Service =
+    Service::new(ServiceName::Gateway, "", false, Replicas::Multiple, None);
+
+const MEMPOOL_MAIN: Service = Service::new(ServiceName::Mempool, "", false, Replicas::Single, None);
+
+pub const MAIN_DEPLOYMENT: Deployment<'_> =
+    Deployment::new(ChainId::Mainnet, &[BATCHER_MAIN, GATEWAY_MAIN, MEMPOOL_MAIN]);
