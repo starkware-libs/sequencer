2e1d6d18ebaf8b2b6d42d4194edc5b0c9e36b28d
Lev Roitman <lev@starkware.co>
2025-03-04 19:14:06 +0200

    feat(starknet_sequencer_metrics): adding labeled histogram metric and test
    commit-id:75b4c2ef

--- crates/starknet_sequencer_metrics/src/metrics_test.rs
+++ crates/starknet_sequencer_metrics/src/metrics_test.rs
@@ -1,13 +1,22 @@
 use metrics::set_default_local_recorder;
 use metrics_exporter_prometheus::PrometheusBuilder;
 
-use crate::metrics::{HistogramValue, MetricHistogram, MetricScope};
+use crate::metrics::{HistogramValue, LabeledMetricHistogram, MetricHistogram, MetricScope};
 
 const HISTOGRAM_TEST_METRIC: MetricHistogram =
     MetricHistogram::new(MetricScope::Infra, "histogram_test_metric", "Histogram test metrics");
 
+const LABEL: &[(&str, &str)] = &[("label1", "value1")];
+
+const LABELED_HISTOGRAM_TEST_METRIC: LabeledMetricHistogram = LabeledMetricHistogram::new(
+    MetricScope::Infra,
+    "labeled_histogram_test_metric",
+    "Labeled histogram test metrics",
+    &[LABEL]
+);
+
 #[test]
-fn histogram_parse() {
+fn histogram_run_and_parse() {
     let recorder = PrometheusBuilder::new().build_recorder();
     let _recorder_guard = set_default_local_recorder(&recorder);
 
@@ -37,6 +46,41 @@ fn histogram_parse() {
 
     assert_eq!(
         HISTOGRAM_TEST_METRIC.parse_histogram_metric(&metrics_as_string).unwrap(),
+        expected_histogram
+    );
+}
+
+#[test]
+fn labeled_histogram_run_and_parse() {
+    let recorder = PrometheusBuilder::new().build_recorder();
+    let _recorder_guard = set_default_local_recorder(&recorder);
+
+    LABELED_HISTOGRAM_TEST_METRIC.register();
+    LABELED_HISTOGRAM_TEST_METRIC.record(1, LABEL);
+    LABELED_HISTOGRAM_TEST_METRIC.record(100, LABEL);
+    LABELED_HISTOGRAM_TEST_METRIC.record(80, LABEL);
+    LABELED_HISTOGRAM_TEST_METRIC.record(50, LABEL);
+    LABELED_HISTOGRAM_TEST_METRIC.record_many(93, 1, LABEL);
+
+    let metrics_as_string = recorder.handle().render();
+
+    let quantiles = vec![
+        ("0".to_string(), 1.0),
+        ("0.5".to_string(), 80.00587021001003),
+        ("0.9".to_string(), 92.99074853701167),
+        ("0.95".to_string(), 92.99074853701167),
+        ("0.99".to_string(), 92.99074853701167),
+        ("0.999".to_string(), 92.99074853701167),
+        ("1".to_string(), 100.0),
+    ];
+    let expected_histogram = HistogramValue {
+        sum: 324.0,
+        count: 5,
+        histogram: quantiles.into_iter().map(|(k, v)| (k.to_string(), v)).collect(),
+    };
+
+    assert_eq!(
+        LABELED_HISTOGRAM_TEST_METRIC.parse_histogram_metric(&metrics_as_string, LABEL).unwrap(),
         expected_histogram
     );
 }
