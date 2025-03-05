f497eb0134264967f3f6cb30822535bffb2ce76b
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-03-05 15:51:55 +0200

    chore(starknet_sequencer_node): add deployment config (#4681)

--- crates/starknet_sequencer_node/src/lib.rs
+++ crates/starknet_sequencer_node/src/lib.rs
@@ -2,6 +2,8 @@ pub mod clients;
 pub mod communication;
 pub mod components;
 pub mod config;
+mod deployment;
+pub mod deployment_definitions;
 pub mod servers;
 #[cfg(any(feature = "testing", test))]
 pub mod test_utils;
