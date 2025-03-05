0769b0f6d1f5a4dc290cd7072842c06517f798f8
Idan Shamam <idan@starkware.co>
2025-03-05 22:34:20 +0200

    refactor(deployment): cdk8s presets instead of configs

--- deployments/sequencer/services/topology.py
+++ deployments/sequencer/services/topology.py
@@ -1,30 +1,53 @@
 import dataclasses
 import typing
+import json
+
+from jsonschema import validate, ValidationError
+
+from services import objects
 
 
-from services import objects, topology_helpers
+class DeploymentConfig:
+    SCHEMA_FILE = "deployment_config_schema.json"
+
+    def __init__(self, deployment_config_file: str):
+        self.deployment_config_file_path = deployment_config_file
+        self._deployment_config_data = self._read_deployment_config_file()
+        self._schema = self._load_schema()
+        self._validate_deployment_config()
+
+    def _validate_deployment_config(self):
+        try:
+            validate(instance=self._deployment_config_data, schema=self._schema)
+        except ValidationError as e:
+            raise ValueError(f"Invalid deployment config file: {e.message}")
+
+    def _load_schema(self):
+        with open(self.SCHEMA_FILE) as f:
+            return json.load(f)
+
+    def _read_deployment_config_file(self):
+        with open(self.deployment_config_file_path) as f:
+            return json.loads(f.read())
+
+    def get_chain_id(self):
+        return self._deployment_config_data.get("chain_id")
+
+    def get_image(self):
+        return self._deployment_config_data.get("image")
+
+    def get_services(self):
+        return [service for service in self._deployment_config_data.get("services", [])]
 
 
 @dataclasses.dataclass
 class ServiceTopology:
-    images: typing.Optional[typing.Mapping] = dataclasses.field(
-        default_factory=topology_helpers.get_images
-    )
-    deployment: typing.Optional[objects.Deployment] = dataclasses.field(
-        default_factory=topology_helpers.get_deployment
-    )
-    config: typing.Optional[objects.Config] = dataclasses.field(
-        default_factory=topology_helpers.get_dev_config
-    )
-    service: typing.Optional[objects.Service] = dataclasses.field(
-        default_factory=topology_helpers.get_service
-    )
-    pvc: typing.Optional[objects.PersistentVolumeClaim] = dataclasses.field(
-        default_factory=topology_helpers.get_pvc
-    )
-    ingress: typing.Optional[objects.Ingress] = dataclasses.field(
-        default_factory=topology_helpers.get_ingress
-    )
+    config: typing.Optional[objects.Config]
+    image: str
+    ingress: bool
+    autoscale: bool
+    storage: int | None
+
 
 class SequencerDev(ServiceTopology):
     pass
