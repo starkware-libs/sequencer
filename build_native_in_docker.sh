370b11a4e0fa060a9d7592074d7da837693b16dc
Idan Shamam <idan@starkware.co>
2025-03-05 11:32:36 +0200

    chore(ci): add docker-ci folder

--- build_native_in_docker.sh
+++ build_native_in_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/env bash
 set -e
 
+# Enables build-x for older versions of docker.
+export DOCKER_BUILDKIT=1
+
 docker_image_name=sequencer-ci
-dockerfile_path="ci/images/${docker_image_name}.Dockerfile"
+dockerfile_path="docker-ci/images/${docker_image_name}.Dockerfile"
 
 docker build . --build-arg USER_UID=$UID -t ${docker_image_name} --file ${dockerfile_path}
 
