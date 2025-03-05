c8631d94466312bd1a26b94a5a4aa47566777da4
Asmaa Magdoub <asmaa@starkware.co>
2025-03-05 14:34:37 +0200

    test(starknet_l1_gas_price): test eth_to_fri_rate

--- crates/starknet_l1_gas_price/src/price_oracle.rs
+++ crates/starknet_l1_gas_price/src/price_oracle.rs
@@ -4,6 +4,10 @@ use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use serde_json;
 use url::Url;
 
+#[cfg(test)]
+#[path = "price_oracle_test.rs"]
+pub mod price_oracle_test;
+
 fn btreemap_to_headermap(btree_map: BTreeMap<String, String>) -> HeaderMap {
     let mut header_map = HeaderMap::new();
 
