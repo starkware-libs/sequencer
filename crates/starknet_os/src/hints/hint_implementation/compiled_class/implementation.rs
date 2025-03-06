e52af10780161fc1dfa98af4a39d76589d18a06a
Nimrod Weiss <nimrod@starkware.co>
2025-03-06 12:26:31 +0200

    feat(starknet_os): implement load class inner hint

--- crates/starknet_os/src/hints/hint_implementation/compiled_class/implementation.rs
+++ crates/starknet_os/src/hints/hint_implementation/compiled_class/implementation.rs
@@ -1,10 +1,14 @@
+use blockifier::concurrency::test_utils::class_hash;
+use blockifier::execution::contract_class::RunnableCompiledClass;
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::get_integer_from_var_name;
+use cairo_vm::hint_processor::hint_processor_definition::HintExtension;
 
-use crate::hints::error::{OsHintError, OsHintResult};
+use crate::hints::error::{OsHintError, OsHintExtensionResult, OsHintResult};
+use crate::hints::hint_implementation::compiled_class::utils::insert_compiled_class_to_vm;
 use crate::hints::types::HintArgs;
 use crate::hints::vars::{CairoStruct, Ids};
-use crate::vm_utils::get_address_of_nested_fields;
+use crate::vm_utils::{get_address_of_nested_fields, insert_value_to_nested_field};
 
 pub(crate) fn assign_bytecode_segments<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
@@ -55,9 +59,6 @@ pub(crate) fn load_class<S: StateReader>(
 
     Ok(())
 }
-pub(crate) fn load_class_inner<S: StateReader>(HintArgs { .. }: HintArgs<'_, S>) -> OsHintResult {
-    todo!()
-}
 
 pub(crate) fn set_ap_to_segment_hash<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
@@ -68,5 +69,48 @@ pub(crate) fn set_ap_to_segment_hash<S: StateReader>(
 pub(crate) fn validate_compiled_class_facts_post_execution<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
+    todo!()
+}
+
+// Hint extensions.
+pub(crate) fn load_class_inner<S: StateReader>(
+    HintArgs { hint_processor, constants, vm, .. }: HintArgs<'_, S>,
+) -> OsHintExtensionResult {
+    let class_hash_to_contract_class =
+        &hint_processor.execution_helper.cached_state.class_hash_to_class;
+    let mut hint_extension = HintExtension::new();
+    let mut compiled_class_facts = vm.add_memory_segment();
+    // Iterate only over cairo 1 classes.
+    for (class_hash, class) in
+        class_hash_to_contract_class.borrow().iter().filter_map(|(class_hash, class)| {
+            if let RunnableCompiledClass::V1(v1_class) = class {
+                Some((class_hash, v1_class))
+            } else {
+                None
+            }
+        })
+    {
+        // Insert the class hash.
+        insert_value_to_nested_field(
+            compiled_class_facts,
+            var_type,
+            vm,
+            nested_fields,
+            identifier_getter,
+            val,
+        );
+        // Allocate for the compiled class and fill.
+        let compiled_class_address = vm.add_memory_segment();
+        insert_compiled_class_to_vm(
+            vm,
+            class.0.as_ref(),
+            &hint_processor.execution_helper.os_program,
+            &compiled_class_address,
+            constants,
+        )?;
+        // Insert the compiled class address.
+        vm.insert_value(compiled_class_facts, compiled_class_address)?;
+    }
+
     todo!()
 }
