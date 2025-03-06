f43b2f0b53ebdf95a36a15e143208249ea8e212a
Aner Ben Efraim <aner@starkware.co>
2025-03-06 14:09:04 +0200

    feat(starknet_os): implement hints is_deprecated and check_is_deprecated

--- crates/starknet_os/src/hints/vars.rs
+++ crates/starknet_os/src/hints/vars.rs
@@ -17,6 +17,7 @@ pub(crate) enum Scope {
     DictManager,
     DictTracker,
     InitialDict,
+    IsDeprecated,
     UseKzgDa,
 }
 
@@ -31,6 +32,7 @@ impl From<Scope> for &'static str {
             Scope::DictManager => "dict_manager",
             Scope::DictTracker => "dict_tracker",
             Scope::InitialDict => "initial_dict",
+            Scope::IsDeprecated => "is_deprecated",
             Scope::UseKzgDa => "use_kzg_da",
         }
     }
@@ -53,6 +55,7 @@ pub enum Ids {
     ContractAddress,
     ContractStateChanges,
     DictPtr,
+    ExecutionContext,
     FullOutput,
     Hash,
     NCompiledClassFacts,
@@ -81,6 +84,7 @@ impl From<Ids> for &'static str {
             Ids::ContractAddress => "contract_address",
             Ids::ContractStateChanges => "contract_state_changes",
             Ids::DictPtr => "dict_ptr",
+            Ids::ExecutionContext => "execution_context",
             Ids::FullOutput => "full_output",
             Ids::Hash => "hash",
             Ids::NCompiledClassFacts => "n_compiled_class_facts",
@@ -162,6 +166,7 @@ pub enum CairoStruct {
     DeprecatedCompiledClass,
     DeprecatedCompiledClassFact,
     DictAccess,
+    ExecutionContext,
     OsStateUpdate,
     StorageReadPtr,
     StorageReadRequestPtr,
@@ -182,6 +187,10 @@ impl From<CairoStruct> for &'static str {
                  DeprecatedCompiledClassFact"
             }
             CairoStruct::DictAccess => "starkware.cairo.common.dict_access.DictAccess",
+
+            CairoStruct::ExecutionContext => {
+                "starkware.starknet.core.os.execution.execute_entry_point.ExecutionContext"
+            }
             CairoStruct::OsStateUpdate => "starkware.starknet.core.os.state.state.OsStateUpdate",
             CairoStruct::StorageReadPtr => "starkware.starknet.common.syscalls.StorageRead*",
             CairoStruct::StorageReadRequestPtr => {
