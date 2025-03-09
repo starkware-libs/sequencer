783bbe00d054e138047917340f5bdd2ecbc9e805
Guy Nir <guy.n@starkware.co>
2025-03-09 12:15:12 +0200

    chore(consensus): add metric for consensus max cached height

--- crates/starknet_consensus/src/metrics.rs
+++ crates/starknet_consensus/src/metrics.rs
@@ -4,11 +4,13 @@ use starknet_sequencer_metrics::metrics::MetricGauge;
 define_metrics!(
     Consensus => {
         MetricGauge { CONSENSUS_BLOCK_NUMBER, "consensus_block_number", "The block number consensus is working to decide" },
-        MetricGauge { CONSENSUS_ROUND, "consensus_round", "The round of the state machine"}
+        MetricGauge { CONSENSUS_ROUND, "consensus_round", "The round of the state machine"},
+        MetricGauge { CONSENSUS_MAX_CACHED_HEIGHT, "consesnus_max_cached_height", "How many heights above current are cached"}
     },
 );
 
 pub(crate) fn register_metrics() {
     CONSENSUS_BLOCK_NUMBER.register();
     CONSENSUS_ROUND.register();
+    CONSENSUS_MAX_CACHED_HEIGHT.register();
 }
