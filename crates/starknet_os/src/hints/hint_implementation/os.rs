59bb2557ae7bf36c3c12b8623c216700580dcc0a
Aner Ben Efraim <aner@starkware.co>
2025-03-09 15:52:13 +0200

    fix(starknet_os): remove commented moonsong code

--- crates/starknet_os/src/hints/hint_implementation/os.rs
+++ crates/starknet_os/src/hints/hint_implementation/os.rs
@@ -3,7 +3,6 @@ use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_i
 
 use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
-use crate::io::os_input;
 
 pub(crate) fn initialize_class_hashes<S: StateReader>(
     HintArgs { .. }: HintArgs<'_, S>,
@@ -29,42 +28,12 @@ pub(crate) fn configure_kzg_manager<S: StateReader>(
     todo!()
 }
 
-// pub const SET_AP_TO_PREV_BLOCK_HASH: &str =
-//     indoc! {r#"memory[ap] = to_felt_or_relocatable(os_input.prev_block_hash)"#};
-
-// pub fn set_ap_to_prev_block_hash(
-//     vm: &mut VirtualMachine,
-//     exec_scopes: &mut ExecutionScopes,
-//     _ids_data: &HashMap<String, HintReference>,
-//     _ap_tracking: &ApTracking,
-//     _constants: &HashMap<String, Felt252>,
-// ) -> Result<(), HintError> { let os_input: Rc<StarknetOsInput> =
-//   exec_scopes.get(vars::scopes::OS_INPUT)?; insert_value_into_ap(vm, os_input.prev_block_hash)?;
-
-//     Ok(())
-// }
-
 pub(crate) fn set_ap_to_prev_block_hash<S: StateReader>(
     HintArgs { hint_processor, vm, .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
     let os_input = &hint_processor.execution_helper.os_input;
     Ok(insert_value_into_ap(vm, os_input.prev_block_hash.0)?)
 }
-
-// pub const SET_AP_TO_NEW_BLOCK_HASH: &str =
-//     "memory[ap] = to_felt_or_relocatable(os_input.new_block_hash)";
-
-// pub fn set_ap_to_new_block_hash(
-//     vm: &mut VirtualMachine,
-//     exec_scopes: &mut ExecutionScopes,
-//     _ids_data: &HashMap<String, HintReference>,
-//     _ap_tracking: &ApTracking,
-//     _constants: &HashMap<String, Felt252>,
-// ) -> Result<(), HintError> { let os_input: Rc<StarknetOsInput> =
-//   exec_scopes.get(vars::scopes::OS_INPUT)?; insert_value_into_ap(vm, os_input.new_block_hash)?;
-
-//     Ok(())
-// }
 
 pub(crate) fn set_ap_to_new_block_hash<S: StateReader>(
     HintArgs { hint_processor, vm, .. }: HintArgs<'_, S>,
