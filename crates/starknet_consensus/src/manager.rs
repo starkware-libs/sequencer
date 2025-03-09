55cfddeb891045c334177334f85fba5520f0523a
Guy Nir <guy.n@starkware.co>
2025-03-09 14:52:23 +0200

    chore(consensus): add metric for consensus number of cached messages

--- crates/starknet_consensus/src/manager.rs
+++ crates/starknet_consensus/src/manager.rs
@@ -25,7 +25,12 @@ use starknet_api::block::BlockNumber;
 use tracing::{debug, error, info, instrument, trace, warn};
 
 use crate::config::TimeoutsConfig;
-use crate::metrics::{register_metrics, CONSENSUS_BLOCK_NUMBER, CONSENSUS_MAX_CACHED_HEIGHT};
+use crate::metrics::{
+    register_metrics,
+    CONSENSUS_BLOCK_NUMBER,
+    CONSENSUS_CACHED_MESSAGES,
+    CONSENSUS_MAX_CACHED_HEIGHT,
+};
 use crate::single_height_consensus::{ShcReturn, SingleHeightConsensus};
 use crate::types::{BroadcastVoteChannel, ConsensusContext, ConsensusError, Decision, ValidatorId};
 
@@ -240,6 +245,7 @@ impl<ContextT: ConsensusContext> MultiHeightManager<ContextT> {
         height: BlockNumber,
         shc: &mut SingleHeightConsensus,
     ) -> Result<ShcReturn, ConsensusError> {
+        CONSENSUS_CACHED_MESSAGES.set_lossy(self.future_votes.entry(height.0).or_default().len());
         let mut tasks = match shc.start(context).await? {
             decision @ ShcReturn::Decision(_) => {
                 // Start should generate either TimeoutProposal (validator) or GetProposal
