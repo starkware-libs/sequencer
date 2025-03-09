eaf791d3d28cc56eee4ee6d5b625b1416e7f98cb
Guy Nir <guy.n@starkware.co>
2025-03-09 14:52:23 +0200

    chore(consensus): add metric for consensus number of cached messages

--- crates/starknet_consensus/src/metrics.rs
+++ crates/starknet_consensus/src/metrics.rs
@@ -5,7 +5,8 @@ define_metrics!(
     Consensus => {
         MetricGauge { CONSENSUS_BLOCK_NUMBER, "consensus_block_number", "The block number consensus is working to decide" },
         MetricGauge { CONSENSUS_ROUND, "consensus_round", "The round of the state machine"},
-        MetricGauge { CONSENSUS_MAX_CACHED_HEIGHT, "consesnus_max_cached_height", "How many heights above current are cached"}
+        MetricGauge { CONSENSUS_MAX_CACHED_HEIGHT, "consesnus_max_cached_height", "How many heights above current are cached"},
+        MetricGauge { CONSENSUS_CACHED_MESSAGES, "consensus_cached_messages", "How many messages are cached when starting to work on a new block number" },
     },
 );
 
@@ -13,4 +14,5 @@ pub(crate) fn register_metrics() {
     CONSENSUS_BLOCK_NUMBER.register();
     CONSENSUS_ROUND.register();
     CONSENSUS_MAX_CACHED_HEIGHT.register();
+    CONSENSUS_CACHED_MESSAGES.register();
 }
