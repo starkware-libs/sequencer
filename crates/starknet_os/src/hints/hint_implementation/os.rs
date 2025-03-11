49facd2a440daf2f4844427c7c134d58b277ed81
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2025-03-11 10:39:06 +0200

    feat(starknet_os): implement hint write_full_output_to_memory (#4771)
    * feat(starknet_os): implement hint write_full_output_to_memory
    
    * fix(starknet_os): remove commented moonsong code

--- crates/starknet_os/src/hints/hint_implementation/os.rs
+++ crates/starknet_os/src/hints/hint_implementation/os.rs
@@ -1,7 +1,10 @@
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_into_ap;
+use starknet_types_core::felt::Felt;
 
+use crate::hints::enum_definition::{AllHints, OsHint};
 use crate::hints::error::OsHintResult;
+use crate::hints::nondet_offsets::fetch_offset;
 use crate::hints::types::HintArgs;
 
 pub(crate) fn initialize_class_hashes<S: StateReader>(
@@ -17,9 +20,14 @@ pub(crate) fn initialize_state_changes<S: StateReader>(
 }
 
 pub(crate) fn write_full_output_to_memory<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, S>,
+    HintArgs { vm, hint_processor, .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
-    todo!()
+    let os_input = &hint_processor.execution_helper.os_input;
+    let full_output = Felt::from(os_input.full_output);
+    let offset = fetch_offset(AllHints::OsHint(OsHint::WriteFullOutputToMemory))?;
+    // TODO(Aner): maybe consider get_fp_with_offset(offset) instead of get_fp + offset? Or maybe
+    // even insert_value_to_fp_with_offset?
+    Ok(vm.insert_value((vm.get_fp() + offset)?, full_output)?)
 }
 
 pub(crate) fn configure_kzg_manager<S: StateReader>(
