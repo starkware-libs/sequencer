cca8212edfd38cf03bbfe4416181727cd7d410b7
Aner Ben Efraim <aner@starkware.co>
2025-03-11 11:27:10 +0200

    WIP: feat(starknet_os): implement hint initialize_class_hashes

--- crates/starknet_os/src/hints/hint_implementation/os.rs
+++ crates/starknet_os/src/hints/hint_implementation/os.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
+
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::insert_value_into_ap;
+use cairo_vm::types::relocatable::MaybeRelocatable;
+use cairo_vm::vm::vm_core::VirtualMachine;
 use starknet_types_core::felt::Felt;
 
 use crate::hints::enum_definition::{AllHints, OsHint};
@@ -7,10 +11,27 @@ use crate::hints::error::OsHintResult;
 use crate::hints::nondet_offsets::fetch_offset;
 use crate::hints::types::HintArgs;
 
+// "initial_dict = os_input.class_hash_to_compiled_class_hash"
+
+fn insert_hashmap_into_ap(vm: &mut VirtualMachine, hashmap: HashMap<Felt, Felt>) -> OsHintResult {
+    let flattened_hashmap: Vec<MaybeRelocatable> = hashmap
+        .iter()
+        .flat_map(|entry| [MaybeRelocatable::from(entry.0), MaybeRelocatable::from(entry.1)])
+        .collect();
+    let _addr_after_hashmap = vm.load_data(vm.get_ap(), &flattened_hashmap)?;
+    Ok(())
+}
+
 pub(crate) fn initialize_class_hashes<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, S>,
+    HintArgs { hint_processor, vm, .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
-    todo!()
+    let state_input = &hint_processor.execution_helper.cached_state;
+    let class_hash_to_compiled_class_hash =
+        &state_input.cache.clone().into_inner().initial_reads.compiled_class_hashes;
+    insert_hashmap_into_ap(
+        vm,
+        class_hash_to_compiled_class_hash.iter().map(|entry| (entry.0.0, entry.1.0)).collect(),
+    )
 }
 
 pub(crate) fn initialize_state_changes<S: StateReader>(
