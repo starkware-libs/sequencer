a8335d825ee1360b1ceed4a15e6636f0ac3359e7
Dori Medini <dori@starkware.co>
2025-03-12 18:09:58 +0200

    feat(starknet_os): fix store_da_segment implementation

--- crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
@@ -184,7 +184,6 @@ fn polynomial_coefficients_to_blob(coefficients: Vec<BigInt>) -> Result<Vec<u8>,
     serialize_blob(&fft_result)
 }
 
-#[allow(dead_code)]
 pub(crate) fn polynomial_coefficients_to_kzg_commitment(
     coefficients: Vec<BigInt>,
 ) -> Result<(BigInt, BigInt), FftError> {
