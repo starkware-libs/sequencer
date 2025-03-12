4ff0d6912da3a5b3b13d4117c22074ad9acd6d74
Idan Shamam <idan@starkware.co>
2025-03-12 14:04:04 +0200

    chore(deployment): python black all cdk8s files

--- deployments/sequencer/services/helpers.py
+++ deployments/sequencer/services/helpers.py
@@ -4,17 +4,9 @@ import argparse
 def argument_parser():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--namespace", required=True, type=str, help="Kubernetes namespace.")
     parser.add_argument(
-        "--namespace",
-        required=True,
-        type=str,
-        help="Kubernetes namespace."
-    )
-    parser.add_argument(
-        "--deployment-config-file",
-        required=True,
-        type=str,
-        help="Path to deployment config file."
+        "--deployment-config-file", required=True, type=str, help="Path to deployment config file."
     )
 
     return parser.parse_args()
--- deployments/sequencer/services/topology.py
+++ deployments/sequencer/services/topology.py
@@ -1,57 +1,15 @@
 import dataclasses
 import typing
-import json
-
-from jsonschema import validate, ValidationError
-
-from services import objects
 
 
-class DeploymentConfig:
-    SCHEMA_FILE = "deployment_config_schema.json"
-
-    def __init__(self, deployment_config_file: str):
-        self.deployment_config_file_path = deployment_config_file
-        self._deployment_config_data = self._read_deployment_config_file()
-        self._schema = self._load_schema()
-        self._validate_deployment_config()
-
-    def _validate_deployment_config(self):
-        try:
-            validate(instance=self._deployment_config_data, schema=self._schema)
-        except ValidationError as e:
-            raise ValueError(f"Invalid deployment config file: {e.message}")
-
-    def _load_schema(self):
-        with open(self.SCHEMA_FILE) as f:
-            return json.load(f)
-
-    def _read_deployment_config_file(self):
-        with open(self.deployment_config_file_path) as f:
-            return json.loads(f.read())
-
-    def get_chain_id(self):
-        return self._deployment_config_data.get("chain_id")
-
-    def get_image(self):
-        return self._deployment_config_data.get("image")
-
-    def get_services(self):
-        return [service for service in self._deployment_config_data.get("services", [])]
+from services.config import SequencerConfig
 
 
 @dataclasses.dataclass
 class ServiceTopology:
-    config: typing.Optional[objects.Config]
+    config: typing.Optional[SequencerConfig]
     image: str
     ingress: bool
+    replicas: int
     autoscale: bool
     storage: int | None
-
-
-class SequencerDev(ServiceTopology):
-    pass
-
-
-class SequencerProd(SequencerDev):
-    pass
