3d7ecf14951a1a5aff81d82751008992ef0620d4
Itay Tsabary <itayt@starkware.co>
2025-03-12 16:02:51 +0200

    chore(starknet_sequencer_node): add test deployment

--- crates/starknet_sequencer_node/src/bin/deployment_generator.rs
+++ crates/starknet_sequencer_node/src/bin/deployment_generator.rs
@@ -2,9 +2,15 @@ use starknet_infra_utils::dumping::serialize_to_file;
 use starknet_sequencer_node::deployment_definitions::{
     MAIN_DEPLOYMENT,
     MAIN_DEPLOYMENT_PRESET_PATH,
+    TESTING_DEPLOYMENT,
+    TESTING_DEPLOYMENT_PRESET_PATH,
 };
+
+// TODO(Tsabary): bundle deployment and its preset path together, and create a list of all of these
+// pairs. Then in the test, iterate over them and test each one.
 
 /// Creates the deployment json file.
 fn main() {
     serialize_to_file(MAIN_DEPLOYMENT, MAIN_DEPLOYMENT_PRESET_PATH);
+    serialize_to_file(TESTING_DEPLOYMENT, TESTING_DEPLOYMENT_PRESET_PATH);
 }
--- crates/starknet_sequencer_node/src/deployment_definitions_test.rs
+++ crates/starknet_sequencer_node/src/deployment_definitions_test.rs
@@ -1,10 +1,19 @@
 use starknet_infra_utils::dumping::serialize_to_file_test;
 
-use crate::deployment_definitions::{MAIN_DEPLOYMENT, MAIN_DEPLOYMENT_PRESET_PATH};
+use crate::deployment_definitions::{
+    MAIN_DEPLOYMENT,
+    MAIN_DEPLOYMENT_PRESET_PATH,
+    TESTING_DEPLOYMENT,
+    TESTING_DEPLOYMENT_PRESET_PATH,
+};
+
+// TODO(Tsabary): bundle deployment and its preset path together, and create a list of all of these
+// pairs. Then in the test, iterate over them and test each one.
 
 /// Test that the deployment file is up to date. To update it run:
 /// cargo run --bin deployment_generator -q
 #[test]
-fn default_dev_grafana_dashboard() {
+fn deployment_files_are_up_to_date() {
     serialize_to_file_test(MAIN_DEPLOYMENT, MAIN_DEPLOYMENT_PRESET_PATH);
+    serialize_to_file_test(TESTING_DEPLOYMENT, TESTING_DEPLOYMENT_PRESET_PATH);
 }
