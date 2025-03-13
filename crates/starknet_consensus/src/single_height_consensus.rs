d54077f451405e98900cb6b420ac7ea11e20c4c9
Guy Nir <guy.n@starkware.co>
2025-03-13 14:30:30 +0200

    feat(starknet_consensus_orchestrator): add metrics for consensus timeouts

--- crates/starknet_consensus/src/single_height_consensus.rs
+++ crates/starknet_consensus/src/single_height_consensus.rs
@@ -23,12 +23,15 @@ use tracing::{debug, info, instrument, trace, warn};
 
 use crate::config::TimeoutsConfig;
 use crate::metrics::{
+    TimeoutReason,
     CONSENSUS_BUILD_PROPOSAL_FAILED,
     CONSENSUS_BUILD_PROPOSAL_TOTAL,
     CONSENSUS_PROPOSALS_INVALID,
     CONSENSUS_PROPOSALS_VALIDATED,
     CONSENSUS_PROPOSALS_VALID_INIT,
     CONSENSUS_REPROPOSALS,
+    CONSENSUS_TIMEOUTS,
+    LABEL_NAME_TIMEOUT_REASON,
 };
 use crate::state_machine::{StateMachine, StateMachineEvent};
 use crate::types::{
@@ -269,10 +272,7 @@ impl SingleHeightConsensus {
             ShcEvent::TimeoutPropose(event)
             | ShcEvent::TimeoutPrevote(event)
             | ShcEvent::TimeoutPrecommit(event) => {
-                let leader_fn =
-                    |round: Round| -> ValidatorId { context.proposer(self.height, round) };
-                let sm_events = self.state_machine.handle_event(event, &leader_fn);
-                self.handle_state_machine_events(context, sm_events).await
+                self.handle_timeout(context, event, TimeoutReason::Propose.into()).await
             }
             ShcEvent::Prevote(StateMachineEvent::Prevote(proposal_id, round)) => {
                 let Some(last_vote) = &self.last_prevote else {
@@ -368,6 +368,18 @@ impl SingleHeightConsensus {
         };
         context.set_height_and_round(self.height, self.state_machine.round()).await;
         ret
+    }
+
+    async fn handle_timeout<ContextT: ConsensusContext>(
+        &mut self,
+        context: &mut ContextT,
+        event: StateMachineEvent,
+        timeout_reason: &'static str,
+    ) -> Result<ShcReturn, ConsensusError> {
+        CONSENSUS_TIMEOUTS.increment(1, &[(LABEL_NAME_TIMEOUT_REASON, timeout_reason)]);
+        let leader_fn = |round: Round| -> ValidatorId { context.proposer(self.height, round) };
+        let sm_events = self.state_machine.handle_event(event, &leader_fn);
+        self.handle_state_machine_events(context, sm_events).await
     }
 
     /// Handle vote messages from peer nodes.
