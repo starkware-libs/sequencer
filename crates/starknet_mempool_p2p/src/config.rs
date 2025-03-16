35d58a990ae3f3f44ccf6464b81ead79c8f0f9a4
Alon Lukatch <alon.l@starkware.co>
2025-03-16 11:36:33 +0200

    feat(starknet_mempool_p2p): add transaction batch size and rate to config

--- crates/starknet_mempool_p2p/src/config.rs
+++ crates/starknet_mempool_p2p/src/config.rs
@@ -1,5 +1,7 @@
 use std::collections::BTreeMap;
+use std::time::Duration;
 
+use papyrus_config::converters::deserialize_milliseconds_to_duration;
 use papyrus_config::dumping::{append_sub_config_name, ser_param, SerializeConfig};
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use papyrus_network::NetworkConfig;
@@ -13,6 +15,9 @@ pub struct MempoolP2pConfig {
     #[validate]
     pub network_config: NetworkConfig,
     pub network_buffer_size: usize,
+    pub max_transaction_batch_size: usize,
+    #[serde(deserialize_with = "deserialize_milliseconds_to_duration")]
+    pub transaction_batch_rate: Duration,
 }
 
 impl Default for MempoolP2pConfig {
@@ -20,6 +25,9 @@ impl Default for MempoolP2pConfig {
         Self {
             network_config: NetworkConfig { port: MEMPOOL_TCP_PORT, ..Default::default() },
             network_buffer_size: 10000,
+            // TODO(Eitan): Change to appropriate values.
+            max_transaction_batch_size: 1,
+            transaction_batch_rate: Duration::from_secs(1),
         }
     }
 }
@@ -27,12 +35,27 @@ impl Default for MempoolP2pConfig {
 impl SerializeConfig for MempoolP2pConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         vec![
-            BTreeMap::from_iter([ser_param(
-                "network_buffer_size",
-                &self.network_buffer_size,
-                "Network buffer size.",
-                ParamPrivacyInput::Public,
-            )]),
+            BTreeMap::from_iter([
+                ser_param(
+                    "network_buffer_size",
+                    &self.network_buffer_size,
+                    "Network buffer size.",
+                    ParamPrivacyInput::Public,
+                ),
+                ser_param(
+                    "transaction_batch_size",
+                    &self.max_transaction_batch_size,
+                    "Maximum number of transactions in each batch.",
+                    ParamPrivacyInput::Public,
+                ),
+                ser_param(
+                    "transaction_batch_rate",
+                    &self.transaction_batch_rate.as_millis(),
+                    "Maximum time until a transaction batch is closed and propagated in \
+                     milliseconds.",
+                    ParamPrivacyInput::Public,
+                ),
+            ]),
             append_sub_config_name(self.network_config.dump(), "network_config"),
         ]
         .into_iter()
