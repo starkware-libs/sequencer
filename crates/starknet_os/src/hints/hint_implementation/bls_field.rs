000b5959041fda39331aebc50af5b2d46680fbe2
Aner Ben Efraim <aner@starkware.co>
2025-03-17 13:25:15 +0200

    fix(starknet_os): remove commented moonsong code

--- crates/starknet_os/src/hints/hint_implementation/bls_field.rs
+++ crates/starknet_os/src/hints/hint_implementation/bls_field.rs
@@ -11,34 +11,6 @@ use crate::hints::types::HintArgs;
 use crate::hints::vars::{CairoStruct, Const, Ids};
 use crate::vm_utils::get_address_of_nested_fields;
 
-// /// From the Cairo code, we can make the current assumptions:
-// ///
-// /// * The limbs of value are in the range [0, BASE * 3).
-// /// * value is in the range [0, 2 ** 256).
-// pub fn compute_ids_low(
-//     vm: &mut VirtualMachine,
-//     _exec_scopes: &mut ExecutionScopes,
-//     ids_data: &HashMap<String, HintReference>,
-//     ap_tracking: &ApTracking,
-//     constants: &HashMap<String, Felt252>,
-// ) -> Result<(), HintError> { let value_ptr = get_relocatable_from_var_name(vars::ids::VALUE, vm,
-//   ids_data, ap_tracking)?; let d0 = vm.get_integer((value_ptr + BigInt3::d0_offset())?)?; let d1
-//   = vm.get_integer((value_ptr + BigInt3::d1_offset())?)?;
-
-//     let base = get_constant(vars::constants::BASE, constants)?;
-
-//     let mask = (BigUint::from(1u64) << 128) - BigUint::from(1u64);
-//     let low = (d0.as_ref() + d1.as_ref() * base).to_biguint() & mask;
-
-//     insert_value_from_var_name(vars::ids::LOW, Felt252::from(low), vm, ids_data, ap_tracking)?;
-
-//     Ok(())
-// }
-
-// pub const COMPUTE_IDS_LOW: &str = indoc! {r#"
-//     ids.low = (ids.value.d0 + ids.value.d1 * ids.BASE) & ((1 << 128) - 1)"#
-// };
-
 /// From the Cairo code, we can make the current assumptions:
 ///
 /// * The limbs of value are in the range [0, BASE * 3).
