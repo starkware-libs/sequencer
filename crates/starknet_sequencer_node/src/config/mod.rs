99bdf08584761d69f7b1a3f283577d3f3a16220b
Itay Tsabary <itayt@starkware.co>
2025-03-17 12:15:00 +0200

    chore(starknet_sequencer_node): add monitoring config

--- crates/starknet_sequencer_node/src/config/mod.rs
+++ crates/starknet_sequencer_node/src/config/mod.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 mod config_test;
 
+// TODO(Tsabary): deprecate usage of mod.rs files.
+
 pub mod component_config;
 pub mod component_execution_config;
 pub mod config_utils;
+pub mod monitoring;
 pub mod node_config;
