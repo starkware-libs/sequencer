e2128585b358d6c27467a1e0ece2dce9b61bfc0e
Alon Lukatch <alon.l@starkware.co>
2025-03-17 14:40:34 +0200

    test(starknet_mempool_p2p): test batch is broadcasted when queue is full

--- crates/starknet_mempool_p2p/src/propagator/test.rs
+++ crates/starknet_mempool_p2p/src/propagator/test.rs
@@ -1,4 +1,5 @@
 use futures::stream::StreamExt;
+use futures::FutureExt;
 use mockall::predicate;
 use papyrus_network::network_manager::test_utils::{
     mock_register_broadcast_topic,
@@ -62,4 +63,58 @@ async fn process_handle_continue_propagation() {
         .await;
     let message = timeout(TIMEOUT, continue_propagation_receiver.next()).await.unwrap().unwrap();
     assert_eq!(message, propagation_metadata);
+}
+
+#[tokio::test]
+async fn transaction_batch_broadcasted_on_max_size() {
+    let TestSubscriberChannels { mock_network, subscriber_channels } =
+        mock_register_broadcast_topic().expect("Failed to create mock network");
+    let BroadcastTopicChannels { broadcasted_messages_receiver: _, broadcast_topic_client } =
+        subscriber_channels;
+    let BroadcastNetworkMock { mut messages_to_broadcast_receiver, .. } = mock_network;
+
+    let mut seq = mockall::Sequence::new();
+    let mut transaction_converter = MockTransactionConverterTrait::new();
+
+    let first_internal_tx = InternalRpcTransaction::get_test_instance(&mut get_rng());
+    let first_rpc_transaction = RpcTransaction::get_test_instance(&mut get_rng());
+    let first_cloned_rpc_transaction = first_rpc_transaction.clone();
+
+    let second_internal_tx = InternalRpcTransaction::get_test_instance(&mut get_rng());
+    let second_rpc_transaction = RpcTransaction::get_test_instance(&mut get_rng());
+    let second_cloned_rpc_transaction = second_rpc_transaction.clone();
+
+    transaction_converter
+        .expect_convert_internal_rpc_tx_to_rpc_tx()
+        .with(predicate::eq(first_internal_tx.clone()))
+        .times(1)
+        .in_sequence(&mut seq)
+        .return_once(move |_| Ok(first_rpc_transaction));
+
+    transaction_converter
+        .expect_convert_internal_rpc_tx_to_rpc_tx()
+        .with(predicate::eq(second_internal_tx.clone()))
+        .times(1)
+        .in_sequence(&mut seq)
+        .return_once(move |_| Ok(second_rpc_transaction));
+
+    let mut mempool_p2p_propagator =
+        MempoolP2pPropagator::new(broadcast_topic_client, Box::new(transaction_converter), 2);
+    mempool_p2p_propagator
+        .handle_request(MempoolP2pPropagatorRequest::AddTransaction(first_internal_tx))
+        .await;
+
+    // Assert the first transaction does not trigger batch broadcast
+    assert!(messages_to_broadcast_receiver.next().now_or_never().is_none());
+
+    mempool_p2p_propagator
+        .handle_request(MempoolP2pPropagatorRequest::AddTransaction(second_internal_tx))
+        .await;
+
+    // Assert the second message does trigger batch broadcast
+    let message = timeout(TIMEOUT, messages_to_broadcast_receiver.next()).await.unwrap().unwrap();
+    assert_eq!(
+        message,
+        RpcTransactionBatch(vec![first_cloned_rpc_transaction, second_cloned_rpc_transaction])
+    );
 }
