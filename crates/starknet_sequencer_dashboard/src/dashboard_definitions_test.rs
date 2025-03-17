4d2cbe7df671426b67ff131e606899d76235937b
Yael Doweck <yael@starkware.co>
2025-03-17 17:53:03 +0200

    chore(starknet_sequencer_dashboard): add infra for alerts, and add the first example alert

--- crates/starknet_sequencer_dashboard/src/dashboard_definitions_test.rs
+++ crates/starknet_sequencer_dashboard/src/dashboard_definitions_test.rs
@@ -1,10 +1,12 @@
 use starknet_infra_utils::dumping::serialize_to_file_test;
 
+use crate::alert_definitions::{DEV_ALERTS_JSON_PATH, SEQUENCER_ALERTS};
 use crate::dashboard_definitions::{DEV_JSON_PATH, SEQUENCER_DASHBOARD};
 
-/// Test that the grafana dev dashboard file is up to date. To update the default config file, run:
-/// cargo run --bin sequencer_dashboard_generator -q
+/// Test that the grafana dev dashboard and alert files are up to date. To update the default config
+/// file, run: cargo run --bin sequencer_dashboard_generator -q
 #[test]
 fn default_dev_grafana_dashboard() {
     serialize_to_file_test(SEQUENCER_DASHBOARD, DEV_JSON_PATH);
+    serialize_to_file_test(SEQUENCER_ALERTS, DEV_ALERTS_JSON_PATH);
 }
