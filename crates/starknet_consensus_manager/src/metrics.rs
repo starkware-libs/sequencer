c220ee851b8a3736ae152d5bf6c80e389913d0e4
Alon Lukatch <alon.l@starkware.co>
2025-03-18 17:45:06 +0200

    refactor(papyrus_network): split metrics by topic

--- crates/starknet_consensus_manager/src/metrics.rs
+++ crates/starknet_consensus_manager/src/metrics.rs
@@ -1,5 +1,5 @@
-use starknet_sequencer_metrics::define_metrics;
-use starknet_sequencer_metrics::metrics::{MetricCounter, MetricGauge};
+use starknet_sequencer_metrics::metrics::{LabeledMetricCounter, MetricCounter, MetricGauge};
+use starknet_sequencer_metrics::{define_metrics, generate_permutation_labels};
 
 define_metrics!(
     Consensus => {
@@ -8,5 +8,21 @@ define_metrics!(
         // Counters
         MetricCounter { CONSENSUS_NUM_SENT_MESSAGES, "apollo_consensus_num_sent_messages", "The number of messages sent by the consensus p2p component", init = 0 },
         MetricCounter { CONSENSUS_NUM_RECEIVED_MESSAGES, "apollo_consensus_num_received_messages", "The number of messages received by the consensus p2p component", init = 0 },
+
+        // Labeled Counters (require changing topic to an enum)
+        LabeledMetricCounter { CONSENSUS_NUM_SENT_MESSAGES, "apollo_consensus_num_sent_messages", "The number of messages sent by the consensus p2p component", init = 0, labels = BROADCAST_TOPIC_LABELS },
+        LabeledMetricCounter { CONSENSUS_NUM_RECEIVED_MESSAGES, "apollo_consensus_num_received_messages", "The number of messages received by the consensus p2p component", init = 0, labels = BROADCAST_TOPIC_LABELS },
     },
 );
+
+pub const LABEL_NAME_BROADCAST_TOPIC: &str = "broadcast_topic";
+
+pub(crate) enum BroadcastTopic {
+    MempoolP2p,
+    Consensus,
+}
+
+generate_permutation_labels! {
+    BROADCAST_TOPIC_LABELS,
+    (LABEL_NAME_BROADCAST_TOPIC, BroadcastTopic),
+}
