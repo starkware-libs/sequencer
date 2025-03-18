cfe1c3848a26cdfae291fbfd739f5c7f7c4d741c
Noam Spiegelstein <noam.s@starkware.co>
2025-03-19 01:16:51 +0200

    feat(starknet_integration_tests): use sustained tps in restart test

--- crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_integration_tests/integration_test_restart_flow.rs
+++ crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_integration_tests/integration_test_restart_flow.rs
@@ -1,28 +1,32 @@
-use starknet_api::block::BlockNumber;
+use std::collections::HashMap;
+use std::time::Duration;
+
 use starknet_infra_utils::test_utils::TestIdentifier;
-use starknet_integration_tests::integration_test_manager::IntegrationTestManager;
+use starknet_integration_tests::integration_test_manager::{
+    IntegrationTestManager,
+    DEFAULT_SENDER_ACCOUNT,
+};
 use starknet_integration_tests::integration_test_utils::integration_test_setup;
+use starknet_integration_tests::utils::{ConsensusTxs, N_TXS_IN_FIRST_BLOCK, TPS};
+use tokio::join;
+use tokio::time::sleep;
 use tracing::info;
+
+const PHASE_DURATION_SECS: u64 = 30;
 
 #[tokio::main]
 async fn main() {
     integration_test_setup("restart").await;
-    const BLOCK_TO_SHUTDOWN_NODE_1_AT: BlockNumber = BlockNumber(5);
-    const BLOCK_TO_RESTART_NODE_1_FROM: BlockNumber = BlockNumber(10);
-    const BLOCK_TO_SHUTDOWN_NODE_2_AT: BlockNumber = BlockNumber(15);
-    const BLOCK_TO_WAIT_FOR_AFTER_NODE_2_SHUTDOWN: BlockNumber = BlockNumber(20);
-    // TODO(Eitan): keep a steady tps throughout the test instead of sending in batches
-    const N_TXS: usize = 10;
-    /// The number of consolidated local sequencers that participate in the test.
+
     const N_CONSOLIDATED_SEQUENCERS: usize = 5;
-    /// The number of distributed remote sequencers that participate in the test.
     const N_DISTRIBUTED_SEQUENCERS: usize = 0;
-    // The indices of the nodes that we will be shutting down. Node 0 is skipped because we use it
-    // to verify the metrics.
     const NODE_1: usize = 1;
     const NODE_2: usize = 2;
+    const TOTAL_PHASES: u64 = 4;
+    const TOTAL_DURATION_SECS: u64 = PHASE_DURATION_SECS * TOTAL_PHASES;
+    const TOTAL_INVOKE_TXS: u64 = TPS * TOTAL_DURATION_SECS;
 
-    // Get the sequencer configurations.
+    // Get the sequencer configurations
     let mut integration_test_manager = IntegrationTestManager::new(
         N_CONSOLIDATED_SEQUENCERS,
         N_DISTRIBUTED_SEQUENCERS,
@@ -38,41 +42,100 @@ async fn main() {
 
     integration_test_manager.send_bootstrap_txs_and_verify().await;
 
-    info!("Sending transactions while all nodes are up");
-    integration_test_manager.send_txs_and_verify(N_TXS, 0, BLOCK_TO_SHUTDOWN_NODE_1_AT).await;
+    let mut nodes_accepted_txs_mapping =
+        integration_test_manager.get_num_accepted_txs_on_all_running_nodes().await;
 
-    info!("Network reached block {BLOCK_TO_SHUTDOWN_NODE_1_AT}. Shutting down node {NODE_1}");
-    integration_test_manager.shutdown_nodes([NODE_1].into());
+    // Create a simulator for sustained transaction sending
+    let simulator = integration_test_manager.create_simulator();
+    let mut tx_generator = integration_test_manager.tx_generator().snapshot();
+    let test_scenario = ConsensusTxs {
+        n_invoke_txs: TOTAL_INVOKE_TXS.try_into().expect("Failed to convert TPS to usize"),
+        n_l1_handler_txs: 0,
+    };
 
-    info! {"Sending transactions while node {NODE_1} is down"}
-    integration_test_manager.send_txs_and_verify(N_TXS, 0, BLOCK_TO_RESTART_NODE_1_FROM).await;
+    // Spawn transaction sending task that runs for the entire test duration
+    let tx_sending_task =
+        simulator.send_txs(&mut tx_generator, &test_scenario, DEFAULT_SENDER_ACCOUNT);
 
-    info!("Restarting node {NODE_1}");
-    integration_test_manager.run_nodes([NODE_1].into()).await;
+    let await_and_restart_nodes_task = async {
+        info!("Awaiting transactions while all nodes are up");
+        sleep_and_verify_running_nodes_received_more_txs(
+            &mut nodes_accepted_txs_mapping,
+            &integration_test_manager,
+        )
+        .await;
 
-    // We want node 1 to rejoin the network while its building blocks to check the catch-up
-    // mechanism.
-    info!(
-        "Sending transactions after node {NODE_1} was restarted and before node {NODE_2} is shut \
-         down"
-    );
-    integration_test_manager.send_txs_and_verify(N_TXS, 0, BLOCK_TO_SHUTDOWN_NODE_2_AT).await;
+        integration_test_manager.shutdown_nodes([NODE_1].into());
+        info! {"Awaiting transactions while node {NODE_1} is down"}
+        sleep_and_verify_running_nodes_received_more_txs(
+            &mut nodes_accepted_txs_mapping,
+            &integration_test_manager,
+        )
+        .await;
 
-    // Shutdown second node to test that the first node has joined consensus (the network can't
-    // reach consensus without the first node if the second node is down).
-    info!("Network reached block {BLOCK_TO_SHUTDOWN_NODE_2_AT}. Shutting down node {NODE_2}");
-    integration_test_manager.shutdown_nodes([NODE_2].into());
+        // We want node 1 to rejoin the network while its building blocks to check the catch-up
+        // mechanism.
+        integration_test_manager.run_nodes([NODE_1].into()).await;
+        info!(
+            "Awaiting transactions after node {NODE_1} was restarted and before node {NODE_2} is \
+             shut down"
+        );
+        sleep_and_verify_running_nodes_received_more_txs(
+            &mut nodes_accepted_txs_mapping,
+            &integration_test_manager,
+        )
+        .await;
+
+        // Shutdown second node to test that the first node has joined consensus (the network can't
+        // reach consensus without the first node if the second node is down).
+        integration_test_manager.shutdown_nodes([NODE_2].into());
+        info!("Awaiting transactions while node {NODE_1} is up and node {NODE_2} is down");
+        sleep_and_verify_running_nodes_received_more_txs(
+            &mut nodes_accepted_txs_mapping,
+            &integration_test_manager,
+        )
+        .await;
+    };
+
+    let _ = join!(tx_sending_task, await_and_restart_nodes_task);
+
+    // Verify that each running node processed all transactions
+    // The expected total number of transactions is the total number of invoke transactions sent +
+    // the number of transactions sent during the bootstrap phase
+    info!("Verifying that all running nodes processed all transactions");
+    integration_test_manager
+        .await_txs_accepted_on_all_running_nodes(
+            N_TXS_IN_FIRST_BLOCK
+                + TryInto::<usize>::try_into(TOTAL_INVOKE_TXS)
+                    .expect("Failed to convert TOTAL_INVOKE_TXS to usize"),
+        )
+        .await;
+
     // Shutting down a node that's already down results in an error so we remove it from the set
     // here
     node_indices.remove(&NODE_2);
-
-    info!("Sending transactions while node {NODE_1} is up and node {NODE_2} is down");
-    integration_test_manager
-        .send_txs_and_verify(N_TXS, 0, BLOCK_TO_WAIT_FOR_AFTER_NODE_2_SHUTDOWN)
-        .await;
-
-    info!("Shutting down all nodes.");
     integration_test_manager.shutdown_nodes(node_indices);
-
     info!("Restart flow integration test completed successfully!");
+}
+
+// Sleeps for the duration of a phase and verifies that all running nodes processed more
+// transactions during the sleep. The function takes a mutable reference to a mapping of the number
+// of transactions processed by each node at the end of the previous phase. The function updates the
+// mapping with the number of transactions processed by each node at the end of the current phase.
+async fn sleep_and_verify_running_nodes_received_more_txs(
+    prev_txs: &mut HashMap<usize, usize>,
+    integration_test_manager: &IntegrationTestManager,
+) {
+    sleep(Duration::from_secs(PHASE_DURATION_SECS)).await;
+    let curr_txs = integration_test_manager.get_num_accepted_txs_on_all_running_nodes().await;
+    for (node_idx, curr_n_processed) in curr_txs {
+        let prev_n_processed = *prev_txs.get(&node_idx).unwrap();
+        info!("Node {} processed {} transactions", node_idx, curr_n_processed);
+        assert!(
+            curr_n_processed > prev_n_processed,
+            "Node {} did not process more transactions",
+            node_idx
+        );
+        prev_txs.insert(node_idx, curr_n_processed);
+    }
 }
--- crates/starknet_integration_tests/src/integration_test_manager.rs
+++ crates/starknet_integration_tests/src/integration_test_manager.rs
@@ -37,6 +37,7 @@ use crate::monitoring_utils::{
     await_block,
     await_sync_block,
     await_txs_accepted,
+    sequencer_num_accepted_txs,
     verify_txs_accepted,
 };
 use crate::node_component_configs::{
@@ -59,7 +60,7 @@ use crate::utils::{
     TestScenario,
 };
 
-const DEFAULT_SENDER_ACCOUNT: AccountId = 0;
+pub const DEFAULT_SENDER_ACCOUNT: AccountId = 0;
 pub const BLOCK_TO_WAIT_FOR_BOOTSTRAP: BlockNumber = BlockNumber(2);
 
 pub const HTTP_PORT_ARG: &str = "http-port";
@@ -626,6 +627,16 @@ impl IntegrationTestManager {
             .chain_info
             .chain_id
             .clone()
+    }
+
+    pub async fn get_num_accepted_txs_on_all_running_nodes(&self) -> HashMap<usize, usize> {
+        let mut result = HashMap::new();
+        for (idx, running_node) in self.running_nodes.iter() {
+            let monitoring_client = running_node.node_setup.state_sync_monitoring_client();
+            let n_processed = sequencer_num_accepted_txs(monitoring_client).await;
+            result.insert(*idx, n_processed);
+        }
+        result
     }
 }
 
