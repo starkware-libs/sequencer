15550569a57000c5495dce0b3b081e16a05f027c
amos rothberg <amos@starkware.co>
2025-03-18 14:57:51 +0200

    feat(starknet_os): add EndpointArg and support parameters passed by value

--- crates/starknet_os/src/test_utils/errors.rs
+++ crates/starknet_os/src/test_utils/errors.rs
@@ -1,7 +1,8 @@
 use cairo_vm::serde::deserialize_program::Member;
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
-use cairo_vm::vm::runners::cairo_runner::CairoArg;
+
+use crate::test_utils::cairo_runner::EndpointArg;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Cairo0EntryPointRunnerError {
@@ -18,22 +19,11 @@ pub enum ExplicitArgError {
     #[error(
         "Mismatch for explicit arg {index}. expected arg: {expected:?}, actual arg: {actual:?}"
     )]
-    Mismatch { index: usize, expected: Member, actual: CairoArg },
+    Mismatch { index: usize, expected: Member, actual: EndpointArg },
     #[error(
         "Expected {} explicit arguments, got {}. Expected args: {expected:?}, actual args: \
         {actual:?}",
         .expected.len(), .actual.len()
     )]
-    WrongNumberOfArgs { expected: Vec<Member>, actual: Vec<CairoArg> },
-    #[error(
-        "Cairo runner does not support passing the endpoint structs / tuples / named tuples by \
-         value. The endpoint can receive these types only via a pointer. unsupported param: \
-         {expected_arg:?}, index: {index}"
-    )]
-    UnsupportedArgType { index: usize, expected_arg: Member },
-    #[error(
-        "To pass a pointer to the endpoint, use an array or a compound arg. index: {index}, bad \
-         arg: {actual_arg:?}"
-    )]
-    SingleRelocatableParam { index: usize, actual_arg: CairoArg },
+    WrongNumberOfArgs { expected: Vec<Member>, actual: Vec<EndpointArg> },
 }
