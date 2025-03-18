7a3401622f2280da8c25c11423d44ea4f8747528
Elin <elin@starkware.co>
2025-03-18 14:13:06 +0200

    test(starknet_sierra_multicompile): add compiler version alignment test (#4937)

--- crates/starknet_sierra_multicompile/build.rs
+++ crates/starknet_sierra_multicompile/build.rs
@@ -15,9 +15,6 @@ fn main() {
     install_starknet_native_compile();
 }
 
-// TODO(Elin): test version alignment with Cargo.
-const REQUIRED_CAIRO_LANG_VERSION: &str = "2.11.2";
-
 /// Downloads the Cairo crate from StarkWare's release page and extracts its contents into the
 /// `target` directory. This crate includes the `starknet-sierra-compile` binary, which is used to
 /// compile Sierra to Casm. The binary is executed as a subprocess whenever Sierra compilation is
