d7b061b8d2b507f8f77a4d2b98a039e63082488e
lev-starkware <155880815+lev-starkware@users.noreply.github.com>
2025-03-18 09:23:59 +0200

    chore(papyrus_proc_macros): change latency_histogram macro (#4981)

--- crates/papyrus_proc_macros/src/lib.rs
+++ crates/papyrus_proc_macros/src/lib.rs
@@ -144,7 +144,7 @@ pub fn latency_histogram(attr: TokenStream, input: TokenStream) -> TokenStream {
     // instead of boolean
 
     let metric_recording_logic = quote! {
-        metrics::histogram!(#metric_name).record(exec_time);
+        ::metrics::histogram!(#metric_name).record(exec_time);
     };
 
     let collect_metric_flag = quote! {
