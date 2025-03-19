277187c2017c309b5bd70c10a04128565b2a0d88
Aner Ben Efraim <aner@starkware.co>
2025-03-19 17:00:40 +0200

    feat(starknet_os): implement hint remaining_gas_gt_max
    fix(starknet_os): remove commented python code

--- crates/starknet_os/src/hints/hint_implementation/resources.rs
+++ crates/starknet_os/src/hints/hint_implementation/resources.rs
@@ -1,12 +1,22 @@
 use blockifier::state::state_api::StateReader;
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
+    get_integer_from_var_name,
+    insert_value_into_ap,
+};
+use starknet_types_core::felt::Felt;
 
 use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
+use crate::hints::vars::Ids;
 
 pub(crate) fn remaining_gas_gt_max<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, S>,
+    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_, S>,
 ) -> OsHintResult {
-    todo!()
+    let remaining_gas =
+        get_integer_from_var_name(Ids::RemainingGas.into(), vm, ids_data, ap_tracking)?;
+    let max_gas = get_integer_from_var_name(Ids::MaxGas.into(), vm, ids_data, ap_tracking)?;
+    let remaining_gas_gt_max: Felt = (remaining_gas > max_gas).into();
+    Ok(insert_value_into_ap(vm, remaining_gas_gt_max)?)
 }
 
 pub(crate) fn debug_expected_initial_gas<S: StateReader>(
