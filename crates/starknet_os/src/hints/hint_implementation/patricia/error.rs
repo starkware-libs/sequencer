4af714a846a88b92ea4f77adb746b8f323590763
rotem-starkware <rotem@starkware.co>
2025-03-19 09:39:36 +0200

    feat(starknet_os): implement build_update_tree function and test

--- crates/starknet_os/src/hints/hint_implementation/patricia/error.rs
+++ crates/starknet_os/src/hints/hint_implementation/patricia/error.rs
@@ -1,7 +1,19 @@
-use super::utils::Preimage;
+use ethnum::U256;
+use starknet_patricia::patricia_merkle_tree::types::SubTreeHeight;
+
+use super::utils::{LayerIndex, Preimage};
 
 #[derive(Debug, thiserror::Error)]
 pub enum PatriciaError {
+    #[error(
+        "Children of node at height {height} with LayerIndex {index} are None. Node should be \
+         None."
+    )]
+    BothChildrenAreNone { index: LayerIndex, height: SubTreeHeight },
     #[error("Expected a binary node, found: {0:?}")]
     ExpectedBinary(Preimage),
+    #[error("Expected that one of the children is not None, found both None.")]
+    InvalidInnerNode,
+    #[error("Exceeded the max index: {0:?}")]
+    MaxLayerIndexExceeded(U256),
 }
