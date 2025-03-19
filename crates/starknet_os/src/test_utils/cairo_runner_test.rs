80bc96d64fcf47d1566e1a88f364358048e0b511
amosStarkware <88497213+amosStarkware@users.noreply.github.com>
2025-03-19 17:15:28 +0200

    feat(starknet_os): support implicit args in cairo0 runner (#4747)

--- crates/starknet_os/src/test_utils/cairo_runner_test.rs
+++ crates/starknet_os/src/test_utils/cairo_runner_test.rs
@@ -1,38 +1,50 @@
 use blockifier::retdata;
+use cairo_vm::types::builtin_name::BuiltinName;
 
-use crate::test_utils::cairo_runner::{EndpointArg, PointerArg, ValueArg};
-use crate::test_utils::errors::Cairo0EntryPointRunnerError;
+use crate::test_utils::cairo_runner::{
+    Cairo0EntryPointRunnerResult,
+    EndpointArg,
+    ImplicitArg,
+    PointerArg,
+    ValueArg,
+};
 use crate::test_utils::utils::run_cairo_function_and_check_result;
 
 /// struct CompoundStruct {
-///     a: felt,
-///     simple_struct: SimpleStruct*,
+/// a: felt,
+/// simple_struct: SimpleStruct*,
 /// }
 ///
 /// struct SimpleStruct {
-///     a: felt,
-///     b: felt,
+/// a: felt,
+/// b: felt,
 /// }
 ///
 /// func pass_felt_and_pointers(number: felt, array: felt*, tuple: felt*, simple_struct:
-///     SimpleStruct*, compound_struct: CompoundStruct*) -> (res1: felt, res2: felt, res3: felt) {
-///     let res1 = number + array[0];
-///     let res2 = tuple[0] + tuple[1];
-///     let res3 = simple_struct.a + compound_struct.simple_struct.b;
-///     return (res1=res1, res2=res2, res3=res3);
+/// SimpleStruct*, compound_struct: CompoundStruct*) -> (res1: felt, res2: felt, res3: felt) {
+/// let res1 = number + array[0];
+/// let res2 = tuple[0] + tuple[1];
+/// let res3 = simple_struct.a + compound_struct.simple_struct.b;
+/// return (res1=res1, res2=res2, res3=res3);
 /// }
 ///
 /// func pass_structs_and_tuples(tuple: (felt, felt), named_tuple: (a: felt, b:felt), simple_struct:
-///     SimpleStruct, compound_struct: CompoundStruct) -> (res1: felt, res2: felt, res3: felt) {
-///     let res1 = tuple[0] + tuple[1];
-///     let res2 = named_tuple.a + named_tuple.b;
-///     let res3 = simple_struct.a + compound_struct.simple_struct.b;
-///     return (res1=res1, res2=res2, res3=res3);
+/// SimpleStruct, compound_struct: CompoundStruct) -> (res1: felt, res2: felt, res3: felt) {
+/// let res1 = tuple[0] + tuple[1];
+/// let res2 = named_tuple.a + named_tuple.b;
+/// let res3 = simple_struct.a + compound_struct.simple_struct.b;
+/// return (res1=res1, res2=res2, res3=res3);
+/// }
+///
+/// func pass_implicit_args{range_check_ptr, compound_struct: CompoundStruct*}
+/// (number_1: felt, number_2: felt) -> (res: felt) {
+/// let sum = number_1 + number_2;
+/// return (res=sum);
 /// }
 const COMPILED_DUMMY_FUNCTION: &str = include_str!("compiled_dummy_function.json");
 
 #[test]
-fn test_felt_and_pointers() -> Result<(), Cairo0EntryPointRunnerError> {
+fn test_felt_and_pointers() -> Cairo0EntryPointRunnerResult<()> {
     let number = 2;
     let (first_array_val, second_array_val) = (3, 4);
     let (first_tuple_val, second_tuple_val) = (5, 6);
@@ -58,6 +70,7 @@ fn test_felt_and_pointers() -> Result<(), Cairo0EntryPointRunnerError> {
         COMPILED_DUMMY_FUNCTION,
         "pass_felt_and_pointers",
         &[number.into(), array, tuple, simple_struct, compound_struct],
+        &[],
         &retdata![
             (number + first_array_val).into(),
             (first_tuple_val + second_tuple_val).into(),
@@ -67,7 +80,7 @@ fn test_felt_and_pointers() -> Result<(), Cairo0EntryPointRunnerError> {
 }
 
 #[test]
-fn test_tuples_and_structs() -> Result<(), Cairo0EntryPointRunnerError> {
+fn test_tuples_and_structs() -> Cairo0EntryPointRunnerResult<()> {
     let (first_tuple_val, second_tuple_val) = (3, 4);
     let (first_named_tuple_val, second_named_tuple_val) = (5, 6);
     let (first_simple_struct_val, second_simple_struct_val) = (7, 8);
@@ -94,10 +107,34 @@ fn test_tuples_and_structs() -> Result<(), Cairo0EntryPointRunnerError> {
         COMPILED_DUMMY_FUNCTION,
         "pass_structs_and_tuples",
         &[tuple, named_tuple, simple_struct, compound_struct],
+        &[],
         &retdata![
             (first_tuple_val + second_tuple_val).into(),
             (first_named_tuple_val + second_named_tuple_val).into(),
             (first_simple_struct_val + second_simple_struct_val).into()
         ],
+    )
+}
+
+// TODO(Amos): Actually use the range check builtin, once the SNOS hint processor supports cairo0
+// hints.
+#[test]
+fn test_implicit_args() -> Cairo0EntryPointRunnerResult<()> {
+    let number_1 = 1;
+    let number_2 = 2;
+    let (first_simple_struct_val, second_simple_struct_val) = (7, 8);
+    let compound_struct_val = 9;
+    let simple_struct = EndpointArg::Pointer(PointerArg::Array(vec![
+        first_simple_struct_val.into(),
+        second_simple_struct_val.into(),
+    ]));
+    let compound_struct =
+        EndpointArg::Pointer(PointerArg::Composed(vec![compound_struct_val.into(), simple_struct]));
+    run_cairo_function_and_check_result(
+        COMPILED_DUMMY_FUNCTION,
+        "pass_implicit_args",
+        &[number_1.into(), number_2.into()],
+        &[ImplicitArg::Builtin(BuiltinName::range_check), ImplicitArg::NonBuiltin(compound_struct)],
+        &retdata![(number_1 + number_2).into()],
     )
 }
--- crates/starknet_os/src/test_utils/errors.rs
+++ crates/starknet_os/src/test_utils/errors.rs
@@ -1,8 +1,12 @@
+use std::collections::HashSet;
+
 use cairo_vm::serde::deserialize_program::Member;
+use cairo_vm::types::builtin_name::BuiltinName;
+use cairo_vm::types::errors::program_errors::ProgramError;
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
 
-use crate::test_utils::cairo_runner::EndpointArg;
+use crate::test_utils::cairo_runner::{EndpointArg, ImplicitArg};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Cairo0EntryPointRunnerError {
@@ -12,6 +16,21 @@ pub enum Cairo0EntryPointRunnerError {
     ExplicitArg(#[from] ExplicitArgError),
     #[error(transparent)]
     VirtualMachine(#[from] VirtualMachineError),
+    #[error(transparent)]
+    ImplicitArg(#[from] ImplicitArgError),
+    #[error(transparent)]
+    Program(#[from] ProgramError),
+    #[error(transparent)]
+    ProgramSerde(serde_json::Error),
+    #[error(
+        "The cairo runner's builtin list does not match the actual builtins, it should be \
+         updated. Cairo Runner's builtins: {cairo_runner_builtins:?}, actual builtins: \
+         {actual_builtins:?}"
+    )]
+    BuiltinMismatch {
+        cairo_runner_builtins: Vec<BuiltinName>,
+        actual_builtins: HashSet<BuiltinName>,
+    },
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -26,4 +45,20 @@ pub enum ExplicitArgError {
         .expected.len(), .actual.len()
     )]
     WrongNumberOfArgs { expected: Vec<Member>, actual: Vec<EndpointArg> },
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ImplicitArgError {
+    #[error(
+        "Mismatch for implicit arg {index}. expected arg: {expected:?}, actual arg: {actual:?}"
+    )]
+    Mismatch { index: usize, expected: Member, actual: ImplicitArg },
+    #[error(
+        "Expected {} implicit arguments, got {}. Expected args: {expected:?}, actual args: \
+        {actual:?}",
+        .expected.len(), .actual.len()
+    )]
+    WrongNumberOfArgs { expected: Vec<(String, Member)>, actual: Vec<ImplicitArg> },
+    #[error("Incorrect order of builtins. Expected: {correct_order:?}, actual: {actual_order:?}")]
+    WrongBuiltinOrder { correct_order: Vec<BuiltinName>, actual_order: Vec<BuiltinName> },
 }
