9ed7607be862fb55908e8d1866b51f016f354ab6
Aner Ben Efraim <aner@starkware.co>
2025-03-19 11:42:54 +0200

    fix(starknet_os): fix initial_dict hashmap types to mayberelocatable

--- crates/blockifier/src/state/cached_state.rs
+++ crates/blockifier/src/state/cached_state.rs
@@ -108,7 +108,7 @@ impl<S: StateReader> CachedState<S> {
     // TODO(Aner): move under OS cfg flag.
     // TODO(Aner): Try to avoid cloning.
     pub fn writes_compiled_class_hashes(&self) -> HashMap<ClassHash, CompiledClassHash> {
-        self.cache.borrow().writes.get_compiled_class_hashes().clone()
+        self.cache.borrow().writes.compiled_class_hashes.clone()
     }
 }
 
@@ -351,10 +351,6 @@ impl StateMaps {
                 &other.declared_contracts,
             ),
         }
-    }
-
-    pub fn get_compiled_class_hashes(&self) -> &HashMap<ClassHash, CompiledClassHash> {
-        &self.compiled_class_hashes
     }
 
     pub fn get_contract_addresses(&self) -> HashSet<ContractAddress> {
