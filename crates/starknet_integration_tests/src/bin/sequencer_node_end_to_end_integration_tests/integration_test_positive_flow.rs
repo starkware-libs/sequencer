0b06aedd7eaf52d474de7f267e0e27ed1e066026
Itay Tsabary <itayt@starkware.co>
2025-03-20 14:23:05 +0200

    chore(ci): set port range in integration test ci

--- crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_integration_tests/integration_test_positive_flow.rs
+++ crates/starknet_integration_tests/src/bin/sequencer_node_end_to_end_integration_tests/integration_test_positive_flow.rs
@@ -4,6 +4,8 @@ use starknet_integration_tests::integration_test_manager::IntegrationTestManager
 use starknet_integration_tests::integration_test_utils::integration_test_setup;
 use tracing::info;
 
+// TODO(Tsabary): placeholder, make the integration test run.
+
 #[tokio::main]
 async fn main() {
     integration_test_setup("positive").await;
