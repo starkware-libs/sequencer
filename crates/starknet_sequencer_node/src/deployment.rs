a89475a5ad23bdeca9914451ca95cdebcf44f096
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-03-20 17:21:52 +0200

    chore(starknet_sequencer_node): add set url to localhost test util (#5125)

--- crates/starknet_sequencer_node/src/deployment.rs
+++ crates/starknet_sequencer_node/src/deployment.rs
@@ -162,6 +162,13 @@ impl DeploymentName {
     }
 }
 
+#[cfg(any(feature = "testing", test))]
+pub fn set_urls_to_localhost(component_configs: &mut [ComponentConfig]) {
+    for component_config in component_configs.iter_mut() {
+        component_config.set_urls_to_localhost();
+    }
+}
+
 // TODO(Tsabary): each deployment should be in its own module.
 
 pub trait GetComponentConfigs {
