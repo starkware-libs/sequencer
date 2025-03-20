e04fa457d684f3d7abbb83e818dcae872a156ea7
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2025-03-20 21:52:29 +0200

    chore(starknet_sequencer_node): dump formatted config at startup (#5143)

--- crates/starknet_sequencer_node/src/utils.rs
+++ crates/starknet_sequencer_node/src/utils.rs
@@ -39,7 +39,7 @@ pub fn load_and_validate_config(args: Vec<String>) -> Result<SequencerNodeConfig
 
     info!("Config map:");
     info!(
-        "{:?}",
+        "{:#?}",
         get_config_presentation::<SequencerNodeConfig>(&config, false)
             .expect("Should be able to get representation.")
     );
