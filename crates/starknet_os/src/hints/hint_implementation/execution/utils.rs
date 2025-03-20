9fdc701dad3dec7e6ff48d4a11920b49e8df50d3
Yoav Gross <yoav@starkware.co>
2025-03-20 11:49:32 +0200

    feat(starknet_os): load cairo resource bounds object

--- crates/starknet_os/src/hints/hint_implementation/execution/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/execution/utils.rs
@@ -1,1 +1,60 @@
+use std::collections::HashMap;
 
+use blockifier::execution::syscalls::hint_processor::{
+    valid_resource_bounds_as_felts,
+    ResourceAsFelts,
+};
+use cairo_vm::types::relocatable::Relocatable;
+use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_api::transaction::fields::ValidResourceBounds;
+use starknet_types_core::felt::Felt;
+
+use crate::hints::error::{OsHintError, OsHintResult};
+use crate::hints::vars::CairoStruct;
+use crate::vm_utils::{insert_values_to_fields, CairoSized, IdentifierGetter, LoadCairoObject};
+
+impl<IG: IdentifierGetter> LoadCairoObject<IG> for ResourceAsFelts {
+    fn load_into(
+        &self,
+        vm: &mut VirtualMachine,
+        identifier_getter: &IG,
+        address: Relocatable,
+        _constants: &HashMap<String, Felt>,
+    ) -> OsHintResult {
+        let resource_bounds_list = vec![
+            ("resource_name", self.resource_name.into()),
+            ("max_amount", self.max_amount.into()),
+            ("max_price_per_unit", self.max_price_per_unit.into()),
+        ];
+        insert_values_to_fields(
+            address,
+            CairoStruct::ResourceBounds,
+            vm,
+            &resource_bounds_list,
+            identifier_getter,
+        )
+    }
+}
+
+impl<IG: IdentifierGetter> CairoSized<IG> for ResourceAsFelts {
+    fn size(_identifier_getter: &IG) -> usize {
+        3
+    }
+}
+
+impl<IG: IdentifierGetter> LoadCairoObject<IG> for ValidResourceBounds {
+    fn load_into(
+        &self,
+        vm: &mut VirtualMachine,
+        identifier_getter: &IG,
+        address: Relocatable,
+        constants: &HashMap<String, Felt>,
+    ) -> OsHintResult {
+        valid_resource_bounds_as_felts(self).map_err(OsHintError::ResourceBoundsParsing)?.load_into(
+            vm,
+            identifier_getter,
+            address,
+            constants,
+        )
+    }
+}
