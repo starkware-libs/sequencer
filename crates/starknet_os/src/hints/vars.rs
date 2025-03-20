b5d0e20c8cad467d91059e254065639d2432c63a
Yoav Gross <yoav@starkware.co>
2025-03-20 11:49:32 +0200

    feat(starknet_os): load cairo resource bounds object

--- crates/starknet_os/src/hints/vars.rs
+++ crates/starknet_os/src/hints/vars.rs
@@ -283,6 +283,7 @@ pub enum CairoStruct {
     ExecutionContext,
     NodeEdge,
     OsStateUpdate,
+    ResourceBounds,
     StateEntry,
     StorageReadPtr,
     StorageReadRequestPtr,
@@ -317,6 +318,7 @@ impl From<CairoStruct> for &'static str {
             }
             CairoStruct::NodeEdge => "starkware.cairo.common.patricia_utils.NodeEdge",
             CairoStruct::OsStateUpdate => "starkware.starknet.core.os.state.state.OsStateUpdate",
+            CairoStruct::ResourceBounds => "starkware.starknet.common.new_syscalls.ResourceBounds",
             CairoStruct::StateEntry => "starkware.starknet.core.os.state.state.StateEntry",
             CairoStruct::StorageReadPtr => "starkware.starknet.common.syscalls.StorageRead*",
             CairoStruct::StorageReadRequestPtr => {
