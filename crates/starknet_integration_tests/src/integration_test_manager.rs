26ba896297454a6af2b199b307ce3be0ad10fc9c
nadin-Starkware <nadin@starkware.co>
2025-03-22 12:32:50 +0200

    refactor(starknet_integration_tests): move storage-related structs to a separate module

--- crates/starknet_integration_tests/src/integration_test_manager.rs
+++ crates/starknet_integration_tests/src/integration_test_manager.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::future::Future;
 use std::net::Ipv4Addr;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::time::Duration;
 
 use alloy::node_bindings::AnvilInstance;
@@ -45,11 +45,7 @@ use crate::node_component_configs::{
     NodeComponentConfigs,
 };
 use crate::sequencer_simulator_utils::SequencerSimulator;
-use crate::state_reader::{
-    BATCHER_DB_PATH_SUFFIX,
-    CLASS_MANAGER_DB_PATH_SUFFIX,
-    STATE_SYNC_DB_PATH_SUFFIX,
-};
+use crate::storage::{CustomPaths, StorageExecutablePaths};
 use crate::utils::{
     create_consensus_manager_configs_from_network_configs,
     create_integration_test_tx_generator,
@@ -200,86 +196,6 @@ pub struct IntegrationTestManager {
     #[allow(dead_code)]
     l1_handle: AnvilInstance,
     starknet_l1_contract: StarknetL1Contract,
-}
-
-// TODO(Nadin): remove Clone derive.
-#[derive(Debug, Clone)]
-pub struct StorageExecutablePaths {
-    batcher_path: PathBuf,
-    state_sync_path: PathBuf,
-    class_manager_path: PathBuf,
-}
-
-impl StorageExecutablePaths {
-    pub fn new(
-        db_base: &Path,
-        node_index: usize,
-        batcher_index: usize,
-        state_sync_index: usize,
-        class_manager_index: usize,
-    ) -> Self {
-        let batcher_node_index = NodeExecutionId::new(node_index, batcher_index);
-        let state_sync_node_index = NodeExecutionId::new(node_index, state_sync_index);
-        let class_manager_node_index = NodeExecutionId::new(node_index, class_manager_index);
-
-        let batcher_path = batcher_node_index.build_path(db_base);
-        let state_sync_path = state_sync_node_index.build_path(db_base);
-        let class_manager_path = class_manager_node_index.build_path(db_base);
-
-        Self { batcher_path, state_sync_path, class_manager_path }
-    }
-
-    pub fn get_batcher_exec_path(&self) -> &PathBuf {
-        &self.batcher_path
-    }
-
-    pub fn get_state_sync_exec_path(&self) -> &PathBuf {
-        &self.state_sync_path
-    }
-
-    pub fn get_class_manager_exec_path(&self) -> &PathBuf {
-        &self.class_manager_path
-    }
-
-    pub fn get_batcher_path_with_db_suffix(&self) -> PathBuf {
-        self.batcher_path.join(BATCHER_DB_PATH_SUFFIX)
-    }
-
-    pub fn get_state_sync_path_with_db_suffix(&self) -> PathBuf {
-        self.state_sync_path.join(STATE_SYNC_DB_PATH_SUFFIX)
-    }
-
-    pub fn get_class_manager_path_with_db_suffix(&self) -> PathBuf {
-        self.class_manager_path.join(CLASS_MANAGER_DB_PATH_SUFFIX)
-    }
-}
-
-pub struct CustomPaths {
-    db_base: Option<PathBuf>,
-    config_base: Option<PathBuf>,
-    data_prefix_base: Option<PathBuf>,
-}
-
-impl CustomPaths {
-    pub fn new(
-        db_base: Option<PathBuf>,
-        config_base: Option<PathBuf>,
-        data_prefix_base: Option<PathBuf>,
-    ) -> Self {
-        Self { db_base, config_base, data_prefix_base }
-    }
-
-    pub fn get_db_base(&self) -> Option<&PathBuf> {
-        self.db_base.as_ref()
-    }
-
-    pub fn get_config_path(&self, node_execution_id: &NodeExecutionId) -> Option<PathBuf> {
-        self.config_base.as_ref().map(|p| node_execution_id.build_path(p))
-    }
-
-    pub fn get_data_prefix_path(&self, node_execution_id: &NodeExecutionId) -> Option<PathBuf> {
-        self.data_prefix_base.as_ref().map(|p| node_execution_id.build_path(p))
-    }
 }
 
 impl IntegrationTestManager {
