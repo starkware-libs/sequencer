447ddcd8c10ebc68016d49e44ad5085e311b022b
nadin-Starkware <nadin@starkware.co>
2025-03-22 15:26:56 +0200

    refactor(starknet_integration_tests): create node base config per node instead of per executable

--- crates/starknet_infra_utils/src/test_utils.rs
+++ crates/starknet_infra_utils/src/test_utils.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 use tracing::{info, instrument};
 
 const PORTS_PER_INSTANCE: u16 = 60;
-pub const MAX_NUMBER_OF_INSTANCES_PER_TEST: u16 = 27;
+pub const MAX_NUMBER_OF_INSTANCES_PER_TEST: u16 = 28;
 const MAX_NUMBER_OF_TESTS: u16 = 10;
 const BASE_PORT: u16 = 43000;
 
