59abe9aa83af818b88a527f56c26694097868008
Ayelet Zilber <ayelet@starkware.co>
2025-03-23 15:10:01 +0200

    refactor(starknet_client): next_l2_gas_price type from u64 to GasPrice

--- crates/papyrus_test_utils/src/lib.rs
+++ crates/papyrus_test_utils/src/lib.rs
@@ -446,7 +446,7 @@ auto_impl_get_test_instance! {
         pub l1_data_gas_price: GasPricePerToken,
         pub l2_gas_price: GasPricePerToken,
         pub l2_gas_consumed: u64,
-        pub next_l2_gas_price: u64,
+        pub next_l2_gas_price: GasPrice,
         pub state_root: GlobalRoot,
         pub sequencer: SequencerContractAddress,
         pub timestamp: BlockTimestamp,
