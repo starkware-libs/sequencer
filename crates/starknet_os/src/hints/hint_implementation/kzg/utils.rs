1f406d4115d401704079ab1752eedeeb757aac85
Dori Medini <dori@starkware.co>
2025-03-23 10:59:30 +0200

    feat(starknet_os): integrate the split function

--- crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
@@ -129,3 +129,24 @@ pub(crate) fn polynomial_coefficients_to_kzg_commitment(
     let commitment_bytes = blob_to_kzg_commitment(&Blob::from_bytes(&blob)?)?;
     split_commitment(&commitment_bytes)
 }
+
+/// Takes an integer and returns its canonical representation as:
+///    d0 + d1 * BASE + d2 * BASE**2.
+/// d2 can be in the range (-2**127, 2**127).
+pub fn split(num: Felt252) -> Result<Vec<Felt252>, HintError> {
+    let mut a = Vec::with_capacity(3);
+    let mut num = num.to_bigint();
+    for _ in 0..2 {
+        let (q, residue) = num.div_mod_floor(&BASE);
+        num = q;
+        a.push(residue);
+    }
+    if num.abs() >= BigInt::from(2).pow(127) {
+        return Err(HintError::AssertionFailed(
+            "remainder should be less than 2**127".to_string().into_boxed_str(),
+        ));
+    }
+    a.push(num);
+
+    Ok(a.into_iter().map(|big| big.into()).collect())
+}
