a608ee1022585a5a3eaa14252b54db2a9f7e857f
Dafna Matsry <dafna.matsry@starkware.co>
2025-03-24 11:29:06 +0200

    feat(starknet_gateway): reject txs with nonce beyoned allowed gap

--- crates/starknet_gateway/src/gateway_test.rs
+++ crates/starknet_gateway/src/gateway_test.rs
@@ -3,7 +3,12 @@ use std::sync::Arc;
 use assert_matches::assert_matches;
 use blockifier::context::ChainInfo;
 use blockifier_test_utils::cairo_versions::{CairoVersion, RunnableCairo1};
-use mempool_test_utils::starknet_api_test_utils::{declare_tx, invoke_tx};
+use blockifier_test_utils::contracts::FeatureContract;
+use mempool_test_utils::starknet_api_test_utils::{
+    declare_tx,
+    invoke_tx,
+    test_valid_resource_bounds,
+};
 use metrics_exporter_prometheus::PrometheusBuilder;
 use mockall::predicate::eq;
 use papyrus_network_types::network_types::BroadcastedMessageMetadata;
@@ -17,6 +22,7 @@ use starknet_api::rpc_transaction::{
     RpcTransaction,
     RpcTransactionLabelValue,
 };
+use starknet_api::test_utils::invoke::rpc_invoke_tx;
 use starknet_api::test_utils::CHAIN_ID_FOR_TESTS;
 use starknet_api::transaction::{
     InvokeTransaction,
@@ -24,6 +30,7 @@ use starknet_api::transaction::{
     TransactionHasher,
     TransactionVersion,
 };
+use starknet_api::{invoke_tx_args, nonce};
 use starknet_class_manager_types::transaction_converter::TransactionConverter;
 use starknet_class_manager_types::{EmptyClassManagerClient, SharedClassManagerClient};
 use starknet_gateway_types::errors::GatewaySpecError;
