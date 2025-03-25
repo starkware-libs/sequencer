060623ee2e4fb2f3ce221a4c272e06bbb8a8ca0a
Lev Roitman <lev@starkware.co>
2025-03-25 16:17:11 +0200

    chore(apollo_rpc_execution): add using of class manager to execution state reader

--- crates/apollo_integration_tests/src/utils.rs
+++ crates/apollo_integration_tests/src/utils.rs
@@ -176,6 +176,7 @@ pub fn create_node_config(
     let class_manager_config =
         create_class_manager_config(storage_config.class_manager_storage_config);
     state_sync_config.storage_config = storage_config.state_sync_storage_config;
+    let starknet_url = state_sync_config.rpc_config.starknet_url.clone();
 
     // Update config pointer values.
     let mut config_pointers_map = ConfigPointersMap::new(CONFIG_POINTERS.clone());
@@ -200,6 +201,10 @@ pub fn create_node_config(
     config_pointers_map.change_target_value(
         "recorder_url",
         to_value(recorder_url).expect("Failed to serialize Url"),
+    );
+    config_pointers_map.change_target_value(
+        "starknet_url",
+        to_value(starknet_url).expect("Failed to serialize starknet_url"),
     );
     (
         SequencerNodeConfig {
