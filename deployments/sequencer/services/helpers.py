f63fd07c38014cad3f18fd69a07f7f65e5ee6bba
Idan Shamam <idan@starkware.co>
2025-03-26 15:25:27 +0200

    feat(deployment): add name sanitize helper function according to k8s naming convention

--- deployments/sequencer/services/helpers.py
+++ deployments/sequencer/services/helpers.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 
 def argument_parser():
@@ -10,3 +11,27 @@ def argument_parser():
     )
 
     return parser.parse_args()
+
+
+def sanitize_name(name: str) -> str:
+    """
+    Sanitize a Kubernetes resource name to comply with k8s naming conventions:
+    - Only lowercase letters (a-z), numbers (0-9), and hyphens (-) are allowed.
+    - The name must start and end with a letter or number.
+    - The name must not exceed 253 characters.
+    - Underscores (_) are replaced with hyphens (-), and invalid characters are removed.
+
+    Args:
+        name (str): The original name to be sanitized.
+
+    Returns:
+        str: The sanitized name.
+    """
+
+    name = name.lower()
+    name = name.replace("_", "-")
+    name = re.sub(r"[^a-z0-9-]", "", name)
+    name = name.strip("-")
+    name = name[:253]
+
+    return name
