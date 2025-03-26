ded517dc403f3f2fe503c82f7a18aa29bd73d3e4
dorimedini-starkware <dori@starkware.co>
2025-03-26 14:46:40 +0200

    feat(starknet_os): integrate the split function (#5161)
    * feat(starknet_os): integrate the split function
    
    * refactor(starknet_os): split to bigint3
    
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    ---------
    
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/kzg/utils.rs
@@ -6,11 +6,14 @@ use ark_bls12_381::Fr;
 use ark_ff::{BigInteger, PrimeField};
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
 use c_kzg::{Blob, KzgCommitment, KzgSettings, BYTES_PER_FIELD_ELEMENT};
-use num_bigint::ParseBigIntError;
-use num_traits::Zero;
+use num_bigint::{BigInt, ParseBigIntError};
+use num_traits::{Signed, Zero};
 use starknet_infra_utils::compile_time_cargo_manifest_dir;
 use starknet_types_core::felt::Felt;
 
+use crate::hints::error::OsHintError;
+
+static BASE: LazyLock<BigInt> = LazyLock::new(|| BigInt::from(1u128 << 86));
 const COMMITMENT_BYTES_LENGTH: usize = 48;
 const COMMITMENT_BYTES_MIDPOINT: usize = COMMITMENT_BYTES_LENGTH / 2;
 const LOG2_FIELD_ELEMENTS_PER_BLOB: usize = 12;
@@ -132,4 +135,21 @@ pub(crate) fn polynomial_coefficients_to_kzg_commitment(
     let blob = polynomial_coefficients_to_blob(coefficients)?;
     let commitment_bytes = blob_to_kzg_commitment(&Blob::from_bytes(&blob)?)?;
     split_commitment(&commitment_bytes)
+}
+
+/// Takes an integer and returns its canonical representation as:
+///    d0 + d1 * BASE + d2 * BASE**2.
+/// d2 can be in the range (-2**127, 2**127).
+// TODO(Dori): Consider using bls_split from the VM crate if and when public.
+#[allow(dead_code)]
+pub(crate) fn split_bigint3(num: BigInt) -> Result<[Felt; 3], OsHintError> {
+    let (q1, d0) = (&num / &*BASE, Felt::from(num % &*BASE));
+    let (d2, d1) = (&q1 / &*BASE, Felt::from(q1 % &*BASE));
+    if d2.abs() >= BigInt::from(1_u128 << 127) {
+        return Err(OsHintError::AssertionFailed {
+            message: format!("Remainder should be in (-2**127, 2**127), got {d2}."),
+        });
+    }
+
+    Ok([d0, d1, Felt::from(d2)])
 }
