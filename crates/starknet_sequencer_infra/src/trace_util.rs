727e2aa5a47d9dd7fcd284f099df6ee7d9717fc0
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-03-27 16:35:47 +0200

    chore(starknet_sequencer_node): avoid debug of spammy crates (#5363)

--- crates/starknet_sequencer_infra/src/trace_util.rs
+++ crates/starknet_sequencer_infra/src/trace_util.rs
@@ -13,8 +13,16 @@ pub async fn configure_tracing() {
     TRACING_INITIALIZED
         .get_or_init(|| async {
             let fmt_layer = fmt::layer().compact().with_target(true);
-            let level_filter_layer =
-                EnvFilter::builder().with_default_directive(DEFAULT_LEVEL.into()).from_env_lossy();
+            let level_filter_layer = EnvFilter::builder()
+                .with_default_directive(DEFAULT_LEVEL.into())
+                .from_env_lossy()
+                .add_directive("alloy_provider=info".parse().unwrap())
+                .add_directive("alloy_transport_http=info".parse().unwrap())
+                .add_directive("alloy_rpc_client=info".parse().unwrap())
+                .add_directive("hyper=info".parse().unwrap())
+                .add_directive("hyper_util=info".parse().unwrap())
+                .add_directive("multistream_select=info".parse().unwrap())
+                .add_directive("yamux=info".parse().unwrap());
 
             // This sets a single subscriber to all of the threads. We may want to implement
             // different subscriber for some threads and use set_global_default instead
