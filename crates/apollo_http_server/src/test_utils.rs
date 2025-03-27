448c047bb09755aa9c693b2925e0059fdaf7e1a0
Yael Doweck <yael@starkware.co>
2025-03-27 08:46:04 +0200

    test(apollo_http_server): test deprecated gateway endpoint

--- crates/apollo_http_server/src/test_utils.rs
+++ crates/apollo_http_server/src/test_utils.rs
@@ -4,13 +4,19 @@ use std::sync::Arc;
 use apollo_gateway_types::communication::MockGatewayClient;
 use apollo_gateway_types::gateway_types::GatewayOutput;
 use axum::body::Body;
+use blockifier_test_utils::cairo_versions::CairoVersion;
 use hyper::StatusCode;
+use mempool_test_utils::starknet_api_test_utils::invoke_tx;
 use reqwest::{Client, Response};
-use starknet_api::rpc_transaction::RpcTransaction;
-use starknet_api::test_utils::rpc_tx_to_json;
+use serde::Serialize;
+use starknet_api::rpc_transaction::{RpcInvokeTransaction, RpcTransaction};
 use starknet_api::transaction::TransactionHash;
 
 use crate::config::HttpServerConfig;
+use crate::deprecated_gateway_transaction::{
+    DeprecatedGatewayInvokeTransaction,
+    DeprecatedGatewayTransactionV3,
+};
 use crate::http_server::HttpServer;
 
 /// A test utility client for interacting with an http server.
@@ -25,8 +31,8 @@ impl HttpTestClient {
         Self { socket, client }
     }
 
-    pub async fn assert_add_tx_success(&self, rpc_tx: RpcTransaction) -> TransactionHash {
-        let response = self.add_tx(rpc_tx).await;
+    pub async fn assert_add_tx_success(&self, tx: impl GatewayTransaction) -> TransactionHash {
+        let response = self.add_tx(tx).await;
         assert!(response.status().is_success());
         let text = response.text().await.unwrap();
         let response: GatewayOutput = serde_json::from_str(&text)
@@ -46,27 +52,26 @@ impl HttpTestClient {
 
     // Prefer using assert_add_tx_success or other higher level methods of this client, to ensure
     // tests are boilerplate and implementation-detail free.
-    pub async fn add_tx(&self, rpc_tx: RpcTransaction) -> Response {
-        self.add_tx_with_headers(rpc_tx, []).await
+    pub async fn add_tx(&self, tx: impl GatewayTransaction) -> Response {
+        self.add_tx_with_headers(tx, []).await
     }
 
     pub async fn add_tx_with_headers<I>(
         &self,
-        rpc_tx: RpcTransaction,
+        tx: impl GatewayTransaction,
         header_members: I,
     ) -> Response
     where
         I: IntoIterator<Item = (&'static str, &'static str)>,
     {
-        let tx_json = rpc_tx_to_json(&rpc_tx);
         let mut request =
-            self.client.post(format!("http://{}/gateway/add_rpc_transaction", self.socket));
+            self.client.post(format!("http://{}/gateway/{}", self.socket, tx.endpoint()));
         for (key, value) in header_members {
             request = request.header(key, value);
         }
         request
-            .header("content-type", "application/json")
-            .body(Body::from(tx_json))
+            .header("content-type", tx.content_type())
+            .body(Body::from(serde_json::to_string(&tx).unwrap()))
             .send()
             .await
             .unwrap()
@@ -94,4 +99,44 @@ pub async fn http_client_server_setup(
     tokio::task::yield_now().await;
 
     add_tx_http_client
+}
+
+pub trait GatewayTransaction: Serialize {
+    fn endpoint(&self) -> &str;
+    fn content_type(&self) -> &str;
+}
+
+impl GatewayTransaction for RpcTransaction {
+    fn endpoint(&self) -> &str {
+        "add_rpc_transaction"
+    }
+
+    fn content_type(&self) -> &str {
+        "application/json"
+    }
+}
+
+impl GatewayTransaction for DeprecatedGatewayTransactionV3 {
+    fn endpoint(&self) -> &str {
+        "add_transaction"
+    }
+
+    fn content_type(&self) -> &str {
+        "application/text"
+    }
+}
+
+pub fn rpc_tx() -> RpcTransaction {
+    invoke_tx(CairoVersion::default())
+}
+
+pub fn deprecated_gateway_tx() -> DeprecatedGatewayTransactionV3 {
+    let tx = invoke_tx(CairoVersion::default());
+    if let RpcTransaction::Invoke(RpcInvokeTransaction::V3(invoke_tx)) = tx {
+        DeprecatedGatewayTransactionV3::Invoke(DeprecatedGatewayInvokeTransaction::V3(
+            invoke_tx.into(),
+        ))
+    } else {
+        panic!("Expected invoke transaction")
+    }
 }
