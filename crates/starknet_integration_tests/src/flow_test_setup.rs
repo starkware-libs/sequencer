94ae5048ed46b76f85b98052f2aa812a10696652
Yair <92672946+yair-starkware@users.noreply.github.com>
2025-03-27 11:17:27 +0200

    feat(starknet_integration_tests): refactor e2e flow test (#4787)
    Check success on aggregated txs instead of per height

--- crates/starknet_integration_tests/src/flow_test_setup.rs
+++ crates/starknet_integration_tests/src/flow_test_setup.rs
@@ -50,7 +50,7 @@ use starknet_sequencer_node::utils::create_node_modules;
 use starknet_state_sync::config::StateSyncConfig;
 use starknet_types_core::felt::Felt;
 use tokio::sync::Mutex;
-use tracing::{debug, instrument};
+use tracing::{debug, instrument, Instrument};
 use url::Url;
 
 use crate::state_reader::{StorageTestHandles, StorageTestSetup};
@@ -80,9 +80,9 @@ pub struct FlowTestSetup {
     l1_handle: AnvilInstance,
     starknet_l1_contract: StarknetL1Contract,
 
-    // Channels for consensus proposals, used for asserting the right transactions are proposed.
-    pub consensus_proposals_channels:
-        BroadcastTopicChannels<StreamMessage<ProposalPart, HeightAndRound>>,
+    // The transactions that were streamed in the consensus proposals, used for asserting the right
+    // transactions are batched.
+    pub accumulated_txs: Arc<Mutex<AccumulatedTransactions>>,
 }
 
 impl FlowTestSetup {
@@ -119,6 +119,16 @@ impl FlowTestSetup {
         let (anvil, starknet_l1_contract) =
             spawn_anvil_and_deploy_starknet_l1_contract(&base_layer_config).await;
 
+        // Spawn a thread that listens to proposals and collects batched transactions.
+        let accumulated_txs = Arc::new(Mutex::new(AccumulatedTransactions::default()));
+        let tx_collector_task = TxCollector {
+            consensus_proposals_channels,
+            accumulated_txs: accumulated_txs.clone(),
+            chain_id: chain_info.chain_id.clone(),
+        };
+
+        tokio::spawn(tx_collector_task.collect_streamd_txs().in_current_span());
+
         // Create nodes one after the other in order to make sure the ports are not overlapping.
         let sequencer_0 = FlowSequencerSetup::new(
             accounts.to_vec(),
@@ -146,13 +156,7 @@ impl FlowTestSetup {
         )
         .await;
 
-        Self {
-            sequencer_0,
-            sequencer_1,
-            l1_handle: anvil,
-            starknet_l1_contract,
-            consensus_proposals_channels,
-        }
+        Self { sequencer_0, sequencer_1, l1_handle: anvil, starknet_l1_contract, accumulated_txs }
     }
 
     pub async fn assert_add_tx_error(&self, tx: RpcTransaction) -> GatewaySpecError {
@@ -313,14 +317,14 @@ pub fn create_consensus_manager_configs_and_channels(
 }
 
 // Collects batched transactions.
-struct _TxCollector {
+struct TxCollector {
     pub consensus_proposals_channels:
         BroadcastTopicChannels<StreamMessage<ProposalPart, HeightAndRound>>,
     pub accumulated_txs: Arc<Mutex<AccumulatedTransactions>>,
     pub chain_id: ChainId,
 }
 
-impl _TxCollector {
+impl TxCollector {
     #[instrument(skip(self))]
     pub async fn collect_streamd_txs(mut self) {
         loop {
@@ -366,7 +370,11 @@ impl _TxCollector {
             panic!("Expected an init message. Got: {:?}", init_message)
         };
 
-        let mut received_tx_hashes = Vec::new();
+        self.accumulated_txs
+            .lock()
+            .await
+            .start_round(incoming_proposal_init.height, incoming_proposal_init.round);
+
         let mut got_proposal_fin = false;
         let mut got_channel_fin = false;
         for i in 1_u64..messages_cache.len().try_into().unwrap() {
@@ -386,8 +394,10 @@ impl _TxCollector {
                 StreamMessageBody::Content(ProposalPart::Transactions(transactions)) => {
                     // TODO(Arni): add calculate_transaction_hash to consensus transaction and use
                     // it here.
-                    received_tx_hashes.extend(transactions.transactions.iter().map(|tx| {
-                        match tx {
+                    let received_tx_hashes: Vec<_> = transactions
+                        .transactions
+                        .iter()
+                        .map(|tx| match tx {
                             ConsensusTransaction::RpcTransaction(tx) => {
                                 let starknet_api_tx =
                                     starknet_api::transaction::Transaction::from(tx.clone());
@@ -399,14 +409,9 @@ impl _TxCollector {
                                     &TransactionVersion::ZERO,
                                 )
                                 .unwrap(),
-                        }
-                    }));
-
-                    self.accumulated_txs.lock().await.add_transactions(
-                        incoming_proposal_init.height,
-                        incoming_proposal_init.round,
-                        &received_tx_hashes,
-                    );
+                        })
+                        .collect();
+                    self.accumulated_txs.lock().await.add_transactions(&received_tx_hashes);
                 }
                 StreamMessageBody::Fin => {
                     got_channel_fin = true;
