d6023a1f5bed9a0cca09ff4d3a512b6f218547fb
Yael Doweck <yael@starkware.co>
2025-03-27 08:31:41 +0200

    feat(starknet_http_server): add conversion functions from rest transactions to rpc

--- crates/starknet_http_server/src/deprecated_gateway_transaction.rs
+++ crates/starknet_http_server/src/deprecated_gateway_transaction.rs
@@ -1,9 +1,20 @@
 use serde::{Deserialize, Serialize};
 use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
 use starknet_api::data_availability::DataAvailabilityMode;
-use starknet_api::rpc_transaction::EntryPointByType;
+use starknet_api::rpc_transaction::{
+    EntryPointByType,
+    RpcDeclareTransaction,
+    RpcDeclareTransactionV3,
+    RpcDeployAccountTransaction,
+    RpcDeployAccountTransactionV3,
+    RpcInvokeTransaction,
+    RpcInvokeTransactionV3,
+    RpcTransaction,
+};
+use starknet_api::state::SierraContractClass;
 use starknet_api::transaction::fields::{
     AccountDeploymentData,
+    AllResourceBounds,
     Calldata,
     ContractAddressSalt,
     PaymasterData,
@@ -31,6 +42,24 @@ pub enum DeprecatedGatewayTransactionV3 {
     Invoke(DeprecatedGatewayInvokeTransaction),
 }
 
+impl From<DeprecatedGatewayTransactionV3> for RpcTransaction {
+    fn from(deprecated_tx: DeprecatedGatewayTransactionV3) -> Self {
+        match deprecated_tx {
+            DeprecatedGatewayTransactionV3::Declare(DeprecatedGatewayDeclareTransaction::V3(
+                deprecated_declare_tx,
+            )) => RpcTransaction::Declare(RpcDeclareTransaction::V3(deprecated_declare_tx.into())),
+            DeprecatedGatewayTransactionV3::DeployAccount(
+                DeprecatedGatewayDeployAccountTransaction::V3(deprecated_deploy_account_tx),
+            ) => RpcTransaction::DeployAccount(RpcDeployAccountTransaction::V3(
+                deprecated_deploy_account_tx.into(),
+            )),
+            DeprecatedGatewayTransactionV3::Invoke(DeprecatedGatewayInvokeTransaction::V3(
+                deprecated_invoke_tx,
+            )) => RpcTransaction::Invoke(RpcInvokeTransaction::V3(deprecated_invoke_tx.into())),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, Hash)]
 #[serde(tag = "version")]
 pub enum DeprecatedGatewayInvokeTransaction {
@@ -52,6 +81,23 @@ pub struct DeprecatedGatewayInvokeTransactionV3 {
     pub fee_data_availability_mode: DataAvailabilityMode,
 }
 
+impl From<DeprecatedGatewayInvokeTransactionV3> for RpcInvokeTransactionV3 {
+    fn from(deprecated_invoke_tx: DeprecatedGatewayInvokeTransactionV3) -> Self {
+        RpcInvokeTransactionV3 {
+            sender_address: deprecated_invoke_tx.sender_address,
+            calldata: deprecated_invoke_tx.calldata,
+            signature: deprecated_invoke_tx.signature,
+            nonce: deprecated_invoke_tx.nonce,
+            resource_bounds: deprecated_invoke_tx.resource_bounds.into(),
+            tip: deprecated_invoke_tx.tip,
+            paymaster_data: deprecated_invoke_tx.paymaster_data,
+            account_deployment_data: deprecated_invoke_tx.account_deployment_data,
+            nonce_data_availability_mode: deprecated_invoke_tx.nonce_data_availability_mode,
+            fee_data_availability_mode: deprecated_invoke_tx.fee_data_availability_mode,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, Hash)]
 #[serde(tag = "version")]
 pub enum DeprecatedGatewayDeployAccountTransaction {
@@ -71,6 +117,23 @@ pub struct DeprecatedGatewayDeployAccountTransactionV3 {
     pub paymaster_data: PaymasterData,
     pub nonce_data_availability_mode: DataAvailabilityMode,
     pub fee_data_availability_mode: DataAvailabilityMode,
+}
+
+impl From<DeprecatedGatewayDeployAccountTransactionV3> for RpcDeployAccountTransactionV3 {
+    fn from(deprecated_deploy_account_tx: DeprecatedGatewayDeployAccountTransactionV3) -> Self {
+        RpcDeployAccountTransactionV3 {
+            signature: deprecated_deploy_account_tx.signature,
+            nonce: deprecated_deploy_account_tx.nonce,
+            class_hash: deprecated_deploy_account_tx.class_hash,
+            contract_address_salt: deprecated_deploy_account_tx.contract_address_salt,
+            constructor_calldata: deprecated_deploy_account_tx.constructor_calldata,
+            resource_bounds: deprecated_deploy_account_tx.resource_bounds.into(),
+            tip: deprecated_deploy_account_tx.tip,
+            paymaster_data: deprecated_deploy_account_tx.paymaster_data,
+            nonce_data_availability_mode: deprecated_deploy_account_tx.nonce_data_availability_mode,
+            fee_data_availability_mode: deprecated_deploy_account_tx.fee_data_availability_mode,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, Hash)]
@@ -95,12 +158,36 @@ pub struct DeprecatedGatewayDeclareTransactionV3 {
     pub fee_data_availability_mode: DataAvailabilityMode,
 }
 
+impl From<DeprecatedGatewayDeclareTransactionV3> for RpcDeclareTransactionV3 {
+    fn from(deprecated_declare_tx: DeprecatedGatewayDeclareTransactionV3) -> Self {
+        RpcDeclareTransactionV3 {
+            sender_address: deprecated_declare_tx.sender_address,
+            compiled_class_hash: deprecated_declare_tx.compiled_class_hash,
+            signature: deprecated_declare_tx.signature,
+            nonce: deprecated_declare_tx.nonce,
+            contract_class: deprecated_declare_tx.contract_class.into(),
+            resource_bounds: deprecated_declare_tx.resource_bounds.into(),
+            tip: deprecated_declare_tx.tip,
+            paymaster_data: deprecated_declare_tx.paymaster_data,
+            account_deployment_data: deprecated_declare_tx.account_deployment_data,
+            nonce_data_availability_mode: deprecated_declare_tx.nonce_data_availability_mode,
+            fee_data_availability_mode: deprecated_declare_tx.fee_data_availability_mode,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, Hash)]
 pub struct DeprecatedGatewaySierraContractClass {
     pub sierra_program: String,
     pub contract_class_version: String,
     pub entry_points_by_type: EntryPointByType,
     pub abi: String,
+}
+
+impl From<DeprecatedGatewaySierraContractClass> for SierraContractClass {
+    fn from(_deprecated_sierra_contract_class: DeprecatedGatewaySierraContractClass) -> Self {
+        unimplemented!()
+    }
 }
 
 #[derive(
@@ -111,4 +198,14 @@ pub struct DeprecatedGatewayAllResourceBounds {
     pub l1_gas: ResourceBounds,
     pub l2_gas: ResourceBounds,
     pub l1_data_gas: ResourceBounds,
+}
+
+impl From<DeprecatedGatewayAllResourceBounds> for AllResourceBounds {
+    fn from(deprecated_all_resource_bounds: DeprecatedGatewayAllResourceBounds) -> Self {
+        AllResourceBounds {
+            l1_gas: deprecated_all_resource_bounds.l1_gas,
+            l2_gas: deprecated_all_resource_bounds.l2_gas,
+            l1_data_gas: deprecated_all_resource_bounds.l1_data_gas,
+        }
+    }
 }
