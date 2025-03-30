26e7fc91b3cd0ec1e4415fbbc76ea16c20fbb33f
dafnamatsry <92669167+dafnamatsry@users.noreply.github.com>
2025-03-30 11:48:23 +0300

    feat(starknet_mempool): reject tx if mempool exceeds its capacity (#5178)

--- crates/starknet_mempool/src/transaction_pool.rs
+++ crates/starknet_mempool/src/transaction_pool.rs
@@ -44,6 +44,10 @@ impl TransactionPool {
         self.capacity.n_txs()
     }
 
+    pub fn size_in_bytes(&self) -> u64 {
+        self.capacity.size_in_bytes()
+    }
+
     pub fn insert(&mut self, tx: InternalRpcTransaction) -> MempoolResult<()> {
         let tx_reference = TransactionReference::new(&tx);
         let tx_hash = tx_reference.tx_hash;
@@ -296,6 +300,10 @@ impl PoolCapacity {
 
     fn n_txs(&self) -> usize {
         self.n_txs
+    }
+
+    fn size_in_bytes(&self) -> u64 {
+        self.size_in_bytes
     }
 }
 
