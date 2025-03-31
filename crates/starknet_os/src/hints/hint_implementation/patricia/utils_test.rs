83e2dc4a3237b2e57e30ba0a1352e5e87ca20299
rotem-starkware <rotem@starkware.co>
2025-03-31 15:55:47 +0300

    refactor(starknet_os): remove optional nodes from UpdatTree

--- crates/starknet_os/src/hints/hint_implementation/patricia/utils_test.rs
+++ crates/starknet_os/src/hints/hint_implementation/patricia/utils_test.rs
@@ -2,12 +2,12 @@ use starknet_patricia::hash::hash_trait::HashOutput;
 use starknet_patricia::patricia_merkle_tree::types::SubTreeHeight;
 use starknet_types_core::felt::Felt;
 
-use super::{build_update_tree, InnerNode, LayerIndex, UpdateTreeInner};
+use super::{build_update_tree, InnerNode, LayerIndex, UpdateTree};
 
 #[test]
 fn test_build_update_tree_empty() {
     let update_tree = build_update_tree(SubTreeHeight::new(3), vec![]).unwrap();
-    assert!(update_tree.is_none());
+    assert_eq!(update_tree, UpdateTree::None);
 }
 
 #[test]
@@ -20,21 +20,19 @@ fn test_build_update_tree() {
     let update_tree = build_update_tree(SubTreeHeight::new(3), modifications).unwrap();
 
     // expected_update_tree = (((None, 12), None), ((1000, None), (30, None)))
-    let expected_update_tree = Some(UpdateTreeInner::InnerNode(InnerNode::Both(
-        Box::new(Some(UpdateTreeInner::InnerNode(InnerNode::Left(Box::new(Some(
-            UpdateTreeInner::InnerNode(InnerNode::Right(Box::new(Some(UpdateTreeInner::Leaf(
-                HashOutput(Felt::from(12)),
+    let expected_update_tree = UpdateTree::InnerNode(InnerNode::Both(
+        Box::new(UpdateTree::InnerNode(InnerNode::Left(Box::new(UpdateTree::InnerNode(
+            InnerNode::Right(Box::new(UpdateTree::Leaf(HashOutput(Felt::from(12))))),
+        ))))),
+        Box::new(UpdateTree::InnerNode(InnerNode::Both(
+            Box::new(UpdateTree::InnerNode(InnerNode::Left(Box::new(UpdateTree::Leaf(
+                HashOutput(Felt::from(1000)),
             ))))),
-        )))))),
-        Box::new(Some(UpdateTreeInner::InnerNode(InnerNode::Both(
-            Box::new(Some(UpdateTreeInner::InnerNode(InnerNode::Left(Box::new(Some(
-                UpdateTreeInner::Leaf(HashOutput(Felt::from(1000))),
-            )))))),
-            Box::new(Some(UpdateTreeInner::InnerNode(InnerNode::Left(Box::new(Some(
-                UpdateTreeInner::Leaf(HashOutput(Felt::from(30))),
-            )))))),
-        )))),
-    )));
+            Box::new(UpdateTree::InnerNode(InnerNode::Left(Box::new(UpdateTree::Leaf(
+                HashOutput(Felt::from(30)),
+            ))))),
+        ))),
+    ));
 
     assert_eq!(update_tree, expected_update_tree);
 }
