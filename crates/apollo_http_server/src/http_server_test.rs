6fd481e02d2d3478541e02b037a2a9450f80df28
Noam Spiegelstein <noam.s@starkware.co>
2025-03-31 15:58:32 +0300

    fix(apollo_http_server): return error code 400 when gateway returns error

--- crates/apollo_http_server/src/http_server_test.rs
+++ crates/apollo_http_server/src/http_server_test.rs
@@ -1,5 +1,4 @@
 use std::net::{IpAddr, Ipv4Addr};
-use std::panic::AssertUnwindSafe;
 
 use apollo_gateway_types::communication::{GatewayClientError, MockGatewayClient};
 use apollo_gateway_types::errors::{GatewayError, GatewaySpecError};
@@ -16,7 +15,6 @@ use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::Json;
 use blockifier_test_utils::cairo_versions::CairoVersion;
-use futures::FutureExt;
 use jsonrpsee::types::error::ErrorCode;
 use jsonrpsee::types::ErrorObjectOwned;
 use mempool_test_utils::starknet_api_test_utils::invoke_tx;
@@ -175,10 +173,9 @@ async fn test_response() {
 
     // Set the failed response.
     let expected_error = GatewaySpecError::ClassAlreadyDeclared;
-    let expected_err_str = format!(
-        "Gateway responded with: {}",
-        serde_json::to_string(&ErrorObjectOwned::from(expected_error.clone().into_rpc())).unwrap()
-    );
+    let expected_err_str =
+        serde_json::to_string(&ErrorObjectOwned::from(expected_error.clone().into_rpc())).unwrap();
+
     mock_gateway_client.expect_add_tx().times(1).return_const(Err(
         GatewayClientError::GatewayError(GatewayError::GatewaySpecError {
             source: expected_error,
@@ -199,8 +196,6 @@ async fn test_response() {
 
     // Test a failed response.
     let rpc_tx = invoke_tx(CairoVersion::default());
-    let panicking_task = AssertUnwindSafe(add_tx_http_client.assert_add_tx_success(rpc_tx));
-    let error = panicking_task.catch_unwind().await.unwrap_err().downcast::<String>().unwrap();
-    let error_str = format!("{}", error);
+    let error_str = add_tx_http_client.assert_add_tx_error(rpc_tx).await;
     assert_eq!(error_str, expected_err_str);
 }
