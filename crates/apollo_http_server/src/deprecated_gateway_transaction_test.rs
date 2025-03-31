038c81af1dd9497371e5cc9b46017b29499a0473
Arni Hod <arnon@starkware.co>
2025-03-31 11:24:32 +0300

    test(apollo_http_server): add negative flow test for deprecated declare

--- crates/apollo_http_server/src/deprecated_gateway_transaction_test.rs
+++ crates/apollo_http_server/src/deprecated_gateway_transaction_test.rs
@@ -1,4 +1,7 @@
+use std::io::Write;
+
 use assert_matches::assert_matches;
+use rstest::rstest;
 use starknet_api::compression_utils::CompressionError;
 use starknet_api::rpc_transaction::RpcDeclareTransactionV3;
 use starknet_api::test_utils::read_json_file;
@@ -19,6 +22,28 @@ const DEPRECATED_GATEWAY_DECLARE_TX_JSON_PATH: &str = "deprecated_gateway/declar
 fn deprecated_gateway_declare_tx() -> DeprecatedGatewayDeclareTransaction {
     serde_json::from_value(read_json_file(DEPRECATED_GATEWAY_DECLARE_TX_JSON_PATH))
         .expect("Failed to deserialize json to RestDeclareTransactionV3")
+}
+
+enum CompressionErrorType {
+    Io,
+    Serde,
+    Decode,
+}
+
+impl CompressionErrorType {
+    fn assert_matches_error_type(&self, error: CompressionError) {
+        match self {
+            CompressionErrorType::Io => {
+                assert_matches!(error, CompressionError::Io(..))
+            }
+            CompressionErrorType::Serde => {
+                assert_matches!(error, CompressionError::Serde(..))
+            }
+            CompressionErrorType::Decode => {
+                assert_matches!(error, CompressionError::Decode(base64::DecodeError::InvalidLength))
+            }
+        }
+    }
 }
 
 // Tests.
@@ -49,8 +74,22 @@ fn deprecated_gateway_declare_tx_conversion() {
     let _declare_tx: RpcDeclareTransactionV3 = deprecate_declare_tx.try_into().unwrap();
 }
 
-#[test]
-fn deprecated_gateway_declare_tx_negative_flow_conversion() {
+fn create_malformed_sierra_program_for_serde_error() -> String {
+    let invalid_json = b"arbitrary";
+    let mut encoder = flate2::write::GzEncoder::new(Vec::new(), flate2::Compression::default());
+    encoder.write_all(invalid_json).unwrap();
+    let compressed = encoder.finish().unwrap();
+    base64::encode(compressed)
+}
+
+#[rstest]
+#[case::io_error(base64::encode("arbitrary"), CompressionErrorType::Io)]
+#[case::serde_error(create_malformed_sierra_program_for_serde_error(), CompressionErrorType::Serde)]
+#[case::decode_error("arbitrary".to_string(), CompressionErrorType::Decode)]
+fn deprecated_gateway_declare_tx_negative_flow_conversion(
+    #[case] sierra_program: String,
+    #[case] expected_error_type: CompressionErrorType,
+) {
     let deprecate_tx = deprecated_gateway_declare_tx();
     let mut deprecate_declare_tx = assert_matches!(
         deprecate_tx,
@@ -58,7 +97,8 @@ fn deprecated_gateway_declare_tx_negative_flow_conversion() {
         deprecated_declare_tx
     );
 
-    deprecate_declare_tx.contract_class.sierra_program += "arbitrary";
-    let error = RpcDeclareTransactionV3::try_from(deprecate_declare_tx).unwrap_err();
-    assert_matches!(error, CompressionError::Decode(base64::DecodeError::InvalidLength));
+    deprecate_declare_tx.contract_class.sierra_program = sierra_program;
+    expected_error_type.assert_matches_error_type(
+        RpcDeclareTransactionV3::try_from(deprecate_declare_tx).unwrap_err(),
+    );
 }
