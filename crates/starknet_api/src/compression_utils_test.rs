1ccf104168f2a8b137f0e60816352662d9c11388
Arnon Hod <arnon@starkware.co>
2025-04-01 10:47:22 +0300

    refactor(starknet_api): generic return type on decode and decompress (#5413)

--- crates/starknet_api/src/compression_utils_test.rs
+++ crates/starknet_api/src/compression_utils_test.rs
@@ -1,6 +1,7 @@
 use pretty_assertions::assert_eq;
+use starknet_crypto::Felt;
 
-use super::{compress_and_encode, decode_and_decompress};
+use crate::compression_utils::{compress_and_encode, decode_and_decompress};
 use crate::test_utils::read_json_file;
 
 #[test]
@@ -15,7 +16,8 @@ fn compress_and_encode_hardcoded_value() {
 fn decode_and_decompress_hardcoded_value() {
     let sierra_program_base64 =
         read_json_file("sierra_program_base64.json").as_str().unwrap().to_owned();
-    let expected_value = read_json_file("sierra_program.json");
-    let value = decode_and_decompress(&sierra_program_base64).unwrap();
+    let expected_value: Vec<Felt> =
+        serde_json::from_value(read_json_file("sierra_program.json")).unwrap();
+    let value: Vec<Felt> = decode_and_decompress(&sierra_program_base64).unwrap();
     assert_eq!(value, expected_value);
 }
