26438a2c5473d1d2a8dcefdd4ab41c230591dac7
amos rothberg <amos@starkware.co>
2025-04-01 13:31:54 +0300

    feat(starknet_os, starknet_committer_and_os_cli): added test utils

--- crates/starknet_os/src/test_utils/utils.rs
+++ crates/starknet_os/src/test_utils/utils.rs
@@ -1,11 +1,15 @@
 use std::any::Any;
 use std::collections::HashMap;
 
+use starknet_types_core::felt::Felt;
+
 use crate::test_utils::cairo_runner::{
     run_cairo_0_entry_point,
     Cairo0EntryPointRunnerResult,
     EndpointArg,
     ImplicitArg,
+    PointerArg,
+    ValueArg,
 };
 
 pub fn run_cairo_function_and_check_result(
@@ -28,4 +32,22 @@ pub fn run_cairo_function_and_check_result(
     assert_eq!(expected_explicit_retdata, &actual_explicit_retdata);
     assert_eq!(expected_implicit_retdata, &actual_implicit_retdata);
     Ok(())
+}
+
+pub fn create_squashed_cairo_dict(
+    prev_values: &HashMap<Felt, EndpointArg>,
+    new_values: &HashMap<Felt, EndpointArg>,
+) -> PointerArg {
+    let mut squashed_dict: Vec<EndpointArg> = vec![];
+    let mut sorted_new_values: Vec<_> = new_values.iter().collect();
+    sorted_new_values.sort_by_key(|(key, _)| *key);
+
+    for (key, value) in sorted_new_values {
+        let prev_value: &EndpointArg =
+            prev_values.get(key).unwrap_or(&EndpointArg::Value(ValueArg::Single(Felt::ZERO)));
+        squashed_dict.push((*key).into());
+        squashed_dict.push(prev_value.clone());
+        squashed_dict.push(value.clone());
+    }
+    PointerArg::Composed(squashed_dict)
 }
