62c575716ada393399377c90f4ed9b495d808cc8
Yael Doweck <yael@starkware.co>
2025-04-01 12:38:19 +0300

    core(apollo_htpp_server): record metrics for serde failure

--- crates/apollo_http_server/src/metrics_test.rs
+++ crates/apollo_http_server/src/metrics_test.rs
@@ -4,6 +4,8 @@ use apollo_gateway_types::communication::{GatewayClientError, MockGatewayClient}
 use apollo_infra_utils::test_utils::{AvailablePorts, TestIdentifier};
 use apollo_sequencer_infra::component_client::ClientError;
 use metrics_exporter_prometheus::PrometheusBuilder;
+use rstest::rstest;
+use serde::Serialize;
 use starknet_api::transaction::TransactionHash;
 
 use crate::config::HttpServerConfig;
@@ -12,11 +14,17 @@ use crate::metrics::{
     ADDED_TRANSACTIONS_SUCCESS,
     ADDED_TRANSACTIONS_TOTAL,
 };
-use crate::test_utils::{http_client_server_setup, rpc_tx, HttpServerEndpoint};
+use crate::test_utils::{http_client_server_setup, rest_tx, rpc_tx, HttpServerEndpoint};
 
-// TODO(Yael): add a test for rest_api endpoint
+#[rstest]
+#[case::add_rest_tx(0, HttpServerEndpoint::AddTx, rest_tx())]
+#[case::add_rpc_tx(1, HttpServerEndpoint::AddRpcTx, rpc_tx())]
 #[tokio::test]
-async fn get_metrics_test() {
+async fn add_tx_metrics_test(
+    #[case] index: u16,
+    #[case] endpoint: HttpServerEndpoint,
+    #[case] tx: impl Serialize + Clone,
+) {
     // Create a mock gateway client that returns a successful response and a failure response.
     const SUCCESS_TXS_TO_SEND: usize = 1;
     const FAILURE_TXS_TO_SEND: usize = 1;
@@ -41,38 +49,55 @@ async fn get_metrics_test() {
 
     let ip = IpAddr::from(Ipv4Addr::LOCALHOST);
     let mut available_ports = AvailablePorts::new(TestIdentifier::HttpServerUnitTests.into(), 0);
-    let http_server_config = HttpServerConfig { ip, port: available_ports.get_next_port() };
+    let http_server_config = HttpServerConfig { ip, port: available_ports.get_next_port() + index };
     let add_tx_http_client =
         http_client_server_setup(mock_gateway_client, http_server_config).await;
 
     // Send transactions to the server.
     for _ in std::iter::repeat(()).take(SUCCESS_TXS_TO_SEND + FAILURE_TXS_TO_SEND) {
-        add_tx_http_client.add_tx(rpc_tx(), HttpServerEndpoint::AddRpcTx).await;
+        add_tx_http_client.add_tx(tx.clone(), endpoint).await;
     }
 
     // Obtain and parse metrics.
     let metrics = prometheus_handle.render();
-    let added_transactions_total_count =
-        ADDED_TRANSACTIONS_TOTAL.parse_numeric_metric::<usize>(&metrics);
-    let added_transactions_success_count =
-        ADDED_TRANSACTIONS_SUCCESS.parse_numeric_metric::<usize>(&metrics);
-    let added_transactions_failure_count =
-        ADDED_TRANSACTIONS_FAILURE.parse_numeric_metric::<usize>(&metrics);
 
     // Ensure the metric values are as expected.
-    assert_eq!(
-        added_transactions_total_count.unwrap(),
-        SUCCESS_TXS_TO_SEND + FAILURE_TXS_TO_SEND,
-        "Total transaction count mismatch"
-    );
-    assert_eq!(
-        added_transactions_success_count.unwrap(),
-        SUCCESS_TXS_TO_SEND,
-        "Successful transaction count mismatch"
-    );
-    assert_eq!(
-        added_transactions_failure_count.unwrap(),
-        FAILURE_TXS_TO_SEND,
-        "Failing transaction count mismatch"
-    );
+    ADDED_TRANSACTIONS_TOTAL
+        .assert_eq::<usize>(&metrics, SUCCESS_TXS_TO_SEND + FAILURE_TXS_TO_SEND);
+    ADDED_TRANSACTIONS_SUCCESS.assert_eq::<usize>(&metrics, SUCCESS_TXS_TO_SEND);
+    ADDED_TRANSACTIONS_FAILURE.assert_eq::<usize>(&metrics, FAILURE_TXS_TO_SEND);
+}
+
+#[tokio::test]
+async fn add_tx_serde_failure_metrics_test() {
+    let mut mock_gateway_client = MockGatewayClient::new();
+    // Set the successful response.
+    mock_gateway_client
+        .expect_add_tx()
+        .times(1)
+        .return_once(move |_| Ok(TransactionHash::default()));
+
+    // Initialize the metrics directly instead of spawning a monitoring endpoint task.
+    let recorder = PrometheusBuilder::new().build_recorder();
+    let _recorder_guard = metrics::set_default_local_recorder(&recorder);
+    let prometheus_handle = recorder.handle();
+
+    // TODO(Yael): share the creation of add_tx_http_client code for all tests in test_utils.
+    let ip = IpAddr::from(Ipv4Addr::LOCALHOST);
+    let mut available_ports = AvailablePorts::new(TestIdentifier::HttpServerUnitTests.into(), 0);
+    let http_server_config = HttpServerConfig { ip, port: available_ports.get_next_port() + 2 };
+    let add_tx_http_client =
+        http_client_server_setup(mock_gateway_client, http_server_config).await;
+
+    // Send a transaction that fails deserialization.
+    let tx = "invalid_tx";
+    add_tx_http_client.add_tx(tx, HttpServerEndpoint::AddTx).await;
+
+    // Obtain and parse metrics.
+    let metrics = prometheus_handle.render();
+
+    // Ensure the metric values are as expected.
+    ADDED_TRANSACTIONS_TOTAL.assert_eq::<usize>(&metrics, 1);
+    ADDED_TRANSACTIONS_SUCCESS.assert_eq::<usize>(&metrics, 0);
+    ADDED_TRANSACTIONS_FAILURE.assert_eq::<usize>(&metrics, 1);
 }
