3e0e621105821cb29b204d0d2d6f922e64c7b273
Itay Tsabary <itayt@starkware.co>
2025-04-02 22:24:53 +0300

    chore(apollo_deployments): add base app config file for system test

--- crates/apollo_integration_tests/src/consts.rs
+++ crates/apollo_integration_tests/src/consts.rs
@@ -1,4 +1,4 @@
-// TODO(Tsabray): avoid re-exporting here, and directly import when needed.
-pub use apollo_deployments::deployment_definitions::SINGLE_NODE_CONFIG_PATH;
+pub const SINGLE_NODE_CONFIG_PATH: &str =
+    "config/sequencer/presets/system_test_presets/single_node/node_0/executable_0/node_config.json";
 
 pub const DATA_PREFIX_PATH: &str = "/data";
