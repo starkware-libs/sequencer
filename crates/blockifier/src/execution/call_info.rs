ff26c08c1283c548ae069b5012999c773c033263
Yoav Gross <yoav@starkware.co>
2025-04-02 16:25:21 +0300

    feat(starknet_os): implement start_tx

--- crates/blockifier/src/execution/call_info.rs
+++ crates/blockifier/src/execution/call_info.rs
@@ -211,9 +211,8 @@ pub struct StorageAccessTracker {
 }
 
 /// Represents the full effects of executing an entry point, including the inner calls it invoked.
-#[cfg_attr(any(test, feature = "testing"), derive(Clone))]
 #[cfg_attr(feature = "transaction_serde", derive(serde::Deserialize))]
-#[derive(Debug, Default, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize)]
 pub struct CallInfo {
     pub call: CallEntryPoint,
     pub execution: CallExecution,
--- crates/starknet_os/src/hint_processor/execution_helper.rs
+++ crates/starknet_os/src/hint_processor/execution_helper.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
+use std::vec::IntoIter;
 
+use blockifier::execution::call_info::CallInfo;
 use blockifier::state::cached_state::{CachedState, StateMaps};
 use blockifier::state::state_api::StateReader;
 #[cfg(any(feature = "testing", test))]
 use blockifier::test_utils::dict_state_reader::DictStateReader;
+use shared_execution_objects::central_objects::CentralTransactionExecutionInfo;
+use starknet_api::executable_transaction::TransactionType;
+use starknet_api::transaction::fields::Fee;
 
 use crate::errors::StarknetOsError;
 use crate::hint_processor::os_logger::OsLogger;
@@ -14,6 +19,7 @@ pub struct OsExecutionHelper<S: StateReader> {
     pub(crate) cached_state: CachedState<S>,
     pub(crate) os_input: StarknetOsInput,
     pub(crate) os_logger: OsLogger,
+    tx_execution_info_iter: TxExecutionInfoIter,
 }
 
 impl<S: StateReader> OsExecutionHelper<S> {
@@ -27,7 +33,15 @@ impl<S: StateReader> OsExecutionHelper<S> {
             cached_state: Self::initialize_cached_state(state_reader, state_input)?,
             os_input,
             os_logger: OsLogger::new(debug_mode),
+            tx_execution_info_iter: TxExecutionInfoIter::new(),
         })
+    }
+
+    pub(crate) fn next_tx_execution_infos(
+        &mut self,
+        tx_type: TransactionType,
+    ) -> Option<(TransactionExecutionInfoForExecutionHelper, IntoIter<CallInfo>)> {
+        self.tx_execution_info_iter.next(&self.os_input.tx_execution_infos, tx_type)
     }
 
     fn initialize_cached_state(
@@ -66,6 +80,48 @@ impl OsExecutionHelper<DictStateReader> {
             cached_state: CachedState::from(state_reader),
             os_input,
             os_logger: OsLogger::new(true),
+            tx_execution_info_iter: TxExecutionInfoIter::new(),
+        }
+    }
+}
+
+struct TxExecutionInfoIter {
+    next_tx_execution_infos_index: usize,
+}
+
+impl TxExecutionInfoIter {
+    pub fn new() -> Self {
+        Self { next_tx_execution_infos_index: 0 }
+    }
+
+    pub fn next(
+        &mut self,
+        tx_execution_infos: &[CentralTransactionExecutionInfo],
+        tx_type: TransactionType,
+    ) -> Option<(TransactionExecutionInfoForExecutionHelper, IntoIter<CallInfo>)> {
+        let tx_execution_info = tx_execution_infos.get(self.next_tx_execution_infos_index)?;
+        self.next_tx_execution_infos_index += 1;
+        // TODO(Yoav): See if we can avoid cloning here.
+        Some((
+            tx_execution_info.into(),
+            tx_execution_info.call_info_iter(tx_type).cloned().collect::<Vec<_>>().into_iter(),
+        ))
+    }
+}
+
+#[derive(Clone)]
+pub struct TransactionExecutionInfoForExecutionHelper {
+    pub actual_fee: Fee,
+    pub is_reverted: bool,
+}
+
+impl From<&CentralTransactionExecutionInfo> for TransactionExecutionInfoForExecutionHelper {
+    fn from(
+        tx_execution_info: &CentralTransactionExecutionInfo,
+    ) -> TransactionExecutionInfoForExecutionHelper {
+        TransactionExecutionInfoForExecutionHelper {
+            actual_fee: tx_execution_info.actual_fee,
+            is_reverted: tx_execution_info.revert_error.is_some(),
         }
     }
 }
