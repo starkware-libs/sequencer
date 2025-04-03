0ad6f1285418fba47e00aa56263bc4bce87d5379
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-04-03 10:19:42 +0300

    chore(apollo_deployments): add integration base app config (#5529)

--- crates/apollo_deployments/src/deployment_definitions.rs
+++ crates/apollo_deployments/src/deployment_definitions.rs
@@ -10,17 +10,35 @@ use crate::service::DeploymentName;
 #[path = "deployment_definitions_test.rs"]
 mod deployment_definitions_test;
 
+// TODO(Tsabary): separate deployments to different modules.
+
 const SYSTEM_TEST_BASE_APP_CONFIG_PATH: &str =
     "config/sequencer/testing/base_app_configs/single_node_deployment_test.json";
+
+const INTEGRATION_BASE_APP_CONFIG_PATH: &str =
+    "config/sequencer/sepolia_integration/base_app_configs/config.json";
 
 const CONFIG_BASE_DIR: &str = "config/sequencer/";
 const DEPLOYMENT_CONFIG_DIR_NAME: &str = "deployment_configs/";
 
 type DeploymentFn = fn() -> DeploymentAndPreset;
 
-pub const DEPLOYMENTS: &[DeploymentFn] =
-    &[system_test_distributed_deployment, system_test_consolidated_deployment];
+pub const DEPLOYMENTS: &[DeploymentFn] = &[
+    system_test_distributed_deployment,
+    system_test_consolidated_deployment,
+    integration_consolidated_deployment,
+];
 
+// Integration deployments
+fn integration_consolidated_deployment() -> DeploymentAndPreset {
+    DeploymentAndPreset::new(
+        Deployment::new(ChainId::IntegrationSepolia, DeploymentName::ConsolidatedNode),
+        deployment_file_path(Environment::SepoliaIntegration, "integration_consolidated"),
+        INTEGRATION_BASE_APP_CONFIG_PATH,
+    )
+}
+
+// System test deployments
 fn system_test_distributed_deployment() -> DeploymentAndPreset {
     DeploymentAndPreset::new(
         Deployment::new(ChainId::IntegrationSepolia, DeploymentName::DistributedNode),
