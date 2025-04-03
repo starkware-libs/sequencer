35265fef4ff3e17244180e4abb17a412bf429508
YaelD <70628564+Yael-Starkware@users.noreply.github.com>
2025-04-03 14:36:33 +0300

    chore(apollo_http_server): record metrics for serde failure (#5469)

--- crates/apollo_http_server/src/metrics.rs
+++ crates/apollo_http_server/src/metrics.rs
@@ -6,6 +6,7 @@ use tracing::info;
 #[path = "metrics_test.rs"]
 pub mod metrics_test;
 
+// TODO(Yael): consider adding labels for different endpoints.
 define_metrics!(
     HttpServer => {
         MetricCounter { ADDED_TRANSACTIONS_TOTAL, "http_server_added_transactions_total", "Total number of transactions added", init = 0 },
@@ -19,17 +20,4 @@ pub(crate) fn init_metrics() {
     ADDED_TRANSACTIONS_TOTAL.register();
     ADDED_TRANSACTIONS_SUCCESS.register();
     ADDED_TRANSACTIONS_FAILURE.register();
-}
-
-// TODO(Yael): add label for rpc/rest transaction
-pub(crate) fn record_added_transaction() {
-    ADDED_TRANSACTIONS_TOTAL.increment(1);
-}
-
-pub(crate) fn record_added_transaction_status(add_tx_success: bool) {
-    if add_tx_success {
-        ADDED_TRANSACTIONS_SUCCESS.increment(1);
-    } else {
-        ADDED_TRANSACTIONS_FAILURE.increment(1);
-    }
 }
