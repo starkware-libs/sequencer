60f6e635f06d63bd6776d8758701df48466d7912
YaelD <70628564+Yael-Starkware@users.noreply.github.com>
2025-04-03 11:47:13 +0300

    test(apollo_http_server): add the second endpoint to metrics test (#5539)

--- crates/apollo_http_server/src/metrics_test.rs
+++ crates/apollo_http_server/src/metrics_test.rs
@@ -5,6 +5,7 @@ use apollo_gateway_types::gateway_types::{GatewayOutput, InvokeGatewayOutput};
 use apollo_infra::component_client::ClientError;
 use apollo_infra_utils::test_utils::{AvailablePorts, TestIdentifier};
 use metrics_exporter_prometheus::PrometheusBuilder;
+use rstest::rstest;
 use starknet_api::transaction::TransactionHash;
 
 use crate::config::HttpServerConfig;
@@ -13,11 +14,18 @@ use crate::metrics::{
     ADDED_TRANSACTIONS_SUCCESS,
     ADDED_TRANSACTIONS_TOTAL,
 };
-use crate::test_utils::{http_client_server_setup, rpc_tx};
+use crate::test_utils::{
+    deprecated_gateway_tx,
+    http_client_server_setup,
+    rpc_tx,
+    GatewayTransaction,
+};
 
-// TODO(Yael): add a test for rest_api endpoint
+#[rstest]
+#[case::add_deprecated_gateway_tx(0, deprecated_gateway_tx())]
+#[case::add_rpc_tx(1, rpc_tx())]
 #[tokio::test]
-async fn get_metrics_test() {
+async fn add_tx_metrics_test(#[case] index: u16, #[case] tx: impl GatewayTransaction) {
     // Create a mock gateway client that returns a successful response and a failure response.
     const SUCCESS_TXS_TO_SEND: usize = 1;
     const FAILURE_TXS_TO_SEND: usize = 1;
@@ -41,13 +49,13 @@ async fn get_metrics_test() {
 
     let ip = IpAddr::from(Ipv4Addr::LOCALHOST);
     let mut available_ports = AvailablePorts::new(TestIdentifier::HttpServerUnitTests.into(), 0);
-    let http_server_config = HttpServerConfig { ip, port: available_ports.get_next_port() };
+    let http_server_config = HttpServerConfig { ip, port: available_ports.get_next_port() + index };
     let add_tx_http_client =
         http_client_server_setup(mock_gateway_client, http_server_config).await;
 
     // Send transactions to the server.
     for _ in std::iter::repeat(()).take(SUCCESS_TXS_TO_SEND + FAILURE_TXS_TO_SEND) {
-        add_tx_http_client.add_tx(rpc_tx()).await;
+        add_tx_http_client.add_tx(tx.clone()).await;
     }
 
     // Obtain and parse metrics.
