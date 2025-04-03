e817e917e8ca10fc1fd7ef40b18dedd07d6d0d8f
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-04-03 09:06:54 +0300

    chore(apollo_deployments): add base app config file for system test (#5528)

--- crates/apollo_deployments/src/deployment_definitions.rs
+++ crates/apollo_deployments/src/deployment_definitions.rs
@@ -10,9 +10,9 @@ use crate::service::DeploymentName;
 #[path = "deployment_definitions_test.rs"]
 mod deployment_definitions_test;
 
-// TODO(Tsabary): temporarily moved this definition here, to include it in the deployment.
-pub const SINGLE_NODE_CONFIG_PATH: &str =
-    "config/sequencer/presets/system_test_presets/single_node/node_0/executable_0/node_config.json";
+const SYSTEM_TEST_BASE_APP_CONFIG_PATH: &str =
+    "config/sequencer/testing/base_app_configs/single_node_deployment_test.json";
+
 const CONFIG_BASE_DIR: &str = "config/sequencer/";
 const DEPLOYMENT_CONFIG_DIR_NAME: &str = "deployment_configs/";
 
@@ -25,7 +25,7 @@ fn system_test_distributed_deployment() -> DeploymentAndPreset {
     DeploymentAndPreset::new(
         Deployment::new(ChainId::IntegrationSepolia, DeploymentName::DistributedNode),
         deployment_file_path(Environment::Testing, "deployment_test_distributed"),
-        SINGLE_NODE_CONFIG_PATH,
+        SYSTEM_TEST_BASE_APP_CONFIG_PATH,
     )
 }
 
@@ -33,7 +33,7 @@ fn system_test_consolidated_deployment() -> DeploymentAndPreset {
     DeploymentAndPreset::new(
         Deployment::new(ChainId::IntegrationSepolia, DeploymentName::ConsolidatedNode),
         deployment_file_path(Environment::Testing, "deployment_test_consolidated"),
-        SINGLE_NODE_CONFIG_PATH,
+        SYSTEM_TEST_BASE_APP_CONFIG_PATH,
     )
 }
 
