75d1dc4eabafb44d3cd864ebbca4392e17a9c595
Arni Hod <arnon@starkware.co>
2025-04-06 09:08:21 +0300

    feat(papyrus_base_layer_client): send actual L1 handler transactions to Sepolia

---
+++ crates/papyrus_base_layer_client/src/send_l1_tx_utils/secretes.rs
@@ -0,0 +1,5 @@
+// Fill in these values:
+
+pub const SEPOLIA_RPC_URL: &str = "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID";
+pub const PRIVATE_KEY: &str = "YOUR_PRIVATE_KEY_FROM_METAMASK";
+pub const L1_SENDER_ADDRESS: &str = "YOUR_L1_SENDER_ADDRESS";
