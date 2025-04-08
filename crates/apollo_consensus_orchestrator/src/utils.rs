0850e3dd7a9d641d17ab5a5270997a139c13cda9
Alon Lukatch <alon.l@starkware.co>
2025-04-08 16:59:33 +0300

    test(apollo_consensus_orchestrator): oracle fails on second block leads to previous block values

--- crates/apollo_consensus_orchestrator/src/utils.rs
+++ crates/apollo_consensus_orchestrator/src/utils.rs
@@ -18,28 +18,22 @@ pub(crate) async fn get_oracle_rate_and_prices(
         eth_to_strk_oracle_client.eth_to_fri_rate(timestamp),
         l1_gas_price_provider_client.get_price_info(BlockTimestamp(timestamp))
     );
-    match (eth_to_strk_rate, price_info) {
-        (Ok(eth_to_strk_rate), Ok(price_info)) => (eth_to_strk_rate, price_info),
-        _ => {
-            warn!("Failed to get oracle prices, using values from previous block info");
-            if let Some(previous_block_info) = previous_block_info {
-                (
-                    previous_block_info.eth_to_fri_rate,
-                    PriceInfo {
-                        base_fee_per_gas: min_l1_gas_price,
-                        blob_fee: min_l1_data_gas_price,
-                    },
-                )
-            } else {
-                warn!("No previous block info available, using minimum values");
-                (
-                    default_eth_to_strk_rate,
-                    PriceInfo {
-                        base_fee_per_gas: min_l1_gas_price,
-                        blob_fee: min_l1_data_gas_price,
-                    },
-                )
-            }
-        }
+
+    if let (Ok(eth_to_strk_rate), Ok(price_info)) = (eth_to_strk_rate, price_info) {
+        return (eth_to_strk_rate, price_info);
     }
+    warn!("Failed to get oracle prices, using values from previous block info");
+
+    if let Some(previous_block_info) = previous_block_info {
+        return (
+            previous_block_info.eth_to_fri_rate,
+            PriceInfo { base_fee_per_gas: min_l1_gas_price, blob_fee: min_l1_data_gas_price },
+        );
+    }
+    warn!("No previous block info available, using minimum values");
+
+    (
+        default_eth_to_strk_rate,
+        PriceInfo { base_fee_per_gas: min_l1_gas_price, blob_fee: min_l1_data_gas_price },
+    )
 }
