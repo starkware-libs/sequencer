9b4939280468df6e4153f84bb49aacf84f96d137
avivg-starkware <aviv.g@starkware.co>
2025-04-08 14:57:46 +0300

    refactor(blockifier): add fn entry_point_for_deploy for code share (#5659)

--- crates/blockifier/src/execution/syscalls/syscall_tests/deploy.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/deploy.rs
@@ -6,7 +6,6 @@ use blockifier_test_utils::contracts::FeatureContract;
 use cairo_vm::types::builtin_name::BuiltinName;
 use pretty_assertions::assert_eq;
 use rstest::rstest;
-use starknet_api::abi::abi_utils::selector_from_name;
 use starknet_api::contract_class::SierraVersion;
 use starknet_api::core::calculate_contract_address;
 use starknet_api::transaction::fields::{Calldata, ContractAddressSalt, Fee};
@@ -16,13 +15,12 @@ use test_case::test_case;
 use crate::context::{BlockContext, ChainInfo};
 use crate::execution::call_info::CallExecution;
 use crate::execution::common_hints::ExecutionMode;
-use crate::execution::entry_point::CallEntryPoint;
 use crate::execution::syscalls::hint_processor::SyscallUsage;
 use crate::execution::syscalls::SyscallSelector;
 use crate::retdata;
 use crate::state::state_api::StateReader;
+use crate::test_utils::create_deploy_entry_point;
 use crate::test_utils::initial_test_state::test_state;
-use crate::test_utils::{calldata_for_deploy_test, trivial_external_entry_point_new};
 use crate::transaction::objects::{CurrentTransactionInfo, TransactionInfo};
 
 #[test_case(RunnableCairo1::Casm;"VM")]
@@ -39,12 +37,7 @@ fn no_constructor(runnable_version: RunnableCairo1) {
         &[(deployer_contract, 1), (empty_contract, 0)],
     );
 
-    let calldata = calldata_for_deploy_test(class_hash, &[], true);
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(deployer_contract)
-    };
+    let entry_point_call = create_deploy_entry_point(class_hash, &[], true, deployer_contract);
 
     let deploy_call = &entry_point_call.execute_directly(&mut state).unwrap();
     assert_eq!(
@@ -83,13 +76,8 @@ fn no_constructor_nonempty_calldata(runnable_version: RunnableCairo1) {
         &[(deployer_contract, 1), (empty_contract, 0)],
     );
 
-    let calldata = calldata_for_deploy_test(class_hash, &[felt!(1_u8), felt!(1_u8)], true);
-
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(deployer_contract)
-    };
+    let entry_point_call =
+        create_deploy_entry_point(class_hash, &[felt!(1_u8), felt!(1_u8)], true, deployer_contract);
 
     let error = entry_point_call.execute_directly(&mut state).unwrap_err().to_string();
     assert!(error.contains(
@@ -110,13 +98,8 @@ fn with_constructor(runnable_version: RunnableCairo1) {
         felt!(1_u8), // Calldata: value.
     ];
 
-    let calldata = calldata_for_deploy_test(class_hash, &constructor_calldata, true);
-
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(deployer_contract)
-    };
+    let entry_point_call =
+        create_deploy_entry_point(class_hash, &constructor_calldata, true, deployer_contract);
 
     // No errors expected.
     let contract_address = calculate_contract_address(
@@ -162,13 +145,8 @@ fn to_unavailable_address(runnable_version: RunnableCairo1) {
         felt!(1_u8), // Calldata: value.
     ];
 
-    let calldata = calldata_for_deploy_test(class_hash, &constructor_calldata, true);
-
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(deployer_contract)
-    };
+    let entry_point_call =
+        create_deploy_entry_point(class_hash, &constructor_calldata, true, deployer_contract);
 
     entry_point_call.clone().execute_directly(&mut state).unwrap();
     let error = entry_point_call.execute_directly(&mut state).unwrap_err().to_string();
@@ -204,43 +182,40 @@ fn calldata_length(cairo_version: CairoVersion) {
 
     // Deploy account contract.
     let account_constructor_calldata = vec![felt!(0_u8)];
-    let calldata = calldata_for_deploy_test(
+
+    let deploy_account_call = create_deploy_entry_point(
         account_contract.get_class_hash(),
         &account_constructor_calldata,
         deploy_from_zero,
+        test_contract,
     );
-    let deploy_account_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(test_contract)
-    };
+
     let deploy_account_call_info = &deploy_account_call
         .execute_directly_given_block_context(&mut state, block_context.clone())
         .unwrap();
 
     // Deploy test contract.
     let test_constructor_calldata = vec![felt!(1_u8), felt!(1_u8)];
-    let calldata = calldata_for_deploy_test(
+
+    let deploy_test_call = create_deploy_entry_point(
         test_contract.get_class_hash(),
         &test_constructor_calldata,
         deploy_from_zero,
+        test_contract,
     );
-    let deploy_test_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(test_contract)
-    };
+
     let deploy_test_call_info = deploy_test_call
         .execute_directly_given_block_context(&mut state, block_context.clone())
         .unwrap();
 
     // Deploy empty contract.
-    let calldata = calldata_for_deploy_test(empty_contract.get_class_hash(), &[], deploy_from_zero);
-    let deploy_empty_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(test_contract)
-    };
+    let deploy_empty_call = create_deploy_entry_point(
+        empty_contract.get_class_hash(),
+        &[],
+        deploy_from_zero,
+        test_contract,
+    );
+
     let deploy_empty_call_info = deploy_empty_call
         .execute_directly_given_block_context(&mut state, block_context.clone())
         .unwrap();
@@ -328,12 +303,7 @@ fn disable_deploy_in_validate_mode_flag_behavior(
         &[(deployer_contract, 1), (empty_contract, 0)],
     );
 
-    let calldata = calldata_for_deploy_test(class_hash, &[], true);
-    let entry_point_call = CallEntryPoint {
-        entry_point_selector: selector_from_name("test_deploy"),
-        calldata,
-        ..trivial_external_entry_point_new(deployer_contract)
-    };
+    let entry_point_call = create_deploy_entry_point(class_hash, &[], true, deployer_contract);
 
     let limit_steps_by_resources = false;
     let res = entry_point_call.clone().execute_directly_given_tx_info(
