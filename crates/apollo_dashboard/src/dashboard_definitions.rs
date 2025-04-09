ee18270df4ebe9ff63626adb98d2faee957c7309
AvivYossef-starkware <aviv.y@starkware.co>
2025-04-09 09:55:05 +0300

    fix(apollo_state_reader): fix the query definition.

--- crates/apollo_dashboard/src/dashboard_definitions.rs
+++ crates/apollo_dashboard/src/dashboard_definitions.rs
@@ -277,7 +277,7 @@ const PANEL_APOLLO_STATE_READER_CLASS_CACHE_MISS_RATIO: Panel = Panel::new(
     "class_cache_miss_ratio",
     "The ratio of cache misses when requesting compiled classes from the apollo state reader",
     formatcp!(
-        "100 * ({} / max(({} + {}), 1))",
+        "100 * ({} / clamp_min(({} + {}), 1))",
         CLASS_CACHE_MISSES.get_name(),
         CLASS_CACHE_MISSES.get_name(),
         CLASS_CACHE_HITS.get_name()
@@ -288,7 +288,7 @@ const PANEL_APOLLO_STATE_READER_NATIVE_CLASS_RETURNED_RATIO: Panel = Panel::new(
     "native_class_returned_ratio",
     "The ratio of Native classes returned by the apollo state reader",
     formatcp!(
-        "100 * ({} / max(({} + {}), 1))",
+        "100 * ({} / clamp_min(({} + {}), 1))",
         NATIVE_CLASS_RETURNED.get_name(),
         CLASS_CACHE_HITS.get_name(),
         CLASS_CACHE_MISSES.get_name()
