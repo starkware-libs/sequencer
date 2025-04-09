75ff999a03e0c199df04218e609442e19d3b43df
Dori Medini <dori@starkware.co>
2025-04-09 11:56:34 +0300

    feat(apollo_integration_tests): inject validate_non_zero_resource_bounds boolean
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_integration_tests/tests/end_to_end_flow_test.rs
+++ crates/apollo_integration_tests/tests/end_to_end_flow_test.rs
@@ -47,18 +47,21 @@ fn tx_generator() -> MultiAccountTransactionGenerator {
     TestIdentifier::EndToEndFlowTest,
     create_test_scenarios(),
     GasAmount(29000000),
-    false
+    false,
+    true
 )]
 #[case::many_txs_scenario(
     TestIdentifier::EndToEndFlowTestManyTxs,
     create_many_txs_scenario(),
     GasAmount(17500000),
+    true,
     true
 )]
 #[case::bootstrap_declare_scenario(
     TestIdentifier::EndToEndFlowTestBootstrapDeclare,
     create_bootstrap_declare_scenario(),
     GasAmount(29000000),
+    false,
     false
 )]
 #[tokio::test]
@@ -68,6 +71,7 @@ async fn end_to_end_flow(
     #[case] test_blocks_scenarios: Vec<TestScenario>,
     #[case] block_max_capacity_sierra_gas: GasAmount,
     #[case] expecting_full_blocks: bool,
+    #[case] block_bootstrap_txs: bool,
 ) {
     configure_tracing().await;
     let recorder = PrometheusBuilder::new().build_recorder();
@@ -79,6 +83,7 @@ async fn end_to_end_flow(
         &tx_generator,
         test_identifier.into(),
         block_max_capacity_sierra_gas,
+        block_bootstrap_txs,
     )
     .await;
 
