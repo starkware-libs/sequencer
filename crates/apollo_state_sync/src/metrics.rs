fe112e0290d1801cbfd68c667e1181189349a975
Noam Spiegelstein <noam.s@starkware.co>
2025-04-09 15:55:47 +0300

    fix(apollo_state_sync): fix metric names

--- crates/apollo_state_sync/src/metrics.rs
+++ crates/apollo_state_sync/src/metrics.rs
@@ -4,9 +4,9 @@ use apollo_metrics::metrics::MetricGauge;
 define_metrics!(
     StateSync => {
         // Gauges
-        MetricGauge { STATE_SYNC_P2P_NUM_CONNECTED_PEERS, "apollo_central_sync_num_connected_peers", "The number of connected peers to the state sync p2p component" },
-        MetricGauge { STATE_SYNC_P2P_NUM_ACTIVE_INBOUND_SESSIONS, "apollo_central_sync_num_active_inbound_sessions", "The number of inbound sessions to the state sync p2p component" },
-        MetricGauge { STATE_SYNC_P2P_NUM_ACTIVE_OUTBOUND_SESSIONS, "apollo_central_sync_num_active_outbound_sessions", "The number of outbound sessions to the state sync p2p component" },
-        MetricGauge { STATE_SYNC_P2P_NUM_BLACKLISTED_PEERS, "apollo_central_sync_num_blacklisted_peers", "The number of currently blacklisted peers by the state sync p2p component" },
+        MetricGauge { P2P_SYNC_NUM_CONNECTED_PEERS, "apollo_p2p_sync_num_connected_peers", "The number of connected peers to the p2p sync component" },
+        MetricGauge { P2P_SYNC_NUM_ACTIVE_INBOUND_SESSIONS, "apollo_p2p_sync_num_active_inbound_sessions", "The number of inbound sessions to the p2p sync component" },
+        MetricGauge { P2P_SYNC_NUM_ACTIVE_OUTBOUND_SESSIONS, "apollo_p2p_sync_num_active_outbound_sessions", "The number of outbound sessions to the p2p sync component" },
+        MetricGauge { P2P_SYNC_NUM_BLACKLISTED_PEERS, "apollo_p2p_sync_num_blacklisted_peers", "The number of currently blacklisted peers by the p2p sync component" },
     },
 );
