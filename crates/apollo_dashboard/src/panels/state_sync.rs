46cd000653a1e015e68f2a5c3c768b00fa9aed1e
nadin-Starkware <nadin@starkware.co>
2025-04-10 15:49:48 +0300

    refactor(apollo_dashboard): move state sync panels to seperate module

---
+++ crates/apollo_dashboard/src/panels/state_sync.rs
@@ -0,0 +1,35 @@
+use apollo_infra::metrics::{
+    STATE_SYNC_LOCAL_MSGS_PROCESSED,
+    STATE_SYNC_LOCAL_MSGS_RECEIVED,
+    STATE_SYNC_LOCAL_QUEUE_DEPTH,
+    STATE_SYNC_REMOTE_MSGS_PROCESSED,
+    STATE_SYNC_REMOTE_MSGS_RECEIVED,
+    STATE_SYNC_REMOTE_VALID_MSGS_RECEIVED,
+};
+use apollo_state_sync::metrics::{
+    STATE_SYNC_P2P_NUM_ACTIVE_INBOUND_SESSIONS,
+    STATE_SYNC_P2P_NUM_ACTIVE_OUTBOUND_SESSIONS,
+    STATE_SYNC_P2P_NUM_CONNECTED_PEERS,
+};
+
+use crate::dashboard::{Panel, PanelType};
+
+pub(crate) const PANEL_STATE_SYNC_LOCAL_MSGS_RECEIVED: Panel =
+    Panel::from_counter(STATE_SYNC_LOCAL_MSGS_RECEIVED, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_LOCAL_MSGS_PROCESSED: Panel =
+    Panel::from_counter(STATE_SYNC_LOCAL_MSGS_PROCESSED, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_REMOTE_MSGS_RECEIVED: Panel =
+    Panel::from_counter(STATE_SYNC_REMOTE_MSGS_RECEIVED, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_REMOTE_VALID_MSGS_RECEIVED: Panel =
+    Panel::from_counter(STATE_SYNC_REMOTE_VALID_MSGS_RECEIVED, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_REMOTE_MSGS_PROCESSED: Panel =
+    Panel::from_counter(STATE_SYNC_REMOTE_MSGS_PROCESSED, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_LOCAL_QUEUE_DEPTH: Panel =
+    Panel::from_gauge(STATE_SYNC_LOCAL_QUEUE_DEPTH, PanelType::Stat);
+
+pub(crate) const PANEL_STATE_SYNC_P2P_NUM_CONNECTED_PEERS: Panel =
+    Panel::from_gauge(STATE_SYNC_P2P_NUM_CONNECTED_PEERS, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_P2P_NUM_ACTIVE_INBOUND_SESSIONS: Panel =
+    Panel::from_gauge(STATE_SYNC_P2P_NUM_ACTIVE_INBOUND_SESSIONS, PanelType::Stat);
+pub(crate) const PANEL_STATE_SYNC_P2P_NUM_ACTIVE_OUTBOUND_SESSIONS: Panel =
+    Panel::from_gauge(STATE_SYNC_P2P_NUM_ACTIVE_OUTBOUND_SESSIONS, PanelType::Stat);
