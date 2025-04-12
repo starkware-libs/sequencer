046e6d597e1eb381317f04bf113e3f4ce5a3abaa
Noa Oved <noa@starkware.co>
2025-04-12 11:40:47 +0300

    chore(blockifier): add meta tx v0 syscall to native

--- crates/blockifier/src/execution/syscalls/hint_processor.rs
+++ crates/blockifier/src/execution/syscalls/hint_processor.rs
@@ -1,6 +1,5 @@
 use std::any::Any;
 use std::collections::HashMap;
-use std::sync::Arc;
 
 use cairo_lang_casm::hints::{Hint, StarknetHint};
 use cairo_lang_runner::casm_run::execute_core_hint_base;
@@ -17,22 +16,14 @@ use cairo_vm::vm::vm_core::VirtualMachine;
 use num_traits::ToPrimitive;
 use starknet_api::block::BlockHash;
 use starknet_api::contract_class::EntryPointType;
-use starknet_api::core::{ClassHash, ContractAddress, EntryPointSelector, Nonce};
+use starknet_api::core::{ClassHash, ContractAddress, EntryPointSelector};
 use starknet_api::execution_resources::GasAmount;
 use starknet_api::transaction::fields::{
     AllResourceBounds,
     Calldata,
-    Fee,
     Resource,
     ResourceBounds,
     ValidResourceBounds,
-};
-use starknet_api::transaction::{
-    signed_tx_version,
-    InvokeTransactionV0,
-    TransactionHasher,
-    TransactionOptions,
-    TransactionVersion,
 };
 use starknet_api::StarknetApiError;
 use starknet_types_core::felt::{Felt, FromStrError};
@@ -40,7 +31,6 @@ use thiserror::Error;
 
 use crate::abi::sierra_types::SierraTypeError;
 use crate::blockifier_versioned_constants::{GasCosts, SyscallGasCost};
-use crate::context::TransactionContext;
 use crate::execution::common_hints::{ExecutionMode, HintExecutionResult};
 use crate::execution::contract_class::TrackedResource;
 use crate::execution::entry_point::{
@@ -119,12 +109,7 @@ use crate::execution::syscalls::{
 };
 use crate::state::errors::StateError;
 use crate::state::state_api::State;
-use crate::transaction::objects::{
-    CommonAccountFields,
-    CurrentTransactionInfo,
-    DeprecatedTransactionInfo,
-    TransactionInfo,
-};
+use crate::transaction::objects::{CurrentTransactionInfo, TransactionInfo};
 
 #[derive(Clone, Debug, Default)]
 pub struct SyscallUsage {
@@ -789,73 +774,17 @@ impl SyscallExecutor for SyscallHintProcessor<'_> {
             request.get_linear_factor_length(),
         );
 
-        if syscall_handler.is_validate_mode() {
-            return Err(SyscallExecutionError::InvalidSyscallInExecutionMode {
-                syscall_name: "meta_tx_v0".to_string(),
-                execution_mode: syscall_handler.execution_mode(),
-            });
-        }
-
         let storage_address = request.contract_address;
         let selector = request.entry_point_selector;
-        let class_hash = syscall_handler.base.state.get_class_hash_at(storage_address)?;
-        let entry_point = CallEntryPoint {
-            class_hash: None,
-            code_address: Some(storage_address),
-            entry_point_type: EntryPointType::External,
-            entry_point_selector: selector,
-            calldata: request.calldata.clone(),
+
+        let raw_retdata = syscall_handler.base.meta_tx_v0(
             storage_address,
-            caller_address: ContractAddress::default(),
-            call_type: CallType::Call,
-            // NOTE: this value might be overridden later on.
-            initial_gas: *remaining_gas,
-        };
-
-        let old_tx_context = syscall_handler.base.context.tx_context.clone();
-        let only_query = old_tx_context.tx_info.only_query();
-
-        // Compute meta-transaction hash.
-        let transaction_hash = InvokeTransactionV0 {
-            max_fee: Fee(0),
-            signature: request.signature.clone(),
-            contract_address: storage_address,
-            entry_point_selector: selector,
-            calldata: request.calldata,
-        }
-        .calculate_transaction_hash(
-            &syscall_handler.base.context.tx_context.block_context.chain_info.chain_id,
-            &signed_tx_version(&TransactionVersion::ZERO, &TransactionOptions { only_query }),
+            selector,
+            request.calldata,
+            request.signature,
+            remaining_gas,
         )?;
-
-        // Replace `tx_context`.
-        let new_tx_info = TransactionInfo::Deprecated(DeprecatedTransactionInfo {
-            common_fields: CommonAccountFields {
-                transaction_hash,
-                version: TransactionVersion::ZERO,
-                signature: request.signature,
-                nonce: Nonce(0.into()),
-                sender_address: storage_address,
-                only_query,
-            },
-            max_fee: Fee(0),
-        });
-        syscall_handler.base.context.tx_context = Arc::new(TransactionContext {
-            block_context: old_tx_context.block_context.clone(),
-            tx_info: new_tx_info,
-        });
-
-        let retdata_segment = execute_inner_call(entry_point, vm, syscall_handler, remaining_gas)
-            .map_err(|error| match error {
-            SyscallExecutionError::Revert { .. } => error,
-            _ => {
-                // TODO(lior): Change to meta-tx specific error.
-                error.as_call_contract_execution_error(class_hash, storage_address, selector)
-            }
-        })?;
-
-        // Restore the old `tx_context`.
-        syscall_handler.base.context.tx_context = old_tx_context;
+        let retdata_segment = create_retdata_segment(vm, syscall_handler, &raw_retdata)?;
 
         Ok(MetaTxV0Response { segment: retdata_segment })
     }
