8a51815a8a6c43c1bb39c57c9a1038d680c82c42
Dori Medini <dori@starkware.co>
2025-04-13 14:03:59 +0300

    feat(apollo_starknet_os_program): define and implement static Program

--- crates/apollo_starknet_os_program/src/lib.rs
+++ crates/apollo_starknet_os_program/src/lib.rs
@@ -1,10 +1,19 @@
 #[cfg(feature = "dump_source_files")]
 use std::collections::HashMap;
-#[cfg(feature = "dump_source_files")]
 use std::sync::LazyLock;
+
+use cairo_vm::types::program::Program;
 
 #[cfg(feature = "dump_source_files")]
 pub static CAIRO_FILES_MAP: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
     serde_json::from_str(include_str!(concat!(env!("OUT_DIR"), "/cairo_files_map.json")))
         .unwrap_or_else(|error| panic!("Failed to deserialize cairo_files_map.json: {error:?}."))
+});
+
+pub static OS_PROGRAM: LazyLock<Program> = LazyLock::new(|| {
+    Program::from_bytes(
+        include_bytes!(concat!(env!("OUT_DIR"), "/starknet_os_bytes")),
+        Some("main"),
+    )
+    .expect("Failed to load the OS bytes.")
 });
