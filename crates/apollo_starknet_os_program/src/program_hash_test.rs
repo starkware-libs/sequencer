213b1bcfec7ef43f70f40566c8bdef8cd8a4212d
Dori Medini <dori@starkware.co>
2025-04-13 17:13:27 +0300

    feat(apollo_starknet_os_program): add fixer for program hash test

--- crates/apollo_starknet_os_program/src/program_hash_test.rs
+++ crates/apollo_starknet_os_program/src/program_hash_test.rs
@@ -1,7 +1,22 @@
-use crate::program_hash::compute_os_program_hash;
+use crate::program_hash::{compute_os_program_hash, ProgramHash, PROGRAM_HASH_PATH};
 use crate::PROGRAM_HASH;
 
+/// Asserts the program hash of the compiled Starknet OS program matches the program hash in the
+/// JSON.
+/// To fix this test, run the following command:
+/// ```bash
+/// FIX_PROGRAM_HASH=1 cargo test -p apollo_starknet_os_program test_program_hash
+/// ```
 #[test]
 fn test_program_hash() {
-    assert_eq!(compute_os_program_hash().unwrap(), PROGRAM_HASH.os)
+    let computed_hash = ProgramHash { os: compute_os_program_hash().unwrap() };
+    if std::env::var("FIX_PROGRAM_HASH").is_ok() {
+        std::fs::write(
+            PROGRAM_HASH_PATH.as_path(),
+            serde_json::to_string_pretty(&computed_hash).unwrap(),
+        )
+        .expect("Failed to write the program hash file.");
+    } else {
+        assert_eq!(computed_hash, *PROGRAM_HASH);
+    }
 }
