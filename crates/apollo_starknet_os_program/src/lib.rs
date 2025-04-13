85985d9267f0b3f50e73e9ee5cd792b785a22d01
Dori Medini <dori@starkware.co>
2025-04-13 16:56:13 +0300

    feat(apollo_starknet_os_program): define and implement static ProgramHash

--- crates/apollo_starknet_os_program/src/lib.rs
+++ crates/apollo_starknet_os_program/src/lib.rs
@@ -2,10 +2,19 @@ use std::sync::LazyLock;
 
 use cairo_vm::types::program::Program;
 
+use crate::program_hash::ProgramHash;
+
+pub mod program_hash;
+
 pub static OS_PROGRAM: LazyLock<Program> = LazyLock::new(|| {
     Program::from_bytes(
         include_bytes!(concat!(env!("OUT_DIR"), "/starknet_os_bytes")),
         Some("main"),
     )
     .expect("Failed to load the OS bytes.")
+});
+
+pub static PROGRAM_HASH: LazyLock<ProgramHash> = LazyLock::new(|| {
+    serde_json::from_str(include_str!("program_hash.json"))
+        .expect("Failed to deserialize program_hash.json.")
 });
