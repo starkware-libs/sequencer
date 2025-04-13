0a8908f6e7dfa06522497e0bd1d2bf6db41c32c5
Dori Medini <dori@starkware.co>
2025-04-13 14:03:59 +0300

    feat(apollo_starknet_os_program): define and implement static Program

--- crates/apollo_infra_utils/src/cairo0_compiler.rs
+++ crates/apollo_infra_utils/src/cairo0_compiler.rs
@@ -1,4 +1,3 @@
-#[cfg(any(test, feature = "testing"))]
 use std::path::PathBuf;
 use std::process::Command;
 #[cfg(any(test, feature = "testing"))]
@@ -22,10 +21,26 @@ pub(crate) static PIP_REQUIREMENTS_FILE: LazyLock<PathBuf> =
 
 #[derive(thiserror::Error, Debug)]
 pub enum Cairo0CompilerVersionError {
+    #[error("{0} not found.")]
+    CompilerNotFound(String),
     #[error("{compiler} version is not correct: required {required}, got {existing}.")]
     IncorrectVersion { compiler: String, existing: String, required: String },
-    #[error("{0} not found.")]
-    NotFound(String),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Cairo0CompilerError {
+    #[error(transparent)]
+    Cairo0CompilerVersion(#[from] Cairo0CompilerVersionError),
+    #[error("Cairo root path not found at {0:?}.")]
+    CairoRootNotFound(PathBuf),
+    #[error("Failed to compile the program. Error: {0}.")]
+    CompileError(String),
+    #[error("Invalid path unicode: {0:?}.")]
+    InvalidPath(PathBuf),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("No file found at path {0:?}.")]
+    SourceFileNotFound(PathBuf),
 }
 
 pub fn cairo0_compilers_correct_version() -> Result<(), Cairo0CompilerVersionError> {
@@ -33,18 +48,14 @@ pub fn cairo0_compilers_correct_version() -> Result<(), Cairo0CompilerVersionErr
         let version = match Command::new(compiler).arg("--version").output() {
             Ok(output) => String::from_utf8_lossy(&output.stdout).to_string(),
             Err(error) => {
-                return Err(Cairo0CompilerVersionError::NotFound(format!(
+                return Err(Cairo0CompilerVersionError::CompilerNotFound(format!(
                     "Failed to get {compiler} version: {error}."
                 )));
             }
         };
-        if version
-            .trim()
-            .replace("==", " ")
-            .split(" ")
-            .nth(1)
-            .ok_or(Cairo0CompilerVersionError::NotFound("No compiler version found.".to_string()))?
-            != EXPECTED_CAIRO0_VERSION
+        if version.trim().replace("==", " ").split(" ").nth(1).ok_or(
+            Cairo0CompilerVersionError::CompilerNotFound("No compiler version found.".to_string()),
+        )? != EXPECTED_CAIRO0_VERSION
         {
             return Err(Cairo0CompilerVersionError::IncorrectVersion {
                 compiler: compiler.to_string(),
@@ -57,6 +68,39 @@ pub fn cairo0_compilers_correct_version() -> Result<(), Cairo0CompilerVersionErr
     Ok(())
 }
 
+/// Compile a Cairo0 program.
+pub fn compile_cairo0_program(
+    path_to_main: PathBuf,
+    cairo_root_path: PathBuf,
+) -> Result<Vec<u8>, Cairo0CompilerError> {
+    cairo0_compilers_correct_version()?;
+    if !path_to_main.exists() {
+        return Err(Cairo0CompilerError::SourceFileNotFound(path_to_main));
+    }
+    if !cairo_root_path.exists() {
+        return Err(Cairo0CompilerError::CairoRootNotFound(cairo_root_path));
+    }
+    let mut compile_command = Command::new(CAIRO0_COMPILE);
+    compile_command.args([
+        path_to_main.to_str().ok_or(Cairo0CompilerError::InvalidPath(path_to_main.clone()))?,
+        "--debug_info_with_source",
+        "--cairo_path",
+        cairo_root_path
+            .to_str()
+            .ok_or(Cairo0CompilerError::InvalidPath(cairo_root_path.clone()))?,
+    ]);
+    let compile_output = compile_command.output()?;
+
+    // Verify output.
+    if !compile_output.status.success() {
+        return Err(Cairo0CompilerError::CompileError(
+            String::from_utf8_lossy(&compile_output.stderr).trim().to_string(),
+        ));
+    }
+
+    Ok(compile_output.stdout)
+}
+
 /// Verifies that the required Cairo0 compiler is available; panics if unavailable.
 /// For use in tests only. If cairo0 compiler verification is required in business logic, use
 /// `crate::cairo0_compiler::cairo0_compilers_correct_version` instead.
@@ -66,7 +110,9 @@ pub fn verify_cairo0_compiler_deps() {
         Ok(_) => {
             return;
         }
-        Err(Cairo0CompilerVersionError::NotFound(_)) => "no installed cairo-lang found".to_string(),
+        Err(Cairo0CompilerVersionError::CompilerNotFound(_)) => {
+            "no installed cairo-lang found".to_string()
+        }
         Err(Cairo0CompilerVersionError::IncorrectVersion { existing, .. }) => {
             format!("installed version: {existing}")
         }
