d5c325fdd72a68cc53baf4a920b05b0b89f63d6c
Aviv Greenburg <aviv.g@starkware.co>
2025-04-14 21:13:14 +0300

    refactor(blockifier): move cairo_native logic and metrices from papyrus_reader to   blockifier

--- crates/blockifier/src/metrics.rs
+++ crates/blockifier/src/metrics.rs
@@ -2,7 +2,7 @@ use apollo_metrics::define_metrics;
 use apollo_metrics::metrics::MetricCounter;
 
 define_metrics!(
-    ApolloStateReader => {
+    Blockifier => {
         MetricCounter { CLASS_CACHE_MISSES, "class_cache_misses", "Counter of global class cache misses", init=0 },
         MetricCounter { CLASS_CACHE_HITS, "class_cache_hits", "Counter of global class cache hits", init=0 },
         MetricCounter {
