dc9733796327ea876d99652fae03153748e7782e
rotem-starkware <rotem@starkware.co>
2025-04-14 11:41:21 +0300

    feat(starknet_os): implement build_update_tree function (#5304)
    * feat(starknet_os): implement build_update_tree function and test
    
    * refactor(starknet_os): remove optional nodes from UpdatTree

--- crates/starknet_os/src/hints/hint_implementation/patricia/error.rs
+++ crates/starknet_os/src/hints/hint_implementation/patricia/error.rs
@@ -1,7 +1,11 @@
-use super::utils::Preimage;
+use num_bigint::BigUint;
+
+use crate::hints::hint_implementation::patricia::utils::Preimage;
 
 #[derive(Debug, thiserror::Error)]
 pub enum PatriciaError {
     #[error("Expected a binary node, found: {0:?}")]
     ExpectedBinary(Preimage),
+    #[error("Exceeded the max index: {0:?}")]
+    MaxLayerIndexExceeded(BigUint),
 }
