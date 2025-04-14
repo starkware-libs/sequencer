c94b6582af3e7fc1c1efb41336f09f270d9ef0c5
Ayelet Zilber <ayelet@starkware.co>
2025-04-14 15:07:36 +0300

    feat(apollo_dashboard): add l2 gas price to metrics

--- crates/apollo_consensus_orchestrator/src/metrics.rs
+++ crates/apollo_consensus_orchestrator/src/metrics.rs
@@ -5,6 +5,7 @@ define_metrics!(
     Consensus => {
         MetricGauge { CONSENSUS_NUM_BATCHES_IN_PROPOSAL, "consensus_num_batches_in_proposal", "The number of transaction batches in a valid proposal received" },
         MetricGauge { CONSENSUS_NUM_TXS_IN_PROPOSAL, "consensus_num_txs_in_proposal", "The total number of individual transactions in a valid proposal received" },
+        MetricGauge { CONSENSUS_L2_GAS_PRICE, "consensus_l2_gas_price", "The L2 gas price calculated in a valid proposal received" },
 
         // Cende metrics
         MetricGauge { CENDE_LAST_PREPARED_BLOB_BLOCK_NUMBER, "cende_last_prepared_blob_block_number", "The blob block number that cende knows. That means the sequencer can be the proposer only if the current height is greater by one than this value." },
@@ -18,6 +19,7 @@ define_metrics!(
 pub(crate) fn register_metrics() {
     CONSENSUS_NUM_BATCHES_IN_PROPOSAL.register();
     CONSENSUS_NUM_TXS_IN_PROPOSAL.register();
+    CONSENSUS_L2_GAS_PRICE.register();
     CENDE_LAST_PREPARED_BLOB_BLOCK_NUMBER.register();
     CENDE_PREPARE_BLOB_FOR_NEXT_HEIGHT_LATENCY.register();
     CENDE_WRITE_PREV_HEIGHT_BLOB_LATENCY.register();
