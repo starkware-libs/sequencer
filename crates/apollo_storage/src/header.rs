1e5feb537beac77903b7e96b615ccd57c36c100d
Ayelet Zilber <ayelet@starkware.co>
2025-04-14 10:59:45 +0300

    refactor(apollo_starknet_client): fee market info types

--- crates/apollo_storage/src/header.rs
+++ crates/apollo_storage/src/header.rs
@@ -46,6 +46,7 @@ use starknet_api::block::{
     BlockNumber,
     BlockSignature,
     BlockTimestamp,
+    GasPrice,
     GasPricePerToken,
     StarknetVersion,
 };
@@ -58,6 +59,7 @@ use starknet_api::core::{
     TransactionCommitment,
 };
 use starknet_api::data_availability::L1DataAvailabilityMode;
+use starknet_api::execution_resources::GasAmount;
 use tracing::debug;
 
 use crate::db::serialization::NoVersionValueWrapper;
@@ -73,8 +75,8 @@ pub(crate) struct StorageBlockHeader {
     pub l1_gas_price: GasPricePerToken,
     pub l1_data_gas_price: GasPricePerToken,
     pub l2_gas_price: GasPricePerToken,
-    pub l2_gas_consumed: u64,
-    pub next_l2_gas_price: u64,
+    pub l2_gas_consumed: GasAmount,
+    pub next_l2_gas_price: GasPrice,
     pub state_root: GlobalRoot,
     pub sequencer: SequencerContractAddress,
     pub timestamp: BlockTimestamp,
