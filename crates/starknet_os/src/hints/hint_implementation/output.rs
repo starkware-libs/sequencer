d177d80b44cef097687f6985c5f7cc5f2e9b6ac7
Nimrod Weiss <nimrod@starkware.co>
2025-04-16 14:16:01 +0300

    feat(starknet_os): impl set_tree_structure hint

--- crates/starknet_os/src/hints/hint_implementation/output.rs
+++ crates/starknet_os/src/hints/hint_implementation/output.rs
@@ -1,6 +1,9 @@
+use std::cmp::min;
+
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
     get_integer_from_var_name,
+    get_ptr_from_var_name,
     insert_value_from_var_name,
 };
 use starknet_types_core::felt::Felt;
@@ -9,10 +12,44 @@ use crate::hints::error::{OsHintError, OsHintResult};
 use crate::hints::types::HintArgs;
 use crate::hints::vars::{Ids, Scope};
 
+const MAX_PAGE_SIZE: usize = 3800;
+const OUTPUT_ATTRIBUTE_FACT_TOPOLOGY: &str = "gps_fact_topology";
+
 pub(crate) fn set_tree_structure<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { hint_processor, vm, ids_data, ap_tracking, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    if !hint_processor.serialize_data_availability_create_pages {
+        return Ok(());
+    }
+    let onchain_data_start = get_ptr_from_var_name(Ids::DaStart.into(), vm, ids_data, ap_tracking)?;
+    let output_ptr = get_ptr_from_var_name(Ids::OutputPtr.into(), vm, ids_data, ap_tracking)?;
+    let onchain_data_size = (output_ptr - onchain_data_start)?;
+    let output_builtin = vm.get_output_builtin_mut()?;
+
+    let n_pages = onchain_data_size.div_ceil(MAX_PAGE_SIZE);
+    for i in 0..n_pages {
+        let start_offset = i * MAX_PAGE_SIZE;
+        let page_id = i + 1;
+        let page_start = (onchain_data_start + start_offset)?;
+        let page_size = min(onchain_data_size - start_offset, MAX_PAGE_SIZE);
+        output_builtin.add_page(page_id, page_start, page_size)?;
+    }
+    output_builtin.add_attribute(
+        OUTPUT_ATTRIBUTE_FACT_TOPOLOGY.to_string(),
+        vec![
+            // Push 1 + n_pages pages (all of the pages).
+            1 + n_pages,
+            // Create a parent node for the last n_pages.
+            n_pages,
+            // Don't push additional pages.
+            0,
+            // Take the first page (the main part) and the node that was created (onchain data)
+            // and use them to construct the root of the fact tree.
+            2,
+        ],
+    );
+
+    Ok(())
 }
 
 pub(crate) fn set_state_updates_start<S: StateReader>(
