fa52ca25c97465010c52ef4935a54e21fcfec660
Arnon Hod <arnon@starkware.co>
2025-04-16 13:50:17 +0300

    feat(apollo_consensus_orchestrator): add data gas price multiplier calculation (#5627)

--- crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
+++ crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
@@ -58,6 +58,7 @@ use futures::channel::{mpsc, oneshot};
 use futures::{FutureExt, SinkExt, StreamExt};
 #[cfg(any(feature = "testing", test))]
 use mockall::automock;
+use num_rational::Ratio;
 use starknet_api::block::{
     BlockHash,
     BlockHashAndNumber,
@@ -826,8 +827,13 @@ async fn initiate_build(args: &ProposalBuildArguments) -> ProposalResult<Consens
     };
     l1_prices.base_fee_per_gas =
         l1_prices.base_fee_per_gas.clamp(args.min_l1_gas_price_wei, args.max_l1_gas_price_wei);
-    l1_prices.blob_fee =
-        l1_prices.blob_fee.clamp(args.min_l1_data_gas_price_wei, args.max_l1_data_gas_price_wei);
+
+    // TODO(Arni): This value should be an input of this function.
+    let part_per_thousand_data_gas_price_multiplier: u128 = 1000;
+    let data_gas_price_multiplier = Ratio::new(part_per_thousand_data_gas_price_multiplier, 1000);
+    l1_prices.blob_fee = (data_gas_price_multiplier * l1_prices.blob_fee)
+        .to_integer()
+        .clamp(args.min_l1_data_gas_price_wei, args.max_l1_data_gas_price_wei);
 
     let block_info = ConsensusBlockInfo {
         height: args.proposal_init.height,
@@ -1107,8 +1113,12 @@ async fn is_block_info_valid(
     gas_prices.base_fee_per_gas = gas_prices
         .base_fee_per_gas
         .clamp(min_max_prices.min_l1_gas_price_wei, min_max_prices.max_l1_gas_price_wei);
-    gas_prices.blob_fee = gas_prices
-        .blob_fee
+
+    // TODO(Arni): This value should be an input of this function.
+    let part_per_thousand_data_gas_price_multiplier: u128 = 1000;
+    let data_gas_price_multiplier = Ratio::new(part_per_thousand_data_gas_price_multiplier, 1000);
+    gas_prices.blob_fee = (data_gas_price_multiplier * gas_prices.blob_fee)
+        .to_integer()
         .clamp(min_max_prices.min_l1_data_gas_price_wei, min_max_prices.max_l1_data_gas_price_wei);
 
     let l1_gas_price_fri =
