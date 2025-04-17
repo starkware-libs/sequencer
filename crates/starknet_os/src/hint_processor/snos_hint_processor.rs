ff021552d34cbbf2dc7ac86cf226d34a20ac7d61
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-04-17 17:34:56 +0300

    feat(starknet_os): add create pages field to snos hint processor (#5883)

--- crates/starknet_os/src/hint_processor/snos_hint_processor.rs
+++ crates/starknet_os/src/hint_processor/snos_hint_processor.rs
@@ -103,6 +103,8 @@ pub struct SnosHintProcessor<'a, S: StateReader> {
     pub(crate) commitment_type: CommitmentType,
     // KZG fields.
     da_segment: Option<Vec<Felt>>,
+    // Indicates wether to create pages or not when serializing data-availability.
+    _serialize_data_availability_create_pages: bool,
 }
 
 impl<'a, S: StateReader> SnosHintProcessor<'a, S> {
@@ -146,6 +148,7 @@ impl<'a, S: StateReader> SnosHintProcessor<'a, S> {
             compiled_classes: os_hints.os_input.compiled_classes,
             state_update_pointers: None,
             commitment_type: CommitmentType::State,
+            _serialize_data_availability_create_pages: false,
         })
     }
 
