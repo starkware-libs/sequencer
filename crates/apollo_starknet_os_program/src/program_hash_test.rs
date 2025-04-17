604a78928c9dd3ac3fa92776e003375ebaa988fd
Dori Medini <dori@starkware.co>
2025-04-17 17:17:12 +0300

    feat(apollo_starknet_os_program): add aggregator program + program hash(es)

--- crates/apollo_starknet_os_program/src/program_hash_test.rs
+++ crates/apollo_starknet_os_program/src/program_hash_test.rs
@@ -1,4 +1,10 @@
-use crate::program_hash::{compute_os_program_hash, ProgramHash, PROGRAM_HASH_PATH};
+use crate::program_hash::{
+    compute_aggregator_program_hash,
+    compute_os_program_hash,
+    AggregatorHash,
+    ProgramHash,
+    PROGRAM_HASH_PATH,
+};
 use crate::PROGRAM_HASH;
 
 /// Asserts the program hash of the compiled Starknet OS program matches the program hash in the
@@ -9,7 +15,12 @@ use crate::PROGRAM_HASH;
 /// ```
 #[test]
 fn test_program_hash() {
-    let computed_hash = ProgramHash { os: compute_os_program_hash().unwrap() };
+    let AggregatorHash { with_prefix, without_prefix } = compute_aggregator_program_hash().unwrap();
+    let computed_hash = ProgramHash {
+        os: compute_os_program_hash().unwrap(),
+        aggregator: without_prefix,
+        aggregator_with_prefix: with_prefix,
+    };
     if std::env::var("FIX_PROGRAM_HASH").is_ok() {
         std::fs::write(
             PROGRAM_HASH_PATH.as_path(),
