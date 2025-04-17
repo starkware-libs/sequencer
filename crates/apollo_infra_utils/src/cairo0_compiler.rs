f38a4b35b0f7f19b6840646a86dd106a6c94571d
Dori Medini <dori@starkware.co>
2025-04-17 12:55:19 +0300

    feat(apollo_infra_utils): better cairo0 compiler checks

--- crates/apollo_infra_utils/src/cairo0_compiler.rs
+++ crates/apollo_infra_utils/src/cairo0_compiler.rs
@@ -1,38 +1,67 @@
-use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::LazyLock;
 
 use crate::path::resolve_project_relative_path;
 
+#[cfg(test)]
+#[path = "cairo0_compiler_test.rs"]
+pub mod test;
+
+pub const STARKNET_COMPILE_DEPRECATED: &str = "starknet-compile-deprecated";
+pub const CAIRO0_COMPILE: &str = "cairo-compile";
+pub const EXPECTED_CAIRO0_VERSION: &str = "0.13.5";
+
 /// The local python requirements used to determine the cairo0 compiler version.
-static PIP_REQUIREMENTS_FILE: LazyLock<PathBuf> =
+pub(crate) static PIP_REQUIREMENTS_FILE: LazyLock<PathBuf> =
     LazyLock::new(|| resolve_project_relative_path("scripts/requirements.txt").unwrap());
+
+#[derive(thiserror::Error, Debug)]
+pub enum Cairo0CompilerVersionError {
+    #[error("{compiler} version is not correct: required {required}, got {existing}.")]
+    IncorrectVersion { compiler: String, existing: String, required: String },
+    #[error("{0} not found.")]
+    NotFound(String),
+}
+
+pub fn cairo0_compilers_correct_version() -> Result<(), Cairo0CompilerVersionError> {
+    for compiler in [CAIRO0_COMPILE, STARKNET_COMPILE_DEPRECATED] {
+        let version = match Command::new(compiler).arg("--version").output() {
+            Ok(output) => String::from_utf8_lossy(&output.stdout).to_string(),
+            Err(error) => {
+                return Err(Cairo0CompilerVersionError::NotFound(format!(
+                    "Failed to get {compiler} version: {error}."
+                )));
+            }
+        };
+        if !version.trim().ends_with(EXPECTED_CAIRO0_VERSION) {
+            return Err(Cairo0CompilerVersionError::IncorrectVersion {
+                compiler: compiler.to_string(),
+                existing: version,
+                required: EXPECTED_CAIRO0_VERSION.to_string(),
+            });
+        }
+    }
+
+    Ok(())
+}
 
 /// Verifies that the required Cairo0 compiler is available; panics if unavailable.
 pub fn verify_cairo0_compiler_deps() {
-    // Python compiler. Verify correct version.
-    let cairo_lang_version_output =
-        Command::new("sh").arg("-c").arg("pip freeze | grep cairo-lang").output().unwrap().stdout;
-    let cairo_lang_version_untrimmed = String::from_utf8(cairo_lang_version_output).unwrap();
-    let cairo_lang_version = cairo_lang_version_untrimmed.trim();
-    let requirements_contents = fs::read_to_string(&*PIP_REQUIREMENTS_FILE).unwrap();
-    let expected_cairo_lang_version = requirements_contents
-        .lines()
-        .find(|line| line.starts_with("cairo-lang"))
-        .unwrap_or_else(|| panic!("Could not find cairo-lang in {:?}.", *PIP_REQUIREMENTS_FILE))
-        .trim();
+    let specific_error = match cairo0_compilers_correct_version() {
+        Ok(_) => {
+            return;
+        }
+        Err(Cairo0CompilerVersionError::NotFound(_)) => "no installed cairo-lang found".to_string(),
+        Err(Cairo0CompilerVersionError::IncorrectVersion { existing, .. }) => {
+            format!("installed version: {existing}")
+        }
+    };
 
-    assert!(
-        expected_cairo_lang_version.ends_with(cairo_lang_version),
-        "cairo-lang version {expected_cairo_lang_version} not found ({}). Run the following \
-         commands (enter a python venv and install dependencies) and retry:\npython -m venv \
-         sequencer_venv\n. sequencer_venv/bin/activate\npip install -r {:?}",
-        if cairo_lang_version.is_empty() {
-            String::from("no installed cairo-lang found")
-        } else {
-            format!("installed version: {cairo_lang_version}")
-        },
-        *PIP_REQUIREMENTS_FILE
+    panic!(
+        "cairo-lang version {EXPECTED_CAIRO0_VERSION} not found ({specific_error}). Run the \
+         following commands (enter a python venv and install dependencies) and retry:\npython -m \
+         venv sequencer_venv\n. sequencer_venv/bin/activate\npip install -r {:?}",
+        PIP_REQUIREMENTS_FILE.to_str().expect("Path to requirements.txt is valid unicode.")
     );
 }
