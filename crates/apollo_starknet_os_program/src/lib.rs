a8a5dadd3698de7824a1513498cc12aaf63bfd64
Dori Medini <dori@starkware.co>
2025-04-19 18:04:36 +0300

    feat(apollo_starknet_os_program): add test contracts, add dumper in CLI

--- crates/apollo_starknet_os_program/src/lib.rs
+++ crates/apollo_starknet_os_program/src/lib.rs
@@ -8,6 +8,8 @@ use cairo_vm::types::program::Program;
 use crate::program_hash::{ProgramHash, PROGRAM_HASH_PATH};
 
 pub mod program_hash;
+#[cfg(feature = "test_programs")]
+pub mod test_programs;
 
 #[cfg(feature = "dump_source_files")]
 pub static CAIRO_FILES_MAP: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
