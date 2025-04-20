50ad7f3ab4713140ea256ce2eafec7515fda15e6
guyf-starkware <guy.f@starkware.co>
2025-04-20 11:51:40 +0300

    Refactor checks to atually run the check rather than return the command line args

--- scripts/presubmit_fast_cheks.py
+++ scripts/presubmit_fast_cheks.py
@@ -10,6 +10,7 @@ For local use it should be wrapped by a caller which computes the relevant commi
 from abc import ABC, abstractmethod
 from enum import Enum
 from os import path
+from run_tests import run_test, BaseCommand
 
 import argparse
 import subprocess
@@ -33,24 +34,39 @@ class Check(ABC):
         return cls()
 
     @abstractmethod
-    def get_cmds(self):
+    def run_check(self):
         pass
 
 
 class RunTestsCheck(Check):
-    def __init__(self, run_test_args: list[str]):
-        self.run_test_args = run_test_args
+    def __init__(self, command: BaseCommand, from_commit_hash: str):
+        self.command = command
+        self.from_commit_hash = from_commit_hash
 
-    def get_cmds(self):
-        return [["python", SCRIPTS_LOCATION + "/run_tests.py"] + self.run_test_args]
+    def run_check(self):
+        print(f"Calling run_test with command: {self.command}")
+        run_test(
+            changes_only=True,
+            commit_id=self.from_commit_hash,
+            base_command=self.command,
+            include_dependencies=False,
+        )
+
+
+class ExternalCommandCheck(Check):
+    def __init__(self, commands: list[list[str]]):
+        self.commands = commands
+
+    def run_check(self):
+        for cmd in self.commands:
+            print(f"Running: {' '.join(cmd)}", flush=True)
+            subprocess.run(cmd, check=True)
 
 
 class ClippyCheck(RunTestsCheck):
     def __init__(self, from_commit_hash: str):
         assert from_commit_hash, "from_commit_hash is required for clippy check."
-        super().__init__(
-            ["--command", "clippy", "--changes_only", "--commit_id", from_commit_hash]
-        )
+        super().__init__(BaseCommand.CLIPPY, from_commit_hash)
 
     @classmethod
     def from_args(cls, args):
@@ -60,98 +76,97 @@ class ClippyCheck(RunTestsCheck):
 class DocCheck(RunTestsCheck):
     def __init__(self, from_commit_hash: str):
         assert from_commit_hash, "from_commit_hash is required for doc check."
-        super().__init__(
-            ["--command", "doc", "--changes_only", "--commit_id", from_commit_hash]
-        )
+        super().__init__(BaseCommand.DOC, from_commit_hash)
 
     @classmethod
     def from_args(cls, args):
         return DocCheck(args.from_commit_hash)
 
 
-class GitSubmodulesCheck(Check):
-    def get_cmds(self):
-        return [["git", "submodule", "status"]]
+class GitSubmodulesCheck(ExternalCommandCheck):
+    def __init__(self):
+        super().__init__([["git", "submodule", "status"]])
 
 
-class CommitLintCheck(Check):
+class CommitLintCheck(ExternalCommandCheck):
     def __init__(self, from_commit_hash: str, to_commit_hash: str):
         assert from_commit_hash, "from_commit_hash is required for commit lint check."
         assert to_commit_hash, "to_commit_hash is required for commit lint check."
-        self.from_commit_hash = from_commit_hash
-        self.to_commit_hash = to_commit_hash
+        super().__init__(
+            [["commitlint"] + ["--from", from_commit_hash] + ["--to", to_commit_hash]]
+        )
 
     @classmethod
     def from_args(cls, args):
         return CommitLintCheck(args.from_commit_hash, args.to_commit_hash)
 
-    def get_cmds(self):
-        return [
-            ["commitlint"]
-            + ["--from", self.from_commit_hash]
-            + ["--to", self.to_commit_hash]
-        ]
 
-
-class TodosCheck(Check):
+class TodosCheck(ExternalCommandCheck):
     def __init__(self, from_commit_hash: str):
         assert from_commit_hash, "from_commit_hash is required for TODOs check."
-        self.from_commit_hash = from_commit_hash
+        super().__init__(
+            [
+                ["python", SCRIPTS_LOCATION + "/named_todos.py"]
+                + ["--commit_id", from_commit_hash]
+            ]
+        )
 
     @classmethod
     def from_args(cls, args):
         return TodosCheck(args.from_commit_hash)
 
-    def get_cmds(self):
-        return [
-            ["python", SCRIPTS_LOCATION + "/named_todos.py"]
-            + ["--commit_id", self.from_commit_hash]
-        ]
+
+class CargoLockCheck(ExternalCommandCheck):
+    def __init__(self):
+        super().__init__(
+            [
+                ["cargo", "update", "-w", "--locked"],
+                ["git", "diff", "--exit-code", "Cargo.lock"],
+            ]
+        )
 
 
-class CargoLockCheck(Check):
-    def get_cmds(self):
-        return [
-            ["cargo", "update", "-w", "--locked"],
-            ["git", "diff", "--exit-code", "Cargo.lock"],
-        ]
-
-
-class RustFormatCheck(Check):
+class RustFormatCheck(ExternalCommandCheck):
     def __init__(self, extra_rust_toolchains: str):
         assert (
             extra_rust_toolchains
         ), "extra_rust_toolchains is required for rust formatting checks."
-        self.extra_rust_toolchains = extra_rust_toolchains
+        super().__init__(
+            [
+                [
+                    "cargo",
+                    f"+{extra_rust_toolchains}",
+                    "fmt",
+                    "--all",
+                    "--",
+                    "--check",
+                ]
+            ]
+        )
 
     @classmethod
     def from_args(cls, args):
         return RustFormatCheck(args.extra_rust_toolchains)
 
-    def get_cmds(self):
-        return [
-            ["cargo", f"+{self.extra_rust_toolchains}", "fmt", "--all", "--", "--check"]
-        ]
+
+class TaploCheck(ExternalCommandCheck):
+    def __init__(self):
+        super().__init__([["bash", SCRIPTS_LOCATION + "/taplo.sh"]])
 
 
-class TaploCheck(Check):
-    def get_cmds(self):
-        return [["bash", SCRIPTS_LOCATION + "/taplo.sh"]]
-
-
-class MacheteCheck(Check):
-    def get_cmds(self):
-        return [["cargo", "machete"]]
+class MacheteCheck(ExternalCommandCheck):
+    def __init__(self):
+        super().__init__([["cargo", "machete"]])
 
 
 class DummyCheck(Check):
-    def get_cmds(self):
-        return [["echo", "dummy test"]]
+    def run_check(self):
+        pass
 
 
 class FailingCheck(Check):
-    def get_cmds(self):
-        return ["fail"]
+    def run_check(self):
+        subprocess.run(["fail"], check=True)
 
 
 def parse_args() -> argparse.Namespace:
@@ -189,7 +204,7 @@ def GetChecksForMode(args: argparse.Namespace) -> list[Check]:
     match mode:
         case RunModes.LOCAL_PRESUBMIT | RunModes.CI_PRESUBMIT:
             return [
-                CommitLintCheck.from_args(args),
+                # CommitLintCheck.from_args(args),
                 GitSubmodulesCheck.from_args(args),
                 TodosCheck.from_args(args),
                 CargoLockCheck.from_args(args),
@@ -211,9 +226,7 @@ def main():
     checks = GetChecksForMode(args)
 
     for check in checks:
-        for cmd in check.get_cmds():
-            print(f"Running: {' '.join(cmd)}", flush=True)
-            subprocess.run(cmd, check=True)
+        check.run_check()
 
 
 if __name__ == "__main__":
