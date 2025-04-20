3a13f45922906e32887b8c7baab8775cf09adf18
ron-starkware <ron@starkware.co>
2025-04-20 13:33:03 +0300

    refactor(apollo_rpc): invoke transaction v0 refactoring

--- crates/apollo_rpc/src/v0_8/api/mod.rs
+++ crates/apollo_rpc/src/v0_8/api/mod.rs
@@ -590,7 +590,7 @@ impl From<InvokeTransaction> for starknet_api::transaction::InvokeTransaction {
                 calldata,
             }) => Self::V0(starknet_api::transaction::InvokeTransactionV0 {
                 max_fee,
-                signature: TransactionSignature(Arc::new(signature.0)),
+                signature,
                 contract_address,
                 entry_point_selector,
                 calldata,
