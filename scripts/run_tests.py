57397a0c9873a6084b74228d9d15dabbc3610849
Alon Haramati <alonh@starkware.co>
2025-04-20 13:16:43 +0300

    fix(ci): reenable all integration tests

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -12,7 +12,12 @@ from tests_utils import (
 )
 
 # Set of files which - if changed - should trigger tests for all packages.
-ALL_TEST_TRIGGERS: Set[str] = {"Cargo.toml", "Cargo.lock", "rust-toolchain.toml"}
+ALL_TEST_TRIGGERS: Set[str] = {
+    "Cargo.toml",
+    "Cargo.lock",
+    "rust-toolchain.toml",
+    "scripts/run_tests.py",
+}
 
 # Set of crates which - if changed - should trigger the integration tests.
 INTEGRATION_TEST_CRATE_TRIGGERS: Set[str] = {"apollo_integration_tests"}
