c3394d983da8f0dc7fd4200dd491e00d9cb0ea09
amos rothberg <amos@starkware.co>
2025-04-21 18:29:12 +0300

    test(starknet_os, starknet_committer_and_os_cli): added test_bigint3_to_uint256

--- crates/starknet_committer_and_os_cli/src/os_cli/tests/utils.rs
+++ crates/starknet_committer_and_os_cli/src/os_cli/tests/utils.rs
@@ -1,11 +1,24 @@
 use std::any::Any;
 use std::collections::HashMap;
+use std::sync::LazyLock;
 
+use ethnum::U256;
+use num_bigint::{BigInt, Sign};
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 use starknet_os::test_utils::cairo_runner::{EndpointArg, EntryPointRunnerConfig, ImplicitArg};
 use starknet_os::test_utils::utils::run_cairo_function_and_check_result;
 
 use crate::os_cli::tests::types::{OsPythonTestResult, OsSpecificTestError};
 use crate::shared_utils::types::PythonTestError;
+
+// 2**251 + 17 * 2**192 + 1
+pub static DEFAULT_PRIME: LazyLock<BigInt> = LazyLock::new(|| {
+    BigInt::from_bytes_be(
+        Sign::Plus,
+        &(U256::from(2_u32).pow(251) + 17 * U256::from(2_u32).pow(192) + 1).to_be_bytes(),
+    )
+});
 
 #[allow(clippy::too_many_arguments)]
 pub(crate) fn test_cairo_function(
@@ -32,4 +45,8 @@ pub(crate) fn test_cairo_function(
         PythonTestError::SpecificError(OsSpecificTestError::Cairo0EntryPointRunner(error))
     })?;
     Ok("".to_string())
+}
+
+pub(crate) fn seeded_random_prng() -> StdRng {
+    StdRng::seed_from_u64(42)
 }
