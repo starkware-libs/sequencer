205688ccddb60ca03b0e89502b53b0256f6d4c7e
Dori Medini <dori@starkware.co>
2025-04-22 01:15:46 +0300

    feat(apollo_starknet_os_program): add OS constants template file

---
+++ crates/apollo_starknet_os_program/src/constants_test.rs
@@ -0,0 +1,3 @@
+const _CONSTS_CONTENTS: &str = include_str!("cairo/starkware/starknet/core/os/constants.cairo");
+const _CONSTS_TEMPLATE: &str =
+    include_str!("cairo/starkware/starknet/core/os/constants_template.txt");
--- crates/apollo_starknet_os_program/src/lib.rs
+++ crates/apollo_starknet_os_program/src/lib.rs
@@ -7,6 +7,8 @@ use cairo_vm::types::program::Program;
 
 use crate::program_hash::{ProgramHash, PROGRAM_HASH_PATH};
 
+#[cfg(test)]
+pub mod constants_test;
 pub mod program_hash;
 #[cfg(feature = "test_programs")]
 pub mod test_programs;
