6d9150f2f9cffeca62383f545e0f00976268acdf
Aviv Greenburg <aviv.g@starkware.co>
2025-04-22 13:49:39 +0300

    test(blockifier): add native feature to transactions_test

--- crates/blockifier/src/test_utils/test_templates.rs
+++ crates/blockifier/src/test_utils/test_templates.rs
@@ -38,36 +38,6 @@ fn runnable_version(
 ) {
 }
 
-#[cfg(not(feature = "cairo_native"))]
-#[template]
-#[rstest]
-fn two_cairo_versions(
-    #[values(CairoVersion::Cairo0, CairoVersion::Cairo1(RunnableCairo1::Casm))]
-    cairo_version1: CairoVersion,
-    #[values(CairoVersion::Cairo0, CairoVersion::Cairo1(RunnableCairo1::Casm))]
-    cairo_version2: CairoVersion,
-) {
-}
-
-#[cfg(feature = "cairo_native")]
-#[template]
-#[rstest]
-fn two_cairo_versions(
-    #[values(
-        CairoVersion::Cairo0,
-        CairoVersion::Cairo1(RunnableCairo1::Casm),
-        CairoVersion::Cairo1(RunnableCairo1::Native)
-    )]
-    cairo_version1: CairoVersion,
-    #[values(
-        CairoVersion::Cairo0,
-        CairoVersion::Cairo1(RunnableCairo1::Casm),
-        CairoVersion::Cairo1(RunnableCairo1::Native)
-    )]
-    cairo_version2: CairoVersion,
-) {
-}
-
 // When creating a function using a template, every function name is slightly different to avoid
 // having multiple functions with the same name in the same scope. This means that the fact that we
 // do not use the template in the file it's in makes the compiler think it is an unused macro.
@@ -76,11 +46,6 @@ fn two_cairo_versions(
 #[apply(cairo_version)]
 fn test_cairo_version(cairo_version: CairoVersion) {
     println!("test {:?}", cairo_version);
-}
-
-#[apply(two_cairo_versions)]
-fn test_two_cairo_version(cairo_version1: CairoVersion, cairo_version2: CairoVersion) {
-    println!("test {:?} {:?}", cairo_version1, cairo_version2);
 }
 
 #[apply(runnable_version)]
