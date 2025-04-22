4cf030f2afbc333a39dba16db73f43804fce5c51
Asmaa Magdoub <asmaa@starkware.co>
2025-04-22 17:20:12 +0300

    feat(apollo_l1_gas_price): querys are spawned into a side task and we always return immediately

--- crates/apollo_l1_gas_price/src/eth_to_strk_oracle_test.rs
+++ crates/apollo_l1_gas_price/src/eth_to_strk_oracle_test.rs
@@ -1,42 +1,11 @@
+use std::time::Duration;
+
 use apollo_l1_gas_price_types::EthToStrkOracleClientTrait;
 use serde_json::json;
 use tokio;
 use url::Url;
 
 use crate::eth_to_strk_oracle::EthToStrkOracleClient;
-
-#[tokio::test]
-async fn eth_to_fri_rate() {
-    let expected_rate = 123456;
-    let expected_rate_hex = format!("0x{:x}", expected_rate);
-    let timestamp = 1234567890;
-    let lag_interval_seconds = 10;
-    let quantized_timestamp = (timestamp - lag_interval_seconds) / lag_interval_seconds;
-    let adjusted_timestamp = quantized_timestamp * lag_interval_seconds;
-    // Create a mock HTTP server.
-    let mut server = mockito::Server::new_async().await;
-
-    // Define a mock response for a GET request with a specific timestamp in the path
-    let _m = server
-        .mock("GET", format!("/{}", adjusted_timestamp).as_str())
-        .with_header("Content-Type", "application/json")
-        .with_body(
-            json!({
-                "price": expected_rate_hex,
-                "decimals": 18
-            })
-            .to_string(),
-        )
-        .create();
-
-    // Construct the base URL from the mock server
-    let base_url = Url::parse(&server.url()).unwrap();
-
-    let client = EthToStrkOracleClient::new(base_url, None, lag_interval_seconds);
-    let rate = client.eth_to_fri_rate(timestamp).await.unwrap();
-
-    assert_eq!(rate, expected_rate);
-}
 
 #[tokio::test]
 async fn eth_to_fri_rate_uses_cache_on_quantized_hit() {
@@ -67,11 +36,13 @@ async fn eth_to_fri_rate_uses_cache_on_quantized_hit() {
     let base_url = Url::parse(&server.url()).unwrap();
     let client = EthToStrkOracleClient::new(base_url, None, lag_interval_seconds);
 
-    let rate1 = client.eth_to_fri_rate(timestamp1).await.unwrap();
+    let rate1 = client.eth_to_fri_rate(timestamp1).await;
     // Because caching is used, the second request reuses the first response and doesn't trigger
     // another server call.
-    let rate2 = client.eth_to_fri_rate(timestamp2).await.unwrap();
+    // Sleep to ensure the first request is completed before the second.
+    tokio::time::sleep(Duration::from_millis(1)).await;
+    let rate2 = client.eth_to_fri_rate(timestamp2).await;
 
-    assert_eq!(rate1, expected_rate);
-    assert_eq!(rate2, expected_rate);
+    assert!(rate1.is_err());
+    assert_eq!(rate2.unwrap(), expected_rate);
 }
