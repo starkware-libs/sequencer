cd558e00d0380e33103189c0254490c9be797cc3
amos rothberg <amos@starkware.co>
2025-04-22 18:05:53 +0300

    chore(starknet_committer_and_os_cli): added test_felt_to_bigint3

--- crates/starknet_committer_and_os_cli/src/os_cli/tests/bls_field.rs
+++ crates/starknet_committer_and_os_cli/src/os_cli/tests/bls_field.rs
@@ -5,7 +5,7 @@ use cairo_vm::types::layout_name::LayoutName;
 use ethnum::U256;
 use num_bigint::{BigInt, BigUint, RandomBits, Sign};
 use rand::Rng;
-use starknet_os::hints::hint_implementation::kzg::utils::split_bigint3;
+use starknet_os::hints::hint_implementation::kzg::utils::{split_bigint3, BASE};
 use starknet_os::test_utils::cairo_runner::{
     EndpointArg,
     EntryPointRunnerConfig,
@@ -16,11 +16,12 @@ use starknet_types_core::felt::Felt;
 use tracing::info;
 
 use crate::os_cli::tests::types::OsPythonTestResult;
-use crate::os_cli::tests::utils::{seeded_random_prng, test_cairo_function};
+use crate::os_cli::tests::utils::{seeded_random_prng, test_cairo_function, DEFAULT_PRIME};
 
 // TODO(Amos): This test is incomplete. Add the rest of the test cases and remove this todo.
 pub(crate) fn test_bls_field(input: &str) -> OsPythonTestResult {
     test_bigint3_to_uint256(input)?;
+    test_felt_to_bigint3(input)?;
     Ok("".to_string())
 }
 
@@ -56,5 +57,46 @@ fn test_bigint3_to_uint256(input: &str) -> OsPythonTestResult {
         &expected_implicit_args,
         HashMap::new(),
     )?;
+    Ok("".to_string())
+}
+
+fn test_felt_to_bigint3(input: &str) -> OsPythonTestResult {
+    let values: [BigInt; 9] = [
+        0.into(),
+        1.into(),
+        DEFAULT_PRIME.clone() - 1,
+        DEFAULT_PRIME.clone() - 2,
+        BASE.clone() - 1,
+        BASE.clone(),
+        BASE.pow(2_u32) - 1,
+        BASE.pow(2_u32),
+        DEFAULT_PRIME.clone() / 2,
+    ];
+    let entrypoint_runner_config = EntryPointRunnerConfig {
+        layout: LayoutName::small,
+        add_main_prefix_to_entrypoint: false,
+        ..Default::default()
+    };
+    for value in values {
+        let explicit_args: [EndpointArg; 1] = [Felt::from(value.clone()).into()];
+        let implicit_args = [ImplicitArg::Builtin(BuiltinName::range_check)];
+
+        let split_value = split_bigint3(value.clone()).unwrap();
+        let expected_explicit_args: Vec<EndpointArg> =
+            split_value.iter().map(|&x| x.into()).collect::<Vec<EndpointArg>>();
+        let n_range_checks = if value == DEFAULT_PRIME.clone() - 1 { 0 } else { 6 };
+        let expected_implicit_args: [EndpointArg; 1] = [n_range_checks.into()];
+
+        test_cairo_function(
+            &entrypoint_runner_config,
+            input,
+            "starkware.starknet.core.os.data_availability.bls_field.felt_to_bigint3",
+            &explicit_args,
+            &implicit_args,
+            &expected_explicit_args,
+            &expected_implicit_args,
+            HashMap::new(),
+        )?;
+    }
     Ok("".to_string())
 }
