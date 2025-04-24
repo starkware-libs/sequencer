863a928c3429c6d6b4a5e287e359610619bf513f
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-04-24 16:28:14 +0300

    chore(apollo_deployments): remove redundant self arg (#6123)

--- crates/apollo_deployments/src/bin/deployment_generator.rs
+++ crates/apollo_deployments/src/bin/deployment_generator.rs
@@ -7,8 +7,6 @@ fn main() {
         let deployment = deployment_fn();
         serialize_to_file(&deployment, deployment.deployment_file_path().to_str().unwrap());
 
-        deployment.dump_application_config_files(
-            deployment.get_base_app_config_file_path().to_str().unwrap(),
-        );
+        deployment.dump_application_config_files();
     }
 }
