197cb81f6b094f683fce5e5b778c221e8678117f
Itay Tsabary <itayt@starkware.co>
2025-04-27 07:18:44 +0300

    chore(deployment): automate dashboard convertion in deployment

--- deployments/sequencer/services/monitoring.py
+++ deployments/sequencer/services/monitoring.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 
 class GrafanaDashboard:
@@ -11,5 +12,18 @@ class GrafanaDashboard:
         self.dashboard_path = os.path.join(self.ROOT_DIR, dashboard_file)
 
     def get_dashboard(self):
+        print("Generating dashboard file:")
+        generate_dashboard_json_cmd = [
+            "python",
+            "./deployments/monitoring/src/dashboard_builder.py",
+            "builder",
+            "-j",
+            "./Monitoring/sequencer/dev_grafana.json",
+            "-o",
+            self.ROOT_DIR,
+        ]
+        print(generate_dashboard_json_cmd, flush=True)
+        subprocess.run(generate_dashboard_json_cmd, check=True)
+
         with open(self.dashboard_path, "r") as f:
             return json.load(f)
