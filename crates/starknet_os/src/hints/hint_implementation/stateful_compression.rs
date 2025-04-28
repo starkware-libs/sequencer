5fae9ff631c39e6bcc861ac1ee7a8943b950a0d2
Nimrod Weiss <nimrod@starkware.co>
2025-04-28 09:17:28 +0300

    feat(starknet_os): impl update_contract_addr_to_storage_ptr hint

--- crates/starknet_os/src/hints/hint_implementation/stateful_compression.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateful_compression.rs
@@ -141,9 +141,33 @@ pub(crate) fn guess_contract_addr_storage_ptr<S: StateReader>(
 }
 
 pub(crate) fn update_contract_addr_to_storage_ptr<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { hint_processor, vm, ids_data, ap_tracking, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    if let Some(state_update_pointers) = &mut hint_processor.state_update_pointers {
+        let key_address = get_address_of_nested_fields(
+            ids_data,
+            Ids::StateChanges,
+            CairoStruct::DictAccessPtr,
+            vm,
+            ap_tracking,
+            &["key"],
+            hint_processor.os_program,
+        )?;
+        let contract_address =
+            ContractAddress(vm.get_integer(key_address)?.into_owned().try_into()?);
+        let squashed_new_state =
+            get_ptr_from_var_name(Ids::SquashedNewState.into(), vm, ids_data, ap_tracking)?;
+        let squashed_storage_ptr_end =
+            get_ptr_from_var_name(Ids::SquashedStoragePtrEnd.into(), vm, ids_data, ap_tracking)?;
+
+        state_update_pointers.set_contract_state_entry_and_storage_ptr(
+            contract_address,
+            StateEntryPtr(squashed_new_state),
+            StoragePtr(squashed_storage_ptr_end),
+        );
+    }
+
+    Ok(())
 }
 
 pub(crate) fn guess_aliases_contract_storage_ptr<S: StateReader>(
