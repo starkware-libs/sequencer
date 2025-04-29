c5bbbd51d06b75260cddd303cb7a336315e58ab3
Aner Ben Efraim <aner@starkware.co>
2025-04-29 14:45:18 +0300

    refactor(blockifier): add segment_base to hint processor

--- crates/blockifier/src/execution/syscalls/hint_processor.rs
+++ crates/blockifier/src/execution/syscalls/hint_processor.rs
@@ -285,8 +285,8 @@ impl<'a> SyscallHintProcessor<'a> {
             syscall_ptr: initial_syscall_ptr,
             hints,
             execution_info_ptr: None,
-            secp256k1_hint_processor: SecpHintProcessor::default(),
-            secp256r1_hint_processor: SecpHintProcessor::default(),
+            secp256k1_hint_processor: SecpHintProcessor::new(),
+            secp256r1_hint_processor: SecpHintProcessor::new(),
             sha256_segment_end_ptr: None,
         }
     }
@@ -775,11 +775,11 @@ impl SyscallExecutor for SyscallHintProcessor<'_> {
 
     fn secp256k1_new(
         request: SecpNewRequest,
-        _vm: &mut VirtualMachine,
+        vm: &mut VirtualMachine,
         syscall_handler: &mut Self,
         _remaining_gas: &mut u64,
     ) -> SyscallResult<SecpNewResponse> {
-        syscall_handler.secp256k1_hint_processor.secp_new(request)
+        syscall_handler.secp256k1_hint_processor.secp_new(vm, request)
     }
 
     fn secp256r1_add(
@@ -820,11 +820,11 @@ impl SyscallExecutor for SyscallHintProcessor<'_> {
 
     fn secp256r1_new(
         request: Secp256r1NewRequest,
-        _vm: &mut VirtualMachine,
+        vm: &mut VirtualMachine,
         syscall_handler: &mut Self,
         _remaining_gas: &mut u64,
     ) -> SyscallResult<Secp256r1NewResponse> {
-        syscall_handler.secp256r1_hint_processor.secp_new(request)
+        syscall_handler.secp256r1_hint_processor.secp_new(vm, request)
     }
 
     fn send_message_to_l1(
