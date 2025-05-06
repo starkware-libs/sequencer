ce1d446c5535545ea91d1ff861ea37b9749474e8
nadin-Starkware <nadin@starkware.co>
2025-05-06 10:00:08 +0300

    refactor(apollo_dashboard): move state reader panels to seperate module

---
+++ crates/apollo_dashboard/src/panels/state_reader.rs
@@ -0,0 +1,38 @@
+use blockifier::metrics::{
+    CLASS_CACHE_HITS,
+    CLASS_CACHE_MISSES,
+    NATIVE_CLASS_RETURNED,
+    STATE_READER_METRIC_RATE_DURATION,
+};
+use const_format::formatcp;
+
+use crate::dashboard::{Panel, PanelType};
+
+pub(crate) const PANEL_BLOCKIFIER_STATE_READER_CLASS_CACHE_MISS_RATIO: Panel = Panel::new(
+    "class_cache_miss_ratio",
+    "The ratio of cache misses when requesting compiled classes from the Blockifier State Reader",
+    formatcp!(
+        "100 * (rate({}[{}]) / (rate({}[{}]) + rate({}[{}])))",
+        CLASS_CACHE_MISSES.get_name(),
+        STATE_READER_METRIC_RATE_DURATION,
+        CLASS_CACHE_MISSES.get_name(),
+        STATE_READER_METRIC_RATE_DURATION,
+        CLASS_CACHE_HITS.get_name(),
+        STATE_READER_METRIC_RATE_DURATION
+    ),
+    PanelType::Graph,
+);
+pub(crate) const PANEL_BLOCKIFIER_STATE_READER_NATIVE_CLASS_RETURNED_RATIO: Panel = Panel::new(
+    "native_class_returned_ratio",
+    "The ratio of Native classes returned by the Blockifier State Reader",
+    formatcp!(
+        "100 * (rate({}[{}]) / (rate({}[{}]) + rate({}[{}])))",
+        NATIVE_CLASS_RETURNED.get_name(),
+        STATE_READER_METRIC_RATE_DURATION,
+        CLASS_CACHE_HITS.get_name(),
+        STATE_READER_METRIC_RATE_DURATION,
+        CLASS_CACHE_MISSES.get_name(),
+        STATE_READER_METRIC_RATE_DURATION,
+    ),
+    PanelType::Graph,
+);
