71853632f7c8bd0d30105bcd779f422ee8670c7e
Dori Medini <dori@starkware.co>
2025-05-07 11:29:45 +0300

    WIP: basic files
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_infra_utils/src/lib.rs
+++ crates/apollo_infra_utils/src/lib.rs
@@ -1,9 +1,13 @@
 #[cfg(any(feature = "testing", test))]
 pub mod cairo_compiler_version;
 pub mod command;
+#[cfg(test)]
+mod dummy_test;
 pub mod dumping;
 pub mod global_allocator;
 pub mod path;
+#[cfg(any(feature = "testing", test))]
+pub mod regression_test_utils;
 pub mod run_until;
 pub mod tasks;
 #[cfg(any(feature = "testing", test))]
