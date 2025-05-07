9e85891f66487ad4b03b16a5a91a786316575b0f
Aner Ben Efraim <aner@starkware.co>
2025-05-07 14:33:03 +0300

    test(blockifier): test secp_get_point_from_x

--- crates/blockifier/src/execution/syscalls/syscall_tests/secp.rs
+++ crates/blockifier/src/execution/syscalls/syscall_tests/secp.rs
@@ -30,6 +30,26 @@ fn test_secp256k1(runnable_version: RunnableCairo1) {
     );
 }
 
+#[cfg_attr(feature = "cairo_native", test_case(RunnableCairo1::Native; "Native"))]
+#[test_case(RunnableCairo1::Casm; "VM")]
+fn test_secp256k1_point_from_x(runnable_version: RunnableCairo1) {
+    let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1(runnable_version));
+    let chain_info = &ChainInfo::create_for_testing();
+    let mut state = test_state(chain_info, BALANCE, &[(test_contract, 1)]);
+
+    let calldata = Calldata(vec![].into());
+    let entry_point_call = CallEntryPoint {
+        entry_point_selector: selector_from_name("test_secp256k1_point_from_x"),
+        calldata,
+        ..trivial_external_entry_point_new(test_contract)
+    };
+
+    pretty_assertions::assert_eq!(
+        entry_point_call.execute_directly(&mut state).unwrap().execution,
+        CallExecution { gas_consumed: 186480, ..Default::default() }
+    );
+}
+
 #[cfg_attr(feature = "cairo_native",test_case(RunnableCairo1::Native; "Native"))]
 #[test_case(RunnableCairo1::Casm; "VM")]
 fn test_secp256r1(runnable_version: RunnableCairo1) {
