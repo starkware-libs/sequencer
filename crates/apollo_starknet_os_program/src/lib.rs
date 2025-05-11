28bdb78d8603f5257d00d4d7d70d2cb9e62bd074
dorimedini-starkware <dori@starkware.co>
2025-05-11 13:33:17 +0300

    feat(apollo_starknet_os_program): add feature for exposing cairo file source map (#6259)
    * feat(apollo_starknet_os_program): cairo file list test + fixer
    
    * feat(apollo_starknet_os_program): source file dump on build
    
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    ---------
    
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_starknet_os_program/src/lib.rs
+++ crates/apollo_starknet_os_program/src/lib.rs
@@ -1,1 +1,10 @@
+#[cfg(feature = "dump_source_files")]
+use std::collections::HashMap;
+#[cfg(feature = "dump_source_files")]
+use std::sync::LazyLock;
 
+#[cfg(feature = "dump_source_files")]
+pub static CAIRO_FILES_MAP: LazyLock<HashMap<String, String>> = LazyLock::new(|| {
+    serde_json::from_str(include_str!(concat!(env!("OUT_DIR"), "/cairo_files_map.json")))
+        .unwrap_or_else(|error| panic!("Failed to deserialize cairo_files_map.json: {error:?}."))
+});
