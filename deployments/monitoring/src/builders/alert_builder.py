c549465ba8bcc2074329d50f10aa35bcc6a69003
Idan Shamam <idan@starkware.co>
2025-05-13 18:19:45 +0300

    chore(deployment): import shared code

--- deployments/monitoring/src/builders/alert_builder.py
+++ deployments/monitoring/src/builders/alert_builder.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse
-import logging
-import colorlog
-import datetime
 import json
 import os
+import argparse
 
 from typing import Optional
 from tenacity import retry, stop_after_attempt, wait_fixed, before_sleep_log
@@ -17,7 +14,8 @@ from grafana_client.client import (
     GrafanaBadInputError,
 )
 
-from grafana10_objects import (
+from common.helpers import get_logger
+from common.grafana10_objects import (
     alert_rule_object,
     alert_query_object,
     alert_query_model_object,
@@ -25,100 +23,35 @@ from grafana10_objects import (
 )
 
 
-def setup_logger(debug: bool):
-    handler = colorlog.StreamHandler()
-    handler.setFormatter(
-        colorlog.ColoredFormatter(
-            "%(asctime)s %(log_color)s%(levelname)s%(reset)s %(message)s",
-            log_colors={
-                "DEBUG": "blue",
-                "INFO": "green",
-                "WARNING": "yellow",
-                "ERROR": "red",
-                "CRITICAL": "bold_red",
-            },
-        )
-    )
-
-    logger = colorlog.getLogger()
-    logger.setLevel(logging.DEBUG if debug else logging.INFO)
-    logger.handlers = []  # Clear any existing handlers
-    logger.addHandler(handler)
-    return logger
-
-
-parser = argparse.ArgumentParser(description="Build And Upload Grafana Alerts")
-parser.add_argument(
-    "-j",
-    "--dev_alerts_file",
-    type=str,
-    default="./examples/dev_grafana_alerts.json",
-    help="Path to the dev alerts file. Default is ./examples/dev_grafana_alerts.json",
-)
-parser.add_argument(
-    "-d",
-    "--debug",
-    action="store_true",
-    help="Enable debug logging. Default is False",
-)
-parser.add_argument(
-    "-u",
-    "--grafana_url",
-    type=str,
-    default="http://localhost:3000",
-    help="Grafana URL. Default is http://localhost:3000",
-)
-parser.add_argument(
-    "-n",
-    "--dry-run",
-    action="store_true",
-    help="Dry run, do not upload alerts to Grafana. Default is False",
-),
-parser.add_argument(
-    "-o",
-    "--out-dir",
-    type=str,
-    default="./out",
-    help="Output directory. Default is ./out",
-)
-parser.add_argument(
-    "-f",
-    "--folder_uid",
-    type=str,
-    default="",
-    help='Provide Grafana folder_uid for the alerts. Default is ""',
-)
-
-
 def create_alert_expression_model(conditions: list[dict[str, any]]):
-    logging.debug(f"Creating alert expression model {conditions}")
+    logger.debug(f"Creating alert expression model {conditions}")
     model = alert_expression_model_object.copy()
     model["conditions"] = conditions
-    logging.debug(f"Alert expression model created: {model}")
+    logger.debug(f"Alert expression model created: {model}")
     return model
 
 
 def create_alert_query_model(expr: str):
-    logging.debug(f"Creating alert query model {expr}")
+    logger.debug(f"Creating alert query model {expr}")
     model = alert_query_model_object.copy()
     model["expr"] = expr
-    logging.debug(f"Alert query model created: {model}")
+    logger.debug(f"Alert query model created: {model}")
     return model
 
 
 def create_alert_query(
     model: dict[any, any],
+    datasource_uid: str,
     ref_id: str = "A",
-    relative_time_range: dict[str, int] = {"from": 600, "to": 0},
-    datasource_uid: str = "PBFA97CFB590B2093",
+    relative_time_range: dict[str, int] = {"from": 600, "to": 0}
 ):
-    logging.debug(f"Creating alert query {model}")
+    logger.debug(f"Creating alert query {model}")
     alert_query = alert_query_object.copy()
     alert_query["refId"] = ref_id
     alert_query["relativeTimeRange"] = relative_time_range
     alert_query["datasourceUid"] = datasource_uid
     alert_query["model"] = model
-    logging.debug(f"Alert query created: {alert_query}")
+    logger.debug(f"Alert query created: {alert_query}")
     return alert_query
 
 
@@ -131,8 +64,9 @@ def create_alert_rule(
     _for: str,
     expr: str,
     conditions: list[dict[str, any]],
+    datasource_uid: str
 ):
-    logging.debug(f"Creating alert rule {name}")
+    logger.debug(f"Creating alert rule {name}")
     alert_rule = alert_rule_object.copy()
     alert_rule["name"] = name
     alert_rule["title"] = title
@@ -141,7 +75,7 @@ def create_alert_rule(
     alert_rule["intervalSec"] = interval_sec
     alert_rule["for"] = _for
     alert_rule["data"] = [
-        create_alert_query(model=create_alert_query_model(expr=expr)),
+        create_alert_query(datasource_uid=datasource_uid, model=create_alert_query_model(expr=expr)),
         create_alert_query(
             ref_id="B",
             relative_time_range={"from": 1, "to": 0},
@@ -149,12 +83,12 @@ def create_alert_rule(
             model=create_alert_expression_model(conditions=conditions),
         ),
     ]
-    logging.debug(f"Alert rule created: {alert_rule}")
+    logger.debug(f"Alert rule created: {alert_rule}")
     return alert_rule
 
 
 def get_all_folders(client: GrafanaApi) -> list[dict[str, any]]:
-    logging.debug("Getting all folders")
+    logger.debug("Getting all folders")
     return client.folder.get_all_folders()
 
 
@@ -172,12 +106,12 @@ def get_folder_uid(client: GrafanaApi, title: str) -> Optional[str]:
 def create_folder_return_uid(client: GrafanaApi, title: str) -> str:
     folder_uid = get_folder_uid(client=client, title=title)
     if folder_uid:
-        logging.info(f"Folder '{title}' already exists. Returning existing UID: {folder_uid}")
+        logger.info(f"Folder '{title}' already exists. Returning existing UID: {folder_uid}")
         return folder_uid
     else:
-        logging.info(f"Creating folder '{title}'")
+        logger.info(f"Creating folder '{title}'")
         folder = client.folder.create_folder(title)
-        logging.info(f"Folder '{title}' created successfully. {folder}")
+        logger.info(f"Folder '{title}' created successfully. {folder}")
         return folder["uid"]
 
 
@@ -186,22 +120,22 @@ def dump_alert(output_dir: str, alert: dict[str, any]) -> None:
     os.makedirs(output_dir, exist_ok=True)
     with open(alert_full_path, "w") as f:
         json.dump(alert, f, indent=2)
-    logging.info(f'Alert "{alert["name"]}" saved to {alert_full_path}')
+    logger.info(f'Alert "{alert["name"]}" saved to {alert_full_path}')
 
 
 def get_alert_rule_group(client: GrafanaApi, folder_uid: str, group_uid: str) -> str:
-    logging.debug(f'Getting alert rule group "{group_uid}"')
+    logger.debug(f'Getting alert rule group "{group_uid}"')
     rule_group = client.alertingprovisioning.get_rule_group(
         folder_uid=folder_uid, group_uid=group_uid
     )
-    logging.debug(f"Got alert group: {rule_group}")
+    logger.debug(f"Got alert group: {rule_group}")
     return rule_group
 
 
 @retry(
     stop=stop_after_attempt(10),
     wait=wait_fixed(2),
-    before_sleep=before_sleep_log(logging.getLogger(), logging.DEBUG),
+    before_sleep=before_sleep_log(logger=get_logger(name="tenacity_retry"), log_level="DEBUG"),
 )
 def update_alert_rule_group(
     client: GrafanaApi,
@@ -210,7 +144,7 @@ def update_alert_rule_group(
     alertrule_group: dict[any, any],
     disable_provenance=True,
 ) -> None:
-    logging.debug(f'Updating alert rule group "{group_uid}"')
+    logger.debug(f'Updating alert rule group "{group_uid}"')
 
     try:
         client.alertingprovisioning.update_rule_group(
@@ -219,19 +153,15 @@ def update_alert_rule_group(
             alertrule_group=alertrule_group,
             disable_provenance=disable_provenance,
         )
-        logging.info(f"Successfully updated alert rule group {group_uid}")
+        logger.info(f"Successfully updated alert rule group {group_uid}")
     except Exception as e:
-        logging.error(f"Failed to update alert rule group {group_uid}: {e}")
+        logger.error(f"Failed to update alert rule group {group_uid}: {e}")
         raise
 
 
-def main():
-    args = parser.parse_args()
-    logger = setup_logger(debug=args.debug)
-    start_time = datetime.datetime.now()
-    logger.info(
-        f'Starting to build grafana dashboard, time is {start_time.strftime("%Y-%m-%d %H:%M:%S")}'
-    )
+def alert_builder(args: argparse.Namespace):
+    global logger
+    logger = get_logger(name="alert_builder", debug=args.debug)
 
     with open(args.dev_alerts_file, "r") as f:
         dev_alerts = json.load(f)
@@ -255,47 +185,43 @@ def main():
                 _for=dev_alert["for"],
                 expr=dev_alert["expr"],
                 conditions=dev_alert["conditions"],
+                datasource_uid=args.datasource_uid,
             )
         )
 
     for alert in alerts:
         if args.debug:
-            logging.debug(json.dumps(alert))
+            logger.debug(json.dumps(alert))
         if not args.dry_run:
             try:
                 client.alertingprovisioning.create_alertrule(
-                    alertrule=alert, disable_provenance=True
+                    alertrule=alert, 
+                    disable_provenance=True,
                 )
-                logging.info(f'Alert "{alert["name"]}" uploaded to Grafana successfully')
+                logger.info(f'Alert "{alert["name"]}" uploaded to Grafana successfully')
 
             except GrafanaBadInputError as e:
                 if "alerting.alert-rule.conflict" in e.message:
-                    logging.warning(
-                        f'Alert "{alert["name"]}" already exists. Skipping creation. Conflict message: {e.message}'
-                    )
+                    logger.info(f'Alert "{alert["name"]}" already exists. Skipping creation.')
                 else:
                     # Handle other bad input errors
-                    logging.error(
+                    logger.error(
                         f'Failed to create alert "{alert["name"]}". Bad input: {e.message}'
                     )
             except GrafanaClientError as e:
                 # Handle other client-side errors (e.g., invalid request)
-                logging.error(
-                    f'Failed to create alert "{alert["name"]}". Client error: {e.message}'
-                )
+                logger.error(f'Failed to create alert "{alert["name"]}". Client error: {e.message}')
             except GrafanaServerError as e:
                 # Handle server-side errors (5xx errors)
-                logging.error(
-                    f'Failed to create alert "{alert["name"]}". Server error: {e.message}'
-                )
+                logger.error(f'Failed to create alert "{alert["name"]}". Server error: {e.message}')
             except GrafanaException as e:
                 # Catch any other Grafana-related exceptions
-                logging.error(
+                logger.error(
                     f'Failed to create alert "{alert["name"]}". Grafana error: {e.message}'
                 )
             except Exception as e:
                 # Catch any other exceptions (non-Grafana-related)
-                logging.error(f'Failed to create alert "{alert["name"]}". Unexpected error: {e}')
+                logger.error(f'Failed to create alert "{alert["name"]}". Unexpected error: {e}')
 
             try:
                 group_uid = alert["ruleGroup"]
@@ -310,16 +236,12 @@ def main():
                         group_uid=group_uid,
                         alertrule_group=rule_group,
                     )
-                    logging.info(f'Alert rule group "{group_uid}" updated successfully')
+                    logger.info(f'Alert rule group "{group_uid}" updated successfully')
             except Exception as e:
-                logging.error(f'Failed to update alert rule group "{alert["ruleGroup"]}". {e}')
+                logger.error(f'Failed to update alert rule group "{alert["ruleGroup"]}". {e}')
 
-        dump_alert(output_dir=args.out_dir, alert=alert)
+        if args.out_dir:
+            output_dir = f"{args.out_dir}/alerts"
+            dump_alert(output_dir=output_dir, alert=alert)
 
-    logging.info(
-        f'Done building grafana alerts, time is {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
-    )
-
-
-if __name__ == "__main__":
-    main()
+    logger.info("Done building grafana alerts")
