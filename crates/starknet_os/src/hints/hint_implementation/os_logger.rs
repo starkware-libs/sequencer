4f650ca5f43f4df75f56fd1ae5ca0b1540c59fdb
Nimrod Weiss <nimrod@starkware.co>
2025-05-14 16:17:41 +0300

    feat(starknet_os): impl log_enter_syscall hint

--- crates/starknet_os/src/hints/hint_implementation/os_logger.rs
+++ crates/starknet_os/src/hints/hint_implementation/os_logger.rs
@@ -1,12 +1,32 @@
-use blockifier::state::state_api::StateReader;
+use std::collections::HashMap;
 
+use blockifier::state::state_api::StateReader;
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
+    get_integer_from_var_name,
+    get_ptr_from_var_name,
+};
+use cairo_vm::hint_processor::hint_processor_definition::HintReference;
+use cairo_vm::serde::deserialize_program::ApTracking;
+use cairo_vm::types::program::Program;
+use cairo_vm::vm::vm_core::VirtualMachine;
+
+use crate::hint_processor::snos_hint_processor::SnosHintProcessor;
 use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
+use crate::hints::vars::Ids;
 
 pub(crate) fn os_logger_enter_syscall_prepare_exit_syscall<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { ap_tracking, ids_data, hint_processor, vm, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    let is_deprecated = true;
+    log_enter_syscall_helper(
+        hint_processor,
+        ids_data,
+        ap_tracking,
+        is_deprecated,
+        hint_processor.os_program,
+        vm,
+    )
 }
 
 pub(crate) fn os_logger_exit_syscall<S: StateReader>(
@@ -16,7 +36,42 @@ pub(crate) fn os_logger_exit_syscall<S: StateReader>(
 }
 
 pub(crate) fn log_enter_syscall<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { ap_tracking, ids_data, hint_processor, vm, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    let is_deprecated = false;
+    log_enter_syscall_helper(
+        hint_processor,
+        ids_data,
+        ap_tracking,
+        is_deprecated,
+        hint_processor.os_program,
+        vm,
+    )
+}
+
+fn log_enter_syscall_helper<S: StateReader>(
+    execution_helper: &mut SnosHintProcessor<'_, S>,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+    is_deprecated: bool,
+    os_program: &Program,
+    vm: &VirtualMachine,
+) -> OsHintResult {
+    let execution_helper = execution_helper.get_mut_current_execution_helper()?;
+    // TODO(Nimrod): Get `n_steps` by calling vm.get_current_step after we upgrade the VM.
+    let dummy_n_steps = 7;
+    let range_check_ptr =
+        get_ptr_from_var_name(Ids::RangeCheckPtr.into(), vm, ids_data, ap_tracking)?;
+    let selector = get_integer_from_var_name(Ids::Selector.into(), vm, ids_data, ap_tracking)?;
+
+    Ok(execution_helper.os_logger.enter_syscall(
+        selector.try_into()?,
+        is_deprecated,
+        dummy_n_steps,
+        range_check_ptr,
+        ids_data,
+        vm,
+        ap_tracking,
+        os_program,
+    )?)
 }
