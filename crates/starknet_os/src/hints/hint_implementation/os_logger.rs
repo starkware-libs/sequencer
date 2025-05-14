740c17623a4643b734ff25a2360c04bea9961d55
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-05-14 17:18:44 +0300

    refactor(starknet_os): move log_enter_syscall hint (#6515)

--- crates/starknet_os/src/hints/hint_implementation/os_logger.rs
+++ crates/starknet_os/src/hints/hint_implementation/os_logger.rs
@@ -14,3 +14,9 @@ pub(crate) fn os_logger_exit_syscall<S: StateReader>(
 ) -> OsHintResult {
     todo!()
 }
+
+pub(crate) fn log_enter_syscall<S: StateReader>(
+    HintArgs { .. }: HintArgs<'_, '_, S>,
+) -> OsHintResult {
+    todo!()
+}
