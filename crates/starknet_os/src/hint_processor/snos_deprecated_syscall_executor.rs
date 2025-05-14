d8167f7abe07a5dc8c54567b1b6ef42853ba50d0
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-05-14 17:30:24 +0300

    feat(starknet_os): impl deprecated storage read syscall (#6503)
    * refactor(starknet_os): impl deprecated syscall trait on snos hint processor
    
    * feat(starknet_os): impl deprecated storage write syscall
    
    * feat(starknet_os): impl deprecated storage read syscall

--- crates/starknet_os/src/hint_processor/snos_deprecated_syscall_executor.rs
+++ crates/starknet_os/src/hint_processor/snos_deprecated_syscall_executor.rs
@@ -202,7 +202,20 @@ impl<S: StateReader> DeprecatedSyscallExecutor for SnosHintProcessor<'_, S> {
         vm: &mut VirtualMachine,
         syscall_handler: &mut Self,
     ) -> DeprecatedSyscallResult<StorageReadResponse> {
-        todo!()
+        // TODO(Nimrod): Don't unwrap here, use the error handling mechanism.
+        let execution_helper = syscall_handler.get_mut_current_execution_helper().unwrap();
+        let value = *execution_helper
+            .tx_execution_iter
+            .tx_execution_info_ref
+            .as_mut()
+            .unwrap()
+            .call_info_tracker
+            .as_mut()
+            .unwrap()
+            .execute_code_read_iterator
+            .next()
+            .unwrap();
+        Ok(StorageReadResponse { value })
     }
 
     fn storage_write(
