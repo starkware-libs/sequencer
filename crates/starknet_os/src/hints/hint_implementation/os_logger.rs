c48bf32cfc16def1ef66f85c2f516b920af77f1e
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-05-15 10:21:01 +0300

    feat(starknet_os): impl os_log_exit_syscall hint (#6522)

--- crates/starknet_os/src/hints/hint_implementation/os_logger.rs
+++ crates/starknet_os/src/hints/hint_implementation/os_logger.rs
@@ -30,9 +30,24 @@ pub(crate) fn os_logger_enter_syscall_prepare_exit_syscall<S: StateReader>(
 }
 
 pub(crate) fn os_logger_exit_syscall<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { ap_tracking, ids_data, hint_processor, vm, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    let execution_helper =
+        hint_processor.execution_helpers_manager.get_mut_current_execution_helper()?;
+    let selector = get_integer_from_var_name(Ids::Selector.into(), vm, ids_data, ap_tracking)?;
+    // TODO(Nimrod): Get `n_steps` by calling vm.get_current_step after we upgrade the VM.
+    let dummy_n_steps = 7;
+    let range_check_ptr =
+        get_ptr_from_var_name(Ids::RangeCheckPtr.into(), vm, ids_data, ap_tracking)?;
+    Ok(execution_helper.os_logger.exit_syscall(
+        selector.try_into()?,
+        dummy_n_steps,
+        range_check_ptr,
+        ids_data,
+        vm,
+        ap_tracking,
+        hint_processor.os_program,
+    )?)
 }
 
 pub(crate) fn log_enter_syscall<S: StateReader>(
