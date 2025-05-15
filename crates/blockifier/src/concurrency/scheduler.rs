7ad14589e70b9a1f0d81dec94720f9a0b8b91250
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2025-05-15 16:07:20 +0300

    feat(blockifier): use dashmap in scheduler.rs (#6489)
    commit-id:50e42606

--- crates/blockifier/src/concurrency/scheduler.rs
+++ crates/blockifier/src/concurrency/scheduler.rs
@@ -2,7 +2,9 @@ use std::cmp::min;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::{Mutex, MutexGuard, TryLockError};
 
-use crate::concurrency::utils::lock_mutex_in_array;
+use dashmap::mapref::one::RefMut;
+use dashmap::DashMap;
+
 use crate::concurrency::TxIndex;
 
 #[cfg(test)]
@@ -63,8 +65,7 @@ pub struct Scheduler {
     // The index of the next transaction to commit.
     commit_index: Mutex<usize>,
     chunk_size: usize,
-    // TODO(Avi, 15/05/2024): Consider using RwLock instead of Mutex.
-    tx_statuses: Box<[Mutex<TransactionStatus>]>,
+    tx_statuses: DashMap<TxIndex, TransactionStatus>,
     // Set to true when all transactions have been committed, or when calling the halt_scheduler
     // procedure, providing a cheap way for all threads to exit their main loops.
     done_marker: AtomicBool,
@@ -77,9 +78,7 @@ impl Scheduler {
             validation_index: AtomicUsize::new(chunk_size),
             commit_index: Mutex::new(0),
             chunk_size,
-            tx_statuses: std::iter::repeat_with(|| Mutex::new(TransactionStatus::ReadyToExecute))
-                .take(chunk_size)
-                .collect(),
+            tx_statuses: DashMap::new(),
             done_marker: AtomicBool::new(false),
         }
     }
@@ -165,8 +164,8 @@ impl Scheduler {
         self.done_marker.store(true, Ordering::Release);
     }
 
-    fn lock_tx_status(&self, tx_index: TxIndex) -> MutexGuard<'_, TransactionStatus> {
-        lock_mutex_in_array(&self.tx_statuses, tx_index)
+    fn lock_tx_status(&self, tx_index: TxIndex) -> RefMut<'_, TxIndex, TransactionStatus> {
+        self.tx_statuses.entry(tx_index).or_insert(TransactionStatus::ReadyToExecute)
     }
 
     fn set_executed_status(&self, tx_index: TxIndex) {
@@ -175,7 +174,8 @@ impl Scheduler {
             *status,
             TransactionStatus::Executing,
             "Only executing transactions can gain status executed. Transaction {tx_index} is not \
-             executing. Transaction status: {status:?}."
+             executing. Transaction status: {:?}.",
+            *status
         );
         *status = TransactionStatus::Executed;
     }
@@ -186,7 +186,8 @@ impl Scheduler {
             *status,
             TransactionStatus::Aborting,
             "Only aborting transactions can be re-executed. Transaction {tx_index} is not \
-             aborting. Transaction status: {status:?}."
+             aborting. Transaction status: {:?}.",
+            *status
         );
         *status = TransactionStatus::ReadyToExecute;
     }
