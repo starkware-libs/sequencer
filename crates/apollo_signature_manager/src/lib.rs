b989999a6931673a4d76fc28c4efc3ef56daa034
Elin Tulchinsky <elin@starkware.co>
2025-05-22 13:59:54 +0300

    feat(apollo_signature_manager): add empty service, still no implementation

--- crates/apollo_signature_manager/src/lib.rs
+++ crates/apollo_signature_manager/src/lib.rs
@@ -1,1 +1,20 @@
+use apollo_signature_manager_types::SignatureManagerResult;
+use starknet_api::crypto::utils::{Message, PublicKey, RawSignature};
 
+/// Provides signing and signature verification functionality.
+pub struct SignatureManager;
+
+impl SignatureManager {
+    pub async fn sign(&self, _message: Message) -> SignatureManagerResult<RawSignature> {
+        unimplemented!("SignatureManager::sign is not implemented");
+    }
+
+    pub async fn verify(
+        &self,
+        _signature: RawSignature,
+        _message: Message,
+        _public_key: PublicKey,
+    ) -> SignatureManagerResult<bool> {
+        unimplemented!("SignatureManager::verify is not implemented");
+    }
+}
