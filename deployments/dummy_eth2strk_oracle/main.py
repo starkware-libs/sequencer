c308e1d0fe89dc7d5b002f606618346e041e178f
Idan Shamam <161198342+idan-starkware@users.noreply.github.com>
2025-05-22 16:04:50 +0300

    chore(deployment): add argument parser (#6611)

--- deployments/dummy_eth2strk_oracle/main.py
+++ deployments/dummy_eth2strk_oracle/main.py
@@ -1,24 +1,67 @@
 #!/usr/bin/env python
+
+import argparse
+from typing import Optional
 from constructs import Construct
 from cdk8s import App, Chart, Names, YamlOutputType
 from imports import k8s
 
-
-SERVICE_NAME = "dummy-eth2strk-oracle"
 SERVICE_PORT = 9000
-CLUSTER = "sequencer-dev"
-NAMESPACE = "dummy-eth2strk-oracle"
-IMAGE = "us-central1-docker.pkg.dev/starkware-dev/sequencer/dummy_eth2strk_oracle:latest"
+IMAGE = "ghcr.io/starkware-libs/sequencer/dummy_eth2strk_oracle:latest"
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--namespace", required=True, type=str, help="Kubernetes namespace.")
+    parser.add_argument(
+        "--create-ingress",
+        default=False,
+        action="store_true",
+        help="Enable ingress.",
+    )
+    parser.add_argument(
+        "--cluster",
+        type=str,
+        help="Kubernetes cluster name. Required if --create-ingress is used.",
+    )
+    parser.add_argument(
+        "--ingress-domain",
+        type=str,
+        help="Ingress domain. Required if --create-ingress is used.",
+    )
+
+    args = parser.parse_args()
+    assert not args.create_ingress or (
+        args.cluster and args.ingress_domain
+    ), "--cluster and --ingress-domain are required if --create-ingress is used."
+
+    return args
 
 
 class DummyEth2StrkOracle(Chart):
-    def __init__(self, scope: Construct, id: str, namespace: str):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        namespace: str,
+        create_ingress: bool,
+        cluster: Optional[str],
+        domain: Optional[str],
+    ):
         super().__init__(scope, id, disable_resource_name_hashes=True, namespace=namespace)
 
         self.label = {"app": Names.to_label_value(self, include_hash=False)}
-        self.host = f"{self.node.id}.{CLUSTER}.sw-dev.io"
+        self.cluster = cluster
+        self.create_ingress = create_ingress
+        self.domain = domain
 
-        k8s.KubeService(
+        self._get_service()
+        self._get_deployment()
+        if self.create_ingress:
+            self._get_ingress()
+
+    def _get_service(self):
+        return k8s.KubeService(
             self,
             "service",
             spec=k8s.ServiceSpec(
@@ -34,7 +77,8 @@ class DummyEth2StrkOracle(Chart):
             ),
         )
 
-        k8s.KubeDeployment(
+    def _get_deployment(self):
+        return k8s.KubeDeployment(
             self,
             "deployment",
             spec=k8s.DeploymentSpec(
@@ -56,7 +100,9 @@ class DummyEth2StrkOracle(Chart):
             ),
         )
 
-        k8s.KubeIngress(
+    def _get_ingress(self):
+        host = f"{self.node.id}.{self.cluster}.{self.domain}"
+        return k8s.KubeIngress(
             self,
             "ingress",
             metadata=k8s.ObjectMeta(
@@ -68,7 +114,7 @@ class DummyEth2StrkOracle(Chart):
                 ingress_class_name="nginx",
                 rules=[
                     k8s.IngressRule(
-                        host=self.host,
+                        host=host,
                         http=k8s.HttpIngressRuleValue(
                             paths=[
                                 k8s.HttpIngressPath(
@@ -89,7 +135,21 @@ class DummyEth2StrkOracle(Chart):
         )
 
 
-app = App(yaml_output_type=YamlOutputType.FOLDER_PER_CHART_FILE_PER_RESOURCE)
-DummyEth2StrkOracle(scope=app, id=SERVICE_NAME, namespace=NAMESPACE)
+def main():
+    args = argument_parser()
+    app = App(yaml_output_type=YamlOutputType.FOLDER_PER_CHART_FILE_PER_RESOURCE)
 
-app.synth()
+    DummyEth2StrkOracle(
+        scope=app,
+        id="dummy-eth2strk-oracle",
+        namespace=args.namespace,
+        cluster=args.cluster,
+        domain=args.ingress_domain,
+        create_ingress=args.create_ingress,
+    )
+
+    app.synth()
+
+
+if __name__ == "__main__":
+    main()
