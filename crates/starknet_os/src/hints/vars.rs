48d5445d8165abc82906b0da077ef416a8401d44
Nimrod Weiss <nimrod@starkware.co>
2025-05-26 09:07:17 +0300

    feat(starknet_os): implement get_execution_info syscall

--- crates/starknet_os/src/hints/vars.rs
+++ crates/starknet_os/src/hints/vars.rs
@@ -358,6 +358,7 @@ define_string_enum! {
     #[derive(Copy, Clone)]
     pub enum CairoStruct {
         (BigInt3, "starkware.starknet.core.os.data_availability.bls_field.BigInt3"),
+        (BlockInfo, "starkware.starknet.common.new_syscalls.BlockInfo"),
         (BuiltinParamsPtr, "starkware.starknet.core.os.builtins.BuiltinParams*"),
         (BuiltinPointersPtr, "starkware.starknet.core.os.builtins.BuiltinPointers*"),
         (CallContractResponse, "starkware.starknet.common.new_syscalls.CallContractResponse"),
@@ -397,6 +398,7 @@ define_string_enum! {
             EntryPointReturnValuesPtr,
             "starkware.starknet.core.os.execution.execute_entry_point.EntryPointReturnValues*"
         ),
+        (ExecutionInfo, "starkware.starknet.common.new_syscalls.ExecutionInfo"),
         (
             ExecutionContextPtr,
             "starkware.starknet.core.os.execution.execute_entry_point.ExecutionContext*"
@@ -415,6 +417,7 @@ define_string_enum! {
         (StorageReadRequest, "starkware.starknet.common.new_syscalls.StorageReadRequest"),
         (StorageWritePtr, "starkware.starknet.common.syscalls.StorageWrite*"),
         (StorageWriteRequest, "starkware.starknet.common.new_syscalls.StorageWriteRequest"),
+        (TxInfo, "starkware.starknet.common.new_syscalls.TxInfo"),
         (TxInfoPtr, "starkware.starknet.common.new_syscalls.TxInfo*"),
     }
 }
