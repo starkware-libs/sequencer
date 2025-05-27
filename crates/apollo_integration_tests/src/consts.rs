5dce4490e6b9d4287a02b423cf44dd3828a01e42
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-05-27 08:45:10 +0300

    chore(ci): modify the docker system test to use generated config file (#6770)

--- crates/apollo_integration_tests/src/consts.rs
+++
@@ -1,4 +0,0 @@
-pub const SINGLE_NODE_CONFIG_PATH: &str =
-    "config/sequencer/presets/system_test_presets/single_node/node_0/executable_0/node_config.json";
-
-pub const DATA_PREFIX_PATH: &str = "/data";
