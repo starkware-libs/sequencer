7275871c4d2426f836d08b8074a7d0e1039cd06b
Elin Tulchinsky <elin@starkware.co>
2025-05-28 16:50:57 +0300

    feat(apollo_signature_manager): add keystore to signature manager

--- crates/apollo_signature_manager/src/lib.rs
+++ crates/apollo_signature_manager/src/lib.rs
@@ -2,9 +2,15 @@ use apollo_signature_manager_types::SignatureManagerResult;
 use starknet_api::crypto::utils::{Message, PublicKey, RawSignature};
 
 /// Provides signing and signature verification functionality.
-pub struct SignatureManager;
+pub struct SignatureManager<KS: KeyStore> {
+    pub keystore: KS,
+}
 
-impl SignatureManager {
+impl<KS: KeyStore> SignatureManager<KS> {
+    fn _new(keystore: KS) -> Self {
+        Self { keystore }
+    }
+
     pub async fn sign(&self, _message: Message) -> SignatureManagerResult<RawSignature> {
         todo!("SignatureManager::sign is not yet implemented");
     }
