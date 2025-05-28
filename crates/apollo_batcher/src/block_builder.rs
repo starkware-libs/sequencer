9508076cf9064d57d65e7c04614e39b80395c7b4
Itamar Shechter <itamar@starkware.co>
2025-05-28 15:18:54 +0300

    fix(apollo_integration_tests): make invoke tx succeed by calling functions in test contract

--- crates/apollo_batcher/src/block_builder.rs
+++ crates/apollo_batcher/src/block_builder.rs
@@ -45,7 +45,7 @@ use starknet_api::state::ThinStateDiff;
 use starknet_api::transaction::TransactionHash;
 use thiserror::Error;
 use tokio::sync::Mutex;
-use tracing::{debug, error, info, trace};
+use tracing::{debug, error, info, trace, warn};
 
 use crate::block_builder::FailOnErrorCause::L1HandlerTransactionValidationFailed;
 use crate::cende_client_types::StarknetClientTransactionReceipt;
@@ -425,6 +425,13 @@ async fn collect_execution_results_and_stream_txs(
 
         match result {
             Ok(tx_execution_info) => {
+                if let Some(ref revert_error) = tx_execution_info.revert_error {
+                    warn!(
+                        "Transaction {} is reverted while accepted. Revert Error: {:?}",
+                        input_tx.tx_hash(),
+                        revert_error,
+                    );
+                }
                 *l2_gas_used = l2_gas_used
                     .checked_add(tx_execution_info.receipt.gas.l2_gas)
                     .expect("Total L2 gas overflow.");
@@ -449,7 +456,7 @@ async fn collect_execution_results_and_stream_txs(
             // feature is added.
             Err(err) => {
                 info!(
-                    "Transaction {} failed with error: {}.",
+                    "Transaction failed to execute. Transaction hash: {} with error: {}.",
                     tx_hash,
                     err.log_compatible_to_string()
                 );
