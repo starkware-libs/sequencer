00a04b5c096ccd6bac36815e3e459ce4b00d452d
Ayelet Zilber <ayelet@starkware.co>
2025-05-28 10:16:25 +0300

    feat(apollo_mempool): add tx_controller, wraps tx_pool

--- crates/apollo_mempool/src/lib.rs
+++ crates/apollo_mempool/src/lib.rs
@@ -3,6 +3,7 @@ pub mod config;
 pub mod mempool;
 pub mod metrics;
 pub(crate) mod suspended_transaction_pool;
+pub(crate) mod transaction_controller;
 pub(crate) mod transaction_pool;
 pub(crate) mod transaction_queue;
 pub(crate) mod utils;
