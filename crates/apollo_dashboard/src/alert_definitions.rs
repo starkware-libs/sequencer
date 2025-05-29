37f75736b8ec23485590d75d6b4a79c834a1e6cb
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2025-05-29 19:53:30 +0300

    chore(apollo_deployments): add filtering to alerts (#6876)

--- crates/apollo_dashboard/src/alert_definitions.rs
+++ crates/apollo_dashboard/src/alert_definitions.rs
@@ -18,12 +18,14 @@ use crate::dashboard::{
 };
 
 pub const DEV_ALERTS_JSON_PATH: &str = "Monitoring/sequencer/dev_grafana_alerts.json";
+// TODO(Tsabary): remove the following constant, and create relevant "_sum" and "_count" metric fns.
+const FILTER_STR: &str = "{cluster=~\"$cluster\", namespace=~\"$namespace\"}";
 
 const CONSENSUS_BLOCK_NUMBER_STUCK: Alert = Alert {
     name: "consensus_block_number_stuck",
     title: "Consensus block number stuck",
     alert_group: AlertGroup::Consensus,
-    expr: formatcp!("changes({}[10s])", CONSENSUS_BLOCK_NUMBER.get_name()),
+    expr: formatcp!("changes({}[10s])", CONSENSUS_BLOCK_NUMBER.get_name_with_filter()),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::LessThan,
         comparison_value: 1.0,
@@ -37,7 +39,10 @@ const GATEWAY_ADD_TX_RATE_DROP: Alert = Alert {
     name: "gateway_add_tx_rate_drop",
     title: "Gateway add_tx rate drop",
     alert_group: AlertGroup::Gateway,
-    expr: formatcp!("sum(rate({}[20m])) or vector(0)", GATEWAY_TRANSACTIONS_RECEIVED.get_name()),
+    expr: formatcp!(
+        "sum(rate({}[20m])) or vector(0)",
+        GATEWAY_TRANSACTIONS_RECEIVED.get_name_with_filter()
+    ),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::LessThan,
         comparison_value: 0.1,
@@ -52,9 +57,11 @@ const GATEWAY_ADD_TX_LATENCY_INCREASE: Alert = Alert {
     title: "Gateway avg add_tx latency increase",
     alert_group: AlertGroup::Gateway,
     expr: formatcp!(
-        "sum(rate({}_sum[1m]))/sum(rate({}_count[1m]))",
+        "sum(rate({}_sum{}[1m]))/sum(rate({}_count{}[1m]))",
         GATEWAY_ADD_TX_LATENCY.get_name(),
-        GATEWAY_ADD_TX_LATENCY.get_name()
+        FILTER_STR,
+        GATEWAY_ADD_TX_LATENCY.get_name(),
+        FILTER_STR
     ),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::GreaterThan,
@@ -69,7 +76,10 @@ const MEMPOOL_ADD_TX_RATE_DROP: Alert = Alert {
     name: "mempool_add_tx_rate_drop",
     title: "Mempool add_tx rate drop",
     alert_group: AlertGroup::Mempool,
-    expr: formatcp!("sum(rate({}[20m])) or vector(0)", MEMPOOL_TRANSACTIONS_RECEIVED.get_name()),
+    expr: formatcp!(
+        "sum(rate({}[20m])) or vector(0)",
+        MEMPOOL_TRANSACTIONS_RECEIVED.get_name_with_filter()
+    ),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::LessThan,
         comparison_value: 0.1,
@@ -83,7 +93,7 @@ const HTTP_SERVER_IDLE: Alert = Alert {
     name: "http_server_idle",
     title: "http server idle",
     alert_group: AlertGroup::HttpServer,
-    expr: formatcp!("rate(max({})[60m:])", ADDED_TRANSACTIONS_TOTAL.get_name()),
+    expr: formatcp!("rate(max({})[60m:])", ADDED_TRANSACTIONS_TOTAL.get_name_with_filter()),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::LessThan,
         comparison_value: 0.000001,
@@ -99,7 +109,7 @@ const MEMPOOL_GET_TXS_SIZE_DROP: Alert = Alert {
     name: "mempool_get_txs_size_drop",
     title: "Mempool get_txs size drop",
     alert_group: AlertGroup::Mempool,
-    expr: formatcp!("avg_over_time({}[20m])", MEMPOOL_GET_TXS_SIZE.get_name()),
+    expr: formatcp!("avg_over_time({}[20m])", MEMPOOL_GET_TXS_SIZE.get_name_with_filter()),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::LessThan,
         comparison_value: 0.01,
@@ -113,7 +123,7 @@ const MEMPOOL_POOL_SIZE_INCREASE: Alert = Alert {
     name: "mempool_pool_size_increase",
     title: "Mempool pool size increase",
     alert_group: AlertGroup::Mempool,
-    expr: formatcp!("{}", MEMPOOL_POOL_SIZE.get_name()),
+    expr: formatcp!("{}", MEMPOOL_POOL_SIZE.get_name_with_filter()),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::GreaterThan,
         comparison_value: 2000.0,
@@ -127,7 +137,7 @@ const CONSENSUS_ROUND_HIGH_AVG: Alert = Alert {
     name: "consensus_round_high_avg",
     title: "Consensus round high average",
     alert_group: AlertGroup::Consensus,
-    expr: formatcp!("avg_over_time({}[10m])", CONSENSUS_ROUND.get_name()),
+    expr: formatcp!("avg_over_time({}[10m])", CONSENSUS_ROUND.get_name_with_filter()),
     conditions: &[AlertCondition {
         comparison_op: AlertComparisonOp::GreaterThan,
         comparison_value: 0.2,
