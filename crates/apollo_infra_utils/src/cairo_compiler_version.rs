60d083a9fe2e455cfa3190bf1e8b4420d1fb6a40
dorimedini-starkware <dori@starkware.co>
2025-05-29 18:01:57 +0300

    feat(apollo_infra_utils): single point of truth for cairo1 version (#6894)
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_infra_utils/src/cairo_compiler_version.rs
+++ crates/apollo_infra_utils/src/cairo_compiler_version.rs
@@ -1,53 +1,5 @@
-use cached::proc_macro::cached;
-use serde::{Deserialize, Serialize};
+#[cfg(test)]
+#[path = "cairo_compiler_version_test.rs"]
+mod cairo_compiler_version_test;
 
-/// Objects for simple deserialization of `Cargo.toml`, to fetch the Cairo1 compiler version.
-/// The compiler itself isn't actually a dependency, so we compile by using the version of the
-/// `cairo-lang-casm` crate.
-/// The choice of this crate is arbitrary, as all compiler crate dependencies should have the
-/// same version.
-/// Deserializes:
-/// """
-/// ...
-/// [workspace.dependencies]
-/// ...
-/// cairo-lang-casm = VERSION
-/// ...
-/// """
-/// where `VERSION` can be a simple "x.y.z" version string or an object with a "version" field.
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum DependencyValue {
-    // cairo-lang-casm = "x.y.z".
-    String(String),
-    // cairo-lang-casm = { version = "x.y.z", .. }.
-    Object { version: String },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CairoLangCasmDependency {
-    #[serde(rename = "cairo-lang-casm")]
-    cairo_lang_casm: DependencyValue,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct WorkspaceFields {
-    dependencies: CairoLangCasmDependency,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CargoToml {
-    workspace: WorkspaceFields,
-}
-
-#[cached]
-/// Returns the version of the Cairo1 compiler defined in the root Cargo.toml (by checking the
-/// package version of one of the crates from the compiler in the dependencies).
-pub fn cairo1_compiler_version() -> String {
-    let cargo_toml: CargoToml = toml::from_str(include_str!("../../../Cargo.toml")).unwrap();
-    match cargo_toml.workspace.dependencies.cairo_lang_casm {
-        DependencyValue::String(version) | DependencyValue::Object { version } => {
-            version.trim_start_matches("=").to_string()
-        }
-    }
-}
+pub const CAIRO1_COMPILER_VERSION: &str = "2.12.0-dev.1";
