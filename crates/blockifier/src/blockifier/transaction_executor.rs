470c650f0fa4974e7ca77d3a3fe8a298def4f9a5
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2025-06-03 23:53:27 +0300

    refactor(blockifier): add WorkerPoolConfig (#6871)
    commit-id:ab431eee

--- crates/blockifier/src/blockifier/transaction_executor.rs
+++ crates/blockifier/src/blockifier/transaction_executor.rs
@@ -369,8 +369,7 @@ impl<S: StateReader + Send + Sync> TransactionExecutor<S> {
             worker_pool.run_and_wait(worker_executor.clone(), chunk.len());
         } else {
             // If a pool is not given, create a new pool and wait for it to finish.
-            let worker_pool =
-                WorkerPool::start(self.config.stack_size, self.config.concurrency_config.clone());
+            let worker_pool = WorkerPool::start(&self.config.get_worker_pool_config());
             worker_pool.run_and_wait(worker_executor.clone(), chunk.len());
             worker_pool.join();
         }
