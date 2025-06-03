00948c18045aab3104923f12c20577b90583674d
Nimrod Weiss <nimrod@starkware.co>
2025-06-03 12:06:43 +0300

    feat(starknet_os): implement read_ec_point_from_address hint

--- crates/starknet_os/src/hints/hint_implementation/secp.rs
+++ crates/starknet_os/src/hints/hint_implementation/secp.rs
@@ -1,7 +1,14 @@
 use blockifier::state::state_api::StateReader;
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
+    get_integer_from_var_name,
+    insert_value_into_ap,
+};
+use starknet_types_core::felt::Felt;
 
 use crate::hints::error::OsHintResult;
 use crate::hints::types::HintArgs;
+use crate::hints::vars::{CairoStruct, Ids};
+use crate::vm_utils::get_address_of_nested_fields;
 
 #[allow(clippy::result_large_err)]
 pub(crate) fn is_on_curve<S: StateReader>(HintArgs { .. }: HintArgs<'_, '_, S>) -> OsHintResult {
@@ -10,7 +17,21 @@ pub(crate) fn is_on_curve<S: StateReader>(HintArgs { .. }: HintArgs<'_, '_, S>) 
 
 #[allow(clippy::result_large_err)]
 pub(crate) fn read_ec_point_from_address<S: StateReader>(
-    HintArgs { .. }: HintArgs<'_, '_, S>,
+    HintArgs { ids_data, ap_tracking, vm, hint_processor, .. }: HintArgs<'_, '_, S>,
 ) -> OsHintResult {
-    todo!()
+    let not_on_curve =
+        get_integer_from_var_name(Ids::NotOnCurve.into(), vm, ids_data, ap_tracking)?;
+    let ec_point_address = get_address_of_nested_fields(
+        ids_data,
+        Ids::Response,
+        CairoStruct::SecpNewResponse,
+        vm,
+        ap_tracking,
+        &["ec_point"],
+        hint_processor.os_program,
+    )?;
+    let result =
+        if not_on_curve == Felt::ZERO { ec_point_address } else { vm.add_memory_segment() };
+    insert_value_into_ap(vm, result)?;
+    Ok(())
 }
