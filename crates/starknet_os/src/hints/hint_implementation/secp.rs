0be1705c1c82ccc39a166304d0c67bd886200f4b
Nimrod Weiss <nimrod@starkware.co>
2025-06-03 13:45:37 +0300

    feat(starknet_os): implement is_on_curve hint

--- crates/starknet_os/src/hints/hint_implementation/secp.rs
+++ crates/starknet_os/src/hints/hint_implementation/secp.rs
@@ -1,8 +1,12 @@
+use std::str::FromStr;
+
 use blockifier::state::state_api::StateReader;
 use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
     get_integer_from_var_name,
+    insert_value_from_var_name,
     insert_value_into_ap,
 };
+use num_bigint::BigInt;
 use starknet_types_core::felt::Felt;
 
 use crate::hints::error::OsHintResult;
@@ -11,8 +15,21 @@ use crate::hints::vars::{CairoStruct, Ids};
 use crate::vm_utils::get_address_of_nested_fields;
 
 #[allow(clippy::result_large_err)]
-pub(crate) fn is_on_curve<S: StateReader>(HintArgs { .. }: HintArgs<'_, '_, S>) -> OsHintResult {
-    todo!()
+pub(crate) fn is_on_curve<S: StateReader>(
+    HintArgs { exec_scopes, vm, ap_tracking, ids_data, .. }: HintArgs<'_, '_, S>,
+) -> OsHintResult {
+    // SECP_P = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
+    let secp_p: BigInt = BigInt::from_str(
+        "115792089237316195423570985008687907853269984665640564039457584007908834671663",
+    )
+    .expect("Failed to convert a numerical string to BigInt.");
+    let y: BigInt = exec_scopes.get("y")?;
+    let y_square_int: BigInt = exec_scopes.get("y_square_int")?;
+
+    let is_on_curve = ((y.pow(2)) % secp_p.to_owned()) == y_square_int;
+    insert_value_from_var_name("is_on_curve", Felt::from(is_on_curve), vm, ids_data, ap_tracking)?;
+
+    Ok(())
 }
 
 #[allow(clippy::result_large_err)]
