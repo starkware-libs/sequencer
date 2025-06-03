a831a0a78cd1a993e86d34b7105d06d55db497c7
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-06-03 11:55:47 +0300

    feat(starknet_os): implement prepare_state_entry_for_revert hint (#6912)

--- crates/starknet_os/src/hints/vars.rs
+++ crates/starknet_os/src/hints/vars.rs
@@ -86,6 +86,7 @@ define_string_enum! {
         (CompiledClass),
         (CompiledClassFacts),
         (CompiledClassHash),
+        (ContractAddressForRevert),
         (Descend),
         (DescentMap),
         (DictManager),
