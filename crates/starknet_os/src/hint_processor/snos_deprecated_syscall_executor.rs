a011bbcf1cf5d1fa206c21f1836882d240d7e7c9
rotem-starkware <rotem@starkware.co>
2025-06-03 15:09:41 +0300

    feat(starknet_os): impl deprecated get_contract_address syscall

--- crates/starknet_os/src/hint_processor/snos_deprecated_syscall_executor.rs
+++ crates/starknet_os/src/hint_processor/snos_deprecated_syscall_executor.rs
@@ -214,7 +214,9 @@ impl<S: StateReader> DeprecatedSyscallExecutor for SnosHintProcessor<'_, S> {
         vm: &mut VirtualMachine,
         syscall_handler: &mut Self,
     ) -> DeprecatedSyscallResult<GetContractAddressResponse> {
-        todo!()
+        Ok(GetContractAddressResponse {
+            address: Self::get_call_entry_point(syscall_handler).storage_address,
+        })
     }
 
     #[allow(clippy::result_large_err)]
