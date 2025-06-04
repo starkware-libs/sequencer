9310ac9f8efb7cb123fef50984de734b3afa2ebd
Gilad Chase <gilad@starkware.com>
2025-06-04 15:22:22 +0300

    feat(ci): dont lint external deps

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -51,7 +51,7 @@ class BaseCommand(Enum):
             return [["cargo", "test"] + package_args]
         elif self == BaseCommand.CLIPPY:
             clippy_args = package_args if len(package_args) > 0 else ["--workspace"]
-            return [["cargo", "clippy"] + clippy_args + ["--all-targets", "--all-features"]]
+            return [["cargo", "clippy"] + clippy_args + ["--all-targets", "--all-features", "--no-deps"]]
         elif self == BaseCommand.DOC:
             doc_args = package_args if len(package_args) > 0 else ["--workspace"]
             return [["cargo", "doc", "--document-private-items", "--no-deps"] + doc_args]
