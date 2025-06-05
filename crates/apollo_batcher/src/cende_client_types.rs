da53dfd06d442434abc7add1c1c562e687ad3145
Noam Spiegelstein <noam.s@starkware.co>
2025-06-05 09:21:48 +0300

    chore(apollo_batcher): add starknet client transaction receipt copy type

---
+++ crates/apollo_batcher/src/cende_client_types.rs
@@ -0,0 +1,121 @@
+//! Local copies of types from apollo_starknet_client for use in batcher, prefixed with
+//! StarknetClient.
+use std::collections::HashMap;
+
+// TODO(noamsp): find a way to share the TransactionReceipt from apollo_starknet_client and
+// remove this module.
+use serde::{Deserialize, Serialize};
+use starknet_api::core::{ContractAddress, EntryPointSelector, EthAddress};
+use starknet_api::execution_resources::GasVector;
+use starknet_api::hash::StarkHash;
+use starknet_api::transaction::fields::Fee;
+use starknet_api::transaction::{
+    Event,
+    L1ToL2Payload,
+    L2ToL1Payload,
+    TransactionHash,
+    TransactionOffsetInBlock,
+};
+
+#[derive(Debug, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+struct L1ToL2Nonce(pub StarkHash);
+
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+struct L1ToL2Message {
+    pub from_address: EthAddress,
+    pub to_address: ContractAddress,
+    pub selector: EntryPointSelector,
+    pub payload: L1ToL2Payload,
+    #[serde(default)]
+    pub nonce: L1ToL2Nonce,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+struct L2ToL1Message {
+    pub from_address: ContractAddress,
+    pub to_address: EthAddress,
+    pub payload: L2ToL1Payload,
+}
+
+// Note: the serialization is different from the one in starknet_api.
+#[derive(Hash, Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+enum Builtin {
+    #[serde(rename = "range_check_builtin")]
+    RangeCheck,
+    #[serde(rename = "pedersen_builtin")]
+    Pedersen,
+    #[serde(rename = "poseidon_builtin")]
+    Poseidon,
+    #[serde(rename = "ec_op_builtin")]
+    EcOp,
+    #[serde(rename = "ecdsa_builtin")]
+    Ecdsa,
+    #[serde(rename = "bitwise_builtin")]
+    Bitwise,
+    #[serde(rename = "keccak_builtin")]
+    Keccak,
+    // Note: in starknet_api this variant doesn't exist.
+    #[serde(rename = "output_builtin")]
+    Output,
+    #[serde(rename = "segment_arena_builtin")]
+    SegmentArena,
+    #[serde(rename = "add_mod_builtin")]
+    AddMod,
+    #[serde(rename = "mul_mod_builtin")]
+    MulMod,
+    #[serde(rename = "range_check96_builtin")]
+    RangeCheck96,
+}
+
+/// The execution resources used by a transaction.
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+struct ExecutionResources {
+    // Note: in starknet_api this field is named `steps`
+    pub n_steps: u64,
+    pub builtin_instance_counter: HashMap<Builtin, u64>,
+    // Note: in starknet_api this field is named `memory_holes`
+    pub n_memory_holes: u64,
+    // This field is missing in blocks created before v0.13.1, even if the feeder gateway is of
+    // that version
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub data_availability: Option<GasVector>,
+    // This field is missing in blocks created before v0.13.2, even if the feeder gateway is of
+    // that version
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub total_gas_consumed: Option<GasVector>,
+}
+
+/// Transaction execution status.
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord, Default)]
+enum TransactionExecutionStatus {
+    #[serde(rename = "SUCCEEDED")]
+    #[default]
+    Succeeded,
+    #[serde(rename = "REVERTED")]
+    Reverted,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub(crate) struct StarknetClientTransactionReceipt {
+    transaction_index: TransactionOffsetInBlock,
+    transaction_hash: TransactionHash,
+    #[serde(default)]
+    l1_to_l2_consumed_message: L1ToL2Message,
+    l2_to_l1_messages: Vec<L2ToL1Message>,
+    events: Vec<Event>,
+    #[serde(default)]
+    execution_resources: ExecutionResources,
+    actual_fee: Fee,
+    // TODO(Yair): Check if we can remove the serde(default).
+    #[serde(default)]
+    execution_status: TransactionExecutionStatus,
+    // Note that in starknet_api this field is named `revert_reason`.
+    // Assumption: if the transaction execution status is Succeeded, then revert_error is None, and
+    // if the transaction execution status is Reverted, then revert_error is Some.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    revert_error: Option<String>,
+}
