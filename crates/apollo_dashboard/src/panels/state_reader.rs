288bc2e9c45c36c2f456b4c336751307a5a8b1f2
AvivYossef-starkware <141143145+AvivYossef-starkware@users.noreply.github.com>
2025-06-09 13:00:31 +0300

    refactor(apollo_dashboard): state reader metrics (#6986)

--- crates/apollo_dashboard/src/panels/state_reader.rs
+++ crates/apollo_dashboard/src/panels/state_reader.rs
@@ -13,7 +13,7 @@ pub(crate) const PANEL_BLOCKIFIER_STATE_READER_CLASS_CACHE_MISS_RATIO: Panel = P
     "class_cache_miss_ratio",
     "The ratio of cache misses when requesting compiled classes from the Blockifier State Reader",
     formatcp!(
-        "100 * (rate({}[{}]) / (rate({}[{}]) + rate({}[{}])))",
+        "100 * (increase({}[{}]) / (increase({}[{}]) + increase({}[{}])))",
         CLASS_CACHE_MISSES.get_name_with_filter(),
         STATE_READER_METRIC_RATE_DURATION,
         CLASS_CACHE_MISSES.get_name_with_filter(),
@@ -23,11 +23,12 @@ pub(crate) const PANEL_BLOCKIFIER_STATE_READER_CLASS_CACHE_MISS_RATIO: Panel = P
     ),
     PanelType::Graph,
 );
+
 pub(crate) const PANEL_BLOCKIFIER_STATE_READER_NATIVE_CLASS_RETURNED_RATIO: Panel = Panel::new(
     "native_class_returned_ratio",
     "The ratio of Native classes returned by the Blockifier State Reader",
     formatcp!(
-        "100 * (rate({}[{}]) / (rate({}[{}]) + rate({}[{}])))",
+        "100 * (increase({}[{}]) / (increase({}[{}]) + increase({}[{}])))",
         NATIVE_CLASS_RETURNED.get_name_with_filter(),
         STATE_READER_METRIC_RATE_DURATION,
         CLASS_CACHE_HITS.get_name_with_filter(),
