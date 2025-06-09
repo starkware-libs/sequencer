d7db1d14cc8012acac731ffe1eae1d1cc4af9d88
Alon Lukatch <alon.l@starkware.co>
2025-06-09 15:42:29 +0300

    apollo_consensus_orchestrator: rename ProposalResult and and error variants for build

--- crates/apollo_consensus_orchestrator/src/build_proposal.rs
+++ crates/apollo_consensus_orchestrator/src/build_proposal.rs
@@ -29,8 +29,8 @@ use tokio_util::task::AbortOnDropHandle;
 use tracing::{debug, error, info, trace, warn};
 
 use crate::sequencer_consensus_context::{
+    BuildProposalResult,
     BuiltProposals,
-    ProposalResult,
     SequencerConsensusContextDeps,
 };
 use crate::utils::{
@@ -106,7 +106,9 @@ pub(crate) async fn build_proposal(mut args: ProposalBuildArguments) {
     }
 }
 
-async fn initiate_build(args: &ProposalBuildArguments) -> ProposalResult<ConsensusBlockInfo> {
+pub(crate) async fn initiate_build(
+    args: &ProposalBuildArguments,
+) -> BuildProposalResult<ConsensusBlockInfo> {
     let batcher_timeout = chrono::Duration::from_std(args.batcher_timeout)
         .expect("Can't convert timeout to chrono::Duration");
     let timestamp = args.deps.time.unix_now();
