b810d56f08cac330f0d76e873044f6750269bc52
Elin Tulchinsky <elin@starkware.co>
2025-06-12 12:02:58 +0300

    feat(apollo_signature_manager): convert to server

--- crates/apollo_signature_manager/src/lib.rs
+++ crates/apollo_signature_manager/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod communication;
 
+use apollo_infra::component_definitions::ComponentStarter;
 use apollo_signature_manager_types::{
     KeyStore,
     PeerId,
     SignatureManagerError,
     SignatureManagerResult,
 };
+use async_trait::async_trait;
 use blake2s::blake2s_to_felt;
 use starknet_api::block::BlockHash;
 use starknet_api::crypto::utils::{PublicKey, RawSignature};
@@ -49,6 +51,13 @@ impl<KS: KeyStore> SignatureManager<KS> {
         Ok(signature.into())
     }
 }
+
+pub fn create_signature_manager<KS: KeyStore>(key_store: KS) -> SignatureManager<KS> {
+    SignatureManager::new(key_store)
+}
+
+#[async_trait]
+impl<KS: KeyStore> ComponentStarter for SignatureManager<KS> {}
 
 // Utils.
 
