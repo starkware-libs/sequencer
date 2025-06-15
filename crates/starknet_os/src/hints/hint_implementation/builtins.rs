5bb331082c3b621212b3471ed50951ee540f3049
Nimrod Weiss <nimrod@starkware.co>
2025-06-15 13:33:40 +0300

    starknet_os: fix update_builtin_ptrs hint

--- crates/starknet_os/src/hints/hint_implementation/builtins.rs
+++ crates/starknet_os/src/hints/hint_implementation/builtins.rs
@@ -65,8 +65,9 @@ pub(crate) fn update_builtin_ptrs<S: StateReader>(
     HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
 ) -> OsHintResult {
     let n_builtins = get_integer_from_var_name(Ids::NBuiltins.into(), vm, ids_data, ap_tracking)?;
-
-    let builtins_encoding_address = get_address_of_nested_fields(
+    let n_selected_builtins =
+        get_integer_from_var_name(Ids::NSelectedBuiltins.into(), vm, ids_data, ap_tracking)?;
+    let builtins_encoding_address = vm.get_relocatable(get_address_of_nested_fields(
         ids_data,
         Ids::BuiltinParams,
         CairoStruct::BuiltinParamsPtr,
@@ -74,65 +75,55 @@ pub(crate) fn update_builtin_ptrs<S: StateReader>(
         ap_tracking,
         &["builtin_encodings"],
         hint_processor.os_program,
-    )?;
-
-    let builtins_encoding = vm.get_relocatable(builtins_encoding_address)?;
-
-    let n_selected_builtins =
-        get_integer_from_var_name(Ids::NSelectedBuiltins.into(), vm, ids_data, ap_tracking)?;
-
+    )?)?;
     let selected_encodings =
         get_ptr_from_var_name(Ids::SelectedEncodings.into(), vm, ids_data, ap_tracking)?;
-
-    let builtin_ptrs = get_ptr_from_var_name(Ids::BuiltinPtrs.into(), vm, ids_data, ap_tracking)?;
-
-    let orig_builtin_ptrs = builtin_ptrs;
-
+    let all_builtins: Vec<Felt> = vm
+        .get_integer_range(builtins_encoding_address, felt_to_usize(&n_builtins)?)?
+        .into_iter()
+        .map(|x| x.into_owned())
+        .collect();
+    let selected_builtins: Vec<Felt> = vm
+        .get_integer_range(selected_encodings, felt_to_usize(&n_selected_builtins)?)?
+        .into_iter()
+        .map(|x| x.into_owned())
+        .collect();
+    let orig_builtin_ptrs_address = get_address_of_nested_fields(
+        ids_data,
+        Ids::BuiltinPtrs,
+        CairoStruct::BuiltinPointersPtr,
+        vm,
+        ap_tracking,
+        &["selectable"],
+        hint_processor.os_program,
+    )?;
     let selected_ptrs = get_ptr_from_var_name(Ids::SelectedPtrs.into(), vm, ids_data, ap_tracking)?;
-
-    let all_builtins = vm.get_continuous_range(builtins_encoding, felt_to_usize(&n_builtins)?)?;
-
-    let selected_builtins =
-        vm.get_continuous_range(selected_encodings, felt_to_usize(&n_selected_builtins)?)?;
 
     let mut returned_builtins: Vec<MaybeRelocatable> = Vec::new();
     let mut selected_builtin_offset: usize = 0;
 
     for (i, builtin) in all_builtins.iter().enumerate() {
-        // For debugging purposes, try to decode the builtin name - ignore failures.
-        let decoded_builtin: Option<String> = match builtin {
-            MaybeRelocatable::Int(builtin_value) => as_cairo_short_string(builtin_value),
-            MaybeRelocatable::RelocatableValue(builtin_ptr) => vm
-                .get_integer(*builtin_ptr)
-                .map(|felt| as_cairo_short_string(&felt))
-                .unwrap_or(None),
-        };
         if selected_builtins.contains(builtin) {
-            // TODO(Dori): consider computing the address of the selected builtin via the
-            //   `get_address_of_nested_fields` utility. See `OsLogger::insert_builtins` for an
-            //   example for accessing fields of `CairoStruct::BuiltinPointers`.
             returned_builtins.push(
-                vm.get_maybe(&(selected_ptrs + selected_builtin_offset)?).ok_or_else(|| {
+                vm.get_maybe(&(selected_ptrs + selected_builtin_offset)?).ok_or(
                     OsHintError::MissingSelectedBuiltinPtr {
-                        builtin: builtin.clone(),
-                        decoded: decoded_builtin,
-                    }
-                })?,
+                        builtin: builtin.into(),
+                        decoded: as_cairo_short_string(builtin),
+                    },
+                )?,
             );
             selected_builtin_offset += 1;
         } else {
-            // The builtin is unselected, hence its value is the same as before calling the program.
-            returned_builtins.push(vm.get_maybe(&(orig_builtin_ptrs + i)?).ok_or_else(|| {
+            returned_builtins.push(vm.get_maybe(&(orig_builtin_ptrs_address + i)?).ok_or(
                 OsHintError::MissingUnselectedBuiltinPtr {
-                    builtin: builtin.clone(),
-                    decoded: decoded_builtin,
-                }
-            })?);
+                    builtin: builtin.into(),
+                    decoded: as_cairo_short_string(builtin),
+                },
+            )?);
         }
     }
 
-    let return_builtin_ptrs_base = vm.add_memory_segment();
-    vm.load_data(return_builtin_ptrs_base, &returned_builtins)?;
+    let return_builtin_ptrs_base = vm.gen_arg(&returned_builtins)?;
     insert_value_from_var_name(
         Ids::ReturnBuiltinPtrs.into(),
         return_builtin_ptrs_base,
