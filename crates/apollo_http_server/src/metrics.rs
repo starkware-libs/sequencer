091bd6af0f19b4d027f3e5bf49dd17dd1624ced2
Shahak Shama <shahak@starkware.co>
2025-06-18 17:20:54 +0300

    apollo_http_server: add internal errors counter metric

--- crates/apollo_http_server/src/metrics.rs
+++ crates/apollo_http_server/src/metrics.rs
@@ -11,6 +11,7 @@ define_metrics!(
         MetricCounter { ADDED_TRANSACTIONS_TOTAL, "http_server_added_transactions_total", "Total number of transactions added", init = 0 },
         MetricCounter { ADDED_TRANSACTIONS_SUCCESS, "http_server_added_transactions_success", "Number of successfully added transactions", init = 0 },
         MetricCounter { ADDED_TRANSACTIONS_FAILURE, "http_server_added_transactions_failure", "Number of faulty added transactions", init = 0 },
+        MetricCounter { ADDED_TRANSACTIONS_INTERNAL_ERROR, "http_server_added_transactions_internal_error", "Number of faulty added transactions failing on internal error", init = 0 },
     },
 );
 
@@ -19,4 +20,5 @@ pub(crate) fn init_metrics() {
     ADDED_TRANSACTIONS_TOTAL.register();
     ADDED_TRANSACTIONS_SUCCESS.register();
     ADDED_TRANSACTIONS_FAILURE.register();
+    ADDED_TRANSACTIONS_INTERNAL_ERROR.register();
 }
