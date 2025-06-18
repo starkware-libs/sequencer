52d1395236367a55a2c088afa275b097a456e46e
Itay Tsabary <itayt@starkware.co>
2025-06-18 23:28:09 +0300

    more verbose test message

--- crates/apollo_deployments/src/deployment_definitions_test.rs
+++ crates/apollo_deployments/src/deployment_definitions_test.rs
@@ -32,6 +32,13 @@ fn load_and_process_service_config_files() {
         .expect("Couldn't set working dir.");
     for deployment in DEPLOYMENTS.iter().flat_map(|f| f()) {
         for service_config_paths in deployment.get_config_file_paths().into_iter() {
+            println!(
+                "Loading deployment {} in path {:?} with application files {:?} ... ",
+                deployment.get_deployment_name(),
+                deployment.deployment_file_path(),
+                service_config_paths
+            );
+
             let config_file_args: Vec<String> = service_config_paths
                 .into_iter()
                 .flat_map(|path| vec![CONFIG_FILE_ARG.to_string(), path])
@@ -40,7 +47,16 @@ fn load_and_process_service_config_files() {
             let mut config_load_command: Vec<String> = vec!["command_name_placeholder".to_string()];
             config_load_command.extend(config_file_args);
             let load_result = SequencerNodeConfig::load_and_process(config_load_command);
-            assert!(load_result.is_ok());
+
+            println!("load_result {:?} ", load_result);
+
+            assert!(
+                load_result.is_ok(),
+                "Loading deployment {} in path {:?} resulted in error {}",
+                deployment.get_deployment_name(),
+                deployment.get_base_app_config_file_path(),
+                load_result.as_ref().err().unwrap()
+            );
         }
     }
 }
