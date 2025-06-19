3895546156ff61c0ccb45e48941829f448c1370e
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-06-19 14:25:38 +0300

    starknet_os,blockifier: os syscall handler returns explicit segment (#7432)

--- crates/starknet_os/src/hint_processor/snos_syscall_executor.rs
+++ crates/starknet_os/src/hint_processor/snos_syscall_executor.rs
@@ -23,6 +23,7 @@ use blockifier::execution::syscalls::vm_syscall_utils::{
     MetaTxV0Response,
     ReplaceClassRequest,
     ReplaceClassResponse,
+    RevertData,
     SelfOrRevert,
     SendMessageToL1Request,
     SendMessageToL1Response,
@@ -70,7 +71,7 @@ pub enum SnosSyscallError {
     #[error(transparent)]
     Memory(#[from] MemoryError),
     #[error("Syscall revert.")]
-    Revert { error_data: Vec<Felt> },
+    Revert(RevertData),
     #[error(transparent)]
     SyscallExecutorBase(#[from] SyscallExecutorBaseError),
     #[error(transparent)]
@@ -93,7 +94,7 @@ impl TryExtractRevert for SnosSyscallError {
             Self::SyscallExecutorBase(base_error) => {
                 base_error.try_extract_revert().map_original(Self::SyscallExecutorBase)
             }
-            Self::Revert { error_data } => SelfOrRevert::Revert(error_data),
+            Self::Revert(revert_data) => SelfOrRevert::Revert(revert_data),
             Self::ExecutionHelper(_)
             | Self::Math(_)
             | Self::InvalidResourceBounds(_)
@@ -102,8 +103,8 @@ impl TryExtractRevert for SnosSyscallError {
         }
     }
 
-    fn as_revert(error_data: Vec<Felt>) -> Self {
-        SyscallExecutorBaseError::Revert { error_data }.into()
+    fn as_revert(revert_data: RevertData) -> Self {
+        Self::Revert(revert_data)
     }
 }
 
@@ -142,9 +143,7 @@ impl<S: StateReader> SyscallExecutor for SnosHintProcessor<'_, S> {
         remaining_gas: &mut u64,
     ) -> Result<CallContractResponse, Self::Error> {
         if request.function_selector == selector_from_name(EXECUTE_ENTRY_POINT_NAME) {
-            return Err(Self::Error::Revert {
-                error_data: vec![Felt::from_hex_unchecked(INVALID_ARGUMENT)],
-            });
+            return Err(handle_failure(Felt::from_hex_unchecked(INVALID_ARGUMENT)));
         }
         call_contract_helper(vm, syscall_handler, remaining_gas)
     }
@@ -170,9 +169,8 @@ impl<S: StateReader> SyscallExecutor for SnosHintProcessor<'_, S> {
         let retdata_base = vm.add_temporary_segment();
         vm.load_data(retdata_base, &retdata).map_err(SyscallExecutorBaseError::from)?;
         if execution.failed {
-            return Err(Self::Error::from(SyscallExecutorBaseError::Revert {
-                error_data: execution.retdata.0.clone(),
-            }));
+            let revert_data = RevertData::new_temp(execution.retdata.0.clone());
+            return Err(SnosSyscallError::Revert(revert_data));
         };
         Ok(DeployResponse {
             contract_address: deployed_contract_address,
@@ -274,9 +272,7 @@ impl<S: StateReader> SyscallExecutor for SnosHintProcessor<'_, S> {
         remaining_gas: &mut u64,
     ) -> Result<MetaTxV0Response, Self::Error> {
         if request.entry_point_selector != selector_from_name(EXECUTE_ENTRY_POINT_NAME) {
-            return Err(Self::Error::Revert {
-                error_data: vec![Felt::from_hex_unchecked(INVALID_ARGUMENT)],
-            });
+            return Err(handle_failure(Felt::from_hex_unchecked(INVALID_ARGUMENT)));
         }
         call_contract_helper(vm, syscall_handler, remaining_gas)
     }
@@ -430,8 +426,14 @@ fn call_contract_helper(
     if next_call_execution.failed {
         let mut retdata = retdata.clone();
         retdata.push(revert_error_code);
-        return Err(SnosSyscallError::Revert { error_data: retdata });
+        let revert_data = RevertData::new_temp(retdata);
+        return Err(SnosSyscallError::Revert(revert_data));
     };
 
     Ok(CallContractResponse { segment: write_to_temp_segment(retdata, vm)? })
+}
+
+/// Returns an revert error with the given error code which will be written to a normal segment.
+fn handle_failure(error_code: Felt) -> SnosSyscallError {
+    SnosSyscallError::Revert(RevertData::new_normal(vec![error_code]))
 }
