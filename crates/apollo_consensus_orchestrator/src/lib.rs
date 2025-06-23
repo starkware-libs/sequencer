7d8cac80dda8118da88c3c1d5f4947ba0ef54d79
asmaa-starkware <163830216+asmaastarkware@users.noreply.github.com>
2025-06-23 09:32:04 +0300

    apollo_consensus_orchestrator: move validate funcs to validate_proposal.rs (#7505)

--- crates/apollo_consensus_orchestrator/src/lib.rs
+++ crates/apollo_consensus_orchestrator/src/lib.rs
@@ -8,6 +8,9 @@ pub mod sequencer_consensus_context;
 #[allow(missing_docs)]
 pub mod build_proposal;
 
+#[allow(missing_docs)]
+pub mod validate_proposal;
+
 /// Centralized and decentralized communication types and functionality.
 #[allow(missing_docs)]
 pub mod cende;
