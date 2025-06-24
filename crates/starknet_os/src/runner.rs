aba497868dc0bf863c02547cae794d16e0882ed4
Aner Ben Efraim <aner@starkware.co>
2025-06-24 11:20:42 +0300

    starknet_os: refactor run_os

--- crates/starknet_os/src/runner.rs
+++ crates/starknet_os/src/runner.rs
@@ -1,9 +1,14 @@
 use apollo_starknet_os_program::OS_PROGRAM;
 use blockifier::state::state_api::StateReader;
 use cairo_vm::cairo_run::CairoRunConfig;
+use cairo_vm::hint_processor::hint_processor_definition::HintProcessor;
 use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::program::Program;
 use cairo_vm::vm::errors::vm_exception::VmException;
+use cairo_vm::vm::runners::cairo_pie::CairoPie;
 use cairo_vm::vm::runners::cairo_runner::CairoRunner;
+#[cfg(feature = "include_program_output")]
+use starknet_types_core::felt::Felt;
 
 use crate::errors::StarknetOsError;
 use crate::hint_processor::aggregator_hint_processor::AggregatorInput;
@@ -13,6 +18,76 @@ use crate::hint_processor::snos_hint_processor::SnosHintProcessor;
 use crate::io::os_input::{OsHints, StarknetOsInput};
 use crate::io::os_output::{StarknetAggregatorRunnerOutput, StarknetOsRunnerOutput};
 use crate::metrics::OsMetrics;
+
+pub struct RunnerReturnObject {
+    #[cfg(feature = "include_program_output")]
+    pub raw_output: Vec<Felt>,
+    pub cairo_pie: CairoPie,
+    pub cairo_runner: CairoRunner,
+}
+
+// TODO(Aner): replace the return type with Result<StarknetRunnerOutput,...>
+// TODO(Aner): Make generic (CommonHintProcessor trait) depend on testing flag.
+fn run_program<'a, HP: HintProcessor + CommonHintProcessor<'a>>(
+    layout: LayoutName,
+    program: &Program,
+    hint_processor: &mut HP,
+) -> Result<RunnerReturnObject, StarknetOsError> {
+    // Init CairoRunConfig.
+    let cairo_run_config =
+        CairoRunConfig { layout, relocate_mem: true, trace_enabled: true, ..Default::default() };
+    let allow_missing_builtins = cairo_run_config.allow_missing_builtins.unwrap_or(false);
+
+    // Init cairo runner.
+    let mut cairo_runner = CairoRunner::new(
+        program,
+        cairo_run_config.layout,
+        cairo_run_config.dynamic_layout_params,
+        cairo_run_config.proof_mode,
+        cairo_run_config.trace_enabled,
+        cairo_run_config.disable_trace_padding,
+    )?;
+
+    // Init the Cairo VM.
+    let end = cairo_runner.initialize(allow_missing_builtins)?;
+
+    // Run the Cairo VM.
+    cairo_runner
+        .run_until_pc(end, hint_processor)
+        .map_err(|err| Box::new(VmException::from_vm_error(&cairo_runner, err)))?;
+
+    // End the Cairo VM run.
+    let disable_finalize_all = false;
+    cairo_runner.end_run(
+        cairo_run_config.disable_trace_padding,
+        disable_finalize_all,
+        hint_processor,
+    )?;
+
+    if cairo_run_config.proof_mode {
+        cairo_runner.finalize_segments()?;
+    }
+
+    #[cfg(feature = "include_program_output")]
+    let raw_output = crate::io::os_output::get_run_output(&cairo_runner.vm)?;
+
+    cairo_runner.vm.verify_auto_deductions().map_err(StarknetOsError::VirtualMachineError)?;
+    cairo_runner
+        .read_return_values(allow_missing_builtins)
+        .map_err(StarknetOsError::RunnerError)?;
+    cairo_runner
+        .relocate(cairo_run_config.relocate_mem)
+        .map_err(|e| StarknetOsError::VirtualMachineError(e.into()))?;
+
+    // Parse the Cairo VM output.
+    let cairo_pie = cairo_runner.get_cairo_pie().map_err(StarknetOsError::RunnerError)?;
+    Ok(RunnerReturnObject {
+        #[cfg(feature = "include_program_output")]
+        raw_output,
+        cairo_pie,
+        cairo_runner,
+    })
+}
 
 pub fn run_os<S: StateReader>(
     layout: LayoutName,
@@ -28,24 +103,6 @@ pub fn run_os<S: StateReader>(
     }: OsHints,
     state_readers: Vec<S>,
 ) -> Result<StarknetOsRunnerOutput, StarknetOsError> {
-    // Init CairoRunConfig.
-    let cairo_run_config =
-        CairoRunConfig { layout, relocate_mem: true, trace_enabled: true, ..Default::default() };
-    let allow_missing_builtins = cairo_run_config.allow_missing_builtins.unwrap_or(false);
-
-    // Init cairo runner.
-    let mut cairo_runner = CairoRunner::new(
-        &OS_PROGRAM,
-        cairo_run_config.layout,
-        cairo_run_config.dynamic_layout_params,
-        cairo_run_config.proof_mode,
-        cairo_run_config.trace_enabled,
-        cairo_run_config.disable_trace_padding,
-    )?;
-
-    // Init the Cairo VM.
-    let end = cairo_runner.initialize(allow_missing_builtins)?;
-
     // Create the hint processor.
     let mut snos_hint_processor = SnosHintProcessor::new(
         &OS_PROGRAM,
@@ -57,52 +114,23 @@ pub fn run_os<S: StateReader>(
         state_readers,
     )?;
 
-    // Run the Cairo VM.
-    cairo_runner
-        .run_until_pc(end, &mut snos_hint_processor)
-        .map_err(|err| Box::new(VmException::from_vm_error(&cairo_runner, err)))?;
-
-    // End the Cairo VM run.
-    let disable_finalize_all = false;
-    cairo_runner.end_run(
-        cairo_run_config.disable_trace_padding,
-        disable_finalize_all,
-        &mut snos_hint_processor,
-    )?;
-
-    if cairo_run_config.proof_mode {
-        cairo_runner.finalize_segments()?;
-    }
-
-    #[cfg(feature = "include_program_output")]
-    let os_output = {
-        // Prepare and check expected output.
-        let os_raw_output = crate::io::os_output::get_run_output(&cairo_runner.vm)?;
-        let os_output =
-            crate::io::os_output::OsOutput::from_raw_output_iter(os_raw_output.into_iter())?;
-        log::debug!("OsOutput for block number={}: {os_output:?}", os_output.new_block_number);
-        os_output
-    };
-
-    cairo_runner.vm.verify_auto_deductions().map_err(StarknetOsError::VirtualMachineError)?;
-    cairo_runner
-        .read_return_values(allow_missing_builtins)
-        .map_err(StarknetOsError::RunnerError)?;
-    cairo_runner
-        .relocate(cairo_run_config.relocate_mem)
-        .map_err(|e| StarknetOsError::VirtualMachineError(e.into()))?;
-
-    // Parse the Cairo VM output.
-    let cairo_pie = cairo_runner.get_cairo_pie().map_err(StarknetOsError::RunnerError)?;
+    let mut runner_output = run_program(layout, &OS_PROGRAM, &mut snos_hint_processor)?;
 
     Ok(StarknetOsRunnerOutput {
         #[cfg(feature = "include_program_output")]
-        os_output,
-        cairo_pie,
+        os_output: {
+            // Prepare and check expected output.
+            let os_raw_output = runner_output.raw_output;
+            let os_output =
+                crate::io::os_output::OsOutput::from_raw_output_iter(os_raw_output.into_iter())?;
+            log::debug!("OsOutput for block number={}: {os_output:?}", os_output.new_block_number);
+            os_output
+        },
+        cairo_pie: runner_output.cairo_pie,
         da_segment: snos_hint_processor.get_da_segment().take(),
-        metrics: OsMetrics::new(&mut cairo_runner, &snos_hint_processor)?,
+        metrics: OsMetrics::new(&mut runner_output.cairo_runner, &snos_hint_processor)?,
         #[cfg(any(test, feature = "testing"))]
-        unused_hints: snos_hint_processor.unused_hints,
+        unused_hints: snos_hint_processor.get_unused_hints(),
     })
 }
 
