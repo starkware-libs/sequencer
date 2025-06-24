bbc18558ea38c0736a08502aff582a00802bf9e8
Ayelet Zilber <ayelet@starkware.co>
2025-06-24 11:38:07 +0300

    apollo_dashboard: http server tps metric and alert

--- crates/apollo_dashboard/src/panels/http_server.rs
+++ crates/apollo_dashboard/src/panels/http_server.rs
@@ -23,11 +23,24 @@ fn get_panel_added_transactions_internal_error() -> Panel {
     Panel::from_counter(ADDED_TRANSACTIONS_INTERNAL_ERROR, PanelType::TimeSeries)
 }
 
+fn get_panel_http_server_transactions_received_rate() -> Panel {
+    Panel::new(
+        "http_server_transactions_received_rate (TPS)",
+        "The rate of transactions received by the HTTP Server during the last 20 minutes",
+        vec![format!(
+            "sum(rate({}[20m])) or vector(0)",
+            ADDED_TRANSACTIONS_TOTAL.get_name_with_filter()
+        )],
+        PanelType::TimeSeries,
+    )
+}
+
 pub(crate) fn get_http_server_row() -> Row {
     Row::new(
         "Http Server",
         vec![
             get_panel_added_transactions_total(),
+            get_panel_http_server_transactions_received_rate(),
             get_panel_added_transactions_success(),
             get_panel_added_transactions_failure(),
             get_panel_added_transactions_internal_error(),
