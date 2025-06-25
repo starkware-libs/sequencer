8f73a7be86f6ea118499474eceaca65b39272470
victork <victor.k@starkware.co>
2025-06-25 19:36:06 +0300

    starknet_api: internal implementation of sizeof

--- crates/sizeof/sizeof_macro/src/lib.rs
+++ crates/sizeof/sizeof_macro/src/lib.rs
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 // The code in this file uses code from and inspired by https://github.com/dtolnay/syn/tree/master/examples/heapsize and https://github.com/Kixiron/size-of
 
+=======
+>>>>>>> 59c12cabd (starknet_api: internal implementation of sizeof)
 extern crate proc_macro;
 extern crate quote;
 extern crate syn;
 
 use proc_macro::TokenStream;
+<<<<<<< HEAD
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
@@ -159,10 +163,51 @@ fn derive_macro_for_enum(data_enum: DataEnum) -> TokenStream2 {
                     Self::#vname { #(#bindings),* } => {
                         let mut size = 0;
                         #(#sizes)*
+=======
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+/// This macro derives the `SizeOf` trait for structs and enums, allowing them to calculate
+/// their size in bytes. The `SizeOf` trait requires implementing the `size_bytes`
+/// method, which returns the size of the type in bytes.
+#[proc_macro_derive(SizeOf)]
+pub fn derive_size_of(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let gen = match input.data {
+        Data::Struct(data_struct) => {
+            let field_exprs = match data_struct.fields {
+                Fields::Named(ref fields) => fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let ident = f.ident.as_ref().unwrap();
+                        quote! {
+                            size += self.#ident.size_bytes();
+                        }
+                    })
+                    .collect::<Vec<_>>(),
+                Fields::Unnamed(ref fields) => (0..fields.unnamed.len())
+                    .map(|i| {
+                        let idx = syn::Index::from(i);
+                        quote! {
+                            size += self.#idx.size_bytes();
+                        }
+                    })
+                    .collect(),
+                Fields::Unit => vec![],
+            };
+            quote! {
+                impl SizeOf for #name {
+                    fn size_bytes(&self) -> usize {
+                        let mut size = 0;
+                        #(#field_exprs)*
+>>>>>>> 59c12cabd (starknet_api: internal implementation of sizeof)
                         size
                     }
                 }
             }
+<<<<<<< HEAD
             Fields::Unnamed(ref fields) => {
                 let bindings: Vec<syn::Ident> = (0..fields.unnamed.len())
                     .map(|i| syn::Ident::new(&format!("f{}", i), vname.span()))
@@ -203,4 +248,59 @@ fn add_trait_bounds(mut generics: Generics) -> Generics {
         }
     }
     generics
+=======
+        }
+        Data::Enum(data_enum) => {
+            let variant_matches = data_enum.variants.iter().map(|variant| {
+                let vname = &variant.ident;
+                match &variant.fields {
+                    Fields::Named(ref fields) => {
+                        let idents: Vec<_> =
+                            fields.named.iter().map(|f| f.ident.as_ref().unwrap()).collect();
+                        let bindings: Vec<_> = idents.iter().map(|id| quote! { #id }).collect();
+                        let sizes: Vec<_> =
+                            idents.iter().map(|id| quote! { size += #id.size_bytes(); }).collect();
+                        quote! {
+                            Self::#vname { #(#bindings),* } => {
+                                let mut size = 0;
+                                #(#sizes)*
+                                size
+                            }
+                        }
+                    }
+                    Fields::Unnamed(ref fields) => {
+                        let bindings: Vec<syn::Ident> = (0..fields.unnamed.len())
+                            .map(|i| syn::Ident::new(&format!("f{}", i), vname.span()))
+                            .collect();
+                        let sizes: Vec<_> =
+                            bindings.iter().map(|b| quote! { size += #b.size_bytes(); }).collect();
+                        quote! {
+                            Self::#vname( #(#bindings),* ) => {
+                                let mut size = 0;
+                                #(#sizes)*
+                                size
+                            }
+                        }
+                    }
+                    Fields::Unit => {
+                        quote! {
+                            Self::#vname => 0
+                        }
+                    }
+                }
+            });
+            quote! {
+                impl SizeOf for #name {
+                    fn size_bytes(&self) -> usize {
+                        match self {
+                            #(#variant_matches),*
+                        }
+                    }
+                }
+            }
+        }
+        _ => unimplemented!("SizeOf can only be derived for structs and enums."),
+    };
+    gen.into()
+>>>>>>> 59c12cabd (starknet_api: internal implementation of sizeof)
 }
