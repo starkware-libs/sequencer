ae1c33c8347e355aaa266141b45e586ef303fc16
Aviv Greenburg <aviv.g@starkware.co>
2025-06-25 11:27:48 +0300

    fix(blockifier): default proving_gas to 4b

--- crates/blockifier/src/bouncer.rs
+++ crates/blockifier/src/bouncer.rs
@@ -165,7 +165,7 @@ impl Default for BouncerWeights {
             n_txs: 600,
             state_diff_size: 4000,
             sierra_gas: GasAmount(4000000000),
-            proving_gas: GasAmount(2000000000),
+            proving_gas: GasAmount(4000000000),
         }
     }
 }
