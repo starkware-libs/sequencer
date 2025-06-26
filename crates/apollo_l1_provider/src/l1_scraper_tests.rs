630391171d9ba9094ddc0df28021754a45dbc787
Gilad Chase <gilad@starkware.com>
2025-06-26 14:16:18 +0300

    l1: switch from fake l1 provider client to mock
    Using `Sequence` to enforce order between the expectations.

--- crates/apollo_l1_provider/src/l1_scraper_tests.rs
+++ crates/apollo_l1_provider/src/l1_scraper_tests.rs
@@ -13,6 +13,8 @@ use apollo_state_sync_types::state_sync_types::SyncBlock;
 use assert_matches::assert_matches;
 use indexmap::IndexSet;
 use itertools::Itertools;
+use mockall::predicate::eq;
+use mockall::Sequence;
 use papyrus_base_layer::ethereum_base_layer_contract::{EthereumBaseLayerContract, Starknet};
 use papyrus_base_layer::test_utils::{
     anvil_instance_from_config,
@@ -79,15 +81,15 @@ async fn txs_happy_flow() {
     // Setup.
     let base_layer_config = ethereum_base_layer_config_for_anvil(None);
     let _anvil_server_guard = anvil_instance_from_config(&base_layer_config);
+    let mut l1_provider_client = MockL1ProviderClient::default();
     let base_layer = EthereumBaseLayerContract::new(base_layer_config);
 
     // Deploy a fresh Starknet contract on Anvil from the bytecode in the JSON file.
     Starknet::deploy(base_layer.contract.provider().clone()).await.unwrap();
 
-    // Scrape multiple events.
+    // Send messages from L1 to L2.
     let l2_contract_address = "0x12";
     let l2_entry_point = "0x34";
-
     let message_to_l2_0 = base_layer.contract.sendMessageToL2(
         l2_contract_address.parse().unwrap(),
         l2_entry_point.parse().unwrap(),
@@ -106,7 +108,7 @@ async fn txs_happy_flow() {
         nonce_of_message_to_l2_0,
     );
 
-    // Send the transactions.
+    // Send the transactions to Anvil, and record the timestamps of the blocks they are included in.
     let mut l1_handler_timestamps: Vec<BlockTimestamp> = Vec::with_capacity(2);
     for msg in &[message_to_l2_0, message_to_l2_1] {
         let receipt = msg.send().await.unwrap().get_receipt().await.unwrap();
@@ -177,7 +179,18 @@ async fn txs_happy_flow() {
         cancellation_request_timestamp: cancel_timestamp,
     };
 
-    let fake_client = Arc::new(FakeL1ProviderClient::default());
+    let mut sequence = Sequence::new();
+    // Expect first call to return all the events defined further down.
+    l1_provider_client
+        .expect_add_events()
+        .once()
+        .in_sequence(&mut sequence)
+        .with(eq(vec![first_expected_log, second_expected_log, expected_cancel_message]))
+        .returning(|_| Ok(()));
+
+    // Expect second call to return nothing, no events left to scrape.
+    l1_provider_client.expect_add_events().once().in_sequence(&mut sequence).returning(|_| Ok(()));
+
     let l1_scraper_config = L1ScraperConfig {
         // Start scraping far enough back to capture all of the events created before.
         startup_rewind_time_seconds: Duration::from_secs(100),
@@ -186,7 +199,7 @@ async fn txs_happy_flow() {
     let l1_start_block = fetch_start_block(&base_layer, &l1_scraper_config).await.unwrap();
     let mut scraper = L1Scraper::new(
         l1_scraper_config,
-        fake_client.clone(),
+        Arc::new(l1_provider_client),
         base_layer.clone(),
         event_identifiers_to_track(),
         l1_start_block,
@@ -196,15 +209,9 @@ async fn txs_happy_flow() {
 
     // Test.
     scraper.send_events_to_l1_provider().await.unwrap();
-    fake_client.assert_add_events_received_with(&[
-        first_expected_log,
-        second_expected_log,
-        expected_cancel_message,
-    ]);
 
     // Previous events had been scraped, should no longer appear.
     scraper.send_events_to_l1_provider().await.unwrap();
-    fake_client.assert_add_events_received_with(&[]);
 }
 
 // TODO(Gilad): figure out how To setup anvil on a specific L1 block (through genesis.json?) and
