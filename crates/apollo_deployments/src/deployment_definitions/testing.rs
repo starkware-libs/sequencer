99fdd786ef2611cad86560839541bc083092c86e
Itay Tsabary <itayt@starkware.co>
2025-06-27 00:15:16 +0300

    apollo_deployments: flatten our dir structure, general cleanups

--- crates/apollo_deployments/src/deployment_definitions/testing.rs
+++ crates/apollo_deployments/src/deployment_definitions/testing.rs
@@ -1,5 +1,3 @@
-use std::path::PathBuf;
-
 use starknet_api::block::BlockNumber;
 
 use crate::config_override::{
@@ -9,7 +7,7 @@ use crate::config_override::{
     NetworkConfigOverride,
 };
 use crate::deployment::{Deployment, PragmaDomain};
-use crate::deployment_definitions::{Environment, BASE_APP_CONFIG_PATH};
+use crate::deployment_definitions::Environment;
 use crate::k8s::IngressParams;
 use crate::service::NodeType;
 
@@ -55,11 +53,10 @@ fn get_ingress_params() -> IngressParams {
 
 fn system_test_distributed_deployment() -> Deployment {
     Deployment::new(
-        NodeType::DistributedNode,
+        NodeType::Distributed,
         Environment::Testing,
-        "deployment_test_distributed",
+        "distributed",
         None,
-        PathBuf::from(BASE_APP_CONFIG_PATH),
         testing_config_override(),
         get_ingress_params(),
         None,
@@ -68,11 +65,10 @@ fn system_test_distributed_deployment() -> Deployment {
 
 fn system_test_hybrid_deployment() -> Deployment {
     Deployment::new(
-        NodeType::HybridNode,
+        NodeType::Hybrid,
         Environment::Testing,
-        "deployment_test_hybrid",
+        "hybrid",
         None,
-        PathBuf::from(BASE_APP_CONFIG_PATH),
         testing_config_override(),
         get_ingress_params(),
         None,
@@ -81,11 +77,10 @@ fn system_test_hybrid_deployment() -> Deployment {
 
 fn system_test_consolidated_deployment() -> Deployment {
     Deployment::new(
-        NodeType::ConsolidatedNode,
+        NodeType::Consolidated,
         Environment::Testing,
-        "deployment_test_consolidated",
+        "consolidated",
         None,
-        PathBuf::from(BASE_APP_CONFIG_PATH),
         testing_config_override(),
         get_ingress_params(),
         None,
