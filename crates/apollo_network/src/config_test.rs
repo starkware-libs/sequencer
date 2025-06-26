df33d161990af8bb0ba8f01bf2891072983ecb3e
guyf-starkware <guy.f@starkware.co>
2025-06-26 11:30:40 +0300

    apollo_network: Add tests for more validation cases.

--- crates/apollo_network/src/config_test.rs
+++ crates/apollo_network/src/config_test.rs
@@ -1,23 +1,57 @@
-use libp2p::Multiaddr;
+use lazy_static::lazy_static;
+use libp2p::{identity, multiaddr, Multiaddr, PeerId};
 use validator::Validate;
 
 use crate::NetworkConfig;
 
-#[test]
-fn test_network_config_bootstrap_peer_multiaddr_validation() {
-    let mut config = NetworkConfig::default();
-    config.validate().unwrap();
+lazy_static! {
+    static ref MULTI_ADDR: Multiaddr = {
+        let key = [0u8; 32];
+        let keypair = identity::Keypair::ed25519_from_bytes(key).unwrap();
+        Multiaddr::empty()
+            .with(multiaddr::Protocol::P2p(PeerId::from_public_key(&keypair.public())))
+    };
+}
 
-    let key = [0u8; 32];
-    let keypair = libp2p::identity::Keypair::ed25519_from_bytes(key).unwrap();
+#[test]
+fn test_bootstrap_peer_multiaddr_empty_is_valid() {
+    let config = NetworkConfig::default();
+    config.validate().unwrap();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_unique_addresses_is_valid() {
+    let key = [1u8; 32];
+    let keypair = identity::Keypair::ed25519_from_bytes(key).unwrap();
+    let second_addr = Multiaddr::empty()
+        .with(multiaddr::Protocol::P2p(PeerId::from_public_key(&keypair.public())));
+
+    let config = NetworkConfig {
+        bootstrap_peer_multiaddr: Some(vec![MULTI_ADDR.clone(), second_addr]),
+        ..NetworkConfig::default()
+    };
+
+    config.validate().unwrap();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_duplicates_are_invalid() {
+    let config = NetworkConfig {
+        bootstrap_peer_multiaddr: Some(vec![MULTI_ADDR.clone(), MULTI_ADDR.clone()]),
+        ..NetworkConfig::default()
+    };
+
+    config.validate().unwrap_err();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_missing_peer_id_is_invalid() {
+    let mut config = NetworkConfig::default();
+
     let mutliaddr = Multiaddr::empty()
-        .with(libp2p::multiaddr::Protocol::Ip4(std::net::Ipv4Addr::LOCALHOST))
-        .with(libp2p::multiaddr::Protocol::Tcp(12345))
-        .with(libp2p::multiaddr::Protocol::P2p(libp2p::PeerId::from_public_key(&keypair.public())));
+        .with(multiaddr::Protocol::Ip4(std::net::Ipv4Addr::LOCALHOST))
+        .with(multiaddr::Protocol::Tcp(12345));
 
     config.bootstrap_peer_multiaddr = Some(vec![mutliaddr.clone()]);
-    config.validate().unwrap();
-
-    config.bootstrap_peer_multiaddr = Some(vec![mutliaddr.clone(), mutliaddr]);
     config.validate().unwrap_err();
 }
