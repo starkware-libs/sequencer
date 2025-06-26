c1a537d2414c08661b356e3f358019523421f8a2
guyf-starkware <guy.f@starkware.co>
2025-06-26 11:30:40 +0300

    apollo_network: Add tests for more validation cases.

--- crates/apollo_network/src/config_test.rs
+++ crates/apollo_network/src/config_test.rs
@@ -1,23 +1,50 @@
 use libp2p::Multiaddr;
+use once_cell::sync::Lazy;
 use validator::Validate;
 
 use crate::NetworkConfig;
 
-#[test]
-fn test_network_config_bootstrap_peer_multiaddr_validation() {
-    let mut config = NetworkConfig::default();
-    config.validate().unwrap();
-
+static MULTI_ADDR: Lazy<Multiaddr> = Lazy::new(|| {
     let key = [0u8; 32];
     let keypair = libp2p::identity::Keypair::ed25519_from_bytes(key).unwrap();
+    Multiaddr::empty()
+        .with(libp2p::multiaddr::Protocol::P2p(libp2p::PeerId::from_public_key(&keypair.public())))
+});
+
+#[test]
+fn test_bootstrap_peer_multiaddr_empty_is_valid() {
+    let config = NetworkConfig::default();
+    config.validate().unwrap();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_accepts_valid() {
+    let config = NetworkConfig {
+        bootstrap_peer_multiaddr: Some(vec![MULTI_ADDR.clone()]),
+        ..NetworkConfig::default()
+    };
+
+    config.validate().unwrap();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_duplicates_are_invalid() {
+    let config = NetworkConfig {
+        bootstrap_peer_multiaddr: Some(vec![MULTI_ADDR.clone(), MULTI_ADDR.clone()]),
+        ..NetworkConfig::default()
+    };
+
+    config.validate().unwrap_err();
+}
+
+#[test]
+fn test_bootstrap_peer_multiaddr_missing_peer_id_is_invalid() {
+    let mut config = NetworkConfig::default();
+
     let mutliaddr = Multiaddr::empty()
         .with(libp2p::multiaddr::Protocol::Ip4(std::net::Ipv4Addr::LOCALHOST))
-        .with(libp2p::multiaddr::Protocol::Tcp(12345))
-        .with(libp2p::multiaddr::Protocol::P2p(libp2p::PeerId::from_public_key(&keypair.public())));
+        .with(libp2p::multiaddr::Protocol::Tcp(12345));
 
     config.bootstrap_peer_multiaddr = Some(vec![mutliaddr.clone()]);
-    config.validate().unwrap();
-
-    config.bootstrap_peer_multiaddr = Some(vec![mutliaddr.clone(), mutliaddr]);
     config.validate().unwrap_err();
 }
