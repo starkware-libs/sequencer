77125eb687452b53a7b206ccce2e9b26ca3e051b
Aner Ben Efraim <aner@starkware.co>
2025-07-01 12:47:44 +0300

    starknet_os: add get_const_from_program fn

--- crates/starknet_os/src/constants_test.rs
+++ crates/starknet_os/src/constants_test.rs
@@ -2,6 +2,10 @@ use cairo_vm::types::program::Program;
 use starknet_types_core::felt::Felt;
 
 #[allow(dead_code)]
-fn get_from_program(_program: &Program, _const_name: &str) -> Felt {
-    todo!()
+fn get_from_program(program: &Program, const_name: &str) -> Felt {
+    program
+        .constants
+        .get(const_name)
+        .cloned()
+        .unwrap_or_else(|| panic!("Constant {const_name} not found in the program."))
 }
