0b9015851098dffc76cb1b5260bd8e679b798d40
Itamar Shechter <itamar@starkware.co>
2025-07-01 23:17:25 +0300

    apollo_integration_tests: generate deploy and replace class txs

--- crates/apollo_integration_tests/tests/test_custom_syscall_invoke_txs.rs
+++ crates/apollo_integration_tests/tests/test_custom_syscall_invoke_txs.rs
@@ -7,17 +7,18 @@ use mempool_test_utils::starknet_api_test_utils::{
     AccountTransactionGenerator,
     MultiAccountTransactionGenerator,
 };
-use starknet_api::core::CompiledClassHash;
+use starknet_api::core::{calculate_contract_address, CompiledClassHash};
 use starknet_api::execution_resources::GasAmount;
-use starknet_api::felt;
 use starknet_api::rpc_transaction::RpcTransaction;
+use starknet_api::transaction::fields::ContractAddressSalt;
+use starknet_api::{calldata, felt};
 
 use crate::common::{end_to_end_flow, validate_tx_count, TestScenario};
 
 mod common;
 
 const DEFAULT_TIP: u64 = 1_u64;
-const CUSTOM_INVOKE_TX_COUNT: usize = 6;
+const CUSTOM_INVOKE_TX_COUNT: usize = 10;
 
 /// Test a wide range of different kinds of invoke transactions.
 #[tokio::test]
@@ -52,6 +53,7 @@ fn create_cairo_1_syscall_test_txs(
     txs.push(generate_empty_contract_declare_tx(account_tx_generator));
     txs.extend(generate_custom_library_call_invoke_txs(account_tx_generator));
     txs.extend(generate_custom_not_nested_invoke_txs(account_tx_generator));
+    txs.extend(generate_test_deploy_txs(account_tx_generator, DEFAULT_TIP));
 
     txs
 }
@@ -112,9 +114,10 @@ fn generate_custom_library_call_invoke_txs(
         .collect()
 }
 
-pub fn generate_empty_contract_declare_tx(
+fn generate_empty_contract_declare_tx(
     account_tx_generator: &mut AccountTransactionGenerator,
 ) -> RpcTransaction {
+    // TODO(Itamar): Consider changing the empty contract to another contract with more functions and check class.
     let empty_contract = FeatureContract::Empty(account_tx_generator.account.cairo_version());
     // Hard coded class hash for empty contract. See example in https://github.com/starkware-libs/sequencer/blob/main-v0.14.0/crates/mempool_test_utils/src/starknet_api_test_utils.rs#L92
     // TODO(Itamar): Move compiled hash to the blockifier constants file as optional trait for
@@ -124,4 +127,80 @@ pub fn generate_empty_contract_declare_tx(
     ));
 
     account_tx_generator.generate_declare_tx(empty_compiled_class_hash, empty_contract.get_sierra())
+}
+
+fn generate_test_deploy_txs(
+    account_tx_generator: &mut AccountTransactionGenerator,
+    tip: u64,
+) -> Vec<RpcTransaction> {
+    let mut txs = vec![];
+    let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1(RunnableCairo1::Casm));
+
+    // test_deploy_contract - constructor args and salt are unique to calculate contract address.
+    let constructor_calldata_arg1 = felt!(1_u8);
+    let constructor_calldata_arg2 = felt!(1_u8);
+    let salt_deployed_account = felt!(7_u64);
+    let test_deploy_args = vec![
+        test_contract.get_class_hash().0, // class hash
+        salt_deployed_account,            // salt
+        felt!(2_u64),                     // len of construct calldata.
+        constructor_calldata_arg1,        // construct calldata: arg1.
+        constructor_calldata_arg2,        // construct calldata: arg2.
+        felt!(0_u64),                     // deploy_from_zero flag is down.
+    ];
+    let calldata =
+        create_calldata(test_contract.get_instance_address(0), "test_deploy", &test_deploy_args);
+    txs.push(account_tx_generator.generate_rpc_invoke_tx(DEFAULT_TIP, calldata));
+
+    // Get the contract address of the newly deployed contract from test_deploy.
+    let newly_deployed_contract_address = calculate_contract_address(
+        ContractAddressSalt(salt_deployed_account),
+        test_contract.get_class_hash(),
+        &calldata!(constructor_calldata_arg1, constructor_calldata_arg2), /* constructor
+                                                                           * calldata */
+        test_contract.get_instance_address(0), // deployer address
+    )
+    .expect("Failed to calculate contract address");
+
+    // Write key and value to storage via test_call_contract of the deployed contract.
+    let key = felt!(1948_u64);
+    let test_storage_write_args = &[
+        felt!(2_u64),    // arguments length
+        key,             // key
+        felt!(1967_u64), // value
+    ];
+
+    txs.push(account_tx_generator.generate_call_contract_invoke_tx(
+        newly_deployed_contract_address,
+        &test_contract,
+        "test_storage_write",
+        test_storage_write_args,
+        tip,
+    ));
+
+    // Read value by key from storage via test_library_call of the deployed contract.
+    let test_storage_read_args = vec![
+        felt!(1_u64), // arguments length
+        key,
+    ];
+    txs.push(account_tx_generator.generate_invoke_tx_library_call(
+        DEFAULT_TIP,
+        "test_storage_read",
+        &test_storage_read_args,
+        &test_contract,
+    ));
+
+    // test_replace_class - replace the class of the deployed contract with an empty contract.
+    let empty_contract = FeatureContract::Empty(account_tx_generator.account.cairo_version());
+    let test_replace_class_args =
+        vec![empty_contract.safe_get_sierra().unwrap().calculate_class_hash().0];
+    let calldata = create_calldata(
+        newly_deployed_contract_address,
+        "test_replace_class",
+        &test_replace_class_args,
+    );
+
+    txs.push(account_tx_generator.generate_rpc_invoke_tx(DEFAULT_TIP, calldata));
+
+    txs
 }
