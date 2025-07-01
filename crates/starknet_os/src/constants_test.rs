7b5f5e5d4124c0fd8d7c715d1930774026888917
Aner Ben Efraim <aner@starkware.co>
2025-07-01 13:33:58 +0300

    starknet_os: test blob constants

--- crates/starknet_os/src/constants_test.rs
+++ crates/starknet_os/src/constants_test.rs
@@ -3,6 +3,8 @@ use cairo_vm::types::program::Program;
 use starknet_api::core::L2_ADDRESS_UPPER_BOUND;
 use starknet_types_core::felt::Felt;
 
+use crate::hints::hint_implementation::kzg::utils::FIELD_ELEMENTS_PER_BLOB;
+
 fn get_from_program(program: &Program, const_path: &str) -> Felt {
     program
         .constants
@@ -16,5 +18,17 @@ fn test_l2_address_bound() {
     assert_eq!(
         Felt::from(*L2_ADDRESS_UPPER_BOUND),
         get_from_program(&OS_PROGRAM, "starkware.starknet.common.storage.ADDR_BOUND")
+    );
+}
+
+#[test]
+fn test_blob_constants() {
+    assert_eq!(
+        // TODO(Aner): should use full name lookup?
+        get_from_program(
+            &OS_PROGRAM,
+            "starkware.starknet.core.os.data_availability.commitment.BLOB_LENGTH"
+        ),
+        Felt::from(FIELD_ELEMENTS_PER_BLOB)
     );
 }
