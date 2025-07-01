de37487befd5a5ce75a36f1175282b171078bbe2
Aner Ben Efraim <aner@starkware.co>
2025-07-01 12:43:43 +0300

    starknet_os: test_l2_address_bound

--- crates/starknet_os/src/constants_test.rs
+++ crates/starknet_os/src/constants_test.rs
@@ -1,11 +1,20 @@
+use apollo_starknet_os_program::OS_PROGRAM;
 use cairo_vm::types::program::Program;
+use starknet_api::core::L2_ADDRESS_UPPER_BOUND;
 use starknet_types_core::felt::Felt;
 
-#[allow(dead_code)]
 fn get_from_program(program: &Program, const_path: &str) -> Felt {
     program
         .constants
         .get(const_path)
         .cloned()
         .unwrap_or_else(|| panic!("Constant {const_path} not found in the program."))
+}
+
+#[test]
+fn test_l2_address_bound() {
+    assert_eq!(
+        Felt::from(*L2_ADDRESS_UPPER_BOUND),
+        get_from_program(&OS_PROGRAM, "starkware.starknet.common.storage.ADDR_BOUND")
+    );
 }
