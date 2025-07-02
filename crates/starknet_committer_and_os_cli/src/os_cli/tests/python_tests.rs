3d4cd1c1fa89176bb9905b16d265eb72df8276dd
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2025-07-02 14:00:08 +0300

    starknet_os: test aggregator input deserialization (#7539)

--- crates/starknet_committer_and_os_cli/src/os_cli/tests/python_tests.rs
+++ crates/starknet_committer_and_os_cli/src/os_cli/tests/python_tests.rs
@@ -2,13 +2,14 @@ use blake2s::encode_felts_to_u32s;
 use starknet_os::test_utils::errors::OsSpecificTestError;
 use starknet_types_core::felt::Felt;
 
-use crate::os_cli::commands::{validate_os_input, OsCliInput};
+use crate::os_cli::commands::{validate_os_input, AggregatorCliInput, OsCliInput};
 use crate::os_cli::tests::types::{OsPythonTestError, OsPythonTestResult};
 use crate::shared_utils::types::{PythonTestError, PythonTestRunner};
 
 // Enum representing different Python tests.
 pub enum OsPythonTestRunner {
-    InputDeserialization,
+    AggregatorInputDeserialization,
+    OsInputDeserialization,
     EncodeFelts,
 }
 
@@ -18,7 +19,8 @@ impl TryFrom<String> for OsPythonTestRunner {
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
         match value.as_str() {
-            "input_deserialization" => Ok(Self::InputDeserialization),
+            "aggregator_input_deserialization" => Ok(Self::AggregatorInputDeserialization),
+            "os_input_deserialization" => Ok(Self::OsInputDeserialization),
             "encode_felts" => Ok(Self::EncodeFelts),
             _ => Err(PythonTestError::UnknownTestName(value)),
         }
@@ -30,7 +32,12 @@ impl PythonTestRunner for OsPythonTestRunner {
     #[allow(clippy::result_large_err)]
     async fn run(&self, input: Option<&str>) -> OsPythonTestResult {
         match self {
-            Self::InputDeserialization => input_deserialization(Self::non_optional_input(input)?),
+            Self::AggregatorInputDeserialization => {
+                aggregator_input_deserialization(Self::non_optional_input(input)?)
+            }
+            Self::OsInputDeserialization => {
+                os_input_deserialization(Self::non_optional_input(input)?)
+            }
             Self::EncodeFelts => {
                 let felts: Vec<Felt> = serde_json::from_str(Self::non_optional_input(input)?)?;
                 Ok(format!("{:?}", encode_felts_to_u32s(felts)))
@@ -39,10 +46,18 @@ impl PythonTestRunner for OsPythonTestRunner {
     }
 }
 
-/// Deserialize the input string into an `Input` struct.
+/// Deserialize the OS input string into an `OsInput` struct.
 #[allow(clippy::result_large_err)]
-fn input_deserialization(input_str: &str) -> OsPythonTestResult {
+fn os_input_deserialization(input_str: &str) -> OsPythonTestResult {
     let input = serde_json::from_str::<OsCliInput>(input_str)?;
     validate_os_input(&input.os_hints.os_input);
+    Ok("Deserialization successful".to_string())
+}
+
+/// Deserialize the aggregator input string into an `AggregatorInput` struct.
+#[allow(clippy::result_large_err)]
+fn aggregator_input_deserialization(input_str: &str) -> OsPythonTestResult {
+    let _input = serde_json::from_str::<AggregatorCliInput>(input_str)?;
+    // TODO(Aner): Validate the aggregator input.
     Ok("Deserialization successful".to_string())
 }
