03da968a03174afd35c300ab94761cf29722a119
Tzahi <tzahi@starkware.co>
2025-07-03 17:17:30 +0300

    starknet_os: use OsOutput in StarknetOsRunnerOutput (#7671)

--- crates/starknet_os/src/io/os_output_test.rs
+++ crates/starknet_os/src/io/os_output_test.rs
@@ -2,6 +2,7 @@ use num_traits::ToPrimitive;
 use starknet_types_core::felt::Felt;
 
 use super::{N_UPDATES_BOUND, N_UPDATES_SMALL_PACKING_BOUND};
+
 #[test]
 fn assert_const_felts() {
     assert_eq!(Into::<Felt>::into(N_UPDATES_BOUND).to_u128().unwrap(), 1 << 64);
