eb133ed20641b988ebf20b4f495b48b0853d156b
Andrew Luka <andrew.l@starkware.co>
2025-07-03 11:49:48 +0300

    apollo_network: added arguments to network stress test (#7704)

--- crates/apollo_network/src/bin/network_stress_test/main.rs
+++ crates/apollo_network/src/bin/network_stress_test/main.rs
@@ -1,4 +1,48 @@
+use clap::Parser;
 mod converters;
 mod utils;
 
-fn main() {}
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// ID for Prometheus logging
+    #[arg(short, long, env)]
+    id: usize,
+
+    /// The port to run the Prometheus metrics server on
+    #[arg(long, env, default_value_t = 2000)]
+    metric_port: u16,
+
+    /// The port to run the P2P network on
+    #[arg(short, env, long, default_value_t = 10000)]
+    p2p_port: u16,
+
+    /// The address to the bootstrap peer
+    #[arg(long, env)]
+    bootstrap: Option<String>,
+
+    /// Set the verbosity level of the logger
+    #[arg(short, long, env, default_value_t = 0)]
+    verbosity: u8,
+
+    /// Buffer size for the broadcast topic
+    // Default from crates/apollo_consensus_manager/src/config.rs
+    #[arg(short, long, env, default_value_t = 10000)]
+    buffer_size: usize,
+
+    /// Size of StressTestMessage
+    #[arg(short, long, env, default_value_t = 1 << 10)]
+    message_size_bytes: usize,
+
+    /// The time to sleep between broadcasts of StressTestMessage in milliseconds
+    #[arg(long, env, default_value_t = 1)]
+    heartbeat_millis: u64,
+
+    /// Maximum duration in seconds to run the node for
+    #[arg(short, long, env, default_value_t = 3_600)]
+    timeout: u64,
+}
+
+fn main() {
+    let _args = Args::parse();
+}
