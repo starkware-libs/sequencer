341a2549404e5d0d07e3b703dd56adfaec6d82fb
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2025-07-03 16:59:13 +0300

    starknet_os: test l2 to l1 message header size (#7725)

--- crates/starknet_os/src/constants_test.rs
+++ crates/starknet_os/src/constants_test.rs
@@ -1,5 +1,5 @@
 use apollo_starknet_os_program::OS_PROGRAM;
-use blockifier::abi::constants::L1_TO_L2_MSG_HEADER_SIZE;
+use blockifier::abi::constants::{L1_TO_L2_MSG_HEADER_SIZE, L2_TO_L1_MSG_HEADER_SIZE};
 use cairo_vm::types::program::Program;
 use starknet_api::core::L2_ADDRESS_UPPER_BOUND;
 use starknet_committer::hash_function::hash::TreeHashFunctionImpl;
@@ -52,5 +52,13 @@ fn test_l1_to_l2_message_header_size() {
     assert_eq!(
         get_size_of_cairo_struct(CairoStruct::L1ToL2MessageHeader, &*OS_PROGRAM).unwrap(),
         L1_TO_L2_MSG_HEADER_SIZE
+    );
+}
+
+#[test]
+fn test_l2_to_l1_message_header_size() {
+    assert_eq!(
+        get_size_of_cairo_struct(CairoStruct::L2ToL1MessageHeader, &*OS_PROGRAM).unwrap(),
+        L2_TO_L1_MSG_HEADER_SIZE
     );
 }
