5bd55b1a67f7e46c7c20021664fa879c1df7a795
aner-starkware <147302140+aner-starkware@users.noreply.github.com>
2025-07-03 15:59:12 +0300

    starknet_os: test l1 to l2 message header size (#7724)

--- crates/starknet_os/src/constants_test.rs
+++ crates/starknet_os/src/constants_test.rs
@@ -1,10 +1,13 @@
 use apollo_starknet_os_program::OS_PROGRAM;
+use blockifier::abi::constants::L1_TO_L2_MSG_HEADER_SIZE;
 use cairo_vm::types::program::Program;
 use starknet_api::core::L2_ADDRESS_UPPER_BOUND;
 use starknet_committer::hash_function::hash::TreeHashFunctionImpl;
 use starknet_types_core::felt::Felt;
 
 use crate::hints::hint_implementation::kzg::utils::FIELD_ELEMENTS_PER_BLOB;
+use crate::hints::vars::CairoStruct;
+use crate::vm_utils::get_size_of_cairo_struct;
 
 fn get_from_program(program: &Program, const_path: &str) -> Felt {
     program
@@ -41,5 +44,13 @@ fn test_contract_class_hash_version() {
             "starkware.starknet.core.os.state.commitment.CONTRACT_CLASS_LEAF_VERSION"
         ),
         Felt::from_hex(TreeHashFunctionImpl::CONTRACT_CLASS_LEAF_V0).unwrap()
+    );
+}
+
+#[test]
+fn test_l1_to_l2_message_header_size() {
+    assert_eq!(
+        get_size_of_cairo_struct(CairoStruct::L1ToL2MessageHeader, &*OS_PROGRAM).unwrap(),
+        L1_TO_L2_MSG_HEADER_SIZE
     );
 }
