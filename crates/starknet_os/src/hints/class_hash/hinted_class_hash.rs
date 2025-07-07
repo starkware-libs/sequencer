c0f85dc20b8d729ade0a542131237529afc062a0
rotem-starkware <rotem@starkware.co>
2025-07-07 13:33:36 +0300

    starknet_os: modify spaces in versions before 0.10.0 for hinted class hash computation

--- crates/starknet_os/src/hints/class_hash/hinted_class_hash.rs
+++ crates/starknet_os/src/hints/class_hash/hinted_class_hash.rs
@@ -4,6 +4,7 @@ use std::io::Write;
 use std::string::FromUtf8Error;
 
 use papyrus_common::python_json::PythonJsonFormatter;
+use regex::Regex;
 use serde::{Deserialize, Serialize, Serializer};
 use sha3::digest::Digest;
 use starknet_api::contract_class::EntryPointType;
@@ -142,7 +143,10 @@ pub fn compute_cairo_hinted_class_hash(
     let mut ser = serde_json::Serializer::with_formatter(&mut string_buffer, PythonJsonFormatter);
     contract_definition.serialize(&mut ser)?;
 
-    let raw_json_output = String::from_utf8(string_buffer)?;
+    let mut raw_json_output = String::from_utf8(string_buffer)?;
+    if contract_definition.program.compiler_version.is_none() {
+        add_backward_compatibility_space(&mut raw_json_output);
+    }
 
     let mut keccak_writer = KeccakWriter::default();
     keccak_writer
@@ -151,4 +155,21 @@ pub fn compute_cairo_hinted_class_hash(
 
     let KeccakWriter(hash) = keccak_writer;
     Ok(truncated_keccak(<[u8; 32]>::from(hash.finalize())))
+}
+
+/// Adds space before the colon when formatting named tuple types.
+/// This should be used for backward compatibility in the contract hash computation in versions
+/// before 0.10.0.
+/// For example, use "(a : felt)" instead of "(a: felt)".
+pub fn add_backward_compatibility_space(input: &mut String) {
+    let cairo_regex = Regex::new(r#""cairo_type"\s*:\s*"\(([^"]*)\)""#).unwrap();
+    let pair_regex = Regex::new(r"([^:\s,]+):\s*([^,\s)]+)").unwrap();
+
+    *input = cairo_regex
+        .replace_all(input, |caps: &regex::Captures<'_>| {
+            let inner = &caps[1];
+            let fixed = pair_regex.replace_all(inner, "$1 : $2");
+            format!("\"cairo_type\": \"({fixed})\"")
+        })
+        .into_owned();
 }
