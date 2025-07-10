f26038d3861e453242a76a34ed65eeae8ca11daf
Nimrod Weiss <nimrod@starkware.co>
2025-07-10 15:56:21 +0300

    starknet_os_flow_tests: create partial flow test body

--- crates/starknet_os_flow_tests/src/utils.rs
+++ crates/starknet_os_flow_tests/src/utils.rs
@@ -3,15 +3,16 @@ use std::collections::HashMap;
 use blockifier::blockifier::config::TransactionExecutorConfig;
 use blockifier::blockifier::transaction_executor::{
     BlockExecutionSummary,
+    TransactionExecutionOutput,
     TransactionExecutor,
     TransactionExecutorError,
 };
 use blockifier::context::BlockContext;
-use blockifier::state::cached_state::{CachedState, CommitmentStateDiff, StateMaps};
+use blockifier::state::cached_state::{CachedState, CommitmentStateDiff};
 use blockifier::test_utils::dict_state_reader::DictStateReader;
 use blockifier::test_utils::maybe_dummy_block_hash_and_number;
-use blockifier::transaction::objects::TransactionExecutionInfo;
 use blockifier::transaction::transaction_execution::Transaction;
+use starknet_committer::block_committer::commit::commit_block;
 use starknet_committer::block_committer::input::{
     ConfigImpl,
     Input,
@@ -21,17 +22,23 @@ use starknet_committer::block_committer::input::{
 };
 use starknet_committer::patricia_merkle_tree::types::CompiledClassHash;
 use starknet_patricia::hash::hash_trait::HashOutput;
+use starknet_patricia_storage::map_storage::MapStorage;
 use starknet_patricia_storage::storage_trait::{DbKey, DbValue};
 
 #[allow(dead_code)]
 pub(crate) type CommitterInput = Input<ConfigImpl>;
 
 #[allow(dead_code)]
-pub(crate) type ExecutionOutput = (
-    Vec<(TransactionExecutionInfo, StateMaps)>,
-    BlockExecutionSummary,
-    CachedState<DictStateReader>,
-);
+pub(crate) type ExecutionOutput =
+    (Vec<TransactionExecutionOutput>, BlockExecutionSummary, CachedState<DictStateReader>);
+
+#[allow(dead_code)]
+pub(crate) struct InitialState {
+    pub(crate) state_reader: DictStateReader,
+    pub(crate) fact_storage: HashMap<DbKey, DbValue>,
+    pub(crate) contracts_trie_root_hash: HashOutput,
+    pub(crate) classes_trie_root_hash: HashOutput,
+}
 
 #[allow(dead_code)]
 pub(crate) fn create_committer_input(
@@ -75,10 +82,9 @@ pub(crate) fn create_committer_input(
 
 #[allow(dead_code)]
 pub(crate) fn execute_transactions(
-    initial_state: Option<DictStateReader>,
+    initial_state_reader: DictStateReader,
     txs: &[Transaction],
 ) -> ExecutionOutput {
-    let initial_state_reader = initial_state.unwrap_or_default();
     let dummy_block_context = BlockContext::create_for_testing();
     let block_number_hash_pair =
         maybe_dummy_block_hash_and_number(dummy_block_context.block_info().block_number);
@@ -103,4 +109,29 @@ pub(crate) fn execute_transactions(
     let block_summary = executor.finalize().expect("Failed to finalize block.");
     let final_state = executor.block_state.unwrap();
     (execution_outputs, block_summary, final_state)
+}
+
+#[allow(dead_code)]
+pub(crate) async fn flow_test_body(initial_state: InitialState, txs: &[Transaction]) {
+    // Execute the transactions.
+    let (_txs_execution_outputs, execution_summary, _cached_state) =
+        execute_transactions(initial_state.state_reader, txs);
+
+    // Prepare the committer input.
+    // TODO(Nimrod): Figure out how to avoid cloning the fact storage.
+    let fact_storage = initial_state.fact_storage.clone();
+    let committer_input = create_committer_input(
+        execution_summary.state_diff,
+        initial_state.fact_storage,
+        initial_state.contracts_trie_root_hash,
+        initial_state.classes_trie_root_hash,
+    );
+
+    // Run the committer and save the new facts.
+    let filled_forest =
+        commit_block(committer_input).await.expect("Failed to commit the given block.");
+    let mut map_storage = MapStorage { storage: fact_storage };
+    filled_forest.write_to_storage(&mut map_storage);
+    let _new_contracts_trie_root_hash = filled_forest.get_contract_root_hash();
+    let _new_classes_trie_root_hash = filled_forest.get_compiled_class_root_hash();
 }
