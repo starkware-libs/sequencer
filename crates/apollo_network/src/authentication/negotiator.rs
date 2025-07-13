44763653c1f1cef14dacdbf1f4b1a8ddb3750880
guyf-starkware <guy.f@starkware.co>
2025-07-13 11:46:47 +0300

    apollo_network: Create an extendable security negotiation (on top of noise)

--- crates/apollo_network/src/authentication/negotiator.rs
+++ crates/apollo_network/src/authentication/negotiator.rs
@@ -3,6 +3,19 @@ use std::io::Error as IoError;
 use async_trait::async_trait;
 use futures::{Sink, Stream};
 use libp2p::PeerId;
+#[cfg(any(feature = "testing", test))]
+use mockall::automock;
+
+pub trait NegotiatorChannel:
+    Sink<Vec<u8>, Error = IoError> + Stream<Item = Result<Vec<u8>, IoError>> + Unpin + Send
+{
+}
+
+// Blanket implementation for any type that implements all the required traits
+impl<T> NegotiatorChannel for T where
+    T: Sink<Vec<u8>, Error = IoError> + Stream<Item = Result<Vec<u8>, IoError>> + Unpin + Send
+{
+}
 
 pub enum NegotiatorOutput {
     None,
@@ -21,6 +34,7 @@ pub enum NegotiatorError {
 }
 
 #[async_trait]
+#[cfg_attr(any(feature = "testing", test), automock)]
 pub trait Negotiator: Send + Clone {
     /// Performs the handshake protocol when we are the incoming connection side.
     /// `connection` is the channel that can be used to communicate with the other peer.
@@ -53,4 +67,12 @@ pub trait ConnectionEndpoint: Unpin + Send {
 
     /// Receives data from the connection.
     async fn receive(&mut self) -> Result<Vec<u8>, IoError>;
+}
+
+// Automock does not implement Clone, so we need to do it manually.
+#[cfg(any(feature = "testing", test))]
+impl Clone for MockNegotiator {
+    fn clone(&self) -> Self {
+        MockNegotiator::default()
+    }
 }
