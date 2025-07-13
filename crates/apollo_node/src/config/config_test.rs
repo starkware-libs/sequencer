253e53fbf9bcd14f35e8c85efd9621b077cd43cf
Arni Hod <arnon@starkware.co>
2025-07-13 15:07:25 +0300

    apollo_node: add validation on the l1 providers cooldown

--- crates/apollo_node/src/config/config_test.rs
+++ crates/apollo_node/src/config/config_test.rs
@@ -1,9 +1,13 @@
 use std::net::{IpAddr, Ipv4Addr};
+use std::time::Duration;
 
 use apollo_config::dumping::{combine_config_map_and_pointers, SerializeConfig};
 use apollo_infra::component_client::RemoteClientConfig;
 use apollo_infra::component_server::LocalServerConfig;
 use apollo_infra_utils::dumping::serialize_to_file_test;
+use apollo_l1_provider::l1_scraper::{L1ScraperConfig, L1_BLOCK_TIME};
+use apollo_l1_provider::L1ProviderConfig;
+use assert_matches::assert_matches;
 use rstest::rstest;
 use validator::Validate;
 
@@ -105,6 +109,83 @@ fn default_config_file_is_up_to_date() {
 #[test]
 fn validate_config_success() {
     let config = SequencerNodeConfig::default();
+    assert!(config.validate().is_ok());
+}
+
+#[rstest]
+#[case::polling_interval(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(2),
+        finality: 0,
+        ..L1ScraperConfig::default()
+    }
+)]
+#[case::finality(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(0),
+        finality: 1,
+        ..L1ScraperConfig::default()
+    }
+)]
+#[case::polling_interval_and_finality(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(1),
+        finality: 1,
+        ..L1ScraperConfig::default()
+    }
+)]
+fn validate_l1_handler_cooldown_failure(#[case] l1_scraper_config: L1ScraperConfig) {
+    let config = SequencerNodeConfig {
+        l1_scraper_config,
+        l1_provider_config: L1ProviderConfig {
+            new_l1_handler_cooldown_seconds: Duration::from_secs(1),
+            ..L1ProviderConfig::default()
+        },
+        ..SequencerNodeConfig::default()
+    };
+
+    assert_matches!(
+        config.validate(),
+        Err(e) if e.to_string().contains("L1 handler cooldown validation failed.")
+    );
+}
+
+#[rstest]
+#[case::polling_interval(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(2),
+        finality: 0,
+        ..L1ScraperConfig::default()
+    }
+)]
+#[case::finality(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(0),
+        finality: 1,
+        ..L1ScraperConfig::default()
+    }
+)]
+#[case::polling_interval_and_finality(
+    L1ScraperConfig {
+        polling_interval_seconds: Duration::from_secs(1),
+        finality: 1,
+        ..L1ScraperConfig::default()
+    }
+)]
+fn validate_l1_handler_cooldown_success(#[case] l1_scraper_config: L1ScraperConfig) {
+    let new_l1_handler_cooldown_seconds = l1_scraper_config.polling_interval_seconds
+        + Duration::from_secs(L1_BLOCK_TIME * l1_scraper_config.finality)
+        + Duration::from_secs(1);
+
+    let config = SequencerNodeConfig {
+        l1_scraper_config,
+        l1_provider_config: L1ProviderConfig {
+            new_l1_handler_cooldown_seconds,
+            ..L1ProviderConfig::default()
+        },
+        ..SequencerNodeConfig::default()
+    };
+
     assert!(config.validate().is_ok());
 }
 
