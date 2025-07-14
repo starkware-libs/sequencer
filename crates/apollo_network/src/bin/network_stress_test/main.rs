4818006f0c8470537ff0b291031003f80aa91bd6
Andrew Luka <andrew.l@starkware.co>
2025-07-14 10:24:10 +0300

    apollo_network: network_stress_test: changed listening address to 0.0.0.0

--- crates/apollo_network/src/bin/network_stress_test/main.rs
+++ crates/apollo_network/src/bin/network_stress_test/main.rs
@@ -173,7 +173,7 @@ async fn main() {
     );
 
     let builder = PrometheusBuilder::new().with_http_listener(SocketAddr::V4(SocketAddrV4::new(
-        Ipv4Addr::LOCALHOST,
+        Ipv4Addr::UNSPECIFIED,
         args.metric_port,
     )));
 
