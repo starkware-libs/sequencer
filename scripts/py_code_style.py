db2f2be31c132f9cb78fe65b5d14d047434e2fb7
Dori Medini <dori@starkware.co>
2025-07-15 12:35:10 +0300

    ci: add python code style script, with black

---
+++ scripts/py_code_style.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.9
+
+import argparse
+import os
+import subprocess
+
+
+ROOT_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run Python code style checks.")
+    parser.add_argument(
+        "--fix", action="store_true", help="Fix mode. Runs only fixable checks, in fix mode."
+    )
+    return parser.parse_args()
+
+
+def run_black(fix: bool):
+    command = ["black", "-l", "100", "-t", "py37", ROOT_PROJECT_DIR]
+    if not fix:
+        command += ["--check", "--diff", "--color"]
+    subprocess.check_output(command)
+
+
+def main():
+    args = parse_args()
+    run_black(fix=args.fix)
+
+
+if __name__ == "__main__":
+    main()
