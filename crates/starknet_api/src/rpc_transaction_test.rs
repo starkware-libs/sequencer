d33ef62de407496e2c1fb5a3ea23fe383da5f3b8
victork <victor.k@starkware.co>
2025-07-15 18:08:29 +0300

    starknet_api: using internal implementation of sizeof part3

--- crates/starknet_api/src/rpc_transaction_test.rs
+++ crates/starknet_api/src/rpc_transaction_test.rs
@@ -1,10 +1,18 @@
 use rstest::rstest;
+use sizeof::SizeOf;
 use starknet_types_core::felt::Felt;
 
 use crate::block::GasPrice;
 use crate::core::CompiledClassHash;
 use crate::execution_resources::GasAmount;
-use crate::rpc_transaction::{DataAvailabilityMode, RpcTransaction};
+use crate::rpc_transaction::{
+    DataAvailabilityMode,
+    RpcDeployAccountTransaction,
+    RpcDeployAccountTransactionV3,
+    RpcInvokeTransaction,
+    RpcInvokeTransactionV3,
+    RpcTransaction,
+};
 use crate::state::SierraContractClass;
 use crate::test_utils::declare::{rpc_declare_tx, DeclareTxArgs};
 use crate::test_utils::deploy_account::{rpc_deploy_account_tx, DeployAccountTxArgs};
@@ -84,4 +92,67 @@ fn test_rpc_transactions(#[case] tx: RpcTransaction) {
     let serialized = serde_json::to_string(&tx).unwrap();
     let deserialized: RpcTransaction = serde_json::from_str(&serialized).unwrap();
     assert_eq!(tx, deserialized);
+}
+
+// Check size of RPC transactions.
+#[test]
+fn test_deploy_account_tx_size_of() {
+    let tx = create_deploy_account_tx();
+    if let RpcTransaction::DeployAccount(deploy_tx) = tx {
+        let RpcDeployAccountTransaction::V3(tx_v3) = deploy_tx;
+
+        // Calculate the expected size of the V3 deploy account transaction.
+        let expected_size = std::mem::size_of::<RpcDeployAccountTransactionV3>()
+            + tx_v3.resource_bounds.l1_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l1_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.resource_bounds.l2_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l2_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.resource_bounds.l1_data_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l1_data_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.tip.dynamic_size()
+            + tx_v3.contract_address_salt.dynamic_size()
+            + tx_v3.class_hash.dynamic_size()
+            + tx_v3.constructor_calldata.dynamic_size()
+            + tx_v3.nonce.dynamic_size()
+            + tx_v3.signature.dynamic_size()
+            + tx_v3.nonce_data_availability_mode.dynamic_size()
+            + tx_v3.fee_data_availability_mode.dynamic_size()
+            + tx_v3.paymaster_data.dynamic_size();
+
+        // Check the size of the V3 deploy account transaction.
+        assert_eq!(tx_v3.size_bytes(), expected_size);
+    } else {
+        panic!("Expected RpcTransaction::DeployAccount");
+    }
+}
+
+#[test]
+fn test_invoke_tx_size_of() {
+    let tx = create_invoke_tx();
+    if let RpcTransaction::Invoke(invoke_tx) = tx {
+        let RpcInvokeTransaction::V3(tx_v3) = invoke_tx;
+
+        // Calculate the expected size of the V3 invoke transaction.
+        let expected_size = std::mem::size_of::<RpcInvokeTransactionV3>()
+            + tx_v3.resource_bounds.l1_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l1_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.resource_bounds.l2_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l2_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.resource_bounds.l1_data_gas.max_amount.dynamic_size()
+            + tx_v3.resource_bounds.l1_data_gas.max_price_per_unit.dynamic_size()
+            + tx_v3.tip.dynamic_size()
+            + tx_v3.calldata.dynamic_size()
+            + tx_v3.sender_address.dynamic_size()
+            + tx_v3.nonce.dynamic_size()
+            + tx_v3.signature.dynamic_size()
+            + tx_v3.nonce_data_availability_mode.dynamic_size()
+            + tx_v3.fee_data_availability_mode.dynamic_size()
+            + tx_v3.paymaster_data.dynamic_size()
+            + tx_v3.account_deployment_data.dynamic_size();
+
+        // Check the size of the V3 invoke transaction.
+        assert_eq!(tx_v3.size_bytes(), expected_size);
+    } else {
+        panic!("Expected RpcTransaction::Invoke");
+    }
 }
