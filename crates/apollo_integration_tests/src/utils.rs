b8b42efaf4249b0bbc0843eb9a1aadbb464627b3
Guy Nir <guy.n@starkware.co>
2025-07-16 13:58:09 +0300

    apollo_node: add validation on L1 gas price lag margin, considering finality and polling interval

--- crates/apollo_integration_tests/src/utils.rs
+++ crates/apollo_integration_tests/src/utils.rs
@@ -27,6 +27,7 @@ use apollo_infra_utils::test_utils::AvailablePorts;
 use apollo_l1_endpoint_monitor::monitor::L1EndpointMonitorConfig;
 use apollo_l1_gas_price::eth_to_strk_oracle::{EthToStrkOracleConfig, ETH_TO_STRK_QUANTIZATION};
 use apollo_l1_gas_price::l1_gas_price_provider::L1GasPriceProviderConfig;
+use apollo_l1_gas_price::l1_gas_price_scraper::L1GasPriceScraperConfig;
 use apollo_l1_gas_price_types::DEFAULT_ETH_TO_FRI_RATE;
 use apollo_l1_provider::l1_scraper::L1ScraperConfig;
 use apollo_l1_provider::L1ProviderConfig;
@@ -209,8 +210,11 @@ pub fn create_node_config(
     let l1_gas_price_provider_config = L1GasPriceProviderConfig {
         // Use newly minted blocks on Anvil to be used for gas price calculations.
         lag_margin_seconds: 0,
+
         ..Default::default()
     };
+    let l1_gas_price_scraper_config =
+        L1GasPriceScraperConfig { polling_interval: Duration::from_secs(0), ..Default::default() };
     let http_server_config =
         create_http_server_config(available_ports.get_next_local_host_socket());
     let class_manager_config =
@@ -264,6 +268,7 @@ pub fn create_node_config(
             l1_provider_config,
             l1_endpoint_monitor_config,
             l1_gas_price_provider_config,
+            l1_gas_price_scraper_config,
             ..Default::default()
         },
         config_pointers_map,
