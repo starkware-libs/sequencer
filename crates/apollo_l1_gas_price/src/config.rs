5c427b3238a94869964f2c0f96bc537f6f708b6b
Guy Nir <guy.n@starkware.co>
2025-07-20 13:32:00 +0300

    apollo_l1_gas_price: create a shared config for provider/scraper

--- crates/apollo_l1_gas_price/src/config.rs
+++ crates/apollo_l1_gas_price/src/config.rs
@@ -9,6 +9,111 @@ use serde::{Deserialize, Serialize};
 use starknet_api::core::ChainId;
 use validator::Validate;
 
+// TODO(guyn): This is not yet used. We'll use it in the next PR, when removing the individual
+// configs.
+#[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
+pub struct L1GasPriceConfig {
+    // TODO(guyn): these two fields need to go into VersionedConstants.
+    pub number_of_blocks_for_mean: u64,
+    // Use seconds not Duration since seconds is the basic quanta of time for both Starknet and
+    // Ethereum.
+    pub lag_margin_seconds: u64,
+    pub storage_limit: usize,
+    // Maximum valid time gap between the requested timestamp and the last price sample in seconds.
+    pub max_time_gap_seconds: u64,
+    pub starting_block: Option<u64>,
+    #[validate(custom = "validate_ascii")]
+    pub chain_id: ChainId,
+    pub finality: u64,
+    #[serde(deserialize_with = "deserialize_float_seconds_to_duration")]
+    pub polling_interval: Duration,
+    // How many sets of config.num_blocks_for_mean blocks to go back
+    // on the chain when starting to scrape.
+    pub startup_num_blocks_multiplier: u64,
+}
+
+impl Default for L1GasPriceConfig {
+    fn default() -> Self {
+        const MEAN_NUMBER_OF_BLOCKS: u64 = 300;
+        Self {
+            number_of_blocks_for_mean: MEAN_NUMBER_OF_BLOCKS,
+            lag_margin_seconds: 60,
+            storage_limit: usize::try_from(10 * MEAN_NUMBER_OF_BLOCKS).unwrap(),
+            max_time_gap_seconds: 900, // 15 minutes
+            starting_block: None,
+            chain_id: ChainId::Other("0x0".to_string()),
+            finality: 0,
+            polling_interval: Duration::from_secs(1),
+            startup_num_blocks_multiplier: 2,
+        }
+    }
+}
+
+impl SerializeConfig for L1GasPriceConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        let mut config = BTreeMap::from([
+            ser_param(
+                "number_of_blocks_for_mean",
+                &self.number_of_blocks_for_mean,
+                "Number of blocks to use for the mean gas price calculation",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "lag_margin_seconds",
+                &self.lag_margin_seconds,
+                "Difference between the time of the block from L1 used to calculate the gas price \
+                 and the time of the L2 block this price is used in",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "storage_limit",
+                &self.storage_limit,
+                "Maximum number of L1 blocks to keep cached",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "max_time_gap_seconds",
+                &self.max_time_gap_seconds,
+                "Maximum valid time gap between the requested timestamp and the last price sample \
+                 in seconds",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "chain_id",
+                &self.chain_id,
+                "The chain to follow. For more details see https://docs.starknet.io/documentation/architecture_and_concepts/Blocks/transactions/#chain-id",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "finality",
+                &self.finality,
+                "Number of blocks to wait for finality in L1",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "polling_interval",
+                &self.polling_interval.as_secs(),
+                "The duration (seconds) between each scraping attempt of L1",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "startup_num_blocks_multiplier",
+                &self.startup_num_blocks_multiplier,
+                "How many sets of config.num_blocks_for_mean blocks to go back on the chain when starting to scrape.",
+                ParamPrivacyInput::Public,
+            ),
+        ]);
+        config.extend(ser_optional_param(
+            &self.starting_block,
+            0, // This value is never used, since #is_none turns it to a None.
+            "starting_block",
+            "Starting block to scrape from",
+            ParamPrivacyInput::Public,
+        ));
+        config
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
 pub struct L1GasPriceProviderConfig {
     // TODO(guyn): these two fields need to go into VersionedConstants.
