0cc33edcfefd78cea972276df114cdd9b127df50
Avi Cohen <avi.cohen@starkware.co>
2025-07-22 12:45:40 +0300

    apollo_compile_to_casm: remove the feature flag cairo_native

--- crates/apollo_compile_to_native/src/compile_test.rs
+++ crates/apollo_compile_to_native/src/compile_test.rs
@@ -1,12 +1,12 @@
 use apollo_compilation_utils::errors::CompilationUtilError;
 use apollo_compilation_utils::test_utils::contract_class_from_file;
+use apollo_compile_to_native_types::SierraCompilationConfig;
 use apollo_infra_utils::path::resolve_project_relative_path;
 use assert_matches::assert_matches;
 use cairo_lang_starknet_classes::contract_class::ContractClass;
 use mempool_test_utils::{FAULTY_ACCOUNT_CLASS_FILE, TEST_FILES_FOLDER};
 
 use crate::compiler::SierraToNativeCompiler;
-use crate::config::SierraCompilationConfig;
 
 fn compiler() -> SierraToNativeCompiler {
     SierraToNativeCompiler::new(SierraCompilationConfig::create_for_testing())
