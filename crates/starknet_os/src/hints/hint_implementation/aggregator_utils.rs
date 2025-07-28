858676bdcca0f7313909ea43e4890798672c40f7
Tzahi Taub <tzahi@starkware.co>
2025-07-28 16:30:11 +0300

    starknet_os: add messages load_into to the aggregator load output hint

--- crates/starknet_os/src/hints/hint_implementation/aggregator_utils.rs
+++ crates/starknet_os/src/hints/hint_implementation/aggregator_utils.rs
@@ -2,10 +2,60 @@ use std::collections::HashMap;
 
 use cairo_vm::types::relocatable::{MaybeRelocatable, Relocatable};
 use cairo_vm::vm::vm_core::VirtualMachine;
+use starknet_api::transaction::MessageToL1;
 use starknet_types_core::felt::Felt;
 
-use crate::io::os_output::FullOsOutput;
+use crate::io::os_output::{
+    FullOsOutput,
+    MessageToL2,
+    MESSAGE_TO_L1_CONST_FIELD_SIZE,
+    MESSAGE_TO_L2_CONST_FIELD_SIZE,
+};
 use crate::vm_utils::{IdentifierGetter, LoadCairoObject, VmUtilsResult};
+
+pub(crate) trait ToMaybeRelocatables {
+    fn to_maybe_relocatables(&self) -> Vec<MaybeRelocatable>;
+}
+
+impl ToMaybeRelocatables for MessageToL1 {
+    fn to_maybe_relocatables(&self) -> Vec<MaybeRelocatable> {
+        let mut res = Vec::<MaybeRelocatable>::with_capacity(
+            MESSAGE_TO_L1_CONST_FIELD_SIZE + self.payload.0.len(),
+        );
+        res.push(Felt::from(self.from_address).into());
+        res.push(Felt::from(self.to_address).into());
+        res.push(Felt::from(self.payload.0.len()).into());
+        res.extend(self.payload.0.iter().map(|felt| felt.into()));
+        res
+    }
+}
+
+impl ToMaybeRelocatables for MessageToL2 {
+    fn to_maybe_relocatables(&self) -> Vec<MaybeRelocatable> {
+        let mut res = Vec::<MaybeRelocatable>::with_capacity(
+            MESSAGE_TO_L2_CONST_FIELD_SIZE + self.payload.0.len(),
+        );
+        res.push(Felt::from(self.from_address).into());
+        res.push(Felt::from(self.to_address).into());
+        res.push((self.nonce.0).into());
+        res.push((self.selector.0).into());
+        res.push(Felt::from(self.payload.0.len()).into());
+        res.extend(self.payload.0.iter().map(|felt| felt.into()));
+        res
+    }
+}
+
+impl<IG: IdentifierGetter, T: ToMaybeRelocatables> LoadCairoObject<IG> for T {
+    fn load_into(
+        &self,
+        vm: &mut VirtualMachine,
+        _identifier_getter: &IG,
+        address: Relocatable,
+        _constants: &std::collections::HashMap<String, Felt>,
+    ) -> VmUtilsResult<Relocatable> {
+        Ok(vm.load_data(address, &self.to_maybe_relocatables())?)
+    }
+}
 
 struct StateEntryManager {
     _state_entry_ptr: Relocatable,
@@ -40,13 +90,29 @@ impl FullStateDiffWriter {
 
 /// Writes the given `FullOsOutput` to the VM at the specified address.
 fn write_full_os_output<IG: IdentifierGetter>(
-    _output: &FullOsOutput,
-    _vm: &mut VirtualMachine,
-    _identifier_getter: &IG,
+    output: &FullOsOutput,
+    vm: &mut VirtualMachine,
+    identifier_getter: &IG,
     _address: Relocatable,
-    _constants: &std::collections::HashMap<String, Felt>,
+    constants: &std::collections::HashMap<String, Felt>,
     _state_diff_writer: &mut FullStateDiffWriter,
 ) -> VmUtilsResult<Relocatable> {
+    let FullOsOutput { common_os_output, .. } = output;
+    let messages_to_l1_start = vm.add_temporary_segment();
+    let _messages_to_l1_end = common_os_output.messages_to_l1.load_into(
+        vm,
+        identifier_getter,
+        messages_to_l1_start,
+        constants,
+    )?;
+
+    let messages_to_l2_start = vm.add_temporary_segment();
+    let _messages_to_l2_end = common_os_output.messages_to_l2.load_into(
+        vm,
+        identifier_getter,
+        messages_to_l2_start,
+        constants,
+    )?;
     todo!()
 }
 
