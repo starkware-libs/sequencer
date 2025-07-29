259e4a2963a36f265f70b69c0907a5b0a0304f5e
AvivYossef-starkware <aviv.y@starkware.co>
2025-07-29 14:55:42 +0300

    apollo_central_sync,apollo_compile_to_casm: compute the actual compiled class hash v2

--- crates/apollo_central_sync/src/lib.rs
+++ crates/apollo_central_sync/src/lib.rs
@@ -56,6 +56,7 @@ use starknet_api::block::{
     BlockSignature,
     StarknetVersion,
 };
+use starknet_api::contract_class::compiled_class_hash::{HashVersion, HashableCompiledClass};
 use starknet_api::contract_class::{ContractClass, SierraVersion};
 use starknet_api::core::{ClassHash, CompiledClassHash, SequencerPublicKey};
 use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContractClass;
@@ -620,8 +621,7 @@ impl<
         compiled_class: CasmContractClass,
         is_compiler_backward_compatible: bool,
     ) -> StateSyncResult {
-        // TODO(Aviv): Compute the actual compiled class hash v2.
-        let compiled_class_hash_v2 = CompiledClassHash::default();
+        let compiled_class_hash_v2 = compiled_class.hash(Some(&HashVersion::V2));
 
         if !is_compiler_backward_compatible {
             if let Some(class_manager_client) = &self.class_manager_client {
