7856920c34defe7a4dcab5eab3615c91b561ae73
Nimrod Weiss <nimrod@starkware.co>
2025-07-31 10:55:55 +0300

    starknet_os_flow_tests: create state diff for the committer

--- crates/starknet_os_flow_tests/src/utils.rs
+++ crates/starknet_os_flow_tests/src/utils.rs
@@ -7,9 +7,15 @@ use blockifier::blockifier::transaction_executor::{
     TransactionExecutorError,
 };
 use blockifier::context::BlockContext;
-use blockifier::state::cached_state::CachedState;
+use blockifier::state::cached_state::{CachedState, CommitmentStateDiff};
 use blockifier::test_utils::maybe_dummy_block_hash_and_number;
 use blockifier::transaction::transaction_execution::Transaction;
+use starknet_committer::block_committer::input::{
+    StarknetStorageKey,
+    StarknetStorageValue,
+    StateDiff,
+};
+use starknet_committer::patricia_merkle_tree::types::CompiledClassHash;
 
 use crate::state_trait::FlowTestState;
 
@@ -49,4 +55,30 @@ pub(crate) fn execute_transactions<S: FlowTestState>(
     let block_summary = executor.finalize().expect("Failed to finalize block.");
     let final_state = executor.block_state.unwrap();
     ExecutionOutput { execution_outputs, block_summary, final_state }
+}
+
+/// Creates a state diff input for the committer based on the execution state diff.
+pub(crate) fn create_committer_state_diff(state_diff: CommitmentStateDiff) -> StateDiff {
+    StateDiff {
+        address_to_class_hash: state_diff.address_to_class_hash.into_iter().collect(),
+        address_to_nonce: state_diff.address_to_nonce.into_iter().collect(),
+        class_hash_to_compiled_class_hash: state_diff
+            .class_hash_to_compiled_class_hash
+            .into_iter()
+            .map(|(k, v)| (k, CompiledClassHash(v.0)))
+            .collect(),
+        storage_updates: state_diff
+            .storage_updates
+            .into_iter()
+            .map(|(address, updates)| {
+                (
+                    address,
+                    updates
+                        .into_iter()
+                        .map(|(k, v)| (StarknetStorageKey(k), StarknetStorageValue(v)))
+                        .collect(),
+                )
+            })
+            .collect(),
+    }
 }
