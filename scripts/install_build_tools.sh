ea93d9c44826f7d216ed9594e652cc61e8693897
Dori Medini <dori@starkware.co>
2025-07-31 13:13:01 +0300

    ci: base docker should use python 3.9
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/install_build_tools.sh
+++ scripts/install_build_tools.sh
@@ -6,12 +6,18 @@ set -e
 
 function install_common_packages() {
     $SUDO  bash -c '
-        apt update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install \
+        apt update && \
+        DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install software-properties-common && \
+        add-apt-repository ppa:deadsnakes/ppa && \
+        apt update && \
+        DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install \
             build-essential \
             clang \
             curl \
             gnupg \
             libzstd-dev \
+            python3.9-dev \
+            python3.9-venv \
             python3-dev \
             python3-venv \
             sudo \
