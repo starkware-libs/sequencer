4bd32dd4b337906a444f95f969484649f7fddba1
Itay Tsabary <itayt@starkware.co>
2025-07-31 14:14:20 +0300

    apollo_node: add component config validations

--- crates/apollo_node/src/utils.rs
+++ crates/apollo_node/src/utils.rs
@@ -1,5 +1,4 @@
 use apollo_config::presentation::get_config_presentation;
-use apollo_config::validators::config_validate;
 use apollo_config::ConfigError;
 use tracing::info;
 
@@ -25,11 +24,11 @@ pub async fn create_node_modules(
 pub fn load_and_validate_config(args: Vec<String>) -> Result<SequencerNodeConfig, ConfigError> {
     let config_load_result = SequencerNodeConfig::load_and_process(args);
     let config =
-        config_load_result.unwrap_or_else(|err| panic!("Failed loading configuration: {}", err));
+        config_load_result.unwrap_or_else(|err| panic!("Failed loading configuration: {err}"));
     info!("Finished loading configuration.");
 
-    if let Err(error) = config_validate(&config) {
-        panic!("Config validation failed: {}", error);
+    if let Err(error) = config.validate_node_config() {
+        panic!("Config validation failed: {error}");
     }
     info!("Finished validating configuration.");
 
