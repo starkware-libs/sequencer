3a67c94d2a605c739df3badb8a5bc240d012dce1
guyf-starkware <guy.f@starkware.co>
2025-07-31 17:46:16 +0300

    A proto for consensus network authenticaion

--- crates/apollo_protobuf/src/protobuf/protoc_output.rs
+++ crates/apollo_protobuf/src/protobuf/protoc_output.rs
@@ -512,6 +512,20 @@ pub mod proposal_part {
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
+pub struct PublicKeyAndChallenge {
+    #[prost(message, optional, tag = "1")]
+    pub public_key: ::core::option::Option<Felt252>,
+    #[prost(message, optional, tag = "2")]
+    pub challenge: ::core::option::Option<Felt252>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct SignedChallengeAndIdentity {
+    #[prost(message, repeated, tag = "1")]
+    pub signature: ::prost::alloc::vec::Vec<Felt252>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct StateDiffCommitment {
     #[prost(uint64, tag = "1")]
     pub state_diff_length: u64,
