5c3b9c247ab1b42d547d3c035b17d4b941dbcd9c
Nimrod Weiss <nimrod@starkware.co>
2025-08-03 11:59:32 +0300

    starknet_os_flow_tests: implement state trait on dict state reader

--- crates/starknet_os_flow_tests/src/state_trait.rs
+++ crates/starknet_os_flow_tests/src/state_trait.rs
@@ -1,4 +1,13 @@
 #![allow(dead_code)]
 use blockifier::state::state_api::UpdatableState;
+use blockifier::test_utils::dict_state_reader::DictStateReader;
 
-pub(crate) trait FlowTestState: UpdatableState + Default + Sync + Send + 'static {}
+pub(crate) trait FlowTestState: UpdatableState + Sync + Send + 'static {
+    fn create_empty_state() -> Self;
+}
+
+impl FlowTestState for DictStateReader {
+    fn create_empty_state() -> Self {
+        DictStateReader::default()
+    }
+}
