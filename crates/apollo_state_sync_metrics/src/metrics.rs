a2e4f05144f21ccd6a8143c4672dfd195c5fabf6
Noam Spiegelstein <noam.s@starkware.co>
2025-08-05 14:32:28 +0300

    apollo_state_sync_metrics: optimize processed transactions reconstruction

--- crates/apollo_state_sync_metrics/src/metrics.rs
+++ crates/apollo_state_sync_metrics/src/metrics.rs
@@ -6,8 +6,6 @@ use apollo_storage::db::TransactionKind;
 use apollo_storage::header::HeaderStorageReader;
 use apollo_storage::state::StateStorageReader;
 use apollo_storage::{StorageReader, StorageTxn};
-use starknet_api::block::BlockNumber;
-use tracing::debug;
 
 define_metrics!(
     StateSync => {
@@ -27,14 +25,7 @@ define_metrics!(
         // Counters
         MetricCounter { STATE_SYNC_PROCESSED_TRANSACTIONS, "apollo_state_sync_processed_transactions", "The number of transactions processed by the state sync component", init = 0 },
         MetricCounter { STATE_SYNC_REVERTED_TRANSACTIONS, "apollo_state_sync_reverted_transactions", "The number of transactions reverted by the state sync component", init = 0 },
-    },
-    Infra => {
-        LabeledMetricHistogram { STATE_SYNC_LABELED_PROCESSING_TIMES_SECS, "state_sync_labeled_processing_times_secs", "Request processing times of the state sync, per label (secs)", labels = STATE_SYNC_REQUEST_LABELS },
-        LabeledMetricHistogram { STATE_SYNC_LABELED_QUEUEING_TIMES_SECS, "state_sync_labeled_queueing_times_secs", "Request queueing times of the state sync, per label (secs)", labels = STATE_SYNC_REQUEST_LABELS },
-        LabeledMetricHistogram { STATE_SYNC_LABELED_LOCAL_RESPONSE_TIMES_SECS, "state_sync_labeled_local_response_times_secs", "Request local response times of the state sync, per label (secs)", labels = STATE_SYNC_REQUEST_LABELS },
-        LabeledMetricHistogram { STATE_SYNC_LABELED_REMOTE_RESPONSE_TIMES_SECS, "state_sync_labeled_remote_response_times_secs", "Request remote response times of the state sync, per label (secs)", labels = STATE_SYNC_REQUEST_LABELS },
-        LabeledMetricHistogram { STATE_SYNC_LABELED_REMOTE_CLIENT_COMMUNICATION_FAILURE_TIMES_SECS, "state_sync_labeled_remote_client_communication_failure_times_secs", "Request communication failure times of the state sync, per label (secs)", labels = STATE_SYNC_REQUEST_LABELS },
-    },
+    }
 );
 
 pub async fn register_metrics(storage_reader: StorageReader) {
@@ -46,11 +37,9 @@ pub async fn register_metrics(storage_reader: StorageReader) {
     STATE_SYNC_PROCESSED_TRANSACTIONS.register();
     STATE_SYNC_REVERTED_TRANSACTIONS.register();
     CENTRAL_SYNC_CENTRAL_BLOCK_MARKER.register();
-    CENTRAL_SYNC_FORKS_FROM_FEEDER.register();
     let _ = tokio::task::spawn_blocking(move || {
         let txn = storage_reader.begin_ro_txn().unwrap();
         update_marker_metrics(&txn);
-        reconstruct_processed_transactions_metric(&txn);
     })
     .await;
 }
