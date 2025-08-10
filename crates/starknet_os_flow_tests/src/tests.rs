515c4d5d62cd6ec5be0bee9df1797c862183ddcd
Nimrod Weiss <nimrod@starkware.co>
2025-08-10 14:39:52 +0300

    starknet_os_flow_tests: state diff comparison

--- crates/starknet_os_flow_tests/src/tests.rs
+++ crates/starknet_os_flow_tests/src/tests.rs
@@ -6,6 +6,8 @@ use blockifier_test_utils::cairo_versions::{CairoVersion, RunnableCairo1};
 use blockifier_test_utils::calldata::create_calldata;
 use blockifier_test_utils::contracts::FeatureContract;
 use rstest::rstest;
+use starknet_api::abi::abi_utils::get_storage_var_address;
+use starknet_api::core::calculate_contract_address;
 use starknet_api::executable_transaction::{DeclareTransaction, InvokeTransaction};
 use starknet_api::execution_resources::GasAmount;
 use starknet_api::test_utils::declare::declare_tx;
@@ -18,12 +20,18 @@ use starknet_api::test_utils::{
     DEFAULT_STRK_L2_GAS_PRICE,
 };
 use starknet_api::transaction::constants::DEPLOY_CONTRACT_FUNCTION_ENTRY_POINT_NAME;
-use starknet_api::transaction::fields::{AllResourceBounds, ResourceBounds, ValidResourceBounds};
+use starknet_api::transaction::fields::{
+    AllResourceBounds,
+    Calldata,
+    ContractAddressSalt,
+    ResourceBounds,
+    ValidResourceBounds,
+};
 use starknet_api::{declare_tx_args, invoke_tx_args};
 use starknet_types_core::felt::Felt;
 
 use crate::initial_state::create_default_initial_state_data;
-use crate::test_manager::{TestManager, FUNDED_ACCOUNT_ADDRESS};
+use crate::test_manager::{OsTestOutput, TestManager, FUNDED_ACCOUNT_ADDRESS};
 use crate::utils::{divide_vec_into_n_parts, get_class_info_of_cairo_1_feature_contract};
 
 pub(crate) static NON_TRIVIAL_RESOURCE_BOUNDS: LazyLock<ValidResourceBounds> =
@@ -92,16 +100,17 @@ async fn declare_deploy_scenario(#[values(1, 2)] n_blocks: usize) {
         DeclareTransaction::create(account_declare_tx, class_info, &CHAIN_ID_FOR_TESTS).unwrap();
     // Add the transaction to the test manager.
     test_manager.add_cairo1_declare_tx(tx, &test_contract_sierra);
-
+    let arg1 = Felt::from(7);
+    let arg2 = Felt::from(90);
     // Deploy the test contract using the deploy contract syscall.
     let constructor_calldata = [
-        2.into(),  // constructor length
-        7.into(),  // arg1
-        90.into(), // arg2
+        2.into(), // constructor length
+        arg1,
+        arg2,
     ];
-    let contract_address_salt = Felt::ONE;
+    let contract_address_salt = ContractAddressSalt(Felt::ONE);
     let calldata: Vec<_> =
-        [class_hash.0, contract_address_salt].into_iter().chain(constructor_calldata).collect();
+        [class_hash.0, contract_address_salt.0].into_iter().chain(constructor_calldata).collect();
     let deploy_contract_calldata = create_calldata(
         *FUNDED_ACCOUNT_ADDRESS,
         DEPLOY_CONTRACT_FUNCTION_ENTRY_POINT_NAME,
@@ -113,14 +122,40 @@ async fn declare_deploy_scenario(#[values(1, 2)] n_blocks: usize) {
         calldata: deploy_contract_calldata,
         resource_bounds: *NON_TRIVIAL_RESOURCE_BOUNDS,
     };
+    let expected_contract_address = calculate_contract_address(
+        contract_address_salt,
+        class_hash,
+        &Calldata(constructor_calldata[1..].to_vec().into()),
+        *FUNDED_ACCOUNT_ADDRESS,
+    )
+    .unwrap();
     let deploy_contract_tx = invoke_tx(invoke_tx_args);
     let deploy_contract_tx =
         InvokeTransaction::create(deploy_contract_tx, &CHAIN_ID_FOR_TESTS).unwrap();
     test_manager.add_invoke_tx(deploy_contract_tx);
     test_manager.divide_transactions_into_n_blocks(n_blocks);
     let initial_block_number = CURRENT_BLOCK_NUMBER + 1;
-    let _os_runner_output =
+    let OsTestOutput { decompressed_state_diff, .. } =
         test_manager.execute_test_with_default_block_contexts(initial_block_number).await;
 
-    // TODO(Nimrod): Validate the OS output.
+    // Verify that the OS output contains the entry in the classes changes from the declare tx.
+    assert!(decompressed_state_diff.compiled_class_hashes.get(&class_hash).is_some_and(
+        |actual_compiled_class_hash| *actual_compiled_class_hash == compiled_class_hash
+    ));
+
+    // Verify the deployed contract.
+    assert!(
+        decompressed_state_diff
+            .class_hashes
+            .get(&expected_contract_address)
+            .is_some_and(|actual_class_hash| *actual_class_hash == class_hash)
+    );
+
+    // Verify the storage update from the contract's constructor.
+    assert!(
+        decompressed_state_diff
+            .storage
+            .get(&(expected_contract_address, get_storage_var_address("my_storage_var", &[])))
+            .is_some_and(|actual_value| *actual_value == arg1 + arg2)
+    );
 }
