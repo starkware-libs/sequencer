81326165f44ee94979c16cb6612c2331dc8a1ad9
Yonatan-Starkware <yonatan.k@starkware.co>
2025-08-11 13:43:53 +0300

    starknet_os: output txs_trace from os_run

--- crates/starknet_committer_and_os_cli/src/os_cli/run_os_cli.rs
+++ crates/starknet_committer_and_os_cli/src/os_cli/run_os_cli.rs
@@ -5,6 +5,7 @@ use cairo_vm::types::relocatable::MaybeRelocatable;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use clap::{Parser, Subcommand};
 use serde::Serialize;
+use starknet_os::hint_processor::os_logger::OsTransactionTrace;
 use starknet_os::hints::enum_definition::AllHints;
 use starknet_os::metrics::OsMetrics;
 use starknet_types_core::felt::Felt;
@@ -62,6 +63,8 @@ enum Command {
     RunOsStateless {
         #[clap(flatten)]
         io_args: IoArgs,
+        #[clap(long)]
+        include_txs_trace: bool,
     },
     RunAggregator {
         #[clap(flatten)]
@@ -81,8 +84,11 @@ pub async fn run_os_cli(
         Command::PythonTest(python_test_arg) => {
             run_python_test::<OsPythonTestRunner>(python_test_arg).await;
         }
-        Command::RunOsStateless { io_args: IoArgs { input_path, output_path } } => {
-            parse_and_run_os(input_path, output_path, log_filter_handle);
+        Command::RunOsStateless {
+            io_args: IoArgs { input_path, output_path },
+            include_txs_trace,
+        } => {
+            parse_and_run_os(input_path, output_path, log_filter_handle, include_txs_trace);
         }
         Command::RunAggregator { io_args: IoArgs { input_path, output_path } } => {
             parse_and_run_aggregator(input_path, output_path, log_filter_handle);
@@ -141,6 +147,8 @@ pub(crate) struct OsCliOutput {
     pub(crate) da_segment: Option<Vec<Felt>>,
     pub(crate) metrics: OsCliMetrics,
     pub unused_hints: HashSet<AllHints>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) txs_trace: Option<Vec<OsTransactionTrace>>,
 }
 
 #[derive(Serialize)]
