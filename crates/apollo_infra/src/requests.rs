a1982602b93390c5306bbe2ed096613bc9fa3a0d
Alon Lukatch <alon.l@starkware.co>
2025-08-17 13:40:28 +0300

    apollo_infra: change time metrics to labeled on localservermetrics

--- crates/apollo_infra/src/requests.rs
+++ crates/apollo_infra/src/requests.rs
@@ -1,17 +1,16 @@
 pub trait LabeledRequest {
-    type Disc: Send + Sync + 'static;
-    fn disc(&self) -> Self::Disc;
+    fn request_label(&self) -> &'static str;
 }
 
 pub const LABEL_NAME_REQUEST_VARIANT: &str = "request_variant";
 
 #[macro_export]
 macro_rules! impl_labeled_request {
-    ($Enum:ty, $Disc:ty) => {
+    ($Enum:ty, $RequestLabel:ty) => {
         impl $crate::requests::LabeledRequest for $Enum {
-            type Disc = $Disc;
-            fn disc(&self) -> Self::Disc {
-                <$Disc>::from(self)
+            fn request_label(&self) -> &'static str {
+                let label: $RequestLabel = <$RequestLabel>::from(self);
+                label.into()
             }
         }
     };
