bd7eb266370777c29bb87f61204346516e98903f
Ayelet Zilber <ayelet@starkware.co>
2025-08-20 13:59:00 +0300

    apollo_storage: add panels

--- crates/apollo_dashboard/src/panels/storage.rs
+++ crates/apollo_dashboard/src/panels/storage.rs
@@ -1,5 +1,17 @@
-use crate::dashboard::Row;
+use apollo_storage::metrics::{STORAGE_APPEND_THIN_STATE_DIFF_LATENCY, STORAGE_COMMIT_LATENCY};
+
+use crate::dashboard::{Panel, PanelType, Row};
+
+fn get_storage_append_thin_state_diff_latency() -> Panel {
+    Panel::from_hist(STORAGE_APPEND_THIN_STATE_DIFF_LATENCY, PanelType::TimeSeries)
+}
+fn get_storage_commit_latency() -> Panel {
+    Panel::from_hist(STORAGE_COMMIT_LATENCY, PanelType::TimeSeries)
+}
 
 pub(crate) fn get_storage_row() -> Row {
-    Row::new("Storage", vec![])
+    Row::new(
+        "Storage",
+        vec![get_storage_append_thin_state_diff_latency(), get_storage_commit_latency()],
+    )
 }
