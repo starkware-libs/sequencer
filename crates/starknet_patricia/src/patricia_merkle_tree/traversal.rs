f8d07c591f39e364c6a2cd447d494cc5b0b15945
rotem-starkware <rotem@starkware.co>
2025-08-21 15:33:02 +0300

    starknet_patricia: impl generic fetch_witnesses and test

--- crates/starknet_patricia/src/patricia_merkle_tree/traversal.rs
+++ crates/starknet_patricia/src/patricia_merkle_tree/traversal.rs
@@ -1,3 +1,4 @@
+use starknet_api::core::PatriciaKey;
 use starknet_patricia_storage::errors::{DeserializationError, StorageError};
 use starknet_patricia_storage::storage_trait::{create_db_key, DbKey, Storage};
 use thiserror::Error;
@@ -5,7 +6,12 @@ use thiserror::Error;
 use crate::hash::hash_trait::HashOutput;
 use crate::patricia_merkle_tree::filled_tree::node::FilledNode;
 use crate::patricia_merkle_tree::filled_tree::node_serde::PatriciaPrefix;
-use crate::patricia_merkle_tree::node_data::inner_node::PathToBottom;
+use crate::patricia_merkle_tree::node_data::inner_node::{
+    NodeData,
+    PathToBottom,
+    Preimage,
+    PreimageMap,
+};
 use crate::patricia_merkle_tree::node_data::leaf::Leaf;
 use crate::patricia_merkle_tree::original_skeleton_tree::utils::split_leaves;
 use crate::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices, SubTreeHeight};
@@ -136,4 +142,82 @@ pub(crate) fn calculate_subtrees_roots<'a, L: Leaf>(
         subtrees_roots.push(FilledNode::deserialize(subtree.root_hash, val, subtree.is_leaf())?)
     }
     Ok(subtrees_roots)
+}
+
+#[allow(dead_code)]
+/// Returns the Patricia witnesses (`PreimageMap`) of the given tree according to the root hash.
+/// Fetches only inner nodes. The witnesses are required make a commitment.
+pub fn fetch_witnesses<L: Leaf>(
+    storage: &impl Storage,
+    root_hash: HashOutput,
+    partial_leaf_indices: &[PatriciaKey],
+) -> TraversalResult<PreimageMap> {
+    let mut witnesses = PreimageMap::new();
+    if partial_leaf_indices.is_empty() {
+        return Ok(witnesses);
+    }
+
+    let mut full_leaf_indices: Vec<_> =
+        partial_leaf_indices.iter().map(|idx| NodeIndex::from_leaf_felt(idx.key())).collect();
+    let main_subtree = SubTree {
+        sorted_leaf_indices: SortedLeafIndices::new(&mut full_leaf_indices),
+        root_index: NodeIndex::ROOT,
+        root_hash,
+    };
+
+    fetch_witnesses_inner::<L>(storage, vec![main_subtree], &mut witnesses)?;
+    Ok(witnesses)
+}
+
+#[allow(dead_code)]
+/// Fetches the Patricia witnesses, required to make a commitment.
+/// Given a list of subtrees, traverses towards their leaves and fetches all non-empty,
+/// unmodified inner nodes, and the parents of the modified nodes, if the sibling is unmodified.
+fn fetch_witnesses_inner<'a, L: Leaf>(
+    storage: &impl Storage,
+    subtrees: Vec<SubTree<'a>>,
+    witnesses: &mut PreimageMap,
+) -> TraversalResult<()> {
+    if subtrees.is_empty() {
+        return Ok(());
+    }
+
+    let mut next_subtrees = Vec::new();
+    let filled_roots = calculate_subtrees_roots::<L>(&subtrees, storage)?;
+    for (filled_root, subtree) in filled_roots.into_iter().zip(subtrees.iter()) {
+        match filled_root.data {
+            // Binary node.
+            NodeData::Binary(binary_data) => {
+                if subtree.is_unmodified()
+                    || (subtree.get_height() == SubTreeHeight::new(1)
+                        && subtree.sorted_leaf_indices.len() == 1)
+                {
+                    witnesses.insert(subtree.root_hash, Preimage::Binary(binary_data));
+                    continue;
+                }
+                let (left_subtree, right_subtree) =
+                    subtree.get_children_subtrees(binary_data.left_hash, binary_data.right_hash);
+                if !left_subtree.is_leaf() {
+                    next_subtrees.push(left_subtree);
+                    next_subtrees.push(right_subtree);
+                }
+            }
+            // Edge node.
+            NodeData::Edge(edge_data) => {
+                if subtree.is_unmodified() {
+                    witnesses.insert(subtree.root_hash, Preimage::Edge(edge_data));
+                    continue;
+                }
+                // Parse bottom.
+                let (bottom_subtree, _) =
+                    subtree.get_bottom_subtree(&edge_data.path_to_bottom, edge_data.bottom_hash);
+                if !bottom_subtree.is_leaf() {
+                    next_subtrees.push(bottom_subtree);
+                }
+            }
+            // Leaf node.
+            NodeData::Leaf(_) => {}
+        }
+    }
+    fetch_witnesses_inner::<L>(storage, next_subtrees, witnesses)
 }
