bf304d39457380d383a065d1a65eb8220d9ccdf0
Dori Medini <dori@starkware.co>
2025-08-21 15:39:33 +0300

    starknet_os: add STARKNET_OS_CONFIG_HASH_VERSION const and test it

--- crates/starknet_os/src/io/os_output.rs
+++ crates/starknet_os/src/io/os_output.rs
@@ -27,6 +27,9 @@ use crate::metrics::OsMetrics;
 const MESSAGE_TO_L1_CONST_FIELD_SIZE: usize = 3; // from_address, to_address, payload_size.
 // from_address, to_address, nonce, selector, payload_size.
 const MESSAGE_TO_L2_CONST_FIELD_SIZE: usize = 5;
+// Hex of 'StarknetOsConfig3'.
+pub const STARKNET_OS_CONFIG_HASH_VERSION: Felt =
+    Felt::from_hex_unchecked("0x537461726b6e65744f73436f6e66696733");
 
 #[derive(Debug, thiserror::Error)]
 pub enum OsOutputError {
