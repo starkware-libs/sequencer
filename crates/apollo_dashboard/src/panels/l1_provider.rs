d0bc8c60d9cface063a90146a4fcff4623c5e9e6
Guy Nir <guy.n@starkware.co>
2025-08-24 15:55:40 +0300

    apollo_l1_provider: add metric for time between successful scrapes

--- crates/apollo_dashboard/src/panels/l1_provider.rs
+++ crates/apollo_dashboard/src/panels/l1_provider.rs
@@ -1,6 +1,7 @@
 use apollo_l1_provider::metrics::{
     L1_MESSAGE_SCRAPER_BASELAYER_ERROR_COUNT,
     L1_MESSAGE_SCRAPER_REORG_DETECTED,
+    L1_MESSAGE_SCRAPER_SECONDS_SINCE_LAST_SUCCESSFUL_SCRAPE,
     L1_MESSAGE_SCRAPER_SUCCESS_COUNT,
 };
 
@@ -15,6 +16,12 @@ fn get_panel_l1_message_scraper_baselayer_error_count() -> Panel {
 fn get_panel_l1_message_scraper_reorg_detected() -> Panel {
     Panel::from(&L1_MESSAGE_SCRAPER_REORG_DETECTED)
 }
+fn get_panel_l1_message_scraper_seconds_since_last_successful_scrape() -> Panel {
+    Panel::from_gauge(
+        &L1_MESSAGE_SCRAPER_SECONDS_SINCE_LAST_SUCCESSFUL_SCRAPE,
+        PanelType::TimeSeries,
+    )
+}
 
 // TODO(noamsp): rename to l1_event_row
 pub(crate) fn get_l1_provider_row() -> Row {
@@ -24,6 +31,7 @@ pub(crate) fn get_l1_provider_row() -> Row {
             get_panel_l1_message_scraper_success_count(),
             get_panel_l1_message_scraper_baselayer_error_count(),
             get_panel_l1_message_scraper_reorg_detected(),
+            get_panel_l1_message_scraper_seconds_since_last_successful_scrape(),
         ],
     )
 }
