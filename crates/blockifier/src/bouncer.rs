422016f138511cc7d5bccac711f1c729e9bf352f
Yonatan-Starkware <yonatan.k@starkware.co>
2025-08-24 12:25:21 +0300

    blockifier: change computation logic in get_tx_weight

--- crates/blockifier/src/bouncer.rs
+++ crates/blockifier/src/bouncer.rs
@@ -615,10 +615,10 @@ fn memory_holes_to_gas(
 fn proving_gas_from_builtins_and_sierra_gas(
     builtin_counters: &BuiltinCounterMap,
     sierra_gas: GasAmount,
-    builtin_weights: &BuiltinWeights,
+    builtin_gas_costs: &BuiltinGasCosts,
     versioned_constants: &VersionedConstants,
 ) -> GasAmount {
-    let builtins_proving_gas = builtins_to_gas(builtin_counters, &builtin_weights.weights);
+    let builtins_proving_gas = builtins_to_gas(builtin_counters, builtin_gas_costs);
     let steps_proving_gas =
         sierra_gas_to_steps_gas(sierra_gas, builtin_counters, versioned_constants);
 
@@ -693,7 +693,69 @@ fn add_casm_hash_computation_gas_cost(
     (casm_hash_computation_data_gas.total_gas(), casm_hash_computation_data_gas)
 }
 
-// TODO(Noa):Fix.
+fn compute_sierra_gas(
+    vm_resources: &ExecutionResources,
+    sierra_builtin_gas_costs: &BuiltinGasCosts,
+    versioned_constants: &VersionedConstants,
+    tx_resources: &TransactionResources,
+    sierra_migration_gas: GasAmount,
+    class_hash_to_casm_hash_computation_resources: &HashMap<ClassHash, ExecutionResources>,
+) -> (GasAmount, CasmHashComputationData, GasAmount) {
+    let mut vm_resources_sierra_gas =
+        vm_resources_to_gas(vm_resources, sierra_builtin_gas_costs, versioned_constants);
+    let sierra_gas = tx_resources.computation.sierra_gas;
+
+    vm_resources_sierra_gas = vm_resources_sierra_gas.checked_add_panic_on_overflow(sierra_gas);
+
+    // Each contract is migrated only once, and this migration resources is not part of the CASM
+    // hash computation, which is performed every time a contract is loaded.
+    let sierra_gas_without_casm_hash_computation =
+        vm_resources_sierra_gas.checked_add_panic_on_overflow(sierra_migration_gas);
+
+    let (total_sierra_gas, casm_hash_computation_data_sierra_gas) =
+        add_casm_hash_computation_gas_cost(
+            class_hash_to_casm_hash_computation_resources,
+            sierra_gas_without_casm_hash_computation,
+            sierra_builtin_gas_costs,
+            versioned_constants,
+        );
+    (total_sierra_gas, casm_hash_computation_data_sierra_gas, vm_resources_sierra_gas)
+}
+
+fn compute_proving_gas(
+    builtin_counters: &BuiltinCounterMap,
+    vm_resources_sierra_gas: GasAmount,
+    proving_builtin_gas_costs: &BuiltinGasCosts,
+    versioned_constants: &VersionedConstants,
+    migration_poseidon_builtin_counter: &BuiltinCounterMap,
+    sierra_migration_gas: GasAmount,
+    class_hash_to_casm_hash_computation_resources: &HashMap<ClassHash, ExecutionResources>,
+) -> (GasAmount, CasmHashComputationData) {
+    let vm_resources_proving_gas = proving_gas_from_builtins_and_sierra_gas(
+        builtin_counters,
+        vm_resources_sierra_gas,
+        proving_builtin_gas_costs,
+        versioned_constants,
+    );
+
+    let proving_migration_gas = proving_gas_from_builtins_and_sierra_gas(
+        migration_poseidon_builtin_counter,
+        sierra_migration_gas,
+        proving_builtin_gas_costs,
+        versioned_constants,
+    );
+
+    let proving_gas_without_casm_hash_computation =
+        vm_resources_proving_gas.checked_add_panic_on_overflow(proving_migration_gas);
+
+    add_casm_hash_computation_gas_cost(
+        class_hash_to_casm_hash_computation_resources,
+        proving_gas_without_casm_hash_computation,
+        proving_builtin_gas_costs,
+        versioned_constants,
+    )
+}
+
 #[allow(clippy::too_many_arguments)]
 pub fn get_tx_weights<S: StateReader>(
     state_reader: &S,
@@ -717,12 +779,12 @@ pub fn get_tx_weights<S: StateReader>(
     let patrticia_update_resources = get_particia_update_resources(n_visited_storage_entries);
     let vm_resources = &patrticia_update_resources + &tx_resources.computation.total_vm_resources();
 
-    // Sierra gas computation.
-    let builtin_gas_cost = versioned_constants.os_constants.gas_costs.builtins;
-    let vm_resources_sierra_gas =
-        vm_resources_to_gas(&vm_resources, &builtin_gas_cost, versioned_constants);
-    let sierra_gas = tx_resources.computation.sierra_gas;
-    let (class_hashes_to_migrate, migration_gas, migration_poseidon_builtin_counter) =
+    // Builtin gas costs for stone and for stwo.
+    let sierra_builtin_gas_costs = &versioned_constants.os_constants.gas_costs.builtins;
+    let proving_builtin_gas_costs = &bouncer_config.builtin_weights.weights;
+
+    // Prepare migration data.
+    let (class_hashes_to_migrate, sierra_migration_gas, migration_poseidon_builtin_counter) =
         if versioned_constants.enable_casm_hash_migration {
             get_migration_data(
                 state_reader,
@@ -733,52 +795,32 @@ pub fn get_tx_weights<S: StateReader>(
         } else {
             (HashMap::new(), GasAmount::ZERO, HashMap::new())
         };
-    let sierra_gas_without_casm_hash_computation =
-        sierra_gas.checked_add_panic_on_overflow(vm_resources_sierra_gas);
-    // Each contract is migrated only once, and this migration resources is not part of the CASM
-    // hash computation, which is performed every time a contract is loaded.
-    sierra_gas_without_casm_hash_computation.checked_add_panic_on_overflow(migration_gas);
 
-    let (total_sierra_gas, casm_hash_computation_data_sierra_gas) =
-        add_casm_hash_computation_gas_cost(
-            &class_hash_to_casm_hash_computation_resources,
-            sierra_gas_without_casm_hash_computation,
-            &builtin_gas_cost,
+    // Sierra gas computation.
+    let (total_sierra_gas, casm_hash_computation_data_sierra_gas, vm_resources_sierra_gas) =
+        compute_sierra_gas(
+            &vm_resources,
+            sierra_builtin_gas_costs,
             versioned_constants,
+            tx_resources,
+            sierra_migration_gas,
+            &class_hash_to_casm_hash_computation_resources,
         );
 
     // Proving gas computation.
-    let mut builtin_counters_without_casm_hash_computation =
-        patrticia_update_resources.prover_builtins();
-    add_maps(&mut builtin_counters_without_casm_hash_computation, tx_builtin_counters);
-    // The transaction builtin counters does not include the transaction overhead ('additional')
-    // resources.
-    add_maps(
-        &mut builtin_counters_without_casm_hash_computation,
-        &tx_resources.computation.os_vm_resources.prover_builtins(),
-    );
-    // Migration occurs once per contract, and thus is not treated as part of the CASM hash
-    // computation.
-    add_maps(
-        &mut builtin_counters_without_casm_hash_computation,
-        &migration_poseidon_builtin_counter,
-    );
+    let mut vm_resources_builtins_for_proving_gas_computation =
+        (&patrticia_update_resources + &tx_resources.computation.os_vm_resources).prover_builtins();
+    add_maps(&mut vm_resources_builtins_for_proving_gas_computation, tx_builtin_counters);
 
-    let builtin_proving_weights = &bouncer_config.builtin_weights;
-    let proving_gas_without_casm_hash_computation = proving_gas_from_builtins_and_sierra_gas(
-        &builtin_counters_without_casm_hash_computation,
-        sierra_gas_without_casm_hash_computation,
-        builtin_proving_weights,
+    let (total_proving_gas, casm_hash_computation_data_proving_gas) = compute_proving_gas(
+        &vm_resources_builtins_for_proving_gas_computation,
+        vm_resources_sierra_gas,
+        proving_builtin_gas_costs,
         versioned_constants,
+        &migration_poseidon_builtin_counter,
+        sierra_migration_gas,
+        &class_hash_to_casm_hash_computation_resources,
     );
-
-    let (total_proving_gas, casm_hash_computation_data_proving_gas) =
-        add_casm_hash_computation_gas_cost(
-            &class_hash_to_casm_hash_computation_resources,
-            proving_gas_without_casm_hash_computation,
-            &builtin_proving_weights.weights,
-            versioned_constants,
-        );
 
     let bouncer_weights = BouncerWeights {
         l1_gas: message_starknet_l1gas,
