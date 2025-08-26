7d9711689b5a87663d42ca5d7d16be6e6157b6ef
Aviv Greenburg <aviv.g@starkware.co>
2025-08-26 13:26:36 +0300

    blockifier: replace estimate_casm_blake with trait function

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -25,7 +25,6 @@ use serde::de::Error as DeserializationError;
 use serde::{Deserialize, Deserializer, Serialize};
 use starknet_api::contract_class::compiled_class_hash::{
     EntryPointHashable,
-    HashVersion,
     HashableCompiledClass,
     HashableNestedIntList,
 };
@@ -48,7 +47,6 @@ use crate::execution::casm_hash_estimation::{
     CasmV1HashResourceEstimate,
     CasmV2HashResourceEstimate,
     EstimateCasmHashResources,
-    EstimatedExecutionResources,
 };
 use crate::execution::entry_point::{EntryPointExecutionContext, EntryPointTypeAndSelector};
 use crate::execution::errors::PreExecutionError;
@@ -489,7 +487,10 @@ impl CompiledClassV1 {
         blake_weight: usize,
     ) -> (GasAmount, BuiltinCounterMap) {
         let blake_hash_resources =
-            estimate_casm_blake_hash_computation_resources(&self.bytecode_segment_felt_sizes);
+            CasmV2HashResourceEstimate::estimated_resources_of_compiled_class_hash(
+                &self.bytecode_segment_felt_sizes,
+                &self.entry_points_by_type,
+            );
         let blake_hash_gas = blake_execution_resources_estimation_to_gas(
             blake_hash_resources,
             versioned_constants,
@@ -562,64 +563,6 @@ impl HashableCompiledClass<EntryPointV1, NestedFeltCounts> for CompiledClassV1 {
     fn get_bytecode_segment_lengths(&self) -> Cow<'_, NestedFeltCounts> {
         Cow::Borrowed(&self.bytecode_segment_felt_sizes)
     }
-}
-
-/// Cost to hash a single flat segment of `len` felts.
-fn leaf_cost(felt_size_groups: &FeltSizeCount) -> EstimatedExecutionResources {
-    CasmV2HashResourceEstimate::estimated_resources_of_hash_function(felt_size_groups)
-}
-
-/// Cost to hash a multi-segment contract:
-fn node_cost(segs: &[NestedFeltCounts]) -> EstimatedExecutionResources {
-    // TODO(AvivG): Add base estimation for node.
-    let mut resources = EstimatedExecutionResources::new(HashVersion::V2);
-
-    // TODO(AvivG): Add base estimation of each segment. Could this be part of 'leaf_cost'?
-    let segment_overhead = ExecutionResources::default();
-
-    // For each segment, hash its felts.
-    for seg in segs {
-        match seg {
-            NestedFeltCounts::Leaf(_, felt_size_groups) => {
-                resources += &segment_overhead;
-                resources += &leaf_cost(felt_size_groups);
-            }
-            _ => panic!("Estimating hash cost only supports at most one level of segmentation."),
-        }
-    }
-
-    // Node‐level hash over (hash1, len1, hash2, len2, …): one segment hash (“big” felt))
-    // and one segment length (“small” felt) per segment.
-    resources +=
-        &CasmV2HashResourceEstimate::estimated_resources_of_hash_function(&FeltSizeCount {
-            large: segs.len(),
-            small: segs.len(),
-        });
-
-    resources
-}
-
-/// Estimates the VM resources to compute the CASM Blake hash for a Cairo-1 contract:
-/// - Uses only bytecode size.
-pub fn estimate_casm_blake_hash_computation_resources(
-    bytecode_segment_lengths: &NestedFeltCounts,
-) -> EstimatedExecutionResources {
-    // TODO(AvivG): Currently ignores entry-point hashing costs.
-    // TODO(AvivG): Missing base overhead estimation for compiled_class_hash.
-
-    // Basic frame overhead.
-    let mut resources = EstimatedExecutionResources::new(HashVersion::V2);
-
-    // Add leaf vs node cost
-    let added_resources = match &bytecode_segment_lengths {
-        // Single-segment contract (e.g., older Sierra contracts).
-        NestedFeltCounts::Leaf(_, felt_size_groups) => leaf_cost(felt_size_groups),
-        NestedFeltCounts::Node(segs) => node_cost(segs),
-    };
-
-    resources += &added_resources;
-
-    resources
 }
 
 // Returns the set of segments that were visited according to the given visited PCs and segment
