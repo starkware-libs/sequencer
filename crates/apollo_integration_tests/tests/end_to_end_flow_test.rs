7f697ab871cd958b4b52f2f0dfa1122cc13ff0a3
Aviv Greenburg <aviv.g@starkware.co>
2025-08-26 12:13:21 +0300

    blockifier: add casm entry_points estimation

--- crates/apollo_integration_tests/tests/end_to_end_flow_test.rs
+++ crates/apollo_integration_tests/tests/end_to_end_flow_test.rs
@@ -21,7 +21,7 @@ async fn test_end_to_end_flow() {
     end_to_end_flow(
         TestIdentifier::EndToEndFlowTest,
         create_test_scenarios(),
-        GasAmount(57000000),
+        GasAmount(59000000),
         false,
         false,
     )
