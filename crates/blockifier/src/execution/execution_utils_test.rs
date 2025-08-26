ed26a1d4123899409aa6be40b50a4e986d861a6c
avivg-starkware <aviv.g@starkware.co>
2025-08-26 20:49:44 +0300

    blockifier: remove test todos (#8620)

--- crates/blockifier/src/execution/execution_utils_test.rs
+++ crates/blockifier/src/execution/execution_utils_test.rs
@@ -54,12 +54,3 @@ fn test_zero_inputs() {
     assert_eq!(resources.resources(), &expected, "Unexpected resources values for zero-input hash");
     assert_eq!(resources.blake_count(), 0, "Expected zero BLAKE opcodes for zero inputs");
 }
-
-// TODO(AvivG): Add tests for:
-// - `estimate_steps_of_encode_felt252_data_and_calc_blake_hash` simple cases (felts input).
-// - `blake_opcode_count` simple cases (felts input).
-// - `cost_of_encode_felt252_data_and_calc_blake_hash` simple cases (felts input) (including partial
-//   remainder).
-// - `cost_of_encode_felt252_data_and_calc_blake_hash` compare against actual execution resources
-//   from running a Cairo entry point (computing blake).
-// - base steps costs - compare against actual execution resources by running on an empty input.
