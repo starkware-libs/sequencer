30341190fa651e0b92de845a0530dd6ced7353b2
Yoav Gross <yoav@starkware.co>
2025-09-03 10:07:30 +0300

    starknet_committer: wrapped map storage

--- crates/starknet_committer_and_os_cli/src/committer_cli/filled_tree_output/filled_forest.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/filled_tree_output/filled_forest.rs
@@ -21,7 +21,7 @@ pub struct Output {
 impl SerializedForest {
     pub fn forest_to_output(&self) -> Output {
         // Create an empty storage for the new facts.
-        let mut storage = HashMap::new();
+        let mut storage = MapStorage::new();
         self.0.write_to_storage(&mut storage);
         let contract_storage_root_hash = self.0.get_contract_root_hash().0;
         let compiled_class_root_hash = self.0.get_compiled_class_root_hash().0;
