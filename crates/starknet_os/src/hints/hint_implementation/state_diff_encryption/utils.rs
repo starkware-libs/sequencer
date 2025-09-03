0efeba587988628de2907b5b51b95bfa63db4aa9
Avi Cohen <avi.cohen@starkware.co>
2025-09-03 15:36:17 +0300

    starknet_os: add state diff decryption test utils

---
+++ crates/starknet_os/src/hints/hint_implementation/state_diff_encryption/utils.rs
@@ -0,0 +1,50 @@
+use blake2s::encode_felt252_data_and_calc_blake_hash;
+use lambdaworks_math::elliptic_curve::short_weierstrass::curves::stark_curve::StarkCurve;
+use lambdaworks_math::elliptic_curve::short_weierstrass::traits::IsShortWeierstrass;
+use starknet_types_core::curve::AffinePoint;
+use starknet_types_core::felt::Felt;
+
+#[cfg(test)]
+#[path = "utils_test.rs"]
+mod utils_test;
+
+// TODO(Avi, 10/09/2025): Remove this and use [AffinePoint::new_from_x] instead after bumping
+// starknet-types-core to 0.2.0.
+#[allow(dead_code)]
+fn recover_y(x: Felt) -> Felt {
+    let alpha = Felt::from_bytes_be(&StarkCurve::a().to_bytes_be());
+    let beta = Felt::from_bytes_be(&StarkCurve::b().to_bytes_be());
+    let y_sq = x * x * x + alpha * x + beta;
+    y_sq.sqrt().expect("x is not on the curve")
+}
+
+#[allow(dead_code)]
+pub fn decrypt_state_diff(
+    private_key: Felt,
+    sn_public_key: Felt,
+    encrypted_symmetric_key: Felt,
+    encrypted_state_diff: &[Felt],
+) -> Vec<Felt> {
+    // Compute the shared secret using Diffie-Hellman key exchange.
+    let sn_public_key_y = recover_y(sn_public_key);
+    let sn_public_key_point =
+        AffinePoint::new(sn_public_key, sn_public_key_y).expect("invalid public key coordinates");
+    let shared_secret_point = &sn_public_key_point * private_key;
+    let shared_secret = shared_secret_point.x();
+
+    // Decrypt the symmetric key using the shared secret.
+    // TODO(Avi, 10/09/2025): Use the naive felt encoding once avialable.
+    let symmetric_key =
+        encrypted_symmetric_key - encode_felt252_data_and_calc_blake_hash(&[shared_secret]);
+
+    // Decrypt the state diff using the symmetric key.
+    // TODO(Avi, 10/09/2025): Use the naive felt encoding once avialable.
+    encrypted_state_diff
+        .iter()
+        .enumerate()
+        .map(|(i, encrypted_felt)| {
+            encrypted_felt
+                - encode_felt252_data_and_calc_blake_hash(&[symmetric_key, Felt::from(i)])
+        })
+        .collect()
+}
