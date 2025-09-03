bffc47f1826fffcd09f6f685f6dd41dbb6dcf29a
Yoav Gross <yoav@starkware.co>
2025-09-03 11:28:49 +0300

    starknet_committer: mut in get from storage

--- crates/starknet_committer_and_os_cli/src/committer_cli/commands.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/commands.rs
@@ -31,9 +31,9 @@ pub async fn parse_and_commit(
     commit(input, output_path, storage).await;
 }
 
-pub async fn commit(input: InputImpl, output_path: String, storage: MapStorage) {
+pub async fn commit(input: InputImpl, output_path: String, mut storage: MapStorage) {
     let serialized_filled_forest = SerializedForest(
-        commit_block(input, &storage).await.expect("Failed to commit the given block."),
+        commit_block(input, &mut storage).await.expect("Failed to commit the given block."),
     );
     let output = serialized_filled_forest.forest_to_output();
     write_to_file(&output_path, &output);
