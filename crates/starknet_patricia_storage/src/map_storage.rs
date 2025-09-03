cf4d1227a8f89242f3cc480f0e45c3a68ba8b648
Yoav Gross <yoav@starkware.co>
2025-09-03 11:56:31 +0300

    starknet_committer: cached storage

--- crates/starknet_patricia_storage/src/map_storage.rs
+++ crates/starknet_patricia_storage/src/map_storage.rs
@@ -1,3 +1,6 @@
+use std::num::NonZeroUsize;
+
+use lru::LruCache;
 use serde::Serialize;
 
 use crate::storage_trait::{DbKey, DbValue, PatriciaStorageResult, Storage, StorageHashMap};
@@ -36,5 +39,54 @@ impl Storage for MapStorage {
 
     fn mget(&mut self, keys: &[DbKey]) -> PatriciaStorageResult<Vec<Option<DbValue>>> {
         Ok(keys.iter().map(|key| self.0.get(key).cloned()).collect())
+    }
+}
+
+/// A storage wrapper that adds an LRU cache to an underlying storage.
+/// Only getter methods are cached.
+pub struct CachedStorage<S: Storage> {
+    pub storage: S,
+    pub cache: LruCache<DbKey, DbValue>,
+}
+
+impl<S: Storage> CachedStorage<S> {
+    pub fn new(storage: S, cache_capacity: NonZeroUsize) -> Self {
+        Self { storage, cache: LruCache::new(cache_capacity) }
+    }
+}
+
+impl<S: Storage> Storage for CachedStorage<S> {
+    fn get(&mut self, key: &DbKey) -> PatriciaStorageResult<Option<DbValue>> {
+        if let Some(cached_value) = self.cache.get(key) {
+            return Ok(Some(cached_value.clone()));
+        }
+
+        let storage_value = self.storage.get(key)?;
+        if let Some(value) = &storage_value {
+            self.cache.put(key.clone(), value.clone());
+        }
+
+        Ok(storage_value)
+    }
+
+    fn set(&mut self, key: DbKey, value: DbValue) -> PatriciaStorageResult<Option<DbValue>> {
+        self.cache.pop(&key);
+        self.storage.set(key, value)
+    }
+
+    fn mget(&mut self, keys: &[DbKey]) -> PatriciaStorageResult<Vec<Option<DbValue>>> {
+        keys.iter().map(|key| self.get(key)).collect::<PatriciaStorageResult<Vec<_>>>()
+    }
+
+    fn mset(&mut self, key_to_value: StorageHashMap) -> PatriciaStorageResult<()> {
+        key_to_value.keys().for_each(|key| {
+            self.cache.pop(&key);
+        });
+        self.storage.mset(key_to_value)
+    }
+
+    fn delete(&mut self, key: &DbKey) -> PatriciaStorageResult<Option<DbValue>> {
+        self.cache.pop(key);
+        self.storage.delete(key)
     }
 }
---
+++ crates/starknet_patricia_storage/src/map_storage_test.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+use std::num::NonZeroUsize;
+
+use rstest::rstest;
+
+use crate::map_storage::{CachedStorage, MapStorage};
+use crate::storage_trait::{DbKey, DbValue, Storage};
+
+#[rstest]
+#[case::map_storage(MapStorage::new())]
+#[case::cached_storage(
+    CachedStorage::new(MapStorage::new(), NonZeroUsize::new(2).unwrap())
+)]
+fn test_storage_impl(#[case] mut storage: impl Storage) {
+    let (key_1, key_2, key_3) = (DbKey(vec![1_u8]), DbKey(vec![2_u8]), DbKey(vec![3_u8]));
+    let (val_1, val_2, val_3) = (DbValue(vec![1_u8]), DbValue(vec![2_u8]), DbValue(vec![3_u8]));
+
+    storage.set(key_1.clone(), val_1.clone()).unwrap();
+    // storage = {1: 1}
+    assert_eq!(storage.get(&key_1.clone()).unwrap(), Some(val_1.clone()));
+
+    storage.set(key_2.clone(), val_2.clone()).unwrap();
+    storage.delete(&key_1).unwrap();
+    // storage = {2: 2}
+    assert!(storage.get(&key_1.clone()).unwrap().is_none());
+    assert_eq!(storage.get(&key_2.clone()).unwrap(), Some(val_2.clone()));
+
+    storage
+        .mset(HashMap::from([(key_1.clone(), val_1.clone()), (key_3.clone(), val_3.clone())]))
+        .unwrap();
+    // storage = {1:1, 2: 2, 3:3}
+    assert_eq!(storage.get(&key_2.clone()).unwrap(), Some(val_2.clone()));
+    let expected_stored_values =
+        storage.mget(&[key_1.clone(), key_2.clone(), key_3.clone()]).unwrap();
+    assert_eq!(
+        expected_stored_values,
+        vec![Some(val_1.clone()), Some(val_2.clone()), Some(val_3.clone())]
+    );
+
+    storage.delete(&key_2).unwrap();
+    // storage = {1:1, 3:3}
+    assert!(storage.get(&key_2.clone()).unwrap().is_none());
+}
