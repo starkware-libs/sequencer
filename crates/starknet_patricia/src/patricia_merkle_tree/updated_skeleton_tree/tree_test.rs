67a9a62d98b3e448324778aad8067d0f10a198bb
Yoav Gross <yoav@starkware.co>
2025-09-03 10:07:30 +0300

    starknet_committer: wrapped map storage

--- crates/starknet_patricia/src/patricia_merkle_tree/updated_skeleton_tree/tree_test.rs
+++ crates/starknet_patricia/src/patricia_merkle_tree/updated_skeleton_tree/tree_test.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use rstest::{fixture, rstest};
+use starknet_patricia_storage::map_storage::MapStorage;
 use starknet_types_core::felt::Felt;
 
 use crate::hash::hash_trait::HashOutput;
@@ -148,7 +149,7 @@ fn test_updated_skeleton_tree_impl_create(
 #[case::empty_modifications(HashMap::new())]
 #[case::non_empty_modifications(HashMap::from([(NodeIndex::FIRST_LEAF + NodeIndex::from(7), MockLeaf::default())]))]
 fn test_updated_empty_tree(#[case] modifications: LeafModifications<MockLeaf>) {
-    let storage = HashMap::new();
+    let storage = MapStorage::default();
     let mut indices: Vec<NodeIndex> = modifications.keys().copied().collect();
     let mut original_skeleton = OriginalSkeletonTreeImpl::create(
         &storage,
