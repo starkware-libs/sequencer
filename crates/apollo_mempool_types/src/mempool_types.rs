f8df4bab37d89868f15de4d7849851e11079273e
Arni Hod <arnon@starkware.co>
2025-09-07 16:32:26 +0300

    apollo_gateway: remove uneccessary todos

--- crates/apollo_mempool_types/src/mempool_types.rs
+++ crates/apollo_mempool_types/src/mempool_types.rs
@@ -32,6 +32,13 @@ pub struct AddTransactionArgs {
     pub account_state: AccountState,
 }
 
+impl From<(InternalRpcTransaction, Nonce)> for AddTransactionArgs {
+    fn from((tx, nonce): (InternalRpcTransaction, Nonce)) -> Self {
+        let address = tx.contract_address();
+        Self { tx, account_state: AccountState { address, nonce } }
+    }
+}
+
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct CommitBlockArgs {
     pub address_to_nonce: HashMap<ContractAddress, Nonce>,
