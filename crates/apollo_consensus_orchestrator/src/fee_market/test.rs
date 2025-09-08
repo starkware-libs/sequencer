ce46fa4c85d9c01a09228328d33e588abec7b989
Ayelet Zilber <ayelet@starkware.co>
2025-09-08 17:09:52 +0300

    apollo_consensus_orchestrator: refactor fee market extreme test

--- crates/apollo_consensus_orchestrator/src/fee_market/test.rs
+++ crates/apollo_consensus_orchestrator/src/fee_market/test.rs
@@ -54,11 +54,9 @@ fn price_calculation_snapshot(
 }
 
 #[test]
-// This test ensures that the gas price calculation does not overflow with extreme values,
 fn test_gas_price_with_extreme_values() {
     let max_block_size = VERSIONED_CONSTANTS.max_block_size;
     let min_gas_price = VERSIONED_CONSTANTS.min_gas_price;
-    let gas_price_max_change_denominator = VERSIONED_CONSTANTS.gas_price_max_change_denominator;
 
     let price = min_gas_price;
     let gas_target = max_block_size / 2;
@@ -69,21 +67,17 @@ fn test_gas_price_with_extreme_values() {
     let gas_target = max_block_size / 2;
     let gas_used = max_block_size;
     assert!(calculate_next_base_gas_price(price, gas_used, gas_target) > min_gas_price);
+}
 
+#[rstest]
+#[case::extreme_price_zero_usage(GasAmount(0))]
+#[case::extreme_price_full_usage(VERSIONED_CONSTANTS.max_block_size)]
+fn price_does_not_overflow(#[case] gas_used: GasAmount) {
     let price = GasPrice(u128::from(u64::MAX));
-    let gas_target = max_block_size / 2;
-    let gas_used = GasAmount(0);
-    calculate_next_base_gas_price(price, gas_used, gas_target); // Should not panic.
+    let gas_target = VERSIONED_CONSTANTS.max_block_size / 2;
 
-    // To avoid overflow when updating the price, the value is set below a certain threshold so that
-    // the new price does not exceed u64::MAX.
-    let max_u128 = u128::from(u64::MAX);
-    let calculated_price = GasPrice(
-        max_u128 * gas_price_max_change_denominator / (gas_price_max_change_denominator + 1),
-    );
-    let gas_target = max_block_size / 2;
-    let gas_used = max_block_size;
-    calculate_next_base_gas_price(calculated_price, gas_used, gas_target); // Should not panic.
+    // Should not panic.
+    let _ = calculate_next_base_gas_price(price, gas_used, gas_target);
 }
 
 #[test]
