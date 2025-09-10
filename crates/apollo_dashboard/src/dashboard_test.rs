712991b797b585b6acfa510467a0beee9fd1ec42
Itay Tsabary <itayt@starkware.co>
2025-09-10 10:42:50 +0300

    apollo_dashboard: fix observer serde to be string

--- crates/apollo_dashboard/src/dashboard_test.rs
+++ crates/apollo_dashboard/src/dashboard_test.rs
@@ -49,7 +49,7 @@ fn serialize_alert() {
         "for": "5m",
         "intervalSec": 20,
         "severity": "p1",
-        "observer_applicable": true
+        "observer_applicable": "true"
     });
     assert_json_eq(&serialized, &expected, "Json Comparison failed".to_string());
 }
