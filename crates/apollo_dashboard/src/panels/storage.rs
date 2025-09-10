7ba18a8e798fb5a0b77ce47c7141e85b61a99b3d
guyf-starkware <guy.f@starkware.co>
2025-09-10 13:51:50 +0300

    apollo_dashboard: new dashboards for the number of storage readers.

--- crates/apollo_dashboard/src/panels/storage.rs
+++ crates/apollo_dashboard/src/panels/storage.rs
@@ -1,4 +1,10 @@
-use apollo_storage::metrics::{STORAGE_APPEND_THIN_STATE_DIFF_LATENCY, STORAGE_COMMIT_LATENCY};
+use apollo_storage::metrics::{
+    STORAGE_APPEND_THIN_STATE_DIFF_LATENCY,
+    STORAGE_COMMIT_LATENCY,
+    STORAGE_OPEN_BATCHER_READ_TRANSACTIONS,
+    STORAGE_OPEN_CLASS_MANAGER_READ_TRANSACTIONS,
+    STORAGE_OPEN_SYNC_READ_TRANSACTIONS,
+};
 
 use crate::dashboard::{Panel, Row};
 
@@ -8,10 +14,25 @@ fn get_storage_append_thin_state_diff_latency() -> Panel {
 fn get_storage_commit_latency() -> Panel {
     Panel::from(&STORAGE_COMMIT_LATENCY)
 }
+fn get_storage_open_sync_read_transactions() -> Panel {
+    Panel::from(&STORAGE_OPEN_SYNC_READ_TRANSACTIONS)
+}
+fn get_storage_open_batcher_read_transactions() -> Panel {
+    Panel::from(&STORAGE_OPEN_BATCHER_READ_TRANSACTIONS)
+}
+fn get_storage_open_class_manager_read_transactions() -> Panel {
+    Panel::from(&STORAGE_OPEN_CLASS_MANAGER_READ_TRANSACTIONS)
+}
 
 pub(crate) fn get_storage_row() -> Row {
     Row::new(
         "Storage",
-        vec![get_storage_append_thin_state_diff_latency(), get_storage_commit_latency()],
+        vec![
+            get_storage_append_thin_state_diff_latency(),
+            get_storage_commit_latency(),
+            get_storage_open_sync_read_transactions(),
+            get_storage_open_batcher_read_transactions(),
+            get_storage_open_class_manager_read_transactions(),
+        ],
     )
 }
