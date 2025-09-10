46306a3df89e5d2f80e352ecb347f98b46eaf023
nadin-Starkware <nadin@starkware.co>
2025-09-10 17:13:51 +0300

    apollo_l1_provider_config: move the l1 provider config to seperate crate

--- crates/apollo_integration_tests/src/utils.rs
+++ crates/apollo_integration_tests/src/utils.rs
@@ -32,7 +32,7 @@ use apollo_l1_gas_price::l1_gas_price_provider::L1GasPriceProviderConfig;
 use apollo_l1_gas_price::l1_gas_price_scraper::L1GasPriceScraperConfig;
 use apollo_l1_gas_price_types::DEFAULT_ETH_TO_FRI_RATE;
 use apollo_l1_provider::l1_scraper::L1ScraperConfig;
-use apollo_l1_provider::L1ProviderConfig;
+use apollo_l1_provider_config::config::L1ProviderConfig;
 use apollo_mempool::config::MempoolConfig;
 use apollo_mempool_p2p::config::MempoolP2pConfig;
 use apollo_monitoring_endpoint::config::MonitoringEndpointConfig;
