69ca15e8d39a3b9587c8fa81c5c393b70ec85f0b
nadin-Starkware <nadin@starkware.co>
2025-09-17 12:50:36 +0300

    apollo_config_manager: replace ConsensusDynamicConfig with NodeDynamicConfig in ConfigManager

--- crates/apollo_node_config/src/node_config.rs
+++ crates/apollo_node_config/src/node_config.rs
@@ -19,6 +19,7 @@ use apollo_config::loading::load_and_process_config;
 use apollo_config::validators::config_validate;
 use apollo_config::{ConfigError, ParamPath, SerializedParam};
 use apollo_config_manager_config::config::ConfigManagerConfig;
+use apollo_consensus_config::config::ConsensusDynamicConfig;
 use apollo_consensus_manager_config::config::ConsensusManagerConfig;
 use apollo_gateway_config::config::GatewayConfig;
 use apollo_http_server_config::config::HttpServerConfig;
@@ -283,6 +284,12 @@ impl Default for SequencerNodeConfig {
             state_sync_config: Some(StateSyncConfig::default()),
         }
     }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Validate, Default)]
+pub struct NodeDynamicConfig {
+    #[validate]
+    pub consensus_dynamic_config: ConsensusDynamicConfig,
 }
 
 macro_rules! validate_component_config_is_set_iff_running_locally {
