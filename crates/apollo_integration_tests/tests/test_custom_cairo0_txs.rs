edc21206f8c71b4ca9d9ef7251e2f3ffc76a1bb7
Itamar Shechter <itamar@starkware.co>
2025-09-18 15:00:35 +0300

    apollo_integration_tests: add tests for newly deployed contract

--- crates/apollo_integration_tests/tests/test_custom_cairo0_txs.rs
+++ crates/apollo_integration_tests/tests/test_custom_cairo0_txs.rs
@@ -7,17 +7,18 @@ use mempool_test_utils::starknet_api_test_utils::{
     AccountTransactionGenerator,
     MultiAccountTransactionGenerator,
 };
+use starknet_api::core::calculate_contract_address;
 use starknet_api::execution_resources::GasAmount;
-use starknet_api::felt;
 use starknet_api::rpc_transaction::RpcTransaction;
-use starknet_api::transaction::fields::{Tip, TransactionSignature};
+use starknet_api::transaction::fields::{ContractAddressSalt, Tip, TransactionSignature};
+use starknet_api::{calldata, felt};
 
 use crate::common::{end_to_end_flow, validate_tx_count, TestScenario};
 
 mod common;
 
 const DEFAULT_TIP: Tip = Tip(1_u64);
-const CUSTOM_CAIRO_0_INVOKE_TX_COUNT: usize = 5;
+const CUSTOM_CAIRO_0_INVOKE_TX_COUNT: usize = 9;
 
 #[tokio::test]
 async fn custom_cairo0_txs() {
@@ -48,7 +49,7 @@ fn create_custom_cairo0_test_txs(
     let mut txs = vec![];
     txs.extend(generate_direct_test_contract_invoke_txs_cairo_0_syscall(account_tx_generator));
     txs.extend(generate_invoke_txs_with_signature_cairo_0_syscall(account_tx_generator));
-    txs.extend(generate_deploy_contract_invoke_tx(account_tx_generator));
+    txs.extend(generate_invoke_txs_tests_for_deploy_contract(account_tx_generator));
 
     txs
 }
@@ -110,7 +111,8 @@ fn generate_invoke_txs_with_signature_cairo_0_syscall(
     ]
 }
 
-fn generate_deploy_contract_invoke_tx(
+// Deploy a contract and test with 4 more transactions the deployed contract functionality.
+fn generate_invoke_txs_tests_for_deploy_contract(
     account_tx_generator: &mut AccountTransactionGenerator,
 ) -> Vec<RpcTransaction> {
     let mut txs = vec![];
@@ -118,6 +120,8 @@ fn generate_deploy_contract_invoke_tx(
     let salt = felt!(34_u64);
     let constructor_calldata_arg1 = felt!(321_u64);
     let constructor_calldata_arg2 = felt!(543_u64);
+
+    // deploy_contract - deploy the test contract.
     let deploy_calldata = vec![
         test_contract.get_class_hash().0, // class hash for test contract
         salt,                             // salt
@@ -134,5 +138,85 @@ fn generate_deploy_contract_invoke_tx(
             .calldata(calldata)
             .build_rpc_invoke_tx(),
     );
+
+    // Get the contract address of the newly deployed contract from deploy_contract.
+    let newly_deployed_contract_address = calculate_contract_address(
+        ContractAddressSalt(salt),
+        test_contract.get_class_hash(),
+        &calldata!(constructor_calldata_arg1, constructor_calldata_arg2),
+        account_tx_generator.sender_address(),
+    )
+    .expect("Failed to calculate contract address");
+
+    // 1. test_call_storage_consistency - Change storage of newly deployed contract via
+    //    test_contract and verify it.
+    let test_call_storage_consistency_args =
+        vec![*newly_deployed_contract_address.0.key(), felt!(5_u64)];
+    let test_call_storage_consistency_calldata = create_calldata(
+        test_contract.get_instance_address(0),
+        "test_call_storage_consistency",
+        &test_call_storage_consistency_args,
+    );
+    txs.push(
+        account_tx_generator
+            .invoke_tx_builder()
+            .tip(DEFAULT_TIP)
+            .calldata(test_call_storage_consistency_calldata)
+            .build_rpc_invoke_tx(),
+    );
+
+    // 2. test_re_entrance - check calculations based on the storage of the newly deployed contract.
+    let test_re_entrance_args = vec![*newly_deployed_contract_address.0.key(), felt!(2_u64)];
+    let test_re_entrance_calldata = create_calldata(
+        test_contract.get_instance_address(0),
+        "test_re_entrance",
+        &test_re_entrance_args,
+    );
+    txs.push(
+        account_tx_generator
+            .invoke_tx_builder()
+            .tip(DEFAULT_TIP)
+            .calldata(test_re_entrance_calldata)
+            .build_rpc_invoke_tx(),
+    );
+
+    // 3. test_deploy_and_call - This function deploys test_contract.cairo and calls it.
+    let deploy_and_call_args = vec![
+        test_contract.get_class_hash().0, // class hash
+        salt,                             // salt
+        felt!(0_u64),                     // deploy_from_zero flag is down.
+        felt!(2_u64),                     // constructor calldata length
+        constructor_calldata_arg1,        // constructor arg1
+        constructor_calldata_arg2,        // constructor arg2
+        felt!(167_u64),                   // key for set_value
+        felt!(160_u64),                   // value for set_value
+    ];
+    let deploy_and_call_calldata = create_calldata(
+        test_contract.get_instance_address(0),
+        "test_deploy_and_call",
+        &deploy_and_call_args,
+    );
+    txs.push(
+        account_tx_generator
+            .invoke_tx_builder()
+            .tip(DEFAULT_TIP)
+            .calldata(deploy_and_call_calldata)
+            .build_rpc_invoke_tx(),
+    );
+
+    // 4. set_value - Set value in storage by key via test_library_call of the deployed contract.
+    let set_value_args = vec![
+        felt!(2_u64),    // arguments length
+        felt!(8_u64),    // key
+        felt!(2023_u64), // value
+    ];
+    txs.push(account_tx_generator.generate_library_call_invoke_tx(
+        DEFAULT_TIP,
+        &test_contract,
+        &test_contract,
+        "set_value",
+        &set_value_args,
+    ));
+
     txs
 }
