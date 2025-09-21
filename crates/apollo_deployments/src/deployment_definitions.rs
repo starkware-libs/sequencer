819d36dc4d26dba54c78dc68e8411781224227ac
Itay Tsabary <itayt@starkware.co>
2025-09-21 11:05:45 +0300

    apollo_deployments: add potc mock params

--- crates/apollo_deployments/src/deployment_definitions.rs
+++ crates/apollo_deployments/src/deployment_definitions.rs
@@ -41,7 +41,7 @@ const SIERRA_COMPILER_PORT: u16 = 55007;
 const STATE_SYNC_PORT: u16 = 55009;
 
 pub const DEPLOYMENTS: &[DeploymentFn] = &[
-    || load_and_create_hybrid_deployments(POTC2_DEPLOYMENT_INPUTS_PATH),
+    || load_and_create_hybrid_deployments(POTC_MOCK_DEPLOYMENT_INPUTS_PATH),
     || load_and_create_hybrid_deployments(MAINNET_DEPLOYMENT_INPUTS_PATH),
     || load_and_create_hybrid_deployments(INTEGRATION_DEPLOYMENT_INPUTS_PATH),
     || load_and_create_hybrid_deployments(TESTNET_DEPLOYMENT_INPUTS_PATH),
@@ -55,8 +55,8 @@ pub(crate) const DEPLOYMENT_CONFIG_DIR_NAME: &str = "deployments/";
 
 const BASE_APP_CONFIGS_DIR_PATH: &str = "crates/apollo_deployments/resources/app_configs";
 
-const POTC2_DEPLOYMENT_INPUTS_PATH: &str =
-    "crates/apollo_deployments/resources/deployment_inputs/potc2_sepolia.json";
+const POTC_MOCK_DEPLOYMENT_INPUTS_PATH: &str =
+    "crates/apollo_deployments/resources/deployment_inputs/potc_mock.json";
 const MAINNET_DEPLOYMENT_INPUTS_PATH: &str =
     "crates/apollo_deployments/resources/deployment_inputs/mainnet.json";
 const INTEGRATION_DEPLOYMENT_INPUTS_PATH: &str =
@@ -129,7 +129,7 @@ impl Display for Environment {
 #[derive(EnumString, Clone, Display, PartialEq, Debug, Serialize, Deserialize)]
 #[strum(serialize_all = "snake_case")]
 pub enum CloudK8sEnvironment {
-    Potc2,
+    PotcMock,
     Mainnet,
     SepoliaIntegration,
     SepoliaTestnet,
