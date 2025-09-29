d7d84ce3803db3d6841a95a38cedd36e72803c27
Andrew Luka <andrew.l@starkware.co>
2025-09-29 16:23:24 +0300

    apollo_network: broadcast network stress test draft

---
+++ crates/apollo_network_benchmark/bin/broadcast_network_stress_test_node/args.rs
@@ -0,0 +1,125 @@
+use std::fmt::Display;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Clone, ValueEnum, PartialEq, Eq)]
+pub enum Mode {
+    /// All nodes broadcast messages
+    #[value(name = "all")]
+    AllBroadcast,
+    /// Only the node specified by --broadcaster broadcasts messages
+    #[value(name = "one")]
+    OneBroadcast,
+    /// Nodes take turns broadcasting in round-robin fashion
+    #[value(name = "rr")]
+    RoundRobin,
+    /// Only the node specified by --broadcaster broadcasts messages,
+    /// Every explore_run_duration_seconds + explore_cool_down_duration_seconds seconds
+    /// a new combination of MPS and message size is explored.
+    /// Increases the throughput with each new trial.
+    /// Configurations are filtered by minimum throughput and minimum message size.
+    #[value(name = "explore")]
+    Explore,
+}
+
+#[derive(Debug, Clone, ValueEnum, PartialEq, Eq)]
+pub enum NetworkProtocol {
+    /// Use gossipsub for broadcasting (default)
+    #[value(name = "gossipsub")]
+    Gossipsub,
+    /// Use SQMR for point-to-point communication
+    #[value(name = "sqmr")]
+    Sqmr,
+    /// Use Reversed SQMR where receivers initiate requests to broadcasters
+    #[value(name = "reversed-sqmr")]
+    ReveresedSqmr,
+}
+
+impl Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_possible_value().unwrap().get_name())
+    }
+}
+
+impl Display for NetworkProtocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_possible_value().unwrap().get_name())
+    }
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// ID for Prometheus logging
+    #[arg(short, long, env)]
+    pub id: u64,
+
+    /// Total number of nodes in the network
+    #[arg(long, env)]
+    pub num_nodes: u64,
+
+    /// The port to run the Prometheus metrics server on
+    #[arg(long, env)]
+    pub metric_port: u16,
+
+    /// The port to run the P2P network on
+    #[arg(short, env, long)]
+    pub p2p_port: u16,
+
+    /// The addresses of the bootstrap peers (can specify multiple)
+    #[arg(long, env, value_delimiter = ',')]
+    pub bootstrap: Vec<String>,
+
+    /// Set the verbosity level of the logger, the higher the more verbose
+    #[arg(short, long, env)]
+    pub verbosity: u8,
+
+    /// Buffer size for the broadcast topic
+    #[arg(long, env)]
+    pub buffer_size: usize,
+
+    /// The mode to use for the stress test.
+    #[arg(long, env)]
+    pub mode: Mode,
+
+    /// The network protocol to use for communication (default: gossipsub)
+    #[arg(long, env)]
+    pub network_protocol: NetworkProtocol,
+
+    /// Which node ID should do the broadcasting - for OneBroadcast and Explore modes
+    #[arg(long, env, required_if_eq_any([("mode", "one"), ("mode", "explore")]))]
+    pub broadcaster: Option<u64>,
+
+    /// Duration each node broadcasts before switching (in seconds) - for RoundRobin mode
+    #[arg(long, env, required_if_eq("mode", "rr"))]
+    pub round_duration_seconds: Option<u64>,
+
+    /// Size of StressTestMessage
+    #[arg(long, env, required_if_eq_any([("mode", "one"), ("mode", "all"), ("mode", "rr")]))]
+    pub message_size_bytes: Option<usize>,
+
+    /// The time to sleep between broadcasts of StressTestMessage in milliseconds
+    #[arg(long, env, required_if_eq_any([("mode", "one"), ("mode", "all"), ("mode", "rr")]))]
+    pub heartbeat_millis: Option<u64>,
+
+    /// Cool down duration between configuration changes in seconds - for Explore mode
+    #[arg(long, env, required_if_eq("mode", "explore"))]
+    pub explore_cool_down_duration_seconds: Option<u64>,
+
+    /// Duration to run each configuration in seconds - for Explore mode
+    #[arg(long, env, required_if_eq("mode", "explore"))]
+    pub explore_run_duration_seconds: Option<u64>,
+
+    /// Minimum throughput in bytes per second - for Explore mode
+    #[arg(long, env, required_if_eq("mode", "explore"))]
+    pub explore_min_throughput_byte_per_seconds: Option<f64>,
+
+    /// Minimum message size in bytes - for Explore mode
+    #[arg(long, env, required_if_eq("mode", "explore"))]
+    pub explore_min_message_size_bytes: Option<usize>,
+
+    /// The timeout in seconds for the node.
+    /// When the node runs for longer than this, it will be killed.
+    #[arg(long, env)]
+    pub timeout: u64,
+}
---
+++ crates/apollo_network_benchmark/bin/broadcast_network_stress_test_node/utils.rs
@@ -0,0 +1,106 @@
+use std::collections::{BTreeMap, HashSet};
+use std::net::Ipv4Addr;
+use std::vec;
+
+use apollo_config::dumping::{prepend_sub_config_name, ser_param, SerializeConfig};
+use apollo_config::{ParamPath, ParamPrivacyInput, SerializedParam};
+use apollo_network::utils::make_multiaddr;
+use apollo_network::NetworkConfig;
+use libp2p::identity::Keypair;
+use serde::{Deserialize, Serialize};
+
+pub const BOOTSTRAP_CONFIG_FILE_PATH: &str =
+    "crates/apollo_network/src/bin/network_stress_test/bootstrap_test_config.json";
+pub const BOOTSTRAP_OUTPUT_FILE_PATH: &str =
+    "crates/apollo_network/src/bin/network_stress_test/bootstrap_output.csv";
+pub const DEFAULT_CONFIG_FILE_PATH: &str =
+    "crates/apollo_network/src/bin/network_stress_test/test_config.json";
+pub const DEFAULT_OUTPUT_FILE_PATH: &str =
+    "crates/apollo_network/src/bin/network_stress_test/output.csv";
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TestConfig {
+    pub network_config: NetworkConfig,
+    pub buffer_size: usize,
+    pub message_size: usize,
+    pub num_messages: u32,
+    pub output_path: String,
+}
+
+impl SerializeConfig for TestConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        let mut config = BTreeMap::from_iter([
+            ser_param(
+                "buffer_size",
+                &self.buffer_size,
+                "The buffer size for the network receiver.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "message_size",
+                &self.message_size,
+                "The size of the payload for the test messages.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "num_messages",
+                &self.num_messages,
+                "The amount of messages to send and receive.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "output_path",
+                &self.output_path,
+                "The path of the output file.",
+                ParamPrivacyInput::Public,
+            ),
+        ]);
+        config.extend(prepend_sub_config_name(self.network_config.dump(), "network_config"));
+        config
+    }
+}
+
+impl Default for TestConfig {
+    fn default() -> Self {
+        Self {
+            network_config: NetworkConfig::default(),
+            buffer_size: 1000,
+            message_size: 1000,
+            num_messages: 10000,
+            output_path: BOOTSTRAP_OUTPUT_FILE_PATH.to_string(),
+        }
+    }
+}
+
+impl TestConfig {
+    #[allow(dead_code)]
+    pub fn create_config_files() {
+        let secret_key = vec![0; 32];
+        let keypair = Keypair::ed25519_from_bytes(secret_key.clone()).unwrap();
+        let peer_id = keypair.public().to_peer_id();
+
+        let _ = TestConfig {
+            network_config: NetworkConfig {
+                port: 10000,
+                secret_key: Some(secret_key),
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+        .dump_to_file(&vec![], &HashSet::new(), BOOTSTRAP_CONFIG_FILE_PATH);
+        let _ = TestConfig {
+            network_config: NetworkConfig {
+                port: 10002,
+                bootstrap_peer_multiaddr: Some(vec![make_multiaddr(
+                    Ipv4Addr::LOCALHOST,
+                    10000,
+                    Some(peer_id),
+                )]),
+                ..Default::default()
+            },
+            output_path: DEFAULT_OUTPUT_FILE_PATH.to_string(),
+            ..Default::default()
+        }
+        .dump_to_file(&vec![], &HashSet::new(), DEFAULT_CONFIG_FILE_PATH);
+    }
+}
