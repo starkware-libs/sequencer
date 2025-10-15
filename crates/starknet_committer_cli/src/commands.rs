dc173d3511e1411e73fa8adea8d7ccb2d180a240
Tzahi Taub <tzahi@starkware.co>
2025-10-15 15:32:24 +0300

    starknet_committer,starknet_committer_cli: add checkpointing to the benchmark

--- crates/starknet_committer_cli/src/commands.rs
+++ crates/starknet_committer_cli/src/commands.rs
@@ -13,16 +13,29 @@ pub type InputImpl = Input<ConfigImpl>;
 /// Runs the committer on n_iterations random generated blocks.
 /// Prints the time measurement to the console and saves statistics to a CSV file in the given
 /// output directory.
-pub async fn run_storage_benchmark(seed: u64, n_iterations: usize, output_dir: &str) {
-    let mut rng = SmallRng::seed_from_u64(seed);
-    let mut time_measurement = TimeMeasurement::new(n_iterations);
+pub async fn run_storage_benchmark(
+    seed: u64,
+    n_iterations: usize,
+    output_dir: &str,
+    checkpoint_dir: Option<&str>,
+    checkpoint_interval: usize,
+) {
+    let mut time_measurement = TimeMeasurement::new(checkpoint_interval);
+    let mut contracts_trie_root_hash = match checkpoint_dir {
+        Some(checkpoint_dir) => {
+            time_measurement.try_load_from_checkpoint(checkpoint_dir).unwrap_or_default()
+        }
+        None => HashOutput::default(),
+    };
+    let curr_block_number = time_measurement.block_number;
 
     let mut storage = MapStorage::default();
-    let mut contracts_trie_root_hash = HashOutput::default();
     let mut classes_trie_root_hash = HashOutput::default();
 
-    for i in 0..n_iterations {
+    for i in curr_block_number..n_iterations {
         info!("Committer storage benchmark iteration {}/{}", i + 1, n_iterations);
+        // Seed is created from block number, to be independent of restarts using checkpoints.
+        let mut rng = SmallRng::seed_from_u64(seed + u64::try_from(i).unwrap());
         let input = InputImpl {
             state_diff: generate_random_state_diff(&mut rng),
             contracts_trie_root_hash,
@@ -40,10 +53,21 @@ pub async fn run_storage_benchmark(seed: u64, n_iterations: usize, output_dir: &
 
         time_measurement.stop_measurement(Some(n_new_facts), Action::EndToEnd);
 
+        // Export to csv in the checkpoint interval.
+        if (i + 1) % checkpoint_interval == 0 {
+            time_measurement.to_csv(&format!("{}.csv", i + 1), output_dir);
+            if let Some(checkpoint_dir) = checkpoint_dir {
+                time_measurement.save_checkpoint(checkpoint_dir, i + 1, &contracts_trie_root_hash)
+            }
+        }
         contracts_trie_root_hash = filled_forest.get_contract_root_hash();
         classes_trie_root_hash = filled_forest.get_compiled_class_root_hash();
     }
 
+    // Export to csv in the last iteration.
+    if n_iterations % checkpoint_interval != 0 {
+        time_measurement.to_csv(&format!("{n_iterations}.csv"), output_dir);
+    }
+
     time_measurement.pretty_print(50);
-    time_measurement.to_csv(&format!("{n_iterations}.csv"), output_dir);
 }
