b26ed9156f12aaccfa56042e8191ec89dda5567d
Yonatan Iluz <yonatan@starkware.co>
2025-10-19 22:52:23 +0300

    starknet_committer_and_os_cli: support cell blobs

--- crates/starknet_committer_and_os_cli/src/kzg_cli/run_kzg_cli.rs
+++ crates/starknet_committer_and_os_cli/src/kzg_cli/run_kzg_cli.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, Subcommand};
-use starknet_os::hints::hint_implementation::kzg::utils::compute_blob_commitments;
+use starknet_os::hints::hint_implementation::kzg::utils::{
+    compute_blob_commitments,
+    compute_legacy_blob_commitments,
+};
 use tracing::info;
 
 use crate::shared_utils::read::{load_input, write_to_file};
@@ -17,6 +20,10 @@ enum Command {
         #[clap(flatten)]
         io_args: IoArgs,
     },
+    ComputeLegacyBlobCommitments {
+        #[clap(flatten)]
+        io_args: IoArgs,
+    },
 }
 
 pub fn run_kzg_cli(kzg_command: KzgCliCommand) {
@@ -26,6 +33,14 @@ pub fn run_kzg_cli(kzg_command: KzgCliCommand) {
             let raw_blobs: Vec<Vec<u8>> = load_input(input_path);
             let blobs = compute_blob_commitments(raw_blobs)
                 .unwrap_or_else(|error| panic!("Failed to calculate blob commitments: {error}"));
+            write_to_file(&output_path, &blobs);
+        }
+        // TODO(Yoni): remove this command once python migrates to the new blob commitments.
+        Command::ComputeLegacyBlobCommitments { io_args: IoArgs { input_path, output_path } } => {
+            let raw_blobs: Vec<Vec<u8>> = load_input(input_path);
+            let blobs = compute_legacy_blob_commitments(raw_blobs).unwrap_or_else(|error| {
+                panic!("Failed to calculate legacy blob commitments: {error}")
+            });
             write_to_file(&output_path, &blobs);
         }
     };
