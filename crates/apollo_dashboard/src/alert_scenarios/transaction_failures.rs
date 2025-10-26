47051697a013f060211289b3eb24786cdffb8721
Itay Tsabary <itayt@starkware.co>
2025-10-26 18:43:03 +0200

    apollo_dashboard: update http server high transaction failure ratio to 0.5

--- crates/apollo_dashboard/src/alert_scenarios/transaction_failures.rs
+++ crates/apollo_dashboard/src/alert_scenarios/transaction_failures.rs
@@ -57,7 +57,7 @@ pub(crate) fn get_http_server_high_transaction_failure_ratio() -> Alert {
         ),
         vec![AlertCondition {
             comparison_op: AlertComparisonOp::GreaterThan,
-            comparison_value: 0.3,
+            comparison_value: 0.5,
             logical_op: AlertLogicalOp::And,
         }],
         PENDING_DURATION_DEFAULT,
