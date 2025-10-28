f0e8048cb302528b99e4c5c33378755589a5f4e1
Nimrod Weiss <nimrod@starkware.co>
2025-10-28 16:46:28 +0200

    starknet_api: txs signature for BH

--- crates/starknet_api/src/consensus_transaction.rs
+++ crates/starknet_api/src/consensus_transaction.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::rpc_transaction::{InternalRpcTransaction, RpcTransaction};
+use crate::transaction::fields::TransactionSignature;
 use crate::transaction::TransactionHash;
 use crate::{executable_transaction, transaction};
 
@@ -21,6 +22,15 @@ impl InternalConsensusTransaction {
         match self {
             Self::RpcTransaction(tx) => tx.tx_hash,
             Self::L1Handler(tx) => tx.tx_hash,
+        }
+    }
+
+    pub fn tx_signature_for_block_hash(&self) -> TransactionSignature {
+        match self {
+            Self::RpcTransaction(tx) => tx.signature(),
+            // L1Handler transactions do not have a signature in their structure, but for block hash
+            // calculation the signature is considered to be empty.
+            Self::L1Handler(_) => TransactionSignature::default(),
         }
     }
 }
