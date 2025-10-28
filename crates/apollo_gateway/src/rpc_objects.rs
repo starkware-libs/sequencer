196d1696cc30bdb6b5262e3238f30afad3e97157
Nimrod Weiss <nimrod@starkware.co>
2025-10-28 16:09:39 +0200

    starknet_api: l1_da_mode comfortable conversions to use_kzg_da

--- crates/apollo_gateway/src/rpc_objects.rs
+++ crates/apollo_gateway/src/rpc_objects.rs
@@ -96,7 +96,7 @@ impl TryInto<BlockInfo> for BlockHeader {
                 parse_gas_price(self.l2_gas_price.price_in_wei)?,
                 parse_gas_price(self.l2_gas_price.price_in_fri)?,
             ),
-            use_kzg_da: matches!(self.l1_da_mode, L1DataAvailabilityMode::Blob),
+            use_kzg_da: self.l1_da_mode.to_use_kzg_da(),
         })
     }
 }
