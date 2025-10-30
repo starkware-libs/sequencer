93f35573cbb86937a5695825df984a03f90ca9b7
Dori Medini <dori@starkware.co>
2025-10-30 16:07:39 +0200

    starknet_committer_cli: couple the key size arg with the valid values

--- crates/starknet_patricia_storage/src/short_key_storage.rs
+++ crates/starknet_patricia_storage/src/short_key_storage.rs
@@ -1,24 +1,5 @@
 use std::marker::PhantomData;
 
-use blake2::digest::consts::{
-    U16,
-    U17,
-    U18,
-    U19,
-    U20,
-    U21,
-    U22,
-    U23,
-    U24,
-    U25,
-    U26,
-    U27,
-    U28,
-    U29,
-    U30,
-    U31,
-    U32,
-};
 use blake2::Blake2s;
 use digest::Digest;
 
@@ -26,17 +7,22 @@ use crate::storage_trait::{DbHashMap, DbKey, DbValue, PatriciaStorageResult, Sto
 
 #[macro_export]
 macro_rules! define_short_key_storage {
-    ($( ( $sizes:ty, $names:ident ) ),+ $(,)?) => {
+    ($( ( $sizes:ident, $names:ident ) ),+ $(,)?) => {
         $(
             $crate::define_short_key_storage!($sizes, $names);
         )+
+
+        /// Utility enum to define the possible sizes of the short key storage.
+        pub enum ShortKeySize {
+            $($sizes,)+
+        }
     };
 
-    ($size:ty, $name:ident) => {
+    ($size:ident, $name:ident) => {
         /// A storage that hashes (using blake) each key to a $size - byte key.
         pub struct $name<S: Storage> {
             storage: S,
-            _n_bytes: PhantomData<$size>,
+            _n_bytes: PhantomData<blake2::digest::consts::$size>,
         }
 
         impl<S: Storage> $name<S> {
@@ -45,7 +31,7 @@ macro_rules! define_short_key_storage {
             }
 
             pub fn small_key(key: &DbKey) -> DbKey {
-                let mut hasher = Blake2s::<$size>::new();
+                let mut hasher = Blake2s::<blake2::digest::consts::$size>::new();
                 hasher.update(key.0.as_slice());
                 let result = hasher.finalize();
                 DbKey(result.as_slice().to_vec())
