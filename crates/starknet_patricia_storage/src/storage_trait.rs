d4b66e3b4b91ded46c70fb21e0c49e00cf082a6b
rotem-starkware <rotem@starkware.co>
2025-10-31 08:43:12 +0200

    starknet_committer_cli,starknet_patricia_storage: add stats to MdbxStorage (#9847)

--- crates/starknet_patricia_storage/src/storage_trait.rs
+++ crates/starknet_patricia_storage/src/storage_trait.rs
@@ -40,6 +40,11 @@ pub trait Storage {
 
     /// Deletes value from storage and returns its value if it exists. Returns None if not.
     fn delete(&mut self, key: &DbKey) -> PatriciaStorageResult<Option<DbValue>>;
+
+    /// If implemented, returns the statistics of the storage.
+    fn get_stats(&self) -> Option<String> {
+        None
+    }
 }
 
 #[derive(Debug)]
