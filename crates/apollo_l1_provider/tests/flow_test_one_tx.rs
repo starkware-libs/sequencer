d77bea3d58ed88bed51ea59b89bab82add550a80
Guy Nir <guy.n@starkware.co>
2025-11-02 17:29:33 +0200

    apollo_l1_provider: add flow test for cancellation

---
+++ crates/apollo_l1_provider/tests/flow_test_one_tx.rs
@@ -0,0 +1,49 @@
+mod common;
+use std::time::Duration;
+
+use apollo_l1_provider_types::{L1ProviderClient, SessionState, ValidationStatus};
+use common::{
+    send_message_from_l1_to_l2,
+    setup_anvil_base_layer,
+    setup_scraper_and_provider,
+    COOLDOWN_MILLIS,
+    TARGET_L2_HEIGHT,
+};
+use starknet_api::block::BlockNumber;
+
+#[tokio::test]
+async fn new_l1_handler_tx_propose_validate_cooldown() {
+    // Setup.
+
+    // Setup the base layer.
+    let base_layer = setup_anvil_base_layer().await;
+
+    let (l2_hash, _nonce) = send_message_from_l1_to_l2(&base_layer).await;
+
+    let l1_provider_client = setup_scraper_and_provider(&base_layer).await;
+
+    // Test.
+    let next_block_height = BlockNumber(TARGET_L2_HEIGHT.0 + 1);
+
+    // Check that we can validate this message even though no time has passed.
+    l1_provider_client.start_block(SessionState::Validate, next_block_height).await.unwrap();
+    assert_eq!(
+        l1_provider_client.validate(l2_hash, next_block_height).await.unwrap(),
+        ValidationStatus::Validated
+    );
+
+    // Test that we do not propose anything before the cooldown is over.
+    l1_provider_client.start_block(SessionState::Propose, next_block_height).await.unwrap();
+    let n_txs = 1;
+    let txs = l1_provider_client.get_txs(n_txs, next_block_height).await.unwrap();
+    assert!(txs.is_empty());
+
+    // Sleep at least two times the cooldown to make sure we are not failing due to fractional
+    // seconds.
+    tokio::time::sleep(Duration::from_millis(COOLDOWN_MILLIS * 2)).await;
+
+    // Test that we propose after the cooldown is over.
+    l1_provider_client.start_block(SessionState::Propose, next_block_height).await.unwrap();
+    let txs = l1_provider_client.get_txs(n_txs, next_block_height).await.unwrap();
+    assert!(!txs.is_empty());
+}
