d1812604e2c609cfc91012a3536f66b927a2f605
Yoav Gross <yoav@starkware.co>
2025-11-06 12:57:08 +0200

    committer: add apollo_committer_types crate

--- crates/apollo_committer_types/src/lib.rs
+++ crates/apollo_committer_types/src/lib.rs
@@ -2,11 +2,18 @@ pub mod errors;
 
 use std::sync::Arc;
 
+use apollo_infra::component_client::{LocalComponentClient, RemoteComponentClient};
+use apollo_infra::component_definitions::{PrioritizedRequest, RequestWrapper};
+use apollo_infra::requests::LABEL_NAME_REQUEST_VARIANT;
+use apollo_infra::{impl_debug_for_infra_requests_and_responses, impl_labeled_request};
+use apollo_metrics::generate_permutation_labels;
 use async_trait::async_trait;
-use errors::CommitterClientResult;
+use errors::{CommitterClientResult, CommitterResult};
 use serde::{Deserialize, Serialize};
 use starknet_committer::block_committer::input::StateDiff;
 use starknet_patricia::hash::hash_trait::HashOutput;
+use strum::{EnumVariantNames, VariantNames};
+use strum_macros::{AsRefStr, EnumDiscriminants, EnumIter, IntoStaticStr};
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct StateRoots {
@@ -26,3 +33,33 @@ pub trait CommitterClient: Send + Sync {
 }
 
 pub type SharedCommitterClient = Arc<dyn CommitterClient>;
+
+#[derive(Serialize, Deserialize, Clone, AsRefStr, EnumDiscriminants)]
+#[strum_discriminants(
+    name(CommitterRequestLabelValue),
+    derive(IntoStaticStr, EnumIter, EnumVariantNames),
+    strum(serialize_all = "snake_case")
+)]
+pub enum CommitterRequest {
+    CommitBlock { state_diff: StateDiff, prev_state_roots: StateRoots },
+}
+
+impl_debug_for_infra_requests_and_responses!(CommitterRequest);
+impl_labeled_request!(CommitterRequest, CommitterRequestLabelValue);
+impl PrioritizedRequest for CommitterRequest {}
+
+#[derive(Clone, Serialize, Deserialize, AsRefStr)]
+pub enum CommitterResponse {
+    CommitBlock(CommitterResult<StateRoots>),
+}
+
+impl_debug_for_infra_requests_and_responses!(CommitterResponse);
+
+pub type LocalCommitterClient = LocalComponentClient<CommitterRequest, CommitterResponse>;
+pub type RemoteCommitterClient = RemoteComponentClient<CommitterRequest, CommitterResponse>;
+pub type CommitterRequestWrapper = RequestWrapper<CommitterRequest, CommitterResponse>;
+
+generate_permutation_labels! {
+    COMMITTER_REQUEST_LABELS,
+    (LABEL_NAME_REQUEST_VARIANT, CommitterRequestLabelValue),
+}
