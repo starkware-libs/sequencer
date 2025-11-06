bb86f31af35485bbb56bcbaa00c38386d98a4913
Yoav Gross <yoav@starkware.co>
2025-11-06 12:57:08 +0200

    committer: add apollo_committer_types crate

--- crates/apollo_committer_types/src/lib.rs
+++ crates/apollo_committer_types/src/lib.rs
@@ -1,1 +1,23 @@
 pub mod errors;
+
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use errors::CommitterClientResult;
+use serde::{Deserialize, Serialize};
+use starknet_committer::block_committer::input::StateDiff;
+use starknet_committer::hash_function::hash::StateRoots;
+use starknet_patricia::hash::hash_trait::HashOutput;
+
+#[async_trait]
+#[cfg_attr(any(feature = "testing", test), mockall::automock)]
+pub trait CommitterClient: Send + Sync {
+    /// Applies the state diff on the state trees and computes the new state roots.
+    async fn commit_block(
+        &self,
+        state_diff: StateDiff,
+        prev_state_roots: StateRoots,
+    ) -> CommitterClientResult<StateRoots>;
+}
+
+pub type SharedCommitterClient = Arc<dyn CommitterClient>;
