465741478fe8b73e3241021ef6c18fc89eb3ca3d
Yoav Gross <yoav@starkware.co>
2025-11-06 15:09:50 +0200

    committer: add apollo_committer crate

---
+++ crates/apollo_committer/src/communication.rs
@@ -0,0 +1,22 @@
+use apollo_committer_types::communication::{CommitterRequest, CommitterResponse};
+use apollo_infra::component_definitions::ComponentRequestHandler;
+use apollo_infra::component_server::{ConcurrentLocalComponentServer, RemoteComponentServer};
+use async_trait::async_trait;
+
+use crate::Committer;
+
+pub type LocalCommitterServer =
+    ConcurrentLocalComponentServer<Committer, CommitterRequest, CommitterResponse>;
+pub type RemoteCommitterServer = RemoteComponentServer<CommitterRequest, CommitterResponse>;
+
+#[async_trait]
+impl ComponentRequestHandler<CommitterRequest, CommitterResponse> for Committer {
+    async fn handle_request(&mut self, request: CommitterRequest) -> CommitterResponse {
+        match request {
+            CommitterRequest::CommitBlock(_) => {
+                // TODO(Yoav): Call the committer.
+                unimplemented!()
+            }
+        }
+    }
+}
