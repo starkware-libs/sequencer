bd540e1c11e4d46ee4661e0ecbd30b2974c2f302
Itamar Shechter <itamar@starkware.co>
2025-11-11 14:41:30 +0200

    apollo_gateway: move get_state_reader_from_latest_block to be async

--- crates/apollo_gateway/src/gateway.rs
+++ crates/apollo_gateway/src/gateway.rs
@@ -260,7 +260,7 @@ impl ProcessTxBlockingTask {
     fn process_tx(self) -> GatewayResult<Nonce> {
         let mut stateful_transaction_validator = self
             .stateful_tx_validator_factory
-            .instantiate_validator(self.state_reader_factory.as_ref())?;
+            .instantiate_validator(self.state_reader_factory.as_ref(), self.runtime.clone())?;
 
         let nonce = stateful_transaction_validator.extract_state_nonce_and_run_validations(
             &self.executable_tx,
