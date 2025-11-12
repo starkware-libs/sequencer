edb8af8a58c56b51d213474001d60b77a2a93976
nimrod-starkware <143319383+nimrod-starkware@users.noreply.github.com>
2025-11-12 10:15:49 +0200

    blockifier: get tx output for hashing from tx execution info (#9833)

--- crates/blockifier/src/transaction/objects.rs
+++ crates/blockifier/src/transaction/objects.rs
@@ -1,6 +1,7 @@
 use cairo_vm::types::builtin_name::BuiltinName;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use starknet_api::block::{BlockInfo, FeeType};
+use starknet_api::block_hash::block_hash_calculator::TransactionOutputForHash;
 use starknet_api::core::{ContractAddress, Nonce};
 use starknet_api::data_availability::DataAvailabilityMode;
 use starknet_api::execution_resources::GasVector;
@@ -16,6 +17,8 @@ use starknet_api::transaction::fields::{
 };
 use starknet_api::transaction::{
     signed_tx_version,
+    RevertedTransactionExecutionStatus,
+    TransactionExecutionStatus,
     TransactionHash,
     TransactionOptions,
     TransactionVersion,
@@ -262,7 +265,34 @@ impl TransactionExecutionInfo {
         }
         builtin_counters
     }
+
+    /// Information needed to compute the block hash of the block that the transaction is part of.
+    pub fn output_for_hashing(&self) -> TransactionOutputForHash {
+        let execution_status = self
+            .revert_error
+            .as_ref()
+            .map(|err| {
+                TransactionExecutionStatus::Reverted(RevertedTransactionExecutionStatus {
+                    revert_reason: err.to_string(),
+                })
+            })
+            .unwrap_or(TransactionExecutionStatus::Succeeded);
+        TransactionOutputForHash {
+            actual_fee: self.receipt.fee,
+            execution_status,
+            gas_consumed: self.receipt.gas,
+            events: self
+                .non_optional_call_infos()
+                .flat_map(|call_info| call_info.get_sorted_events())
+                .collect(),
+            messages_sent: self
+                .non_optional_call_infos()
+                .flat_map(|call_info| call_info.get_sorted_l2_to_l1_messages())
+                .collect(),
+        }
+    }
 }
+
 pub trait ExecutionResourcesTraits {
     fn total_n_steps(&self) -> usize;
     fn prover_builtins(&self) -> BuiltinCounterMap;
