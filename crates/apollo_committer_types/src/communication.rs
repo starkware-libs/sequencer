e1ab3c0b5668bba3cd01b7537d722e5709e25853
Yoav Gross <yoav@starkware.co>
2025-11-12 11:56:34 +0200

    committer: impl ComponentClientType

--- crates/apollo_committer_types/src/communication.rs
+++ crates/apollo_committer_types/src/communication.rs
@@ -1,17 +1,19 @@
 use std::sync::Arc;
 
 use apollo_infra::component_client::{LocalComponentClient, RemoteComponentClient};
-use apollo_infra::component_definitions::{PrioritizedRequest, RequestWrapper};
+use apollo_infra::component_definitions::{ComponentClient, PrioritizedRequest, RequestWrapper};
 use apollo_infra::requests::LABEL_NAME_REQUEST_VARIANT;
 use apollo_infra::{impl_debug_for_infra_requests_and_responses, impl_labeled_request};
+use apollo_infra::component_client::ClientError;
 use apollo_metrics::generate_permutation_labels;
+use apollo_proc_macros::handle_all_response_variants;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use strum::{EnumVariantNames, VariantNames};
 use strum_macros::{AsRefStr, EnumDiscriminants, EnumIter, IntoStaticStr};
 
 use crate::committer_types::{CommitBlockRequest, CommitBlockResponse};
-use crate::errors::{CommitterClientResult, CommitterResult};
+use crate::errors::{CommitterClientError, CommitterClientResult, CommitterResult};
 
 pub type LocalCommitterClient = LocalComponentClient<CommitterRequest, CommitterResponse>;
 pub type RemoteCommitterClient = RemoteComponentClient<CommitterRequest, CommitterResponse>;
@@ -53,4 +55,24 @@ impl_debug_for_infra_requests_and_responses!(CommitterResponse);
 generate_permutation_labels! {
     COMMITTER_REQUEST_LABELS,
     (LABEL_NAME_REQUEST_VARIANT, CommitterRequestLabelValue),
+}
+
+#[async_trait]
+impl<ComponentClientType> CommitterClient for ComponentClientType
+where
+    ComponentClientType: Send + Sync + ComponentClient<CommitterRequest, CommitterResponse>,
+{
+    async fn commit_block(
+        &self,
+        input: CommitBlockRequest,
+    ) -> CommitterClientResult<CommitBlockResponse> {
+        let request = CommitterRequest::CommitBlock(input);
+        handle_all_response_variants!(
+            CommitterResponse,
+            CommitBlock,
+            CommitterClientError,
+            Committer,
+            Direct
+        )
+    }
 }
