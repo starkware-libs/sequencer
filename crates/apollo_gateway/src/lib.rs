9b203aad1978f7a819300fbf3b5716aa825cb85f
Arni Hod <arnon@starkware.co>
2025-11-13 16:44:20 +0200

    apollo_gateway: use state reader and contract manager

--- crates/apollo_gateway/src/lib.rs
+++ crates/apollo_gateway/src/lib.rs
@@ -7,6 +7,8 @@ pub mod rpc_state_reader;
 #[cfg(test)]
 mod rpc_state_reader_test;
 mod state_reader;
+#[cfg(test)]
+mod state_reader_test;
 #[cfg(any(feature = "testing", test))]
 mod state_reader_test_utils;
 mod stateful_transaction_validator;
