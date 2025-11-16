6b07a73aa7897729feae0d62cc3fb7668af3663d
Nadin Jbara <93648739+nadin-Starkware@users.noreply.github.com>
2025-11-16 10:14:38 +0200

    apollo_infra: return ReloadHandle from configure_tracing (#10083)

--- crates/apollo_infra/src/trace_util.rs
+++ crates/apollo_infra/src/trace_util.rs
@@ -3,16 +3,36 @@ use tokio::sync::OnceCell;
 use tracing::metadata::LevelFilter;
 use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
-use tracing_subscriber::{fmt, EnvFilter};
+use tracing_subscriber::{fmt, reload, EnvFilter};
+
+// Crates we always keep at INFO regardless of operator-supplied spec.
+const QUIET_LIBS: &[&str] = &[
+    "alloy_provider",
+    "alloy_transport_http",
+    "alloy_rpc_client",
+    "futures-util",
+    "hickory-proto",
+    "hyper",
+    "hyper_util",
+    "h2",
+    "libp2p",
+    "libp2p-gossipsub",
+    "multistream_select",
+    "netlink_proto",
+    "reqwest",
+    "yamux",
+];
 
 const DEFAULT_LEVEL: LevelFilter = LevelFilter::INFO;
+type ReloadHandle = reload::Handle<EnvFilter, tracing_subscriber::Registry>;
+
 // Define a OnceCell to ensure the configuration is initialized only once
-static TRACING_INITIALIZED: OnceCell<()> = OnceCell::const_new();
+static TRACING_INITIALIZED: OnceCell<ReloadHandle> = OnceCell::const_new();
 
 pub static PID: std::sync::LazyLock<u32> = std::sync::LazyLock::new(std::process::id);
 
-pub async fn configure_tracing() {
-    TRACING_INITIALIZED
+pub async fn configure_tracing() -> ReloadHandle {
+    let reload_handle = TRACING_INITIALIZED
         .get_or_init(|| async {
             // Use default time formatting with sub-second precision limited to three digits.
             let time_format = format_description!(
@@ -29,31 +49,25 @@ pub async fn configure_tracing() {
                 .with_line_number(true)
                 .flatten_event(true);
 
-            let level_filter_layer = EnvFilter::builder()
-                .with_default_directive(DEFAULT_LEVEL.into())
-                .from_env_lossy()
-                .add_directive("alloy_provider=info".parse().unwrap())
-                .add_directive("alloy_transport_http=info".parse().unwrap())
-                .add_directive("alloy_rpc_client=info".parse().unwrap())
-                .add_directive("futures-util=info".parse().unwrap())
-                .add_directive("hickory-proto=info".parse().unwrap())
-                .add_directive("hyper=info".parse().unwrap())
-                .add_directive("hyper_util=info".parse().unwrap())
-                .add_directive("h2=info".parse().unwrap())
-                .add_directive("libp2p=info".parse().unwrap())
-                .add_directive("libp2p-gossipsub=info".parse().unwrap())
-                .add_directive("multistream_select=info".parse().unwrap())
-                .add_directive("netlink_proto=info".parse().unwrap())
-                .add_directive("reqwest=info".parse().unwrap())
-                .add_directive("yamux=info".parse().unwrap());
+            let level_filter_layer = QUIET_LIBS.iter().fold(
+                EnvFilter::builder().with_default_directive(DEFAULT_LEVEL.into()).from_env_lossy(),
+                |layer, lib| layer.add_directive(format!("{lib}=info").parse().unwrap()),
+            );
+
+            // Wrap the EnvFilter in a reloadable layer so that it can be updated at runtime.
+            let (filtered_layer, reload_handle) = reload::Layer::new(level_filter_layer);
 
             // This sets a single subscriber to all of the threads. We may want to implement
             // different subscriber for some threads and use set_global_default instead
             // of init.
-            tracing_subscriber::registry().with(fmt_layer).with(level_filter_layer).init();
+            tracing_subscriber::registry().with(filtered_layer).with(fmt_layer).init();
             tracing::info!("Tracing has been successfully initialized.");
+
+            reload_handle
         })
         .await;
+
+    reload_handle.clone()
 }
 
 #[macro_export]
