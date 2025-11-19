24ef51ca2a9abc4f0a991286f417fbf0293d493f
Yoav Gross <yoav@starkware.co>
2025-11-19 15:22:47 +0200

    starknet_committer: define trie writer trait

--- crates/starknet_committer/src/db/forest_trait.rs
+++ crates/starknet_committer/src/db/forest_trait.rs
@@ -7,6 +7,7 @@ use starknet_patricia::patricia_merkle_tree::types::NodeIndex;
 use starknet_patricia_storage::storage_trait::Storage;
 
 use crate::block_committer::input::{ConfigImpl, StarknetStorageValue};
+use crate::forest::filled_forest::FilledForest;
 use crate::forest::forest_errors::ForestResult;
 use crate::forest::original_skeleton_forest::{ForestSortedIndices, OriginalSkeletonForest};
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
@@ -24,6 +25,12 @@ pub trait ForestReader<'a> {
         config: ConfigImpl,
     ) -> ForestResult<(OriginalSkeletonForest<'a>, HashMap<NodeIndex, ContractState>)>;
 }
+
+pub trait ForestWriter {
+    fn write(&mut self, filled_forest: &FilledForest);
+}
+
+pub trait ForestStorage<'a>: ForestReader<'a> + ForestWriter {}
 
 pub struct FactsDb<S: Storage> {
     pub storage: S,
@@ -54,5 +61,11 @@ impl<'a, S: Storage> ForestReader<'a> for FactsDb<S> {
             forest_sorted_indices,
             &config,
         )
+    }
+}
+
+impl<S: Storage> ForestWriter for FactsDb<S> {
+    fn write(&mut self, filled_forest: &FilledForest) {
+        filled_forest.write_to_storage(&mut self.storage);
     }
 }
