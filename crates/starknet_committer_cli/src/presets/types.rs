264c01a6a3afed63112d10df82597f5c4f3ca323
Dori Medini <dori@starkware.co>
2025-11-19 16:26:50 +0200

    starknet_committer_cli: define Preset, validate, and define StorageLayoutName

--- crates/starknet_committer_cli/src/presets/types.rs
+++ crates/starknet_committer_cli/src/presets/types.rs
@@ -1,2 +1,29 @@
+use crate::presets::types::flavors::{FlavorFields, InterferenceFlavor};
+use crate::presets::types::storage::{StorageLayout, StorageLayoutName};
+
 pub mod flavors;
 pub mod storage;
+
+pub struct Preset {
+    flavor_fields: FlavorFields,
+    storage_layout: StorageLayout,
+}
+
+impl Preset {
+    pub fn new(flavor_fields: FlavorFields, storage_layout: StorageLayout) -> Self {
+        let preset = Self { flavor_fields, storage_layout };
+        preset.validate();
+        preset
+    }
+
+    fn validate(&self) {
+        if self.flavor_fields.interference_fields.interference_type != InterferenceFlavor::None
+            && !self.storage_layout.supports_interference()
+        {
+            panic!(
+                "Storage layout {} does not support interference",
+                self.storage_layout.short_name()
+            );
+        }
+    }
+}
