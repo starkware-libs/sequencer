e142d0f48a54809aca4cabb2234d616553248514
Dori Medini <dori@starkware.co>
2025-11-19 14:00:17 +0200

    starknet_committer_cli: copy+restructure arg types into separate modules

---
+++ crates/starknet_committer_cli/src/presets/types/flavors.rs
@@ -0,0 +1,47 @@
+pub const DEFAULT_INTERFERENCE_CONCURRENCY_LIMIT: usize = 20;
+
+/// Specific flavors of workloads to run in the benchmark.
+#[derive(Default)]
+pub enum BenchmarkFlavor {
+    // Constant number of updates per iteration.
+    #[default]
+    Constant,
+    // Periodic peaks of a constant number of updates per peak iteration, with 20% of the number
+    // of updates on non-peak iterations. Peaks are 10 iterations every 500 iterations.
+    PeriodicPeaks,
+    // Constant number of state diffs per iteration, with 20% new leaves per iteration. The other
+    // 80% leaf updates are sampled randomly from recent leaf updates.
+    // For the first blocks, behaves just like [Self::Constant] ("warmup" phase).
+    Overlap,
+    // Constant number of updates per iteration, where block N generates updates for leaf keys
+    // [N * C, (N + 1) * C).
+    Continuous,
+}
+
+#[derive(Default)]
+pub enum InterferenceFlavor {
+    // No interference.
+    #[default]
+    None,
+    // Read 1000 random keys every block.
+    Read1KEveryBlock,
+}
+
+/// Settings for interference (spawned tasks that run in parallel to the main benchmark).
+pub struct InterferenceFields {
+    // The type of interference to apply.
+    pub interference_type: InterferenceFlavor,
+
+    // The maximum number of interference tasks to run concurrently.
+    // Any attempt to spawn a new interference task will log a warning and not spawn the task.
+    pub interference_concurrency_limit: usize,
+}
+
+impl Default for InterferenceFields {
+    fn default() -> Self {
+        Self {
+            interference_type: InterferenceFlavor::None,
+            interference_concurrency_limit: DEFAULT_INTERFERENCE_CONCURRENCY_LIMIT,
+        }
+    }
+}
