5fc12e5520abb2b908efc515c53f51ccc1a71977
Dori Medini <dori@starkware.co>
2025-11-20 16:34:01 +0200

    blockifier: add VC diff regression test
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/versioned_constants_test.rs
+++ crates/blockifier/src/versioned_constants_test.rs
@@ -2,11 +2,13 @@ use std::path::PathBuf;
 
 use apollo_infra_utils::compile_time_cargo_manifest_dir;
 use cached::proc_macro::cached;
+use expect_test::expect_file;
 use glob::{glob, Paths};
 use indexmap::IndexMap;
 use pretty_assertions::assert_eq;
 use rstest::rstest;
 use serde_json::Value;
+use similar::{ChangeTag, TextDiff};
 use starknet_api::block::StarknetVersion;
 
 use super::*;
@@ -135,6 +137,37 @@ fn test_backward_compatibility_accounts_increasing() {
             )),
             "data_gas_accounts is not a superset from version {prev_version} to {version}",
         );
+        prev_version = version;
+        prev_vc = current_vc;
+    }
+}
+
+/// Test that the differences between VCs of different versions do not regress.
+#[test]
+fn test_vc_diffs_regression() {
+    let first_version = StarknetVersion::V0_13_0;
+    let mut prev_vc = VersionedConstants::json_str(&first_version).unwrap();
+    let mut prev_version = first_version;
+    for version in StarknetVersion::iter().filter(|v| v > &first_version) {
+        let current_vc = VersionedConstants::json_str(&version).unwrap();
+        let diff = TextDiff::from_lines(prev_vc, current_vc);
+        let diff_string = diff
+            .iter_all_changes()
+            .filter_map(|change| match change.tag() {
+                ChangeTag::Equal => None,
+                ChangeTag::Delete => {
+                    Some(format!("-[{:3}]   {change}", change.old_index().unwrap()))
+                }
+                ChangeTag::Insert => {
+                    Some(format!("+[{:3}]   {change}", change.new_index().unwrap()))
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("");
+        expect_file![format!(
+            "../resources/versioned_constants_diff_regression/{prev_version}_{version}.txt"
+        )]
+        .assert_eq(&diff_string);
         prev_version = version;
         prev_vc = current_vc;
     }
