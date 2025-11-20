59a4f4ca0056181863f653a1d885ba404bf460c4
yoavGrs <97383386+yoavGrs@users.noreply.github.com>
2025-11-20 14:57:25 +0200

    committer: move read skeleton implementation to facts_db (#10308)

--- crates/starknet_committer/src/db/facts_db.rs
+++ crates/starknet_committer/src/db/facts_db.rs
@@ -3,15 +3,29 @@ use std::collections::HashMap;
 use starknet_api::core::ContractAddress;
 use starknet_api::hash::HashOutput;
 use starknet_patricia::patricia_merkle_tree::node_data::leaf::LeafModifications;
-use starknet_patricia::patricia_merkle_tree::types::NodeIndex;
+use starknet_patricia::patricia_merkle_tree::original_skeleton_tree::tree::{
+    OriginalSkeletonTree,
+    OriginalSkeletonTreeImpl,
+};
+use starknet_patricia::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices};
 use starknet_patricia_storage::storage_trait::Storage;
 
-use crate::block_committer::input::{ConfigImpl, StarknetStorageValue};
+use crate::block_committer::input::{
+    contract_address_into_node_index,
+    Config,
+    ConfigImpl,
+    StarknetStorageValue,
+};
 use crate::db::forest_trait::{ForestReader, ForestWriter};
 use crate::forest::filled_forest::FilledForest;
-use crate::forest::forest_errors::ForestResult;
+use crate::forest::forest_errors::{ForestError, ForestResult};
 use crate::forest::original_skeleton_forest::{ForestSortedIndices, OriginalSkeletonForest};
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
+use crate::patricia_merkle_tree::tree::{
+    OriginalSkeletonClassesTrieConfig,
+    OriginalSkeletonContractsTrieConfig,
+    OriginalSkeletonStorageTrieConfig,
+};
 use crate::patricia_merkle_tree::types::CompiledClassHash;
 
 pub struct FactsDb<S: Storage> {
@@ -28,9 +42,76 @@ impl<S: Storage> FactsDb<S> {
     pub fn consume_storage(self) -> S {
         self.storage
     }
+
+    /// Creates the contracts trie original skeleton.
+    /// Also returns the previous contracts state of the modified contracts.
+    fn create_contracts_trie<'a>(
+        &mut self,
+        contracts_trie_root_hash: HashOutput,
+        contracts_trie_sorted_indices: SortedLeafIndices<'a>,
+    ) -> ForestResult<(OriginalSkeletonTreeImpl<'a>, HashMap<NodeIndex, ContractState>)> {
+        Ok(OriginalSkeletonTreeImpl::create_and_get_previous_leaves(
+            &mut self.storage,
+            contracts_trie_root_hash,
+            contracts_trie_sorted_indices,
+            &OriginalSkeletonContractsTrieConfig::new(),
+            &HashMap::new(),
+        )?)
+    }
+
+    fn create_storage_tries<'a>(
+        &mut self,
+        actual_storage_updates: &HashMap<ContractAddress, LeafModifications<StarknetStorageValue>>,
+        original_contracts_trie_leaves: &HashMap<NodeIndex, ContractState>,
+        config: &impl Config,
+        storage_tries_sorted_indices: &HashMap<ContractAddress, SortedLeafIndices<'a>>,
+    ) -> ForestResult<HashMap<ContractAddress, OriginalSkeletonTreeImpl<'a>>> {
+        let mut storage_tries = HashMap::new();
+        for (address, updates) in actual_storage_updates {
+            let sorted_leaf_indices = storage_tries_sorted_indices
+                .get(address)
+                .ok_or(ForestError::MissingSortedLeafIndices(*address))?;
+            let contract_state = original_contracts_trie_leaves
+                .get(&contract_address_into_node_index(address))
+                .ok_or(ForestError::MissingContractCurrentState(*address))?;
+            let config =
+                OriginalSkeletonStorageTrieConfig::new(config.warn_on_trivial_modifications());
+
+            let original_skeleton = OriginalSkeletonTreeImpl::create(
+                &mut self.storage,
+                contract_state.storage_root_hash,
+                *sorted_leaf_indices,
+                &config,
+                updates,
+            )?;
+            storage_tries.insert(*address, original_skeleton);
+        }
+        Ok(storage_tries)
+    }
+
+    fn create_classes_trie<'a>(
+        &mut self,
+        actual_classes_updates: &LeafModifications<CompiledClassHash>,
+        classes_trie_root_hash: HashOutput,
+        config: &impl Config,
+        contracts_trie_sorted_indices: SortedLeafIndices<'a>,
+    ) -> ForestResult<OriginalSkeletonTreeImpl<'a>> {
+        let config = OriginalSkeletonClassesTrieConfig::new(config.warn_on_trivial_modifications());
+
+        Ok(OriginalSkeletonTreeImpl::create(
+            &mut self.storage,
+            classes_trie_root_hash,
+            contracts_trie_sorted_indices,
+            &config,
+            actual_classes_updates,
+        )?)
+    }
 }
 
 impl<'a, S: Storage> ForestReader<'a> for FactsDb<S> {
+    /// Creates an original skeleton forest that includes the storage tries of the modified
+    /// contracts, the classes trie and the contracts trie. Additionally, returns the original
+    /// contract states that are needed to compute the contract state tree.
     fn read(
         &mut self,
         contracts_trie_root_hash: HashOutput,
@@ -40,17 +121,27 @@ impl<'a, S: Storage> ForestReader<'a> for FactsDb<S> {
         forest_sorted_indices: &'a ForestSortedIndices<'a>,
         config: ConfigImpl,
     ) -> ForestResult<(OriginalSkeletonForest<'a>, HashMap<NodeIndex, ContractState>)> {
-        // TODO(Yoav): Move here OriginalSkeletonForest constructor  with all the (facts)
-        // storage-related functions.
-        OriginalSkeletonForest::create(
-            &mut self.storage,
+        let (contracts_trie, original_contracts_trie_leaves) = self.create_contracts_trie(
             contracts_trie_root_hash,
-            classes_trie_root_hash,
+            forest_sorted_indices.contracts_trie_sorted_indices,
+        )?;
+        let storage_tries = self.create_storage_tries(
             storage_updates,
-            classes_updates,
-            forest_sorted_indices,
+            &original_contracts_trie_leaves,
             &config,
-        )
+            &forest_sorted_indices.storage_tries_sorted_indices,
+        )?;
+        let classes_trie = self.create_classes_trie(
+            classes_updates,
+            classes_trie_root_hash,
+            &config,
+            forest_sorted_indices.classes_trie_sorted_indices,
+        )?;
+
+        Ok((
+            OriginalSkeletonForest { classes_trie, contracts_trie, storage_tries },
+            original_contracts_trie_leaves,
+        ))
     }
 }
 
