04199f1d3d7bb1da5c16bc88bc5fd7117c6f10b2
guyf-starkware <guy.f@starkware.co>
2025-11-20 14:52:18 +0200

    apollo_consensus_config: add config for the consensus storage (#10211)

--- crates/papyrus_node/src/config/pointers.rs
+++ crates/papyrus_node/src/config/pointers.rs
@@ -57,6 +57,7 @@ pub static CONFIG_POINTERS: LazyLock<ConfigPointers> = LazyLock::new(|| {
                 "The chain to follow. For more details see https://docs.starknet.io/documentation/architecture_and_concepts/Blocks/transactions/#chain-id.",
             ),
             set_pointing_param_paths(&[
+                "consensus.static_config.storage_config.db_config.chain_id",
                 "context.chain_id",
                 "network.chain_id",
                 "rpc.chain_id",
