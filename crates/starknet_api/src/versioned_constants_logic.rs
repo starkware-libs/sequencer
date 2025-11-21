776dc971242482caa0aed9da92ab91113cb433ab
Dori Medini <dori@starkware.co>
2025-11-21 19:17:17 +0200

    starknet_api: auto-test diff regression in versioned constants

--- crates/starknet_api/src/versioned_constants_logic.rs
+++ crates/starknet_api/src/versioned_constants_logic.rs
@@ -1,7 +1,26 @@
+use std::fmt::Debug;
+#[cfg(any(test, feature = "testing"))]
+use std::path::Path;
+
+#[cfg(any(test, feature = "testing"))]
+use expect_test::expect_file;
+#[cfg(any(test, feature = "testing"))]
+use json_patch::{
+    diff as json_diff,
+    AddOperation,
+    PatchOperation,
+    RemoveOperation,
+    ReplaceOperation,
+};
+#[cfg(any(test, feature = "testing"))]
+use serde_json::Value;
+#[cfg(any(test, feature = "testing"))]
+use strum::IntoEnumIterator;
+
 use crate::block::StarknetVersion;
 
-pub trait VersionedConstantsTrait {
-    type Error;
+pub trait VersionedConstantsTrait: Debug {
+    type Error: Debug;
 
     /// Gets the first version with versioned constants.
     fn first_version() -> StarknetVersion;
@@ -17,14 +36,50 @@ pub trait VersionedConstantsTrait {
     fn get(version: &StarknetVersion) -> Result<&'static Self, Self::Error>;
 }
 
+/// Helper function to test for regression in diffs between versioned constant values.
+/// An instance of this test is generated for each versioned constants struct generated via the
+/// [crate::define_versioned_constants] macro.
+#[cfg(any(test, feature = "testing"))]
+pub fn vc_diffs_regression_test_body<V: VersionedConstantsTrait>(path_to_diff_dir: &Path) {
+    let first_version = V::first_version();
+    let mut prev_vc = serde_json::from_str::<Value>(V::json_str(&first_version).unwrap()).unwrap();
+    let mut prev_version = first_version;
+    for version in StarknetVersion::iter().filter(|v| v > &first_version) {
+        let current_vc = serde_json::from_str::<Value>(V::json_str(&version).unwrap()).unwrap();
+        let diff = json_diff(&prev_vc, &current_vc);
+        let diff_string = diff
+            .0
+            .iter()
+            .map(|patch| match patch {
+                PatchOperation::Add(AddOperation { path, value }) => format!("+ {path}: {value}"),
+                PatchOperation::Remove(RemoveOperation { path }) => format!("- {path}"),
+                PatchOperation::Replace(ReplaceOperation { path, value }) => {
+                    format!("~ {path}: {value}")
+                }
+                other => panic!("Unexpected patch operation: {other}"),
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+            + "\n";
+        expect_file![path_to_diff_dir.join(format!("{prev_version}_{version}.txt"))]
+            .assert_eq(&diff_string);
+        prev_version = version;
+        prev_vc = current_vc;
+    }
+}
+
 /// Auto-generate getters for listed versioned constants versions.
 /// Optionally provide an intermediate struct for deserialization.
+/// Also, provide the path to the directory containing the diffs between versions (for initial
+/// definition of the struct, run `UPDATE_EXPECT=1 cargo test -p <crate> test_vc_diffs_regression`
+/// to generate the diffs).
 #[macro_export]
 macro_rules! define_versioned_constants {
     (
         $struct_name:ident,
         $error_type:ident,
         $first_version:expr,
+        $path_to_diff_dir:literal,
         $(($variant:ident, $path_to_json:expr)),* $(,)?
     ) => {
         // Static (lazy) instances of the versioned constants.
@@ -41,7 +96,11 @@ macro_rules! define_versioned_constants {
         }
 
         $crate::define_versioned_constants_inner!(
-            $struct_name, $error_type, $first_version, $(($variant, $path_to_json)),*
+            $struct_name,
+            $error_type,
+            $first_version,
+            $path_to_diff_dir,
+            $(($variant, $path_to_json)),*
         );
     };
 
@@ -50,6 +109,7 @@ macro_rules! define_versioned_constants {
         $intermediate_struct_name:ident,
         $error_type:ident,
         $first_version:expr,
+        $path_to_diff_dir:literal,
         $(($variant:ident, $path_to_json:expr)),* $(,)?
     ) => {
         // Static (lazy) instances of the versioned constants.
@@ -69,7 +129,11 @@ macro_rules! define_versioned_constants {
         }
 
         $crate::define_versioned_constants_inner!(
-            $struct_name, $error_type, $first_version, $(($variant, $path_to_json)),*
+            $struct_name,
+            $error_type,
+            $first_version,
+            $path_to_diff_dir,
+            $(($variant, $path_to_json)),*
         );
     };
 }
@@ -80,6 +144,7 @@ macro_rules! define_versioned_constants_inner {
         $struct_name:ident,
         $error_type:ident,
         $first_version:expr,
+        $path_to_diff_dir:literal,
         $(($variant:ident, $path_to_json:expr)),* $(,)?
     ) => {
         paste::paste! {
@@ -133,5 +198,17 @@ macro_rules! define_versioned_constants_inner {
                 $struct_name::json_str(&latest_variant)
                     .expect("Latest version should support VC.").to_string()
             });
+
+        #[cfg(test)]
+        #[test]
+        fn test_vc_diffs_regression() {
+            let path = std::path::Path::new($path_to_diff_dir);
+            if !path.exists() {
+                std::fs::create_dir_all(path).unwrap();
+            }
+            $crate::versioned_constants_logic::vc_diffs_regression_test_body::<$struct_name>(
+                &path.canonicalize().unwrap()
+            );
+        }
     };
 }
