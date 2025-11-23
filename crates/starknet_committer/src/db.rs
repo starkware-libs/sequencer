f2fb9978e642dd93672e1c83a31f8f0b4a243a74
Yoav Gross <yoav@starkware.co>
2025-11-23 23:44:58 +0200

    committer: move skeleton trie creation
    rest

--- crates/starknet_committer/src/db.rs
+++ crates/starknet_committer/src/db.rs
@@ -1,3 +1,4 @@
+pub mod create_facts_tree;
 #[cfg(any(feature = "testing", test))]
 pub mod external_test_utils;
 pub mod facts_db;
--- crates/starknet_committer/src/db/facts_db.rs
+++ crates/starknet_committer/src/db/facts_db.rs
@@ -14,6 +14,10 @@ use crate::block_committer::input::{
     ConfigImpl,
     StarknetStorageValue,
 };
+use crate::db::create_facts_tree::{
+    create_original_skeleton_tree,
+    create_original_skeleton_tree_and_get_previous_leaves,
+};
 use crate::db::forest_trait::{ForestReader, ForestWriter};
 use crate::forest::filled_forest::FilledForest;
 use crate::forest::forest_errors::{ForestError, ForestResult};
@@ -44,7 +48,7 @@ impl<S: Storage> FactsDb<S> {
         contracts_trie_root_hash: HashOutput,
         contracts_trie_sorted_indices: SortedLeafIndices<'a>,
     ) -> ForestResult<(OriginalSkeletonTreeImpl<'a>, HashMap<NodeIndex, ContractState>)> {
-        Ok(OriginalSkeletonTreeImpl::create_and_get_previous_leaves(
+        Ok(create_original_skeleton_tree_and_get_previous_leaves(
             &mut self.storage,
             contracts_trie_root_hash,
             contracts_trie_sorted_indices,
@@ -71,7 +75,7 @@ impl<S: Storage> FactsDb<S> {
             let config =
                 OriginalSkeletonStorageTrieConfig::new(config.warn_on_trivial_modifications());
 
-            let original_skeleton = OriginalSkeletonTreeImpl::create(
+            let original_skeleton = create_original_skeleton_tree(
                 &mut self.storage,
                 contract_state.storage_root_hash,
                 *sorted_leaf_indices,
@@ -92,7 +96,7 @@ impl<S: Storage> FactsDb<S> {
     ) -> ForestResult<OriginalSkeletonTreeImpl<'a>> {
         let config = OriginalSkeletonClassesTrieConfig::new(config.warn_on_trivial_modifications());
 
-        Ok(OriginalSkeletonTreeImpl::create(
+        Ok(create_original_skeleton_tree(
             &mut self.storage,
             classes_trie_root_hash,
             contracts_trie_sorted_indices,
