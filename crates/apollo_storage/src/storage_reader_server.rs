b57a52a6c464b56c409668ee347fa0c40028351e
nadin-Starkware <nadin@starkware.co>
2025-11-25 15:10:06 +0200

    apollo_storage: add AppState struct for axum server

--- crates/apollo_storage/src/storage_reader_server.rs
+++ crates/apollo_storage/src/storage_reader_server.rs
@@ -1,9 +1,11 @@
 use std::marker::PhantomData;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use async_trait::async_trait;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use tokio::sync::Semaphore;
 
 use crate::{StorageError, StorageReader};
 
@@ -46,10 +48,33 @@ where
     Request: Serialize + DeserializeOwned + Send + 'static,
     Response: Serialize + DeserializeOwned + Send + 'static,
 {
-    storage_reader: StorageReader,
-    request_handler: RequestHandler,
+    app_state: AppState<RequestHandler, Request, Response>,
     config: ServerConfig,
+}
+
+/// Application state shared across request handlers.
+struct AppState<RequestHandler, Request, Response>
+where
+    RequestHandler: StorageReaderServerHandler<Request, Response>,
+{
+    storage_reader: Arc<StorageReader>,
+    request_handler: Arc<RequestHandler>,
+    semaphore: Arc<Semaphore>,
     _req_res: PhantomData<(Request, Response)>,
+}
+
+impl<RequestHandler, Request, Response> Clone for AppState<RequestHandler, Request, Response>
+where
+    RequestHandler: StorageReaderServerHandler<Request, Response>,
+{
+    fn clone(&self) -> Self {
+        Self {
+            storage_reader: Arc::clone(&self.storage_reader),
+            request_handler: Arc::clone(&self.request_handler),
+            semaphore: Arc::clone(&self.semaphore),
+            _req_res: PhantomData,
+        }
+    }
 }
 
 impl<RequestHandler, Request, Response> StorageReaderServer<RequestHandler, Request, Response>
@@ -60,11 +85,17 @@ where
 {
     /// Creates a new storage reader server with the given handler and configuration.
     pub fn new(
-        storage_reader: StorageReader,
-        request_handler: RequestHandler,
+        storage_reader: Arc<StorageReader>,
+        request_handler: Arc<RequestHandler>,
         config: ServerConfig,
     ) -> Self {
-        Self { storage_reader, request_handler, config, _req_res: PhantomData }
+        let app_state = AppState {
+            storage_reader,
+            request_handler,
+            semaphore: Arc::new(Semaphore::new(config.max_concurrency)),
+            _req_res: PhantomData,
+        };
+        Self { app_state, config }
     }
 
     /// Starts the server to handle incoming requests.
