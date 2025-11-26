edcb663149a1f4f8cea7cb574c62e9c94c6d7bc9
Ayelet Zilber <ayelet@starkware.co>
2025-11-26 16:02:15 +0200

    echonet: match ethereum log to feeder gateway result

--- echonet/tests/test_l1_events.py
+++ echonet/tests/test_l1_events.py
@@ -14,84 +14,107 @@ class TestL1Events(unittest.TestCase):
     RAW_JSON_LOG = {
         "address": "0xc662c410c0ecf747543f5ba90660f6abebd9c8c4",
         "topics": [
-            "0xdb80dd488acf86d17c747445b0eabb5d57c541d3bd7b6b87af987858e5066b2b",
-            "0x000000000000000000000000023a2aac5d0fa69e3243994672822ba43e34e5c9",
-            "0x07c76a71952ce3acd1f953fd2a3fda8564408b821ff367041c89f44526076633",
-            "0x02d757788a8d8d6f21d1cd40bce38a8222d70654214e96ff95d8086e684fbee5",
+            "0xdb80dd488acf86d17c747445b0eabb5d57c541d3bd7b6b87af987858e5066b2b",  # event_signature
+            "0x000000000000000000000000f5b6ee2caeb6769659f6c091d209dfdcaf3f69eb",  # from_address
+            "0x0616757a151c21f9be8775098d591c2807316d992bbc3bb1a5c1821630589256",  # to_address
+            "0x01b64b1b3b690b43b9b514fb81377518f4039cd3e4f4914d8a6bdf01d679fb19",  # selector
         ],
-        "data": (
-            "0x0000000000000000000000000000000000000000000000000000000000000060"
-            "0000000000000000000000000000000000000000000000000000000000195c23"
-            "0000000000000000000000000000000000000000000000000000000000000001"
-            "0000000000000000000000000000000000000000000000000000000000000003"
-            "001e220c4ac08b2f247d45721e08af1b2d8d65b640cea780534c8f20dc6ea981"
-            "000000000000000000000000000000000000000000001c468e3281804cca0000"
-            "0000000000000000000000000000000000000000000000000000000000000000"
-        ),
-        "blockNumber": "0x13e51a0",
-        "blockHash": "0xe090b2c6fbffb35b6e07d5943938384daa59c8c9fefe487d9952ef9894f2483e",
-        "transactionHash": "0x66c2ef5ae6708ede5e47daaabfc4b54a53c423160ec27eac06524ea3cd939622",
-        "transactionIndex": "0x92",
-        "logIndex": "0x2ed",
+        "data": "0x0000000000000000000000000000000000000000000000000000000000000060"
+        "000000000000000000000000000000000000000000000000000000000019b255"
+        "00000000000000000000000000000000000000000000000000001308aba4ade2"
+        "0000000000000000000000000000000000000000000000000000000000000005"
+        "00000000000000000000000004c46e830bb56ce22735d5d8fc9cb90309317d0f"
+        "000000000000000000000000c50a951c4426760ba75c5253985a16196b342168"
+        "011bf9dbebdd770c31ff13808c96a1cb2de15a240274dc527e7d809bb2bf38df"
+        "0000000000000000000000000000000000000000000000956dfdeac59085edc3"
+        "0000000000000000000000000000000000000000000000000000000000000000",
+        "blockHash": "0xb33512d13e1a2ff4f3aa6e799a4a2455249be5198760a3f41300a8362d802bf8",
+        "blockNumber": "0x16cda82",
+        "blockTimestamp": "0x692c23df",
+        "transactionHash": "0x726df509fdd23a944f923a6fc18e80cbe7300a54aa34f8e6bd77e9961ca6ce52",
+        "transactionIndex": "0x4f",
+        "logIndex": "0x7b",
         "removed": False,
-        "blockTimestamp": "0x66fa358f",
     }
 
     L1_CLIENT_LOG = L1Client.Log(
         address="0xc662c410c0ecf747543f5ba90660f6abebd9c8c4",
         topics=[
             "0xdb80dd488acf86d17c747445b0eabb5d57c541d3bd7b6b87af987858e5066b2b",
-            "0x000000000000000000000000023a2aac5d0fa69e3243994672822ba43e34e5c9",
-            "0x07c76a71952ce3acd1f953fd2a3fda8564408b821ff367041c89f44526076633",
-            "0x02d757788a8d8d6f21d1cd40bce38a8222d70654214e96ff95d8086e684fbee5",
+            "0x000000000000000000000000f5b6ee2caeb6769659f6c091d209dfdcaf3f69eb",
+            "0x0616757a151c21f9be8775098d591c2807316d992bbc3bb1a5c1821630589256",
+            "0x01b64b1b3b690b43b9b514fb81377518f4039cd3e4f4914d8a6bdf01d679fb19",
         ],
         data="0x0000000000000000000000000000000000000000000000000000000000000060"
-        "0000000000000000000000000000000000000000000000000000000000195c23"
-        "0000000000000000000000000000000000000000000000000000000000000001"
-        "0000000000000000000000000000000000000000000000000000000000000003"
-        "001e220c4ac08b2f247d45721e08af1b2d8d65b640cea780534c8f20dc6ea981"
-        "000000000000000000000000000000000000000000001c468e3281804cca0000"
+        "000000000000000000000000000000000000000000000000000000000019b255"
+        "00000000000000000000000000000000000000000000000000001308aba4ade2"
+        "0000000000000000000000000000000000000000000000000000000000000005"
+        "00000000000000000000000004c46e830bb56ce22735d5d8fc9cb90309317d0f"
+        "000000000000000000000000c50a951c4426760ba75c5253985a16196b342168"
+        "011bf9dbebdd770c31ff13808c96a1cb2de15a240274dc527e7d809bb2bf38df"
+        "0000000000000000000000000000000000000000000000956dfdeac59085edc3"
         "0000000000000000000000000000000000000000000000000000000000000000",
-        block_number=20861344,
-        block_hash="0xe090b2c6fbffb35b6e07d5943938384daa59c8c9fefe487d9952ef9894f2483e",
-        transaction_hash="0x66c2ef5ae6708ede5e47daaabfc4b54a53c423160ec27eac06524ea3cd939622",
-        transaction_index=146,
-        log_index=749,
+        block_number=23911042,
+        block_hash="0xb33512d13e1a2ff4f3aa6e799a4a2455249be5198760a3f41300a8362d802bf8",
+        transaction_hash="0x726df509fdd23a944f923a6fc18e80cbe7300a54aa34f8e6bd77e9961ca6ce52",
+        transaction_index=79,
+        log_index=123,
         removed=False,
-        block_timestamp=1727673743,
+        block_timestamp=1764500447,
     )
 
     DECODED_LOG = L1Events.DecodedLogMessageToL2(
-        from_address="0x23a2aac5d0fa69e3243994672822ba43e34e5c9",
-        to_address="0x7c76a71952ce3acd1f953fd2a3fda8564408b821ff367041c89f44526076633",
-        selector=0x02D757788A8D8D6F21D1CD40BCE38A8222D70654214E96FF95D8086E684FBEE5,
+        from_address="0xf5b6ee2caeb6769659f6c091d209dfdcaf3f69eb",
+        to_address="0x616757a151c21f9be8775098d591c2807316d992bbc3bb1a5c1821630589256",
+        selector=0x1B64B1B3B690B43B9B514FB81377518F4039CD3E4F4914D8A6BDF01D679FB19,
         payload=[
-            0x001E220C4AC08B2F247D45721E08AF1B2D8D65B640CEA780534C8F20DC6EA981,
-            0x000000000000000000000000000000000000000000001C468E3281804CCA0000,
-            0,
+            0x04C46E830BB56CE22735D5D8FC9CB90309317D0F,
+            0xC50A951C4426760BA75C5253985A16196B342168,
+            0x11BF9DBEBDD770C31FF13808C96A1CB2DE15A240274DC527E7D809BB2BF38DF,
+            0x956DFDEAC59085EDC3,
+            0x0,
         ],
-        nonce=0x195C23,
-        fee=1,
-        l1_tx_hash="0x66c2ef5ae6708ede5e47daaabfc4b54a53c423160ec27eac06524ea3cd939622",
-        block_timestamp=0x66FA358F,
+        nonce=0x19B255,
+        fee=0x1308ABA4ADE2,
+        l1_tx_hash="0x726df509fdd23a944f923a6fc18e80cbe7300a54aa34f8e6bd77e9961ca6ce52",
+        block_timestamp=1764500447,
     )
 
     L1_EVENT = L1Events.L1Event(
         tx=L1Events.L1HandlerTransaction(
-            contract_address="0x7c76a71952ce3acd1f953fd2a3fda8564408b821ff367041c89f44526076633",
-            entry_point_selector=0x02D757788A8D8D6F21D1CD40BCE38A8222D70654214E96FF95D8086E684FBEE5,
+            contract_address="0x616757a151c21f9be8775098d591c2807316d992bbc3bb1a5c1821630589256",
+            entry_point_selector=0x1B64B1B3B690B43B9B514FB81377518F4039CD3E4F4914D8A6BDF01D679FB19,
             calldata=[
-                0x23A2AAC5D0FA69E3243994672822BA43E34E5C9,
-                0x001E220C4AC08B2F247D45721E08AF1B2D8D65B640CEA780534C8F20DC6EA981,
-                0x000000000000000000000000000000000000000000001C468E3281804CCA0000,
-                0,
+                0xF5B6EE2CAEB6769659F6C091D209DFDCAF3F69EB,
+                0x04C46E830BB56CE22735D5D8FC9CB90309317D0F,
+                0xC50A951C4426760BA75C5253985A16196B342168,
+                0x11BF9DBEBDD770C31FF13808C96A1CB2DE15A240274DC527E7D809BB2BF38DF,
+                0x956DFDEAC59085EDC3,
+                0x0,
             ],
-            nonce=0x195C23,
+            nonce=0x19B255,
         ),
-        fee=1,
-        l1_tx_hash="0x66c2ef5ae6708ede5e47daaabfc4b54a53c423160ec27eac06524ea3cd939622",
-        block_timestamp=1727673743,
+        fee=0x1308ABA4ADE2,
+        l1_tx_hash="0x726df509fdd23a944f923a6fc18e80cbe7300a54aa34f8e6bd77e9961ca6ce52",
+        block_timestamp=1764500447,
     )
+
+    FEEDER_TX = {
+        "transaction_hash": "0x83c298ad90f4d1b35c0a324fa162a3ab3d3d3a4dcc046f0965bd045083a472",
+        "version": "0x0",
+        "contract_address": "0x616757a151c21f9be8775098d591c2807316d992bbc3bb1a5c1821630589256",
+        "entry_point_selector": "0x1b64b1b3b690b43b9b514fb81377518f4039cd3e4f4914d8a6bdf01d679fb19",
+        "nonce": "0x19b255",
+        "calldata": [
+            "0xf5b6ee2caeb6769659f6c091d209dfdcaf3f69eb",
+            "0x4c46e830bb56ce22735d5d8fc9cb90309317d0f",
+            "0xc50a951c4426760ba75c5253985a16196b342168",
+            "0x11bf9dbebdd770c31ff13808c96a1cb2de15a240274dc527e7d809bb2bf38df",
+            "0x956dfdeac59085edc3",
+            "0x0",
+        ],
+        "type": "L1_HANDLER",
+    }
 
     def test_decode_log_success(self):
         decoded_log_result = L1Events.decode_log(self.L1_CLIENT_LOG)
@@ -126,7 +149,34 @@ class TestL1Events(unittest.TestCase):
     def test_parse_event_success(self):
         result = L1Events.parse_event(self.L1_CLIENT_LOG)
         self.assertIsInstance(result, L1Events.L1Event)
+
         self.assertEqual(result, self.L1_EVENT)
+
+    def test_matches_l1_handler_tx_success(self):
+        l1_event = self.L1_EVENT
+
+        feeder_tx = self.FEEDER_TX
+
+        self.assertTrue(L1Events.l1_event_matches_feeder_tx(l1_event, feeder_tx))
+
+    def test_matches_l1_handler_tx_mismatches(self):
+        l1_event = self.L1_EVENT
+
+        base_feeder_tx = self.FEEDER_TX
+
+        mismatch_cases = [
+            ("type", {"type": "INVOKE"}),
+            ("contract", {"contract_address": "0x1"}),
+            ("selector", {"entry_point_selector": "0x1"}),
+            ("nonce", {"nonce": "0x1"}),
+            ("calldata", {"calldata": ["0xabc"]}),
+        ]
+
+        for field_name, overrides in mismatch_cases:
+            with self.subTest(field=field_name):
+                # Builds a tx that is valid except for one mismatching field
+                feeder_tx = {**base_feeder_tx, **overrides}
+                self.assertFalse(L1Events.l1_event_matches_feeder_tx(l1_event, feeder_tx))
 
 
 if __name__ == "__main__":
