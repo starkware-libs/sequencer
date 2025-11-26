f47a34bbf55455e81c3a0cb83d551b05602142aa
Ayelet Zilber <ayelet@starkware.co>
2025-11-26 16:02:15 +0200

    echonet: match ethereum log to feeder gateway result

--- echonet/l1_events.py
+++ echonet/l1_events.py
@@ -90,3 +90,30 @@ def parse_event(log: dict) -> L1Event:
         l1_tx_hash=decoded.l1_tx_hash,
         block_timestamp=decoded.block_timestamp,
     )
+
+
+def l1_event_matches_feeder_tx(l1_event: L1Event, feeder_tx: dict) -> bool:
+    """
+    Compares L1Event with an L1_HANDLER feeder tx using only contract_address, entry_point_selector, nonce, and calldata.
+    Transaction hashes are ignored.
+    """
+    if feeder_tx.get("type") != "L1_HANDLER":
+        return False
+
+    feeder_contract = int(feeder_tx["contract_address"], 16)
+    if l1_event.tx.contract_address != feeder_contract:
+        return False
+
+    feeder_selector = int(feeder_tx["entry_point_selector"], 16)
+    if l1_event.tx.entry_point_selector != feeder_selector:
+        return False
+
+    feeder_nonce = int(feeder_tx["nonce"], 16)
+    if l1_event.tx.nonce != feeder_nonce:
+        return False
+
+    feeder_calldata = [int(item, 16) for item in feeder_tx["calldata"]]
+    if l1_event.tx.calldata != feeder_calldata:
+        return False
+
+    return True
