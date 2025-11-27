112f5adf98c40131a4db13b0187717f190199f58
Arni Hod <arnon@starkware.co>
2025-11-27 10:51:51 +0200

    apollo_gateway: add tests for fetch_contract_classes on sync state reader

--- crates/blockifier/src/state/state_api_test_utils.rs
+++ crates/blockifier/src/state/state_api_test_utils.rs
@@ -1,11 +1,11 @@
-use crate::execution::contract_class::RunnableCompiledClass;
+use std::fmt::Debug;
+
 use crate::state::errors::StateError;
 use crate::state::state_api::StateResult;
 
-pub fn assert_eq_state_result(
-    a: &StateResult<RunnableCompiledClass>,
-    b: &StateResult<RunnableCompiledClass>,
-) {
+// Compares the results of class retrieval from state.
+// Note: this function does not work for general StateResult comparisons. Add more cases as needed.
+pub fn assert_eq_state_result<T: PartialEq + Debug>(a: &StateResult<T>, b: &StateResult<T>) {
     match (a, b) {
         (Ok(a), Ok(b)) => assert_eq!(a, b),
         (Err(StateError::UndeclaredClassHash(a)), Err(StateError::UndeclaredClassHash(b))) => {
