21595020a76d64c0d7aec694d91a03a4e91e63a3
Noam Spiegelstein <noam.s@starkware.co>
2025-11-27 18:29:09 +0200

    apollo_dashboard: add http server min add tx latency alery

--- crates/apollo_dashboard/src/alert_scenarios/transaction_delays.rs
+++ crates/apollo_dashboard/src/alert_scenarios/transaction_delays.rs
@@ -94,6 +94,55 @@ pub(crate) fn get_http_server_avg_add_tx_latency_alert_vec() -> Vec<Alert> {
     ]
 }
 
+/// Triggers if the latency of all `add_tx` calls, across all HTTP servers, exceeds 1 second
+/// over a 2-minute window.
+fn get_http_server_min_add_tx_latency_alert(
+    alert_env_filtering: AlertEnvFiltering,
+    alert_severity: AlertSeverity,
+) -> Alert {
+    const TIME_WINDOW: &str = "2m";
+    let bucket_metric =
+        HTTP_SERVER_ADD_TX_LATENCY.get_name_with_filer_and_additional_fields("le=\"1.0\"");
+    let count_metric = HTTP_SERVER_ADD_TX_LATENCY.get_name_count_with_filter();
+    Alert::new(
+        "http_server_min_add_tx_latency",
+        "High HTTP server minimal add_tx latency",
+        AlertGroup::HttpServer,
+        // The lhs expr checks that there were transaction observations during the time window.
+        // The rhs expr verifies that none of these observations had a latency of 1 second or less
+        // (i.e., the le="1.0" bucket is empty).
+        // Multiplying these two conditions serves as a logical "and": it triggers only when there
+        // was activity, and all observed transactions took longer than 1 second.
+        format!(
+            "(sum(increase({count_metric}[{TIME_WINDOW}])) > 0) * \
+             (sum(increase({bucket_metric}[{TIME_WINDOW}])) < 1)"
+        ),
+        vec![AlertCondition {
+            comparison_op: AlertComparisonOp::GreaterThan,
+            comparison_value: 0.0,
+            logical_op: AlertLogicalOp::And,
+        }],
+        PENDING_DURATION_DEFAULT,
+        EVALUATION_INTERVAL_SEC_DEFAULT,
+        alert_severity,
+        ObserverApplicability::NotApplicable,
+        alert_env_filtering,
+    )
+}
+
+pub(crate) fn get_http_server_min_add_tx_latency_alert_vec() -> Vec<Alert> {
+    vec![
+        get_http_server_min_add_tx_latency_alert(
+            AlertEnvFiltering::MainnetStyleAlerts,
+            AlertSeverity::Sos,
+        ),
+        get_http_server_min_add_tx_latency_alert(
+            AlertEnvFiltering::TestnetStyleAlerts,
+            AlertSeverity::Sos,
+        ),
+    ]
+}
+
 /// Triggers when the slowest 5% of transactions for a specific HTTP server are taking longer than 2
 /// seconds over a 5-minute window.
 fn get_http_server_p95_add_tx_latency_alert(
