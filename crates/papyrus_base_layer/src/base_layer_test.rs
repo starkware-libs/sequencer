7cb6d71e61d993cd38e9a7ef783388c7bf2c3a5d
Guy Nir <guy.n@starkware.co>
2025-11-30 11:12:19 +0200

    papyrus_base_layer: add code to calculate data price in the era of Fusaka

--- crates/papyrus_base_layer/src/base_layer_test.rs
+++ crates/papyrus_base_layer/src/base_layer_test.rs
@@ -99,14 +99,55 @@ async fn get_gas_price_and_timestamps() {
         ..Default::default()
     };
 
-    // Test pectra blob.
-
     let mocked_block_response =
         &Some(Block::new(AlloyRpcHeader::new(header), BlockTransactions::<B256>::default()));
+
+    // Test fusaka blob.
+    base_layer.config.fusaka_no_bpo_start_block_number = 0;
+    base_layer.config.bpo1_start_block_number = 10;
+    base_layer.config.bpo2_start_block_number = 10;
+
     asserter.push_success(mocked_block_response);
     let header = base_layer.get_block_header(0).await.unwrap().unwrap();
 
     assert_eq!(header.base_fee_per_gas, 5);
+
+    // See eip4844::fake_exponential().
+    // Roughly e ** (BLOB_GAS / eip7691::BLOB_GASPRICE_UPDATE_FRACTION_PECTRA)
+    let expected_fusaka_blob_calc = 7;
+    assert_eq!(header.blob_fee, expected_fusaka_blob_calc);
+
+    // Test BPO1 blob.
+    base_layer.config.fusaka_no_bpo_start_block_number = 0;
+    base_layer.config.bpo1_start_block_number = 0;
+    base_layer.config.bpo2_start_block_number = 10;
+
+    asserter.push_success(mocked_block_response);
+    let header = base_layer.get_block_header(0).await.unwrap().unwrap();
+
+    // See eip4844::fake_exponential().
+    // Roughly e ** (BLOB_GAS / eip7691::BLOB_GASPRICE_UPDATE_FRACTION_PECTRA)
+    let expected_bpo1_blob_calc = 3;
+    assert_eq!(header.blob_fee, expected_bpo1_blob_calc);
+
+    // Test BPO2 blob.
+    base_layer.config.fusaka_no_bpo_start_block_number = 0;
+    base_layer.config.bpo1_start_block_number = 0;
+    base_layer.config.bpo2_start_block_number = 0;
+
+    asserter.push_success(mocked_block_response);
+    let header = base_layer.get_block_header(0).await.unwrap().unwrap();
+
+    let expected_bpo2_blob_calc = 2;
+    assert_eq!(header.blob_fee, expected_bpo2_blob_calc);
+
+    // Test pectra blob.
+    base_layer.config.fusaka_no_bpo_start_block_number = 10;
+    base_layer.config.bpo1_start_block_number = 10;
+    base_layer.config.bpo2_start_block_number = 10;
+
+    asserter.push_success(mocked_block_response);
+    let header = base_layer.get_block_header(0).await.unwrap().unwrap();
 
     // See eip4844::fake_exponential().
     // Roughly e ** (BLOB_GAS / eip7691::BLOB_GASPRICE_UPDATE_FRACTION_PECTRA)
