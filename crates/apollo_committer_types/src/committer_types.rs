c41666270f18c1de98f7e317079a9447493513e7
yoavGrs <97383386+yoavGrs@users.noreply.github.com>
2025-12-04 15:21:28 +0200

    apollo_committer_types: update commit block fields (#10567)

--- crates/apollo_committer_types/src/committer_types.rs
+++ crates/apollo_committer_types/src/committer_types.rs
@@ -1,14 +1,16 @@
 use serde::{Deserialize, Serialize};
-use starknet_api::hash::StateRoots;
+use starknet_api::block::BlockNumber;
+use starknet_api::core::{GlobalRoot, StateDiffCommitment};
 use starknet_committer::block_committer::input::StateDiff;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CommitBlockRequest {
     state_diff: StateDiff,
-    prev_state_roots: StateRoots,
+    state_diff_commitment: StateDiffCommitment,
+    height: BlockNumber,
 }
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct CommitBlockResponse {
-    new_state_roots: StateRoots,
+    state_root: GlobalRoot,
 }
