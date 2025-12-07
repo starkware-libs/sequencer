74b36f15b676a3fd10b89f18e5f5924ca05777d4
Andrew Luka <andrew.l@starkware.co>
2025-12-07 11:54:15 +0200

    apollo_network_benchmark: setup trace logging in benchmark

--- crates/apollo_network_benchmark/src/bin/broadcast_network_stress_test_node/main.rs
+++ crates/apollo_network_benchmark/src/bin/broadcast_network_stress_test_node/main.rs
@@ -1,6 +1,7 @@
 //! Runs a node that stress tests the p2p communication of the network.
 
 use clap::Parser;
+use tracing::Level;
 
 #[cfg(test)]
 mod message_test;
@@ -11,6 +12,22 @@ use apollo_network_benchmark::node_args::NodeArgs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _args = NodeArgs::parse();
+    let args = NodeArgs::parse();
+
+    let level = match args.user.verbosity {
+        0 => None,
+        1 => Some(Level::ERROR),
+        2 => Some(Level::WARN),
+        3 => Some(Level::INFO),
+        4 => Some(Level::DEBUG),
+        _ => Some(Level::TRACE),
+    };
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder().with_max_level(level).finish(),
+    )
+    .expect("Failed to set global default subscriber");
+
+    println!("Starting network stress test with args:\n{args:?}");
+
     Ok(())
 }
