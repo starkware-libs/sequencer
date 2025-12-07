b728be34b3ce23d04f19728c73d59c7308f4a257
Andrew Luka <andrew.l@starkware.co>
2025-12-07 13:15:17 +0200

    apollo_network_benchmark: added running tasks infrastructure

--- crates/apollo_network_benchmark/src/node_args.rs
+++ crates/apollo_network_benchmark/src/node_args.rs
@@ -16,6 +16,11 @@ pub struct UserArgs {
     /// Set the verbosity level of the logger, the higher the more verbose
     #[arg(short, long, env, default_value = "2")]
     pub verbosity: u8,
+
+    /// The timeout in seconds for the node.
+    /// When the node runs for longer than this, it will be killed.
+    #[arg(long, env, default_value = "4000")]
+    pub timeout: u64,
 }
 
 #[derive(Parser, Debug, Clone)]
