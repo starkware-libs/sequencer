9f9011e4e7591d5cffbd6b4b1e1107ee4c061148
Andrew Luka <andrew.l@starkware.co>
2025-12-07 11:54:15 +0200

    apollo_network_benchmark: setup trace logging in benchmark

--- crates/apollo_network_benchmark/src/node_args.rs
+++ crates/apollo_network_benchmark/src/node_args.rs
@@ -2,4 +2,23 @@ use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
-pub struct NodeArgs {}
+/// Arguments from the runner, not meant to be set by the user.
+pub struct RunnerArgs {}
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+/// Arguments from the user.
+pub struct UserArgs {
+    /// Set the verbosity level of the logger, the higher the more verbose
+    #[arg(short, long, env, default_value = "2")]
+    pub verbosity: u8,
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+pub struct NodeArgs {
+    #[command(flatten)]
+    pub runner: RunnerArgs,
+    #[command(flatten)]
+    pub user: UserArgs,
+}
