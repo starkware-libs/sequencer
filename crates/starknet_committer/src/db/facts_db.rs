5123923c7050e0005b72891e01145785d1d27a3e
Yoav Gross <yoav@starkware.co>
2025-12-08 15:26:16 +0200

    starknet_committer: impl metadata for FactsDb

--- crates/starknet_committer/src/db/facts_db.rs
+++ crates/starknet_committer/src/db/facts_db.rs
@@ -6,7 +6,7 @@ use starknet_patricia::patricia_merkle_tree::node_data::leaf::LeafModifications;
 use starknet_patricia::patricia_merkle_tree::original_skeleton_tree::tree::OriginalSkeletonTreeImpl;
 use starknet_patricia::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices};
 use starknet_patricia_storage::map_storage::MapStorage;
-use starknet_patricia_storage::storage_trait::Storage;
+use starknet_patricia_storage::storage_trait::{create_db_key, DbKey, DbKeyPrefix, Storage};
 
 use crate::block_committer::input::{
     contract_address_into_node_index,
@@ -18,7 +18,7 @@ use crate::db::create_facts_tree::{
     create_original_skeleton_tree,
     create_original_skeleton_tree_and_get_previous_leaves,
 };
-use crate::db::forest_trait::{ForestReader, ForestWriter};
+use crate::db::forest_trait::{ForestMetadata, ForestMetadataType, ForestReader, ForestWriter};
 use crate::forest::filled_forest::FilledForest;
 use crate::forest::forest_errors::{ForestError, ForestResult};
 use crate::forest::original_skeleton_forest::{ForestSortedIndices, OriginalSkeletonForest};
@@ -37,6 +37,9 @@ pub struct FactsDb<S: Storage> {
 }
 
 impl<S: Storage> FactsDb<S> {
+    pub const COMMITMENT_OFFSET_KEY: &[u8; 17] = b"commitment_offset";
+    pub const STATE_DIFF_HASH_PREFIX: &[u8; 15] = b"state_diff_hash";
+
     pub fn new(storage: S) -> Self {
         Self { storage }
     }
@@ -161,5 +164,20 @@ impl<'a, S: Storage> ForestReader<'a> for FactsDb<S> {
 impl<S: Storage> ForestWriter for FactsDb<S> {
     async fn write(&mut self, filled_forest: &FilledForest) -> usize {
         filled_forest.write_to_storage(&mut self.storage).await
+    }
+}
+
+impl<S: Storage> ForestMetadata for FactsDb<S> {
+    /// Returns the db key for the metadata type.
+    /// The data keys in a facts DB are the result of a hash function; therefore, they are not
+    /// expected to collide with these metadata keys.
+    fn metadata_key(metadata_type: ForestMetadataType) -> DbKey {
+        match metadata_type {
+            ForestMetadataType::CommitmentOffset => DbKey(Self::COMMITMENT_OFFSET_KEY.to_vec()),
+            ForestMetadataType::StateDiffHash(block_number) => {
+                let state_diff_hash_key_prefix = DbKeyPrefix::new(Self::STATE_DIFF_HASH_PREFIX);
+                create_db_key(state_diff_hash_key_prefix, &block_number.0.to_be_bytes())
+            }
+        }
     }
 }
