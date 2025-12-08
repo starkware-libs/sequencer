2b870f22cced2a6b36127fc46399b2ee82a956f3
Guy Nir <guy.n@starkware.co>
2025-12-08 11:45:36 +0200

    papyrus_base_layer: extract check and cycle URLs into a helper function with logging

--- crates/papyrus_base_layer/src/cyclic_base_layer_wrapper.rs
+++ crates/papyrus_base_layer/src/cyclic_base_layer_wrapper.rs
@@ -2,6 +2,7 @@ use std::ops::RangeInclusive;
 
 use async_trait::async_trait;
 use starknet_api::block::BlockHashAndNumber;
+use tracing::info;
 use url::Url;
 
 use crate::{BaseLayerContract, L1BlockHeader, L1BlockNumber, L1BlockReference, L1Event};
@@ -18,6 +19,53 @@ impl<B: BaseLayerContract + Send + Sync> CyclicBaseLayerWrapper<B> {
     pub fn new(base_layer: B) -> Self {
         Self { base_layer }
     }
+
+    // Check the result of a function call to the base layer. If it fails, cycle the URL and signal
+    // the caller that we should try again (by returning None).
+    async fn cycle_url_on_error<ReturnType>(
+        &mut self,
+        start_url: &Url,
+        result: Result<ReturnType, B::Error>,
+    ) -> Option<Result<ReturnType, B::Error>> {
+        // In case we succeed, just return the (successful) result.
+        if result.is_ok() {
+            return Some(result);
+        }
+        // Get the current URL (return error in case it fails to get it).
+        let current_url = match self.base_layer.get_url().await {
+            Ok(url) => url,
+            Err(e) => return Some(Err(e)),
+        };
+        // Otherwise, cycle the URL so we can try again. Return error in case it fails to cycle.
+        match self.base_layer.cycle_provider_url().await {
+            Ok(()) => (),
+            Err(e) => return Some(Err(e)),
+        };
+        // Get the new URL (return error in case it fails to get it).
+        let new_url = match self.base_layer.get_url().await {
+            Ok(url) => url,
+            Err(e) => return Some(Err(e)),
+        };
+        info!(
+            "Cycling URL from {:?} to {:?}",
+            to_safe_string(&current_url),
+            to_safe_string(&new_url)
+        );
+
+        // If we've cycled back to the start URL, we need to return the last error we got.
+        if &new_url == start_url {
+            let error_value = result.err().expect("result is checked at start of function");
+            info!(
+                "Cycled back to start URL {:?}, returning error {:?}.",
+                to_safe_string(start_url),
+                error_value
+            );
+            return Some(Err(error_value));
+        }
+        // If we cycled but still haven't reached the start URL, we return None to signal that we
+        // should try again with the new URL.
+        None
+    }
 }
 
 #[async_trait]
@@ -31,12 +79,8 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
         let start_url = self.base_layer.get_url().await?;
         loop {
             let result = self.base_layer.get_proved_block_at(l1_block).await;
-            if result.is_ok() {
-                return result;
-            }
-            self.base_layer.cycle_provider_url().await?;
-            if self.base_layer.get_url().await? == start_url {
-                return result;
+            if let Some(result) = self.cycle_url_on_error(&start_url, result).await {
+                return result; // Could return a success or an error. 
             }
         }
     }
@@ -45,12 +89,8 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
         let start_url = self.base_layer.get_url().await?;
         loop {
             let result = self.base_layer.latest_l1_block_number().await;
-            if result.is_ok() {
-                return result;
-            }
-            self.base_layer.cycle_provider_url().await?;
-            if self.base_layer.get_url().await? == start_url {
-                return result;
+            if let Some(result) = self.cycle_url_on_error(&start_url, result).await {
+                return result; // Could return a success or an error. 
             }
         }
     }
@@ -62,12 +102,8 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
         let start_url = self.base_layer.get_url().await?;
         loop {
             let result = self.base_layer.l1_block_at(block_number).await;
-            if result.is_ok() {
-                return result;
-            }
-            self.base_layer.cycle_provider_url().await?;
-            if self.base_layer.get_url().await? == start_url {
-                return result;
+            if let Some(result) = self.cycle_url_on_error(&start_url, result).await {
+                return result; // Could return a success or an error. 
             }
         }
     }
@@ -80,12 +116,8 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
         let start_url = self.base_layer.get_url().await?;
         loop {
             let result = self.base_layer.events(block_range.clone(), event_identifiers).await;
-            if result.is_ok() {
-                return result;
-            }
-            self.base_layer.cycle_provider_url().await?;
-            if self.base_layer.get_url().await? == start_url {
-                return result;
+            if let Some(result) = self.cycle_url_on_error(&start_url, result).await {
+                return result; // Could return a success or an error. 
             }
         }
     }
@@ -97,12 +129,8 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
         let start_url = self.base_layer.get_url().await?;
         loop {
             let result = self.base_layer.get_block_header(block_number).await;
-            if result.is_ok() {
-                return result;
-            }
-            self.base_layer.cycle_provider_url().await?;
-            if self.base_layer.get_url().await? == start_url {
-                return result;
+            if let Some(result) = self.cycle_url_on_error(&start_url, result).await {
+                return result; // Could return a success or an error. 
             }
         }
     }
@@ -125,4 +153,9 @@ impl<B: BaseLayerContract + Send + Sync> BaseLayerContract for CyclicBaseLayerWr
     async fn cycle_provider_url(&mut self) -> Result<(), Self::Error> {
         self.base_layer.cycle_provider_url().await
     }
+}
+
+fn to_safe_string(url: &Url) -> String {
+    // We print only the hostnames to avoid leaking the API keys.
+    url.host().map_or_else(|| "no host in url!".to_string(), |host| host.to_string())
 }
