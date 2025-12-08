f5f2f273bb8b91b508b5d3104aa1c61e85c78ce3
Yoav Gross <yoav@starkware.co>
2025-12-08 13:47:08 +0200

    starknet_committer: define trait ForestMetadata

--- crates/starknet_committer/src/db/forest_trait.rs
+++ crates/starknet_committer/src/db/forest_trait.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 use std::future::Future;
 
+use starknet_api::block::BlockNumber;
 use starknet_api::core::ContractAddress;
 use starknet_api::hash::HashOutput;
 use starknet_patricia::patricia_merkle_tree::node_data::leaf::LeafModifications;
 use starknet_patricia::patricia_merkle_tree::types::NodeIndex;
+use starknet_patricia_storage::storage_trait::{DbHashMap, DbKey, DbValue, Storage};
 
 use crate::block_committer::input::{ConfigImpl, StarknetStorageValue};
 use crate::forest::filled_forest::FilledForest;
@@ -12,6 +14,38 @@ use crate::forest::forest_errors::ForestResult;
 use crate::forest::original_skeleton_forest::{ForestSortedIndices, OriginalSkeletonForest};
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
 use crate::patricia_merkle_tree::types::CompiledClassHash;
+
+pub enum ForestMetadataType {
+    CommitmentOffset,
+    StateDiffHash(BlockNumber),
+}
+
+pub trait ForestMetadata {
+    /// Returns the db key for the metadata type.
+    fn metadata_key(metadata_type: ForestMetadataType) -> DbKey;
+
+    /// Reads the metadata from the storage.
+    fn read_metadata(
+        &self,
+        storage: &mut impl Storage,
+        metadata_type: ForestMetadataType,
+    ) -> impl Future<Output = ForestResult<Option<DbValue>>> + Send {
+        async {
+            let db_key = Self::metadata_key(metadata_type);
+            Ok(storage.get(&db_key).await?)
+        }
+    }
+
+    /// Sets the metadata in the storage. Returns the previous value if it existed.
+    fn set_metadata(
+        updates: &mut DbHashMap,
+        metadata_type: ForestMetadataType,
+        value: DbValue,
+    ) -> Option<DbValue> {
+        let db_key = Self::metadata_key(metadata_type);
+        updates.insert(db_key, value)
+    }
+}
 
 /// Trait for reading an original skeleton forest from some storage.
 /// The implementation may depend on the underlying storage layout.
