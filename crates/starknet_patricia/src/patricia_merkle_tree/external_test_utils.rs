6fc0a09a2b2a5a2bbb291c97d52d5e21038e41b9
Ariel Elperin <ariel@starkware.co>
2025-12-09 15:06:41 +0200

    starknet_committer,starknet_patricia: remove generic config

--- crates/starknet_patricia/src/patricia_merkle_tree/external_test_utils.rs
+++ crates/starknet_patricia/src/patricia_merkle_tree/external_test_utils.rs
@@ -21,10 +21,8 @@ use super::node_data::leaf::Leaf;
 use super::original_skeleton_tree::node::OriginalSkeletonNode;
 use super::types::{NodeIndex, SubTreeHeight};
 use crate::felt::u256_from_felt;
-use crate::generate_trie_config;
 use crate::patricia_merkle_tree::errors::TypesError;
 use crate::patricia_merkle_tree::node_data::errors::{LeafError, LeafResult};
-use crate::patricia_merkle_tree::original_skeleton_tree::config::OriginalSkeletonTreeConfig;
 
 pub(crate) const TEST_PREFIX: &[u8] = &[0];
 
@@ -69,8 +67,6 @@ impl Leaf for MockLeaf {
         Ok((Self(input), input.to_hex_string()))
     }
 }
-
-generate_trie_config!(OriginalSkeletonMockTrieConfig, MockLeaf);
 
 pub fn u256_try_into_felt(value: &U256) -> Result<Felt, TypesError<U256>> {
     if *value > u256_from_felt(&Felt::MAX) {
