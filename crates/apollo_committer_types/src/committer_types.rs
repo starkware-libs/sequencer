eca00433ca3502bbeb3e344a058620101ad28c5f
Yoav Gross <yoav@starkware.co>
2025-12-09 16:41:31 +0200

    apollo_committer_types: ThinStateDiff type in the request

--- crates/apollo_committer_types/src/committer_types.rs
+++ crates/apollo_committer_types/src/committer_types.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 use starknet_api::block::BlockNumber;
 use starknet_api::core::{GlobalRoot, StateDiffCommitment};
-use starknet_committer::block_committer::input::StateDiff;
+use starknet_api::state::ThinStateDiff;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CommitBlockRequest {
-    state_diff: StateDiff,
+    state_diff: ThinStateDiff,
     state_diff_commitment: StateDiffCommitment,
     height: BlockNumber,
 }
@@ -18,7 +18,7 @@ pub struct CommitBlockResponse {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct RevertBlockRequest {
     // A synthetic state diff that undoes the state diff of the given height.
-    reversed_state_diff: StateDiff,
+    reversed_state_diff: ThinStateDiff,
     height: BlockNumber,
 }
 
