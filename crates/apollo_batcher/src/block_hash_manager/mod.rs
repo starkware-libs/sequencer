e518b31037b0d244f3a7a2c3cf194d0d3d586a97
Nimrod Weiss <nimrod@starkware.co>
2025-12-09 15:55:08 +0200

    apollo_batcher: define BHM and BH calculator

--- crates/apollo_batcher/src/block_hash_manager/mod.rs
+++ crates/apollo_batcher/src/block_hash_manager/mod.rs
@@ -1,2 +1,17 @@
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::task::JoinHandle;
+
+use crate::block_hash_manager::input_types::CommitmentTaskInput;
+use crate::block_hash_manager::output_types::CommitmentTaskOutput;
+
+pub(crate) mod block_hash_calculator;
 pub(crate) mod input_types;
 pub(crate) mod output_types;
+
+#[allow(dead_code)]
+/// Manages block hash calculation tasks.
+pub(crate) struct BlockHashManager {
+    pub(crate) tasks_sender: Sender<CommitmentTaskInput>,
+    pub(crate) results_receiver: Receiver<CommitmentTaskOutput>,
+    pub(crate) handle: JoinHandle<()>,
+}
