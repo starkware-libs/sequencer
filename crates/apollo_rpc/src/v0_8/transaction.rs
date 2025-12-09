e225a3624c906e8e45c57223974521227bdb0d44
Aviv Greenburg <aviv.g@starkware.co>
2025-12-09 13:07:05 +0200

    apollo_protobuf: add proof facts to invoke v3

--- crates/apollo_rpc/src/v0_8/transaction.rs
+++ crates/apollo_rpc/src/v0_8/transaction.rs
@@ -467,6 +467,7 @@ impl From<InvokeTransactionV3> for client_transaction::InvokeTransaction {
             paymaster_data: tx.paymaster_data,
             account_deployment_data: tx.account_deployment_data,
             r#type: client_transaction::InvokeType::Invoke,
+            proof_facts: tx.proof_facts,
         })
     }
 }
