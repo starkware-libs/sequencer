d2d0d5d3725ab7a156d8e620f24feb169d297d24
Ariel Elperin <ariel@starkware.co>
2025-12-09 15:06:41 +0200

    starknet_committer,starknet_patricia: remove generic config

--- crates/starknet_committer/src/db/facts_db/db.rs
+++ crates/starknet_committer/src/db/facts_db/db.rs
@@ -36,11 +36,7 @@ use crate::forest::filled_forest::FilledForest;
 use crate::forest::forest_errors::{ForestError, ForestResult};
 use crate::forest::original_skeleton_forest::{ForestSortedIndices, OriginalSkeletonForest};
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
-use crate::patricia_merkle_tree::tree::{
-    OriginalSkeletonClassesTrieConfig,
-    OriginalSkeletonContractsTrieConfig,
-    OriginalSkeletonStorageTrieConfig,
-};
+use crate::patricia_merkle_tree::tree::OriginalSkeletonTrieConfig;
 use crate::patricia_merkle_tree::types::CompiledClassHash;
 
 /// Facts DB node layout.
@@ -85,7 +81,7 @@ impl<S: Storage> FactsDb<S> {
             contracts_trie_root_hash,
             contracts_trie_sorted_indices,
             &HashMap::new(),
-            &OriginalSkeletonContractsTrieConfig::new(),
+            &OriginalSkeletonTrieConfig::new(false),
             &EmptyKeyContext,
         )
         .await?)
@@ -106,8 +102,7 @@ impl<S: Storage> FactsDb<S> {
             let contract_state = original_contracts_trie_leaves
                 .get(&contract_address_into_node_index(address))
                 .ok_or(ForestError::MissingContractCurrentState(*address))?;
-            let config =
-                OriginalSkeletonStorageTrieConfig::new(config.warn_on_trivial_modifications());
+            let config = OriginalSkeletonTrieConfig::new(config.warn_on_trivial_modifications());
 
             let original_skeleton = create_original_skeleton_tree(
                 &mut self.storage,
@@ -130,7 +125,7 @@ impl<S: Storage> FactsDb<S> {
         config: &ReaderConfig,
         contracts_trie_sorted_indices: SortedLeafIndices<'a>,
     ) -> ForestResult<OriginalSkeletonTreeImpl<'a>> {
-        let config = OriginalSkeletonClassesTrieConfig::new(config.warn_on_trivial_modifications());
+        let config = OriginalSkeletonTrieConfig::new(config.warn_on_trivial_modifications());
 
         Ok(create_original_skeleton_tree(
             &mut self.storage,
