3ca5e79a7e921c37a2fc8b29c8cd5d9cb049ca09
Ariel Elperin <ariel@starkware.co>
2025-12-10 12:24:48 +0200

    starknet_committer,starknet_patricia: add subtree trait

---
+++ crates/starknet_committer/src/db/facts_db/types.rs
@@ -0,0 +1,45 @@
+use starknet_api::hash::HashOutput;
+use starknet_patricia::patricia_merkle_tree::filled_tree::node_serde::PatriciaPrefix;
+use starknet_patricia::patricia_merkle_tree::node_data::leaf::Leaf;
+use starknet_patricia::patricia_merkle_tree::traversal::SubTreeTrait;
+use starknet_patricia::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices};
+use starknet_patricia_storage::storage_trait::DbKeyPrefix;
+
+#[derive(Debug, PartialEq)]
+pub struct FactsSubTree<'a> {
+    sorted_leaf_indices: SortedLeafIndices<'a>,
+    pub root_index: NodeIndex,
+    pub root_hash: HashOutput,
+}
+
+impl<'a> SubTreeTrait<'a> for FactsSubTree<'a> {
+    type NodeData = HashOutput;
+
+    fn create(
+        sorted_leaf_indices: SortedLeafIndices<'a>,
+        root_index: NodeIndex,
+        child_data: Self::NodeData,
+    ) -> Self {
+        Self { sorted_leaf_indices, root_index, root_hash: child_data }
+    }
+
+    fn get_root_index(&self) -> NodeIndex {
+        self.root_index
+    }
+
+    fn get_sorted_leaf_indices(&self) -> &SortedLeafIndices<'a> {
+        &self.sorted_leaf_indices
+    }
+
+    fn should_traverse_unmodified_children() -> bool {
+        false
+    }
+
+    fn get_root_prefix<L: Leaf>(&self) -> DbKeyPrefix {
+        if self.is_leaf() {
+            PatriciaPrefix::Leaf(L::get_static_prefix()).into()
+        } else {
+            PatriciaPrefix::InnerNode.into()
+        }
+    }
+}
