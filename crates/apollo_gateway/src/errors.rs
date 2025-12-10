7b91eb9b62efc77e358aefe5c9af436ada668bf6
Aviv Yossef <aviv.y@starkware.co>
2025-12-10 14:11:32 +0200

    blockifier_reexecution: allow custom chain id

--- crates/apollo_gateway/src/errors.rs
+++ crates/apollo_gateway/src/errors.rs
@@ -436,7 +436,8 @@ fn convert_sn_api_error(err: StarknetApiError) -> StarknetError {
         | StarknetApiError::BlockHashVersion { .. }
         | StarknetApiError::ParseSierraVersionError(..)
         | StarknetApiError::ResourceHexToFeltConversion(..)
-        | StarknetApiError::OutOfRange { .. } => StarknetError {
+        | StarknetApiError::OutOfRange { .. }
+        | StarknetApiError::InvalidChainIdHex(..) => StarknetError {
             code: StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::MalformedRequest),
             message: err.to_string(),
         },
