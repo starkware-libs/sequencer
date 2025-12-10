1b5ee01a158007bb488371d4c53f1dcf34a5b5e9
Ariel Elperin <ariel@starkware.co>
2025-12-10 12:24:48 +0200

    starknet_committer,starknet_patricia: add subtree trait

---
+++ crates/starknet_committer/src/db/facts_db/types.rs
@@ -0,0 +1,46 @@
+use starknet_api::hash::HashOutput;
+use starknet_patricia::patricia_merkle_tree::filled_tree::node_serde::PatriciaPrefix;
+use starknet_patricia::patricia_merkle_tree::node_data::leaf::Leaf;
+use starknet_patricia::patricia_merkle_tree::traversal::SubTreeTrait;
+use starknet_patricia::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices};
+
+#[derive(Debug, PartialEq)]
+pub struct FactsSubTree<'a> {
+    pub sorted_leaf_indices: SortedLeafIndices<'a>,
+    pub root_index: NodeIndex,
+    pub root_hash: HashOutput,
+}
+
+impl<'a> SubTreeTrait<'a> for FactsSubTree<'a> {
+    type ChildData = HashOutput;
+
+    fn create_child(
+        sorted_leaf_indices: SortedLeafIndices<'a>,
+        root_index: NodeIndex,
+        child_data: Self::ChildData,
+    ) -> Self {
+        Self { sorted_leaf_indices, root_index, root_hash: child_data }
+    }
+
+    fn get_root_index(&self) -> NodeIndex {
+        self.root_index
+    }
+
+    fn get_sorted_leaf_indices(&self) -> &SortedLeafIndices<'a> {
+        &self.sorted_leaf_indices
+    }
+
+    fn should_traverse_unmodified_children() -> bool {
+        false
+    }
+}
+
+impl<'a> FactsSubTree<'a> {
+    pub fn get_root_prefix<L: Leaf>(&self) -> PatriciaPrefix {
+        if self.is_leaf() {
+            PatriciaPrefix::Leaf(L::get_static_prefix())
+        } else {
+            PatriciaPrefix::InnerNode
+        }
+    }
+}
--- crates/starknet_patricia/src/patricia_merkle_tree/traversal.rs
+++ crates/starknet_patricia/src/patricia_merkle_tree/traversal.rs
@@ -1,11 +1,8 @@
-use starknet_api::hash::HashOutput;
 use starknet_patricia_storage::errors::{DeserializationError, StorageError};
 use starknet_patricia_storage::storage_trait::PatriciaStorageError;
 use thiserror::Error;
 
-use crate::patricia_merkle_tree::filled_tree::node_serde::PatriciaPrefix;
 use crate::patricia_merkle_tree::node_data::inner_node::PathToBottom;
-use crate::patricia_merkle_tree::node_data::leaf::Leaf;
 use crate::patricia_merkle_tree::original_skeleton_tree::utils::split_leaves;
 use crate::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices, SubTreeHeight};
 
@@ -25,88 +22,83 @@ pub enum TraversalError {
 
 pub type TraversalResult<T> = Result<T, TraversalError>;
 
-#[derive(Debug, PartialEq)]
-pub struct SubTree<'a> {
-    pub sorted_leaf_indices: SortedLeafIndices<'a>,
-    pub root_index: NodeIndex,
-    pub root_hash: HashOutput,
-}
+// The SubTreeTrait allows traversing a trie without knowledge of the concrete node types and data.
+pub trait SubTreeTrait<'a>: Sized {
+    // A node can carry data about its children (e.g. their hashes).
+    type ChildData: Copy;
 
-impl<'a> SubTree<'a> {
-    pub(crate) fn get_height(&self) -> SubTreeHeight {
-        SubTreeHeight::new(SubTreeHeight::ACTUAL_HEIGHT.0 - (self.root_index.bit_length() - 1))
+    // Creates a concrete child node given its index and data.
+    fn create_child(
+        sorted_leaf_indices: SortedLeafIndices<'a>,
+        root_index: NodeIndex,
+        child_data: Self::ChildData,
+    ) -> Self;
+
+    fn get_root_index(&self) -> NodeIndex;
+
+    fn get_sorted_leaf_indices(&self) -> &SortedLeafIndices<'a>;
+
+    fn get_height(&self) -> SubTreeHeight {
+        SubTreeHeight::new(
+            SubTreeHeight::ACTUAL_HEIGHT.0 - (self.get_root_index().bit_length() - 1),
+        )
     }
 
-    pub(crate) fn split_leaves(&self) -> [SortedLeafIndices<'a>; 2] {
-        split_leaves(&self.root_index, &self.sorted_leaf_indices)
+    fn split_leaves(&self) -> [SortedLeafIndices<'a>; 2] {
+        split_leaves(&self.get_root_index(), self.get_sorted_leaf_indices())
     }
 
-    pub fn is_unmodified(&self) -> bool {
-        self.sorted_leaf_indices.is_empty()
-    }
-
-    pub fn get_root_prefix<L: Leaf>(&self) -> PatriciaPrefix {
-        if self.is_leaf() {
-            PatriciaPrefix::Leaf(L::get_static_prefix())
-        } else {
-            PatriciaPrefix::InnerNode
-        }
+    fn is_unmodified(&self) -> bool {
+        self.get_sorted_leaf_indices().is_empty()
     }
 
     /// Returns the bottom subtree which is referred from `self` by the given path. When creating
     /// the bottom subtree some indices that were modified under `self` are not modified under the
     /// bottom subtree (leaves that were previously empty). These indices are returned as well.
-    pub fn get_bottom_subtree(
+    fn get_bottom_subtree(
         &self,
         path_to_bottom: &PathToBottom,
-        bottom_hash: HashOutput,
-    ) -> (Self, Vec<&NodeIndex>) {
-        let bottom_index = path_to_bottom.bottom_index(self.root_index);
+        bottom_data: Self::ChildData,
+    ) -> (Self, Vec<NodeIndex>) {
+        let sorted_leaf_indices = self.get_sorted_leaf_indices();
+        let bottom_index = path_to_bottom.bottom_index(self.get_root_index());
         let bottom_height = self.get_height() - SubTreeHeight::new(path_to_bottom.length.into());
         let leftmost_in_subtree = bottom_index << bottom_height.into();
         let rightmost_in_subtree =
             leftmost_in_subtree - NodeIndex::ROOT + (NodeIndex::ROOT << bottom_height.into());
-        let leftmost_index = self.sorted_leaf_indices.bisect_left(&leftmost_in_subtree);
-        let rightmost_index = self.sorted_leaf_indices.bisect_right(&rightmost_in_subtree);
-        let bottom_leaves = self.sorted_leaf_indices.subslice(leftmost_index, rightmost_index);
-        let previously_empty_leaf_indices = self.sorted_leaf_indices.get_indices()
-            [..leftmost_index]
+        let leftmost_index = sorted_leaf_indices.bisect_left(&leftmost_in_subtree);
+        let rightmost_index = sorted_leaf_indices.bisect_right(&rightmost_in_subtree);
+        let bottom_leaves = sorted_leaf_indices.subslice(leftmost_index, rightmost_index);
+        let previously_empty_leaf_indices = sorted_leaf_indices.get_indices()[..leftmost_index]
             .iter()
-            .chain(self.sorted_leaf_indices.get_indices()[rightmost_index..].iter())
+            .chain(sorted_leaf_indices.get_indices()[rightmost_index..].iter())
+            .cloned()
             .collect();
 
         (
-            Self {
-                sorted_leaf_indices: bottom_leaves,
-                root_index: bottom_index,
-                root_hash: bottom_hash,
-            },
+            Self::create_child(bottom_leaves, bottom_index, bottom_data),
             previously_empty_leaf_indices,
         )
     }
 
-    pub fn get_children_subtrees(
+    fn get_children_subtrees(
         &self,
-        left_hash: HashOutput,
-        right_hash: HashOutput,
+        left_data: Self::ChildData,
+        right_data: Self::ChildData,
     ) -> (Self, Self) {
         let [left_leaves, right_leaves] = self.split_leaves();
-        let left_root_index = self.root_index * 2.into();
+        let left_root_index = self.get_root_index() * 2.into();
         (
-            SubTree {
-                sorted_leaf_indices: left_leaves,
-                root_index: left_root_index,
-                root_hash: left_hash,
-            },
-            SubTree {
-                sorted_leaf_indices: right_leaves,
-                root_index: left_root_index + NodeIndex::ROOT,
-                root_hash: right_hash,
-            },
+            Self::create_child(left_leaves, left_root_index, left_data),
+            Self::create_child(right_leaves, left_root_index + NodeIndex::ROOT, right_data),
         )
     }
 
-    pub fn is_leaf(&self) -> bool {
-        self.root_index.is_leaf()
+    fn is_leaf(&self) -> bool {
+        self.get_root_index().is_leaf()
     }
+
+    // Indicates whether unmodified children should be traversed during the construction of the
+    // skeleton tree.
+    fn should_traverse_unmodified_children() -> bool;
 }
