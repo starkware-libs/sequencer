e32acfd103ea546d4aa2b6f8ce28e5493a2468f9
Aviv Greenburg <aviv.g@starkware.co>
2025-12-10 18:01:54 +0200

    apollo rpc: add proof to invoke v3

--- crates/apollo_rpc/src/v0_8/api/mod.rs
+++ crates/apollo_rpc/src/v0_8/api/mod.rs
@@ -620,6 +620,7 @@ impl From<InvokeTransaction> for starknet_api::transaction::InvokeTransaction {
                 nonce_data_availability_mode,
                 fee_data_availability_mode,
                 proof_facts,
+                proof: _,
             }) => Self::V3(starknet_api::transaction::InvokeTransactionV3 {
                 resource_bounds: resource_bounds.into(),
                 tip,
