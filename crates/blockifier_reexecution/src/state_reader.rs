32b4a67d16b7208269f4a7b050612600854f0f9f
Aviv Yossef <aviv.y@starkware.co>
2025-12-10 17:37:33 +0200

    blockifier_reexecution: rename test state reader

--- crates/blockifier_reexecution/src/state_reader.rs
+++ crates/blockifier_reexecution/src/state_reader.rs
@@ -9,6 +9,6 @@ pub mod reexecution_state_reader;
 pub mod reexecution_test;
 #[cfg(all(test, feature = "blockifier_regression_https_testing"))]
 pub mod rpc_https_test;
+pub mod rpc_state_reader;
 pub mod serde_utils;
-pub mod test_state_reader;
 pub mod utils;
