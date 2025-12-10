6cad50ab0d31512f28edb63e31f113f4675a7cfb
guy-starkware <guy.n@starkware.co>
2025-12-10 17:28:49 +0200

    apollo_l1_provider: improve add_tx and prevent double scraping (#10508)
    * apollo_l1_provider: add l1_block_time as a config parameter
    
    * apollo_l1_provider: improve add_tx and prevent double scraping

--- crates/apollo_l1_provider/src/transaction_manager.rs
+++ crates/apollo_l1_provider/src/transaction_manager.rs
@@ -7,7 +7,7 @@ use apollo_l1_provider_types::{InvalidValidationStatus, ValidationStatus};
 use starknet_api::block::{BlockTimestamp, UnixTimestamp};
 use starknet_api::executable_transaction::L1HandlerTransaction;
 use starknet_api::transaction::TransactionHash;
-use tracing::debug;
+use tracing::{debug, info, warn};
 
 use crate::transaction_record::{
     Records,
@@ -161,14 +161,34 @@ impl TransactionManager {
         tx: L1HandlerTransaction,
         block_timestamp: BlockTimestamp,
         scrape_timestamp: UnixTimestamp,
-    ) -> bool {
+    ) {
         let tx_hash = tx.tx_hash;
+        // If exists, return false and do nothing. If not, create the record as a HashOnly payload.
         let is_new_record = self.create_record_if_not_exist(tx_hash);
-        self.with_record(tx_hash, move |record| {
-            record.tx.set(tx, block_timestamp, scrape_timestamp);
+        // Replace a HashOnly payload with a Full payload. Do not update a Full payload.
+        // A hash only payload can come from bootstrapping from state sync, and then updated by
+        // add_events from the scraper. However, if we get the same full tx twice (from the scraper)
+        // it could indicate a double-scrape, and may cause the tx to be re-added to the proposable
+        // index.
+        self.with_record(tx_hash, move |record| match &record.tx {
+            TransactionPayload::HashOnly(_) => {
+                if !is_new_record {
+                    info!(
+                        "Transaction {tx_hash} already exists as a HashOnly payload. It was \
+                         probably gotten via state sync component, and is now updated with a Full \
+                         payload."
+                    );
+                }
+                record.tx.set(tx, block_timestamp, scrape_timestamp);
+            }
+            TransactionPayload::Full { tx: _, created_at_block_timestamp: _, scrape_timestamp } => {
+                warn!(
+                    "Transaction {tx_hash} already exists as a Full payload, scraped at \
+                     {scrape_timestamp}. This could indicate a double scrape. Ignoring the new \
+                     transaction."
+                );
+            }
         });
-
-        is_new_record
     }
 
     pub fn request_cancellation(
