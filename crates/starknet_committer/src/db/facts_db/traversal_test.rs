35d2ba673a98d950d4579427ef3defa1fef73be4
rotem-starkware <rotem@starkware.co>
2025-12-11 15:36:04 +0200

    starknet_committer: add binary nodes their parents are edge nodes to fetch_patricia_paths_inner

--- crates/starknet_committer/src/db/facts_db/traversal_test.rs
+++ crates/starknet_committer/src/db/facts_db/traversal_test.rs
@@ -90,7 +90,14 @@ async fn test_fetch_patricia_paths_inner_impl(
     .await
     .unwrap();
 
-    assert_eq!(nodes, expected_nodes);
+    // The python facts-fetching logic is more accurate, so we only check that the expected nodes
+    // are a subset of the fetched nodes.
+    for (key, value) in expected_nodes.iter() {
+        match nodes.get(key) {
+            Some(node_value) => assert_eq!(node_value, value, "Value mismatch for key {key:?}"),
+            None => panic!("Expected key {key:?} is in expected nodes but missing in nodes"),
+        }
+    }
     assert_eq!(fetched_leaves, expected_fetched_leaves);
 }
 
@@ -255,7 +262,7 @@ async fn test_fetch_patricia_paths_inner_impl(
 )]
 /// Modified leaf indices: `[14]`
 /// Expected nodes hashes: `[62, 44, 29]`
-/// Siblings hashes (in preimage of nodes): `[18, 15 (inner node), 15 (leaf)]`
+/// Siblings hashes (in preimage of nodes): `[18, 15(node), 15(leaf)]`
 #[case::edge_one_leaf_binary(
     MapStorage(DbHashMap::from([
         create_binary_entry_from_u128::<AdditionHash>(8, 9),
@@ -362,7 +369,9 @@ async fn test_fetch_patricia_paths_inner_impl(
 ///    8  9 10 11 12
 ///   new new    new
 /// ```
-/// Expected nodes hashes: `[24]`
+/// Modified leaf indices: `[8, 9, 12]`
+/// Expected nodes hashes: `[24, 21]`
+/// 21 is a binary node that its parent is an edge node, so it should be fetched.
 /// Siblings hashes (in preimage of nodes): `[21]`
 #[case::should_return_empty_leaves(
     MapStorage(DbHashMap::from([
@@ -377,6 +386,52 @@ async fn test_fetch_patricia_paths_inner_impl(
     // edge: [length, path, bottom]
     to_preimage_map(HashMap::from([
         (24, vec![2, 1, 21]),
+        (21, vec![10, 11]),
+    ])),
+)]
+/// Test new tree with deleted leaves.
+/// Old SubTree structure:
+/// ```text
+///            58
+///         /     \
+///        38     20
+///       /  \     \
+///     17   21    *
+///     / \  / \    \
+///    8  9 10 11   15
+/// ```
+/// /// New SubTree structure:
+/// ```text
+///              38
+///            /   \
+///          18    20
+///          /      \
+///         17       *
+///        / \       \
+///       8  9      15
+/// ```
+/// Modified leaf indices: `[10, 11]`
+/// Expected nodes hashes: `[38, 18, 17]`
+/// 17 is a binary node that its parent is an edge node, so it should be fetched.
+/// Siblings hashes (in preimage of nodes): `[20, 17]`
+#[case::binary_child_of_edge(
+    MapStorage(DbHashMap::from([
+        create_binary_entry_from_u128::<AdditionHash>(8, 9),
+        create_edge_entry_from_u128::<AdditionHash>(17, 0, 1),
+        create_edge_entry_from_u128::<AdditionHash>(15, 3, 2),
+        create_binary_entry_from_u128::<AdditionHash>(18, 20),
+        create_leaf_entry::<MockLeaf>(8),
+        create_leaf_entry::<MockLeaf>(9),
+        create_leaf_entry::<MockLeaf>(15),
+    ])),
+    HashOutput(Felt::from(38_u128)),
+    vec![10, 11],
+    SubTreeHeight::new(3),
+    // edge: [length, path, bottom]
+    to_preimage_map(HashMap::from([
+        (38, vec![18, 20]),
+        (18, vec![1, 0, 17]),
+        (17, vec![8, 9]),
     ])),
 )]
 /// Python generated cases.
@@ -500,10 +555,10 @@ async fn test_fetch_patricia_paths_inner(
 
 #[derive(Deserialize, Debug)]
 struct TestPatriciaPathsInput {
-    initial_preimages: HashMap<Felt, Vec<Felt>>,
+    facts_storage: HashMap<Felt, Vec<Felt>>,
     initial_leaves: Vec<u128>,
     root_hash: Felt,
-    leaf_indices: Vec<u128>,
+    modified_leaves_indices: Vec<u128>,
     height: u8,
     expected_nodes: HashMap<Felt, Vec<Felt>>,
 }
@@ -518,6 +573,10 @@ struct TestPatriciaPathsInput {
 #[case(include_str!("../../../resources/fetch_patricia_paths_test_10_5_2.json"))]
 #[case(include_str!("../../../resources/fetch_patricia_paths_test_10_100_30.json"))]
 #[case(include_str!("../../../resources/fetch_patricia_paths_test_8_120_70.json"))]
+#[case(include_str!("../../../resources/fetch_patricia_paths_test_delete_leaves_10_200_50.json"))]
+#[case(include_str!("../../../resources/fetch_patricia_paths_test_delete_leaves_10_5_2.json"))]
+#[case(include_str!("../../../resources/fetch_patricia_paths_test_delete_leaves_10_100_30.json"))]
+#[case(include_str!("../../../resources/fetch_patricia_paths_test_delete_leaves_8_120_70.json"))]
 async fn test_fetch_patricia_paths_inner_from_json(#[case] input_data: &str) {
     let input: TestPatriciaPathsInput = serde_json::from_str(input_data)
         .unwrap_or_else(|error| panic!("JSON was not well-formatted: {error:?}"));
@@ -525,7 +584,7 @@ async fn test_fetch_patricia_paths_inner_from_json(#[case] input_data: &str) {
     let first_leaf = 2u128.pow(u32::from(input.height));
 
     let storage: HashMap<DbKey, DbValue> = input
-        .initial_preimages
+        .facts_storage
         .values()
         .map(|preimage| match preimage.as_slice() {
             [left, right] => create_binary_entry::<Pedersen>(*left, *right),
@@ -546,7 +605,8 @@ async fn test_fetch_patricia_paths_inner_from_json(#[case] input_data: &str) {
 
     // In Python the indices are relative to the first leaf (indices in the bottom layer).
     // In Rust the indices are absolute (indices in the full tree).
-    let leaf_indices: Vec<u128> = input.leaf_indices.iter().map(|&idx| idx + first_leaf).collect();
+    let leaf_indices: Vec<u128> =
+        input.modified_leaves_indices.iter().map(|&idx| idx + first_leaf).collect();
 
     let expected_nodes: PreimageMap = input
         .expected_nodes
