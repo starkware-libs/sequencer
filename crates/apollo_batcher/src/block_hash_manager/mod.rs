53263fa2316328054faccba9c68ee3e7d69be8a1
amos rothberg <amos@starkware.co>
2025-12-11 14:50:15 +0200

    apollo_batcher: implemented BlockHashManager::initialize()

--- crates/apollo_batcher/src/block_hash_manager/mod.rs
+++ crates/apollo_batcher/src/block_hash_manager/mod.rs
@@ -1,10 +1,21 @@
-use tokio::sync::mpsc::{Receiver, Sender};
-use tokio::task::JoinHandle;
+#![allow(dead_code)]
 
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::task::JoinHandle;
+use tracing::info;
+
+use crate::block_hash_manager::state_committer::StateCommitter;
 use crate::block_hash_manager::types::{CommitmentTaskInput, CommitmentTaskOutput};
 
 pub(crate) mod state_committer;
 pub(crate) mod types;
+
+// TODO(amos): Add to Batcher config.
+#[derive(Debug)]
+pub(crate) struct BlockHashManagerConfig {
+    pub(crate) tasks_channel_size: usize,
+    pub(crate) results_channel_size: usize,
+}
 
 #[allow(dead_code)]
 /// Encapsulates the block hash calculation logic.
@@ -12,4 +23,20 @@ pub(crate) struct BlockHashManager {
     pub(crate) tasks_sender: Sender<CommitmentTaskInput>,
     pub(crate) results_receiver: Receiver<CommitmentTaskOutput>,
     pub(crate) commitment_task_performer: JoinHandle<()>,
+    pub(crate) config: BlockHashManagerConfig,
+}
+
+impl BlockHashManager {
+    /// Initializes the BlockHashManager. This includes starting the state committer task.
+    pub(crate) fn initialize(config: BlockHashManagerConfig) -> Self {
+        info!("Initializing BlockHashManager with config {config:?}");
+        let (tasks_sender, tasks_receiver) = channel(config.tasks_channel_size);
+        let (results_sender, results_receiver) = channel(config.results_channel_size);
+
+        let state_committer = StateCommitter { tasks_receiver, results_sender };
+
+        let commitment_task_performer = state_committer.run();
+
+        Self { tasks_sender, results_receiver, commitment_task_performer, config }
+    }
 }
