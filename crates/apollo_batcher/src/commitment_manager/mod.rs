6059b775054c13ebe0ede9d9e02f1e4d1089f053
amos rothberg <amos@starkware.co>
2025-12-11 16:42:18 +0200

    apollo_batcher: rename BlockHashManager -> CommitmentManager

--- crates/apollo_batcher/src/commitment_manager/mod.rs
+++ crates/apollo_batcher/src/commitment_manager/mod.rs
@@ -7,25 +7,25 @@ use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::task::JoinHandle;
 use tracing::info;
 
-use crate::block_hash_manager::state_committer::StateCommitter;
-use crate::block_hash_manager::types::{CommitmentTaskInput, CommitmentTaskOutput};
+use crate::commitment_manager::state_committer::StateCommitter;
+use crate::commitment_manager::types::{CommitmentTaskInput, CommitmentTaskOutput};
 
 pub(crate) mod state_committer;
 pub(crate) mod types;
 
 #[allow(dead_code)]
 /// Encapsulates the block hash calculation logic.
-pub(crate) struct BlockHashManager {
+pub(crate) struct CommitmentManager {
     pub(crate) tasks_sender: Sender<CommitmentTaskInput>,
     pub(crate) results_receiver: Receiver<CommitmentTaskOutput>,
     pub(crate) commitment_task_performer: JoinHandle<()>,
 }
 
-impl BlockHashManager {
-    /// Initializes the BlockHashManager. This includes starting the state committer task.
+impl CommitmentManager {
+    /// Initializes the CommitmentManager. This includes starting the state committer task.
     pub(crate) fn initialize(tasks_channel_size: usize, results_channel_size: usize) -> Self {
         info!(
-            "Initializing BlockHashManager with input channel size {} and results channel size {}",
+            "Initializing CommitmentManager with input channel size {} and results channel size {}",
             tasks_channel_size, results_channel_size
         );
         let (tasks_sender, tasks_receiver) = channel(tasks_channel_size);
