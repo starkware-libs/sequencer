f3f0b99f6993badae6e1722d7cde7108ca54c9ba
Ariel Elperin <ariel@starkware.co>
2025-12-11 17:51:16 +0200

    starknet_committer: make fetch nodes depend on db layout

--- crates/starknet_committer/src/db/facts_db/db.rs
+++ crates/starknet_committer/src/db/facts_db/db.rs
@@ -3,17 +3,21 @@ use std::collections::HashMap;
 use async_trait::async_trait;
 use starknet_api::core::ContractAddress;
 use starknet_api::hash::HashOutput;
+use starknet_patricia::patricia_merkle_tree::filled_tree::node::FactDbFilledNode;
+use starknet_patricia::patricia_merkle_tree::filled_tree::node_serde::FactNodeDeserializationContext;
 use starknet_patricia::patricia_merkle_tree::filled_tree::tree::FilledTree;
-use starknet_patricia::patricia_merkle_tree::node_data::leaf::LeafModifications;
+use starknet_patricia::patricia_merkle_tree::node_data::leaf::{Leaf, LeafModifications};
 use starknet_patricia::patricia_merkle_tree::original_skeleton_tree::tree::OriginalSkeletonTreeImpl;
 use starknet_patricia::patricia_merkle_tree::types::{NodeIndex, SortedLeafIndices};
-use starknet_patricia_storage::db_object::EmptyKeyContext;
+use starknet_patricia_storage::db_object::{DBObject, EmptyKeyContext};
+use starknet_patricia_storage::errors::DeserializationError;
 use starknet_patricia_storage::map_storage::MapStorage;
 use starknet_patricia_storage::storage_trait::{
     create_db_key,
     DbHashMap,
     DbKey,
     DbKeyPrefix,
+    DbValue,
     Storage,
 };
 
@@ -23,6 +27,7 @@ use crate::block_committer::input::{
     ConfigImpl,
     StarknetStorageValue,
 };
+use crate::db::db_layout::NodeLayout;
 use crate::db::facts_db::create_facts_tree::{
     create_original_skeleton_tree,
     create_original_skeleton_tree_and_get_previous_leaves,
@@ -39,6 +44,18 @@ use crate::patricia_merkle_tree::tree::{
 };
 use crate::patricia_merkle_tree::types::CompiledClassHash;
 
+pub struct FactsNodeLayout {}
+
+impl<L: Leaf> NodeLayout<L> for FactsNodeLayout {
+    type ChildData = HashOutput;
+    type DeserializationContext = FactNodeDeserializationContext;
+    fn deserialize_node(
+        value: &DbValue,
+        deserialize_context: &Self::DeserializationContext,
+    ) -> Result<FactDbFilledNode<L>, DeserializationError> {
+        DBObject::deserialize(value, deserialize_context)
+    }
+}
 pub struct FactsDb<S: Storage> {
     // TODO(Yoav): Define StorageStats trait and impl it here. Then, make the storage field
     // private.
