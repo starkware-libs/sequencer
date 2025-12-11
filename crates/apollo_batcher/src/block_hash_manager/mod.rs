e6b02e475bda7d513c027b270f0dc12db38ecbd2
amos rothberg <amos@starkware.co>
2025-12-11 14:50:15 +0200

    apollo_batcher: implemented BlockHashManager::initialize()

--- crates/apollo_batcher/src/block_hash_manager/mod.rs
+++ crates/apollo_batcher/src/block_hash_manager/mod.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::task::JoinHandle;
 
@@ -12,4 +14,18 @@ pub(crate) struct BlockHashManager {
     pub(crate) tasks_sender: Sender<CommitmentTaskInput>,
     pub(crate) results_receiver: Receiver<CommitmentTaskOutput>,
     pub(crate) commitment_task_performer: JoinHandle<()>,
+}
+
+impl BlockHashManager {
+    /// Initializes the BlockHashManager. This includes starting the state committer task.
+    pub(crate) fn initialize(channel_size: usize) -> Self {
+        let (tasks_sender, tasks_receiver) = tokio::sync::mpsc::channel(channel_size);
+        let (results_sender, results_receiver) = tokio::sync::mpsc::channel(channel_size);
+
+        let state_committer = state_committer::StateCommitter { tasks_receiver, results_sender };
+
+        let commitment_task_performer = state_committer.run();
+
+        Self { tasks_sender, results_receiver, commitment_task_performer }
+    }
 }
