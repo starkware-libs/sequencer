b8307766fd014f93b2f5dcea31496d3d7fc6dafd
Aviv Yossef <aviv.y@starkware.co>
2025-12-11 12:55:20 +0200

    starknet_os_runner: execution data provider

---
+++ crates/starknet_os_runner/src/errors.rs
@@ -0,0 +1,9 @@
+use blockifier_reexecution::errors::ReexecutionError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ExecutionDataError {
+    #[error(transparent)]
+    // Boxed to reduce the size of Result on the stack (ReexecutionError is >128 bytes).
+    ReexecutionError(#[from] Box<ReexecutionError>),
+}
