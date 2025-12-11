6ab15554cd1ade2c074f190084bf7715a92eb5a7
Ariel Elperin <ariel@starkware.co>
2025-12-11 16:57:46 +0200

    starknet_committer,starknet_committer_and_os_cli,starknet_patricia: make FilledNode generic and introduce the FactsFilledNode alias

--- crates/starknet_patricia/src/patricia_merkle_tree/filled_tree/node.rs
+++ crates/starknet_patricia/src/patricia_merkle_tree/filled_tree/node.rs
@@ -5,7 +5,12 @@ use crate::patricia_merkle_tree::node_data::leaf::Leaf;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 /// A node in a Patricia-Merkle tree, complete with its hash and data.
-pub struct FilledNode<L: Leaf> {
+pub struct FilledNode<L: Leaf, ChildData> {
     pub hash: HashOutput,
-    pub data: NodeData<L, HashOutput>,
+    pub data: NodeData<L, ChildData>,
 }
+
+// TODO(Ariel, 14/12/2025): move this type (along with DBObject impl) to the facts_db module in
+// starknet_committer. This can happen after serialization of FilledTree is made generic in the
+// layout.
+pub type FactDbFilledNode<L> = FilledNode<L, HashOutput>;
