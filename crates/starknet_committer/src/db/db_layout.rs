6e2f8cea88a314c71149859d238c9d3e28bf9bf5
Ariel Elperin <ariel@starkware.co>
2025-12-11 17:51:16 +0200

    starknet_committer: make fetch nodes depend on db layout

---
+++ crates/starknet_committer/src/db/db_layout.rs
@@ -0,0 +1,23 @@
+use starknet_patricia::patricia_merkle_tree::filled_tree::node::FilledNode;
+use starknet_patricia::patricia_merkle_tree::node_data::leaf::Leaf;
+use starknet_patricia::patricia_merkle_tree::traversal::SubTreeTrait;
+use starknet_patricia_storage::db_object::DBObject;
+
+/// Specifies the trie db layout.
+pub trait NodeLayout<'a, L: Leaf>
+where
+    FilledNode<L, Self::NodeData>: DBObject<DeserializeContext = Self::DeserializationContext>,
+{
+    /// Additional data that a node stores about its children.
+    type NodeData: Copy;
+
+    /// The context needed to deserialize the node from a raw [DbValue].
+    type DeserializationContext;
+
+    /// The type of the subtree that is used to traverse the trie.
+    type SubTree: SubTreeTrait<
+            'a,
+            NodeData = Self::NodeData,
+            NodeDeserializeContext = Self::DeserializationContext,
+        >;
+}
