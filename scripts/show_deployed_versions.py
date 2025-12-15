ffea181fa6bafc9d793a055f1a3dc18a3f5ba383
Matan Lior <matan.l@starkware.co>
2025-12-15 23:18:13 +0200

    scripts: define the environments and update the CLI to accept them

--- scripts/show_deployed_versions.py
+++ scripts/show_deployed_versions.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+import dataclasses
 import json
 import re
 import subprocess
 import sys
+from typing import Optional
 
+import collections
 import logging
 
 
@@ -97,10 +100,78 @@ class KubeContext:
             self.namespaces.append(kube_ns)
 
 
+@dataclasses.dataclass(frozen=True)
+class EnvInfo:
+    desc: str
+    cluster_re: str
+    namespace_re: str
+    pod_re: Optional[str]
+
+
+class Runner:
+    ENVS = {
+        "Mainnet": EnvInfo(
+            desc="Starknet Mainnet DEC",
+            cluster_re=r"gke_starkware-prod_us-.*_starknet-mainnet-apollo-.*",
+            namespace_re=r"apollo-mainnet-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+        "Testnet": EnvInfo(
+            desc="Starknet Testnet DEC",
+            cluster_re=r"gke_starkware-starknet-testnet_us-.*_starknet-testnet-apollo-.*",
+            namespace_re=r"apollo-sepolia-alpha-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+        "Integration": EnvInfo(
+            desc="Starknet Integration DEC",
+            cluster_re="gke_starkware-starknet-testnet_us-central1_starknet-testnet",
+            namespace_re=r"apollo-sepolia-integration-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+        "POTC_Mainet": EnvInfo(
+            desc="POTC Mainnet DEC",
+            cluster_re=r"gke_starkware-prod_asia-.*_paradigm-mainnet-apollo-.*",
+            namespace_re=r"apollo-potc-mainnet-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+        "POTC_Testnet": EnvInfo(
+            desc="POTC Testnet DEC",
+            cluster_re=r"gke_starkware-integ-cust-tokyo_asia-.*_paradigm-testnet-apollo-.*",
+            namespace_re=r"apollo-potc-testnet-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+        "POTC_Integration": EnvInfo(
+            desc="POTC Integration DEC",
+            cluster_re="gke_starkware-integ-cust-tokyo_asia-northeast1_integ-cust-tokyo",
+            namespace_re=r"apollo-potc-mock-(.*)",
+            pod_re=r"sequencer-(.+?)-.*",
+        ),
+    }
+
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
+        # Select specific envs if provided, otherwise default to all
+        self.active_envs = (
+            [self.ENVS[env] for env in args.env] if args.env else list(self.ENVS.values())
+        )
+        self.contexts = collections.defaultdict(list)
+        self.logger = logging.getLogger("Runner")
+
+
 # --- Main Entry Point ---
 def main():
     parser = argparse.ArgumentParser(
         description="List pod container images across contexts and namespaces."
+    )
+    env_choices = list(Runner.ENVS.keys())
+    parser.add_argument(
+        "--env",
+        nargs="+",
+        action="extend",
+        default=[],
+        choices=env_choices,
+        metavar="ENV",
+        help=f"Optional list of environments ({','.join(env_choices)}). If omitted, all environments are used.",
     )
     parser.add_argument(
         "--verbose",
@@ -111,7 +182,9 @@ def main():
     args = parser.parse_args()
     init_logging(args.verbose)
 
-    logging.info("Script initialized. No logic implemented yet.")
+    # Initialize runner to verify config works
+    runner = Runner(args)
+    logging.debug(f"Runner initialized for {len(runner.active_envs)} environments")
 
 
 if __name__ == "__main__":
