5de101c0789ca46d4a5c2626cddbb58f4bb6c159
Arni Hod <arnon@starkware.co>
2025-12-15 20:59:41 +0200

    apollo_gateway: rename gateway struct to gateway impl and add statless validator generic

--- crates/apollo_gateway/src/gateway_test.rs
+++ crates/apollo_gateway/src/gateway_test.rs
@@ -81,7 +81,7 @@ use strum::VariantNames;
 use tempfile::TempDir;
 
 use crate::errors::{GatewayResult, StatelessTransactionValidatorError};
-use crate::gateway::Gateway;
+use crate::gateway::GatewayImplementation;
 use crate::metrics::{
     register_metrics,
     GatewayMetricHandle,
@@ -150,9 +150,9 @@ struct MockDependencies {
 }
 
 impl MockDependencies {
-    fn gateway(self) -> Gateway {
+    fn gateway(self) -> GatewayImplementation<MockStatelessTransactionValidatorTrait> {
         register_metrics();
-        Gateway::new(
+        GatewayImplementation::new(
             self.config,
             Arc::new(self.state_reader_factory),
             Arc::new(self.mock_mempool_client),
@@ -564,7 +564,7 @@ async fn add_tx_returns_error_when_extract_state_nonce_and_run_validations_fails
 
     let tx_args = invoke_args();
     setup_transaction_converter_mock(&mut mock_dependencies.mock_transaction_converter, &tx_args);
-    let gateway = Gateway {
+    let gateway = GatewayImplementation {
         config: Arc::new(mock_dependencies.config),
         stateless_tx_validator: Arc::new(mock_dependencies.mock_stateless_transaction_validator),
         stateful_tx_validator_factory: Arc::new(mock_stateful_transaction_validator_factory),
@@ -616,7 +616,7 @@ async fn add_tx_returns_error_when_instantiating_validator_fails(
 
     let tx_args = invoke_args();
     setup_transaction_converter_mock(&mut mock_dependencies.mock_transaction_converter, &tx_args);
-    let gateway = Gateway {
+    let gateway = GatewayImplementation {
         config: Arc::new(mock_dependencies.config),
         stateless_tx_validator: Arc::new(mock_dependencies.mock_stateless_transaction_validator),
         stateful_tx_validator_factory: Arc::new(mock_stateful_transaction_validator_factory),
