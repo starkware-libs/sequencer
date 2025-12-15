384f2ca7285a80ce9111c598f6329878dd4bd30f
Ariel Elperin <ariel@starkware.co>
2025-12-15 18:09:29 +0200

    starknet_committer,starknet_patricia: add serde errors

--- crates/starknet_committer_and_os_cli/src/committer_cli/filled_tree_output/filled_forest.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/filled_tree_output/filled_forest.rs
@@ -2,6 +2,7 @@ use serde::Serialize;
 use starknet_committer::db::facts_db::db::FactsDb;
 use starknet_committer::db::forest_trait::ForestWriter;
 use starknet_committer::forest::filled_forest::FilledForest;
+use starknet_patricia_storage::errors::SerializationResult;
 use starknet_patricia_storage::map_storage::MapStorage;
 
 pub struct SerializedForest(pub FilledForest);
@@ -19,16 +20,16 @@ pub struct Output {
 }
 
 impl SerializedForest {
-    pub async fn forest_to_output(&self) -> Output {
+    pub async fn forest_to_output(&self) -> SerializationResult<Output> {
         // Create an empty storage for the new facts.
         let mut output_facts_db = FactsDb::new(MapStorage::default());
-        output_facts_db.write(&self.0).await;
+        output_facts_db.write(&self.0).await?;
         let contract_storage_root_hash = self.0.get_contract_root_hash().0;
         let compiled_class_root_hash = self.0.get_compiled_class_root_hash().0;
-        Output {
+        Ok(Output {
             storage: output_facts_db.storage,
             contract_storage_root_hash: contract_storage_root_hash.to_hex_string(),
             compiled_class_root_hash: compiled_class_root_hash.to_hex_string(),
-        }
+        })
     }
 }
