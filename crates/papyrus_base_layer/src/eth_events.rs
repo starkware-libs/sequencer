2c0b8defd107e29ff862ad02b88b23519d7d7f72
Guy Nir <guy.n@starkware.co>
2025-12-16 11:32:40 +0200

    papyrus_base_layer: cancellation finalized L1 events  rppagates the tx hash to be cancelled

--- crates/papyrus_base_layer/src/eth_events.rs
+++ crates/papyrus_base_layer/src/eth_events.rs
@@ -38,7 +38,9 @@ pub fn parse_event(log: Log, block_timestamp: BlockTimestamp) -> EthereumBaseLay
             Ok(L1Event::ConsumedMessageToL2 { tx, timestamp: block_timestamp })
         }
         Starknet::StarknetEvents::MessageToL2Canceled(event) => {
-            Ok(L1Event::MessageToL2Canceled(event.try_into()?))
+            let event_data: EventData = event.try_into()?;
+            let cancelled_tx = L1HandlerTransaction::from(event_data);
+            Ok(L1Event::MessageToL2Canceled { cancelled_tx })
         }
         Starknet::StarknetEvents::MessageToL2CancellationStarted(event) => {
             Ok(L1Event::MessageToL2CancellationStarted {
