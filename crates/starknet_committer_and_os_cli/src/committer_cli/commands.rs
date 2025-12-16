0528ffb59f05a1573beb4630a2c66d4e16ff5109
yoavGrs <97383386+yoavGrs@users.noreply.github.com>
2025-12-16 14:19:49 +0200

    starknet_committer: remove Config trait (#10784)

--- crates/starknet_committer_and_os_cli/src/committer_cli/commands.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/commands.rs
@@ -1,5 +1,4 @@
 use starknet_committer::block_committer::commit::commit_block;
-use starknet_committer::block_committer::input::Config;
 use starknet_committer::db::facts_db::db::FactsDb;
 use starknet_patricia_storage::map_storage::MapStorage;
 use tracing::info;
