8d3a63a1c667f7cc08bd74b6f37945b3ca874d2e
Guy Nir <guy.n@starkware.co>
2025-12-17 17:51:44 +0200

    apollo_l1_provider: add flow test for finality

--- crates/apollo_l1_provider/tests/flow_test_happy_flow.rs
+++ crates/apollo_l1_provider/tests/flow_test_happy_flow.rs
@@ -31,7 +31,7 @@ async fn new_l1_handler_tx_propose_validate_cooldown() {
     let (l2_hash, _nonce) = send_message_from_l1_to_l2(&mut base_layer, CALL_DATA).await;
 
     let l1_provider_client =
-        setup_scraper_and_provider(base_layer.ethereum_base_layer.clone()).await;
+        setup_scraper_and_provider(base_layer.ethereum_base_layer.clone(), None).await;
 
     tokio::time::pause();
 
