f94be8830c7a32e40a4a7d491ef7e2cbfca3066c
Yoav Gross <yoav@starkware.co>
2025-12-17 11:26:36 +0200

    apollo_committer: read metadata

--- crates/apollo_committer/src/committer.rs
+++ crates/apollo_committer/src/committer.rs
@@ -1,10 +1,16 @@
+use std::error::Error;
+
 use apollo_committer_config::config::CommitterConfig;
+use apollo_committer_types::errors::{CommitterError, CommitterResult};
 use starknet_api::block::BlockNumber;
+use starknet_api::core::{GlobalRoot, StateDiffCommitment};
+use starknet_api::hash::PoseidonHash;
 use starknet_committer::db::forest_trait::{ForestMetadata, ForestMetadataType};
 use starknet_committer::db::mock_forest_storage::MockForestStorage;
-use starknet_committer::db::serde_db_utils::DbBlockNumber;
+use starknet_committer::db::serde_db_utils::{deserialize_felt_no_packing, DbBlockNumber};
 use starknet_patricia_storage::map_storage::MapStorage;
-use starknet_patricia_storage::storage_trait::Storage;
+use starknet_patricia_storage::storage_trait::{DbValue, Storage};
+use tracing::error;
 
 pub type ApolloCommitter = Committer<MapStorage>;
 
@@ -38,5 +44,38 @@ impl<S: Storage + Default> Committer<S> {
             .0;
 
         Self { forest_storage, config, offset }
+    }
+
+    #[allow(dead_code)]
+    async fn load_state_diff_commitment(
+        &mut self,
+        block_number: BlockNumber,
+    ) -> CommitterResult<StateDiffCommitment> {
+        let db_value = self
+            .read_metadata(ForestMetadataType::StateDiffHash(DbBlockNumber(block_number)))
+            .await?;
+        Ok(StateDiffCommitment(PoseidonHash(deserialize_felt_no_packing(&db_value))))
+    }
+
+    #[allow(dead_code)]
+    async fn load_global_root(&mut self, block_number: BlockNumber) -> CommitterResult<GlobalRoot> {
+        let db_value =
+            self.read_metadata(ForestMetadataType::StateRoot(DbBlockNumber(block_number))).await?;
+        Ok(GlobalRoot(deserialize_felt_no_packing(&db_value)))
+    }
+
+    // Reads metadata from the storage, returns an error if it is not found.
+    async fn read_metadata(&mut self, metadata: ForestMetadataType) -> CommitterResult<DbValue> {
+        self.forest_storage
+            .read_metadata(metadata.clone())
+            .await
+            .map_err(|err| self.map_internal_error(err))?
+            .ok_or(CommitterError::MissingMetadata(metadata))
+    }
+
+    fn map_internal_error<E: Error>(&self, err: E) -> CommitterError {
+        let error_message = format!("{err:?}: {err}");
+        error!("Error committing block number {0}. {error_message}.", self.offset);
+        CommitterError::Internal { height: self.offset, message: error_message }
     }
 }
