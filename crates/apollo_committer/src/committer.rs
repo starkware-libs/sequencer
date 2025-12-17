eecbd0be7bbda718a41ca83dfdd634c2fb8f0669
Yoav Gross <yoav@starkware.co>
2025-12-17 11:26:36 +0200

    apollo_committer: create committer

--- crates/apollo_committer/src/committer.rs
+++ crates/apollo_committer/src/committer.rs
@@ -1,1 +1,42 @@
-pub struct Committer;
+use apollo_committer_config::config::CommitterConfig;
+use starknet_api::block::BlockNumber;
+use starknet_committer::db::forest_trait::{ForestMetadata, ForestMetadataType};
+use starknet_committer::db::mock_forest_storage::MockForestStorage;
+use starknet_committer::db::serde_db_utils::DbBlockNumber;
+use starknet_patricia_storage::map_storage::MapStorage;
+use starknet_patricia_storage::storage_trait::Storage;
+
+pub type ApolloCommitter = Committer<MapStorage>;
+
+#[allow(dead_code)]
+/// Apollo committer. Maintains the Starknet state tries in persistent storage.
+pub struct Committer<S: Storage + Default> {
+    /// Storage for forest operations.
+    forest_storage: MockForestStorage<S>,
+    /// Committer config.
+    config: CommitterConfig,
+    /// The next block number to commit.
+    offset: BlockNumber,
+}
+
+impl<S: Storage + Default> Committer<S> {
+    pub async fn new(config: CommitterConfig) -> Self {
+        let mut forest_storage = MockForestStorage { storage: S::default() };
+        let db_offset = forest_storage
+            .read_metadata(ForestMetadataType::CommitmentOffset)
+            .await
+            .expect("Failed to read commitment offset");
+
+        let offset = db_offset
+            .map(|value| {
+                let array_value: [u8; 8] = value.0.try_into().unwrap_or_else(|value| {
+                    panic!("Failed to deserialize commitment offset from {value:?}")
+                });
+                DbBlockNumber::deserialize(array_value)
+            })
+            .unwrap_or_default()
+            .0;
+
+        Self { forest_storage, config, offset }
+    }
+}
--- crates/apollo_committer/src/communication.rs
+++ crates/apollo_committer/src/communication.rs
@@ -2,15 +2,18 @@ use apollo_committer_types::communication::{CommitterRequest, CommitterResponse}
 use apollo_infra::component_definitions::ComponentRequestHandler;
 use apollo_infra::component_server::{LocalComponentServer, RemoteComponentServer};
 use async_trait::async_trait;
+use starknet_patricia_storage::storage_trait::Storage;
 
 use crate::committer::Committer;
 
-pub type LocalCommitterServer =
-    LocalComponentServer<Committer, CommitterRequest, CommitterResponse>;
+pub type LocalCommitterServer<S> =
+    LocalComponentServer<Committer<S>, CommitterRequest, CommitterResponse>;
 pub type RemoteCommitterServer = RemoteComponentServer<CommitterRequest, CommitterResponse>;
 
 #[async_trait]
-impl ComponentRequestHandler<CommitterRequest, CommitterResponse> for Committer {
+impl<S: Storage + Default> ComponentRequestHandler<CommitterRequest, CommitterResponse>
+    for Committer<S>
+{
     async fn handle_request(&mut self, request: CommitterRequest) -> CommitterResponse {
         match request {
             CommitterRequest::CommitBlock(_) => {
