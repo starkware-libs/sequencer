95811f47a54aa3510d8f79ca972dbe4120ff9611
FrancoGiachetta <francogiachetta27@gmail.com>
2025-12-19 17:27:08 -0300

    update cairo lang

--- crates/apollo_infra_utils/src/cairo_compiler_version.rs
+++ crates/apollo_infra_utils/src/cairo_compiler_version.rs
@@ -2,4 +2,4 @@
 #[path = "cairo_compiler_version_test.rs"]
 mod cairo_compiler_version_test;
 
-pub const CAIRO1_COMPILER_VERSION: &str = "2.14.1-dev.1";
+pub const CAIRO1_COMPILER_VERSION: &str = "2.15.0";
