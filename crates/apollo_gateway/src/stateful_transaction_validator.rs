4c4cf5ee71568eb971665be2668587b57e1f4b19
Arni Hod <arnon@starkware.co>
2025-12-22 20:35:34 +0200

    apollo_gateway: return non-boxed fixed block state reader from state reader factory

--- crates/apollo_gateway/src/stateful_transaction_validator.rs
+++ crates/apollo_gateway/src/stateful_transaction_validator.rs
@@ -94,10 +94,9 @@ where
             self.contract_class_manager.clone(),
             Some(GATEWAY_CLASS_CACHE_METRICS),
         );
-        // Convert Box<ConcreteType> to Box<dyn GatewayFixedBlockStateReader>.
-        // This is safe because FixedBlockStateReader: GatewayFixedBlockStateReader.
+        // Convert concrete type to trait object.
         let boxed_gateway_fixed_block_state_reader: Box<dyn GatewayFixedBlockStateReader> =
-            gateway_fixed_block_state_reader;
+            Box::new(gateway_fixed_block_state_reader);
         Ok(Box::new(StatefulTransactionValidator::new(
             self.config.clone(),
             self.chain_info.clone(),
