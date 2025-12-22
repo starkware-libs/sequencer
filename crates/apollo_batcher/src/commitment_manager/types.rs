a6f27d1f5f5d0f9f84f50e26986e2aa94d45a8c8
Nimrod Weiss <nimrod@starkware.co>
2025-12-22 17:29:44 +0200

    apollo_batcher: write commitment results into storage

--- crates/apollo_batcher/src/commitment_manager/types.rs
+++ crates/apollo_batcher/src/commitment_manager/types.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use starknet_api::block::BlockNumber;
+use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::core::{GlobalRoot, StateDiffCommitment};
 use starknet_api::state::ThinStateDiff;
 
@@ -15,4 +15,11 @@ pub(crate) struct CommitmentTaskInput {
 pub(crate) struct CommitmentTaskOutput {
     pub(crate) global_root: GlobalRoot,
     pub(crate) height: BlockNumber,
+}
+
+pub(crate) struct FinalBlockCommitment {
+    pub(crate) height: BlockNumber,
+    // Field is optional because for old blocks, the block hash is learnable from sync.
+    pub(crate) block_hash: Option<BlockHash>,
+    pub(crate) global_root: GlobalRoot,
 }
