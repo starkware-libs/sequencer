5c7b29cd0a069cfd294905b720ed243570786b29
Guy Nir <guy.n@starkware.co>
2025-12-23 14:27:21 +0200

    apollo_consensus_orchestrator: use the config manager client to update consensus context config

--- crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
+++ crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
@@ -18,6 +18,7 @@ use apollo_batcher_types::batcher_types::{
 };
 use apollo_batcher_types::communication::BatcherClient;
 use apollo_class_manager_types::transaction_converter::TransactionConverterTrait;
+use apollo_config_manager_types::communication::SharedConfigManagerClient;
 use apollo_consensus::types::{
     ConsensusContext,
     ConsensusError,
@@ -177,6 +178,7 @@ pub struct SequencerConsensusContextDeps {
     pub outbound_proposal_sender: mpsc::Sender<(HeightAndRound, mpsc::Receiver<ProposalPart>)>,
     // Used to broadcast votes to other consensus nodes.
     pub vote_broadcast_client: BroadcastTopicClient<Vote>,
+    pub config_manager_client: Option<SharedConfigManagerClient>,
 }
 
 impl SequencerConsensusContext {
@@ -724,6 +726,7 @@ impl ConsensusContext for SequencerConsensusContext {
         round: Round,
     ) -> Result<(), ConsensusError> {
         if self.current_height.map(|h| height > h).unwrap_or(true) {
+            self.update_dynamic_config().await;
             self.current_height = Some(height);
             assert_eq!(round, 0);
             self.current_round = round;
@@ -734,13 +737,26 @@ impl ConsensusContext for SequencerConsensusContext {
             // return to the old height.
             return Ok(self.deps.batcher.start_height(StartHeightInput { height }).await?);
         }
-        assert_eq!(Some(height), self.current_height);
+        assert_eq!(
+            Some(height),
+            self.current_height,
+            "height {} is not equal to current height {:?}",
+            height,
+            self.current_height
+        );
         if round == self.current_round {
             return Ok(());
         }
-        assert!(round > self.current_round);
+        assert!(
+            round > self.current_round,
+            "round {} is not greater than current round {}",
+            round,
+            self.current_round
+        );
         self.interrupt_active_proposal().await;
         self.current_round = round;
+        self.update_dynamic_config().await;
+
         let mut to_process = None;
         while let Some(entry) = self.queued_proposals.first_entry() {
             match self.current_round.cmp(entry.key()) {
@@ -850,6 +866,12 @@ impl SequencerConsensusContext {
             .add_new_block(sync_block.clone())
             .await
             .expect("Failed to add new block due to sync error: {e:?}");
+    }
+    async fn update_dynamic_config(&mut self) {
+        if let Some(config_manager_client) = self.deps.config_manager_client.clone() {
+            self.config.dynamic_config =
+                config_manager_client.get_context_dynamic_config().await.unwrap();
+        }
     }
 }
 
