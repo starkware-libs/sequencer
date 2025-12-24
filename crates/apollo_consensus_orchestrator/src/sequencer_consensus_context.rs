625b15f049864e9b2968cc8c13e8605e6841c3a7
Dafna Matsry <dafna.matsry@starkware.co>
2025-12-24 14:50:47 +0200

    apollo_consensus: small fixes

--- crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
+++ crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
@@ -891,7 +891,9 @@ impl SequencerConsensusContext {
     async fn interrupt_active_proposal(&mut self) {
         if let Some((token, handle)) = self.active_proposal.take() {
             token.cancel();
-            handle.await.expect("Proposal task failed, propagating panic");
+            if let Err(e) = handle.await {
+                warn!("Proposal task finished unexpectedly: {e:?}");
+            }
         }
     }
 }
