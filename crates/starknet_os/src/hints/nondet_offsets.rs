c4bc24358117235567c412ec95e4ac0591964b7f
Dori Medini <dori@starkware.co>
2025-12-24 12:02:31 +0200

    starknet_os: refactor nondet hint SegmentsAddTempInitialTxsRangeCheckPtr

--- crates/starknet_os/src/hints/nondet_offsets.rs
+++ crates/starknet_os/src/hints/nondet_offsets.rs
@@ -4,7 +4,7 @@ use std::sync::LazyLock;
 use cairo_vm::types::relocatable::MaybeRelocatable;
 use cairo_vm::vm::vm_core::VirtualMachine;
 
-use crate::hints::enum_definition::{AllHints, OsHint, StatelessHint};
+use crate::hints::enum_definition::{AllHints, OsHint};
 use crate::hints::error::{OsHintError, OsHintResult};
 
 #[cfg(test)]
@@ -21,7 +21,6 @@ pub(crate) static NONDET_FP_OFFSETS: LazyLock<HashMap<AllHints, usize>> = LazyLo
     HashMap::from([
         (AllHints::OsHint(OsHint::OsInputTransactions), 12),
         (AllHints::OsHint(OsHint::ReadAliasFromKey), 0),
-        (AllHints::StatelessHint(StatelessHint::SegmentsAddTemp), 5),
         (AllHints::OsHint(OsHint::SetFpPlus4ToTxNonce), 4),
         (AllHints::OsHint(OsHint::GetBlocksNumber), 3),
         (AllHints::OsHint(OsHint::TxAccountDeploymentDataLen), 4),
