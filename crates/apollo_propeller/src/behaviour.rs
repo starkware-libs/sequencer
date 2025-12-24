abfd6caa620427803ec401250abb339b32bfc2d3
Andrew Luka <andrew.l@starkware.co>
2025-12-24 10:33:08 +0200

    apollo_propeller: add behaviour skeleton

---
+++ crates/apollo_propeller/src/behaviour.rs
@@ -0,0 +1,103 @@
+//! Propeller network behaviour (libp2p adapter).
+
+use std::collections::VecDeque;
+use std::task::{Context, Poll};
+
+use libp2p::core::Endpoint;
+use libp2p::identity::PeerId;
+use libp2p::swarm::behaviour::{ConnectionClosed, ConnectionEstablished, FromSwarm};
+use libp2p::swarm::{
+    ConnectionDenied,
+    ConnectionId,
+    NetworkBehaviour,
+    THandler,
+    THandlerInEvent,
+    THandlerOutEvent,
+    ToSwarm,
+};
+
+use crate::handler::{Handler, HandlerIn, HandlerOut};
+use crate::types::Event;
+
+/// The Propeller network behaviour.
+pub struct Behaviour {
+    /// Events to be returned to the swarm.
+    events: VecDeque<ToSwarm<Event, HandlerIn>>,
+    /// Maximum wire message size for handlers.
+    max_wire_message_size: usize,
+}
+
+impl Behaviour {
+    /// Create a new Propeller behaviour.
+    pub fn new(max_wire_message_size: usize) -> Self {
+        Self { events: VecDeque::new(), max_wire_message_size }
+    }
+}
+
+impl NetworkBehaviour for Behaviour {
+    type ConnectionHandler = Handler;
+    type ToSwarm = Event;
+
+    fn handle_established_inbound_connection(
+        &mut self,
+        _connection_id: ConnectionId,
+        _peer: PeerId,
+        _local_addr: &libp2p::core::Multiaddr,
+        _remote_addr: &libp2p::core::Multiaddr,
+    ) -> Result<THandler<Self>, ConnectionDenied> {
+        Ok(Handler::new(
+            libp2p::swarm::StreamProtocol::new("/propeller/1.0.0"),
+            self.max_wire_message_size,
+        ))
+    }
+
+    fn handle_established_outbound_connection(
+        &mut self,
+        _connection_id: ConnectionId,
+        _peer: PeerId,
+        _addr: &libp2p::core::Multiaddr,
+        _role_override: Endpoint,
+        _port_use: libp2p::core::transport::PortUse,
+    ) -> Result<THandler<Self>, ConnectionDenied> {
+        Ok(Handler::new(
+            libp2p::swarm::StreamProtocol::new("/propeller/1.0.0"),
+            self.max_wire_message_size,
+        ))
+    }
+
+    fn on_swarm_event(&mut self, event: FromSwarm<'_>) {
+        match event {
+            FromSwarm::ConnectionEstablished(ConnectionEstablished { .. }) => {}
+            FromSwarm::ConnectionClosed(ConnectionClosed { .. }) => {}
+            _ => {}
+        }
+    }
+
+    fn on_connection_handler_event(
+        &mut self,
+        _peer_id: PeerId,
+        _connection_id: ConnectionId,
+        event: THandlerOutEvent<Self>,
+    ) {
+        match event {
+            HandlerOut::Unit(_unit) => {
+                // TODO(AndrewL): Forward to core for validation
+            }
+            HandlerOut::SendError(_error) => {
+                // TODO(AndrewL): Forward to core for error handling
+            }
+        }
+    }
+
+    fn poll(
+        &mut self,
+        _cx: &mut Context<'_>,
+    ) -> Poll<ToSwarm<Self::ToSwarm, THandlerInEvent<Self>>> {
+        // Return any pending events
+        if let Some(event) = self.events.pop_front() {
+            return Poll::Ready(event);
+        }
+
+        Poll::Pending
+    }
+}
