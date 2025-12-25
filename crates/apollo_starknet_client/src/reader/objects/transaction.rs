12b80cafc0e77d40a536eb6d27f9809e74ce68d6
Aviv Greenburg <aviv.g@starkware.co>
2025-12-25 16:44:54 +0200

    apollo_starknet_client: add proof fields to IntermediateInvokeTransaction

--- crates/apollo_starknet_client/src/reader/objects/transaction.rs
+++ crates/apollo_starknet_client/src/reader/objects/transaction.rs
@@ -514,9 +514,10 @@ pub struct IntermediateInvokeTransaction {
     pub paymaster_data: Option<PaymasterData>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub account_deployment_data: Option<AccountDeploymentData>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub proof_facts: Option<ProofFacts>,
     pub transaction_hash: TransactionHash,
     pub version: TransactionVersion,
-    // TODO(AvivG): Consider adding proof facts.
 }
 
 // TODO(shahak, 01/11/2023): Add conversion tests.
@@ -630,7 +631,7 @@ impl TryFrom<IntermediateInvokeTransaction> for starknet_api::transaction::Invok
                     msg: "Invoke V3 must contain account_deployment_data field.".to_string(),
                 },
             )?,
-            proof_facts: ProofFacts::default(),
+            proof_facts: invoke_tx.proof_facts.unwrap_or_default(),
         })
     }
 }
