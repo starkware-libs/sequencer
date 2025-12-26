12e0c667d86a9095058148560929ed8c6b9f53db
Dori Medini <dori@starkware.co>
2025-12-26 13:20:50 +0200

    apollo_starknet_os_program: parallelize cairo0 formatting test

--- crates/apollo_starknet_os_program/src/cairo_formatting_test.rs
+++ crates/apollo_starknet_os_program/src/cairo_formatting_test.rs
@@ -3,18 +3,28 @@ use std::path::PathBuf;
 use apollo_infra_utils::cairo0_compiler::cairo0_format;
 use apollo_infra_utils::compile_time_cargo_manifest_dir;
 use expect_test::expect_file;
+use rstest::rstest;
+use tokio::task::JoinSet;
 
 use crate::CAIRO_FILES_MAP;
 
-#[test]
-fn test_cairo0_formatting() {
+#[rstest]
+#[tokio::test(flavor = "multi_thread")]
+async fn test_cairo0_formatting() {
     // Get the path to the apollo_starknet_os directory.
     let apollo_starknet_os_path =
         PathBuf::from(compile_time_cargo_manifest_dir!()).join("src/cairo");
 
+    let mut tasks = JoinSet::new();
+
     // Read all cairo files recursively from the directory.
     for (relative_path, contents) in CAIRO_FILES_MAP.iter() {
-        let formatted = cairo0_format(contents);
-        expect_file![apollo_starknet_os_path.join(relative_path)].assert_eq(&formatted);
+        let path = apollo_starknet_os_path.join(relative_path);
+        tasks.spawn(async move {
+            let formatted = cairo0_format(contents);
+            expect_file![path].assert_eq(&formatted);
+        });
     }
+
+    tasks.join_all().await;
 }
