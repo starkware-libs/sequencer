b52c25ea5b4ae4b5adc16b2a4e0aa71a7929518f
Andrew Luka <andrew.l@starkware.co>
2025-12-28 12:23:55 +0200

    apollo_propeller: added merkle implementation tests

---
+++ crates/apollo_propeller/src/merkle_test.rs
@@ -0,0 +1,59 @@
+use rstest::rstest;
+
+use crate::merkle::*;
+
+#[rstest]
+#[case(1, 0)]
+#[case(2, 1)]
+#[case(3, 2)]
+#[case(4, 2)]
+#[case(5, 3)]
+fn test_merkle_proof_length(#[case] n: u8, #[case] proof_length: usize) {
+    let data: Vec<_> = (0..n).map(|i| vec![i]).collect();
+    let tree = MerkleTree::new(&data);
+    for proof_index in 0..n.into() {
+        let proof = tree.prove(proof_index).unwrap();
+        assert_eq!(proof.siblings.len(), proof_length);
+    }
+}
+
+#[rstest]
+#[case(1)]
+#[case(2)]
+#[case(3)]
+#[case(4)]
+#[case(5)]
+#[case(6)]
+#[case(7)]
+fn test_merkle_proof_validity(#[case] n: u8) {
+    let data: Vec<_> = (0..n).map(|i| vec![i]).collect();
+    let tree = MerkleTree::new(&data);
+    for proof_index in 0..n.into() {
+        let proof = tree.prove(proof_index).unwrap();
+        let leaf_hash = MerkleTree::hash_leaf(&data[proof_index]);
+        assert!(tree.verify(&leaf_hash, &proof, proof_index));
+    }
+}
+
+#[rstest]
+#[case(1)]
+#[case(2)]
+#[case(3)]
+#[case(4)]
+#[case(5)]
+#[case(6)]
+#[case(7)]
+#[case(8)]
+#[case(9)]
+#[case(10)]
+fn test_merkle_tampered_data_proof_invalidity(#[case] n: u8) {
+    let data: Vec<_> = (0..n).map(|i| vec![i]).collect();
+    let tree = MerkleTree::new(&data);
+    for proof_index in 0..n.into() {
+        let proof = tree.prove(proof_index).unwrap();
+        let mut tampered_data = data[proof_index].clone();
+        tampered_data.push(1);
+        let leaf_hash = MerkleTree::hash_leaf(&tampered_data);
+        assert!(!tree.verify(&leaf_hash, &proof, proof_index));
+    }
+}
