ca05334d62090d681842cc011db39d50b9782bfa
Noam Spiegelstein <noam.s@starkware.co>
2025-12-29 17:47:28 +0200

    apollo_network: basic authentication tests

--- crates/apollo_network/src/authentication/negotiator.rs
+++ crates/apollo_network/src/authentication/negotiator.rs
@@ -3,6 +3,8 @@ use std::io::Error as IoError;
 use async_trait::async_trait;
 use futures::{Sink, Stream};
 use libp2p::PeerId;
+#[cfg(any(feature = "testing", test))]
+use mockall::automock;
 
 pub enum NegotiationSide {
     Inbound,
@@ -20,6 +22,7 @@ pub type ConnectionSender = dyn Sink<Vec<u8>, Error = IoError> + Unpin + Send;
 pub type ConnectionReceiver = dyn Stream<Item = Result<Vec<u8>, IoError>> + Unpin + Send;
 
 #[async_trait]
+#[cfg_attr(any(feature = "testing", test), automock(type Error = std::io::Error;))]
 pub trait Negotiator: Send + Clone {
     type Error: std::error::Error + Send + Sync;
 
@@ -39,4 +42,41 @@ pub trait Negotiator: Send + Clone {
     /// "strk_id_v2".
     // TODO(noam.s): Consider making this a const.
     fn protocol_name(&self) -> &'static str;
+}
+
+// Automock does not implement Clone, so we need to do it manually.
+#[cfg(any(feature = "testing", test))]
+impl Clone for MockNegotiator {
+    fn clone(&self) -> Self {
+        MockNegotiator::default()
+    }
+}
+
+/// This is a dummy implementation of the Negotiator trait used only so you can use
+/// `Option<Negotiator>::None` (where you don't have a real type). Instances of this type should
+/// never be created.
+// We make it an enum to enforce that it is not possible to create an instance of this type.
+// TODO(noam.s): Try to remove this when we use the ComposedNoiseConfig in the network manager.
+#[derive(Clone)]
+#[allow(dead_code)]
+pub(crate) enum DummyNegotiatorType {}
+
+#[async_trait]
+impl Negotiator for DummyNegotiatorType {
+    type Error = IoError;
+
+    fn protocol_name(&self) -> &'static str {
+        unreachable!("Methods of DummyNegotiatorType should never be invoked");
+    }
+
+    async fn negotiate_connection(
+        &mut self,
+        _my_peer_id: PeerId,
+        _other_peer_id: PeerId,
+        _connection_sender: &mut ConnectionSender,
+        _connection_receiver: &mut ConnectionReceiver,
+        _side: NegotiationSide,
+    ) -> Result<NegotiatorOutput, Self::Error> {
+        unreachable!("Methods of DummyNegotiatorType should never be invoked");
+    }
 }
