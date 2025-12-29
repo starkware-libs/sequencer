c1c9ec77656435dfb6d60c25e8bb236d017fd2b5
Ariel Elperin <ariel@starkware.co>
2025-12-29 11:13:26 +0200

    starknet_committer,starknet_patricia: layout-based original tree creation tests

--- crates/starknet_committer/src/db/index_db/mod.rs
+++ crates/starknet_committer/src/db/index_db/mod.rs
@@ -1,5 +1,9 @@
+#[cfg(test)]
+pub mod create_index_tree_test;
 pub mod db;
 pub mod leaves;
 #[cfg(test)]
 pub mod serde_tests;
+#[cfg(any(feature = "testing", test))]
+pub mod test_utils;
 pub mod types;
