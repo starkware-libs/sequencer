844109f67add333bf846eff1406514c8b891d531
nadin-Starkware <nadin@starkware.co>
2025-12-30 17:04:17 +0200

    apollo_storage:  implement tx idx by hash query in storage reader

--- crates/apollo_storage/src/storage_reader_types.rs
+++ crates/apollo_storage/src/storage_reader_types.rs
@@ -317,8 +317,13 @@ impl StorageReaderServerHandler<StorageReaderRequest, StorageReaderResponse>
             StorageReaderRequest::TransactionMetadata(_tx_index) => {
                 unimplemented!()
             }
-            StorageReaderRequest::TransactionHashToIdx(_tx_hash) => {
-                unimplemented!()
+            StorageReaderRequest::TransactionHashToIdx(tx_hash) => {
+                let tx_index =
+                    txn.get_transaction_idx_by_hash(&tx_hash)?.ok_or(StorageError::NotFound {
+                        resource_type: "Transaction index".to_string(),
+                        resource_id: format!("tx_hash: {}", tx_hash),
+                    })?;
+                Ok(StorageReaderResponse::TransactionHashToIdx(tx_index))
             }
 
             // ============ Other Requests ============
