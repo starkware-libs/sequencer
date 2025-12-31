98b08d7acdb84069e001cbf564792cf49c867232
Andrew Luka <andrew.l@starkware.co>
2025-12-31 11:21:31 +0200

    deleteme: PR to test integration test ci change

--- crates/apollo_integration_tests/src/integration_test_utils.rs
+++ crates/apollo_integration_tests/src/integration_test_utils.rs
@@ -12,4 +12,6 @@ pub async fn integration_test_setup(test_specifier: &str) {
          to pre-compile the binary before running this test. Alternatively, you can compile the \
          binary and run this test with './scripts/sequencer_integration_test.sh {test_specifier}'"
     );
+
+    info!("Integration test setup completed.");
 }
