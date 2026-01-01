3523e5363844d7161c3263153799c454c93ec321
amos rothberg <amos@starkware.co>
2026-01-01 12:06:13 +0200

    apollo_batcher: add state committer trait

--- crates/apollo_batcher/src/commitment_manager/state_committer.rs
+++ crates/apollo_batcher/src/commitment_manager/state_committer.rs
@@ -7,22 +7,42 @@ use tokio::task::JoinHandle;
 use crate::commitment_manager::types::{CommitmentTaskInput, CommitmentTaskOutput};
 
 /// Commits state changes by calling the committer.
+pub(crate) trait StateCommitterTrait {
+    /// Creates a new instance and starts thread which performs commitment tasks.
+    fn create(
+        tasks_receiver: Receiver<CommitmentTaskInput>,
+        results_sender: Sender<CommitmentTaskOutput>,
+        committer_client: SharedCommitterClient,
+    ) -> Self;
+    /// Returns a handle to the thread performing commitment tasks.
+    fn get_handle(&self) -> &JoinHandle<()>;
+}
+
 pub(crate) struct StateCommitter {
-    pub(crate) tasks_receiver: Receiver<CommitmentTaskInput>,
-    pub(crate) results_sender: Sender<CommitmentTaskOutput>,
-    pub(crate) committer_client: SharedCommitterClient,
+    task_performer_handle: JoinHandle<()>,
+}
+
+impl StateCommitterTrait for StateCommitter {
+    fn create(
+        tasks_receiver: Receiver<CommitmentTaskInput>,
+        results_sender: Sender<CommitmentTaskOutput>,
+        committer_client: SharedCommitterClient,
+    ) -> Self {
+        let handle = tokio::spawn(async move {
+            Self::perform_commitment_tasks(tasks_receiver, results_sender, committer_client).await;
+        });
+        Self { task_performer_handle: handle }
+    }
+    fn get_handle(&self) -> &JoinHandle<()> {
+        &self.task_performer_handle
+    }
 }
 
 impl StateCommitter {
-    pub(crate) fn run(self) -> JoinHandle<()> {
-        tokio::spawn(async move {
-            Self::perform_commitment_tasks(self.tasks_receiver, self.results_sender).await;
-        })
-    }
-
     pub(crate) async fn perform_commitment_tasks(
         mut tasks_receiver: Receiver<CommitmentTaskInput>,
         mut results_sender: Sender<CommitmentTaskOutput>,
+        committer_client: SharedCommitterClient,
     ) {
         // Placeholder: simply drain the receiver and do nothing.
         // TODO(Amos): Implement the actual commitment tasks logic.
