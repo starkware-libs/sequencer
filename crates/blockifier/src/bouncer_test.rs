cb37ac64a5c9e18f9981402444b48aca74cf16e0
Yonatan Iluz <yonatan@starkware.co>
2026-01-05 14:10:36 +0200

    blockifier: improve revert handling
    wip

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -775,11 +775,11 @@ fn class_hash_migration_data_from_state(
 
     if should_migrate {
         expect![[r#"
-            100709658
+            102995620
         "#]]
         .assert_debug_eq(&migration_sierra_gas.0);
         expect![[r#"
-            243210896
+            248629270
         "#]]
         .assert_debug_eq(&migration_proving_gas.0);
     } else {
