043f994cc3936f1f4a4734a587e6a24c754e71fb
einat-starkware <einat@starkware.co>
2026-01-06 17:40:00 +0200

    apollo_proof_manager: log verify and store times

--- crates/apollo_class_manager_types/src/transaction_converter.rs
+++ crates/apollo_class_manager_types/src/transaction_converter.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use apollo_proof_manager_types::{ProofManagerClientError, SharedProofManagerClient};
 use async_trait::async_trait;
 #[cfg(any(feature = "testing", test))]
@@ -27,6 +29,7 @@ use starknet_api::transaction::fields::{Fee, Proof, ProofFacts};
 use starknet_api::transaction::CalculateContractAddress;
 use starknet_api::{executable_transaction, transaction, StarknetApiError};
 use thiserror::Error;
+use tracing::info;
 
 use crate::{ClassHashes, ClassManagerClientError, SharedClassManagerClient};
 
@@ -353,7 +356,10 @@ impl TransactionConverter {
             return Ok(());
         }
 
+        let verify_start = Instant::now();
         self.verify_proof(proof_facts.clone(), proof.clone())?;
+        let verify_duration = verify_start.elapsed();
+        info!("Proof verification took: {verify_duration:?}");
         Ok(())
     }
 
