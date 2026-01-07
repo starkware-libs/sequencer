c1945e8f31953485c2528eb9be7a83a28ae8d73a
Arnon Hod <arnon@starkware.co>
2026-01-07 07:48:13 +0200

    apollo_gateway: set gateway for benchmark as type

--- crates/apollo_gateway/src/gateway.rs
+++ crates/apollo_gateway/src/gateway.rs
@@ -94,7 +94,7 @@ impl Gateway {
 }
 
 #[derive(Clone)]
-pub(crate) struct GenericGateway<
+pub struct GenericGateway<
     TStatelessValidator: StatelessTransactionValidatorTrait,
     TTransactionConverter: TransactionConverterTrait,
     TStatefulValidatorFactory: StatefulTransactionValidatorFactoryTrait,
@@ -147,7 +147,7 @@ impl<
 > GenericGateway<TStatelessValidator, TTransactionConverter, TStatefulValidatorFactory>
 {
     #[sequencer_latency_histogram(GATEWAY_ADD_TX_LATENCY, true)]
-    pub(crate) async fn add_tx(
+    pub async fn add_tx(
         &self,
         tx: RpcTransaction,
         p2p_message_metadata: Option<BroadcastedMessageMetadata>,
