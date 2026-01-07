8fd3ec9a3ca3db510e95ec4c2f046165e106c46c
einat-starkware <einat@starkware.co>
2026-01-07 16:07:56 +0200

    apollo_gateway: create GCS client and write proof to GCS

--- crates/apollo_integration_tests/src/utils.rs
+++ crates/apollo_integration_tests/src/utils.rs
@@ -630,7 +630,7 @@ pub fn create_gateway_config(
         ..Default::default()
     };
     let contract_class_manager_config = ContractClassManagerConfig::default();
-    let proof_archive_writer_config = ProofArchiveWriterConfig::default();
+    let proof_archive_writer_config = ProofArchiveWriterConfig::create_for_testing();
 
     GatewayConfig {
         stateless_tx_validator_config,
