3950ecd98faaf255d761ac756e2559e4439eb394
meship-starkware <meshi@starkware.co>
2026-01-08 17:06:34 +0200

    starknet_os: add proof fact block number to block hash validations

--- crates/apollo_starknet_os_program/src/virtual_os_test.rs
+++ crates/apollo_starknet_os_program/src/virtual_os_test.rs
@@ -18,11 +18,11 @@ fn test_virtual_os_swapped_files() {
 #[test]
 fn test_program_bytecode_lengths() {
     expect![[r#"
-        15558
+        15621
     "#]]
     .assert_debug_eq(&OS_PROGRAM.data_len());
     expect![[r#"
-        12133
+        12144
     "#]]
     .assert_debug_eq(&VIRTUAL_OS_PROGRAM.data_len());
 }
