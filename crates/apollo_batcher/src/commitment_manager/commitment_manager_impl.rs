534d9cb3f75e5c3452cbefd257f3abf75833ae71
Yoav Gross <yoav@starkware.co>
2026-01-11 15:51:21 +0200

    apollo_batcher: define custom input task type

--- crates/apollo_batcher/src/commitment_manager/commitment_manager_impl.rs
+++ crates/apollo_batcher/src/commitment_manager/commitment_manager_impl.rs
@@ -4,11 +4,7 @@ use std::sync::Arc;
 
 use apollo_batcher_config::config::{BatcherConfig, CommitmentManagerConfig};
 use apollo_committer_types::committer_types::{CommitBlockRequest, CommitBlockResponse};
-use apollo_committer_types::communication::{
-    CommitterRequest,
-    CommitterRequestLabelValue,
-    SharedCommitterClient,
-};
+use apollo_committer_types::communication::{CommitterRequestLabelValue, SharedCommitterClient};
 use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::block_hash::block_hash_calculator::{
     calculate_block_hash,
@@ -100,12 +96,11 @@ impl<S: StateCommitterTrait> CommitmentManager<S> {
                 state_diff_commitment,
             });
         }
-        let commitment_task_input =
-            CommitterTaskInput(CommitterRequest::CommitBlock(CommitBlockRequest {
-                height,
-                state_diff,
-                state_diff_commitment,
-            }));
+        let commitment_task_input = CommitterTaskInput::Commit(CommitBlockRequest {
+            height,
+            state_diff,
+            state_diff_commitment,
+        });
         let error_message = format!(
             "Failed to send commitment task to state committer. Block: {height}, state diff \
              commitment: {state_diff_commitment:?}",
