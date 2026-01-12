2e97159494237b381713a7c61c773195870c43a5
Yonatan Iluz <yonatan@starkware.co>
2026-01-12 17:00:15 +0200

    starknet_os: move common utils to HintArgs struct

--- crates/starknet_os/src/hints/hint_implementation/resources.rs
+++ crates/starknet_os/src/hints/hint_implementation/resources.rs
@@ -1,9 +1,5 @@
 use blockifier::execution::contract_class::TrackedResource;
 use blockifier::state::state_api::StateReader;
-use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
-    get_integer_from_var_name,
-    insert_value_from_var_name,
-};
 use starknet_types_core::felt::Felt;
 
 use crate::hint_processor::snos_hint_processor::SnosHintProcessor;
@@ -11,25 +7,16 @@ use crate::hints::error::{OsHintError, OsHintResult};
 use crate::hints::types::HintArgs;
 use crate::hints::vars::Ids;
 
-pub(crate) fn remaining_gas_gt_max(
-    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
-) -> OsHintResult {
-    let remaining_gas =
-        get_integer_from_var_name(Ids::RemainingGas.into(), vm, ids_data, ap_tracking)?;
-    let max_gas = get_integer_from_var_name(Ids::MaxGas.into(), vm, ids_data, ap_tracking)?;
+pub(crate) fn remaining_gas_gt_max(mut ctx: HintArgs<'_>) -> OsHintResult {
+    let remaining_gas = ctx.get_integer(Ids::RemainingGas)?;
+    let max_gas = ctx.get_integer(Ids::MaxGas)?;
     let remaining_gas_gt_max: Felt = (remaining_gas > max_gas).into();
-    Ok(insert_value_from_var_name(
-        Ids::RemainingGasGtMax.into(),
-        remaining_gas_gt_max,
-        vm,
-        ids_data,
-        ap_tracking,
-    )?)
+    Ok(ctx.insert_value(Ids::RemainingGasGtMax, remaining_gas_gt_max)?)
 }
 
 pub(crate) fn debug_expected_initial_gas<S: StateReader>(
     hint_processor: &mut SnosHintProcessor<'_, S>,
-    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
+    ctx: HintArgs<'_>,
 ) -> OsHintResult {
     let current_execution_helper =
         hint_processor.execution_helpers_manager.get_current_execution_helper()?;
@@ -40,8 +27,7 @@ pub(crate) fn debug_expected_initial_gas<S: StateReader>(
             .get_call_info_tracker()?
             .call_info;
         let expected_initial_gas = Felt::from(call_info.call.initial_gas);
-        let call_initial_gas =
-            get_integer_from_var_name(Ids::InnerRemainingGas.into(), vm, ids_data, ap_tracking)?;
+        let call_initial_gas = ctx.get_integer(Ids::InnerRemainingGas)?;
         if expected_initial_gas != call_initial_gas {
             return Err(OsHintError::AssertionFailed {
                 message: format!(
@@ -56,15 +42,9 @@ pub(crate) fn debug_expected_initial_gas<S: StateReader>(
 
 pub(crate) fn is_sierra_gas_mode<S: StateReader>(
     hint_processor: &mut SnosHintProcessor<'_, S>,
-    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
+    mut ctx: HintArgs<'_>,
 ) -> OsHintResult {
     let gas_mode = hint_processor.get_current_call_info_tracker()?.call_info.tracked_resource;
 
-    Ok(insert_value_from_var_name(
-        Ids::IsSierraGasMode.into(),
-        Felt::from(gas_mode == TrackedResource::SierraGas),
-        vm,
-        ids_data,
-        ap_tracking,
-    )?)
+    Ok(ctx.insert_value(Ids::IsSierraGasMode, Felt::from(gas_mode == TrackedResource::SierraGas))?)
 }
--- crates/starknet_os/src/hints/hint_implementation/stateless_compression/implementation.rs
+++ crates/starknet_os/src/hints/hint_implementation/stateless_compression/implementation.rs
@@ -1,11 +1,6 @@
 use std::collections::HashMap;
 
-use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::{
-    get_integer_from_var_name,
-    get_maybe_relocatable_from_var_name,
-    get_ptr_from_var_name,
-    insert_value_from_var_name,
-};
+use cairo_vm::hint_processor::builtin_hint_processor::hint_utils::get_maybe_relocatable_from_var_name;
 use cairo_vm::types::relocatable::MaybeRelocatable;
 use starknet_types_core::felt::Felt;
 
@@ -15,57 +10,57 @@ use crate::hints::hint_implementation::stateless_compression::utils::TOTAL_N_BUC
 use crate::hints::types::HintArgs;
 use crate::hints::vars::{Ids, Scope};
 
-pub(crate) fn dictionary_from_bucket(HintArgs { exec_scopes, .. }: HintArgs<'_>) -> OsHintResult {
+pub(crate) fn dictionary_from_bucket(ctx: HintArgs<'_>) -> OsHintResult {
     let initial_dict: HashMap<MaybeRelocatable, MaybeRelocatable> = (0..TOTAL_N_BUCKETS)
         .map(|bucket_index| (Felt::from(bucket_index).into(), Felt::ZERO.into()))
         .collect();
-    exec_scopes.insert_box(Scope::InitialDict.into(), Box::new(initial_dict));
+    ctx.exec_scopes.insert_box(Scope::InitialDict.into(), Box::new(initial_dict));
     Ok(())
 }
 
-pub(crate) fn get_prev_offset(
-    HintArgs { vm, exec_scopes, ids_data, ap_tracking, .. }: HintArgs<'_>,
-) -> OsHintResult {
-    let dict_manager = exec_scopes.get_dict_manager()?;
+pub(crate) fn get_prev_offset(mut ctx: HintArgs<'_>) -> OsHintResult {
+    let dict_manager = ctx.exec_scopes.get_dict_manager()?;
 
-    let dict_ptr = get_ptr_from_var_name(Ids::DictPtr.into(), vm, ids_data, ap_tracking)?;
-    let bucket_index =
-        get_maybe_relocatable_from_var_name(Ids::BucketIndex.into(), vm, ids_data, ap_tracking)?;
+    let dict_ptr = ctx.get_ptr(Ids::DictPtr)?;
+    let bucket_index = get_maybe_relocatable_from_var_name(
+        Ids::BucketIndex.into(),
+        ctx.vm,
+        ctx.ids_data,
+        ctx.ap_tracking,
+    )?;
     let prev_offset =
         dict_manager.borrow_mut().get_tracker_mut(dict_ptr)?.get_value(&bucket_index)?.clone();
-    insert_value_from_var_name(Ids::PrevOffset.into(), prev_offset, vm, ids_data, ap_tracking)?;
+    ctx.insert_value(Ids::PrevOffset, prev_offset)?;
     Ok(())
 }
 
-pub(crate) fn compression_hint(
-    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
-) -> OsHintResult {
-    let data_start = get_ptr_from_var_name(Ids::DataStart.into(), vm, ids_data, ap_tracking)?;
-    let data_end = get_ptr_from_var_name(Ids::DataEnd.into(), vm, ids_data, ap_tracking)?;
+pub(crate) fn compression_hint(ctx: HintArgs<'_>) -> OsHintResult {
+    let data_start = ctx.get_ptr(Ids::DataStart)?;
+    let data_end = ctx.get_ptr(Ids::DataEnd)?;
     let data_size = (data_end - data_start)?;
 
-    let compressed_dst =
-        get_ptr_from_var_name(Ids::CompressedDst.into(), vm, ids_data, ap_tracking)?;
-    let data =
-        vm.get_integer_range(data_start, data_size)?.into_iter().map(|f| *f).collect::<Vec<_>>();
+    let compressed_dst = ctx.get_ptr(Ids::CompressedDst)?;
+    let data = ctx
+        .vm
+        .get_integer_range(data_start, data_size)?
+        .into_iter()
+        .map(|f| *f)
+        .collect::<Vec<_>>();
     let compress_result =
         compress(&data).into_iter().map(MaybeRelocatable::Int).collect::<Vec<_>>();
 
-    vm.write_arg(compressed_dst, &compress_result)?;
+    ctx.vm.write_arg(compressed_dst, &compress_result)?;
 
     Ok(())
 }
 
-pub(crate) fn set_decompressed_dst(
-    HintArgs { vm, ids_data, ap_tracking, .. }: HintArgs<'_>,
-) -> OsHintResult {
-    let decompressed_dst =
-        get_ptr_from_var_name(Ids::DecompressedDst.into(), vm, ids_data, ap_tracking)?;
+pub(crate) fn set_decompressed_dst(ctx: HintArgs<'_>) -> OsHintResult {
+    let decompressed_dst = ctx.get_ptr(Ids::DecompressedDst)?;
 
-    let packed_felt = get_integer_from_var_name(Ids::PackedFelt.into(), vm, ids_data, ap_tracking)?;
-    let elm_bound = get_integer_from_var_name(Ids::ElmBound.into(), vm, ids_data, ap_tracking)?;
+    let packed_felt = ctx.get_integer(Ids::PackedFelt)?;
+    let elm_bound = ctx.get_integer(Ids::ElmBound)?;
 
-    vm.insert_value(
+    ctx.vm.insert_value(
         decompressed_dst,
         packed_felt.div_rem(&elm_bound.try_into().expect("elm_bound is zero")).1,
     )?;
