99d30103957dc252f7310260d78f92bb3457ea30
Arnon Hod <arnon@starkware.co>
2026-01-13 13:54:48 +0200

    apollo_integration_tests: merge test utils files

--- crates/apollo_integration_tests/tests/multiple_account_txs_flow_test.rs
+++ crates/apollo_integration_tests/tests/multiple_account_txs_flow_test.rs
@@ -1,10 +1,11 @@
 use apollo_infra_utils::test_utils::TestIdentifier;
-use apollo_integration_tests::end_to_end_flow_utils::{
+use apollo_integration_tests::utils::{
     end_to_end_flow,
     EndToEndFlowArgs,
     EndToEndTestScenario,
+    ACCOUNT_ID_0,
+    ACCOUNT_ID_1,
 };
-use apollo_integration_tests::utils::{ACCOUNT_ID_0, ACCOUNT_ID_1};
 use blockifier::bouncer::BouncerWeights;
 use mempool_test_utils::starknet_api_test_utils::MultiAccountTransactionGenerator;
 use starknet_api::rpc_transaction::RpcTransaction;
