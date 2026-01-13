7ed8d4a48a46a31a074f937283bbc94101f77887
Arnon Hod <arnon@starkware.co>
2026-01-13 07:20:40 +0200

    apollo_integration_tests: use validate_tx_count accross integration tests (#11503)

--- crates/apollo_integration_tests/tests/test_many.rs
+++ crates/apollo_integration_tests/tests/test_many.rs
@@ -3,11 +3,12 @@ use apollo_integration_tests::utils::{create_invoke_txs, ACCOUNT_ID_1};
 use mempool_test_utils::starknet_api_test_utils::MultiAccountTransactionGenerator;
 use starknet_api::execution_resources::GasAmount;
 use starknet_api::rpc_transaction::RpcTransaction;
-use starknet_api::transaction::TransactionHash;
 
-use crate::common::{end_to_end_flow, EndToEndFlowArgs, TestScenario};
+use crate::common::{end_to_end_flow, validate_tx_count, EndToEndFlowArgs, TestScenario};
 
 mod common;
+
+const N_TXS: usize = 15;
 
 /// This test checks that at least one block is full.
 /// The test uses 3 threads: 1 for the test's main thread and 2 for the sequencers.
@@ -29,23 +30,13 @@ fn create_many_txs_scenario() -> Vec<TestScenario> {
     vec![TestScenario {
         create_rpc_txs_fn: create_many_invoke_txs,
         create_l1_to_l2_messages_args_fn: |_| vec![],
-        test_tx_hashes_fn: test_many_invoke_txs,
+        test_tx_hashes_fn: |tx_hashes| validate_tx_count(tx_hashes, N_TXS),
     }]
-}
-
-pub fn test_many_invoke_txs(tx_hashes: &[TransactionHash]) -> Vec<TransactionHash> {
-    assert!(
-        tx_hashes.len() == 15,
-        "Unexpected number of transactions sent in the test scenario. Found {} transactions",
-        tx_hashes.len()
-    );
-    tx_hashes.to_vec()
 }
 
 /// Creates and sends more transactions than can fit in a block.
 pub fn create_many_invoke_txs(
     tx_generator: &mut MultiAccountTransactionGenerator,
 ) -> Vec<RpcTransaction> {
-    const N_TXS: usize = 15;
     create_invoke_txs(tx_generator, ACCOUNT_ID_1, N_TXS)
 }
