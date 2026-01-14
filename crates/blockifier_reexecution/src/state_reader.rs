e4862fe60ed086609b05804bba1927565c576fdd
Lev Roitman <lev@starkware.co>
2026-01-14 11:27:09 +0200

    blockifier_reexecution: merge rpc_state_reader into state_reader

--- crates/blockifier_reexecution/src/state_reader.rs
+++ crates/blockifier_reexecution/src/state_reader.rs
@@ -1,9 +1,13 @@
+pub mod config;
 pub mod offline_state_reader;
 #[cfg(test)]
 mod offline_state_reader_test;
 #[cfg(test)]
 mod raw_rpc_json_test;
 pub mod reexecution_state_reader;
+pub mod rpc_objects;
 pub mod rpc_state_reader;
+#[cfg(test)]
+mod rpc_state_reader_basic_test;
 #[cfg(all(test, feature = "blockifier_regression_https_testing"))]
 mod rpc_state_reader_test;
