0d2ffa2d6079788f24651e3b5a2bc3d586ce8c22
github-action-benchmark <github@users.noreply.github.com>
2026-01-17 18:59:37 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e165d744d65f3d562a0add7294fc4910bb8f28c6

---
+++ dev/bench/01-17-ci_add_benchmark_publish_workflow_for_github_pages/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768676377806,
+  "repoUrl": "https://github.com/starkware-libs/sequencer",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dan@starkware.co",
+            "name": "Dan Brownstein",
+            "username": "dan-starkware"
+          },
+          "committer": {
+            "email": "dan@starkware.co",
+            "name": "Dan Brownstein",
+            "username": "dan-starkware"
+          },
+          "distinct": true,
+          "id": "e165d744d65f3d562a0add7294fc4910bb8f28c6",
+          "message": "ci: add benchmark publish workflow for GitHub Pages",
+          "timestamp": "2026-01-17T20:55:06+02:00",
+          "tree_id": "4d60a931216225a42c7e430007b8c2dde872ed64",
+          "url": "https://github.com/starkware-libs/sequencer/commit/e165d744d65f3d562a0add7294fc4910bb8f28c6"
+        },
+        "date": 1768676377534,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "full_committer_flow",
+            "value": 14.082,
+            "unit": "ms"
+          },
+          {
+            "name": "tree_computation_flow",
+            "value": 12.965,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
