962a5889d42a8d8ba2dd8d73317c503280130557
Itay Tsabary <itayt@starkware.co>
2026-01-18 17:13:53 +0200

    apollo_dashboard: add severity placeholders

--- crates/apollo_dashboard/src/alert_placeholders.rs
+++ crates/apollo_dashboard/src/alert_placeholders.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Serializer};
 
+use crate::alerts::AlertSeverity;
+
 #[derive(Clone, Debug, PartialEq)]
 pub(crate) enum ComparisonValueOrPlaceholder {
     ConcreteValue(f64),
@@ -26,6 +28,38 @@ impl Serialize for ComparisonValueOrPlaceholder {
         match self {
             ComparisonValueOrPlaceholder::ConcreteValue(value) => value.serialize(serializer),
             ComparisonValueOrPlaceholder::Placeholder(placeholder) => {
+                placeholder.serialize(serializer)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) enum SeverityValueOrPlaceholder {
+    ConcreteValue(AlertSeverity),
+    Placeholder(String),
+}
+
+impl From<AlertSeverity> for SeverityValueOrPlaceholder {
+    fn from(value: AlertSeverity) -> Self {
+        SeverityValueOrPlaceholder::ConcreteValue(value)
+    }
+}
+
+impl From<String> for SeverityValueOrPlaceholder {
+    fn from(value: String) -> Self {
+        SeverityValueOrPlaceholder::Placeholder(value)
+    }
+}
+
+impl Serialize for SeverityValueOrPlaceholder {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            SeverityValueOrPlaceholder::ConcreteValue(severity) => severity.serialize(serializer),
+            SeverityValueOrPlaceholder::Placeholder(placeholder) => {
                 placeholder.serialize(serializer)
             }
         }
