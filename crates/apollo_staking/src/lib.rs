372d20fac2195a39732186741e6a71d016a1f3db
Dafna Matsry <dafna.matsry@starkware.co>
2026-01-18 13:48:23 +0200

    apollo_staking: split the `StakingMangerConfig` into dynamic and static configs

--- crates/apollo_staking/src/lib.rs
+++ crates/apollo_staking/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod cairo_staking_contract;
 pub mod committee_provider;
-pub mod config;
 pub mod contract_types;
 pub mod staking_contract;
 pub mod staking_manager;
--- crates/apollo_staking_config/src/config.rs
+++ crates/apollo_staking_config/src/config.rs
@@ -1,17 +1,47 @@
 use std::collections::BTreeMap;
 
-use apollo_config::dumping::{ser_param, SerializeConfig};
+use apollo_config::dumping::{prepend_sub_config_name, ser_param, SerializeConfig};
 use apollo_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct StakingManagerConfig {
-    pub max_cached_epochs: usize,
+    pub dynamic_config: StakingManagerDynamicConfig,
+    pub static_config: StakingManagerStaticConfig,
+}
 
+impl SerializeConfig for StakingManagerConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        let mut config = BTreeMap::new();
+        config.extend(prepend_sub_config_name(self.dynamic_config.dump(), "dynamic_config"));
+        config.extend(prepend_sub_config_name(self.static_config.dump(), "static_config"));
+        config
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Default, Validate)]
+pub struct StakingManagerDynamicConfig {
     // The desired number of committee members to select from the available stakers.
     // If there are fewer stakers than `committee_size`, a smaller committee will be selected.
-    // TODO(Dafna): Make this a dynamic config.
+    // TODO(Dafna): Add an epoch, from which this config should be applied.
     pub committee_size: usize,
+}
+
+impl SerializeConfig for StakingManagerDynamicConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from_iter([ser_param(
+            "committee_size",
+            &self.committee_size,
+            "The desired number of committee members to select from the available stakers",
+            ParamPrivacyInput::Public,
+        )])
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+pub struct StakingManagerStaticConfig {
+    pub max_cached_epochs: usize,
 
     // Defines how many heights in advance the proposer can be predicted.
     // While the exact identity may depend on staker prediction constraints,
@@ -21,19 +51,13 @@ pub struct StakingManagerConfig {
     pub proposer_prediction_window_in_heights: u64,
 }
 
-impl SerializeConfig for StakingManagerConfig {
+impl SerializeConfig for StakingManagerStaticConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         BTreeMap::from_iter([
             ser_param(
                 "max_cached_epochs",
                 &self.max_cached_epochs,
                 "The maximum number of epochs to cache",
-                ParamPrivacyInput::Public,
-            ),
-            ser_param(
-                "committee_size",
-                &self.committee_size,
-                "The desired number of committee members to select from the available stakers",
                 ParamPrivacyInput::Public,
             ),
             ser_param(
