17c683584bae34e2fa5a1abd58615d23cff5b20a
ArielElp <86294909+ArielElp@users.noreply.github.com>
2026-01-25 10:53:06 +0200

    starknet_committer: index db metadata impl (#11840)
    * starknet_committer,starknet_patricia_storage: index db read initial roots
    
    * starknet_committer: index db metadata impl

--- crates/starknet_committer/src/db/index_db/leaves.rs
+++ crates/starknet_committer/src/db/index_db/leaves.rs
@@ -1,6 +1,4 @@
-use std::sync::LazyLock;
-
-use starknet_api::core::{ClassHash, ContractAddress, Nonce, PATRICIA_KEY_UPPER_BOUND};
+use starknet_api::core::{ClassHash, ContractAddress, Nonce};
 use starknet_api::hash::HashOutput;
 use starknet_patricia::patricia_merkle_tree::node_data::errors::LeafResult;
 use starknet_patricia::patricia_merkle_tree::node_data::leaf::Leaf;
@@ -15,6 +13,7 @@ use starknet_patricia_storage::storage_trait::{DbKeyPrefix, DbValue};
 use starknet_types_core::felt::Felt;
 
 use crate::block_committer::input::StarknetStorageValue;
+use crate::db::index_db::db::{CLASSES_TREE_PREFIX, CONTRACTS_TREE_PREFIX};
 use crate::db::mock_forest_storage::EMPTY_DB_KEY_SEPARATOR;
 use crate::patricia_merkle_tree::leaf::leaf_impl::ContractState;
 use crate::patricia_merkle_tree::types::CompiledClassHash;
@@ -35,22 +34,6 @@ pub struct IndexLayoutCompiledClassHash(pub CompiledClassHash);
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, derive_more::From, derive_more::Into)]
 pub struct IndexLayoutStarknetStorageValue(pub StarknetStorageValue);
-
-/// Set to 2^251 + 1 to avoid collisions with contract addresses prefixes.
-static FIRST_AVAILABLE_PREFIX_FELT: LazyLock<Felt> =
-    LazyLock::new(|| Felt::from_hex_unchecked(PATRICIA_KEY_UPPER_BOUND) + Felt::ONE);
-
-/// The db key prefix of nodes in the contracts trie.
-///
-/// Set to FIRST_AVAILABLE_PREFIX_FELT
-pub(crate) static CONTRACTS_TREE_PREFIX: LazyLock<[u8; 32]> =
-    LazyLock::new(|| FIRST_AVAILABLE_PREFIX_FELT.to_bytes_be());
-
-/// The db key prefix of nodes in the contracts trie.
-///
-/// Set to FIRST_AVAILABLE_PREFIX_FELT + 1
-pub(crate) static CLASSES_TREE_PREFIX: LazyLock<[u8; 32]> =
-    LazyLock::new(|| (*FIRST_AVAILABLE_PREFIX_FELT + Felt::ONE).to_bytes_be());
 
 macro_rules! impl_has_static_prefix_empty_context {
     ($($ty:ty => $prefix:expr),* $(,)?) => {
