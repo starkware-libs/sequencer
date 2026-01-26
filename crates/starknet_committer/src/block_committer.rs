a98ca33e73b4d965e2189915f4a787aad7a1fa80
rotem-starkware <rotem@starkware.co>
2026-01-26 09:23:08 +0200

    starknet_committer: rename timing_util and TimeMeasurementTrait and structs

--- crates/starknet_committer/src/block_committer.rs
+++ crates/starknet_committer/src/block_committer.rs
@@ -1,8 +1,8 @@
 pub mod commit;
 pub mod errors;
 pub mod input;
+pub mod measurements_util;
 #[cfg(any(feature = "testing", test))]
 pub mod random_structs;
 #[cfg(any(feature = "testing", test))]
 pub mod state_diff_generator;
-pub mod timing_util;
