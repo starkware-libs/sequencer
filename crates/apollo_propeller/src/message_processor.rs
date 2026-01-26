17f16dacb5934ee84791c4b52d1051b7065bccb3
Andrew Luka <andrew.l@starkware.co>
2026-01-26 09:17:39 +0200

    apollo_propeller: add unit validation receiving and spawning

--- crates/apollo_propeller/src/message_processor.rs
+++ crates/apollo_propeller/src/message_processor.rs
@@ -9,15 +9,17 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use libp2p::identity::{PeerId, PublicKey};
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 use tokio::time::sleep_until;
 
 use crate::tree::PropellerScheduleManager;
-use crate::types::{Channel, Event, MessageRoot};
+use crate::types::{Channel, Event, MessageRoot, ShardValidationError};
 use crate::unit::PropellerUnit;
+use crate::unit_validator::UnitValidator;
 use crate::ShardIndex;
 
 pub type UnitToValidate = (PeerId, PropellerUnit);
+type ValidationResult = (Result<(), ShardValidationError>, UnitValidator, PropellerUnit);
 
 /// Messages sent from MessageProcessor to Engine.
 #[derive(Debug)]
@@ -62,14 +64,34 @@ impl MessageProcessor {
 
         // Local state variables
         let deadline = tokio::time::Instant::now() + self.timeout;
+        let mut validator = Some(UnitValidator::new(
+            self.channel,
+            self.publisher,
+            self.publisher_public_key.clone(),
+            self.message_root,
+            Arc::clone(&self.tree_manager),
+        ));
+        let mut pending_validation: Option<oneshot::Receiver<ValidationResult>> = None;
 
-        // TODO(AndrewL): remove this
-        #[allow(clippy::never_loop)]
         loop {
             tokio::select! {
                 _ = sleep_until(deadline) => {
                     let _ = self.emit_timeout_and_finalize().await;
                     break;
+                }
+
+                Some((sender, unit)) = self.unit_rx.recv(), if pending_validation.is_none() => {
+                    tracing::trace!("[MSG_PROC] Validating unit from sender={:?} index={:?}", sender, unit.index());
+
+                    let (result_tx, result_rx) = oneshot::channel();
+                    let mut validator_moved = validator.take().unwrap();
+
+                    rayon::spawn(move || {
+                        let r = validator_moved.validate_shard(sender, &unit);
+                        let _ = result_tx.send((r, validator_moved, unit));
+                    });
+
+                    pending_validation = Some(result_rx);
                 }
             }
         }
