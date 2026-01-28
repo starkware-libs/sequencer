27888647ce7e78a09b882b3be010b2b76b8cc67b
Ayelet Zilber <ayelet@starkware.co>
2026-01-28 14:40:16 +0200

    apollo_mempool: add rewind func, add helper func

--- crates/apollo_mempool/src/transaction_queue_trait.rs
+++ crates/apollo_mempool/src/transaction_queue_trait.rs
@@ -1,6 +1,9 @@
+use std::collections::{HashMap, HashSet};
+
 use apollo_mempool_types::mempool_types::TransactionQueueSnapshot;
 use starknet_api::block::GasPrice;
 use starknet_api::core::{ContractAddress, Nonce};
+use starknet_api::transaction::TransactionHash;
 
 use crate::mempool::TransactionReference;
 
@@ -22,4 +25,9 @@ pub trait TransactionQueueTrait: Send + Sync {
     fn iter_over_ready_txs(&self) -> impl Iterator<Item = &TransactionReference>;
 
     fn queue_snapshot(&self) -> TransactionQueueSnapshot;
+
+    fn rewind_txs(
+        &mut self,
+        next_txs_by_address: HashMap<ContractAddress, TransactionReference>,
+    ) -> HashSet<TransactionHash>;
 }
