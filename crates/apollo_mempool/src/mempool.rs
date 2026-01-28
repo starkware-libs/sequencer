363528331e1d3e13451f08d925445ebe34b3cc20
Ayelet Zilber <ayelet@starkware.co>
2026-01-28 14:40:16 +0200

    apollo_mempool: add rewind func, add helper func

--- crates/apollo_mempool/src/mempool.rs
+++ crates/apollo_mempool/src/mempool.rs
@@ -439,6 +439,32 @@ impl Mempool {
         self.tx_queue.insert(tx_reference, self.config.static_config.validate_resource_bounds);
     }
 
+    fn remove_rejected_txs(
+        &mut self,
+        rejected_tx_hashes: IndexSet<TransactionHash>,
+    ) -> AddressToNonce {
+        if !rejected_tx_hashes.is_empty() {
+            debug!("Removed rejected transactions from mempool: {:?}", rejected_tx_hashes);
+        }
+        metric_count_rejected_txs(rejected_tx_hashes.len());
+        let mut account_nonce_updates = AddressToNonce::new();
+        for tx_hash in rejected_tx_hashes {
+            if let Ok(tx) = self.tx_pool.remove(tx_hash) {
+                self.tx_queue.remove_by_address(tx.contract_address());
+                account_nonce_updates
+                    .entry(tx.contract_address())
+                    .and_modify(|nonce| *nonce = (*nonce).min(tx.nonce()))
+                    .or_insert(tx.nonce());
+            } else {
+                continue; // Transaction hash unknown to mempool, from a different node.
+            }
+
+            // TODO(clean_accounts): remove address with no transactions left after a block cycle /
+            // TTL.
+        }
+        account_nonce_updates
+    }
+
     fn add_tx_inner(&mut self, args: AddTransactionArgs) {
         let AddTransactionArgs { tx, account_state } = args;
         info!("Adding transaction to mempool.");
@@ -519,38 +545,21 @@ impl Mempool {
 
         // Commit block and rewind nonces of addresses that were not included in block.
         let addresses_to_rewind = self.state.commit(address_to_nonce);
+        let mut next_txs_by_address = HashMap::new();
         for address in addresses_to_rewind {
             // Account nonce is the minimal nonce of this address: it was proposed but not included.
             let tx_reference =
                 self.tx_pool.account_txs_sorted_by_nonce(address).next().unwrap_or_else(|| {
                     panic!("Address {address} should appear in transaction pool.")
                 });
-            self.tx_queue.remove_by_address(address);
-            self.insert_to_tx_queue(*tx_reference);
+            next_txs_by_address.insert(address, *tx_reference);
         }
+        self.tx_queue.rewind_txs(next_txs_by_address);
 
         debug!("Aligned mempool to committed nonces.");
 
         // Remove rejected transactions from the mempool.
-        if !rejected_tx_hashes.is_empty() {
-            debug!("Removed rejected transactions from mempool: {:?}", rejected_tx_hashes);
-        }
-        metric_count_rejected_txs(rejected_tx_hashes.len());
-        let mut account_nonce_updates = AddressToNonce::new();
-        for tx_hash in rejected_tx_hashes {
-            if let Ok(tx) = self.tx_pool.remove(tx_hash) {
-                self.tx_queue.remove_by_address(tx.contract_address());
-                account_nonce_updates
-                    .entry(tx.contract_address())
-                    .and_modify(|nonce| *nonce = (*nonce).min(tx.nonce()))
-                    .or_insert(tx.nonce());
-            } else {
-                continue; // Transaction hash unknown to mempool, from a different node.
-            };
-
-            // TODO(clean_accounts): remove address with no transactions left after a block cycle /
-            // TTL.
-        }
+        let mut account_nonce_updates = self.remove_rejected_txs(rejected_tx_hashes);
 
         // Committed nonces should overwrite rejected transactions.
         account_nonce_updates.extend(committed_nonce_updates);
