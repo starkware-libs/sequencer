6231c37f743761db955358819c35bf534db64654
rotem-starkware <rotem@starkware.co>
2026-02-02 11:04:33 +0200

    apollo_dashboard: add to committer dashboard panels of modifications counts

--- crates/apollo_dashboard/src/panels/committer.rs
+++ crates/apollo_dashboard/src/panels/committer.rs
@@ -3,6 +3,11 @@ use apollo_committer::metrics::{
     AVERAGE_READ_RATE,
     AVERAGE_WRITE_RATE,
     COMPUTE_DURATION_PER_BLOCK,
+    COUNT_CLASSES_TRIE_MODIFICATIONS_PER_BLOCK,
+    COUNT_CONTRACTS_TRIE_MODIFICATIONS_PER_BLOCK,
+    COUNT_EMPTIED_LEAVES_PER_BLOCK,
+    COUNT_STORAGE_TRIES_MODIFICATIONS_PER_BLOCK,
+    EMPTIED_LEAVES_PERCENTAGE_PER_BLOCK,
     OFFSET,
     READ_DURATION_PER_BLOCK,
     TOTAL_BLOCK_DURATION,
@@ -15,6 +20,7 @@ use crate::panel::{Panel, PanelType, Unit};
 
 const BLOCK_DURATIONS_LOG_QUERY: &str = "\"Block\" AND \"durations in ms\"";
 const RATES_LOG_QUERY: &str = "\"Block\" AND \"rates\"";
+const COUNT_MODIFICATIONS_LOG_QUERY: &str = "\"Block\" AND \"modifications count\"";
 
 fn get_offset_panel() -> Panel {
     Panel::new(
@@ -84,6 +90,52 @@ fn get_average_write_rate_panel() -> Panel {
     .with_log_query(RATES_LOG_QUERY)
 }
 
+fn get_count_storage_tries_modifications_per_block_panel() -> Panel {
+    Panel::from_hist(
+        &COUNT_STORAGE_TRIES_MODIFICATIONS_PER_BLOCK,
+        "Storage Tries Modifications per Block",
+        "Count of storage tries modifications per block",
+    )
+    .with_log_query(COUNT_MODIFICATIONS_LOG_QUERY)
+}
+
+fn get_count_contracts_trie_modifications_per_block_panel() -> Panel {
+    Panel::from_hist(
+        &COUNT_CONTRACTS_TRIE_MODIFICATIONS_PER_BLOCK,
+        "Contracts Trie Modifications per Block",
+        "Count of contracts trie modifications per block",
+    )
+    .with_log_query(COUNT_MODIFICATIONS_LOG_QUERY)
+}
+
+fn get_count_classes_trie_modifications_per_block_panel() -> Panel {
+    Panel::from_hist(
+        &COUNT_CLASSES_TRIE_MODIFICATIONS_PER_BLOCK,
+        "Classes Trie Modifications per Block",
+        "Count of classes trie modifications per block",
+    )
+    .with_log_query(COUNT_MODIFICATIONS_LOG_QUERY)
+}
+
+fn get_count_emptied_leaves_per_block_panel() -> Panel {
+    Panel::from_hist(
+        &COUNT_EMPTIED_LEAVES_PER_BLOCK,
+        "Emptied Leaves per Block",
+        "Count of storage tries leaves emptied per block",
+    )
+    .with_log_query(COUNT_MODIFICATIONS_LOG_QUERY)
+}
+
+fn get_percentage_emptied_leaves_per_block_panel() -> Panel {
+    Panel::from_hist(
+        &EMPTIED_LEAVES_PERCENTAGE_PER_BLOCK,
+        "Percentage Emptied Leaves per Block",
+        "Percentage of storage tries leaves emptied over the total number of storage tries leaves \
+         per block",
+    )
+    .with_log_query(COUNT_MODIFICATIONS_LOG_QUERY)
+}
+
 pub(crate) fn get_committer_row() -> Row {
     Row::new(
         "Committer",
@@ -96,6 +148,11 @@ pub(crate) fn get_committer_row() -> Row {
             get_average_compute_rate_panel(),
             get_write_duration_per_block_panel(),
             get_average_write_rate_panel(),
+            get_count_storage_tries_modifications_per_block_panel(),
+            get_count_contracts_trie_modifications_per_block_panel(),
+            get_count_classes_trie_modifications_per_block_panel(),
+            get_count_emptied_leaves_per_block_panel(),
+            get_percentage_emptied_leaves_per_block_panel(),
         ],
     )
 }
