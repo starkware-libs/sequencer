76b6e1fb2f94ba36502ef0ecc65524d2d7694d83
nadin-Starkware <nadin@starkware.co>
2026-02-05 14:44:01 +0200

    apollo_config_manager: add state_sync dynamic config to config manager

--- crates/apollo_node_config/src/node_config.rs
+++ crates/apollo_node_config/src/node_config.rs
@@ -39,7 +39,7 @@ use apollo_monitoring_endpoint_config::config::MonitoringEndpointConfig;
 use apollo_reverts::RevertConfig;
 use apollo_sierra_compilation_config::config::SierraCompilationConfig;
 use apollo_staking_config::config::StakingManagerDynamicConfig;
-use apollo_state_sync_config::config::StateSyncConfig;
+use apollo_state_sync_config::config::{StateSyncConfig, StateSyncDynamicConfig};
 use blockifier::blockifier_versioned_constants::VersionedConstantsOverrides;
 use clap::Command;
 use papyrus_base_layer::ethereum_base_layer_contract::EthereumBaseLayerConfig;
@@ -320,6 +320,8 @@ pub struct NodeDynamicConfig {
     pub mempool_dynamic_config: Option<MempoolDynamicConfig>,
     #[validate(nested)]
     pub staking_manager_dynamic_config: Option<StakingManagerDynamicConfig>,
+    #[validate(nested)]
+    pub state_sync_dynamic_config: Option<StateSyncDynamicConfig>,
 }
 
 impl SerializeConfig for NodeDynamicConfig {
@@ -334,6 +336,7 @@ impl SerializeConfig for NodeDynamicConfig {
                 &self.staking_manager_dynamic_config,
                 "staking_manager_dynamic_config",
             ),
+            ser_optional_sub_config(&self.state_sync_dynamic_config, "state_sync_dynamic_config"),
         ];
         sub_configs.into_iter().flatten().collect()
     }
@@ -370,6 +373,10 @@ impl From<&SequencerNodeConfig> for NodeDynamicConfig {
             .map(|consensus_manager_config| {
                 consensus_manager_config.staking_manager_config.dynamic_config.clone()
             });
+        let state_sync_dynamic_config = sequencer_node_config
+            .state_sync_config
+            .as_ref()
+            .map(|state_sync_config| state_sync_config.dynamic_config.clone());
         Self {
             batcher_dynamic_config,
             consensus_dynamic_config,
@@ -377,6 +384,7 @@ impl From<&SequencerNodeConfig> for NodeDynamicConfig {
             http_server_dynamic_config,
             mempool_dynamic_config,
             staking_manager_dynamic_config,
+            state_sync_dynamic_config,
         }
     }
 }
