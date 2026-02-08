dc89714e41f02d27239a7bfc3e0c3922cd4077df
Yonatan Iluz <yonatan@starkware.co>
2026-02-08 10:41:06 +0200

    apollo_starknet_os_program: remove keccak syscall from virtual OS

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -772,11 +772,11 @@ fn class_hash_migration_data_from_state(
 
     if should_migrate {
         expect![[r#"
-            109782378
+            109866079
         "#]]
         .assert_debug_eq(&migration_sierra_gas.0);
         expect![[r#"
-            265958264
+            266185200
         "#]]
         .assert_debug_eq(&migration_proving_gas.0);
     } else {
