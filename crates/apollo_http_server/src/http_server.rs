41883960b818a6e0c50de240ac3c4fa8720058f9
Arnon Hod <arnon@starkware.co>
2026-02-10 15:15:12 +0200

    apollo_http_server: add configurable max_request_body_size as 5mb (#12366)

--- crates/apollo_http_server/src/http_server.rs
+++ crates/apollo_http_server/src/http_server.rs
@@ -22,8 +22,10 @@ use apollo_infra_utils::type_name::short_type_name;
 use apollo_metrics::metrics::set_unix_now_seconds;
 use apollo_proc_macros::sequencer_latency_histogram;
 use async_trait::async_trait;
+use axum::extract::DefaultBodyLimit;
+use axum::handler::Handler;
 use axum::http::HeaderMap;
-use axum::routing::{get, post};
+use axum::routing::{get, post, MethodRouter};
 use axum::{serve, Extension, Json, Router};
 use blockifier_reexecution::serde_utils::deserialize_transaction_json_to_starknet_api_tx;
 use serde::de::Error;
@@ -120,9 +122,15 @@ impl HttpServer {
     pub fn app(&self) -> Router {
         Router::new()
             // Json Rpc endpoint
-            .route("/gateway/add_rpc_transaction", post(add_rpc_tx))
+            .route(
+                "/gateway/add_rpc_transaction",
+                self.post_method_router(add_rpc_tx),
+            )
             // Rest api endpoint
-            .route("/gateway/add_transaction", post(add_tx))
+            .route(
+                "/gateway/add_transaction",
+                self.post_method_router(add_tx),
+            )
             // TODO(shahak): Remove this once we fix the centralized simulator to not use is_alive
             // and is_ready.
             .route(
@@ -134,6 +142,15 @@ impl HttpServer {
                 get(|| futures::future::ready("Gateway is ready".to_owned()))
             )
             .layer(Extension(self.app_state.clone()))
+    }
+
+    fn post_method_router<H, T, S>(&self, handler: H) -> MethodRouter<S>
+    where
+        H: Handler<T, S> + Send + Sync + 'static,
+        T: Send + 'static,
+        S: Clone + Send + Sync + 'static,
+    {
+        post(handler).layer(DefaultBodyLimit::max(self.config.static_config.max_request_body_size))
     }
 }
 
