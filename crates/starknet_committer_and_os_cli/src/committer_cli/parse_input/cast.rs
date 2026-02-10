b4106373973332058e39d861e33595a952c78d57
Nimrod Weiss <nimrod@starkware.co>
2026-02-10 11:38:03 +0200

    starknet_committer: add a field indicating if storage tries should be created concurrently

--- crates/starknet_committer_and_os_cli/src/committer_cli/parse_input/cast.rs
+++ crates/starknet_committer_and_os_cli/src/committer_cli/parse_input/cast.rs
@@ -85,6 +85,8 @@ impl TryFrom<RawInput> for CommitterFactsDbInputImpl {
                 inner_map,
             )?;
         }
+        // TODO(Nimrod): Get this by adding a field to `RawInput`.
+        let build_storage_tries_concurrently = false;
         let input = Input {
             initial_read_context: FactsDbInitialRead(StateRoots {
                 contracts_trie_root_hash: HashOutput(Felt::from_bytes_be_slice(
@@ -100,7 +102,10 @@ impl TryFrom<RawInput> for CommitterFactsDbInputImpl {
                 class_hash_to_compiled_class_hash,
                 storage_updates,
             },
-            config: ReaderConfig::new(raw_input.config.warn_on_trivial_modifications),
+            config: ReaderConfig::new(
+                raw_input.config.warn_on_trivial_modifications,
+                build_storage_tries_concurrently,
+            ),
         };
         Ok(Self { input, log_level: raw_input.config.log_level.into(), storage })
     }
