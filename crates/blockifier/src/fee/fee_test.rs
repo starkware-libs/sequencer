c6c73626043fbd6fbca4e85ba0a33529f30b6298
Aviv Greenburg <aviv.g@starkware.co>
2026-02-12 11:13:43 +0200

    blockifier: update get_vm_resources_cost to accept ExtendedExecutionResources

--- crates/blockifier/src/fee/fee_test.rs
+++ crates/blockifier/src/fee/fee_test.rs
@@ -47,14 +47,13 @@ fn test_simple_get_vm_resource_usage(
     gas_vector_computation_mode: GasVectorComputationMode,
 ) {
     let versioned_constants = VersionedConstants::create_for_account_testing();
-    // TODO(AvivG): update to test ExtendedExecutionResources.
-    let mut vm_resource_usage = get_vm_resource_usage().vm_resources;
+    let mut vm_resource_usage = get_vm_resource_usage();
     let n_reverted_steps = 15;
 
     // Positive flow.
     // Verify calculation - in our case, n_steps is the heaviest resource.
     let vm_usage_in_l1_gas = (versioned_constants.vm_resource_fee_cost().n_steps
-        * (u64_from_usize(vm_resource_usage.n_steps + n_reverted_steps)))
+        * (u64_from_usize(vm_resource_usage.vm_resources.n_steps + n_reverted_steps)))
     .ceil()
     .to_integer()
     .into();
@@ -75,10 +74,18 @@ fn test_simple_get_vm_resource_usage(
 
     // Another positive flow, this time the heaviest resource is range_check_builtin.
     let n_reverted_steps = 0;
-    vm_resource_usage.n_steps =
-        vm_resource_usage.builtin_instance_counter.get(&BuiltinName::range_check).unwrap() - 1;
+    vm_resource_usage.vm_resources.n_steps = vm_resource_usage
+        .vm_resources
+        .builtin_instance_counter
+        .get(&BuiltinName::range_check)
+        .unwrap()
+        - 1;
     let vm_usage_in_l1_gas = u64_from_usize(
-        *vm_resource_usage.builtin_instance_counter.get(&BuiltinName::range_check).unwrap(),
+        *vm_resource_usage
+            .vm_resources
+            .builtin_instance_counter
+            .get(&BuiltinName::range_check)
+            .unwrap(),
     )
     .into();
     let expected_gas_vector = gas_vector_from_vm_usage(
@@ -103,14 +110,13 @@ fn test_float_get_vm_resource_usage(
     gas_vector_computation_mode: GasVectorComputationMode,
 ) {
     let versioned_constants = VersionedConstants::create_for_testing();
-    // TODO(AvivG): update to test ExtendedExecutionResources.
-    let mut vm_resource_usage = get_vm_resource_usage().vm_resources;
+    let mut vm_resource_usage = get_vm_resource_usage();
 
     // Positive flow.
     // Verify calculation - in our case, n_steps is the heaviest resource.
     let n_reverted_steps = 300;
     let vm_usage_in_l1_gas = (versioned_constants.vm_resource_fee_cost().n_steps
-        * u64_from_usize(vm_resource_usage.n_steps + n_reverted_steps))
+        * u64_from_usize(vm_resource_usage.vm_resources.n_steps + n_reverted_steps))
     .ceil()
     .to_integer()
     .into();
@@ -130,11 +136,15 @@ fn test_float_get_vm_resource_usage(
     );
 
     // Another positive flow, this time the heaviest resource is ecdsa_builtin.
-    vm_resource_usage.n_steps = 200;
+    vm_resource_usage.vm_resources.n_steps = 200;
     let vm_usage_in_l1_gas =
         ((*versioned_constants.vm_resource_fee_cost().builtins.get(&BuiltinName::ecdsa).unwrap())
             * u64_from_usize(
-                *vm_resource_usage.builtin_instance_counter.get(&BuiltinName::ecdsa).unwrap(),
+                *vm_resource_usage
+                    .vm_resources
+                    .builtin_instance_counter
+                    .get(&BuiltinName::ecdsa)
+                    .unwrap(),
             ))
         .ceil()
         .to_integer()
