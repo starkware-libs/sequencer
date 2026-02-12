c9310aafb422276629eb099e2f43c70e86c1cd43
Dori Medini <dori@starkware.co>
2026-02-12 15:50:27 +0200

    ci: better rustfmt toolchain installation
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/rust_fmt.sh
+++ scripts/rust_fmt.sh
@@ -2,18 +2,9 @@
 
 set -euo pipefail
 
-# TODO(Dori): Since it is not possible to specify multiple toolchains in a single
-#   rust-toolchain.toml file (and directing that specific cargo commands should run with the nightly
-#   toolchain), we need to find a nice way to cache the nightly toolchain installation in the CI,
-#   without defining the specific nightly version in multiple places.
-TOOLCHAIN=nightly-2024-04-29
+source scripts/cargo_tool_utils.sh
 
-function install_rustfmt() {
-    rustup toolchain install "${TOOLCHAIN}"
-    rustup component add --toolchain "${TOOLCHAIN}" rustfmt
-}
-
-rustup toolchain list | grep -q "${TOOLCHAIN}" || install_rustfmt
+TOOLCHAIN=$(verify_and_return_fmt_toolchain)
 
 echo "Running cargo fmt with toolchain ${TOOLCHAIN}"
 cargo +"${TOOLCHAIN}" fmt --all -- "$@"
