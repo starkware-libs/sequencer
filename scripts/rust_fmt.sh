b662c1b9823aef6551dd651822c3b68fbcdbcebc
Dori Medini <dori@starkware.co>
2026-02-12 10:22:43 +0200

    ci: move rustfmt toolchain definition to script
    Signed-off-by: Dori Medini <dori@starkware.co>

--- scripts/rust_fmt.sh
+++ scripts/rust_fmt.sh
@@ -2,13 +2,11 @@
 
 set -euo pipefail
 
-if [[ -n "${CI:-}" ]]; then
-  echo "This script should not be run in a CI environment, as it installs toolchains out of cache."
-  exit 1
-fi
-
-SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-TOOLCHAIN=$(grep "EXTRA_RUST_TOOLCHAINS:" "${SCRIPT_DIR}"/../.github/workflows/main.yml  | awk '{print $2}')
+# TODO(Dori): Since it is not possible to specify multiple toolchains in a single
+#   rust-toolchain.toml file (and directing that specific cargo commands should run with the nightly
+#   toolchain), we need to find a nice way to cache the nightly toolchain installation in the CI,
+#   without defining the specific nightly version in multiple places.
+TOOLCHAIN=nightly-2024-04-29
 
 function install_rustfmt() {
     rustup toolchain install "${TOOLCHAIN}"
@@ -17,4 +15,5 @@ function install_rustfmt() {
 
 rustup toolchain list | grep -q "${TOOLCHAIN}" || install_rustfmt
 
+echo "Running cargo fmt with toolchain ${TOOLCHAIN}"
 cargo +"${TOOLCHAIN}" fmt --all -- "$@"
