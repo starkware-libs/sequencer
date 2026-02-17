f5de26c7fc6589d1b93e767e61a4521e66629e08
Aviv Greenburg <aviv.g@starkware.co>
2026-02-17 11:38:16 +0200

    blockifier: fix test_gas_computation_regression_test for client side proving

--- crates/blockifier/src/fee/gas_usage_test.rs
+++ crates/blockifier/src/fee/gas_usage_test.rs
@@ -46,9 +46,10 @@ fn versioned_constants() -> &'static VersionedConstants {
     VersionedConstants::latest_constants()
 }
 
+const PROOF_FACTS_COUNT: usize = 1;
+
 // Starknet resources with many resources (of arbitrary values) for testing.
-#[fixture]
-fn starknet_resources() -> StarknetResources {
+fn create_test_starknet_resources(has_client_side_proof: bool) -> StarknetResources {
     let call_info_1 = CallInfo {
         execution: CallExecution {
             events: vec![create_event_for_testing(1, 2), create_event_for_testing(1, 2)],
@@ -78,12 +79,13 @@ fn starknet_resources() -> StarknetResources {
         },
         19,
     );
-    let has_client_side_proof = true;
     let calldata_length = 2_usize;
-    let proof_facts_length = 5_usize;
+    let proof_facts_length = if has_client_side_proof { PROOF_FACTS_COUNT } else { 0 };
+    let extended_calldata_length = calldata_length + proof_facts_length;
+    let has_client_side_proof = true;
 
     StarknetResources::new(
-        calldata_length + proof_facts_length,
+        extended_calldata_length,
         3_usize,
         4_usize,
         state_resources,
@@ -342,17 +344,18 @@ fn test_discounted_gas_from_gas_vector_computation() {
 // Assert gas computation results are as expected. The goal of this test is to prevent unwanted
 // changes to the gas computation.
 fn test_gas_computation_regression_test(
-    starknet_resources: StarknetResources,
     #[values(false, true)] use_kzg_da: bool,
     #[values(GasVectorComputationMode::NoL2Gas, GasVectorComputationMode::All)]
     gas_vector_computation_mode: GasVectorComputationMode,
+    #[values(false, true)] has_client_side_proof: bool,
 ) {
     // Client side proving is only supported from V3 transactions, which use AllResourceBounds.
-    if starknet_resources.archival_data.has_client_side_proof
-        && gas_vector_computation_mode == GasVectorComputationMode::NoL2Gas
-    {
+    if has_client_side_proof && gas_vector_computation_mode == GasVectorComputationMode::NoL2Gas {
         return;
     }
+
+    let starknet_resources = create_test_starknet_resources(has_client_side_proof);
+
     // Use a constant version of the versioned constants so that version changes do not break this
     // test. This specific version is arbitrary.
     // TODO(Amos, 1/10/2024): Parameterize the version.
@@ -372,28 +375,27 @@ fn test_gas_computation_regression_test(
         use_kzg_da,
         &gas_vector_computation_mode,
     );
-    let expected_starknet_resources_gas_vector = match gas_vector_computation_mode {
-        GasVectorComputationMode::NoL2Gas => match use_kzg_da {
-            true => GasVector {
-                l1_gas: GasAmount(21544),
-                l1_data_gas: GasAmount(2720),
-                l2_gas: GasAmount(0),
-            },
-            false => GasVector::from_l1_gas(GasAmount(62835)),
-        },
-        GasVectorComputationMode::All => match use_kzg_da {
-            true => GasVector {
-                l1_gas: GasAmount(21543),
-                l1_data_gas: GasAmount(2720),
-                l2_gas: GasAmount(112640),
-            },
-            false => GasVector {
-                l1_gas: GasAmount(62834),
-                l1_data_gas: GasAmount(0),
-                l2_gas: GasAmount(112640),
-            },
-        },
+
+    let (l1, l1_data, mut l2) = match (&gas_vector_computation_mode, use_kzg_da) {
+        (GasVectorComputationMode::NoL2Gas, true) => (21544, 2720, 0),
+        (GasVectorComputationMode::NoL2Gas, false) => (62835, 0, 0),
+        (GasVectorComputationMode::All, true) => (21543, 2720, 87040),
+        (GasVectorComputationMode::All, false) => (62834, 0, 87040),
     };
+
+    let proof_facts_overhead = versioned_constants
+        .get_archival_data_gas_costs(&gas_vector_computation_mode)
+        .gas_per_data_felt
+        .to_integer()
+        * u64_from_usize(PROOF_FACTS_COUNT);
+
+    if has_client_side_proof {
+        l2 += proof_facts_overhead;
+    }
+
+    let expected_starknet_resources_gas_vector =
+        GasVector { l1_gas: GasAmount(l1), l1_data_gas: GasAmount(l1_data), l2_gas: GasAmount(l2) };
+
     assert_eq!(
         actual_starknet_resources_gas_vector, expected_starknet_resources_gas_vector,
         "Unexpected gas computation result for starknet resources. If this is intentional please \
@@ -432,28 +434,24 @@ fn test_gas_computation_regression_test(
         TransactionResources { starknet_resources, computation: computation_resources };
     let actual_gas_vector =
         tx_resources.to_gas_vector(&versioned_constants, use_kzg_da, &gas_vector_computation_mode);
-    let expected_gas_vector = match gas_vector_computation_mode {
-        GasVectorComputationMode::NoL2Gas => match use_kzg_da {
-            true => GasVector {
-                l1_gas: GasAmount(21575),
-                l1_data_gas: GasAmount(2720),
-                l2_gas: GasAmount(0),
-            },
-            false => GasVector::from_l1_gas(GasAmount(62866)),
-        },
-        GasVectorComputationMode::All => match use_kzg_da {
-            true => GasVector {
-                l1_gas: GasAmount(21543),
-                l1_data_gas: GasAmount(2720),
-                l2_gas: GasAmount(1145994),
-            },
-            false => GasVector {
-                l1_gas: GasAmount(62834),
-                l1_data_gas: GasAmount(0),
-                l2_gas: GasAmount(1145994),
-            },
-        },
+
+    let (total_l1, total_l1_data, mut total_l2) = match (&gas_vector_computation_mode, use_kzg_da) {
+        (GasVectorComputationMode::NoL2Gas, true) => (21575, 2720, 0),
+        (GasVectorComputationMode::NoL2Gas, false) => (62866, 0, 0),
+        (GasVectorComputationMode::All, true) => (21543, 2720, 1120394),
+        (GasVectorComputationMode::All, false) => (62834, 0, 1120394),
     };
+
+    if has_client_side_proof {
+        total_l2 += proof_facts_overhead;
+    }
+
+    let expected_gas_vector = GasVector {
+        l1_gas: GasAmount(total_l1),
+        l1_data_gas: GasAmount(total_l1_data),
+        l2_gas: GasAmount(total_l2),
+    };
+
     assert_eq!(
         actual_gas_vector, expected_gas_vector,
         "Unexpected gas computation result for tx resources. If this is intentional please fix \
