9b7c33d974ae359d47c6af716103a700baa54a7b
Dafna Matsry <dafna.matsry@starkware.co>
2026-02-22 15:13:51 +0200

    apollo_staking: move randomness block hash to be inside the random generator

--- crates/apollo_staking/src/utils_test.rs
+++ crates/apollo_staking/src/utils_test.rs
@@ -5,40 +5,41 @@ use crate::utils::{BlockPseudorandomGenerator, BlockRandomGenerator};
 
 #[test]
 fn test_deterministic_same_inputs() {
-    let generator = BlockPseudorandomGenerator;
+    let generator = BlockPseudorandomGenerator {
+        randomness_block_hash: Some(BlockHash(StarkHash::from(123u64))),
+    };
     let height = BlockNumber(100);
     let round = 5;
-    let block_hash = Some(BlockHash(StarkHash::from(123u64)));
     let range = 10000;
 
-    let result1 = generator.generate(height, round, block_hash, range);
-    let result2 = generator.generate(height, round, block_hash, range);
+    let result1 = generator.generate(height, round, range);
+    let result2 = generator.generate(height, round, range);
     assert_eq!(result1, result2, "Same inputs should produce same output");
     assert!(result1 < range, "Result should be in range [0, {range}), but got {result1}");
 }
 
 #[test]
 fn test_deterministic_none_block_hash() {
-    let generator = BlockPseudorandomGenerator;
+    let generator = BlockPseudorandomGenerator { randomness_block_hash: None };
     let height = BlockNumber(100);
     let round = 5;
-    let block_hash = None;
     let range = 10000;
 
-    let result1 = generator.generate(height, round, block_hash, range);
-    let result2 = generator.generate(height, round, block_hash, range);
+    let result1 = generator.generate(height, round, range);
+    let result2 = generator.generate(height, round, range);
     assert_eq!(result1, result2, "Same inputs with None block_hash should produce same output");
     assert!(result1 < range, "Result should be in range [0, {range}), but got {result1}");
 }
 
 #[test]
 fn test_range_zero() {
-    let generator = BlockPseudorandomGenerator;
+    let generator = BlockPseudorandomGenerator {
+        randomness_block_hash: Some(BlockHash(StarkHash::from(123u64))),
+    };
     let height = BlockNumber(100);
     let round = 5;
-    let block_hash = Some(BlockHash(StarkHash::from(123u64)));
     let range = 0;
 
-    let result = generator.generate(height, round, block_hash, range);
+    let result = generator.generate(height, round, range);
     assert_eq!(result, 0, "Range 0 should return 0");
 }
