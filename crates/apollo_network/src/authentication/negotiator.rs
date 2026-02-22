487e1f95d16aaea365b46fb944b694fab72c2199
Noam Spiegelstein <noam.s@starkware.co>
2026-02-22 16:41:42 +0200

    apollo_network: change interface to have an abstraction

--- crates/apollo_network/src/authentication/negotiator.rs
+++ crates/apollo_network/src/authentication/negotiator.rs
@@ -1,35 +1,41 @@
 use std::io::Error as IoError;
 
 use async_trait::async_trait;
-use futures::{Sink, Stream};
 use libp2p::PeerId;
+use prost::Message;
 
 pub enum NegotiationSide {
     Inbound,
     Outbound,
 }
 
+// TODO(noam.s): Remove this enum if we end up only having the Success case.
 pub enum NegotiatorOutput {
-    None,
-    /// Returned when the handshake concluded that the currently connecting peer is a duplicate of
-    /// the currently connected peer. `PeerId` is the (older) currently connected peer.
-    DuplicatePeer(PeerId),
+    Success,
 }
-
-#[derive(Debug, thiserror::Error)]
-#[non_exhaustive]
-pub enum NegotiatorError {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error("Authentication failed")]
-    AuthenticationFailed,
-}
-
-pub type ConnectionSender = dyn Sink<Vec<u8>, Error = IoError> + Unpin + Send;
-pub type ConnectionReceiver = dyn Stream<Item = Result<Vec<u8>, IoError>> + Unpin + Send;
 
 #[async_trait]
+pub trait ConnectionSender<M>: Unpin + Send {
+    async fn send(&mut self, data: M) -> Result<(), IoError>;
+}
+
+#[async_trait]
+pub trait ConnectionReceiver<M>: Unpin + Send {
+    async fn receive(&mut self) -> Result<M, IoError>;
+}
+
+#[async_trait]
+#[cfg_attr(
+    test,
+    automock(
+        type Error = std::io::Error;
+        type WireMessage = apollo_protobuf::protobuf::StarkAuthentication;
+    )
+)]
 pub trait Negotiator: Send + Clone {
+    type WireMessage: Message + Default + Unpin + Send;
+    type Error: std::error::Error + Send + Sync;
+
     /// Performs the handshake protocol.
     /// `connection_sender` is the channel that can be used to send data to the remote peer.
     /// `connection_receiver` is the channel that can be used to receive data from the remote peer.
@@ -37,10 +43,10 @@ pub trait Negotiator: Send + Clone {
         &mut self,
         my_peer_id: PeerId,
         other_peer_id: PeerId,
-        connection_sender: &mut ConnectionSender,
-        connection_receiver: &mut ConnectionReceiver,
+        connection_sender: &mut dyn ConnectionSender<Self::WireMessage>,
+        connection_receiver: &mut dyn ConnectionReceiver<Self::WireMessage>,
         side: NegotiationSide,
-    ) -> Result<NegotiatorOutput, NegotiatorError>;
+    ) -> Result<NegotiatorOutput, Self::Error>;
 
     /// A unique identified for your authentication protocol. For example: "strk_id" or
     /// "strk_id_v2".
