2411a002e709307ac0d7eb0cb9657c2fbb93a273
Aviv Greenburg <aviv.g@starkware.co>
2026-02-26 12:14:39 +0200

    blockifier: add blake to gas test

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -783,11 +783,11 @@ fn class_hash_migration_data_from_state(
 
     if should_migrate {
         expect![[r#"
-            109866079
+            110279227
         "#]]
         .assert_debug_eq(&migration_sierra_gas.0);
         expect![[r#"
-            266185200
+            267171348
         "#]]
         .assert_debug_eq(&migration_proving_gas.0);
     } else {
