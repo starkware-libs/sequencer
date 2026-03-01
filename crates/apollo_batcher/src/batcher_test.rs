53b8e10363e88001732441886e7a0e6bf0589d23
Itamar Shechter <itamar@starkware.co>
2026-03-01 18:58:51 +0200

    apollo_batcher: use abort_proposal api

--- crates/apollo_batcher/src/batcher_test.rs
+++ crates/apollo_batcher/src/batcher_test.rs
@@ -629,8 +629,6 @@ async fn validate_block_full_flow() {
 #[rstest]
 #[case::send_txs(SendProposalContent::Txs(test_txs(0..1)))]
 #[case::send_finish(SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS))]
-// TODO(Itamar): Remove this case once all callers migrate to `abort_proposal`.
-#[case::send_abort(SendProposalContent::Abort)]
 #[tokio::test]
 async fn send_content_to_unknown_proposal(#[case] content: SendProposalContent) {
     let mut batcher = create_batcher(MockDependencies::default()).await;
@@ -657,9 +655,6 @@ async fn abort_unknown_proposal() {
     SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS),
     ProposalStatus::InvalidProposal("Block is full".to_string())
 )]
-// TODO(Itamar): Remove this case once we migrate to `abort_proposal`.
-// This case will be tested in `send_proposal_content_after_abort`.
-#[case::send_abort(SendProposalContent::Abort, ProposalStatus::Aborted)]
 #[tokio::test]
 async fn send_content_to_an_invalid_proposal(
     #[case] content: SendProposalContent,
@@ -677,40 +672,20 @@ async fn send_content_to_an_invalid_proposal(
 
 // TODO(Itamar): Remove this test once all cases are tested separately for each method.
 #[rstest]
-#[case::send_txs_after_finish(SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS), SendProposalContent::Txs(test_txs(0..1)))]
-#[case::send_finish_after_finish(
-    SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS),
-    SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS)
-)]
-#[case::send_abort_after_finish(
-    SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS),
-    SendProposalContent::Abort
-)]
-// TODO(Itamar): Remove this case once we migrate to `abort_proposal`.
-// This case will be tested in `send_proposal_content_after_abort`.
-#[case::send_txs_after_abort(SendProposalContent::Abort, SendProposalContent::Txs(test_txs(0..1)))]
-// TODO(Itamar): Remove this case once we migrate to `abort_proposal`.
-// This case will be tested in `send_proposal_content_after_abort`.
-#[case::send_finish_after_abort(
-    SendProposalContent::Abort,
-    SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS)
-)]
-// TODO(Itamar): Remove this case once we migrate to `abort_proposal`.
-// This case will be tested in `abort_after_abort`.
-#[case::send_abort_after_abort(SendProposalContent::Abort, SendProposalContent::Abort)]
+#[case::send_txs_after_finish(SendProposalContent::Txs(test_txs(0..1)))]
+#[case::send_finish_after_finish(SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS))]
 #[tokio::test]
-async fn send_proposal_content_after_finish_or_abort(
-    #[case] end_proposal_content: SendProposalContent,
-    #[case] content: SendProposalContent,
-) {
+async fn send_proposal_content_after_finish(#[case] content: SendProposalContent) {
     let mut batcher = create_batcher_with_active_validate_block(Ok(
         BlockExecutionArtifacts::create_for_testing().await,
     ))
     .await;
 
     // End the proposal.
-    let end_proposal =
-        SendProposalContentInput { proposal_id: PROPOSAL_ID, content: end_proposal_content };
+    let end_proposal = SendProposalContentInput {
+        proposal_id: PROPOSAL_ID,
+        content: SendProposalContent::Finish(DUMMY_FINAL_N_EXECUTED_TXS),
+    };
     batcher.send_proposal_content(end_proposal).await.unwrap();
 
     // Send another request.
@@ -768,34 +743,6 @@ async fn abort_after_abort() {
 
     let result = batcher.abort_proposal(PROPOSAL_ID).await;
     assert_eq!(result, Err(BatcherError::ProposalNotFound { proposal_id: PROPOSAL_ID }));
-}
-
-// TODO(Itamar): Remove this test once all callers migrate to `abort_proposal`.
-// This case is tested in `abort_proposal_test`.
-#[rstest]
-#[tokio::test]
-async fn send_proposal_content_abort() {
-    let recorder = PrometheusBuilder::new().build_recorder();
-    let _recorder_guard = metrics::set_default_local_recorder(&recorder);
-    let mut batcher =
-        create_batcher_with_active_validate_block(Err(BlockBuilderError::Aborted)).await;
-    let metrics = recorder.handle().render();
-    assert_proposal_metrics(&metrics, 1, 0, 0, 0);
-
-    let send_abort_proposal =
-        SendProposalContentInput { proposal_id: PROPOSAL_ID, content: SendProposalContent::Abort };
-    assert_eq!(
-        batcher.send_proposal_content(send_abort_proposal).await.unwrap(),
-        SendProposalContentResponse { response: ProposalStatus::Aborted }
-    );
-
-    // The block builder is running in a separate task, and the proposal metrics are emitted from
-    // that task, so we need to wait for them (we don't have a way to wait for the completion of the
-    // abort).
-    // TODO(AlonH): Find a way to wait for the metrics to be emitted.
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-    let metrics = recorder.handle().render();
-    assert_proposal_metrics(&metrics, 1, 0, 0, 1);
 }
 
 #[rstest]
