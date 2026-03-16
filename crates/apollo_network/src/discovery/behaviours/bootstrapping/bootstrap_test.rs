b9f5e9d555c3923726f446d01e121d9d9b3b2bc8
Andrew Luka <andrew.l@starkware.co>
2026-03-16 16:31:47 +0200

    apollo_propeller: rename reconstruct_message_from_shards to reconstruct_data_shards (#13251)

--- crates/apollo_network/src/discovery/behaviours/bootstrapping/bootstrap_test.rs
+++ crates/apollo_network/src/discovery/behaviours/bootstrapping/bootstrap_test.rs
@@ -1,5 +1,3 @@
-// TODO(shahak): add flow test
-
 use std::convert::Infallible;
 use std::pin::Pin;
 use std::task::{Context, Poll, Waker};
@@ -11,36 +9,15 @@ use libp2p::core::transport::PortUse;
 use libp2p::core::{ConnectedPoint, Endpoint};
 use libp2p::multihash::Multihash;
 use libp2p::swarm::behaviour::ConnectionEstablished;
-use libp2p::swarm::{
-    ConnectionClosed,
-    ConnectionId,
-    DialError,
-    DialFailure,
-    FromSwarm,
-    NetworkBehaviour,
-    ToSwarm,
-};
+use libp2p::swarm::{ConnectionClosed, ConnectionId, FromSwarm, NetworkBehaviour, ToSwarm};
 use libp2p::{Multiaddr, PeerId};
 use rstest::rstest;
 use tokio::time::timeout;
 
 use super::BootstrappingBehaviour;
-use crate::discovery::{RetryConfig, ToOtherBehaviourEvent};
+use crate::discovery::ToOtherBehaviourEvent;
 
-// Timeout for waiting for events in the tests.
 const TIMEOUT: Duration = Duration::from_millis(10);
-
-// sleep seconds on dial fail should be 0.016, 0.256, 4.096, 10, 10, ...
-const BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS: u64 = 16;
-const BOOTSTRAP_DIAL_SLEEP_BASE: Duration = Duration::from_millis(BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS);
-const BOOTSTRAP_DIAL_SLEEP_MAX: Duration = Duration::from_secs(10);
-
-const CONFIG: RetryConfig = RetryConfig {
-    base_delay_millis: BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS,
-    max_delay_seconds: BOOTSTRAP_DIAL_SLEEP_MAX,
-    factor: 1,
-};
-
 const LOCAL_PEER_ID_INDEX: u8 = 0;
 
 impl Unpin for BootstrappingBehaviour {}
@@ -56,7 +33,6 @@ impl Stream for BootstrappingBehaviour {
     }
 }
 
-// In case we have a bug when we return pending and then return an event.
 const TIMES_TO_CHECK_FOR_PENDING_EVENT: usize = 5;
 
 fn assert_no_event(behaviour: &mut BootstrappingBehaviour) {
@@ -66,31 +42,11 @@ fn assert_no_event(behaviour: &mut BootstrappingBehaviour) {
     }
 }
 
-async fn assert_no_event_happens_before_duration(
-    behaviour: &mut BootstrappingBehaviour,
-    duration: Duration,
-) {
-    const EPSILON_SLEEP: Duration = Duration::from_millis(5);
-    // check no events now
-    assert_no_event(behaviour);
-
-    // Check that there are no events until we sleep for enough time.
-    tokio::time::pause();
-    tokio::time::advance(duration - EPSILON_SLEEP).await;
-    assert_no_event(behaviour);
-
-    // Sleep and check for event.
-    tokio::time::advance(2 * EPSILON_SLEEP).await;
-    tokio::time::resume();
-}
-
-// TODO(AndrewL): Move this to test_utils crate.
 fn get_peer_id(peer_index: u8) -> PeerId {
     let input_digest = vec![peer_index; 32];
     PeerId::from_multihash(Multihash::wrap(0x0, &input_digest).unwrap()).unwrap()
 }
 
-/// Sample a random list of peers.
 fn get_peers(peer_count: usize) -> Vec<(PeerId, Multiaddr)> {
     let peer_start_index: usize = usize::from(LOCAL_PEER_ID_INDEX + 1);
     (peer_start_index..peer_start_index + peer_count)
@@ -101,28 +57,32 @@ fn get_peers(peer_count: usize) -> Vec<(PeerId, Multiaddr)> {
         .collect::<Vec<_>>()
 }
 
-/// Consumes the events that dial the bootstrap peers.
-/// This function checks the dial events after construction of the behaviour.
-async fn consume_dial_events(
+/// Consumes RequestDial events emitted for the bootstrap peers.
+async fn consume_request_dial_events(
     behaviour: &mut BootstrappingBehaviour,
     mut bootstrap_peers: Vec<(PeerId, Multiaddr)>,
 ) {
     let peer_count = bootstrap_peers.len();
     for _ in 0..peer_count {
         let event = timeout(TIMEOUT, behaviour.next()).await.unwrap().unwrap();
-        let bootstrap_peer_id = assert_matches!(
+        let (peer_id, addresses) = assert_matches!(
             event,
-            ToSwarm::Dial{opts} => opts.get_peer_id().unwrap()
+            ToSwarm::GenerateEvent(ToOtherBehaviourEvent::RequestDial {
+                peer_id,
+                addresses,
+            }) => (peer_id, addresses)
         );
         let index_to_remove = bootstrap_peers
             .iter()
-            .position(|(peer_id, _)| peer_id == &bootstrap_peer_id)
-            .expect("Got event for peer that is not in the list");
+            .position(|(expected_id, expected_addr)| {
+                *expected_id == peer_id && addresses.contains(expected_addr)
+            })
+            .expect("Got RequestDial for peer that is not in the list");
         bootstrap_peers.remove(index_to_remove);
     }
 }
 
-/// Consumes the events that found listen addresses.
+/// Consumes FoundListenAddresses events.
 async fn consume_found_listen_address_events(
     behaviour: &mut BootstrappingBehaviour,
     mut bootstrap_peers: Vec<(PeerId, Multiaddr)>,
@@ -132,9 +92,9 @@ async fn consume_found_listen_address_events(
         let event = timeout(TIMEOUT, behaviour.next()).await.unwrap().unwrap();
         let (bootstrap_peer_id, bootstrap_addresses) = assert_matches!(
             event,
-            ToSwarm::GenerateEvent( ToOtherBehaviourEvent::FoundListenAddresses {
+            ToSwarm::GenerateEvent(ToOtherBehaviourEvent::FoundListenAddresses {
                 peer_id,
-                listen_addresses
+                listen_addresses,
             }) => (peer_id, listen_addresses)
         );
         for address in bootstrap_addresses {
@@ -149,26 +109,6 @@ async fn consume_found_listen_address_events(
     }
 }
 
-/// Sends dial fail event to the behaviour.
-fn fail_dial_attempt(behaviour: &mut BootstrappingBehaviour, peer_id: PeerId) {
-    behaviour.on_swarm_event(FromSwarm::DialFailure(DialFailure {
-        peer_id: Some(peer_id),
-        error: &DialError::Aborted,
-        connection_id: ConnectionId::new_unchecked(0),
-    }));
-}
-
-/// Sends dial fail event to the behaviour for all bootstrap peers.
-fn fail_all_dial_attempts(
-    behaviour: &mut BootstrappingBehaviour,
-    bootstrap_peers: &[(PeerId, Multiaddr)],
-) {
-    for peer_id in bootstrap_peers.iter().map(|(peer_id, _)| peer_id).copied() {
-        fail_dial_attempt(behaviour, peer_id);
-    }
-}
-
-/// Sends connection established event to the behaviour.
 fn accept_dial_attempt(
     behaviour: &mut BootstrappingBehaviour,
     peer_id: PeerId,
@@ -187,7 +127,6 @@ fn accept_dial_attempt(
     }));
 }
 
-/// Sends connection established event to the behaviour for all bootstrap peers.
 fn accept_all_dial_attempts(
     behaviour: &mut BootstrappingBehaviour,
     bootstrap_peers: &[(PeerId, Multiaddr)],
@@ -198,7 +137,6 @@ fn accept_all_dial_attempts(
     }
 }
 
-/// Sends connection closed event to the behaviour.
 fn close_connection(
     behaviour: &mut BootstrappingBehaviour,
     peer_id: PeerId,
@@ -218,7 +156,6 @@ fn close_connection(
     }));
 }
 
-/// Sends connection closed event to the behaviour for all bootstrap peers.
 fn close_all_connections(
     behaviour: &mut BootstrappingBehaviour,
     bootstrap_peers: &[(PeerId, Multiaddr)],
@@ -229,30 +166,13 @@ fn close_all_connections(
     }
 }
 
-// /// Checks that the behaviour dials the peer after the given duration and not before.
-// async fn expect_dial_after_duration(
-//     behaviour: &mut BootstrappingBehaviour,
-//     peer_id: PeerId,
-//     current_retry_duration: Duration,
-// ) { let event = check_event_happens_after_given_duration(behaviour,
-//   current_retry_duration).await;
-
-//     assert_matches!(
-//         event,
-//         ToSwarm::Dial{opts} if opts.get_peer_id() == Some(peer_id)
-//     );
-// }
-
 async fn make_and_connect_bootstrap_nodes(
     peer_count: usize,
 ) -> (Vec<(PeerId, Multiaddr)>, BootstrappingBehaviour) {
     let bootstrap_peers = get_peers(peer_count);
-    let mut behaviour = BootstrappingBehaviour::new(
-        get_peer_id(LOCAL_PEER_ID_INDEX),
-        CONFIG,
-        bootstrap_peers.clone(),
-    );
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
+    let mut behaviour =
+        BootstrappingBehaviour::new(get_peer_id(LOCAL_PEER_ID_INDEX), bootstrap_peers.clone());
+    consume_request_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
     assert_no_event(&mut behaviour);
     accept_all_dial_attempts(&mut behaviour, &bootstrap_peers, 0);
     consume_found_listen_address_events(&mut behaviour, bootstrap_peers.clone()).await;
@@ -262,71 +182,21 @@ async fn make_and_connect_bootstrap_nodes(
 
 #[rstest]
 #[tokio::test]
-async fn bootstrapping_outputs_dial_request_per_peer_on_start(
+async fn bootstrapping_outputs_request_dial_per_peer_on_start(
     #[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize,
 ) {
     let bootstrap_peers = get_peers(peer_count);
-    let mut behaviour = BootstrappingBehaviour::new(
-        get_peer_id(LOCAL_PEER_ID_INDEX),
-        CONFIG,
-        bootstrap_peers.clone(),
-    );
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
+    let mut behaviour =
+        BootstrappingBehaviour::new(get_peer_id(LOCAL_PEER_ID_INDEX), bootstrap_peers.clone());
+    consume_request_dial_events(&mut behaviour, bootstrap_peers).await;
     assert_no_event(&mut behaviour);
-}
-
-#[rstest]
-#[tokio::test]
-async fn bootstrapping_redials_on_dial_failure(#[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize) {
-    let bootstrap_peers = get_peers(peer_count);
-    let mut behaviour = BootstrappingBehaviour::new(
-        get_peer_id(LOCAL_PEER_ID_INDEX),
-        CONFIG,
-        bootstrap_peers.clone(),
-    );
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-
-    assert_no_event(&mut behaviour);
-    fail_all_dial_attempts(&mut behaviour, &bootstrap_peers);
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_BASE).await;
-    consume_dial_events(&mut behaviour, bootstrap_peers).await;
-    assert_no_event(&mut behaviour);
-}
-
-#[rstest]
-#[tokio::test]
-async fn bootstrapping_redials_in_accordance_with_strategy_on_dial_failure(
-    #[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize,
-) {
-    use std::cmp::min;
-    const NUMBER_OF_DIAL_RETRIES: u32 = 10;
-
-    let bootstrap_peers = get_peers(peer_count);
-    let mut behaviour = BootstrappingBehaviour::new(
-        get_peer_id(LOCAL_PEER_ID_INDEX),
-        CONFIG,
-        bootstrap_peers.clone(),
-    );
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-    assert_no_event(&mut behaviour);
-
-    for i in 1..=NUMBER_OF_DIAL_RETRIES {
-        fail_all_dial_attempts(&mut behaviour, &bootstrap_peers);
-        let current_retry_duration = min(
-            BOOTSTRAP_DIAL_SLEEP_MAX,
-            Duration::from_millis(BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS.pow(i)),
-        );
-        assert_no_event_happens_before_duration(&mut behaviour, current_retry_duration).await;
-        consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-        assert_no_event(&mut behaviour);
-    }
 }
 
 #[rstest]
 #[tokio::test]
 async fn bootstrapping_full_happy_flow(#[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize) {
     let (_, mut behaviour) = make_and_connect_bootstrap_nodes(peer_count).await;
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_MAX * 2).await;
+    assert_no_event(&mut behaviour);
 }
 
 #[rstest]
@@ -337,56 +207,8 @@ async fn bootstrapping_redials_when_all_connections_closed(
     let (bootstrap_peers, mut behaviour) = make_and_connect_bootstrap_nodes(peer_count).await;
 
     close_all_connections(&mut behaviour, &bootstrap_peers, 0);
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_MAX * 2).await;
-}
-
-#[rstest]
-#[tokio::test]
-async fn bootstrapping_redials_in_accordance_with_strategy_when_all_connections_closed(
-    #[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize,
-) {
-    use std::cmp::min;
-    const NUMBER_OF_DIAL_RETRIES: u32 = 10;
-
-    let bootstrap_peers = get_peers(peer_count);
-    let mut behaviour = BootstrappingBehaviour::new(
-        get_peer_id(LOCAL_PEER_ID_INDEX),
-        CONFIG,
-        bootstrap_peers.clone(),
-    );
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
+    consume_request_dial_events(&mut behaviour, bootstrap_peers).await;
     assert_no_event(&mut behaviour);
-
-    for i in 1..=NUMBER_OF_DIAL_RETRIES {
-        fail_all_dial_attempts(&mut behaviour, &bootstrap_peers);
-        let current_retry_duration = min(
-            BOOTSTRAP_DIAL_SLEEP_MAX,
-            Duration::from_millis(BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS.pow(i)),
-        );
-        assert_no_event_happens_before_duration(&mut behaviour, current_retry_duration).await;
-        consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-        assert_no_event(&mut behaviour);
-    }
-
-    accept_all_dial_attempts(&mut behaviour, &bootstrap_peers, 0);
-    consume_found_listen_address_events(&mut behaviour, bootstrap_peers.clone()).await;
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_MAX * 2).await;
-
-    close_all_connections(&mut behaviour, &bootstrap_peers, 0);
-    consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_MAX * 2).await;
-
-    for i in 1..=NUMBER_OF_DIAL_RETRIES {
-        fail_all_dial_attempts(&mut behaviour, &bootstrap_peers);
-        let current_retry_duration = min(
-            BOOTSTRAP_DIAL_SLEEP_MAX,
-            Duration::from_millis(BOOTSTRAP_DIAL_SLEEP_BASE_MILLIS.pow(i)),
-        );
-        assert_no_event_happens_before_duration(&mut behaviour, current_retry_duration).await;
-        consume_dial_events(&mut behaviour, bootstrap_peers.clone()).await;
-        assert_no_event(&mut behaviour);
-    }
 }
 
 #[rstest]
@@ -396,12 +218,8 @@ async fn bootstrapping_does_not_redial_when_one_connection_closes(
 ) {
     let (bootstrap_peers, mut behaviour) = make_and_connect_bootstrap_nodes(peer_count).await;
 
-    assert_no_event(&mut behaviour);
-    accept_all_dial_attempts(&mut behaviour, &bootstrap_peers, 0);
-    assert_no_event(&mut behaviour);
-
     close_all_connections(&mut behaviour, &bootstrap_peers, 1);
-    assert_no_event_happens_before_duration(&mut behaviour, BOOTSTRAP_DIAL_SLEEP_MAX * 2).await;
+    assert_no_event(&mut behaviour);
 }
 
 #[tokio::test]
@@ -411,9 +229,28 @@ async fn does_not_dial_self() {
     let bootstrap_peers =
         vec![(local_peer_id, Multiaddr::empty()), (remote_peer_id, Multiaddr::empty())];
 
-    let mut behaviour = BootstrappingBehaviour::new(local_peer_id, CONFIG, bootstrap_peers.clone());
-    let expected_bootstrap_peers_to_be_dialed = vec![(remote_peer_id, Multiaddr::empty())];
-    consume_dial_events(&mut behaviour, expected_bootstrap_peers_to_be_dialed).await;
+    let mut behaviour = BootstrappingBehaviour::new(local_peer_id, bootstrap_peers);
+    let expected_peers = vec![(remote_peer_id, Multiaddr::empty())];
+    consume_request_dial_events(&mut behaviour, expected_peers).await;
+    assert_no_event(&mut behaviour);
+}
+
+#[rstest]
+#[tokio::test]
+async fn rapid_reconnect_does_not_emit_stale_request_dial(
+    #[values(1, 2, 3, 4, 5, 6, 7)] peer_count: usize,
+) {
+    let (bootstrap_peers, mut behaviour) = make_and_connect_bootstrap_nodes(peer_count).await;
+
+    // Simulate rapid disconnect+reconnect in the same poll cycle:
+    // ConnectionClosed queues a RequestDial, then ConnectionEstablished should cancel it.
+    for (peer_id, address) in &bootstrap_peers {
+        close_connection(&mut behaviour, *peer_id, address.clone(), 0);
+        accept_dial_attempt(&mut behaviour, *peer_id, 0);
+    }
+
+    // Only FoundListenAddresses should remain — no stale RequestDial events.
+    consume_found_listen_address_events(&mut behaviour, bootstrap_peers).await;
     assert_no_event(&mut behaviour);
 }
 
@@ -421,7 +258,7 @@ async fn does_not_dial_self() {
 async fn returns_pending_if_empty_bootstrap_nodes() {
     let local_peer_id = get_peer_id(LOCAL_PEER_ID_INDEX);
 
-    let mut behaviour = BootstrappingBehaviour::new(local_peer_id, CONFIG, vec![]);
+    let mut behaviour = BootstrappingBehaviour::new(local_peer_id, vec![]);
 
     let mut cx = Context::from_waker(Waker::noop());
     assert_matches!(behaviour.poll(&mut cx), Poll::Pending);
