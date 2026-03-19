6b7806fd6330bcde197d3949ef8796b5e8f92fc6
Nimrod Weiss <nimrod@starkware.co>
2026-03-19 08:32:19 +0200

    starknet_patricia_storage: clone for cached storage

--- crates/starknet_patricia_storage/src/map_storage.rs
+++ crates/starknet_patricia_storage/src/map_storage.rs
@@ -262,6 +262,20 @@ impl<S: Storage> CachedStorage<S> {
     }
 }
 
+impl<S: Storage + Clone> Clone for CachedStorage<S> {
+    fn clone(&self) -> Self {
+        Self {
+            storage: self.storage.clone(),
+            cache: self.cache.clone(), // This clone is cheap.
+            cache_on_write: self.cache_on_write,
+            reads: self.reads.clone(),
+            cached_reads: self.cached_reads.clone(),
+            writes: self.writes.clone(),
+            include_inner_stats: self.include_inner_stats,
+        }
+    }
+}
+
 impl<S: Storage + ImmutableReadOnlyStorage> ImmutableReadOnlyStorage for CachedStorage<S> {
     async fn get(&self, key: &DbKey) -> PatriciaStorageResult<Option<DbValue>> {
         self.reads.fetch_add(1, Ordering::Relaxed);
@@ -430,7 +444,17 @@ impl<S: Storage> Storage for CachedStorage<S> {
     }
 
     fn get_async_self(&self) -> Option<impl AsyncStorage> {
-        // Need a concrete Option type.
-        None::<NullStorage>
+        let inner_async_storage = self.storage.get_async_self()?;
+        Some(CachedStorage {
+            storage: inner_async_storage,
+            cache: self.cache.clone(), // This clone is cheap.
+            cache_on_write: self.cache_on_write,
+            reads: self.reads.clone(),
+            cached_reads: self.cached_reads.clone(),
+            writes: self.writes.clone(),
+            include_inner_stats: self.include_inner_stats,
+        })
     }
 }
+
+impl<S: AsyncStorage> AsyncStorage for CachedStorage<S> {}
