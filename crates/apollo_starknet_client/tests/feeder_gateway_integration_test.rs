cf7ee1162fc9160583ed34cfb8c1cebb3556af19
Itay-Tsabary-Starkware <106665835+Itay-Tsabary-Starkware@users.noreply.github.com>
2026-03-22 10:28:58 +0200

    apollo_starknet_client: disable broken tests (#13380)

--- crates/apollo_starknet_client/tests/feeder_gateway_integration_test.rs
+++ crates/apollo_starknet_client/tests/feeder_gateway_integration_test.rs
@@ -67,8 +67,10 @@ fn into_block_number_vec<T: Serialize>(obj: T) -> Vec<BlockNumber> {
         .collect()
 }
 
-#[tokio::test]
-async fn test_integration_testnet() {
+// TODO(Shahak/Matan): redesign these tests to avoid being externally-dependent on env endpoints.
+#[ignore = "The integration endpoint has been upgraded to an incompatible version."]
+// #[tokio::test]
+async fn _test_integration_testnet() {
     let _ = simple_logger::init_with_env();
     let integration_testnet_data = TestEnvData {
         url: "https://feeder.integration-sepolia.starknet.io".to_owned(),
@@ -98,6 +100,8 @@ async fn test_integration_testnet() {
     run(integration_testnet_data).await;
 }
 
+// TODO(Shahak/Matan): redesign these tests to avoid being externally-dependent on env endpoints.
+#[ignore = "The testnet endpoint has been upgraded to an incompatible version."]
 #[tokio::test]
 async fn test_alpha_testnet() {
     let _ = simple_logger::init_with_env();
