2d29e537e4b060186e6f33e80a345e9405999b83
Arnon Hod <arnon@starkware.co>
2026-03-26 12:42:15 +0200

    apollo_infra: create component clients with channel

---
+++ crates/apollo_infra/src/component_client/local_component_channel_client_test.rs
@@ -0,0 +1,28 @@
+use tokio::sync::watch;
+
+use super::LocalComponentChannelClient;
+use crate::component_definitions::ComponentChannelClient;
+
+#[test]
+fn get_value_returns_initial_value() {
+    let (_, value_rx) = watch::channel(42u32);
+    let client = LocalComponentChannelClient::new(value_rx);
+    assert_eq!(client.get_value(), 42);
+}
+
+#[test]
+fn get_value_reflects_updated_value() {
+    let (value_tx, value_rx) = watch::channel(1u32);
+    let client = LocalComponentChannelClient::new(value_rx);
+    value_tx.send(99).unwrap();
+    assert_eq!(client.get_value(), 99);
+}
+
+#[test]
+fn cloned_client_shares_same_channel() {
+    let (value_tx, value_rx) = watch::channel(0u32);
+    let client_a = LocalComponentChannelClient::new(value_rx);
+    let client_b = client_a.clone();
+    value_tx.send(7).unwrap();
+    assert_eq!(client_a.get_value(), client_b.get_value());
+}
