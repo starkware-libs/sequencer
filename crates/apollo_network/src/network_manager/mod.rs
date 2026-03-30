b837b172e5114dfb3ee2dda993e9743fd93c9231
Andrew Luka <andrew.l@starkware.co>
2026-03-30 14:50:26 +0300

    apollo_network: move e2e discovery test to integration tests

--- crates/apollo_network/src/network_manager/mod.rs
+++ crates/apollo_network/src/network_manager/mod.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-pub(crate) mod swarm_trait;
-#[cfg(not(test))]
-mod swarm_trait;
+pub mod swarm_trait;
 #[cfg(test)]
 mod test;
 #[cfg(any(test, feature = "testing"))]
@@ -196,7 +193,7 @@ impl<SwarmT: SwarmTrait> GenericNetworkManager<SwarmT> {
 
     // TODO(shahak): remove the advertised_multiaddr arg once we manage external addresses
     // in a behaviour.
-    pub(crate) fn generic_new(
+    pub fn generic_new(
         mut swarm: SwarmT,
         advertised_multiaddr: Option<Multiaddr>,
         metrics: Option<NetworkMetrics>,
