34cbc7e203c7bb8026e86aa9d69db478f75ee597
nadin-Starkware <nadin@starkware.co>
2026-04-09 11:58:06 +0300

    apollo_signature_manager: remove KeyStore generic from SignatureManager

--- crates/apollo_signature_manager/src/lib.rs
+++ crates/apollo_signature_manager/src/lib.rs
@@ -4,43 +4,8 @@ pub mod communication;
 pub mod metrics;
 pub mod signature_manager;
 
-use std::ops::Deref;
+pub use crate::signature_manager::{LocalKeyStore, SignatureManager};
 
-use apollo_infra::component_definitions::ComponentStarter;
-use async_trait::async_trait;
-
-use crate::signature_manager::{LocalKeyStore, SignatureManager as GenericSignatureManager};
-
-#[derive(Clone, Debug)]
-pub struct LocalKeyStoreSignatureManager(pub GenericSignatureManager<LocalKeyStore>);
-
-impl LocalKeyStoreSignatureManager {
-    pub fn new() -> Self {
-        Self(GenericSignatureManager::new(LocalKeyStore::new_for_testing()))
-    }
-}
-
-impl Default for LocalKeyStoreSignatureManager {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Deref for LocalKeyStoreSignatureManager {
-    type Target = GenericSignatureManager<LocalKeyStore>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub use LocalKeyStoreSignatureManager as SignatureManager;
-
-// TODO(Elin): understand how key store would look in production and better define the way the
-// signature manager is created.
 pub fn create_signature_manager() -> SignatureManager {
-    SignatureManager::new()
+    SignatureManager::new(LocalKeyStore::new_for_testing())
 }
-
-#[async_trait]
-impl ComponentStarter for SignatureManager {}
