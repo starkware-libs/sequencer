047159c24276537f620a7adf842de0be267f8898
Andrew Luka <andrew.l@starkware.co>
2026-04-19 18:01:45 +0300

    apollo_consensus_orchestrator: add SNIP-35 fee_proposal validation

--- crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
+++ crates/apollo_consensus_orchestrator/src/sequencer_consensus_context.rs
@@ -834,6 +834,7 @@ impl ConsensusContext for SequencerConsensusContext {
                         .map(GasPrice)
                         .unwrap_or(self.l2_gas_price),
                     starknet_version: StarknetVersion::LATEST,
+                    fee_actual: compute_fee_actual(&self.fee_proposals_window, init.height),
                 };
                 self.validate_current_round_proposal(
                     init,
@@ -1100,6 +1101,7 @@ impl ConsensusContext for SequencerConsensusContext {
                 .map(GasPrice)
                 .unwrap_or(self.l2_gas_price),
             starknet_version: StarknetVersion::LATEST,
+            fee_actual: compute_fee_actual(&self.fee_proposals_window, init.height),
         };
         self.validate_current_round_proposal(
             init,
