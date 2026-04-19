e8015184bb8aa23adf6404568260fd3cc666cade
Andrew Luka <andrew.l@starkware.co>
2026-04-19 17:25:37 +0300

    starknet_api: add fee_proposal to block hash calculator

--- crates/starknet_api/src/block_hash/block_hash_calculator_test.rs
+++ crates/starknet_api/src/block_hash/block_hash_calculator_test.rs
@@ -50,7 +50,6 @@ macro_rules! test_hash_changes {
                 $(
                     $partial_block_hash_components_field: $partial_block_hash_components_value,
                 )*
-                ..Default::default()
             };
             let state_root = $state_root;
             let previous_block_hash = $previous_block_hash;
@@ -120,7 +119,7 @@ async fn test_block_hash_regression(
             felt!("0x3d6174623c812f5dc03fa3faa07c42c06fd90ad425629ee5f39e149df65c3ca")
         }
         BlockHashVersion::V0_14_3 => {
-            felt!("0x477e98ed084a0274e4510ab27327f08235d8e4fdb7506e46e92e9ab0c5ea459")
+            felt!("0x4364050fc0f671d5f4241a619ca2c74ddec796894f9bd2ef36476f88837af9b")
         }
     };
 
@@ -234,7 +233,6 @@ fn extract_event_count_from_concatenated_counts_test(
 }
 
 /// Test that if one of the input to block hash changes, the hash changes.
-// TODO(AndrewL): add fee_proposal to the test.
 #[test]
 fn change_field_of_hash_input() {
     // Set non-default values for the header and the commitments fields. Test that changing any of
@@ -256,7 +254,8 @@ fn change_field_of_hash_input() {
             },
             l2_gas_price: GasPricePerToken { price_in_fri: 1_u8.into(), price_in_wei: 1_u8.into() },
             sequencer: SequencerContractAddress(ContractAddress::from(1_u128)),
-            timestamp: BlockTimestamp(1)
+            timestamp: BlockTimestamp(1),
+            fee_proposal: GasPrice(1)
         },
         state_root: GlobalRoot(Felt::ONE),
         previous_block_hash: BlockHash(Felt::ONE)
