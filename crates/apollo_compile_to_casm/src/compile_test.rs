fdac2099d410b2d730b9a5ccb1a612f69c8f39aa
Dori Medini <dori@starkware.co>
2026-04-20 15:53:05 +0300

    chore: fix conflicts
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_compile_to_casm/src/compile_test.rs
+++ crates/apollo_compile_to_casm/src/compile_test.rs
@@ -234,6 +234,8 @@ fn memory_limit_error_message() {
     };
 
     let expected =
-        expect!["memory allocation of 49152 bytes failed (process terminated by signal 6)"];
+        expect![[r#"
+            memory allocation of 142960 bytes failed
+            note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace (process terminated by signal 6)"#]];
     expected.assert_eq(&error_message);
 }
