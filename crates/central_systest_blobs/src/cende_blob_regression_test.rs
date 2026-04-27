0e5db51f22685a1720e7f5e57ab8d3e895d86703
Dori Medini <dori@starkware.co>
2026-04-27 17:55:48 +0300

    central_systest_blobs: first two bootstrap declares

--- crates/central_systest_blobs/src/cende_blob_regression_test.rs
+++ crates/central_systest_blobs/src/cende_blob_regression_test.rs
@@ -14,18 +14,39 @@ use blockifier::blockifier_versioned_constants::VersionedConstants;
 use blockifier::bouncer::BouncerConfig;
 use blockifier::context::{BlockContext, ChainInfo};
 use blockifier::state::cached_state::StateMaps;
+use blockifier::test_utils::contracts::FeatureContractTrait;
 use blockifier::test_utils::dict_state_reader::DictStateReader;
+use blockifier_test_utils::cairo_versions::{CairoVersion, RunnableCairo1};
 use blockifier_test_utils::contracts::FeatureContract;
 use expect_test::expect_file;
+use mockall::predicate::eq;
 use starknet_api::block::{BlockHash, BlockHashAndNumber, BlockInfo, BlockNumber, BlockTimestamp};
 use starknet_api::block_hash::block_hash_calculator::PartialBlockHashComponents;
 use starknet_api::consensus_transaction::InternalConsensusTransaction;
 use starknet_api::contract_address;
-use starknet_api::core::{ChainId, OsChainInfo};
-use starknet_api::executable_transaction::AccountTransaction as ExecutableAccountTx;
+use starknet_api::contract_class::compiled_class_hash::HashVersion;
+use starknet_api::core::{ChainId, Nonce, OsChainInfo};
+use starknet_api::data_availability::DataAvailabilityMode;
+use starknet_api::executable_transaction::{
+    AccountTransaction as ExecutableAccountTx,
+    DeclareTransaction as ExecutableDeclareTransaction,
+};
 use starknet_api::hash::StateRoots;
+use starknet_api::rpc_transaction::{
+    InternalRpcDeclareTransactionV3,
+    InternalRpcTransaction,
+    InternalRpcTransactionWithoutTxHash,
+};
 use starknet_api::state::ThinStateDiff;
 use starknet_api::test_utils::TEST_SEQUENCER_ADDRESS;
+use starknet_api::transaction::fields::{
+    AccountDeploymentData,
+    AllResourceBounds,
+    PaymasterData,
+    Tip,
+    TransactionSignature,
+};
+use starknet_api::transaction::{DeclareTransaction, TransactionHasher, TransactionVersion};
 use starknet_patricia_storage::map_storage::MapStorage;
 
 const N_TXS_PER_BLOCK: usize = 1;
@@ -44,17 +65,87 @@ type TxPair = (ExecutableAccountTx, InternalConsensusTransaction);
 // Tx generation
 // =====================
 
-#[expect(dead_code)]
 fn boostrap_declare_tx(
-    _class_manager: &mut MockClassManagerClient,
-    _contract: FeatureContract,
+    class_manager: &mut MockClassManagerClient,
+    contract: FeatureContract,
 ) -> TxPair {
-    unimplemented!()
+    let sender_address = ExecutableDeclareTransaction::bootstrap_address();
+    let sierra = contract.get_sierra();
+    let class_hash = sierra.calculate_class_hash();
+    let compiled_class_hash = contract.get_compiled_class_hash(&HashVersion::V2);
+    let resource_bounds = AllResourceBounds::new_unlimited_gas_no_fee_enforcement();
+    let nonce = Nonce::default();
+
+    // Create internal tx.
+    let internal_declare_without_hash = InternalRpcDeclareTransactionV3 {
+        sender_address,
+        nonce,
+        class_hash,
+        compiled_class_hash,
+        resource_bounds,
+        signature: TransactionSignature::default(),
+        tip: Tip::default(),
+        paymaster_data: PaymasterData::default(),
+        account_deployment_data: AccountDeploymentData::default(),
+        nonce_data_availability_mode: DataAvailabilityMode::L1,
+        fee_data_availability_mode: DataAvailabilityMode::L1,
+    };
+    let tx_hash = internal_declare_without_hash
+        .calculate_transaction_hash(&CHAIN_ID, &TransactionVersion::THREE)
+        .unwrap();
+    let internal = InternalConsensusTransaction::RpcTransaction(InternalRpcTransaction {
+        tx: InternalRpcTransactionWithoutTxHash::Declare(internal_declare_without_hash.clone()),
+        tx_hash,
+    });
+
+    // Create executable tx.
+    let executable = ExecutableDeclareTransaction::create(
+        DeclareTransaction::V3(internal_declare_without_hash.into()),
+        contract.get_class_info(),
+        &CHAIN_ID,
+    )
+    .unwrap();
+
+    // Mock the class manager.
+    // The class manager methods may not be called if a blob is not created with this declare.
+    class_manager
+        .expect_get_sierra()
+        .with(eq(class_hash))
+        .times(..=1)
+        .returning(move |_| Ok(Some(sierra.clone())));
+    class_manager
+        .expect_get_executable()
+        .with(eq(class_hash))
+        .times(..=1)
+        .returning(move |_| Ok(Some(contract.get_class())));
+
+    // Return the transactions.
+    (executable.into(), internal)
 }
 
 fn make_txs() -> (MockClassManagerClient, Vec<TxPair>) {
-    // TODO(Dori): implement.
-    (MockClassManagerClient::default(), vec![])
+    // Create the list of transactions to be included in the blobs:
+    // 1. bootstrap declare of an ERC20 contract.
+    // 2. bootstrap declare of an account with real validate.
+    // TODO(Dori): the rest of the txs.
+    // 3. deploy account (with zero fees).
+    // 4. deploy ERC20 contract from the account (with zero fees), while minting some tokens to the
+    //    sender account.
+    // (from this point - all txs include non-zero fees, and no more bootstrap declares)
+    // 5. declare the test contract.
+    // 6. deploy the test contract.
+    // 7. deploy another instance of the test contract.
+    // 8. invoke the test contract: something with a state change.
+    // 9. invoke the test contract: test syscalls.
+
+    let mut class_manager = MockClassManagerClient::new();
+    let erc20_contract = FeatureContract::ERC20(CairoVersion::Cairo1(RunnableCairo1::Casm));
+    let account_with_real_validate = FeatureContract::AccountWithRealValidate(RunnableCairo1::Casm);
+
+    let erc20_declare_tx = boostrap_declare_tx(&mut class_manager, erc20_contract);
+    let account_with_real_validate_declare_tx =
+        boostrap_declare_tx(&mut class_manager, account_with_real_validate);
+    (class_manager, vec![erc20_declare_tx, account_with_real_validate_declare_tx])
 }
 
 // =====================
@@ -163,23 +254,18 @@ async fn make_data() -> (Vec<AerospikeBlob>, CendeWritePreconfirmedBlock) {
     let shared_class_manager = Arc::new(class_manager);
     let mut state = DictStateReader::default();
 
-    // TODO(Dori): remove this case, it should never happen when the test is done.
-    if transactions.is_empty() {
-        (vec![], make_preconfirmed_block(0, &mut state, &[]))
-    } else {
-        let block_iterator = transactions.chunks(N_TXS_PER_BLOCK).enumerate().collect::<Vec<_>>();
-        // Split the block iterator into two iterators: one for the blocks to be committed, and one
-        // for the last block.
-        let (blocks_to_commit, last_block) = block_iterator.split_at(block_iterator.len() - 1);
-        let (last_block_number, last_block_txs) = last_block.last().unwrap();
+    let block_iterator = transactions.chunks(N_TXS_PER_BLOCK).enumerate().collect::<Vec<_>>();
+    // Split the block iterator into two iterators: one for the blocks to be committed, and one
+    // for the last block.
+    let (blocks_to_commit, last_block) = block_iterator.split_at(block_iterator.len() - 1);
+    let (last_block_number, last_block_txs) = last_block.last().unwrap();
 
-        let blobs = make_blobs(blocks_to_commit, &mut state, shared_class_manager.clone()).await;
-        // For the last block, create a preconfirmed block.
-        let preconfirmed_block =
-            make_preconfirmed_block(*last_block_number, &mut state, last_block_txs);
+    let blobs = make_blobs(blocks_to_commit, &mut state, shared_class_manager.clone()).await;
+    // For the last block, create a preconfirmed block.
+    let preconfirmed_block =
+        make_preconfirmed_block(*last_block_number, &mut state, last_block_txs);
 
-        (blobs, preconfirmed_block)
-    }
+    (blobs, preconfirmed_block)
 }
 
 // =====================
