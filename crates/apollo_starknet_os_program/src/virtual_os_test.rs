a18639e846487a7888dd7a08c36265e664d38981
Dori Medini <dori@starkware.co>
2026-05-11 15:14:09 +0300

    apollo_starknet_os_program: update cairo-lang to 0.14.3a1
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/apollo_starknet_os_program/src/virtual_os_test.rs
+++ crates/apollo_starknet_os_program/src/virtual_os_test.rs
@@ -19,11 +19,11 @@ fn test_virtual_os_swapped_files() {
 #[test]
 fn test_program_bytecode_lengths() {
     expect![[r#"
-        15610
+        15660
     "#]]
     .assert_debug_eq(&OS_PROGRAM.data_len());
     expect![[r#"
-        10214
+        10261
     "#]]
     .assert_debug_eq(&VIRTUAL_OS_PROGRAM.data_len());
 }
