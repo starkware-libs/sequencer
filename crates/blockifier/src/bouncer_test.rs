3b7106030302c7244b72d68ffe166beccc69e548
Dori Medini <dori@starkware.co>
2026-05-17 22:14:10 +0300

    blockifier: sha512 syscall

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -843,11 +843,11 @@ fn class_hash_migration_data_from_state(
 
     if should_migrate {
         expect![[r#"
-            104123075
+            105991224
         "#]]
         .assert_debug_eq(&migration_sierra_gas.0);
         expect![[r#"
-            225098120
+            229141677
         "#]]
         .assert_debug_eq(&migration_proving_gas.0);
     } else {
