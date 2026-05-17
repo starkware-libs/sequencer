22f6a1d3639e2988789ed497e4092d093baae8a6
dorimedini-starkware <dori@starkware.co>
2026-05-17 23:51:53 +0300

    blockifier: fix blake estimation offset by fixing constants (#14034)

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -804,11 +804,11 @@ fn class_hash_migration_data_from_state(
 
     if should_migrate {
         expect![[r#"
-            111245775
+            111211275
         "#]]
         .assert_debug_eq(&migration_sierra_gas.0);
         expect![[r#"
-            269417662
+            269383162
         "#]]
         .assert_debug_eq(&migration_proving_gas.0);
     } else {
