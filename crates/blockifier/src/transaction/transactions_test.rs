d24049e00db8d12bf1c71d7986e1362cbf543be8
Dori Medini <dori@starkware.co>
2026-05-21 16:08:05 +0300

    starknet_os: initial test, measure call contract resources

--- crates/blockifier/src/transaction/transactions_test.rs
+++ crates/blockifier/src/transaction/transactions_test.rs
@@ -591,7 +591,7 @@ fn add_kzg_da_resources_to_resources_mapping(
         resources: ExtendedExecutionResources::default(),
         validate_gas_consumed: 7820, // The gas consumption results from parsing the input
             // arguments.
-        execute_gas_consumed: 113920,
+        execute_gas_consumed: 113720,
     },
     CairoVersion::Cairo1(RunnableCairo1::Casm))]
 #[cfg_attr(feature = "cairo_native", case::with_cairo1_native_account(
@@ -599,7 +599,7 @@ fn add_kzg_da_resources_to_resources_mapping(
         resources: ExtendedExecutionResources::default(),
         validate_gas_consumed: 7820, // The gas consumption results from parsing the input
             // arguments.
-        execute_gas_consumed: 113920,
+        execute_gas_consumed: 113720,
     },
     CairoVersion::Cairo1(RunnableCairo1::Native)))]
 // TODO(Tzahi): Add calls to cairo1 test contracts (where gas flows to and from the inner call).
