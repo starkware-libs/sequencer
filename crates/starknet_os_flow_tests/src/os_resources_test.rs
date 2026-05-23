8be4f984e03e6fe7a26be347237ceea78b8c38da
Dori Medini <dori@starkware.co>
2026-05-23 17:49:46 +0300

    starknet_os: os resources test - add replace_class

--- crates/starknet_os_flow_tests/src/os_resources_test.rs
+++ crates/starknet_os_flow_tests/src/os_resources_test.rs
@@ -45,7 +45,7 @@ use crate::tests::NON_TRIVIAL_RESOURCE_BOUNDS;
 use crate::utils::get_class_hash_of_feature_contract;
 
 // TODO(Dori): Delete this, or at least reduce it to a minimal set of unmeasurable syscalls.
-const UNMEASURABLE_SYSCALLS: [Selector; 25] = [
+const UNMEASURABLE_SYSCALLS: [Selector; 24] = [
     Selector::DelegateCall,
     Selector::DelegateL1Handler,
     Selector::GetBlockNumber,
@@ -57,7 +57,6 @@ const UNMEASURABLE_SYSCALLS: [Selector; 25] = [
     Selector::GetTxSignature,
     Selector::Sha512ProcessBlock,
     Selector::LibraryCallL1Handler,
-    Selector::ReplaceClass,
     Selector::Secp256k1Add,
     Selector::Secp256k1GetPointFromX,
     Selector::Secp256k1GetXy,
