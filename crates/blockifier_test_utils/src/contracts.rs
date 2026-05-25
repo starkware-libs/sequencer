7ca8faf5daf3e6d692f7c3f732088098343ef1d9
dorimedini-starkware <dori@starkware.co>
2026-05-25 18:07:05 +0300

    blockifier_test_utils: add OS resources measurement contract (#14126)

--- crates/blockifier_test_utils/src/contracts.rs
+++ crates/blockifier_test_utils/src/contracts.rs
@@ -76,6 +76,7 @@ const FUZZ_TEST_BASE: u32 = 19 * CLASS_HASH_BASE;
 const FUZZ_TEST2_BASE: u32 = 20 * CLASS_HASH_BASE;
 const FUZZ_TEST_ORCHESTRATOR_BASE: u32 = 21 * CLASS_HASH_BASE;
 const ACCOUNT_WITH_REAL_VALIDATE_BASE: u32 = 22 * CLASS_HASH_BASE;
+const OS_RESOURCES_TEST_CONTRACT_BASE: u32 = 23 * CLASS_HASH_BASE;
 
 // Contract names.
 const ACCOUNT_LONG_VALIDATE_NAME: &str = "account_with_long_validate";
@@ -99,6 +100,7 @@ const FUZZ_TEST_NAME: &str = "fuzz_revert";
 const FUZZ_TEST2_NAME: &str = "fuzz_revert_2";
 const FUZZ_TEST_ORCHESTRATOR_NAME: &str = "fuzz_revert_orchestrator";
 const ACCOUNT_WITH_REAL_VALIDATE_NAME: &str = "account_with_real_validate";
+const OS_RESOURCES_TEST_CONTRACT_NAME: &str = "os_resources_test_contract";
 // ERC20 contract is in a unique location.
 const ERC20_CAIRO0_CONTRACT_SOURCE_PATH: &str =
     "./resources/ERC20/ERC20_Cairo0/ERC20_without_some_syscalls/ERC20/ERC20.cairo";
@@ -141,6 +143,7 @@ pub enum FeatureContract {
     FuzzTest2(RunnableCairo1),
     FuzzTestOrchestrator(RunnableCairo1),
     AccountWithRealValidate(RunnableCairo1),
+    OsResourcesTest(RunnableCairo1),
 }
 
 impl FeatureContract {
@@ -167,9 +170,8 @@ impl FeatureContract {
             | Self::MockStakingContract(runnable_version)
             | Self::FuzzTest2(runnable_version)
             | Self::FuzzTestOrchestrator(runnable_version)
-            | Self::AccountWithRealValidate(runnable_version) => {
-                CairoVersion::Cairo1(*runnable_version)
-            }
+            | Self::AccountWithRealValidate(runnable_version)
+            | Self::OsResourcesTest(runnable_version) => CairoVersion::Cairo1(*runnable_version),
         }
     }
 
@@ -189,7 +191,8 @@ impl FeatureContract {
             | Self::MockStakingContract(rv)
             | Self::FuzzTest2(rv)
             | Self::FuzzTestOrchestrator(rv)
-            | Self::AccountWithRealValidate(rv) => match version {
+            | Self::AccountWithRealValidate(rv)
+            | Self::OsResourcesTest(rv) => match version {
                 CairoVersion::Cairo0 => panic!("{self:?} must be Cairo1"),
                 CairoVersion::Cairo1(runnable) => *rv = runnable,
             },
@@ -331,6 +334,7 @@ impl FeatureContract {
                 Self::FuzzTest2(_) => FUZZ_TEST2_BASE,
                 Self::FuzzTestOrchestrator(_) => FUZZ_TEST_ORCHESTRATOR_BASE,
                 Self::AccountWithRealValidate(_) => ACCOUNT_WITH_REAL_VALIDATE_BASE,
+                Self::OsResourcesTest(_) => OS_RESOURCES_TEST_CONTRACT_BASE,
             }
     }
 
@@ -365,6 +369,7 @@ impl FeatureContract {
             Self::FuzzTest2(_) => FUZZ_TEST2_NAME,
             Self::FuzzTestOrchestrator(_) => FUZZ_TEST_ORCHESTRATOR_NAME,
             Self::AccountWithRealValidate(_) => ACCOUNT_WITH_REAL_VALIDATE_NAME,
+            Self::OsResourcesTest(_) => OS_RESOURCES_TEST_CONTRACT_NAME,
             Self::ERC20(_) => unreachable!(),
         }
     }
@@ -475,7 +480,8 @@ impl FeatureContract {
                     | FeatureContract::FuzzTest(_)
                     | FeatureContract::FuzzTest2(_)
                     | FeatureContract::FuzzTestOrchestrator(_)
-                    | FeatureContract::AccountWithRealValidate(_) => None,
+                    | FeatureContract::AccountWithRealValidate(_)
+                    | FeatureContract::OsResourcesTest(_) => None,
                     FeatureContract::ERC20(_) | FeatureContract::Experimental => unreachable!(),
                 };
                 cairo0_compile(self.get_source_path(), extra_arg, false)
@@ -524,7 +530,8 @@ impl FeatureContract {
             | Self::MockStakingContract(_)
             | Self::FuzzTest2(_)
             | Self::FuzzTestOrchestrator(_)
-            | Self::AccountWithRealValidate(_) => {
+            | Self::AccountWithRealValidate(_)
+            | Self::OsResourcesTest(_) => {
                 #[cfg(not(feature = "cairo_native"))]
                 {
                     vec![*self]
