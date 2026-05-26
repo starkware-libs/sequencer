354b15cd7f7696e3d3d0446dd0b71f1a5553f394
ArielElp <86294909+ArielElp@users.noreply.github.com>
2026-05-26 11:28:34 +0300

    starknet_committer: fetch patricia paths tests (#13959)

--- crates/starknet_committer/src/db/index_db/create_index_tree_test.rs
+++ crates/starknet_committer/src/db/index_db/create_index_tree_test.rs
@@ -1,11 +1,7 @@
 use rstest::rstest;
 use rstest_reuse::apply;
-use starknet_api::hash::HashOutput;
 use starknet_patricia::patricia_merkle_tree::external_test_utils::MockLeaf;
-use starknet_patricia::patricia_merkle_tree::node_data::inner_node::NodeData;
-use starknet_patricia::patricia_merkle_tree::updated_skeleton_tree::hash_function::TreeHashFunction;
 use starknet_patricia_storage::db_object::EmptyKeyContext;
-use starknet_types_core::felt::Felt;
 
 use crate::db::create_original_skeleton_tests::{
     create_tree_cases,
@@ -15,16 +11,6 @@ use crate::db::create_original_skeleton_tests::{
 use crate::db::index_db::test_utils::convert_facts_db_to_index_db;
 use crate::db::index_db::IndexNodeLayout;
 use crate::hash_function::mock_hash::MockTreeHashFunction;
-
-impl TreeHashFunction<MockLeaf> for MockTreeHashFunction {
-    fn compute_leaf_hash(leaf_data: &MockLeaf) -> HashOutput {
-        HashOutput(leaf_data.0)
-    }
-
-    fn compute_node_hash(_node_data: &NodeData<MockLeaf, HashOutput>) -> HashOutput {
-        HashOutput(Felt::ZERO)
-    }
-}
 
 #[apply(create_tree_cases)]
 #[rstest]
