03ed98ed919191462aa2b6a41451b68dc2095148
github-action-benchmark <github@users.noreply.github.com>
2026-05-28 08:57:37 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9c562b3d76cd87e1c9ac1b4ea84f5a5399fbb1f3

---
+++ dev/bench/main-v0.14.3/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779958657174,
+  "repoUrl": "https://github.com/starkware-libs/sequencer",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dori@starkware.co",
+            "name": "dorimedini-starkware",
+            "username": "dorimedini-starkware"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c562b3d76cd87e1c9ac1b4ea84f5a5399fbb1f3",
+          "message": "release: set workspace crate versions to 0.19.0-rc.0 (#14235)\n\nSigned-off-by: Dori Medini <dori@starkware.co>",
+          "timestamp": "2026-05-28T08:45:32Z",
+          "tree_id": "eb670a561fcdae03d82d2edad48957a07c31b82b",
+          "url": "https://github.com/starkware-libs/sequencer/commit/9c562b3d76cd87e1c9ac1b4ea84f5a5399fbb1f3"
+        },
+        "date": 1779958656612,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "full_committer_flow",
+            "value": 859.91449225,
+            "unit": "ms"
+          },
+          {
+            "name": "tree_computation_flow",
+            "value": 1346.1030244,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
